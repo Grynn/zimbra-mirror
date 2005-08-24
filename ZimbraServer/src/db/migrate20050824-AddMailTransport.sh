72014f9df60230c03433575be9a7dacc0cba6a49
 Unknown Perforce User <anandp>
2005-08-24 17:08:33 +0000

    http://bugzilla.liquidsys.com/show_bug.cgi?id=2214 bug 2214: need zimbraMailTransport
    script to add zimbraMailTransport attribute to all accounts.
    Works by just setting zimbraMailHost to what it already is -
    the callback on zimbraMailHost in turn sets zimbraMailTransport.

---
+++ ZimbraServer/src/db/migrate20050824-AddMailTransport.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+accounts=`zmprov gaa`
+
+for a in $accounts; do
+    mh=`zmprov ga ax | grep '^zimbraMailHost' | awk -F: '{ print $2; }'`
+    cmd="zmprov ma $a zimbraMailHost $mh"
+    if [ "x$1" = "x-f" ]; then
+        echo "Running: $cmd"
+        $cmd
+    else
+        echo "WillRun: $cmd"
+    fi
+done
