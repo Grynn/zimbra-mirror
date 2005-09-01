daca401e43b816336ca5d1caba752638fee30224
 Unknown Perforce User <aclark>
2005-09-01 22:57:36 +0000

    http://bugzilla.liquidsys.com/show_bug.cgi?id=3842 Bug 3842: Fixed tooltip sizing issue when tip width is wider than tooltip content. Also moved tooltip "popping" code out of DwtToolTip and into DwtControl in preparation for other types of delayed popups. And updated "hover" border def to allow element identifier to be passed in when border is created.

--- Ajax/WebRoot/js/dwt/widgets/DwtToolTip.js
+++ Ajax/WebRoot/js/dwt/widgets/DwtToolTip.js
@@ -30,15 +30,7 @@ function DwtToolTip(shell, className, dialog) {
 	this._div = shell.getDocument().createElement("div");
 	this._div.className = className || "DwtToolTip";
 	this._div.style.position = DwtControl.ABSOLUTE_STYLE;
-//	this._div.style.padding = "2px";
-//	this._div.style.whiteSpace = "nowrap";
 	this.shell.getHtmlElement().appendChild(this._div);
-	this._popupAction = new AjxTimedAction();
-	this._popupAction.method = DwtToolTip.prototype._popupToolTip;
-	this._popdownAction = new AjxTimedAction();
-	this._popdownAction.method = DwtToolTip.prototype._popdownToolTip;
-	this._popupActionId = -1;
-	this._popdownActionId = -1;
 	Dwt.setZIndex(this._div, Dwt.Z_HIDDEN);
 }
 
@@ -57,73 +49,16 @@ function() {
 DwtToolTip.prototype.setContent =
 function(content) {
 	if (content) {
-		var divContent = DwtBorder.getBorderStartHtml("hover") + 
+		var substitutions = { id: "tooltip" };
+		var divContent = DwtBorder.getBorderStartHtml("hover", substitutions) + 
 							content + 
-						 DwtBorder.getBorderEndHtml("hover");
+						 DwtBorder.getBorderEndHtml("hover", substitutions);
 		this._div.innerHTML = divContent;
 	}		
 	this._content = content;
 }
 	
-DwtToolTip.prototype.mouseOver =
-function(x, y, delay) {
-	if (this._popdownActionId != -1) {
-		AjxTimedAction.cancelAction(this._popdownActionId);
-		this._popdownActionId = -1;
-	}
-	if (this._popupActionId != -1) {
-		AjxTimedAction.cancelAction(this._popupActionId);
-		this._popupActionId = -1;
-	}
-	if (Dwt.getZIndex(this._div) == Dwt.Z_HIDDEN) {
-		if (this._content != null && this._popupActionId == -1) {
-			delay = (delay == null) ? DwtToolTip._TOOLTIP_DELAY : (delay > 0) ? delay : 0;
-			this._popupAction.params.removeAll();
-			this._popupAction.params.add(x);
-			this._popupAction.params.add(y);
-			this._popupAction.obj = this;
-		 	this._popupActionId = AjxTimedAction.scheduleAction(this._popupAction, delay);
-		}
-	}
-}
-
-DwtToolTip.prototype.mouseMove =
-function(delay, x, y) {
-	if (this._popupActionId != -1) {
-		AjxTimedAction.cancelAction(this._popupActionId);
-		this._popupAction.obj = this;
-		delay = (delay == null) ? DwtToolTip._TOOLTIP_DELAY : (delay > 0) ? delay : 0;
-		if (x) {
-			this._popupAction.params.removeAll();
-			this._popupAction.params.add(x);
-			this._popupAction.params.add(y);
-		}
-	 	this._popupActionId = AjxTimedAction.scheduleAction(this._popupAction, delay);
-	}
-}
-
-DwtToolTip.prototype.mouseDown =
-function() {
-	this._popdownToolTip();
-}
-
-DwtToolTip.prototype.mouseOut =
-function(delay) {
-	delay = delay || 0;
-	if (this._popdownActionId == -1) {
-		if (this._popupActionId != -1 || (this._content != null && (Dwt.getZIndex(this._div) != Dwt.Z_HIDDEN))) {	
-			this._popdownAction.obj = this;
-			delay = (delay == null) ? 50 : delay;
-			if (delay > 0)
-				this._popdownActionId = AjxTimedAction.scheduleAction(this._popdownAction, delay);
-			else 
-				this._popdownToolTip();
-		}
-	}
-}
-
-DwtToolTip.prototype._popupToolTip = 
-function(x, y) {
+DwtToolTip.prototype.popup = function(x, y) {
 	if (this._content != null) {
 		var WINDOW_GUTTER = 5;
 		var POPUP_OFFSET_X = 8;
@@ -198,6 +133,13 @@ function(x, y) {
 		}
 		//DBG.println("position: &lt;"+px+","+py+">");
 
+		// make sure popup is wide enough for tip graphic
+		if (pw - blw - brw < tw) {
+			var contentEl = document.getElementById("tooltip_contents");
+			contentEl.width = tw; // IE
+			contentEl.style.width = String(tw)+"px"; // everyone else
+		}
+		
 		// adjust popup x-location
 		if (px < WINDOW_GUTTER) {
 			px = WINDOW_GUTTER;
@@ -206,15 +148,15 @@ function(x, y) {
 			px = ww - WINDOW_GUTTER - pw;
 		}
 		//DBG.println("position: &lt;"+px+","+py+">");
-		
+
 		// adjust tip x-location
 		var tx = ex - px - tw / 2;
 		//DBG.println("tip: &lt;"+tx+","+ty+">");
-		if (tx < blw) {
-			tx = blw;
-		}
 		if (tx + tw > pw - brw) {
 			tx = pw - brw - tw;
+		}
+		if (tx < blw) {
+			tx = blw;
 		}
 		//DBG.println("tip: &lt;"+tx+","+ty+">");
 
@@ -233,21 +175,15 @@ function(x, y) {
 		//		 position.
 		this._div.style.display = "none";
 		Dwt.setLocation(this._div, px, py);
+		//Dwt.setSize(this._div, pw, ph);
 		var zIndex = this._dialog ? this._dialog.getZIndex() + Dwt.Z_INC : Dwt.Z_TOOLTIP;
 		Dwt.setZIndex(this._div, zIndex);
 		this._div.style.display = "block";
 	}
-	this._popupActionId = -1;
 }
 
-DwtToolTip.prototype._popdownToolTip = 
-function() {
-	if (this._popupActionId != -1) {
-		AjxTimedAction.cancelAction(this._popupActionId);	
-		this._popupActionId = -1;
-	}
+DwtToolTip.prototype.popdown = function() {
 	if (this._content != null) {
 		Dwt.setZIndex(this._div, Dwt.Z_HIDDEN);
 	}
-	this._popdownActionId = -1;
 }
