eb4ac7c141e867bd14ca89c62be23d723b887ad0
Conrad Damon <cdamon@zimbra.com>
2005-10-12 23:31:59 +0000

    Async: item and organizer actions, item caching
    - Created a preliminary cache class (AjxCache) to serve
      as an item cache. Does no cache management yet. The
      key for an item is its ID.
    - We are moving back to notification-based updating of
      the UI, rather than response-based.
    - Use ZmAppCtxt.cacheGet() and ZmAppCtxt.cacheSet()
    - Item constructors now take an ID.
    
    - Clean up callback arg passing
    
    - Item actions (except create) are now done async. That includes
      flag, tag, move, and delete.
    - Since the UI is updated based on the ensuing notifications,
      there's generally no need for a callback on an item action. One
      exception is moving a conv, since the notifs don't tell us if a 
      conv has moved (only msgs).
    
    - Organizer actions (except create) are now done async. That
      includes rename, change color, move, delete, and mark all as
      read.
    
    - Fix tag menu order on rename. Turns out there's no need for a
      tag change listener since the menu is set before display every
      time.
    
    - There are now general methods in ZmAppCtxt for getting and
      setting trees: getTree() and setTree(), with the tree type passed
      in. These replace getFolderTree(), getTagList(), etc.
    
    - Nuked the E_RENAME event, it's now E_MODIFY with field F_NAME

--- Ajax/WebRoot/js/util/AjxCallback.js
+++ Ajax/WebRoot/js/util/AjxCallback.js
@@ -33,9 +33,9 @@
 * client.
 *
 * @author Conrad Damon
-* @param obj	the object to call the function from
-* @param func	the callback function
-* @param args   default arguments
+* @param obj	[Object]				the object to call the function from
+* @param func	[function]				the callback function
+* @param args   [primitive or Array]	default arguments
 */
 function AjxCallback(obj, func, args) {
 	if (arguments.length == 0) return;
@@ -54,24 +54,33 @@ function() {
 * Runs the callback function, from within the object if there is one. Passes a single 
 * argument on to the callback function. If you want to pass more than one argument, 
 * collect them in an array and have the called function break it apart. Whatever the
-* called function returns is passed along.
+* called function returns is returned to the caller.
+* <p>
+* If you need to pass in a single array argument that you want to remain an array,
+* wrap it in another array: <code>callback.run([list]);
 *
-* @param args	arguments to pass to the called function
-* @returns		whatever the called function returns
+* @param args	[primitive or Array]	argument(s) to pass to the called function
 */
 AjxCallback.prototype.run =
 function(args) {
+	// combine original args with new ones
 	var args1;
 	if (this._args != undefined && args != undefined) {
+		args1 = new Array();
 		if (this._args instanceof Array) {
-			args1 = this._args;
+			for (var i = 0; i < this._args.length; i++)
+				args1.push(this._args[i]);
 		} else {
-			args1 = new Array();
 			args1.push(this._args);
 		}
-		args1 = args1.concat(args);
+		if (args instanceof Array) {
+			for (var i = 0; i < args.length; i++)
+				args1.push(args[i]);
+		} else {
+			args1.push(args);
+		}
 	} else {
-		args1 = args || this._args;
+		args1 = args ? args : this._args;
 	}
 	if (this.obj)
 		return this.func.call(this.obj, args1);
