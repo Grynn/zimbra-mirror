6a87a42490a7017c719792f85cfc501fb98b7d27
 Unknown Perforce User <tim>
2005-10-18 03:16:06 +0000

    braindead MTA perl script for running on dev installs in windows

---
+++ ZimbraServer/src/perl/zMTAHack.pl
@@ -0,0 +1,65 @@
+use Net::SMTP::Server;
+use Net::SMTP::Server::Client;
+use Net::SMTP::Server::Relay;
+use Net::LMTP;
+use strict;
+
+#
+# Braindead hack by Tim -- listens on the SMTP port and if the TO address has a
+# domain which maches the value of the ZIMBRA_HOSTNAME environment variable) then it 
+# fwds the message to the LMTP on the zimbra server...otherwise it relays this message
+# via this machine's default SMTP (ie exch1)
+#
+
+my $server = new Net::SMTP::Server('localhost', 25) ||
+    croak("Unable to handle client connection: $!\n");
+
+my $zimbra_hostname  = $ENV{"ZIMBRA_HOSTNAME"};
+
+if (!defined($zimbra_hostname)) {
+    die "\"ZIMBRA_HOSTNAME\" must be set and must contain the Zimbra Server which you want to route email to";
+}
+
+print "Starting SMTP Hack on port 25\n";
+
+while(my $conn = $server->accept()) {
+    # We can perform all sorts of checks here for spammers, ACLs,
+    # and other useful stuff to check on a connection.
+
+    # Handle the client's connection and spawn off a new parser.
+    # This can/should be a fork() or a new thread,
+    # but for simplicity...
+    my $client = new Net::SMTP::Server::Client($conn) ||
+        croak("Unable to handle client connection: $!\n");
+
+    # Process the client.  This command will block until
+    # the connecting client completes the SMTP transaction.
+    $client->process || next;
+
+    foreach my $to (@{$client->{TO}}) {
+
+        if ($to =~ /.*\@$zimbra_hostname/) {
+            my $lmtp = Net::LMTP->new('localhost', 7025);
+
+            print "Got a local message from ".$client->{FROM}." to ".$to."\n";
+            
+            $lmtp->mail($ENV{USER});
+            $lmtp->to($to);
+            
+            $lmtp->data();
+            $lmtp->datasend($client->{MSG});
+            $lmtp->dataend();
+            
+            $lmtp->quit;
+        } else {
+            print "Replaying message from ".$client->{FROM}." to ".$to."....";
+            my $relay = new Net::SMTP::Server::Relay($client->{FROM},
+                                                     $client->{TO},
+                                                     $client->{MSG});
+
+            # if the app hangs before getting here it is likely trying to connect to
+            # itself...this machine's default SMTP server must be a different box (e.g. Exch1)
+            print "Message sent!\n";
+        }
+    }
+}
