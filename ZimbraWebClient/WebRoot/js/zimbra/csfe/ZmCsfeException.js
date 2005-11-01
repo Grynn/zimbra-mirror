a519ad679977488c15a7a50678e311c71710c3f8
 Unknown Perforce User <schemers>
2005-11-01 04:48:30 +0000

    use new ZMsg file for ZmCsfeException so admin/mail are both happy

--- ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
+++ ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
@@ -53,33 +53,33 @@ function(name, code, msg) {
   ZmCsfeException._codeToMsg[code] = msg;
 }
 
-ZmCsfeException.define("CSFE_SVC_ERROR", "CSFE_SVC_ERROR", ZmMsg.errorService);
-ZmCsfeException.define("NETWORK_ERROR", "NETWORK_ERROR", ZmMsg.errorNetwork);
+ZmCsfeException.define("CSFE_SVC_ERROR", "CSFE_SVC_ERROR", ZMsg.errorService);
+ZmCsfeException.define("NETWORK_ERROR", "NETWORK_ERROR", ZMsg.errorNetwork);
 ZmCsfeException.define("NO_AUTH_TOKEN", "NO_AUTH_TOKEN");
-ZmCsfeException.define("SOAP_ERROR", "SOAP_ERROR", ZmMsg.errorNetwork);
+ZmCsfeException.define("SOAP_ERROR", "SOAP_ERROR", ZMsg.errorNetwork);
 
 // CSFE Exceptions
 ZmCsfeException.define("SVC_AUTH_EXPIRED", "service.AUTH_EXPIRED");
 ZmCsfeException.define("SVC_AUTH_REQUIRED", "service.AUTH_REQUIRED");
-ZmCsfeException.define("SVC_FAILURE", "service.FAILURE", ZmMsg.errorService);
+ZmCsfeException.define("SVC_FAILURE", "service.FAILURE", ZMsg.errorService);
 ZmCsfeException.define("SVC_INVALID_REQUEST", "service.INVALID_REQUEST");
 ZmCsfeException.define("SVC_PARSE_ERROR", "service.PARSE_ERROR");
-ZmCsfeException.define("SVC_PERM_DENIED", "service.PERM_DENIED", ZmMsg.errorPermission);
+ZmCsfeException.define("SVC_PERM_DENIED", "service.PERM_DENIED", ZMsg.errorPermission);
 ZmCsfeException.define("SVC_UNKNOWN_DOCUMENT", "service.UNKNOWN_DOCUMENT");
 ZmCsfeException.define("SVC_WRONG_HOST", "service.WRONG_HOST");
 
 ZmCsfeException.define("ACCT_AUTH_FAILED", "account.AUTH_FAILED");
-ZmCsfeException.define("ACCT_CHANGE_PASSWORD", "account.CHANGE_PASSWORD", ZmMsg.errorPassChange);
+ZmCsfeException.define("ACCT_CHANGE_PASSWORD", "account.CHANGE_PASSWORD", ZMsg.errorPassChange);
 ZmCsfeException.define("ACCT_EXISTS", "account.ACCOUNT_EXISTS");
-ZmCsfeException.define("ACCT_INVALID_PASSWORD", "account.INVALID_PASSWORD", ZmMsg.errorInvalidPass);
-ZmCsfeException.define("ACCT_INVALID_PREF_NAME", "account.INVALID_PREF_NAME", ZmMsg.errorInvalidPrefName);
-ZmCsfeException.define("ACCT_INVALID_PREF_VALUE", "account.INVALID_PREF_VALUE", ZmMsg.errorInvalidPrefValue);
+ZmCsfeException.define("ACCT_INVALID_PASSWORD", "account.INVALID_PASSWORD", ZMsg.errorInvalidPass);
+ZmCsfeException.define("ACCT_INVALID_PREF_NAME", "account.INVALID_PREF_NAME", ZMsg.errorInvalidPrefName);
+ZmCsfeException.define("ACCT_INVALID_PREF_VALUE", "account.INVALID_PREF_VALUE", ZMsg.errorInvalidPrefValue);
 ZmCsfeException.define("ACCT_NO_SUCH_ACCOUNT", "account.NO_SUCH_ACCOUNT");
-ZmCsfeException.define("ACCT_NO_SUCH_SAVED_SEARCH", "account.NO_SUCH_SAVED_SEARCH", ZmMsg.errorNoSuchSavedSearch);
-ZmCsfeException.define("ACCT_NO_SUCH_TAG", "account.ACCT_NO_SUCH_TAG", ZmMsg.errorNoSuchTag);
-ZmCsfeException.define("ACCT_PASS_CHANGE_TOO_SOON", "account.PASSWORD_CHANGE_TOO_SOON", ZmMsg.errorPassChangeTooSoon);
-ZmCsfeException.define("ACCT_PASS_LOCKED", "account.PASSWORD_LOCKED", ZmMsg.errorPassLocked);
-ZmCsfeException.define("ACCT_PASS_RECENTLY_USED", "account.PASSWORD_RECENTLY_USED", ZmMsg.errorPassRecentlyUsed);
+ZmCsfeException.define("ACCT_NO_SUCH_SAVED_SEARCH", "account.NO_SUCH_SAVED_SEARCH", ZMsg.errorNoSuchSavedSearch);
+ZmCsfeException.define("ACCT_NO_SUCH_TAG", "account.ACCT_NO_SUCH_TAG", ZMsg.errorNoSuchTag);
+ZmCsfeException.define("ACCT_PASS_CHANGE_TOO_SOON", "account.PASSWORD_CHANGE_TOO_SOON", ZMsg.errorPassChangeTooSoon);
+ZmCsfeException.define("ACCT_PASS_LOCKED", "account.PASSWORD_LOCKED", ZMsg.errorPassLocked);
+ZmCsfeException.define("ACCT_PASS_RECENTLY_USED", "account.PASSWORD_RECENTLY_USED", ZMsg.errorPassRecentlyUsed);
 ZmCsfeException.define("DOMAIN_NOT_EMPTY", "account.DOMAIN_NOT_EMPTY");
 ZmCsfeException.define("DISTRIBUTION_LIST_EXISTS", "account.DISTRIBUTION_LIST_EXISTS");
 
@@ -87,13 +87,13 @@ ZmCsfeException.define("COS_EXISTS", "account.COS_EXISTS");
 
 ZmCsfeException.define("DOMAIN_EXISTS", "account.DOMAIN_EXISTS");
 
-ZmCsfeException.define("MAIL_INVALID_NAME", "mail.INVALID_NAME", ZmMsg.errorInvalidName);
-ZmCsfeException.define("MAIL_NO_SUCH_FOLDER", "mail.NO_SUCH_FOLDER", ZmMsg.errorNoSuchFolder);
-ZmCsfeException.define("MAIL_NO_SUCH_TAG", "mail.NO_SUCH_TAG", ZmMsg.errorNoSuchTag);
-ZmCsfeException.define("MAIL_NO_SUCH_CONV", "mail.NO_SUCH_CONV", ZmMsg.errorNoSuchConv);
-ZmCsfeException.define("MAIL_NO_SUCH_MSG", "mail.NO_SUCH_MSG", ZmMsg.errorNoSuchMsg);
-ZmCsfeException.define("MAIL_NO_SUCH_PART", "mail.NO_SUCH_PART", ZmMsg.errorNoSuchPart);
-ZmCsfeException.define("MAIL_QUOTA_EXCEEDED", "mail.QUOTA_EXCEEDED", ZmMsg.errorQuotaExceeded);
-ZmCsfeException.define("MAIL_QUERY_PARSE_ERROR", "mail.QUERY_PARSE_ERROR", ZmMsg.errorQueryParse);
-ZmCsfeException.define("MAIL_SEND_FAILURE", "mail.SEND_FAILURE", ZmMsg.mailSendFailure);
+ZmCsfeException.define("MAIL_INVALID_NAME", "mail.INVALID_NAME", ZMsg.errorInvalidName);
+ZmCsfeException.define("MAIL_NO_SUCH_FOLDER", "mail.NO_SUCH_FOLDER", ZMsg.errorNoSuchFolder);
+ZmCsfeException.define("MAIL_NO_SUCH_TAG", "mail.NO_SUCH_TAG", ZMsg.errorNoSuchTag);
+ZmCsfeException.define("MAIL_NO_SUCH_CONV", "mail.NO_SUCH_CONV", ZMsg.errorNoSuchConv);
+ZmCsfeException.define("MAIL_NO_SUCH_MSG", "mail.NO_SUCH_MSG", ZMsg.errorNoSuchMsg);
+ZmCsfeException.define("MAIL_NO_SUCH_PART", "mail.NO_SUCH_PART", ZMsg.errorNoSuchPart);
+ZmCsfeException.define("MAIL_QUOTA_EXCEEDED", "mail.QUOTA_EXCEEDED", ZMsg.errorQuotaExceeded);
+ZmCsfeException.define("MAIL_QUERY_PARSE_ERROR", "mail.QUERY_PARSE_ERROR", ZMsg.errorQueryParse);
+ZmCsfeException.define("MAIL_SEND_FAILURE", "mail.SEND_FAILURE", ZMsg.mailSendFailure);
 
