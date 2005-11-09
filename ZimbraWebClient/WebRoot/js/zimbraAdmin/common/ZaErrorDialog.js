f228b8d180ae9f0cef5696453e8ad22190ed9add
 Unknown Perforce User <anandp>
2005-11-09 21:42:33 +0000

    More licensing.
    Original Code must be identified as "ZCS Web Client" (instead of
    just "ZCS").
    
    Convert the Russian translation file with mac2unix.
    
    Some code had been cut pasted from Ajax and had 
    Original Code and so was identified wrong - fix to identify all
    code inside ZimbraWebClient project correctly.

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaErrorDialog.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaErrorDialog.js
@@ -10,7 +10,7 @@ Software distributed under the License is distributed on an "AS IS" basis, WITHO
 ANY KIND, either express or implied. See the License for the specific language governing rights
 and limitations under the License.
 
-The Original Code is: Zimbra AJAX Toolkit.
+The Original Code is: Zimbra Collaboration Suite Web Client
 
 The Initial Developer of the Original Code is Zimbra, Inc.
 Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
