bb8f753099e682c90a4630a0cbc0268c9dffe05a
 Unknown Perforce User <tim>
2005-11-29 01:39:27 +0000

    IM Checkpoint
    Works:
       IMGetRoster
       IMSubscribe (no notification for remote user.  no block/allow list)
       IMSetPresence
       IMSendMessage
       IMGetChat
       IMModifyChat op="close"
    
    Doesn't work:
       Buddy lists aren't saved
       Local-Server only
       Notifications 
       Block/Allow lists
       ...lots of other stuff...

---
+++ ZimbraServer/src/perl/soap/imgetroster.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl -w
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 ("License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.zimbra.com/license
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is: Zimbra Collaboration Suite Server.
+# 
+# The Initial Developer of the Original Code is Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+
+#
+# Simple SOAP test-harness for the AddMsg API
+#
+
+use Date::Parse;
+use Time::HiRes qw ( time );
+use strict;
+
+use lib '.';
+
+use LWP::UserAgent;
+
+use XmlElement;
+use XmlDoc;
+use Soap;
+
+my $userId;
+
+if (defined $ARGV[0] && $ARGV[0] ne "") {
+    $userId = $ARGV[0];
+} else {
+    print "USAGE: imGetRoster USERID\n";
+    exit 1;
+}
+
+my $ACCTNS = "urn:zimbraAccount";
+my $MAILNS = "urn:zimbraMail";
+
+my $url = "http://localhost:7070/service/soap/";
+
+my $SOAP = $Soap::Soap12;
+my $d = new XmlDoc;
+$d->start('AuthRequest', $ACCTNS);
+$d->add('account', undef, { by => "name"}, $userId);
+$d->add('password', undef, undef, "test123");
+$d->end();
+
+my $authResponse = $SOAP->invoke($url, $d->root());
+
+print "AuthResponse = ".$authResponse->to_string("pretty")."\n";
+
+my $authToken = $authResponse->find_child('authToken')->content;
+print "authToken($authToken)\n";
+
+my $sessionId = $authResponse->find_child('sessionId')->content;
+print "sessionId = $sessionId\n";
+
+my $context = $SOAP->zimbraContext($authToken, $sessionId);
+
+my $contextStr = $context->to_string("pretty");
+print("Context = $contextStr\n");
+
+$d = new XmlDoc;
+$d->start('IMGetRosterRequest', $MAILNS);
+
+$d->end(); #
+
+print "\nOUTGOING XML:\n-------------\n";
+my $out =  $d->to_string("pretty")."\n";
+$out =~ s/ns0\://g;
+print $out."\n";
+
+my $response = $SOAP->invoke($url, $d->root(), $context);
+print "\nRESPONSE:\n--------------\n";
+$out =  $response->to_string("pretty")."\n";
+$out =~ s/ns0\://g;
+print $out."\n";
+
