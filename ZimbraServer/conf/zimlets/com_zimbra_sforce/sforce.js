6513f97115a13266586fbc58d3b8c136545bf35d
 Unknown Perforce User <mihai>
2005-12-09 17:03:47 +0000

    Added support for dragging conversations/email messages to the SForce Zimlet, various other fixes.

--- ZimbraServer/conf/zimlets/com_zimbra_sforce/sforce.js
+++ ZimbraServer/conf/zimlets/com_zimbra_sforce/sforce.js
@@ -80,14 +80,14 @@ Com_Zimbra_SForce.prototype.xmlToObject = function(result) {
 		// var xd = new AjxXmlDoc.createFromXml(xml).toJSObject(true, false);
 		var xd = new AjxXmlDoc.createFromDom(result.xml).toJSObject(true, false);
 	} catch(ex) {
-		alert(ex.dump() + "\n" + ex);
+		this.displayErrorMessage(ex.dump() + "<br />" + ex, result.text);
 	}
 	return xd;
 };
 
 /// Utility function that calls the SForce server with the given SOAP data
-Com_Zimbra_SForce.prototype.rpc = function(soap, callback) {
-	this.sendRequest(soap, this.SERVER, { SOAPAction: "m" }, callback);
+Com_Zimbra_SForce.prototype.rpc = function(soap, callback, passErrors) {
+	this.sendRequest(soap, this.SERVER, { SOAPAction: "m" }, callback, false, passErrors);
 };
 
 // SOAP METHOD: login
@@ -95,12 +95,25 @@ Com_Zimbra_SForce.prototype.rpc = function(soap, callback) {
 /// Login to SForce.  The given callback will be called in the case of a
 /// successful login.  Note that callback is a plain function (not AjxCallback)
 Com_Zimbra_SForce.prototype.login = function(callback) {
-	var soap = this._makeEnvelope("login");
-	soap.set("username", this.getUserProperty("user"));
-	soap.set("password", this.getUserProperty("passwd"));
 	if (callback == null)
 		callback = false;
-	this.rpc(soap, new AjxCallback(this, this.done_login, callback));
+	var user = this.getUserProperty("user");
+	var passwd = this.getUserProperty("passwd");
+	if (!user || !passwd) {
+		this.displayStatusMessage("Please fill your SForce credentials first");
+		this.createPropertyEditor(new AjxCallback(this, this.login, callback));
+	} else {
+		this._do_login(callback, user, passwd);
+	}
+};
+
+Com_Zimbra_SForce.prototype._do_login = function(callback, user, passwd) {
+	var soap = this._makeEnvelope("login");
+	soap.set("username", user);
+	soap.set("password", passwd);
+	if (callback == null)
+		callback = false;
+	this.rpc(soap, new AjxCallback(this, this.done_login, callback), true);
 };
 
 Com_Zimbra_SForce.prototype.done_login = function(params) {
@@ -143,6 +156,17 @@ Com_Zimbra_SForce.prototype.logout = function() {
 /// Executes a SOQL (SalesForce Object Query Language) and calls the given
 /// callback upon successful execution.
 Com_Zimbra_SForce.prototype.query = function(query, limit, callback) {
+	// How I wish JS had continuations!  But luckily, we are smart enough
+	// to overcome this limitation. ;-)
+	if (!this.sessionId)
+		this.login(function() {
+			this._do_query(query, limit, callback);
+		});
+	else
+		this._do_query(query, limit, callback);
+};
+
+Com_Zimbra_SForce.prototype._do_query = function(query, limit, callback) {
 	if (typeof limit == "undefined")
 		limit = 1;
 	var soap = this._makeEnvelope("query");
@@ -168,24 +192,20 @@ Com_Zimbra_SForce.prototype.done_query = function(params) {
 	var callback = params[0];
 	var result = params[1];
 	var xd = this.xmlToObject(result);
- 	try {
-		var qr = xd.Body.queryResponse.result.records;
-		if (qr != null) {
-			if (!(qr instanceof Array))
-				qr = [ qr ];
-			// sometimes SForce returns a duplicate <Id> tag
-			for (var i = qr.length; --i >= 0;) {
-				if (qr[i].Id && (qr[i].Id instanceof Array))
-					qr[i].Id = qr[i].Id[0];
-				qr[i].get = Com_Zimbra_SForce.__query_result_get;
-			}
-		} else {
-			qr = [];
+	var qr = xd.Body.queryResponse.result.records;
+	if (qr != null) {
+		if (!(qr instanceof Array))
+			qr = [ qr ];
+		// sometimes SForce returns a duplicate <Id> tag
+		for (var i = qr.length; --i >= 0;) {
+			if (qr[i].Id && (qr[i].Id instanceof Array))
+				qr[i].Id = qr[i].Id[0];
+			qr[i].get = Com_Zimbra_SForce.__query_result_get;
 		}
-		callback.call(this, qr);
- 	} catch(ex) {
- 		this.displayErrorMessage(ex + "<br />" + ex.dump(), result.text);
- 	}
+	} else {
+		qr = [];
+	}
+	callback.call(this, qr);
 };
 
 // SOAP METHOD: create
@@ -220,6 +240,7 @@ Com_Zimbra_SForce.prototype.createSFObject = function(props, type, callback) {
 Com_Zimbra_SForce.prototype.done_createSFObject = function(params) {
 	var callback = params[0];
 	var result = params[1];
+	//alert(result.text);
 	var xd = this.xmlToObject(result);
 	if (callback) {
 		result = xd.Body.createResponse.result;
@@ -227,7 +248,7 @@ Com_Zimbra_SForce.prototype.done_createSFObject = function(params) {
 		if (result instanceof Array) {
 			id = [];
 			for (var i = 0; i < result.length; ++i)
-				id.push(result.id.toString());
+				id.push(result[i].id.toString());
 		} else {
 			id = result.id.toString();
 		}
@@ -349,7 +370,7 @@ Com_Zimbra_SForce.prototype.dlg_createAccount = function(acct_data, contact_data
 	};
 
 	var tmp = document.createElement("h3");
-	tmp.className = "SForce-sec-label";
+	tmp.className = "SForce-sec-label SForce-icon-right";
 	tmp.innerHTML = acct_data.Id
 		? "An existing account matches"
 		: "Add to a new account";
@@ -443,17 +464,172 @@ Com_Zimbra_SForce.prototype.dlg_createAccount = function(acct_data, contact_data
 		}));
 };
 
+Com_Zimbra_SForce.prototype.noteDropped = function(note) {
+	// check out some domains
+	var emails = [];
+	function addEmails(a) {
+		if (a) {
+			if (typeof a == "string") {
+				emails.push(a);
+			} else if (a instanceof Array) {
+				for (var i = 0; i < a.length; ++i)
+					emails.push(a[i]);
+			}
+		}
+	};
+	addEmails(note.participants);
+	addEmails(note.from);
+	addEmails(note.to);
+	addEmails(note.cc);
+	var domains = [], tmp = {};
+	for (var i = 0; i < emails.length; ++i) {
+		if (/@([^>]+)>?$/.test(emails[i])) {
+			var d = RegExp.$1;
+			if (!tmp[d]) {
+				tmp[d] = 1;
+				// kind of pointless, but let's make sure we
+				// backslash any apostrophes
+				domains.push(d.replace(/\x27/, "\\'"));
+			}
+		}
+	}
+	function $search_acct(records) {
+		if (records.length == 0) {
+			this.displayErrorMessage(
+				[ "There are no matching accounts for these domain names:",
+				  domains ].join("<br />"));
+		} else {
+			this.dlg_addNoteToAccounts(records, note);
+		}
+	};
+	if (domains.length == 0) {
+		this.displayErrorMessage("You dropped a message or conversation that contains no emails.<br />"
+					 + "We can't determine an Account to add this note to.");
+	} else {
+		var q = [ "select Id, Name, Website from Account where Website like '%",
+			  domains.join("%' or Website like '%"),
+			  "%'" ].join("");
+		this.query(q, 10, $search_acct);
+	}
+};
+
+Com_Zimbra_SForce.prototype.dlg_addNoteToAccounts = function(records, note) {
+	var view = new DwtComposite(this.getShell());
+	var el = view.getHtmlElement();
+	var h3 = document.createElement("h3");
+	h3.className = "SForce-sec-label SForce-icon-right";
+	h3.innerHTML = "Add note to these Account(s)";
+	el.appendChild(h3);
+
+	var checkboxes = [];
+
+	var div = document.createElement("div");
+	var html = [ "<table><tbody>" ];
+	for (var i = 0; i < records.length; ++i) {
+		var rec = records[i];
+		var cbid = Dwt.getNextId();
+		checkboxes.push(cbid);
+		html.push("<tr>",
+			  "<td>",
+			  "<input type='checkbox' value='",
+			  rec.Id,
+			  "' checked='checked' id='",
+			  cbid,
+			  "' />",
+			  "</td>",
+			  "<td>",
+			  "<label for='", cbid, "'>",
+			  rec.Name);
+
+		if (rec.Website)
+			html.push(" (" + rec.Website + ")");
+
+		html.push("</label>",
+			  "</td>",
+			  "</tr>");
+	}
+	html.push("</tbody></table>");
+	div.innerHTML = html.join("");
+	el.appendChild(div);
+
+	h3 = document.createElement("h3");
+	h3.className = "SForce-sec-label";
+	h3.innerHTML = "Note details";
+	el.appendChild(h3);
+
+	div = document.createElement("div");
+	var subjectId = Dwt.getNextId();
+	var messageId = Dwt.getNextId();
+	div.innerHTML =
+		[ "<table><tbody>",
+		  "<tr>",
+		  "<td align='right'><label for='", subjectId, "'>Subject:</td>",
+		  "<td>",
+		  "<input autocomplete='off' style='width: 20em' type='text' id='", subjectId, "' value='",
+		  AjxStringUtil.htmlEncode(note.subject), "'/>",
+		  "</td>",
+		  "</tr>",
+		  "<td colspan='2'>",
+		  "<textarea rows='5' style='width: 25em' id='", messageId, "'>",
+		  AjxStringUtil.htmlEncode(note.body), "</textarea>",
+		  "</td>",
+		  "<tr>",
+		  "</tr></tbody></table>" ].join("");
+	el.appendChild(div);
+
+	var dialog_args = {
+		view  : view,
+		title : "Add note to SForce"
+	};
+	var dlg = this._createDialog(dialog_args);
+	dlg.popup();
+
+	el = document.getElementById(subjectId);
+	el.select();
+	el.focus();
+
+	dlg.setButtonListener(DwtDialog.OK_BUTTON,
+			      new AjxListener(this, function() {
+				      var acct_ids = [];
+				      for (var i = 0; i < checkboxes.length; ++i) {
+					      var cb = document.getElementById(checkboxes[i]);
+					      if (cb.checked)
+						      acct_ids.push({ ParentId: cb.value });
+				      }
+				      if (acct_ids.length == 0) {
+					      this.displayErrorMessage("You must select at least one account");
+				      } else {
+					      var props = {
+						      Title : document.getElementById(subjectId).value,
+						      Body  : document.getElementById(messageId).value
+					      };
+					      for (var i = 0; i < acct_ids.length; ++i) {
+						      acct_ids[i].Title = props.Title;
+						      acct_ids[i].Body = props.Body;
+					      }
+					      this.createSFObject(acct_ids, "Note", function() {
+						      this.displayStatusMessage("Saved " + acct_ids.length + " notes");
+					      });
+					      dlg.popdown();
+					      dlg.dispose();
+				      }
+			      }));
+
+	dlg.setButtonListener(DwtDialog.CANCEL_BUTTON,
+			      new AjxListener(this, function() {
+				      dlg.popdown();
+				      dlg.dispose();
+			      }));
+};
+
 // UI handlers
 
 /// Called by the Zimbra framework upon an accepted drag'n'drop
 Com_Zimbra_SForce.prototype.doDrop = function(obj) {
 	switch (obj.TYPE) {
 	    case "ZmMailMsg":
-		this.displayErrorMessage("You dropped an email message!");
-		break;
-
 	    case "ZmConv":
-		this.displayErrorMessage("You dropped a conversation!");
+		this.noteDropped(obj);
 		break;
 
 	    case "ZmContact":
