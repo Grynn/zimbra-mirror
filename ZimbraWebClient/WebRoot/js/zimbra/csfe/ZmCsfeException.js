953335e12d7a76d5cc5d35092fa20ef2059ed0cf
Conrad Damon <cdamon@zimbra.com>
2006-01-18 23:26:46 +0000

    Integrate 19870 from Cray
    	Move CSFE error msgs to ZMsg

--- ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
+++ ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
@@ -74,10 +74,10 @@ ZmCsfeException.define("SVC_AUTH_EXPIRED", "service.AUTH_EXPIRED");
 ZmCsfeException.define("SVC_AUTH_REQUIRED", "service.AUTH_REQUIRED");
 ZmCsfeException.define("SVC_FAILURE", "service.FAILURE", ZMsg.errorService);
 ZmCsfeException.define("SVC_INVALID_REQUEST", "service.INVALID_REQUEST");
-ZmCsfeException.define("SVC_PARSE_ERROR", "service.PARSE_ERROR", ZmMsg.errorParse);
+ZmCsfeException.define("SVC_PARSE_ERROR", "service.PARSE_ERROR", ZMsg.errorParse);
 ZmCsfeException.define("SVC_PERM_DENIED", "service.PERM_DENIED", ZMsg.errorPermission);
 ZmCsfeException.define("SVC_RESOURCE_UNREACHABLE", "service.RESOURCE_UNREACHABLE");
-ZmCsfeException.define("SVC_UNKNOWN_DOCUMENT", "service.UNKNOWN_DOCUMENT", ZmMsg.errorUnknownDoc);
+ZmCsfeException.define("SVC_UNKNOWN_DOCUMENT", "service.UNKNOWN_DOCUMENT", ZMsg.errorUnknownDoc);
 ZmCsfeException.define("SVC_WRONG_HOST", "service.WRONG_HOST");
 
 ZmCsfeException.define("ACCT_AUTH_FAILED", "account.AUTH_FAILED");
@@ -87,7 +87,7 @@ ZmCsfeException.define("ACCT_INVALID_PASSWORD", "account.INVALID_PASSWORD", ZMsg
 ZmCsfeException.define("ACCT_INVALID_PREF_NAME", "account.INVALID_PREF_NAME", ZMsg.errorInvalidPrefName);
 ZmCsfeException.define("ACCT_INVALID_PREF_VALUE", "account.INVALID_PREF_VALUE", ZMsg.errorInvalidPrefValue);
 ZmCsfeException.define("ACCT_MAINTENANCE_MODE", "account.MAINTENANCE_MODE");
-ZmCsfeException.define("ACCT_NO_SUCH_ACCOUNT", "account.NO_SUCH_ACCOUNT", ZmMsg.errorNoSuchAcct);
+ZmCsfeException.define("ACCT_NO_SUCH_ACCOUNT", "account.NO_SUCH_ACCOUNT", ZMsg.errorNoSuchAcct);
 ZmCsfeException.define("NO_SUCH_DISTRIBUTION_LIST", "account.NO_SUCH_DISTRIBUTION_LIST");
 ZmCsfeException.define("ACCT_NO_SUCH_SAVED_SEARCH", "account.NO_SUCH_SAVED_SEARCH", ZMsg.errorNoSuchSavedSearch);
 ZmCsfeException.define("ACCT_NO_SUCH_TAG", "account.ACCT_NO_SUCH_TAG", ZMsg.errorNoSuchTag);
