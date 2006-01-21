be34d86d4933a1d07a25757c45c6cb3c9e4855ae
 Unknown Perforce User <jhahm>
2006-01-21 02:32:11 +0000

    Integrate change 20096

---
+++ ZimbraServer/src/db/migrate20060120-Appointment.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 ("License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.zimbra.com/license
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is: Zimbra Collaboration Suite Server.
+# 
+# The Initial Developer of the Original Code is Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+
+
+use strict;
+use Migrate;
+
+my @mailboxIds = Migrate::getMailboxIds();
+
+Migrate::verifySchemaVersion(21);
+foreach my $id (@mailboxIds) {
+    moveApptsOutOfInbox($id);
+}
+
+exit(0);
+
+#####################
+
+sub moveApptsOutOfInbox($) {
+    my ($mailboxId) = @_;
+    my $sql = <<EOF;
+UPDATE mailbox$mailboxId.mail_item SET folder_id=10
+WHERE folder_id=2 AND type=11;
+
+EOF
+    
+    Migrate::log("Fixing appointments in mailbox$mailboxId.mail_item.");
+    Migrate::runSql($sql);
+}
