d993b58d255691050225719d83806a332332c682
Conrad Damon <cdamon@zimbra.com>
2006-01-26 00:03:23 +0000

    Integrate 20381 from Cray
    	Change error messages for failed message send.

--- ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
+++ ZimbraWebClient/WebRoot/js/zimbra/csfe/ZmCsfeException.js
@@ -114,5 +114,5 @@ ZmCsfeException.define("MAIL_SEND_FAILURE", "mail.SEND_FAILURE", ZMsg.mailSendFa
 ZmCsfeException.define("MAIL_TOO_MANY_CONTACTS", "mail.TOO_MANY_CONTACTS", ZMsg.errorTooManyContacts);
 
 // structured data keys
-ZmCsfeException.MAIL_SEND_ABORTED_ADDRESS_FAILURE_INVALID = "invalid";
-ZmCsfeException.MAIL_SEND_ABORTED_ADDRESS_FAILURE_UNSENT = "unsent";
+ZmCsfeException.MAIL_SEND_ADDRESS_FAILURE_INVALID = "invalid";
+ZmCsfeException.MAIL_SEND_ADDRESS_FAILURE_UNSENT = "unsent";
