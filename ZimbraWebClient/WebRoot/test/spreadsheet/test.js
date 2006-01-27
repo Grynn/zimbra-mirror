23cec7b72141312bd2003ebcd6933636332f90c9
 Unknown Perforce User <mihai>
2006-01-27 21:32:29 +0000

    Cut/copy/paste/fill functionality

--- ZimbraWebClient/WebRoot/test/spreadsheet/test.js
+++ ZimbraWebClient/WebRoot/test/spreadsheet/test.js
@@ -4,13 +4,17 @@ function Test() {
 // 	this.shell._setMouseEventHdlrs();
 // 	this.shell.addListener(DwtEvent.ONMOUSEMOVE, new AjxListener(this, this.func));
 
-	var f = new ZmSpreadSheet(this.shell, null, "absolute");
-	f.setModel(new ZmSpreadSheetModel(40, 8));
-	new ZmSpreadSheetToolbars(f, f);
- 	f.setBounds(20, 20, 800, 500);
-// 	f.getHtmlElement().style.width = "800px";
-// 	f._getTable().style.width = "1000px";
-	f.setZIndex(Dwt.Z_VIEW);
+	var f1 = new ZmSpreadSheet(this.shell, null, "absolute");
+	f1.setModel(new ZmSpreadSheetModel(20, 8));
+	new ZmSpreadSheetToolbars(f1, f1);
+ 	f1.setBounds(20, 20, 800, 300);
+	f1.setZIndex(Dwt.Z_VIEW);
+
+	var f2 = new ZmSpreadSheet(this.shell, null, "absolute");
+	f2.setModel(new ZmSpreadSheetModel(20, 8));
+	new ZmSpreadSheetToolbars(f2, f2);
+ 	f2.setBounds(20, 20 + 300 + 20, 800, 300);
+	f2.setZIndex(Dwt.Z_VIEW);
 };
 
 Test.run = function() {
