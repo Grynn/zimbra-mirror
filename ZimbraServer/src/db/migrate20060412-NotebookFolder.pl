bd1345bb2bf1c5af2886087c6c6fbcfdc0d1dddf
 Unknown Perforce User <jylee>
2006-04-13 18:25:09 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=6015 http://bugzilla.zimbra.com/show_bug.cgi?id=7088 bug: 6015,7088 create default Notebook folder with id 12.  change database Version per dkarp.

---
+++ ZimbraServer/src/db/migrate20060412-NotebookFolder.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 ("License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.zimbra.com/license
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is: Zimbra Collaboration Suite Server.
+# 
+# The Initial Developer of the Original Code is Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+
+
+use strict;
+use Migrate;
+
+Migrate::verifySchemaVersion(21);
+
+foreach my $id (@mailboxIds) {
+    createNotebookFolder($id);
+}
+
+Migrate::updateSchemaVersion(21, 22);
+
+exit(0);
+
+#####################
+
+sub createNotebookFolder($) {
+    my ($mailboxId) = @_;
+    my $timestamp = time();
+    my $sql = <<EOF_RENAME_NOTEBOOK_FOLDER;
+    
+UPDATE mailbox$mailboxId.mail_item
+SET subject = "Notebook1"
+WHERE subject = "Notebook";
+
+EOF_RENAME_NOTEBOOK_FOLDER
+    Migrate::runSql($sql);
+    
+    my $sql = <<EOF_CREATE_NOTEBOOK_FOLDER;
+    
+INSERT INTO mailbox$mailboxId.mail_item
+  (subject, id, type, parent_id, folder_id, mod_metadata, mod_content, metadata, date, change_date)
+VALUES
+  ("Notebook", 11, 1, 1, 1, 1, 1, "d1:ai1e1:vi9e2:vti14ee", $timestamp, $timestamp);
+
+EOF_CREATE_NOTEBOOK_FOLDER
+    Migrate::runSql($sql);
+}
