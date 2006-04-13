e2a322f2bbec718bd4670154437ea4ab7e759067
 Unknown Perforce User <dkarp>
2006-04-13 22:34:06 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=6015 http://bugzilla.zimbra.com/show_bug.cgi?id=7088 bug: 6015, 7088
    Only rename existing "Notebook" folders at top level.  Make sure MOD_METADATA and MOD_CONTENT are set correctly for sync.

--- ZimbraServer/src/db/migrate20060412-NotebookFolder.pl
+++ ZimbraServer/src/db/migrate20060412-NotebookFolder.pl
@@ -48,7 +48,7 @@ sub createNotebookFolder($) {
     
 UPDATE mailbox$mailboxId.mail_item
 SET subject = "Notebook1"
-WHERE subject = "Notebook";
+WHERE subject = "Notebook" AND folder_id = 1 AND id != 12;
 
 EOF_RENAME_NOTEBOOK_FOLDER
     Migrate::runSql($sql);
@@ -58,7 +58,14 @@ EOF_RENAME_NOTEBOOK_FOLDER
 INSERT INTO mailbox$mailboxId.mail_item
   (subject, id, type, parent_id, folder_id, mod_metadata, mod_content, metadata, date, change_date)
 VALUES
-  ("Notebook", 12, 1, 1, 1, 1, 1, "d1:ai1e1:vi9e2:vti14ee", $timestamp, $timestamp);
+  ("Notebook", 12, 1, 1, 1, 1, 1, "d1:ai1e1:vi9e2:vti14ee", $timestamp, $timestamp)
+ON DUPLICATE KEY UPDATE id = 12;
+
+UPDATE mailbox$mailboxId.mail_item mi, zimbra.mailbox mbx
+SET mod_metadata = change_checkpoint + 100,
+    mod_content = change_checkpoint + 100,
+    change_checkpoint = change_checkpoint + 200
+WHERE mi.id = 12 AND mbx.id = $mailboxId;
 
 EOF_CREATE_NOTEBOOK_FOLDER
     Migrate::runSql($sql);
