3cf8a970450f5466ce9dad2b715991d5bf3ad4e3
 Unknown Perforce User <rossd>
2006-05-01 00:17:10 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=7314 bug:  7314

--- Ajax/WebRoot/js/net/AjxRpc.js
+++ Ajax/WebRoot/js/net/AjxRpc.js
@@ -14,32 +14,89 @@
  * limitations under the License.
  */
 
-
+/**
+ * @constructor
+ * @class
+ * This static class provides an interface for send requests to a server. It
+ * essentially wraps <i>AjxRpcRequest</i>. <i>AjxRpc</i> maintainse a cache of
+ * <i>AjxRpcRequest</i> objects which it attempts to reuse before allocating additional
+ * objects. It also has a mechanism whereby if an <i>AjxRcpRequest</i> object is 
+ * in a "busy" state for a extended period of time, it will reap it appropriately.
+ *
+ * @author Ross Dargahi
+ * @author Conrad Damon
+ * 
+ * @see AjxRpcRequest
+**/
 function AjxRpc() {
 };
 
-// pool of RPC contexts
-AjxRpc._rpcCache = [];
+/** The pool of RPC contexts
+ * @private*/
+AjxRpc.__rpcCache = [];
 
-AjxRpc._RPC_CACHE_MAX = 100;	// maximum number of busy contexts we can have
-AjxRpc._RPC_REAP_COUNT = 5;		// run reaper when number of busy contexts is multiple of this
-AjxRpc._RPC_REAP_AGE = 300000;	// mark any context older than this (in ms) as free
+/** maximum number of busy contexts we can have
+ * @private */
+AjxRpc.__RPC_CACHE_MAX = 100;
+
+/** run reaper when number of busy contexts is multiple of this
+ * @private */
+AjxRpc.__RPC_REAP_COUNT = 5;
+
+/** mark any context older than this (in ms) as free
+ * @private */
+AjxRpc.__RPC_REAP_AGE = 300000;
 
 /**
-* Submits a request to a URL. The request is handled through a pool of request
-* contexts (each a wrapped XmlHttpRequest). The context does the real work.
-*
-* @param requestStr		[string]		HTTP request string/document
-* @param serverUrl		[string]		request target
-* @param requestHeaders	[Array]			HTTP request headers
-* @param callback		[AjxCallback]	callback (for async requests)
-* @param useGet			[boolean]		if true use get method, else use post
-*/
+ * Submits a request to a URL. The request is handled through a pool of request
+ * contexts (each a wrapped XmlHttpRequest). The context does the real work.
+ *
+ * @param {String} requestStr HTTP request string/document
+ * @param {String} serverUrl request target 
+ * @param {Array} requestHeaders Array of HTTP request headers (optional)
+ * @param {AjxCallback} callback callback for asynchronous requests. This callback 
+ * 		will be invoked when the requests completes. It will be passed the same
+ * 		values as when this method is invoked synchronously (see the return values
+ * 		below) with the exception that if the call times out (see timeout param 
+ * 		below), then the object passed to the callback will be the same as in the 
+ * 		error case with the exception that the status will be set to 
+ * 		<code>AjxRpcRequest.TIMEDOUT</code>
+ * @param {Boolean} useGet if true use get method, else use post. If ommitted
+ * 		defaults to post
+ * @param {Int} timeout Timeout (in milliseconds) after which the request is 
+ * 		cancelled (optional)
+ * 
+ * @return If invoking in asynchronous mode, then it will return the id of the 
+ * 		underlying <i>AjxRpcRequest</i> object. Else if invoked synchronously, if
+ * 		there is no error (i.e. we get a HTTP result code of 200 from the server),
+ * 		an object with the following attributes is returned
+ * 		<ul>
+ * 		<li>text - the string response text</li>
+ * 		<li>xml - the string response xml</li>
+ * 		<li>success - boolean set to true</li>
+ * 		</ul>
+ * 		If there is an eror, then the following will be returned
+ * 		<ul>
+ * 		<li>text - the string response text<li>
+ * 		<li>xml - the string response xml </li>
+ * 		<li>success - boolean set to false </li>
+ * 		<li>status - http status</li>
+ * 		</ul>
+ * @type Object
+ * 
+ * @throws AjxException.NETWORK_ERROR, AjxException.UNKNOWN_ERROR
+ * 
+ * @see AjxRpcRequest#invoke
+ * 
+ * @requires AjxDebug
+ * @requires AjxException
+ * @requires AjxRpcRequest
+ */
 AjxRpc.invoke =
 function(requestStr, serverUrl, requestHeaders, callback, useGet, timeout) {
 
 	var asyncMode = (callback != null);
-	var rpcCtxt = AjxRpc._getFreeRpcCtxt();
+	var rpcCtxt = AjxRpc.__getFreeRpcCtxt();
 
 	try {
 	 	var response = rpcCtxt.req.invoke(requestStr, serverUrl, requestHeaders, callback, useGet, timeout);
@@ -64,17 +121,38 @@ function(requestStr, serverUrl, requestHeaders, callback, useGet, timeout) {
 	return response;
 };
 
-/*
-* Factory method for getting context objects.
-*/
-AjxRpc._getFreeRpcCtxt = 
+/**
+ * Returns the request from the RPC context with the given ID.
+ *
+ * @param {String} id RPC context ID
+ * 
+ * @return The <i>AjxRpcRequest</i> object associated with <code>id</code> or null
+ * 		if no object exists for the supplied id
+ * @type AjxRpcRequest
+ */
+AjxRpc.getRpcRequest = 
+function(id) {
+	for (var i = 0; i < AjxRpc.__rpcCache.length; i++) {
+		var rpcCtxt = AjxRpc.__rpcCache[i];
+		if (rpcCtxt.id == id)
+			return rpcCtxt.req;
+	}
+	return null;
+};
+
+
+/**
+ * Factory method for getting context objects.
+ * @private
+ */
+AjxRpc.__getFreeRpcCtxt = 
 function() {
 
 	var rpcCtxt = null;
 	
 	// See if we have one in the pool that's now free
-	for (var i = 0; i < AjxRpc._rpcCache.length; i++) {
-		rpcCtxt = AjxRpc._rpcCache[i];
+	for (var i = 0; i < AjxRpc.__rpcCache.length; i++) {
+		rpcCtxt = AjxRpc.__rpcCache[i];
 		if (!rpcCtxt.busy) {
 			DBG.println(AjxDebug.DBG1, "Found free RPC context: " + rpcCtxt.id);
 			break;
@@ -82,18 +160,18 @@ function() {
 	}
 	
 	// If there's no free context available, create one
-	if (i == AjxRpc._rpcCache.length) {
-		if (AjxRpc._rpcCache.length == AjxRpc._RPC_CACHE_MAX) {
+	if (i == AjxRpc.__rpcCache.length) {
+		if (AjxRpc.__rpcCache.length == AjxRpc.__RPC_CACHE_MAX) {
 			DBG.println(AjxDebug.DBG1, "Out of RPC contexts");
-			throw new AjxException("Out of RPC cache", AjxException.OUT_OF_RPC_CACHE, "ZmCsfeCommand._getFreeRpcCtxt");
-		} else if (i > 0 && (i % AjxRpc._RPC_REAP_COUNT == 0)) {
+			throw new AjxException("Out of RPC cache", AjxException.OUT_OF_RPC_CACHE, "AjxRpc.__getFreeRpcCtxt");
+		} else if (i > 0 && (i % AjxRpc.__RPC_REAP_COUNT == 0)) {
 			DBG.println(AjxDebug.DBG1, i + " busy RPC contexts");
-			AjxRpc._reap();
+			AjxRpc.__reap();
 		}
-		var id = "_rpcCtxt_" + i;
-		rpcCtxt = new _RpcCtxt(id);
+		var id = "__RpcCtxt_" + i;
+		rpcCtxt = new __RpcCtxt(id);
 		DBG.println(AjxDebug.DBG1, "Created RPC " + id);
-		AjxRpc._rpcCache.push(rpcCtxt);
+		AjxRpc.__rpcCache.push(rpcCtxt);
 	}
 	rpcCtxt.busy = true;
 	rpcCtxt.timestamp = (new Date()).getTime();
@@ -101,30 +179,16 @@ function() {
 };
 
 /**
-* Returns the request from the RPC context with the given ID.
-*
-* @param id		[string]	RPC context ID
-*/
-AjxRpc.getRpcRequest = 
-function(id) {
-	for (var i = 0; i < AjxRpc._rpcCache.length; i++) {
-		var rpcCtxt = AjxRpc._rpcCache[i];
-		if (rpcCtxt.id == id)
-			return rpcCtxt.req;
-	}
-	return null;
-};
-
-/*
-* Frees up busy contexts that are older than a certain age.
-*/
-AjxRpc._reap =
+ * Frees up busy contexts that are older than a certain age.
+ * @private
+ */
+AjxRpc.__reap =
 function() {
 	var time = (new Date()).getTime();
-	for (var i = 0; i < AjxRpc._rpcCache.length; i++) {
-		var rpcCtxt = AjxRpc._rpcCache[i];
-		if (rpcCtxt.timestamp + AjxRpc._RPC_REAP_AGE < time) {
-			DBG.println(AjxDebug.DBG1, "AjxRpc._reap: cleared RPC context " + rpcCtxt.id);
+	for (var i = 0; i < AjxRpc.__rpcCache.length; i++) {
+		var rpcCtxt = AjxRpc.__rpcCache[i];
+		if (rpcCtxt.timestamp + AjxRpc.__RPC_REAP_AGE < time) {
+			DBG.println(AjxDebug.DBG1, "AjxRpc.__reap: cleared RPC context " + rpcCtxt.id);
 			rpcCtxt.req.cancel();
 			rpcCtxt.busy = false;
 		}
@@ -133,12 +197,14 @@ function() {
 };
 
 /**
-* Wrapper for a request context.
+* Wrapper class for a request context.
 *
-* @param id		unique ID for this context
+* @param {String|Int} id Unique ID for this context
+* @private
 */
-function _RpcCtxt(id) {
+function __RpcCtxt(id) {
 	this.id = id;
-	this.req = new AjxRpcRequest(id, this);
+	this.req = new AjxRpcRequest(id);
+	this.req.__setCtxt(this)
 	this.busy = false;
 };
--- Ajax/WebRoot/js/net/AjxRpcRequest.js
+++ Ajax/WebRoot/js/net/AjxRpcRequest.js
@@ -16,110 +16,179 @@
 
 
 /**
-* Creates a new AjxRpcRequest. The request object is an ActiveX object
-* for IE, and an XMLHttpRequest object otherwise.
-* @constructor
-* @class
-* This class represents an XML HTTP request, hiding differences between
-* browsers. The internal request object depends on the browser.
-*
-* @param id		[string]	unique ID for this request
-* @param ctxt	[_RpcCtxt]	owning context
-**/
-function AjxRpcRequest(id, ctxt) {
-	if (!AjxRpcRequest._inited) {
-		AjxRpcRequest._init();
+ * @constructor
+ * @class
+ * This class encapsulates the XML HTTP request, hiding differences between
+ * browsers. The internal request object depends on the browser. While it is 
+ * possible to use this class directly, <i>AjxRpc</i> provides a managed interface
+ * to this class 
+ *
+ * @author Ross Dargahi
+ * @author Conrad Damon
+ * 
+ * @param {String} Optional ID to identify this object (optional)
+ * 
+ * @see AjxRpc
+ * 
+ * @requires AjxCallback
+ * @requires AjxDebug
+ * @requires AjxEnv
+ * @requires AjxException
+ * @requires AjxTimedAction
+ */
+function AjxRpcRequest(id) {
+	if (!AjxRpcRequest.__inited) {
+		AjxRpcRequest.__init();
     }
 
+	/** (optional) id for this object.
+	 * @type String|Int */
     this.id = id;
-	this.ctxt = ctxt;
 	if (AjxEnv.isIE) {
-		this._httpReq = new ActiveXObject(AjxRpcRequest._msxmlVers);
+		/** private*/
+		this.__httpReq = new ActiveXObject(AjxRpcRequest.__msxmlVers);
 	} else if (AjxEnv.isSafari || AjxEnv.isNav) {
-		this._httpReq =  new XMLHttpRequest();
+		/** private*/
+		this.__httpReq =  new XMLHttpRequest();
 	}
 }
 
-AjxRpcRequest._inited = false;
-AjxRpcRequest._msxmlVers = null;
+/**
+ * Timed out exception
+ * @type Int
+ */
 AjxRpcRequest.TIMEDOUT = -1000;
 
+/** @private */
+AjxRpcRequest.__inited = false;
+
+/** @private */
+AjxRpcRequest.__msxmlVers = null;
+
+/**
+ * @return class name
+ * @type String
+ */
 AjxRpcRequest.prototype.toString = 
 function() {
 	return "AjxRpcRequest";
 };
 
 /**
-* Sends this request to the target URL. If there is a callback, the request is
-* performed asynchronously.
-*
-* @param requestStr		[string]		HTTP request string/document
-* @param serverUrl		[string]		request target
-* @param requestHeaders	[Array]*		HTTP request headers
-* @param callback		[AjxCallback]*	callback (for async requests)
-* @param useGet			[boolean]*		if true use GET method, else use POST
-*/
+ * Sends this request to the target URL. If there is a callback, the request is
+ * performed asynchronously.
+ * 
+ * @param {String} requestStr HTTP request string/document
+ * @param {String} serverUrl request target 
+ * @param {Array} requestHeaders Array of HTTP request headers (optional)
+ * @param {AjxCallback} callback callback for asynchronous requests. This callback 
+ * 		will be invoked when the requests completes. It will be passed the same
+ * 		values as when this method is invoked synchronously (see the return values
+ * 		below) with the exception that if the call times out (see timeout param 
+ * 		below), then the object passed to the callback will be the same as in the 
+ * 		error case with the exception that the status will be set to 
+ * 		<code>AjxRpcRequest.TIMEDOUT</code>
+ * @param {Boolean} useGet if true use get method, else use post. If ommitted
+ * 		defaults to post
+ * @param {Int} timeout Timeout (in milliseconds) after which the request is 
+ * 		cancelled (optional)
+ * 
+ * @return If invoking in asynchronous mode, then it will return the id of the 
+ * 		underlying <i>AjxRpcRequest</i> object. Else if invoked synchronously, if
+ * 		there is no error (i.e. we get a HTTP result code of 200 from the server),
+ * 		an object with the following attributes is returned
+ * 		<ul>
+ * 		<li>text - the string response text</li>
+ * 		<li>xml - the string response xml</li>
+ * 		<li>success - boolean set to true</li>
+ * 		</ul>
+ * 		If there is an eror, then the following will be returned
+ * 		<ul>
+ * 		<li>text - the string response text<li>
+ * 		<li>xml - the string response xml </li>
+ * 		<li>success - boolean set to false </li>
+ * 		<li>status - http status</li>
+ * 		</ul>
+ * @type Object
+ * 
+ * @throws AjxException.NETWORK_ERROR, AjxException.UNKNOWN_ERROR
+ * 
+ * @see AjxRpc#invoke
+ */
 AjxRpcRequest.prototype.invoke =
 function(requestStr, serverUrl, requestHeaders, callback, useGet, timeout) {
 
 	var asyncMode = (callback != null);
 	
 	// An exception here will be caught by AjxRpc.invoke
-	this._httpReq.open((useGet) ? "get" : "post", serverUrl, asyncMode);
+	this.__httpReq.open((useGet) ? "get" : "post", serverUrl, asyncMode);
 
 	if (asyncMode) {
-		this._callback = callback;
+		this.__callback = callback;
         if(timeout) {
-            var action = new AjxTimedAction(this, AjxRpcRequest._handleTimeout, {callback: callback, req: this});
+            var action = new AjxTimedAction(this, AjxRpcRequest.__handleTimeout, {callback: callback, req: this});
             callback._timedActionId = AjxTimedAction.scheduleAction(action, timeout);
         }
         var tempThis = this;
 		DBG.println(AjxDebug.DBG3, "Async RPC request");
-		this._httpReq.onreadystatechange = function(ev) {AjxRpcRequest._handleResponse(tempThis, callback);};
+		this.__httpReq.onreadystatechange = function(ev) {AjxRpcRequest.__handleResponse(tempThis, callback);};
 	} else {
 		// IE appears to run handler even on sync requests, so we need to clear it
-		this._httpReq.onreadystatechange = function(ev) {};
+		this.__httpReq.onreadystatechange = function(ev) {};
 	}
 
 	if (requestHeaders) {
 		for (var i in requestHeaders) {
-			this._httpReq.setRequestHeader(i, requestHeaders[i]);
+			this.__httpReq.setRequestHeader(i, requestHeaders[i]);
 			DBG.println(AjxDebug.DBG3, "Async RPC request: Add header " + i + " - " + requestHeaders[i]);
 		}
 	}
 	
-	this._httpReq.send(requestStr);
+	this.__httpReq.send(requestStr);
 	if (asyncMode) {
 		return this.id;
 	} else {
-		if (this._httpReq.status == 200) {
-			return {text: this._httpReq.responseText, xml: this._httpReq.responseXML, success: true};
+		if (this.__httpReq.status == 200) {
+			return {text: this.__httpReq.responseText, xml: this.__httpReq.responseXML, success: true};
 		} else {
-			return {text: this._httpReq.responseText, xml: this._httpReq.responseXML, success: false, status: this._httpReq.status};
+			return {text: this.__httpReq.responseText, xml: this.__httpReq.responseXML, success: false, status: this.__httpReq.status};
 		}
 	}
 };
 
-/*
-* Handler that runs when an asynchronous request timesout.
-*
-* @param callback	[AjxCallback]		callback to run after timeout
-*/
-AjxRpcRequest._handleTimeout =
+/**
+ * Cancels a pending request 
+ */
+AjxRpcRequest.prototype.cancel =
+function() {
+	DBG.println(AjxDebug.DBG1, "Aborting HTTP request");
+	this.__httpReq.abort();
+};
+
+/**
+ * Handler that runs when an asynchronous request timesout.
+ *
+ * @param {AjxCallback} callback callback to run after timeout
+ * 
+ * @private
+ */
+AjxRpcRequest.__handleTimeout =
 function(args) {
     DBG.println(AjxDebug.DBG3, "Async RPC request: _handleTimeout");
     args.req.cancel();
     args.callback.run( {text: null, xml: null, success: false, status: AjxRpcRequest.TIMEDOUT} );
 };
 
-/*
-* Handler that runs when an asynchronous response has been received. It runs a
-* callback to initiate the response handling.
-*
-* @param req		[AjxRpcRequest]		request that generated the response
-* @param callback	[AjxCallback]		callback to run after response is received
-*/
-AjxRpcRequest._handleResponse =
+/**
+ * Handler that runs when an asynchronous response has been received. It runs a
+ * callback to initiate the response handling.
+ *
+ * @param req		[AjxRpcRequest]		request that generated the response
+ * @param callback	[AjxCallback]		callback to run after response is received
+ * 
+ * @private
+ */
+AjxRpcRequest.__handleResponse =
 function(req, callback) {
 	if (!req) {
 		// If IE receives a 500 error, the object reference can be lost
@@ -127,28 +196,36 @@ function(req, callback) {
 		callback.run( {text: null, xml: null, success: false, status: 500} );
 		return;
 	}
-    DBG.println(AjxDebug.DBG3, "Async RPC request: ready state = " + req._httpReq.readyState);
-	if (req._httpReq.readyState == 4) {
+    DBG.println(AjxDebug.DBG3, "Async RPC request: ready state = " + req.__httpReq.readyState);
+	if (req.__httpReq.readyState == 4) {
         if(callback._timedActionId !== null) {
             AjxTimedAction.cancelAction(callback._timedActionId);
         }
-		DBG.println(AjxDebug.DBG3, "Async RPC request: HTTP status = " + req._httpReq.status);
-        if (req._httpReq.status == 200) {
-			callback.run( {text: req._httpReq.responseText, xml: req._httpReq.responseXML, success: true} );				
+		DBG.println(AjxDebug.DBG3, "Async RPC request: HTTP status = " + req.__httpReq.status);
+        if (req.__httpReq.status == 200) {
+			callback.run( {text: req.__httpReq.responseText, xml: req.__httpReq.responseXML, success: true} );				
 		} else {
-			callback.run( {text: req._httpReq.responseText, xml: req._httpReq.responseXML, success: false, status: req._httpReq.status} );
+			callback.run( {text: req.__httpReq.responseText, xml: req.__httpReq.responseXML, success: false, status: req.__httpReq.status} );
 		}
-		req.ctxt.busy = false;
+		
+		if (req.__ctxt)
+			req.__ctxt.busy = false;
 	}
 };
 
-AjxRpcRequest.prototype.cancel =
-function() {
-	DBG.println(AjxDebug.DBG1, "Aborting HTTP request");
-	this._httpReq.abort();
+
+/**
+ * This method is called by <i>AjxRpc</i> which can be considered a "friend" clas
+ * It is used to set the __RpcCtxt associated with this object
+ * @private
+ */
+AjxRpcRequest.prototype.__setCtxt =
+function(ctxt) {
+	this.__ctxt = ctxt;
 };
 
-AjxRpcRequest._init =
+/** @private */
+AjxRpcRequest.__init =
 function() {
 	if (AjxEnv.isIE) {
 		var msxmlVers = ["MSXML2.XMLHTTP.4.0", "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "Microsoft.XMLHTTP"];
@@ -156,15 +233,16 @@ function() {
 			try {
 				// search for the xml version on user's machine
 				var x = new ActiveXObject(msxmlVers[i]);
-				AjxRpcRequest._msxmlVers = msxmlVers[i];
+				AjxRpcRequest.__msxmlVers = msxmlVers[i];
 				break;
 			} catch (ex) {
 				// do nothing
 			}
 		}
-		if (!AjxRpcRequest._msxmlVers) {
+		if (!AjxRpcRequest.__msxmlVers) {
 			throw new AjxException("MSXML not installed", AjxException.INTERNAL_ERROR, "AjxRpc._init");
         }
     }
-	AjxRpcRequest._inited = true;
+	AjxRpcRequest.__inited = true;
 };
+
