771997219cbb74d07cec847e971d87708b2d6ad4
 Unknown Perforce User <mihai>
2006-05-11 14:09:50 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=7400 Bug: 7400 ZmHtmlEditor quick test files

---
+++ ZimbraWebClient/WebRoot/test/htmleditor/EditorTest.js
@@ -0,0 +1,38 @@
+function EditorTest() {};
+
+EditorTest.content = [ "<h1>Test</h1><p>a paragraph here</p>",
+		       "<table width='100%' style='border: 1px solid #000'>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "<tr> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> <td style='border: 1px solid #000'><br /></td> </tr>",
+		       "</table>",
+		       "<p>another paragraph here</p>" ].join("");
+
+EditorTest.run = function() {
+	DBG = new AjxDebug(AjxDebug.NONE, null, false);
+
+	var appCtxt = new ZmAppCtxt();
+	var shell = new DwtShell();
+	appCtxt.setShell(shell);
+
+	// dirty hacks to be able to run without this
+	var kbMgr = shell.getKeyboardMgr();
+	kbMgr.registerKeyMap(new ZmKeyMap());
+	kbMgr.registerGlobalKeyActionHandler({
+		    handleKeyAction : function() {}
+		});
+	kbMgr.__currTabGroup = {
+	    setFocusMember : function() { return false; }
+	};
+
+	var cont = new DwtComposite(shell, null, DwtControl.ABSOLUTE_STYLE);
+	cont.zShow(true);
+	cont.setLocation(10, 10);
+	cont.setSize(800, 550);
+
+	var editor = new ZmHtmlEditor(cont, null, EditorTest.content, DwtHtmlEditor.HTML, appCtxt);
+	editor.setSize(800, 500);
+};
