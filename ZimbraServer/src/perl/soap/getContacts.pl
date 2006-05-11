9d93a105c4cc97bf3af18d7a4c55b5ac6ce4cf16
 Unknown Perforce User <tim>
2006-05-11 03:12:15 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=6355 bug: 6355
    SearchRequest now returns the 'fileAsStr' attribute for contact hits.  It also honors the 'fetch' attribute and will return the full contact for the first result if it is set.
    
    the <GetContactsRequest> soap has been updated so that it proxies correctly for remote ID's and folders.

---
+++ ZimbraServer/src/perl/soap/getContacts.pl
@@ -0,0 +1,97 @@
+#!/usr/bin/perl -w
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 ("License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.zimbra.com/license
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is: Zimbra Collaboration Suite Server.
+# 
+# The Initial Developer of the Original Code is Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+
+use strict;
+use lib '.';
+
+use LWP::UserAgent;
+use Getopt::Long;
+use XmlDoc;
+use Soap;
+use ZimbraSoapTest;
+
+# specific to this app
+my ($sortBy, $folder, $id, $attrs);
+$sortBy = "dateDesc";
+
+#standard options
+my ($user, $pw, $host, $help); #standard
+GetOptions("u|user=s" => \$user,
+           "pw=s" => \$pw,
+           "h|host=s" => \$host,
+           "help|?" => \$help,
+           # add specific params below:
+           "sort=s" => \$sortBy,
+           "id=s" => \$id,
+           "attrs=s" => \$attrs,
+           "folder=s" => \$folder);
+
+
+
+if (!defined($user) || defined($help)) {
+    my $usage = <<END_OF_USAGE;
+    
+USAGE: $0 -u USER [-s SORT] [-i id_list] [-f folder] [-a attrs]
+    SORT = dateDesc|dateAsc|subjDesc|subjAsc|nameDesc|nameAsc|score
+    TYPES = message|conversation|contact|appointment
+END_OF_USAGE
+    die $usage;
+}
+
+my $z = ZimbraSoapTest->new($user, $host, $pw);
+$z->doStdAuth();
+
+my $d = new XmlDoc;
+my $searchName = "GetContactsRequest";
+
+my %args =  ( 
+             'sortBy' => $sortBy,
+            );
+
+if (defined($folder)) {
+  $args{"l"} = $folder;
+}
+  
+
+$d->start("GetContactsRequest", $Soap::ZIMBRA_MAIL_NS, \%args);
+{
+  if (defined $id) {
+    $d->add('cn', undef, { "id" => $id });
+  }
+  
+  if (defined($attrs)) {
+    foreach (split(/,/, $attrs)) {
+      $d->add('a', undef, { "n" => $_ } );
+    }
+  }
+}
+$d->end();                      # 'GetContactsRequest'
+
+my $response = $z->invokeMail($d->root());
+
+print "REQUEST:\n-------------\n".$z->to_string_simple($d);
+print "RESPONSE:\n--------------\n".$z->to_string_simple($response);
+
