0f2649308417662b5c57b65194dc22a8a95cac0e
 Unknown Perforce User <greg>
2006-06-09 21:26:06 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=7880 bug 7880 enable domain search based on access control

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/search/view/ZaSearchField.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/search/view/ZaSearchField.js
@@ -83,9 +83,11 @@ function() {
 	if(this._containedObject[ZaSearch.A_fResources] == "TRUE") {
 		objList.push(ZaSearch.RESOURCES);
 	}
-	if(this._containedObject[ZaSearch.A_fDomains] == "TRUE") {
-		objList.push(ZaSearch.DOMAINS);
-	}	
+	if(ZaSettings.DOMAINS_ENABLED) {
+		if(this._containedObject[ZaSearch.A_fDomains] == "TRUE") {
+			objList.push(ZaSearch.DOMAINS);
+		}	
+	}
 	params.types = objList;
 	params.query = ZaSearch.getSearchByNameQuery(this._containedObject[ZaSearch.A_query]);
 	
@@ -142,7 +144,9 @@ ZaSearchField.prototype.allFilterSelected = function (ev) {
 	this._containedObject[ZaSearch.A_fdistributionlists] = "TRUE";	
 	this._containedObject[ZaSearch.A_fAliases] = "TRUE";
 	this._containedObject[ZaSearch.A_fResources] = "TRUE";
-	this._containedObject[ZaSearch.A_fDomains] = "TRUE";	
+	if(ZaSettings.DOMAINS_ENABLED) {
+		this._containedObject[ZaSearch.A_fDomains] = "TRUE";	
+	}
 	this.setTooltipForSearchButton (ZaMsg.searchForAll);	
 }
 
@@ -175,10 +179,12 @@ ZaSearchField.prototype.resFilterSelected = function (ev) {
 }
 
 ZaSearchField.prototype.domainFilterSelected = function (ev) {
-	this.resetSearchFilter();
-	ev.item.parent.parent.setImage(ev.item.getImage());
-	this._containedObject[ZaSearch.A_fDomains] = "TRUE";
-	this.setTooltipForSearchButton (ZaMsg.searchForDomains);	
+	if(ZaSettings.DOMAINS_ENABLED) {
+		this.resetSearchFilter();
+		ev.item.parent.parent.setImage(ev.item.getImage());
+		this._containedObject[ZaSearch.A_fDomains] = "TRUE";
+		this.setTooltipForSearchButton (ZaMsg.searchForDomains);	
+	}
 }
 
 ZaSearchField.searchChoices = new XFormChoices([],XFormChoices.OBJECT_REFERENCE_LIST, null, "labelId");
@@ -189,7 +195,9 @@ ZaSearchField.prototype._getMyXForm = function() {
 	newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_DLS, ZaMsg.SearchFilter_DLs, ZaMsg.searchForDLs, "Group", "GroupDis", new AjxListener(this,this.dlFilterSelected)));		
 	newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_ALIASES, ZaMsg.SearchFilter_Aliases, ZaMsg.searchForAliases, "AccountAlias", "AccountAlias", new AjxListener(this, this.aliasFilterSelected)));		
 	newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_RESOURCES, ZaMsg.SearchFilter_Resources, ZaMsg.searchForResources, "Resource", "ResourceDis", new AjxListener(this, this.resFilterSelected)));		
-	newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_DOMAINS, ZaMsg.SearchFilter_Domains, ZaMsg.searchForDomains, "Domain", "DomainDis", new AjxListener(this, this.domainFilterSelected)));			
+	if(ZaSettings.DOMAINS_ENABLED) {
+		newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_DOMAINS, ZaMsg.SearchFilter_Domains, ZaMsg.searchForDomains, "Domain", "DomainDis", new AjxListener(this, this.domainFilterSelected)));			
+	}
 	newMenuOpList.push(new ZaOperation(ZaOperation.SEP));				
 	newMenuOpList.push(new ZaOperation(ZaOperation.SEARCH_ALL, ZaMsg.SearchFilter_All, ZaMsg.searchForAll, "SearchAll", "SearchAll", new AjxListener(this, this.allFilterSelected)));		
 	ZaSearchField.searchChoices.setChoices(newMenuOpList);
