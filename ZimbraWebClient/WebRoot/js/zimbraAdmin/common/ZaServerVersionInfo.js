1497b6b9138c1269c7bec1fe8ea5cdf15892ad81
 Unknown Perforce User <ccao>
2006-06-21 02:23:56 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=7395 bug: 7395 revert the checkin with version 10

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaServerVersionInfo.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaServerVersionInfo.js
@@ -55,11 +55,6 @@ ZaServerVersionInfo.load = function () {
 				var now = new Date();
 				ZaServerVersionInfo.licenseExpired = (now.getTime() > ZaServerVersionInfo.licenseExpirationDate.getTime());
 			}
-			//HC: Testing purpose only
-			ZaServerVersionInfo.licenseExists = true ;
-			ZaServerVersionInfo.licenseExpired = true ;
-			ZaServerVersionInfo.licenseExpirationDate = ZaServerVersionInfo._parseDate("20060501") ;
-			///END TESTING
 		} else {
 			ZaServerVersionInfo.licenseExpired = false;
 			ZaServerVersionInfo.licenseExists = false;
