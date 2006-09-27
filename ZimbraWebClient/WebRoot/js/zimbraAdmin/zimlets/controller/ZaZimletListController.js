f3affef6c060c352ec48a6f3f58212465e2e257a
 Unknown Perforce User <greg>
2006-09-27 01:59:23 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=8851 bug 8851 checkpoint

---
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/zimlets/controller/ZaZimletListController.js
@@ -0,0 +1,167 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: ZPL 1.2
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.2 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is: Zimbra Collaboration Suite Web Client
+ * 
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * Contributor(s):
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+* @constructor
+* @class ZaZimletListController
+* This is a singleton object that controls all the user interaction with the list of ZaZimlet objects
+* @author Greg Solovyev
+**/
+function ZaZimletListController(appCtxt, container, app) {
+	ZaListViewController.call(this, appCtxt, container, app,"ZaZimletListController");
+   	this._toolbarOperations = new Array();
+   	this._popupOperations = new Array();			
+	
+	this._helpURL = "/zimbraAdmin/adminhelp/html/WebHelp/managing_servers/managing_servers.htm";					
+}
+
+ZaZimletListController.prototype = new ZaListViewController();
+ZaZimletListController.prototype.constructor = ZaZimletListController;
+
+ZaController.initToolbarMethods["ZaZimletListController"] = new Array();
+ZaController.initPopupMenuMethods["ZaZimletListController"] = new Array();
+
+/**
+* @param list {ZaItemList} a list of ZaZimlet {@link ZaZimlet} objects
+**/
+ZaZimletListController.prototype.show = 
+function(list) {
+    if (!this._UICreated) {
+		this._createUI();
+	} 	
+	if (list != null)
+		this._contentView.set(list.getVector());
+	
+	this._app.pushView(ZaZimbraAdmin._ZIMLET_LIST_VIEW);			
+
+	this._removeList = new Array();
+	if (list != null)
+		this._list = list;
+		
+	this._changeActionsState();		
+}
+
+ZaZimletListController.initToolbarMethod =
+function () {
+   	this._toolbarOperations.push(new ZaOperation(ZaOperation.EDIT, ZaMsg.TBB_Edit, ZaMsg.SERTBB_Edit_tt, "Properties", "PropertiesDis", new AjxListener(this, ZaZimletListController.prototype._editButtonListener)));    	
+   	//this._toolbarOperations.push(new ZaOperation(ZaOperation.DELETE, ZaMsg.TBB_Delete, ZaMsg.SERTBB_Delete_tt, "Delete", "DeleteDis", new AjxListener(this, ZaZimletListController.prototype._deleteButtonListener)));    	    	
+	this._toolbarOperations.push(new ZaOperation(ZaOperation.NONE));
+	this._toolbarOperations.push(new ZaOperation(ZaOperation.HELP, ZaMsg.TBB_Help, ZaMsg.TBB_Help_tt, "Help", "Help", new AjxListener(this, this._helpButtonListener)));				
+}
+ZaController.initToolbarMethods["ZaZimletListController"].push(ZaZimletListController.initToolbarMethod);
+
+ZaZimletListController.initPopupMenuMethod =
+function () {
+   	this._popupOperations.push(new ZaOperation(ZaOperation.EDIT, ZaMsg.TBB_Edit, ZaMsg.SERTBB_Edit_tt, "Properties", "PropertiesDis", new AjxListener(this, ZaZimletListController.prototype._editButtonListener)));    	
+   	//this._popupOperations.push(new ZaOperation(ZaOperation.DELETE, ZaMsg.TBB_Delete, ZaMsg.SERTBB_Delete_tt, "Delete", "DeleteDis", new AjxListener(this, ZaZimletListController.prototype._deleteButtonListener)));    	    	
+}
+ZaController.initPopupMenuMethods["ZaZimletListController"].push(ZaZimletListController.initPopupMenuMethod);
+
+ZaZimletListController.prototype._createUI = function () {
+	try {
+		var elements = new Object();
+		this._contentView = new ZaZimletListView(this._container);
+		this._initToolbar();
+		if(this._toolbarOperations && this._toolbarOperations.length) {
+			this._toolbar = new ZaToolBar(this._container, this._toolbarOperations); 
+			elements[ZaAppViewMgr.C_TOOLBAR_TOP] = this._toolbar;
+		}
+		this._initPopupMenu();
+		if(this._popupOperations && this._popupOperations.length) {
+			this._actionMenu =  new ZaPopupMenu(this._contentView, "ActionMenu", null, this._popupOperations);
+		}
+		elements[ZaAppViewMgr.C_APP_CONTENT] = this._contentView;
+		this._app.createView(ZaZimbraAdmin._ZIMLET_LIST_VIEW, elements);
+
+
+		this._contentView.addSelectionListener(new AjxListener(this, this._listSelectionListener));
+		this._contentView.addActionListener(new AjxListener(this, this._listActionListener));			
+		this._removeConfirmMessageDialog = new ZaMsgDialog(this._app.getAppCtxt().getShell(), null, [DwtDialog.YES_BUTTON, DwtDialog.NO_BUTTON], this._app);					
+	
+		
+		this._UICreated = true;
+	} catch (ex) {
+		this._handleException(ex, "ZaZimletListController.prototype._createUI", null, false);
+		return;
+	}	
+}
+
+ZaZimletListController.prototype.set = 
+function(zimletList) {
+	this.show(zimletList);
+}
+
+
+/**
+* This listener is called when the item in the list is double clicked. It call ZaZimletController.show method
+* in order to display the Zimlet View
+**/
+ZaZimletListController.prototype._listSelectionListener =
+function(ev) {
+	if (ev.detail == DwtListView.ITEM_DBL_CLICKED) {
+		if(ev.item) {
+			this._selectedItem = ev.item;
+			this._app.getZimletController().show(ev.item);
+		}
+	} else {
+		this._changeActionsState();	
+	}
+}
+
+ZaZimletListController.prototype._listActionListener =
+function (ev) {
+	this._changeActionsState();
+	this._actionMenu.popup(0, ev.docX, ev.docY);
+}
+/**
+* This listener is called when the Edit button is clicked. 
+* It call ZaZimletController.show method
+* in order to display the Zimlet View
+**/
+ZaZimletListController.prototype._editButtonListener =
+function(ev) {
+	if(this._contentView.getSelectionCount() == 1) {
+		var item = this._contentView.getSelection()[0];
+		this._app.getZimletController().show(item);
+	}
+}
+
+ZaZimletListController.prototype._changeActionsState = 
+function () {
+	var cnt = this._contentView.getSelectionCount();
+	if(cnt == 1) {
+		var opsArray = [ZaOperation.EDIT];
+		this._toolbar.enable(opsArray, true);
+		this._actionMenu.enable(opsArray, true);
+	} else if (cnt > 1){
+		var opsArray1 = [ZaOperation.EDIT];
+		this._toolbar.enable(opsArray1, false);
+		this._actionMenu.enable(opsArray1, false);
+	} else {
+		var opsArray = [ZaOperation.EDIT];
+		this._toolbar.enable(opsArray, false);
+		this._actionMenu.enable(opsArray, false);
+	}
+}
---
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/zimlets/model/ZaZimlet.js
@@ -0,0 +1,154 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * The Original Code is: Zimbra Network
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+/**
+* @class ZaZimlet
+* @contructor ZaZimlet
+* @param ZaApp app
+* this class is a model for managing Zimlets
+* @author Greg Solovyev
+**/
+function ZaZimlet(app) {
+	ZaItem.call(this, app);
+	this.label = "";
+	this[ZaModel.currentStep] = 1;
+}
+ZaZimlet.prototype = new ZaItem;
+ZaZimlet.prototype.constructor = ZaZimlet;
+ZaItem.loadMethods["ZaZimlet"] = new Array();
+ZaItem.initMethods["ZaZimlet"] = new Array();
+
+ZaZimlet.A_name = "name";
+ZaZimlet.A_zimbraZimletEnabled = "zimbraZimletEnabled";
+ZaZimlet.A_zimbraZimletPriority = "zimbraZimletPriority";
+ZaZimlet.A_zimbraZimletIsExtension = "zimbraZimletIsExtension";
+ZaZimlet.A_zimbraZimletKeyword = "zimbraZimletKeyword";
+ZaZimlet.A_zimbraZimletVersion = "zimbraZimletVersion";
+ZaZimlet.A_zimbraZimletDescription = "zimbraZimletDescription";
+ZaZimlet.A_zimbraZimletIndexingEnabled = "zimbraZimletIndexingEnabled";
+ZaZimlet.A_zimbraZimletStoreMatched = "zimbraZimletStoreMatched";
+ZaZimlet.A_zimbraZimletHandlerClass = "zimbraZimletHandlerClass";
+ZaZimlet.A_zimbraZimletHandlerConfig = "zimbraZimletHandlerConfig";
+ZaZimlet.A_zimbraZimletContentObject = "zimbraZimletContentObject";
+ZaZimlet.A_zimbraZimletPanelItem = "zimbraZimletPanelItem";
+ZaZimlet.A_zimbraZimletScript = "zimbraZimletScript";
+ZaZimlet.A_zimbraZimletServerIndexRegex = "zimbraZimletServerIndexRegex";
+ZaZimlet.EXCLUDE_MAIL = "mail";
+ZaZimlet.EXCLUDE_EXTENSIONS = "extension";
+ZaZimlet.EXCLUDE_NONE = "none";	
+
+ZaZimlet.prototype.toString = function() {
+	return this.name;
+}
+
+ZaZimlet.getAll =
+function(app, exclude) {
+	var exc = exclude ? exclude : "none";
+	var soapDoc = AjxSoapDoc.create("GetAllZimletsRequest", "urn:zimbraAdmin", null);	
+	soapDoc.getMethod().setAttribute("exclude", exc);	
+	var command = new ZmCsfeCommand();
+	var params = new Object();
+	params.soapDoc = soapDoc;	
+	var resp = command.invoke(params).Body.GetAllZimletsResponse;	
+	var list = new ZaItemList(ZaZimlet, app);
+	list.loadFromJS(resp);	
+	return list;
+}
+
+/**
+* @param mods - map of modified attributes
+* modifies object's information in the database
+**/
+ZaZimlet.prototype.modify =
+function(mods) {
+	var soapDoc = AjxSoapDoc.create("ModifyZimletRequest", "urn:zimbraAdmin", null);
+	soapDoc.set("id", this.id);
+	for (var aname in mods) {
+		if (mods[aname] instanceof Array) {
+			var array = mods[aname];
+			if (array.length > 0) {
+				for (var i = 0; i < array.length; i++) {
+					var attr = soapDoc.set("a", array[i]);
+					attr.setAttribute("n", aname);
+				}
+			}
+			else {
+				var attr = soapDoc.set("a");
+				attr.setAttribute("n", aname);
+			}
+		}
+		else {
+			var attr = soapDoc.set("a", mods[aname]);
+			attr.setAttribute("n", aname);
+		}
+	}
+	var command = new ZmCsfeCommand();
+	var params = new Object();
+	params.soapDoc = soapDoc;	
+	var resp = command.invoke(params).Body.ModifyZimletResponse;		
+}
+
+/**
+* Returns HTML for a tool tip for this domain.
+*/
+ZaZimlet.prototype.getToolTip =
+function() {
+	// update/null if modified
+	if (!this._toolTip) {
+		var html = new Array(20);
+		var idx = 0;
+		html[idx++] = "<table cellpadding='0' cellspacing='0' border='0'>";
+		html[idx++] = "<tr valign='center'><td colspan='2' align='left'>";
+		html[idx++] = "<div style='border-bottom: 1px solid black; white-space:nowrap; overflow:hidden;width:350'>";
+		html[idx++] = "<table cellpadding='0' cellspacing='0' border='0' style='width:100%;'>";
+		html[idx++] = "<tr valign='center'>";
+		html[idx++] = "<td><b>" + AjxStringUtil.htmlEncode(this.name) + "</b></td>";
+		html[idx++] = "<td align='right'>";
+		html[idx++] = AjxImg.getImageHtml("ZaZimlet");		
+		html[idx++] = "</td>";
+		html[idx++] = "</table></div></td></tr>";
+		html[idx++] = "<tr></tr>";
+		idx = this._addAttrRow(ZaItem.A_description, html, idx);		
+		idx = this._addAttrRow(ZaItem.A_zimbraId, html, idx);
+		html[idx++] = "</table>";
+		this._toolTip = html.join("");
+	}
+	return this._toolTip;
+}
+
+ZaZimlet.prototype.remove = 
+function() {
+	var soapDoc = AjxSoapDoc.create("UndeployZimletRequest", "urn:zimbraAdmin", null);
+	soapDoc.set("id", this.id);
+	var command = new ZmCsfeCommand();
+	var params = new Object();
+	params.soapDoc = soapDoc;	
+	var resp = command.invoke(params);	
+}
+
+ZaZimlet.prototype.refresh = 
+function() {
+	this.load();	
+}
+
+ZaZimlet.loadMethod = 
+function(by, val, withConfig) {
+	var _val = val ? val : this.name
+	var soapDoc = AjxSoapDoc.create("GetZimletRequest", "urn:zimbraAdmin", null);
+	var elZimlet = soapDoc.set("zimlet", "");
+	elZimlet.setAttribute("name", val);
+	var command = new ZmCsfeCommand();
+	var params = new Object();
+	params.soapDoc = soapDoc;	
+	params.asyncMode = false;
+	resp = command.invoke(params);		
+	this.initFromJS(resp.Body.GetZimletResponse.zimlet[0]);
+}
+ZaItem.loadMethods["ZaZimlet"].push(ZaZimlet.loadMethod);
