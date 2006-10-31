2153c6e0ff7561e5ccc5fe5e13b7b3665d49e15d
 Unknown Perforce User <mihai>
2006-10-31 16:04:20 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=10667 Bug: 10667 - display Alexa Thumbnails
           - added HMAC/SHA1 library (required for Alexa authentication)
    
           - "fixed" ProxyServlet for servers that don't bother sending a
      content-type line
    
           - the URL Zimlet will try to display Alexa Thumbnails if the
      alexaThumbnailId and alexaThumbnailKey properties are filled in
      config_template.xml
    
             It will maintain a cache for images to reduce costs.  The images in
             cache expire in 10 minutes (not configurable for now).
    
             If an Alexa thumbnail can't be retrieved, we're falling back to
             Websnapr.

--- Zimlet/src/zimlet/com_zimbra_url/url.js
+++ Zimlet/src/zimlet/com_zimbra_url/url.js
@@ -1,25 +1,25 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Version: ZPL 1.1
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.1 ("License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.zimbra.com/license
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
  * the License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * The Original Code is: Zimbra Collaboration Suite Web Client
- * 
+ *
  * The Initial Developer of the Original Code is Zimbra, Inc.
  * Portions created by Zimbra are Copyright (C) 2005 Zimbra, Inc.
  * All Rights Reserved.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */
 
@@ -33,6 +33,14 @@ Com_Zimbra_Url.prototype.init =
 function() {
 	// Pre-load placeholder image
 	(new Image()).src = this.getResource('blank_pixel.gif');
+
+	this._alexaId = this.getConfig("alexaThumbnailId");
+	if (this._alexaId) {
+		this._alexaId = AjxStringUtil.trim(this._alexaId);
+		// console.log("Found Alexa ID: %s", this._alexaId);
+		this._alexaKey = AjxStringUtil.trim(this.getConfig("alexaThumbnailKey"));
+		// console.log("Found Alexa Key: %s", this._alexaKey);
+	}
 };
 
 // Const
@@ -78,6 +86,14 @@ function(spanElement, obj, context, canvas) {
 	if (/^\s*true\s*$/i.test(this.getConfig("stripUrls"))) {
 		url = url.replace(/[?#].*$/, "");
 	}
+
+	if (this._alexaId)
+		this._showAlexaThumbnail(url, canvas);
+	else
+		this._showFreeThumbnail(url, canvas);
+};
+
+Com_Zimbra_Url.prototype._showFreeThumbnail = function(url, canvas) {
 	var html = [];
 	var i = 0;
 
@@ -93,4 +109,92 @@ function(spanElement, obj, context, canvas) {
 	html[i++] = ")'/>";
 
 	canvas.innerHTML = html.join("");
+};
+
+Com_Zimbra_Url.ALEXA_THUMBNAIL_CACHE = {};
+Com_Zimbra_Url.ALEXA_CACHE_EXPIRES = 10 * 60 * 1000; // 10 minutes
+
+Com_Zimbra_Url.prototype._showAlexaThumbnail = function(url, canvas) {
+	canvas.innerHTML = [ "<table style='width: 200px; height: 150px; border-collapse: collapse' cellspacing='0' cellpadding='0'><tr><td align='center'>",
+			     ZmMsg.fetchingAlexaThumbnail,
+			     "</td></tr></table>" ].join("");
+
+	// check cache first
+	var cached = Com_Zimbra_Url.ALEXA_THUMBNAIL_CACHE[url];
+	if (cached) {
+		var diff = new Date().getTime() - cached.timestamp;
+		if (diff < Com_Zimbra_Url.ALEXA_CACHE_EXPIRES) {
+			// cached image should still be good, let's use it
+			var html = [ "<img src='", cached.img, "' />" ].join("");
+			canvas.firstChild.rows[0].cells[0].innerHTML = html;
+			return;
+		} else {
+			// expired
+			delete Com_Zimbra_Url.ALEXA_THUMBNAIL_CACHE[url];
+		}
+	}
+
+	var now = new Date(), pad = Com_Zimbra_Url.zeroPad;
+	var timestamp =
+		pad(now.getUTCFullYear()  , 4) + "-" +
+		pad(now.getUTCMonth() + 1 , 2) + "-" +
+		pad(now.getUTCDate()	  , 2) + "T" +
+		pad(now.getUTCHours()	  , 2) + ":" +
+		pad(now.getUTCMinutes()	  , 2) + ":" +
+		pad(now.getUTCSeconds()	  , 2) + ".000Z";
+	// console.log("Timestamp: %s", timestamp);
+	var signature = this._computeAlexaSignature(timestamp);
+	// console.log("Computed signature: %s", signature);
+	var args = {
+		Service		: "AlexaSiteThumbnail",
+		Action		: "Thumbnail",
+		AWSAccessKeyId	: this._alexaId,
+		Timestamp	: timestamp,
+		Signature	: signature,
+		Size		: "Large",
+		Url		: url
+	};
+	var query = [];
+	for (var i in args)
+		query.push(i + "=" + AjxStringUtil.urlComponentEncode(args[i]));
+	query = "http://ast.amazonaws.com/xino/?" + query.join("&");
+	// console.log("Query URL: %s", query);
+	this.sendRequest(null, query, null, new AjxCallback(this, this._alexaDataIn,
+							    [ canvas, url, query ]),
+			 true);
+};
+
+Com_Zimbra_Url.prototype._computeAlexaSignature = function(timestamp) {
+	return AjxSHA1.b64_hmac_sha1(this._alexaKey, "AlexaSiteThumbnailThumbnail" + timestamp)
+		+ "=";		// guess what, it _has_ to end in '=' :-(
+};
+
+Com_Zimbra_Url.prototype._alexaDataIn = function(canvas, url, query, result) {
+	var xml = AjxXmlDoc.createFromDom(result.xml);
+	var res = xml.toJSObject(true /* drop namespace decls. */,
+				 false /* keep case */,
+				 true /* do attributes */);
+	res = res.Response;
+	if (res.ResponseStatus.StatusCode == "Success") {
+		if (res.ThumbnailResult.Thumbnail.Exists == "true") {
+			var html = [ "<img src='", res.ThumbnailResult.Thumbnail, "' />" ].join("");
+			// console.log("HTML: %s", html);
+			canvas.firstChild.rows[0].cells[0].innerHTML = html;
+
+			// cache it
+			Com_Zimbra_Url.ALEXA_THUMBNAIL_CACHE[url] = {
+				img	  : res.ThumbnailResult.Thumbnail,
+				timestamp : new Date().getTime()
+			};
+		} else
+			this._showFreeThumbnail(url, canvas);
+	} else
+		this._showFreeThumbnail(url, canvas);
+};
+
+Com_Zimbra_Url.zeroPad = function(number, width) {
+	var s = "" + number;
+	while (s.length < width)
+		s = "0" + s;
+	return s;
 };
