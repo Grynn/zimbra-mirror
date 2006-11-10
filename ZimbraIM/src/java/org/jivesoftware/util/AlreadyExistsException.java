7328f5c098fb36ec51ed9e8a30d5c8c761b61620
 Unknown Perforce User <tim>
2006-11-10 18:19:36 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=7074 bug: 7074
    Move Wildfire code into ZimbraIM project

---
+++ ZimbraIM/src/java/org/jivesoftware/util/AlreadyExistsException.java
@@ -0,0 +1,30 @@
+/**
+ * $RCSfile$
+ * $Revision: 42 $
+ * $Date: 2004-10-21 00:28:12 -0700 (Thu, 21 Oct 2004) $
+ *
+ * Copyright (C) 2004 Jive Software. All rights reserved.
+ *
+ * This software is published under the terms of the GNU Public License (GPL),
+ * a copy of which is included in this distribution.
+ */
+
+package org.jivesoftware.util;
+
+/**
+ * <p>Flags an exception when something to be created or added already exists.</p>
+ * <p>Use this class when it's not worth creating a unique
+ * xAlreadyExistsException class, or where the context of
+ * the call makes it obvious what type of object was not found.</p>
+ *
+ * @author Iain Shigeoka
+ */
+public class AlreadyExistsException extends Exception {
+
+    public AlreadyExistsException() {
+    }
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
