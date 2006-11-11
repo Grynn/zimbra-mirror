2d3515d2a58acaa607fbb71179dae191f25a489b
 Unknown Perforce User <sam>
2006-11-11 19:40:50 +0000

    bug:none
    * match version numbers to main (4.5.x)

--- ZimbraToast/AssemblyInfo.cs
+++ ZimbraToast/AssemblyInfo.cs
@@ -23,7 +23,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.5.12.0")]
+[assembly: AssemblyFileVersion("4.5.12.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- ZimbraToast/Zimbra.Client/Properties/AssemblyInfo.cs
+++ ZimbraToast/Zimbra.Client/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.5.12.0")]
+[assembly: AssemblyFileVersion("4.5.12.0")]
 
 
