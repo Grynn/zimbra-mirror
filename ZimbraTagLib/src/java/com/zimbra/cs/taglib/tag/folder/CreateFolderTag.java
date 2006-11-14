718efa42540ef9c516d8a308a0ee04603b8ede59
 Unknown Perforce User <schemers>
2006-11-14 05:48:45 +0000

    bug: none - add URL option to folder create - add ability to subscribe to feeds

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/folder/CreateFolderTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/folder/CreateFolderTag.java
@@ -17,17 +17,19 @@ public class CreateFolderTag extends ZimbraSimpleTag {
     private ZFolder.Color mColor;
     private ZFolder.View mView;
     private String mFlags;
+    private String mUrl;
 
     public void setParentid(String parentid) { mParentId = parentid; }
     public void setName(String name) { mName = name; }
     public void setFlags(String flags) { mFlags = flags; }
     public void setVar(String var) { mVar = var; }
+    public void setUrl(String url) { mUrl = url; }
     public void setColor(String color) throws ServiceException { mColor = ZFolder.Color.fromString(color); }
     public void setView(String view) throws ServiceException { mView = ZFolder.View.fromString(view); }    
 
     public void doTag() throws JspException, IOException {
         try {
-            ZFolderBean result = new ZFolderBean(getMailbox().createFolder(mParentId, mName, mView, mColor, mFlags));
+            ZFolderBean result = new ZFolderBean(getMailbox().createFolder(mParentId, mName, mView, mColor, mFlags, mUrl));
             getJspContext().setAttribute(mVar, result, PageContext.PAGE_SCOPE);
         } catch (ServiceException e) {
             throw new JspException(e);
