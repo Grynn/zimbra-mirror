fba72bdce7b227bc94860a15c1504244c06f8814
 Unknown Perforce User <anandp>
2006-11-14 09:27:28 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=5614 bug 5614

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/zimlets/model/ZaZimlet.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/zimlets/model/ZaZimlet.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * 
- * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
  * All Rights Reserved.
  * 
  * The Original Code is: Zimbra Network
