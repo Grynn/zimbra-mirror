9cee5d917c756555288e43bf2ae0bd40f1477619
 Unknown Perforce User <dkarp>
2006-11-15 05:27:33 +0000

    bug: none
    Add license block.

--- ZimbraOffline/src/java/com/zimbra/cs/db/DbOfflineDirectory.java
+++ ZimbraOffline/src/java/com/zimbra/cs/db/DbOfflineDirectory.java
@@ -1,3 +1,13 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ * 
+ * The Original Code is: Zimbra Network
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 package com.zimbra.cs.db;
 
 import java.sql.PreparedStatement;
