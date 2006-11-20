459727ba4556cb2f66404b1cac7b2a3511d14e98
 Unknown Perforce User <dkarp>
2006-11-20 22:25:32 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=12021 bug: 12021
    Move tag, folder, and document names from MAIL_ITEM.SUBJECT to a new MAIL_ITEM.NAME column.  There is a new UNIQUE index on (mailbox_id, folder_id, name), so the DB will now enforce uniqueness on the namespace.
    
    DB schema version is now 32.  Please reset-the-world or run the migrate20061120-AddNameColumn.pl migration script to bring your database schema up to the current version.

--- ZimbraServer/src/db/create_database.sql
+++ ZimbraServer/src/db/create_database.sql
@@ -44,6 +44,7 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item (
    tags          BIGINT NOT NULL DEFAULT 0,
    sender        VARCHAR(128),
    subject       TEXT,
+   name          VARCHAR(128),               # namespace entry for item (e.g. tag name, folder name, document filename)
    metadata      TEXT,
    mod_metadata  INTEGER UNSIGNED NOT NULL,  # change number for last row modification
    change_date   INTEGER UNSIGNED,           # UNIX-style timestamp for last row modification
@@ -62,6 +63,8 @@ CREATE TABLE IF NOT EXISTS ${DATABASE_NAME}.mail_item (
    INDEX i_tags_date (mailbox_id, tags, date),           # for tag searches
    INDEX i_flags_date (mailbox_id, flags, date),         # for flag searches
    INDEX i_volume_id (mailbox_id, volume_id),            # for the foreign key into the volume table
+
+   UNIQUE INDEX i_name_folder_id (mailbox_id, folder_id, name),   # for namespace uniqueness
 
    CONSTRAINT fk_mail_item_mailbox_id FOREIGN KEY (mailbox_id) REFERENCES zimbra.mailbox(id),
    CONSTRAINT fk_mail_item_parent_id FOREIGN KEY (mailbox_id, parent_id) REFERENCES ${DATABASE_NAME}.mail_item(mailbox_id, id),
