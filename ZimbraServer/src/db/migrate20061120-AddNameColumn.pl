59b76e09588bd41ce4a088ee83aaf2f32bbc38fe
 Unknown Perforce User <brian>
2006-11-28 06:49:58 +0000

    bug: none  parallelize AddNameColumn schema upgrade

--- ZimbraServer/src/db/migrate20061120-AddNameColumn.pl
+++ ZimbraServer/src/db/migrate20061120-AddNameColumn.pl
@@ -27,45 +27,70 @@
 
 use strict;
 use Migrate;
+my $concurrent = 10;
 
-Migrate::verifySchemaVersion(31);
-
-my @groups = Migrate::getMailboxGroups();
-my $sql = "";
-foreach my $group (@groups) {
-    $sql .= addNameColumn($group);
-}
-
-Migrate::runSql($sql);
-
-Migrate::updateSchemaVersion(31, 32);
+addNameColumn();
 
 exit(0);
 
 #####################
 
 sub addNameColumn($) {
-    my ($group) = @_;
-    my $date = time();
+  my ($group) = @_;
+  Migrate::verifySchemaVersion(31);
+  my $date = time();
+  my @groups = Migrate::getMailboxGroups();
+  my @sql = ();
+  foreach my $group (@groups) {
     my $sql = <<ADD_NAME_COLUMN_EOF;
 ALTER TABLE $group.mail_item
 ADD COLUMN name VARCHAR(128) AFTER subject;
+ADD_NAME_COLUMN_EOF
+    push(@sql,$sql);
+  }
+  Migrate::runSqlParallel($concurrent,@sql);
 
+  my @sql = ();
+  foreach my $group (@groups) {
+    my $sql = <<ADD_NAME_COLUMN_EOF;
 CREATE UNIQUE INDEX i_name_folder_id ON $group.mail_item(mailbox_id, folder_id, name);
+ADD_NAME_COLUMN_EOF
+    push(@sql,$sql);
+  }
+  Migrate::runSqlParallel($concurrent,@sql);
 
+  my @sql = ();
+  foreach my $group (@groups) {
+    my $sql = <<ADD_NAME_COLUMN_EOF;
 UPDATE IGNORE $group.mail_item
 SET name = subject, subject = null
 WHERE id < 256 AND type IN (1, 3);
+ADD_NAME_COLUMN_EOF
+    push(@sql,$sql);
+  }
+  Migrate::runSqlParallel($concurrent,@sql);
 
+  my @sql = ();
+  foreach my $group (@groups) {
+    my $sql = <<ADD_NAME_COLUMN_EOF;
 UPDATE IGNORE $group.mail_item
 SET name = subject, subject = null
 WHERE type IN (1, 2, 3, 8, 13, 14) AND subject IS NOT NULL;
+ADD_NAME_COLUMN_EOF
+    push(@sql,$sql);
+  }
+  Migrate::runSqlParallel($concurrent,@sql);
 
+  my @sql = ();
+  foreach my $group (@groups) {
+    my $sql = <<ADD_NAME_COLUMN_EOF;
 UPDATE IGNORE $group.mail_item
 SET name = CONCAT(SUBSTRING(subject, 1, 99), '{RENAMED-MIGRATE-$date}'), subject = null
 WHERE type IN (1, 2, 3, 8, 13, 14) AND subject IS NOT NULL;
-
 ADD_NAME_COLUMN_EOF
+    push(@sql,$sql);
+  }
+  Migrate::runSqlParallel($concurrent,@sql);
 
-    return $sql;
+  Migrate::updateSchemaVersion(31, 32);
 }
