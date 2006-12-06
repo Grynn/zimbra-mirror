558ab8671466e6be0c914d2836e11199a269ad2f
 Unknown Perforce User <greg>
2006-12-06 23:10:33 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=11986 bug 11986 align fields on MTA tab of Server view

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/servers/view/ZaServerXFormView.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/servers/view/ZaServerXFormView.js
@@ -315,19 +315,21 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 					  	      onChange: ZaServerXFormView.onFormFieldChanged
 						  	},
 						  	{ ref: ZaServer.A_zimbraSpellServiceEnabled, type: _CHECKBOX_,
-						  	  relevant: "instance.attrs[ZaServer.A_zimbraSpellServiceInstalled]", relevantBehavior: _DISABLE_,
+						  	  relevant: "instance.attrs[ZaServer.A_zimbraSpellServiceInstalled]", 
+						  	  relevantBehavior: _DISABLE_,
 						  	  label: ZaMsg.NAD_Service_Spell,
 					  	      onChange: ZaServerXFormView.onFormFieldChanged
 						  	},
 						  	{ ref: ZaServer.A_zimbraLoggerServiceEnabled, type: _CHECKBOX_,
-						  	  relevant: "instance.attrs[ZaServer.A_zimbraLoggerServiceInstalled]", relevantBehavior: _DISABLE_,
+						  	  relevant: "instance.attrs[ZaServer.A_zimbraLoggerServiceInstalled]", 
+						  	  relevantBehavior: _DISABLE_,
 						  	  label: ZaMsg.NAD_Service_Logger,
 					  	      onChange: ZaServerXFormView.onFormFieldChanged
 						  	}							  	
 						]}
 					]
 				}, 
-				{ type: _ZATABCASE_,  width:"100%", relevant: "instance[ZaModel.currentTab] == 3",colSizes:["250px","300px"],
+				{ type: _ZATABCASE_,  width:"100%", relevant: "instance[ZaModel.currentTab] == 3",
 			      items: [
 			      	{ ref:ZaServer.A_zimbraMtaAuthEnabled, type: _SUPER_CHECKBOX_,
 			      	  trueValue: "TRUE", falseValue: "FALSE",
@@ -351,22 +353,36 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 					  toolTipContent: ZaMsg.tt_MTA_WebMailHostname,
 					  textFieldCssClass:"admin_xform_name_input"
 					},
-					{ref:ZaServer.A_SmtpPort, type:_OUTPUT_, 
-					  label:ZaMsg.NAD_MTA_WebMailPort,
-					   width:"4em"
+					{type:_GROUP_,numCols:3,colSpan:3, colSizes:["275px","275px","150px"],
+						items:[
+						{ref:ZaServer.A_SmtpPort, type:_OUTPUT_, 
+						  label:ZaMsg.NAD_MTA_WebMailPort,
+						   width:"4em"
+						},
+						{type:_SPACER_}
+						]
 					},
-					{ref:ZaServer.A_SmtpTimeout, type:_INPUT_, 
-					  label:ZaMsg.NAD_MTA_WebMailTimeout, width: "4em",
-					  onChange: ZaServerXFormView.onFormFieldChanged
+					{type:_GROUP_,numCols:3,colSpan:3, colSizes:["275px","275px","150px"],
+					items:[
+						{ref:ZaServer.A_SmtpTimeout, type:_INPUT_, 
+						  label:ZaMsg.NAD_MTA_WebMailTimeout, width: "4em",
+						  onChange: ZaServerXFormView.onFormFieldChanged
+						},
+						{type:_SPACER_}
+						]
 					},
-			      	{ ref:ZaServer.A_zimbraMtaRelayHost, type:_SUPER_HOSTPORT_,
-			      	  label: ZaMsg.NAD_MTA_RelayMTA, 
-			      	  onChange: ZaServerXFormView.onFormFieldChanged,
-			      	  onClick: "ZaController.showTooltip",
-					  toolTipContent: ZaMsg.tt_MTA_RelayMTA,
-					  onMouseout: "ZaController.hideTooltip",
-			      	  resetToSuperLabel:ZaMsg.NAD_ResetToGlobal
-			      	},
+					{type:_GROUP_,numCols:2,colSpan:3, colSizes:["275px","475px"],
+						items:[
+					      	{ ref:ZaServer.A_zimbraMtaRelayHost, type:_SUPER_HOSTPORT_,
+					      	  label: ZaMsg.NAD_MTA_RelayMTA, 
+					      	  onChange: ZaServerXFormView.onFormFieldChanged,
+					      	  onClick: "ZaController.showTooltip",
+							  toolTipContent: ZaMsg.tt_MTA_RelayMTA,
+							  onMouseout: "ZaController.hideTooltip",
+					      	  resetToSuperLabel:ZaMsg.NAD_ResetToGlobal
+					      	}
+					      ]
+					},
 			      	{ref:ZaServer.A_zimbraMtaMyNetworks,txtBoxLabel:ZaMsg.NAD_MTA_MyNetworks,
 						type:_SUPER_TEXTFIELD_, 
 						onChange: ZaServerXFormView.onFormFieldChanged,
@@ -404,7 +420,7 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 						},	
 						{ ref: ZaServer.A_zimbraImapProxyBindPort, type:_SUPER_TEXTFIELD_, 
 						  relevant: "ZaServerXFormView.getMailProxyEnabled.call(item)",
-						  relevantBehavior: _HIDE_,
+						  relevantBehavior: _DISABLE_,
 						  txtBoxLabel: ZaMsg.NAD_IMAP_Proxy_Port, width: "5em",
 						  onChange: ZaServerXFormView.onFormFieldChanged,
 				      	  resetToSuperLabel:ZaMsg.NAD_ResetToGlobal
@@ -433,7 +449,7 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 						},		
 						{ ref: ZaServer.A_zimbraImapSSLProxyBindPort, type:_SUPER_TEXTFIELD_, 
 						  relevant: "ZaServerXFormView.getIMAPSSLProxyEnabled.call(item)",
-						  relevantBehavior: _HIDE_,
+						  relevantBehavior: _DISABLE_,
 						  txtBoxLabel: ZaMsg.NAD_IMAP_SSL_Proxy_Port, width: "5em",
 						  onChange: ZaServerXFormView.onFormFieldChanged,
 				      	  resetToSuperLabel:ZaMsg.NAD_ResetToGlobal
@@ -474,7 +490,7 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 					  	},		
 						{ ref: ZaServer.A_zimbraPop3ProxyBindPort, type:_SUPER_TEXTFIELD_,
 						  relevant:"ZaServerXFormView.getPOP3ProxyEnabled.call(item)",
-						  relevantBehavior: _HIDE_,
+						  relevantBehavior: _DISABLE_,
 						  labelLocation:_LEFT_, 
 						  textFieldCssClass:"admin_xform_number_input", 
 						  txtBoxLabel: ZaMsg.NAD_POP_proxy_Port,
@@ -532,7 +548,7 @@ ZaServerXFormView.myXFormModifier = function(xFormObject) {
 					  	},		
 						{ ref: ZaServer.A_zimbraPop3SSLProxyBindPort, type:_SUPER_TEXTFIELD_, 
 						  relevant:"ZaServerXFormView.getPOP3SSLProxyEnabled.call(item)",
-						  relevantBehavior: _HIDE_,
+						  relevantBehavior: _DISABLE_,
 						  labelLocation:_LEFT_, 
 						  txtBoxLabel: ZaMsg.NAD_POP_SSL_proxy_Port,
 						  textFieldCssClass:"admin_xform_number_input", 
