12bb7218522db59e9b1ac69e9e3ad8bf96676205
 Unknown Perforce User <bburtin>
2006-12-07 00:16:00 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=6412 bug: 6412
    Now skipping foreign key checks when recreating the foreign
    key.  Migration failed on dogfood due to dangling
    references in the appointment table.

--- ZimbraServer/src/db/migrate20061205-UniqueAppointmentIndex.pl
+++ ZimbraServer/src/db/migrate20061205-UniqueAppointmentIndex.pl
@@ -56,7 +56,9 @@ exit(0);
 #
 # Updates the index on appointment(mailbox_id, item_id) to be
 # unique.  Also drops/recreates the foreign key, since the
-# index drop will fail if it exists.
+# index drop will fail if it exists.  Disables foreign key
+# constraint checking in case there are dangling references
+# in the appointment table.
 #
 sub createUniqueIndex($) {
   my ($dbName) = @_;
@@ -68,10 +70,13 @@ DROP INDEX i_item_id ON $dbName.appointment;
 
 CREATE UNIQUE INDEX i_item_id ON $dbName.appointment (mailbox_id, item_id);
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 ALTER TABLE $dbName.appointment
 ADD CONSTRAINT fk_appointment_item_id FOREIGN KEY (mailbox_id, item_id)
   REFERENCES $dbName.mail_item(mailbox_id, id) ON DELETE CASCADE;
 
+SET FOREIGN_KEY_CHECKS = 1;
 CREATE_INDEX_EOF
 
   Migrate::runSql($sql);
