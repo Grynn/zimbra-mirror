9c409a63f14d40215336b8220a8ff5f3cf54b082
 Unknown Perforce User <bburtin>
2007-01-15 20:33:50 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=10704 http://bugzilla.zimbra.com/show_bug.cgi?id=10704 bug: 10704 - Factor out core code from ZimbraServer
    Moved constants from ZimbraSoapContext into
    com.zimbra.common.soap.HeaderConstants.  Moved
    ZimbraNamespace into com.zimbra.common.soap.

--- ZimbraCommon/src/java/com/zimbra/common/soap/ZimbraNamespace.java
+++ ZimbraCommon/src/java/com/zimbra/common/soap/ZimbraNamespace.java
@@ -29,7 +29,7 @@
  * TODO To change the template for this generated file go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-package com.zimbra.soap;
+package com.zimbra.common.soap;
 
 import org.dom4j.Namespace;
 import org.dom4j.QName;
@@ -41,8 +41,8 @@ import org.dom4j.QName;
  */
 public class ZimbraNamespace {
 	
-	public static final String ZIMBRA_STR = "urn:zimbra";
-	public static final Namespace ZIMBRA = Namespace.get(ZIMBRA_STR);
+    public static final String ZIMBRA_STR = "urn:zimbra";
+    public static final Namespace ZIMBRA = Namespace.get(ZIMBRA_STR);
 
     public static final QName E_BATCH_REQUEST = QName.get("BatchRequest", ZIMBRA);
     public static final QName E_BATCH_RESPONSE = QName.get("BatchResponse", ZIMBRA);
@@ -60,7 +60,7 @@ public class ZimbraNamespace {
     public static final String E_DELETED  = "deleted";
     public static final String E_MODIFIED = "modified";
     
-	public static final String ZIMBRA_ACCOUNT_STR = "urn:zimbraAccount";
-	public static final Namespace ZIMBRA_ACCOUNT = Namespace.get(ZIMBRA_ACCOUNT_STR);
+    public static final String ZIMBRA_ACCOUNT_STR = "urn:zimbraAccount";
+    public static final Namespace ZIMBRA_ACCOUNT = Namespace.get(ZIMBRA_ACCOUNT_STR);
 
 }
