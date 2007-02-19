68a5fbd51d2c846676c5b3a6eead1babc08a38dc
 Unknown Perforce User <schemers>
2007-02-19 22:02:34 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=14164 bug:  14164 - checkpoint calendar management

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/folder/ModifyFolderFreeBusyTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/folder/ModifyFolderFreeBusyTag.java
@@ -26,24 +26,23 @@ package com.zimbra.cs.taglib.tag.folder;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
-import com.zimbra.cs.zclient.ZFolder;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import java.io.IOException;
 
-public class ModifyFolderColorTag extends ZimbraSimpleTag {
+public class ModifyFolderFreeBusyTag extends ZimbraSimpleTag {
 
     private String mId;
-    private ZFolder.Color mColor;
+    private boolean mExclude;
 
     public void setId(String id) { mId = id; }
-    public void setColor(String color) throws ServiceException { mColor = ZFolder.Color.fromString(color); }
+    public void setExclude(boolean exclude) { mExclude = exclude; }
 
 
     public void doTag() throws JspException, IOException {
         try {
-            getMailbox().modifyFolderColor(mId, mColor);
+            getMailbox().modifyFolderExcludeFreeBusy(mId, mExclude);
         } catch (ServiceException e) {
             throw new JspTagException(e);
         }
