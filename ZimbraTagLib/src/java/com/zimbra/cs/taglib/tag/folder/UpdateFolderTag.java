0abe4e5c8d735a7c632dd450104a58d47c68891f
 Unknown Perforce User <schemers>
2007-02-21 02:50:15 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=14164 bug: 14164 -switch to new calendar management UI

---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/folder/UpdateFolderTag.java
@@ -0,0 +1,64 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.taglib.tag.folder;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.StringUtil;
+import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.zclient.ZFolder;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import java.io.IOException;
+
+public class UpdateFolderTag extends ZimbraSimpleTag {
+
+    private String mId;
+    private ZFolder.Color mColor;
+    private String mName;
+    private String mParentId;
+    private String mFlags;
+
+    public void setId(String id) { mId = id; }
+    public void setName(String name) { mName = name; }
+    public void setParentid(String parentId) { mParentId = parentId; }
+    public void setFlags(String flags) { mFlags = flags; }
+    public void setColor(String color) throws ServiceException { mColor = ZFolder.Color.fromString(color); }
+
+
+    public void doTag() throws JspException, IOException {
+        try {
+            getMailbox().updateFolder(
+                    mId,
+                    StringUtil.isNullOrEmpty(mName) ? null : mName,
+                    StringUtil.isNullOrEmpty(mParentId) ? null : mParentId,
+                    mColor,
+                    mFlags == null ? null : mFlags,
+                    null);
+        } catch (ServiceException e) {
+            throw new JspTagException(e);
+        }
+    }
+}
