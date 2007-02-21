215d5f171bf227d25da6c45237734b4c4344d3e2
 Unknown Perforce User <contractor1>
2007-02-21 22:33:10 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=14483 bug: 14483
    JDK by default caches dns resolution forever.  This is a problem for offline since one may start server from say within zimbra filewall going to dogfood and later on tries to connect from home.  Because dogfood resolves to different IPs in and outside of firewall, at second location one may see dogfood no longer reachable.
    
    This changes sets dns caching to 10 seconds by default, but can be tweaked by localconfig.xml.

--- ZimbraOffline/src/java/com/zimbra/cs/offline/Offline.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/Offline.java
@@ -12,8 +12,23 @@ package com.zimbra.cs.offline;
 
 import java.util.Timer;
 
+import com.zimbra.common.localconfig.LC;
+
 public class Offline {
 
     public static Timer sTimer = new Timer("Timer-Offline-Main", true);
-
+    
+    static {
+    	//If we don't set this, DNS resolution is by default cached forever.  If one starts the offline server from one
+    	//location and then moves to another network, the sync target address may change due to different route.
+    	//This way it's set to
+    	int ttl = 10;
+    	try {
+    		String dnsCacheTtl = LC.get("dns_cache_ttl");
+    		if (dnsCacheTtl != null) {
+    			ttl = Integer.parseInt(dnsCacheTtl);
+    		}
+    	} catch (Throwable t) {}
+    	java.security.Security.setProperty("networkaddress.cache.ttl" , Integer.toString(ttl));
+    }
 }
