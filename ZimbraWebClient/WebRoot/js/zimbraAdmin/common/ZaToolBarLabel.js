996a9d7ff6ff84d4f9b8d9774f75878c453aa567
 Unknown Perforce User <greg>
2007-03-08 01:57:13 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=15002 bug 15002 add ZaToolBarLabel a label that inherits from Dwtlabel and has a different style

---
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaToolBarLabel.js
@@ -0,0 +1,26 @@
+/**
+ * Used to create an application tab and its operations, such as new a  tab, 
+ * close a tab, edit the tab label.
+ * 
+ * It will also remember the state of the tab: hidden/shown and dirty/clean.
+ * @param parent - the tab group containing all the tabs.
+ * 
+ * @param params :
+ *  	closable - whether the close icon and action should be added
+ * 		selected - whether the newly created tab should be selected 
+ *		id - the tabId used to identify an unique tab.
+ * 		toolTip - the tooltip of the tab
+*/
+
+function ZaToolBarLabel(parent, style, className, posStyle, id, index) {
+	if (arguments.length == 0) return ;
+	DwtLabel.call(this,parent, style, className, posStyle, id, index);
+}
+
+ZaToolBarLabel.prototype = new DwtLabel;
+ZaToolBarLabel.prototype.constructor = ZaToolBarLabel;
+
+
+ZaToolBarLabel.prototype._createHtmlFromTemplate = function(templateId, data) {
+    DwtButton.prototype._createHtmlFromTemplate.call(this, "zimbraAdmin.common.templates.Widgets#ZaToolBarLabel", data);
+};
