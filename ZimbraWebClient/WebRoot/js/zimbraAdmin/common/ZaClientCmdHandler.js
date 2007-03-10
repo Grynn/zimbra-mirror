dfc3dd152b976a149424a8bfbe1404d1c99ed3e1
 Unknown Perforce User <greg>
2007-03-10 00:44:30 +0000

    bug none add a convinience command, $set:pagesize N

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaClientCmdHandler.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaClientCmdHandler.js
@@ -53,5 +53,8 @@ function(argv) {
 		ZaDomain.MAXSEARCHRESULTS = argv[1];
 		alert("Setting domain search limit to:" + argv[1]);
         this._app.searchDomains();
+    } else if (argv[0] && argv[0].toLowerCase() == "pagesize") {
+		ZaSettings.RESULTSPERPAGE = argv[1];
+		alert("Setting page size to:" + argv[1]);
     }
 }
