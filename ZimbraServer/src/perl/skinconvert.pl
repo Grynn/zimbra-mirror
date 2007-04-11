1677ccdbbfe121c1e3945df308a96e786cb12ccc
 Unknown Perforce User <aclark>
2007-04-11 23:33:14 +0000

    bug: none Updated skin converter script to rename all relevant files and add entries to substitutions file for the alternate color family.

--- ZimbraServer/src/perl/skinconvert.pl
+++ ZimbraServer/src/perl/skinconvert.pl
@@ -28,24 +28,37 @@
 #
 # 1) Rename "{skin}.xml" to "manifest.xml"
 #
-# 2) Create file "skin.js" with contents:
-#
-#      skin = new BaseSkin();
-#
-# 3) Insert the following <script> section into manifest.xml
+# 2) Insert the following <script> section into manifest.xml
 #    within the <skin> tag:
 #
 #	<script>
 #		<file>../_base/base/ZmSkin.js</file>
 #		<file>../_base/base/BaseSkin.js</file>
-#		<file>skin.js</file>
+#		<file>../_base/base/skin.js</file>
 #	</script>
 #
-# 4) Replace the following string patterns in manifest.xml:
+# 3) Replace the following string patterns in manifest.xml:
 #
 #      _base/base/base_subs.txt  ->  _base/base/skin.properties
 #      _base/base/base.css       ->  _base/base/skin.css
 #      _base/base/base.html      ->  _base/base/skin.html
+#      {skin}_subs.txt           ->  skin.properties
+#      {skin}.css                ->  skin.css
+#
+# 4) Replace string patterns in skin.properties and skin.css
+#
+#      _BaseColor_    ->  AppC
+#      _BaseColorD##  ->  AppC+##
+#      _BaseColorL##  ->  AppC-##
+#      _BaseColor_    ->  AppC
+#      _BaseColorD##  ->  AppC+##
+#      _BaseColorL##  ->  AppC-##
+#
+# This step also adds the following entries to skin.properties:
+#
+#      AltC    = @AppC@
+#      AltC+## = @AppC+##@
+#      AltC-## = @AppC-##@
 
 # Step -1: Check params
 
@@ -56,9 +69,25 @@ die "usage: $0 skin_manifest_file\n" if @ARGV != 1;
 $ofilename = $ARGV[0];
 @dirparts = split("/", $ofilename);
 $dirname = join("/", @dirparts[0 .. $#dirparts - 1]);
-$nfilename = join("/", $dirname, "manifest.xml");
+$skinname = @dirparts[-2];
 
-die "error: Skin already converted" if $ofilename eq $nfilename;
+$mfilename = join("/", $dirname, "manifest.xml");
+$sfilename = join("/", $dirname, "skin.properties");
+$cfilename = join("/", $dirname, "skin.css"); 
+
+%filenames = (
+  manifest => {
+    content => "", old => $ofilename, new => $mfilename
+  },
+  substitutions => {
+    content => "", old => join("/", $dirname, "${skinname}_subs.txt"), new => $sfilename
+  },
+  style => {
+    content => "", old => join("/", $dirname, "${skinname}.css"), new => $cfilename
+  }
+);
+
+die "error: Skin already converted" if $ofilename eq $mfilename;
 
 open(FILE, $ofilename) || die "error: Cannot open $ofilename\n";
 die "error: This skin cannot be converted\n" if !grep(m#_base/base/base.html#, <FILE>);
@@ -66,53 +95,96 @@ close FILE;
 
 # Step 1: Rename "{skin}.xml" to "manifest.xml"
 
-print "Renaming manifest file\n";
+print "Renaming files\n";
 
-rename $ofilename, $nfilename || die "error: Unable to move $ofilename\n";
+foreach $filename (keys %filenames) {
+  $filename1 = $filenames{$filename}{old};
+  $filename2 = $filenames{$filename}{new};
+  rename($filename1, $filename2) || print STDERR "warning: unable to rename $filename1\n";
+}
 
-# Step 2: Create file "skin.js"
+# Step 1.5: Read contents of files
 
-print "Creating skin source file\n";
+foreach $map (values %filenames) {
+  $filename = $map->{new};
 
-$filename = join("/", $dirname, "skin.js");
+  if (open(FILE, $filename)) {
+    $map->{contents} = join("", <FILE>);
+    close FILE;
+  }
+  else {
+    print STDERR "warning: unable to read $filename\n";
+  }
+}
 
-open(FILE, ">$filename") || die "error: Unable to create $filename\n";
-print FILE "skin = new BaseSkin();\n";
-close FILE;
-
-# Step 3: Insert <script> section into manifest.xml
+# Step 2: Insert <script> section into manifest.xml
 
 print "Inserting <script> section in manifest\n";
 
-open(FILE, $nfilename) || die "error: unable to open $nfilename\n";
-@file = <FILE>;
-$file = join("", @file);
-close FILE;
-
-$script = join("\n", 
+$script = join("\n",
 	"", 
 	"\t<script>",
 	"\t\t<file>../_base/base/ZmSkin.js</file>",
 	"\t\t<file>../_base/base/BaseSkin.js</file>",
-	"\t\t<file>skin.js</file>",
+	"\t\t<file>../_base/base/skin.js</file>",
 	"\t</script>"
 );
-$file =~ s/(<skin>)/$1$script/;
 
-# Step 4: Replace string patterns in manifest.xml
+$manifest = $filenames{manifest};
+$manifest->{contents} =~ s/(<skin>)/$1$script/;
+
+# Step 3: Replace string patterns in manifest.xml
 
 print "Converting filenames in manifest\n";
 
-$file =~ s#([^/]+)/\1_subs.txt#$1/skin.properties#;
-$file =~ s#([^/]+)/\1.css#$1/skin.css#;
-$file =~ s#([^/]+)/\1.html#$1/skin.html#;
+$manifest->{contents} =~ s#([^/]+)/\1_subs.txt#$1/skin.properties#;
+$manifest->{contents} =~ s#([^/]+)/\1.css#$1/skin.css#;
+$manifest->{contents} =~ s#([^/]+)/\1.html#$1/skin.html#;
+
+$manifest->{contents} =~ s/${skinname}_subs.txt/skin.properties/;
+$manifest->{contents} =~ s/${skinname}.css/skin.css/;
+
+# Step 4: Replace string patterns in skin.properties
+
+print "Converting color key names in substitution and style file\n";
+
+@filenames = ( $filenames{substitutions}, $filenames{style} );
+
+foreach $map (@filenames) {
+  next unless defined $map->{contents};
+  
+  $map->{contents} =~ s/_BaseColor_/AppC/mg;
+  $map->{contents} =~ s/_BaseColor([DL])(\d+)_/"AppC".($1 eq "D" ? "+" : "-").sprintf("%02d",$2)/emg;
+  $map->{contents} =~ s/_SelColor_/SelC/mg;
+  $map->{contents} =~ s/_SelColor([DL])(\d+)_/"SelC".($1 eq "D" ? "+" : "-").sprintf("%02d",$2)/emg;
+}
+
+$substitutions = $filenames{substitutions};
+if (defined $substitutions->{contents}) {
+  @altc = ();
+  for ($i = 95; $i > 0; $i -= 5) {
+    push(@altc, sprintf("AltC+%02d = \@AppC+%02d\@", $i, $i));
+  }
+  for ($i = 5; $i < 100; $i += 5) {
+    push(@altc, sprintf("AltC-%02d = \@AppC-%02d\@", $i, $i));
+  }
+  $altc = join("\n", @altc);
+
+  $substitutions->{contents} .= "\n$altc\n";
+}
+
+#### TODO: do the same for all the CSS files ####
 
 # Step 5: Finish
 
 print "Saving changes\n";
 
-open(FILE, ">$nfilename");
-print FILE $file;
-close FILE;
+foreach $map (values %filenames) {
+  next unless defined $map->{contents};
+
+  open(FILE, ">$map->{new}");
+  print FILE $map->{contents};
+  close FILE;
+}
 
 print "Done.\n";
