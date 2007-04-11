fe34d44f5e4c4136fab974e448649a6376489d43
 Unknown Perforce User <schemers>
2007-04-11 06:51:33 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=14164 bug: 14164 - checkpoint

---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/item/FlagItemTag.java
@@ -0,0 +1,56 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.taglib.tag.item;
+
+import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.taglib.bean.ZActionResultBean;
+import com.zimbra.cs.zclient.ZMailbox.ZActionResult;
+import com.zimbra.common.service.ServiceException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
+
+public class FlagItemTag extends ZimbraSimpleTag {
+
+    private String mTc;
+    private String mId;
+    private boolean mFlag;
+    private String mVar;
+
+    public void setTc(String tc) { this.mTc = tc; }
+    public void setId(String id) { this.mId = id; }
+    public void setFlag(boolean flag) { this.mFlag = flag; }
+    public void setVar(String var) { this.mVar = var; }
+
+    public void doTag() throws JspException {
+        try {
+            ZActionResult result = getMailbox().flagItem(mId, mFlag, mTc);
+            getJspContext().setAttribute(mVar, new ZActionResultBean(result), PageContext.PAGE_SCOPE);
+        } catch (ServiceException e) {
+            throw new JspTagException(e);
+        }
+    }
+}
---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/item/TagItemTag.java
@@ -0,0 +1,58 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 ("License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.zimbra.com/license
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is: Zimbra Collaboration Suite Server.
+ *
+ * The Initial Developer of the Original Code is Zimbra, Inc.
+ * Portions created by Zimbra are Copyright (C) 2006 Zimbra, Inc.
+ * All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.taglib.tag.item;
+
+import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.taglib.bean.ZActionResultBean;
+import com.zimbra.cs.zclient.ZMailbox.ZActionResult;
+import com.zimbra.common.service.ServiceException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
+
+public class TagItemTag extends ZimbraSimpleTag {
+
+    private String mTc;
+    private String mId;
+    private String mTagid;
+    private String mVar;
+    private boolean mTag;
+
+    public void setVar(String var) { mVar = var; }
+    public void setTc(String tc) { mTc = tc; }
+    public void setId(String id) { mId = id; }
+    public void setTag(boolean tag) { mTag = tag; }
+    public void setTagid(String tagid) { mTagid = tagid; }
+
+    public void doTag() throws JspException {
+        try {
+            ZActionResult result = getMailbox().tagItem(mId, mTagid, mTag, mTc);
+            getJspContext().setAttribute(mVar, new ZActionResultBean(result), PageContext.PAGE_SCOPE);
+        } catch (ServiceException e) {
+            throw new JspTagException(e);
+        }
+    }
+}
