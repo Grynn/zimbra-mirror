b5bbeb631c7ae42fd2ecaccd746b2ef115cfdfcd
 Unknown Perforce User <pshao>
2007-04-13 19:57:10 +0000

    http://bugzilla.zimbra.com/show_bug.cgi?id=14460 bug: 14460
    - added account/COS attribute zimbraMailSignatureMaxLength for signature length limit.  0 means unlimited.
    - removed the LDAP length constraint on zimbraPrefMailSignature
    - added attribute callback for zimbraPrefMailSignature that checks the limit
    - set account id of an Identity do we can getAccount() from an Identity object.

--- ZimbraOffline/src/java/com/zimbra/cs/account/offline/OfflineIdentity.java
+++ ZimbraOffline/src/java/com/zimbra/cs/account/offline/OfflineIdentity.java
@@ -26,22 +26,13 @@ package com.zimbra.cs.account.offline;
 
 import java.util.Map;
 
-import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Identity;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.account.Provisioning.AccountBy;
 
 class OfflineIdentity extends Identity {
-    private final String mAccountZID;
-
     OfflineIdentity(Account acct, String name, Map<String,Object> attrs) {
-        super(name, (String) attrs.get(Provisioning.A_zimbraPrefIdentityId), attrs);
-        mAccountZID = acct.getId();
-    }
-
-    Account getAccount() throws ServiceException {
-        return Provisioning.getInstance().get(AccountBy.id, mAccountZID);
+        super(acct, name, (String) attrs.get(Provisioning.A_zimbraPrefIdentityId), attrs);
     }
 
     void setName(String name) {
