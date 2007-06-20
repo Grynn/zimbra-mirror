0df72d3deafa828112ef55951470db27755ffbea
 Unknown Perforce User <dcomfort>
2007-06-20 18:33:39 +0000

    Bug: 16820 Follow naming convention
    http://bugzilla.zimbra.com/show_bug.cgi?id=16820

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/voice/TrashVoiceMailTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/voice/TrashVoiceMailTag.java
@@ -26,15 +26,16 @@
 package com.zimbra.cs.taglib.tag.voice;
 
 import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.taglib.bean.ZActionResultBean;
 import com.zimbra.cs.zclient.ZMailbox;
 import com.zimbra.common.service.ServiceException;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.PageContext;
 import java.io.IOException;
 
-public class UploadVoiceMail extends ZimbraSimpleTag {
+public class TrashVoiceMailTag extends ZimbraSimpleTag {
 
     private String mId;
     private String mPhone;
@@ -47,8 +48,9 @@ public class UploadVoiceMail extends ZimbraSimpleTag {
     public void doTag() throws JspException, IOException {
         try {
             ZMailbox mbox = getMailbox();
-            String attachId = mbox.uploadVoiceMail(mPhone, mId);
-            getJspContext().setAttribute(mVar, attachId, PageContext.PAGE_SCOPE);
+            ZMailbox.ZActionResult result = mbox.trashVoiceMail(mPhone, mId);
+            getJspContext().setAttribute(mVar, new ZActionResultBean(result), PageContext.PAGE_SCOPE);
+
         } catch (ServiceException e) {
             throw new JspTagException(e);
         }
--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/voice/UploadVoiceMailTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/voice/UploadVoiceMailTag.java
@@ -26,16 +26,15 @@
 package com.zimbra.cs.taglib.tag.voice;
 
 import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
-import com.zimbra.cs.taglib.bean.ZActionResultBean;
 import com.zimbra.cs.zclient.ZMailbox;
 import com.zimbra.common.service.ServiceException;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
 import java.io.IOException;
 
-public class TrashVoiceMail extends ZimbraSimpleTag {
+public class UploadVoiceMailTag extends ZimbraSimpleTag {
 
     private String mId;
     private String mPhone;
@@ -48,9 +47,8 @@ public class TrashVoiceMail extends ZimbraSimpleTag {
     public void doTag() throws JspException, IOException {
         try {
             ZMailbox mbox = getMailbox();
-            ZMailbox.ZActionResult result = mbox.trashVoiceMail(mPhone, mId);
-            getJspContext().setAttribute(mVar, new ZActionResultBean(result), PageContext.PAGE_SCOPE);
-
+            String attachId = mbox.uploadVoiceMail(mPhone, mId);
+            getJspContext().setAttribute(mVar, attachId, PageContext.PAGE_SCOPE);
         } catch (ServiceException e) {
             throw new JspTagException(e);
         }
