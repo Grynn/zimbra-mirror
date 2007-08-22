ad50039aa249d78c1c191d2e591f30d504d30285
 Unknown Perforce User <tim>
2007-08-22 00:32:22 +0000

    bug: 7074
    test script for IMJoinChatRequest
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=7074

---
+++ ZimbraServer/src/perl/soap/imJoinChat.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -w
+# 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License
+# Version 1.1 ("License"); you may not use this file except in
+# compliance with the License. You may obtain a copy of the License at
+# http://www.zimbra.com/license
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and limitations
+# under the License.
+# 
+# The Original Code is: Zimbra Collaboration Suite Server.
+# 
+# The Initial Developer of the Original Code is Zimbra, Inc.
+# Portions created by Zimbra are Copyright (C) 2004, 2005, 2006 Zimbra, Inc.
+# All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+
+use strict;
+use lib '.';
+
+use LWP::UserAgent;
+use Getopt::Long;
+use XmlDoc;
+use Soap;
+use ZimbraSoapTest;
+
+# specific to this app
+my ($threadId, $addr);
+
+#standard options
+my ($user, $pw, $host, $help); #standard
+GetOptions("u|user=s" => \$user,
+           "pw=s" => \$pw,
+           "h|host=s" => \$host,
+           "help|?" => \$help,
+           # add specific params below:
+           "t=s", \$threadId,
+           "a=s", \$addr
+          );
+
+
+
+if (!defined($user) || !defined($threadId) || defined($help)) {
+    my $usage = <<END_OF_USAGE;
+    
+USAGE: $0 -u USER -t threadId -a addr
+END_OF_USAGE
+    die $usage;
+}
+
+my $z = ZimbraSoapTest->new($user, $host, $pw);
+$z->doStdAuth();
+
+my $d = new XmlDoc;
+my $searchName = "SearchRequest";
+
+$d->start("IMJoinChatRequest", $Soap::ZIMBRA_IM_NS, { 'addr' => $addr, 'thread' => $threadId });
+
+ $d->end(); 
+
+my $response = $z->invokeMail($d->root());
+
+print "REQUEST:\n-------------\n".$z->to_string_simple($d);
+print "RESPONSE:\n--------------\n".$z->to_string_simple($response);
+
