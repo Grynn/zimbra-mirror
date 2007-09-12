c6a8b2d51db7923e6325e06cfb12dec696954d92
 Unknown Perforce User <anandp>
2007-09-12 08:56:28 +0000

    bug 19566
    http://bugzilla.zimbra.com/show_bug.cgi?id=19566

--- ZimbraIM/src/java/org/jivesoftware/util/Blowfish.java
+++ ZimbraIM/src/java/org/jivesoftware/util/Blowfish.java
@@ -1,26 +1,15 @@
-/*
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 ("License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.zimbra.com/license
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
- * the License for the specific language governing rights and limitations
- * under the License.
- * 
- * The Original Code is: Zimbra Collaboration Suite Server.
- * 
- * The Initial Developer of the Original Code is Zimbra, Inc.
- * Portions created by Zimbra are Copyright (C) 2006, 2007 Zimbra, Inc.
- * All Rights Reserved.
- * 
- * Contributor(s):
- * 
- * ***** END LICENSE BLOCK *****
+/**
+ * $RCSfile$
+ * $Revision: 3657 $
+ * $Date: 2002-09-09 08:31:31 -0700 (Mon, 09 Sep 2002) $
+ *
+ * Adapted from Markus Hahn's Blowfish package so that all functionality is
+ * in a single source file. Please visit the following URL for his excellent
+ * package: http://www.hotpixel.net/software.html
+ *
+ * Copyright (c) 1997-2002 Markus Hahn <markus_hahn@gmx.net>
+ *
+ * Released under the Apache 2.0 license.
  */
 package org.jivesoftware.util;
 
