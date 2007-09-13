aeed0c7fee99ce2722fa1f37729a7db23dae54b8
 Unknown Perforce User <schemers>
2007-09-13 22:37:31 +0000

    bug: 17090 - inline response of a batch request for GetInfo and initial Search - needs a  "deploy-html"
    http://bugzilla.zimbra.com/show_bug.cgi?id=17090

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/GetInfoJSONTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/GetInfoJSONTag.java
@@ -15,15 +15,21 @@ public class GetInfoJSONTag extends ZimbraSimpleTag {
 
     private String mVar;
     private String mAuthToken;
+    private String mItemsPerPage;
+    private String mTypes;
+    private boolean mDoSearch;
 
     public void setVar(String var) { this.mVar = var; }
     public void setAuthtoken(String authToken) { this.mAuthToken = authToken; }
-
+    public void setDosearch(boolean doSearch) { mDoSearch = doSearch; }
+    public void setItemsperpage(String itemsPerPage) { mItemsPerPage = itemsPerPage; }
+    public void setTypes(String types) { mTypes = types; }
+    
     public void doTag() throws JspException, IOException {
         try {
             JspContext ctxt = getJspContext();
             String url = ZJspSession.getSoapURL((PageContext) ctxt);
-            Element e = ZMailbox.getInfoJSON(url, mAuthToken);
+            Element e = ZMailbox.getBootstrapJSON(url, mAuthToken, mDoSearch, mItemsPerPage, mTypes);
             ctxt.setAttribute(mVar, e.toString(),  PageContext.REQUEST_SCOPE);
         } catch (ServiceException e) {
             throw new JspTagException(e.getMessage(), e);
