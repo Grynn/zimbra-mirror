3e6353247f57f1ba062a905108d11d5a6d7ddcaf
 Unknown Perforce User <anandp>
2007-09-28 21:19:34 +0000

    bug 20667
    http://bugzilla.zimbra.com/show_bug.cgi?id=20667

--- ZimbraLDAPUtilsExtension/src/java/com/zimbra/ldaputils/ZimbraLDAPUtilsExtension.java
+++ ZimbraLDAPUtilsExtension/src/java/com/zimbra/ldaputils/ZimbraLDAPUtilsExtension.java
@@ -1,10 +1,16 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * 
- * Portions created by Zimbra are Copyright (C) 2005, 2006 Zimbra, Inc.
- * All Rights Reserved.
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2007 Zimbra, Inc.
  * 
- * The Original Code is: Zimbra Network
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * 
  * ***** END LICENSE BLOCK *****
  */
