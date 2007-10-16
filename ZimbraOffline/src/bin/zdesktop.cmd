7e4e29c7ea710fc60f3323644bf4510faa92853f
 Unknown Perforce User <jjzhuang>
2007-10-16 09:56:01 +0000

    bug: 20616
    Now we use a shell script, "offline", to start/stop offline dev build just like dev build of jetty.  This way we can do stuff like 'kill -3'.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=20616

--- ZimbraOffline/src/bin/zdesktop.cmd
+++
@@ -1,4 +0,0 @@
-SET ZIMBRA_HOME=/opt/zimbra/zdesktop
-
-java -Xms64m -Xmx256m -Dsun.net.inetaddr.ttl=30 -DSTART=%ZIMBRA_HOME%/jetty/etc/start.config -Dzimbra.config=%ZIMBRA_HOME%/conf/localconfig.xml -Djava.library.path=%ZIMBRA_HOME%/lib -cp %ZIMBRA_HOME%/lib/zdesktop.jar:%ZIMBRA_HOME%/jetty/start.jar com.zimbra.cs.offline.start.Main org.mortbay.start.Main 2> nul
-
