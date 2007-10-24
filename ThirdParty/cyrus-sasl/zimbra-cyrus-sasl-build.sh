f6c8287fbbaef0687b6a91b1421271ebdbeb0026
 Unknown Perforce User <build>
2007-10-24 21:51:25 +0000

    bug: none Fix typo in linux build

--- ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
+++ ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
@@ -84,7 +84,7 @@ LIBS="/opt/zimbra/libxml2/lib/libxml2.a" CFLAGS="-D_REENTRANT -g -O2 -I/opt/zimb
             --with-libxml2=/opt/zimbra/libxml2-${xml2_version}/bin/xml2-config \
             --enable-login
 else 
-CFLAGS="-D_REENTRANT -g -O2" ./configure --enable-zimbra --prefix=/opt/zimbra/${src} \
+LIBS="/opt/zimbra/libxml2/lib/libxml2.a" CFLAGS="-D_REENTRANT -g -O2" ./configure --enable-zimbra --prefix=/opt/zimbra/${src} \
             --with-saslauthd=/opt/zimbra/${src}/state \
             --with-plugindir=/opt/zimbra/${src}/lib/sasl2 \
             --with-dblib=no \
