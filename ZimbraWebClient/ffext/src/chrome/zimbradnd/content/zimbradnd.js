d0fee2dea86bc3564d904c4509d187b5e76ff47b
 Unknown Perforce User <sraj>
2007-10-31 12:25:59 +0000

    bug:none
    modified for generalising drag n drop, created seperate zimlet for drag n drop "com_zimbra_dnd"

--- ZimbraWebClient/ffext/src/chrome/zimbradnd/content/zimbradnd.js
+++ ZimbraWebClient/ffext/src/chrome/zimbradnd/content/zimbradnd.js
@@ -1,66 +1,66 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * Zimbra Collaboration Suite Web Client
  * Copyright (C) 2007 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */
 
 var zUploadService = {
+  fileInputName: "_attFile_",
   uploadFiles:  function(obj) {
 		if (self) try { self.focus(); } catch (e) {}
     obj.click();
   },
   processFiles: function(evt, files) {
 		try {
-        var form = null;
-				var inputName = '_attFile_';
-				var inputSize = 50;
-				var ownDoc = evt.target.ownerDocument;
-				
-        var ulEle = ownDoc.getElementById('zdnd_ul');
-        ulEle.innerHTML = '';
-        
-        if (files.length > 0)
-				{
-          for (var i = 0; i < files.length; i++)
-					{
-            var li = ulEle.ownerDocument.createElement('LI');
-						var input = ulEle.ownerDocument.createElement('INPUT');
-						input.type = 'file';
-						input.name = inputName;
-						input.size = inputSize;
-						input.value = files[i];
-						li.appendChild(input);
-						ulEle.appendChild(li);
-					}
-				}
-				zUploadService.uploadFiles(ownDoc.getElementById('zdnd_button'));
-		}
-		catch(e) {
+            var form = null;
+            var inputName = this.fileInputName;
+            var inputSize = 50;
+            var ownDoc = evt.target.ownerDocument;
+
+            var ulEle = ownDoc.getElementById('zdnd_ul');
+            ulEle.innerHTML = '';
+
+            if (files.length > 0)
+            {
+                for (var i = 0; i < files.length; i++)
+                {
+                    var li = ulEle.ownerDocument.createElement('LI');
+                    var input = ulEle.ownerDocument.createElement('INPUT');
+                    input.type = 'file';
+                    input.name = inputName;
+                    input.size = inputSize;
+                    input.value = files[i];
+                    li.appendChild(input);
+                    ulEle.appendChild(li);
+                }
+            }
+            zUploadService.uploadFiles(ZimbraDnD.butEle);
+		} catch(e) {
 		}
 	}
 }
 
 var zDnDService = {
-  canHandleMultipleItems: true,
+    canHandleMultipleItems: true,
 	onDragStart: function (evt , transferData, action) {
 	},
-	onDragOver: function (evt,flavour,session) { 		
+	onDragOver: function (evt,flavour,session) {
 	},
 	onDrop : function (evt, transferData, session) {
 	try
 	{
-    var td = transferData.flavour? transferData: transferData.first.first; 
+    var td = transferData.flavour? transferData: transferData.first.first;
 		if (td.flavour.contentType == "application/x-moz-file")
 		{
 			var files = [];
@@ -97,18 +97,22 @@ var zDnDService = {
 		return flavours;
 	}
 };
-	
+
 var ZimbraDnD = {
-	init: function(e)
+	butEle: null,
+    init: function(e)
 	{
-    var oZmCv = e.target.parentNode;
-    e.target.ownerDocument.getElementById('zdnd_tooltip').style.display = "block";
-    if(oZmCv.className == "ZmComposeView") {
-      oZmCv.addEventListener("dragdrop", ZimbraDnD.onDrop, false);
-    }
-    
+        var oZmCv = e.target.parentNode;
+        if(e.target.ownerDocument.getElementById('zdnd_tooltip')) {
+            e.target.ownerDocument.getElementById('zdnd_tooltip').style.display = "block";
+        }
+        if(oZmCv.className) {
+          oZmCv.addEventListener("dragdrop", ZimbraDnD.onDrop, false);
+          ZimbraDnD.butEle = e.target.ownerDocument.getElementById("ZDnDBut_"+oZmCv.className+"_zDnD");
+        }
+        zUploadService.fileInputName = "_attFile_";
 	},
-	onDrop: function(e) 
+	onDrop: function(e)
 	{
 		try {
 			nsDragAndDrop.drop(e, zDnDService);
