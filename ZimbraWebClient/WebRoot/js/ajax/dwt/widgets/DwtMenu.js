705488f6de4edb081696adcbadf2b5afc282add4
 Unknown Perforce User <dcomfort>
2007-10-31 22:37:38 +0000

    Bug: none Update menu positioning fudge factor so mail priority menu doesn't go offscreen

--- ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenu.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenu.js
@@ -565,6 +565,7 @@ function(x, y, kbGenerated) {
 
 	// bug 9583 - can't query border size so just subtract generic padding
 	windowSize.y -= 10 + AjxEnv.isIE ? 20 : 0;
+	windowSize.x -= 20;
 
 	if (((this._style == DwtMenu.POPUP_STYLE ||
 		(this._style == DwtMenu.DROPDOWN_STYLE && this.parent instanceof DwtMenuItem)) && mySize.y >= windowSize.y) ||
