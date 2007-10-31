ab9bc574c613cc727d1896ce044b5a5d27febb2f
 Unknown Perforce User <mihai>
2007-10-31 16:19:27 +0000

    Bug: 14156
    Display presence information in mail msg. view.  This is done in the
    Zimlet, since it displays the contacts in the message headers.  We hold
    a cache of the last 50 icons so we can update them when buddies change
    presence (I couldn't find another way to keep track of what's on the
    screen...)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=14156

--- Zimlet/src/zimlet/com_zimbra_email/email.js
+++ Zimlet/src/zimlet/com_zimbra_email/email.js
@@ -1,17 +1,17 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * Zimbra Collaboration Suite Zimlets
  * Copyright (C) 2006, 2007 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Yahoo! Public License
  * Version 1.0 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * 
+ *
  * ***** END LICENSE BLOCK *****
  */
 
@@ -28,9 +28,53 @@ function() {
 
 		this._composeTooltipHint = ZmMsg.leftClickComposeHint + "<br>" + ZmMsg.rightClickHint;
 		this._newTooltipHint = ZmMsg.leftClickNewContactHint + "<br>" + ZmMsg.rightClickHint;
+
+                if (appCtxt.get(ZmSetting.IM_ENABLED)) {
+                        this._presenceCache = [];
+                }
 	} else {
 		this._newTooltipHint = ZmMsg.leftClickComposeHint + "<br>" + ZmMsg.rightClickHint;
 	}
+};
+
+Com_Zimbra_Email.prototype._getRoster = function() {
+        if (!this._roster && appCtxt.get(ZmSetting.IM_ENABLED) && ZmImApp.loggedIn()) {
+                this._roster = AjxDispatcher.run("GetRoster");
+                var list = this._roster.getRosterItemList();
+                list.addChangeListener(new AjxListener(this, this._rosterChangeListener));
+        }
+        return this._roster;
+};
+
+Com_Zimbra_Email.prototype._rosterChangeListener = function(ev) {
+        if (ev.event == ZmEvent.E_MODIFY) {
+                var fields = ev.getDetail("fields");
+                var doPresence = ZmRosterItem.F_PRESENCE in fields;
+                if (doPresence) {
+                        var buddies = ev.getItems();
+                        var hash = {};
+                        for (var i = buddies.length; --i >= 0;) {
+                                var b = buddies[i];
+                                hash[b.getAddress()] = b;
+                        }
+                        var cache = this._presenceCache;
+                        for (var i = cache.length; --i >= 0;) {
+                                var el = cache[i];
+                                var b = hash[el.im_addr];
+                                if (b) {
+                                        // try to update presence state
+                                        var img = document.getElementById(el.img_id);
+                                        if (img) {
+                                                AjxImg.setImage(img, b.getPresence().getIcon(), true);
+                                        } else {
+                                                // no longer visible, remove from cache?
+                                                // cache.splice(i, 1);
+                                                // better not: will fail if we collapse/expand headers
+                                        }
+                                }
+                        }
+                }
+        }
 };
 
 Com_Zimbra_Email.prototype._getHtmlContent =
@@ -40,30 +84,59 @@ function(html, idx, obj) {
 		if (this._contacts && appCtxt.get(ZmSetting.CONTACTS_ENABLED)) {
 			var contact = this._contacts.getContactByEmail(obj.address);
 			if (contact) {
-				content = contact.getFullName();
+				content = AjxStringUtil.htmlEncode(contact.getFullName());
+
+                                var pres = contact.getImPresence();
+                                // returns NULL if IM app not loaded or IM is disabled
+
+                                if (pres) {
+                                        var pres_id = Dwt.getNextId();
+                                        var tmp = [
+                                                content,
+                                                " ",
+                                                AjxImg.getImageHtml(pres.getIcon(),
+                                                                    "display: inline; padding: 1px 8px;",
+                                                                    "id=" + pres_id)
+                                        ];
+                                        content = tmp.join("");
+                                        var buddy = contact.getBuddy();
+
+                                        this._presenceCache.push({ contact : contact,
+                                                                   buddy   : buddy,
+                                                                   im_addr : buddy.getAddress(),
+                                                                   img_id  : pres_id
+                                                                 });
+
+                                        if (this._presenceCache.length > 50) {
+                                                // 50 should be enough.. maybe should be even smaller?
+                                                this._presenceCache.splice(0, 1);
+                                        }
+
+                                        this._getRoster();
+                                }
 			}
 		}
 		if (!content) {
-			content = obj.toString();
+			content = AjxStringUtil.htmlEncode(obj.toString());
 		}
 	} else {
-		content = obj;
+		content = AjxStringUtil.htmlEncode(obj);
 	}
-	html[idx++] = AjxStringUtil.htmlEncode(content);
+	html[idx++] = content;
 	return idx;
 };
 
 Com_Zimbra_Email.prototype.toolTipPoppedUp =
 function(spanElement, contentObjText, matchContext, canvas) {
-	
+
 	var toolTip;
 	var addr = (contentObjText instanceof AjxEmailAddress)
 		? contentObjText.address : contentObjText;
-	
+
 	if(this.isMailToLink(addr)){
 		addr = (this.parseMailToLink(addr)).to || addr;
-	}	
-	
+	}
+
 	var contact = this._contacts ? this._contacts.getContactByEmail(addr) : null;
 	if (contact) {
 		toolTip = contact.getToolTip(addr, false, this._composeTooltipHint);
@@ -79,7 +152,7 @@ function(spanElement, contentObjText, matchContext, canvas) {
 
 Com_Zimbra_Email.prototype.getActionMenu =
 function(obj, span, context) {
-	
+
 	// call base class first to get the action menu
 	var actionMenu = ZmZimletBase.prototype.getActionMenu.call(this, obj, span, context);
 
@@ -95,10 +168,16 @@ function(obj, span, context) {
 			if(this.isMailToLink(addr)){
 				addr = (this.parseMailToLink(addr)).to || addr;
 			}
-			var found = (this._contacts.getContactByEmail(addr) != null);
-			var newOp = found ? ZmOperation.EDIT_CONTACT : ZmOperation.NEW_CONTACT;
-			var newText = found ? null : ZmMsg.AB_ADD_CONTACT;
+
+                        var contact = this._contacts.getContactByEmail(addr);
+			var newOp = contact ? ZmOperation.EDIT_CONTACT : ZmOperation.NEW_CONTACT;
+			var newText = contact ? null : ZmMsg.AB_ADD_CONTACT;
 			ZmOperation.setOperation(actionMenu, "NEWCONTACT", newOp, newText);
+
+                        var buddy = contact ? contact.getBuddy() : null;
+                        if (actionMenu.getOp("NEWIM")) {
+                                actionMenu.getOp("NEWIM").setEnabled(buddy != null);
+                        }
 		}
 	}
 	if (actionMenu.getOp("SEARCH") && !appCtxt.get(ZmSetting.SEARCH_ENABLED)) {
@@ -122,9 +201,9 @@ Com_Zimbra_Email.prototype.isMailToLink = function (str){
 };
 
 Com_Zimbra_Email.prototype.parseMailToLink = function(str){
-		
+
 	var parts = {};
-	
+
 	var match = str.match(/\bsubject=([^&]+)/);
 	parts.subject = match ? decodeURIComponent(match[1]) : null;
 	match = str.match(/\bto\:([^&]+)/);
@@ -132,16 +211,16 @@ Com_Zimbra_Email.prototype.parseMailToLink = function(str){
 	parts.to = match ? decodeURIComponent(match[1]) : null;
 	match = str.match(/\bbody=([^&]+)/);
 	parts.body = match ? decodeURIComponent(match[1]) : null;
-	
+
 	return parts;
 };
 
 Com_Zimbra_Email.prototype.clicked =
 function(spanElement, contentObjText, matchContext, ev) {
-	
-	var addr = (contentObjText instanceof AjxEmailAddress) 
+
+	var addr = (contentObjText instanceof AjxEmailAddress)
 		? contentObjText.address : contentObjText;
-	
+
 	var contact = this._contacts ? this._contacts.getContactByEmail(addr) : null;
 	// if contact found or there is no contact list (i.e. contacts app is disabled), go to compose view
 	if (contact || this._contacts == null || (AjxUtil.isString(addr) && this.isMailToLink(addr)) ) {
@@ -156,24 +235,27 @@ function(spanElement, contentObjText, matchContext, ev) {
 Com_Zimbra_Email.prototype.menuItemSelected =
 function(itemId, item, ev) {
 	switch (itemId) {
-		case "SEARCH":
-			this._searchListener();
-			break;
-		case "SEARCHBUILDER":
-			this._browseListener();
-			break;
-		case "NEWEMAIL":
-			this._composeListener(ev);
-			break;
-		case "NEWCONTACT":
-			this._contactListener();
-			break;
-		case "NEWFILTER":
-			this._filterListener();
-			break;
-		case "GOTOURL":
-			this._goToUrlListener();
-			break;
+	    case "SEARCH":
+		this._searchListener();
+		break;
+	    case "SEARCHBUILDER":
+		this._browseListener();
+		break;
+	    case "NEWEMAIL":
+		this._composeListener(ev);
+                break;
+            case "NEWIM":
+                this._newImListener();
+		break;
+	    case "NEWCONTACT":
+		this._contactListener();
+		break;
+	    case "NEWFILTER":
+		this._filterListener();
+		break;
+	    case "GOTOURL":
+		this._goToUrlListener();
+		break;
 	}
 };
 
@@ -192,9 +274,16 @@ function() {
 	AjxDispatcher.require(["ContactsCore", "Contacts"], false, loadCallback, null, true);
 };
 
-Com_Zimbra_Email.prototype._handleLoadContact =
-function() {
-	// actionObject can be a ZmContact, a String, or a generic Object (phew!)
+Com_Zimbra_Email.prototype._newImListener = function() {
+        var contact = this._getActionedContact(), buddy;
+        if (contact)
+                buddy = contact.getBuddy();
+        if (buddy)
+                AjxDispatcher.run("GetChatListController").chatWithRosterItem(buddy);
+};
+
+Com_Zimbra_Email.prototype._getActionedContact = function(create) {
+        // actionObject can be a ZmContact, a String, or a generic Object (phew!)
 	var contact;
 	var addr = this._actionObject;
 	if (this._actionObject) {
@@ -210,38 +299,42 @@ function() {
 			contact = AjxDispatcher.run("GetContacts").getContactByEmail(this._actionObject.address);
 		}
 	}
-
-	if (contact == null) {
+        if (contact == null && create) {
 		contact = new ZmContact(null);
 		contact.initFromEmail(addr);
 	}
+        return contact;
+};
 
+Com_Zimbra_Email.prototype._handleLoadContact =
+function() {
+        var contact = this._getActionedContact(true);
 	AjxDispatcher.run("GetContactController").show(contact);
 };
 
 Com_Zimbra_Email.prototype._composeListener =
 function(ev,addr) {
-		
+
 	addr = (this._actionObject) ? this._getAddress(this._actionObject) : addr ;
 	if(!addr) addr = "";
 	var params = {};
-	
+
 	var inNewWindow = (!appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && ev.shiftKey) ||
 					  (appCtxt.get(ZmSetting.NEW_WINDOW_COMPOSE) && ev && !ev.shiftKey);
-		
+
 	if(this.isMailToLink(addr)){
 		var mailToParams = this.parseMailToLink(addr);
 		params.toOverride    = mailToParams.to;
-		params.subjOverride  = mailToParams.subject; 
+		params.subjOverride  = mailToParams.subject;
 		params.extraBodyText = mailToParams.body;
 		addr = mailToParams.to || addr;
 	}
-	
+
 	params.action 			= ZmOperation.NEW_MESSAGE;
 	params.inNewWindow 		= inNewWindow;
 	if(!params.toOverride)
 		params.toOverride 	= addr + AjxEmailAddress.SEPARATOR;
-					  
+
 	AjxDispatcher.run("Compose", params );
 };
 
@@ -275,7 +368,7 @@ Com_Zimbra_Email.prototype._handleLoadFilter =
 function() {
 	appCtxt.getAppViewMgr().popView(true, ZmController.LOADING_VIEW);	// pop "Loading..." page
 	var rule = new ZmFilterRule();
-	
+
 	var addr  = this._getAddress(this._actionObject);
 	if(AjxUtil.isString(addr) && this.isMailToLink(addr)){
 		addr = (this.parseMailToLink(addr)).to || addr;
