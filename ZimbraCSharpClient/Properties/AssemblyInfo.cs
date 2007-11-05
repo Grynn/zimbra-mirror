916aa82d83318bc73256ab89d6d83953611935e2
 Unknown Perforce User <build>
2007-11-05 10:11:07 +0000

    bug: 6038 AUTO UPDATE OF Toaster BUILD NUMBER 5.0.331
    http://bugzilla.zimbra.com/show_bug.cgi?id=6038

--- ZimbraCSharpClient/Properties/AssemblyInfo.cs
+++ ZimbraCSharpClient/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.0.330.0")]
-[assembly: AssemblyFileVersion("5.0.330.0")]
+[assembly: AssemblyVersion("5.0.331.0")]
+[assembly: AssemblyFileVersion("5.0.331.0")]
 
 
--- ZimbraToast/AssemblyInfo.cs
+++ ZimbraToast/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.0.330.0")]
-[assembly: AssemblyFileVersion("5.0.330.0")]
+[assembly: AssemblyVersion("5.0.331.0")]
+[assembly: AssemblyFileVersion("5.0.331.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
