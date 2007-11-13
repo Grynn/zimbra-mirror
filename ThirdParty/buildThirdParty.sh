3f354259bb1b80da58fb2e87af58298e5b008259
Quanah Gibson-Mount <quanah@zimbra.com>
2007-11-13 23:41:36 +0000

    bug: 14264 A version of the script we use to build third party, to make life easier
    http://bugzilla.zimbra.com/show_bug.cgi?id=14264

---
+++ ThirdParty/buildThirdParty.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+BUILD_HOME=/home/public/p4/zcs
+
+RELEASE=$1
+
+if [ x$RELEASE = "x" ]; then
+	RELEASE=main
+fi
+
+PLAT=`$BUILD_HOME/$RELEASE/ZimbraBuild/rpmconf/Build/get_plat_tag.sh`;
+
+if [ x$PLAT = "x" ]; then
+    echo "Unknown platform, exiting."
+    exit
+fi
+
+if [ x$PLAT = "xRHEL4" -o x$PLAT = "xRHEL5" -o x$PLAT = "xFC4" -o x$PLAT = "xFC5" -o x$PLAT = "xF7" -o x$PLAT = "xRPL1" -o x$PLAT = "xDEBIAN3.1" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/i386-linux-thread-multi"
+	export PERL5LIB=${PERLLIB}
+elif [ x$PLAT = "xRHEL4_64" -o x$PLAT = "xRHEL5_64" -o x$PLAT = "xSLES10_64" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/x86_64-linux-thread-multi"
+	export PERL5LIB=${PERLLIB}
+elif [ x$PLAT = "xSuSEES9" -o x$PLAT = "xSuSEES10" -o x$PLAT = "xopenSUSE_10.2" -o x$PLAT = "xSuSE10" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/i586-linux-thread-multi"
+	export PERL5LIB=${PERLLIB}
+elif [ x$PLAT = "xDEBIAN4.0" -o x$PLAT = "xUBUNTU6" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/i486-linux-gnu-thread-multi"
+	export PERL5LIB=${PERLLIB}
+elif [ x$PLAT = "xMACOSXx86" -o x$PLAT = "xMACOSX" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/darwin-thread-multi-2level"
+	export PERL5LIB=${PERLLIB}
+elif [ x$PLAT = "xMANDRIVA2006" ]; then
+	export PERLLIB="${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib:${BUILD_HOME}/$RELEASE/ThirdParty/Perl/zimbramon/lib/i386-linux"
+	export PERL5LIB=${PERLLIB}
+fi
+
+echo "Resyncing thirdparty source for $RELEASE"
+cd ${BUILD_HOME}/p4/$RELEASE/ThirdParty
+/usr/local/p4/bin/p4 sync ... > /dev/null 
+cd ${BUILD_HOME}/p4/$RELEASE/ZimbraBuild
+/usr/local/p4/bin/p4 sync ... > /dev/null 
+
+if [ x$RELEASE = "xmain" ]; then
+  cd ${BUILD_HOME}/p4/$RELEASE/ThirdPartyBuilds/$PLAT
+  /usr/local/p4/bin/p4 sync ... > /dev/null 
+fi
+
+echo "Removing /opt/zimbra"
+if [ -d "/opt/zimbra" ]; then
+  rm -rf /opt/zimbra
+  mkdir /opt/zimbra
+fi
+
+if [ -x "/sbin/ldconfig" ]; then
+  /sbin/ldconfig
+fi
+
+
+cd ${BUILD_HOME}/$RELEASE/ThirdParty 
+rm -f make.out 2> /dev/null
+make allclean > /dev/null 2>&1
+make all 2>&1 | tee -a make.out
