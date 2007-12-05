24796be030f50f6c83b38177955bacca0078831a
Quanah Gibson-Mount <quanah@zimbra.com>
2007-12-05 00:23:19 +0000

    bug: 19466 Delete old users
    http://bugzilla.zimbra.com/show_bug.cgi?id=19466

---
+++ ZimbraServer/src/db/migrate20071204-deleteOldLDAPUsers.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+use strict;
+use Net::LDAPapi;
+
+my ($binddn,$bindpwd,$host,$junk,$result,@localconfig,$ismaster);
+@localconfig=`/opt/zimbra/bin/zmlocalconfig -s ldap_master_url zimbra_ldap_userdn zimbra_ldap_password ldap_is_master`;
+$host=$localconfig[0];
+($junk,$host) = split /= /, $host, 2;
+chomp $host;
+
+$binddn=$localconfig[1];
+($junk,$binddn) = split /= /, $binddn, 2;
+chomp $binddn;
+
+$bindpwd=$localconfig[2];
+($junk,$bindpwd) = split /= /, $bindpwd, 2;
+chomp $bindpwd;
+
+$ismaster=$localconfig[3];
+($junk,$ismaster) = split /= /, $ismaster, 2;
+chomp $ismaster;
+
+if ($ismaster ne "true") {
+  exit;
+}
+
+print "Deleting old LDAP users\n";
+my $ld = Net::LDAPapi->new(-url=>"$host");
+my $status;
+
+if ($host !~ /^ldaps/i) {
+  $status=$ld->start_tls_s();
+}
+
+$status = $ld->bind_s($binddn,$bindpwd);
+
+$status = $ld->delete("uid=zimbrareplication,cn=admins,cn=zimbra");
+$status = $ld->delete("uid=zmpostfix,cn=admins,cn=zimbra");
+$status = $ld->delete("uid=zmamavis,cn=admins,cn=zimbra");
+
+$ld->unbind();
+
+print "done.\n";
