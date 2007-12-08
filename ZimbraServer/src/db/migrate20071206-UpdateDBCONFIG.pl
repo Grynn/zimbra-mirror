6e9eb789e1062c50972794f8fc24fe6096152d34
Quanah Gibson-Mount <quanah@zimbra.com>
2007-12-08 06:28:31 +0000

    bug: 19348 Script to update the DB_CONFIG file for its locks & lockers if needed
    http://bugzilla.zimbra.com/show_bug.cgi?id=19348

---
+++ ZimbraServer/src/db/migrate20071206-UpdateDBCONFIG.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use File::Grep qw (fgrep);
+
+my $dbfile= "/opt/zimbra/openldap-data/DB_CONFIG";
+
+print "Checking for necessary locker settings for OpenLDAP database\n";
+if ( -f "${dbfile}" ) {
+  if ( !fgrep { /set_lk_max_locks|SET_LK_MAX_LOCKS/ } "${dbfile}" ) {
+	chmod(0660, "$dbfile");
+	open(DB,">>$dbfile") || die("Cannot Open File");
+	print DB "set_lk_max_locks\t3000\n";
+	close(DB);
+	chmod(0440, "$dbfile");
+  }
+  if ( !fgrep { /set_lk_max_objects|SET_LK_MAX_OBJECTS/ } "${dbfile}" ) {
+	chmod(0660, "$dbfile");
+	open(DB,">>$dbfile") || die("Cannot Open File");
+	print DB "set_lk_max_objects\t1500\n";
+	close(DB);
+	chmod(0440, "$dbfile");
+  }
+  if ( !fgrep { /set_lk_max_lockers|SET_LK_MAX_LOCKERS/ } "${dbfile}" ) {
+	chmod(0660, "$dbfile");
+	open(DB,">>$dbfile") || die("Cannot Open File");
+	print DB "set_lk_max_lockers\t1500\n";
+	close(DB);
+	chmod(0440, "$dbfile");
+  }
+}
+
