d9abcdd8edab35a526c2e94632f9490c76944f80
Quanah Gibson-Mount <quanah@zimbra.com>
2007-12-31 22:49:51 +0000

    bug: 23344 OpenSSL 0.9.8g
    http://bugzilla.zimbra.com/show_bug.cgi?id=23344

--- ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
+++ ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
@@ -18,7 +18,7 @@ p4_root=`cd ${cyrus_root}/../..; pwd`
 build_platform=`sh ${p4_root}/ZimbraBuild/rpmconf/Build/get_plat_tag.sh`
 
 heimdal_version=1.0.2
-openssl_version=0.9.8e
+openssl_version=0.9.8g
 curl_version=7.17.0
 xml2_version=2.6.29
 
