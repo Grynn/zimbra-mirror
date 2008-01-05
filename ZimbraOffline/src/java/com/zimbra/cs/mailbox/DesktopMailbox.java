6fd2c359cb2958fe2ccd3a99b776020d69012fa2
 Unknown Perforce User <jjzhuang>
2008-01-05 22:21:36 +0000

    bug: 22693
    Missed a file in 79181.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=22693

---
+++ ZimbraOffline/src/java/com/zimbra/cs/mailbox/DesktopMailbox.java
@@ -0,0 +1,66 @@
+package com.zimbra.cs.mailbox;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.Constants;
+import com.zimbra.cs.account.offline.OfflineAccount;
+import com.zimbra.cs.offline.OfflineLog;
+import com.zimbra.cs.util.Zimbra;
+
+public abstract class DesktopMailbox extends Mailbox {
+
+	private Timer timer;
+	private TimerTask currentTask;
+	
+	public DesktopMailbox(MailboxData data) throws ServiceException {
+		super(data);
+		resetSyncTimer();
+	}
+	
+	@Override
+    public synchronized void deleteMailbox() throws ServiceException {
+		cancelCurrentTask();
+		super.deleteMailbox();
+    }
+	
+	private synchronized void cancelCurrentTask() {
+		if (currentTask != null)
+			currentTask.cancel();
+		currentTask = null;
+	}
+	
+	protected synchronized void resetSyncTimer() throws ServiceException {
+		if (((OfflineAccount)getAccount()).isLocalAccount())
+			return;
+		
+		cancelCurrentTask();
+		
+//		if (isAutoSyncDisabled())
+//			return;
+		
+		currentTask = new TimerTask() {
+				public void run() {
+					try {
+						syncOnTimer();
+					} catch (Throwable e) { //don't let exceptions kill the timer
+						if (e instanceof OutOfMemoryError)
+							Zimbra.halt("Caught out of memory error", e);
+						OfflineLog.offline.warn("Caught exception in timer ", e);
+					}
+				}
+			};
+		
+		timer = timer == null ? new Timer("sync-timer-" + getAccount().getName()) : timer;
+		timer.schedule(currentTask, 5 * Constants.MILLIS_PER_SECOND, 5 * Constants.MILLIS_PER_SECOND);
+	}
+	
+	protected synchronized void syncNow() {
+		//do nothing
+	}
+	
+	protected abstract void syncOnTimer();
+
+	public abstract boolean isAutoSyncDisabled();
+}
