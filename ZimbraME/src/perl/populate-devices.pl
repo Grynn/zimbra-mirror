ceab22b0acba95b5bbaa22dea03ceb83e9a4cadf
 Unknown Perforce User <anandp>
2008-02-26 00:04:07 +0000

    bug: none change license preamble of J2ME from Zimbra Network Edition -> YPL

--- ZimbraME/src/perl/populate-devices.pl
+++ ZimbraME/src/perl/populate-devices.pl
@@ -1,10 +1,16 @@
 #!/usr/bin/perl
 # 
 # ***** BEGIN LICENSE BLOCK *****
+# Zimbra Collaboration Suite J2ME Client
+# Copyright (C) 2007 Zimbra, Inc.
 # 
-# Zimbra Collaboration Suite, Network Edition.
-# Copyright (C) 2007 Zimbra, Inc.  All Rights Reserved.
+# The contents of this file are subject to the Yahoo! Public License
+# Version 1.0 ("License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://www.zimbra.com/license.
 # 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 # ***** END LICENSE BLOCK *****
 # 
 
