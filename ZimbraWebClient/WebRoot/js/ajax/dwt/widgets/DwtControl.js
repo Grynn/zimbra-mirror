ffc448fa74c9ff01f9883c69ea580e66dd11d87e
Conrad Damon <cdamon@zimbra.com>
2008-03-15 00:04:11 +0000

    Bug: 9441 - The Esc key should cancel a drag-n-drop
    Esc key cancels DnD operation
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=9441

--- ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtControl.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtControl.js
@@ -2347,9 +2347,7 @@ function(ev) {
 						obj._setDragProxyState(true);
 						obj.__dropAllowed = true;
 						destDwtObj._dragEnter(mouseEv);
-					}
-					else
-					{
+					} else {
 						obj._setDragProxyState(false);
 						obj.__dropAllowed = false;
 					}
@@ -2425,20 +2423,7 @@ function(ev) {
 				obj._destroyDragProxy(obj._dndProxy);
 				obj._dragging = DwtControl._NO_DRAG;
 			} else {
-				obj._dragSource._cancelDrag();
-				// The following code sets up the drop effect for when an
-				// item is dropped onto an invalid target. Basically the
-				// drag icon will spring back to its starting location.
-				obj.__dragEndX = mouseEv.docX;
-				obj.__dragEndY = mouseEv.docY;
-				if (obj.__badDropAction == null) {
-					obj.__badDropAction = new AjxTimedAction(obj, obj.__badDropEffect);
-				}
-
-				// Line equation is y = mx + c. Solve for c, and set up d (direction)
-				var m = (obj.__dragEndY - obj.__dragStartY) / (obj.__dragEndX - obj.__dragStartX);
-				obj.__badDropAction.args = [m, obj.__dragStartY - (m * obj.__dragStartX), (obj.__dragStartX - obj.__dragEndX < 0) ? -1 : 1];
-				AjxTimedAction.scheduleAction(obj.__badDropAction, 0);
+				DwtControl.__badDrop(obj, mouseEv);
 			}
 			mouseEv._stopPropagation = true;
 			mouseEv._returnValue = false;
@@ -2446,6 +2431,31 @@ function(ev) {
 			return false;
 		}
 	}
+};
+
+/**
+ * Handles a bad DND drop operation by showing an animation of the icon flying
+ * back to its origin.
+ * 
+ * @param obj		[DwtControl]	control that underlies drag operation
+ * @param mouseEv	[DwtMouseEvent]	mouse event
+ */
+DwtControl.__badDrop =
+function(obj, mouseEv) {
+	obj._dragSource._cancelDrag();
+	// The following code sets up the drop effect for when an
+	// item is dropped onto an invalid target. Basically the
+	// drag icon will spring back to its starting location.
+	obj.__dragEndX = mouseEv.docX;
+	obj.__dragEndY = mouseEv.docY;
+	if (obj.__badDropAction == null) {
+		obj.__badDropAction = new AjxTimedAction(obj, obj.__badDropEffect);
+	}
+
+	// Line equation is y = mx + c. Solve for c, and set up d (direction)
+	var m = (obj.__dragEndY - obj.__dragStartY) / (obj.__dragEndX - obj.__dragStartX);
+	obj.__badDropAction.args = [m, obj.__dragStartY - (m * obj.__dragStartX), (obj.__dragStartX - obj.__dragEndX < 0) ? -1 : 1];
+	AjxTimedAction.scheduleAction(obj.__badDropAction, 0);
 };
 
 /**
