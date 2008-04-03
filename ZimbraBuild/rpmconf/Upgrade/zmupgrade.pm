82ae69f2fefb3d927b6a0398ee09e281496bc1e8
 Unknown Perforce User <brian>
2008-04-03 20:23:14 +0000

    Integrated the following from //depot/FRANKLIN/...: Change 91437 by brian@brian-franklin on 2008/04/03 13:22:17
    bug: 26685
    fix typo in attribute name zimbraPrefCalendarReminderDuration1
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=26685

--- ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
+++ ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
@@ -2075,7 +2075,7 @@ sub upgrade505GA {
 	main::progress("Updating from 5.0.5_GA\n");
   if (main::isInstalled("zimbra-ldap")) {
 	  my @coses = `su - zimbra -c "$ZMPROV gac"`;
-    my %attrs = ( zimbraPrefCalendarReminderDiration1 => "-PT15M",
+    my %attrs = ( zimbraPrefCalendarReminderDuration1 => "-PT15M",
                zimbraFeatureNewAddrBookEnabled => "TRUE",
                zimbraInterceptSendHeadersOnly => "FALSE",
                zimbraInterceptFrom => 'Postmaster <postmaster@\${ACCOUNT_DOMAIN}>',
