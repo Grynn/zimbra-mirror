961e36eec009d274921f2e678b7de16142ed7416
 Unknown Perforce User <jjzhuang>
2008-05-20 13:16:53 +0000

    bug: none
    p4 integrate -b FRANKLIN_to_main @98608,98608
    Don't rely on BuildInfo for app-id used in IMAP ID command.

--- ZimbraOffline/src/java/com/zimbra/cs/offline/OfflineApplication.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/OfflineApplication.java
@@ -5,6 +5,11 @@ import com.zimbra.cs.util.ZimbraApplication;
 public class OfflineApplication extends ZimbraApplication {
 
 	@Override
+	public String getId() {
+		return OfflineLC.zdesktop_app_id.value();
+	}
+	
+	@Override
 	public boolean supports(String className) {
 		return false;
 	}
