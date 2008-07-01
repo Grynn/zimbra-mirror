aa69c4cf4db813b3880b611ae7e34b0b09536213
Conrad Damon <cdamon@zimbra.com>
2008-07-01 21:45:32 +0000

    Integrate change 105100 from FRANKLIN to main
    -------------
    Bug: 28843 - Improve shortcut localizability
    Reformat ZhKeys files. Use fields 'display' and
    'keycode'.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=28843

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/BindKeyTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/BindKeyTag.java
@@ -59,66 +59,6 @@ public class BindKeyTag extends ZimbraSimpleTag {
      public static final String SPACE = "Space";
      public static final String BACKSLASH = "Backslash";
 
-     private static final Map<Integer, String> sKeyCodeToString = new HashMap<Integer, String>();
-     private static final Map<String, Integer> sStringToKeyCode = new HashMap<String, Integer>();
-
-     private static void mapKey(int keyCode, String str) {
-         sKeyCodeToString.put(keyCode, str);
-         sStringToKeyCode.put(str, keyCode);
-     }
-
-     static {
-         mapKey(18, ALT);
-         mapKey(40, ARROW_DOWN);
-         mapKey(37, ARROW_LEFT);
-         mapKey(39, ARROW_RIGHT);
-         mapKey(38, ARROW_UP);
-         mapKey(8, BACKSPACE);
-         mapKey(188, COMMA);
-         mapKey(186, SEMICOLON);
-         mapKey(59, SEMICOLON);
-         mapKey(17, CTRL);
-         mapKey(46, DELETE);
-         mapKey(35, END);
-         mapKey(13, ENTER);
-         mapKey(27, ESC);
-         mapKey(36, HOME);
-         mapKey(91, META);
-         mapKey(34, PAGE_DOWN);
-         mapKey(33, PAGE_UP);
-         mapKey(16, SHIFT);
-         mapKey(32, SPACE);
-         //mapKey(9, TAB);
-         mapKey(220, BACKSLASH);
-
-         // Function keys
-         for (int i = 112; i < 124; i++) {
-             mapKey(i, "F" + (i - 111));
-         }
-
-         // Take advantage of the fact that keycode for capital letters are the
-         // same as the charcode values i.e. ASCII code
-         for (char i = 65; i < 91; i++) {
-             mapKey(i, Character.toString(i));
-         }
-
-         // Numbers 0 - 9
-         for (char i = 48; i < 58; i++) {
-             mapKey(i, Character.toString(i));
-         }
-
-         // punctuation
-         mapKey(222, "'");
-         mapKey(189, "-");
-         mapKey(190, ".");
-         mapKey(191, "/");
-         mapKey(186, ";");
-         mapKey(219, "[");
-         mapKey(221, "]");
-         mapKey(192, "`");
-         mapKey(187, "=");
-     }
-
     private static final String BINDKEY_CACHE = "BindKeyTag.CACHE";
 
     private static final String A_GOTO_TAG = "gototag";
@@ -180,14 +120,15 @@ public class BindKeyTag extends ZimbraSimpleTag {
     private String resolveMessageKey(JspContext ctxt, String message) {
         ZUserAgentBean ua = GetUserAgentTag.getUserAgent(ctxt,true);
 
+        String prop = message + ".keycode";
         if (ua != null) {
             String os = ua.getIsOsWindows() ? ".win" : ua.getIsOsMac() ? ".mac" : ua.getIsOsLinux() ? ".linux" : null;
             if (os != null) {
-                String key = I18nUtil.getLocalizedMessage((PageContext)ctxt, message+os, mBasename);
+                String key = I18nUtil.getLocalizedMessage((PageContext)ctxt, prop + os, mBasename);
                 if (!key.startsWith("???")) return key;
             }
         }
-        return I18nUtil.getLocalizedMessage((PageContext)ctxt, message, mBasename);
+        return I18nUtil.getLocalizedMessage((PageContext)ctxt, prop, mBasename);
     }
 
     private String generateGoToTagAliases(JspContext ctxt, NumericShortCutType type) throws JspException, ServiceException, IOException {
@@ -300,7 +241,7 @@ public class BindKeyTag extends ZimbraSimpleTag {
             if (mFunc != null)
                 js.append(String.format("bindKey('%s', %s);%n", sb.toString(), mFunc));
             else if (mUrl != null)
-                js.append(String.format("bindKey('%s', function(){ window.location=\"%s\";});%n", StringUtil.jsEncode(sb.toString()), mUrl));
+                js.append(String.format("bindKey('%s', function(){ window.location=\"%s\";});%n", sb.toString(), mUrl));
             else
                 js.append(String.format("bindKey('%s', '%s');%n", sb.toString(), mId));
         }
@@ -313,8 +254,8 @@ public class BindKeyTag extends ZimbraSimpleTag {
         if (seq.contains(CTRL)) code.append('c');
         if (seq.contains(META)) code.append('m');
         if (seq.contains(SHIFT)) code.append('s');
-        Integer kc = sStringToKeyCode.get(seq.substring(seq.lastIndexOf('+') + 1));
-        if (kc == null || kc == 0) throw new JspTagException("invalid key binding: "+seq);
+        String kc = seq.substring(seq.lastIndexOf('+') + 1);
+        if (kc == null || kc == "") throw new JspTagException("invalid key binding: "+seq);
         code.append(kc);
         return code.toString();
     }
