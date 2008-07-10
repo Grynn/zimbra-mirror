d23a1880674f0fda5f7e4e17f7fae80703afc142
 Unknown Perforce User <jmhe>
2008-07-10 06:08:28 +0000

    bug 29133 franklin->main
    http://bugzilla.zimbra.com/show_bug.cgi?id=29133

--- ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/PageBean.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/PageBean.java
@@ -1,20 +1,25 @@
 package com.zimbra.cs.offline.jsp;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.text.MessageFormat;
 
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.offline.common.OfflineConstants;
+import com.zimbra.cs.account.Provisioning;
 
 public class PageBean {
 
 	private static final String APP_VERSION = LC.get("zdesktop_relabel") + LC.get("zdesktop_version") + " (build " + LC.get("zdesktop_buildid") + ")";
 	
-	private Locale locale = new Locale("en");
+	private Locale clientLocale = new Locale("en");
+	private static Locale serverLocale = null; 
 	
 	protected String getMessage(String key) {
-		ResourceBundle bundle = ResourceBundle.getBundle("/desktop/ZdMsg", locale);
+		ResourceBundle bundle = ResourceBundle.getBundle("/desktop/ZdMsg", clientLocale);
 		String msg =  bundle.getString(key);
 		return msg == null ? "??" + key + "??" : msg; 
 	}
@@ -37,7 +42,19 @@ public class PageBean {
 		}
 	}
 	
-	public void setLocale(Locale locale) {
-		this.locale = locale;
+	public void setLocale(Locale locale) throws ServiceException {
+		clientLocale = locale;
+		updateServerLocale(locale);
+	}
+	
+	public static synchronized void updateServerLocale(Locale locale) throws ServiceException {
+	    if (serverLocale == null || !serverLocale.equals(locale)) {	        
+	        Map<String, Object> attrs = new HashMap<String, Object>();
+	        attrs.put(Provisioning.A_zimbraPrefLocale, locale.toString());
+	        JspProvStub stub = JspProvStub.getInstance();
+	        stub.modifyOfflineAccount(OfflineConstants.LOCAL_ACCOUNT_ID, attrs);
+	        
+	        serverLocale = (Locale)locale.clone();						
+	    }
 	}
 }
