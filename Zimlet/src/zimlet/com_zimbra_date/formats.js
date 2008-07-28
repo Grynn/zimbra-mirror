4fc0fd7ad2f23b2a9ca967cb88594361c9f4438c
 Unknown Perforce User <aclark>
2008-07-28 17:21:19 +0000

    bug: none Integrate 108523 into main.

--- Zimlet/src/zimlet/com_zimbra_date/formats.js
+++ Zimlet/src/zimlet/com_zimbra_date/formats.js
@@ -1,5 +1,7 @@
 Com_Zimbra_Date.DEFAULT_FORMATS = [
 	"Nicholas Mateo Damon",			"2008-07-18T03:35:00Z",
 	"Shyar Parag Shah",				"2008-07-15T14:51:00Z",
-	"Diego Isaac Comfort",			"2006-12-08T04:05:00Z"
+	"Diego Isaac Comfort",			"2006-12-08T04:05:00Z",
+	"Nikhil Dharmaraj",				"2001-08-22T06:00:00Z",
+	"Nila Dharmaraj",				"2006-03-19T00:30:00Z"
 ];
