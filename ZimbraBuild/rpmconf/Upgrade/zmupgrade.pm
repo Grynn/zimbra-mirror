b0e6f25cd7fd2e7ed8580ede817901693b33477c
Quanah Gibson-Mount <quanah@zimbra.com>
2008-08-05 21:31:26 +0000

    Integrated the following from //depot/FRANKLIN/...: Change 110008 by quanah@qfranklin on 2008/08/05 14:31:06
    bug: 29009
    Add upgrade step
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=29009

--- ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
+++ ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
@@ -2334,6 +2334,7 @@ sub upgrade509GA {
 
   if (main::isInstalled("zimbra-ldap")) {
 	if($isLdapMaster) {
+	  upgradeLdapConfigValue("zimbraCalendarCalDavDisableFreebusy", "FALSE", "");
 	  upgradeLdapConfigValue("zimbraImapExposeVersionOnBanner", "FALSE", "");
 	  upgradeLdapConfigValue("zimbraLmtpExposeVersionOnBanner", "FALSE", "");
 	  upgradeLdapConfigValue("zimbraPop3ExposeVersionOnBanner", "FALSE", "");
