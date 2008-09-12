35377c5ff754a903fd324dbc9547ab102d1c3b49
 Unknown Perforce User <brian>
2008-09-12 23:25:13 +0000

    Integrated the following from //depot/FRANKLIN/...: Change 114303 by brian@brian-franklin on 2008/09/11 12:55:54
    bug: 31502
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31502

--- ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
+++ ZimbraBuild/rpmconf/Upgrade/zmupgrade.pm
@@ -2728,6 +2728,7 @@ sub updateLoggerMySQLcnf {
 }
 sub updateMySQLcnf {
 
+  return if ($mysqlcnfUpdated == 1);
   my $mycnf = "/opt/zimbra/conf/my.cnf";
   my $mysql_pidfile = main::getLocalConfig("mysql_pidfile");
   $mysql_pidfile = "/opt/zimbra/db/mysql.pid" if ($mysql_pidfile eq "");
@@ -2753,12 +2754,29 @@ sub updateMySQLcnf {
         print TMP "pid-file = ${mysql_pidfile}\n";
         $mycnfChanged=1;
         next;
-      } elsif (/^thread_cache\s/) {
+      } elsif (/^thread_cache\s+=\s+(\d+)$/) {
         # 29475 fix thread_cache_size
-        s/^thread_cache/thread_cache_size/g;
-        print TMP;
+        if ($1 > 110) {
+          s/^thread_cache/thread_cache_size/g;
+          print TMP;
+        } else {
+          print TMP "thread_cache_size = 110\n";
+          next;
+        }
         $mycnfChanged=1;
         next;
+      } elsif (/^thread_cache_size\s+=\s+(\d+)$/) {
+        if ($1 < 110) {
+          $mycnfChanged=1;
+          print TMP "thread_cache_size = 110\n";
+          next;
+        }
+      } elsif (/^max_connections\s+=\s+(\d+)$/) {
+        if ($1 < 110) {
+          $mycnfChanged=1;
+          print TMP "max_connections = 110\n";
+          next;
+        }
       } elsif (/^skip-external-locking/) {
         # 19749 remove skip-external-locking
         print TMP "external-locking\n";
