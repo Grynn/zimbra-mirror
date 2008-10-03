7a9a0dce34d44742e6c8bc0d70822698cb808ebc
Quanah Gibson-Mount <quanah@zimbra.com>
2008-10-03 03:13:34 +0000

    bug: none New curl release

--- ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
+++ ThirdParty/cyrus-sasl/zimbra-cyrus-sasl-build.sh
@@ -19,7 +19,7 @@ build_platform=`sh ${p4_root}/ZimbraBuild/rpmconf/Build/get_plat_tag.sh`
 
 heimdal_version=1.2.1
 openssl_version=0.9.8i
-curl_version=7.18.2
+curl_version=7.19.0
 xml2_version=2.6.32
 
 openssl_lib_dir=/opt/zimbra/openssl-${openssl_version}/lib
