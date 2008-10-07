81765889a53790a31b836302eeed2fe9aadd8b1f
 Unknown Perforce User <dac>
2008-10-07 02:45:19 +0000

    bug: 26704 Add missing file to fix build.
    http://bugzilla.zimbra.com/show_bug.cgi?id=26704

--- ZimbraOffline/src/java/com/zimbra/cs/offline/yab/YabImport.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/yab/YabImport.java
@@ -17,20 +17,23 @@
 package com.zimbra.cs.offline.yab;
 
 import com.zimbra.cs.account.DataSource;
+import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.offline.OfflineDataSource;
+import com.zimbra.cs.account.offline.OfflineProvisioning;
 import com.zimbra.cs.offline.util.yab.Session;
 import com.zimbra.cs.offline.util.OfflineYAuth;
-import com.zimbra.cs.offline.OfflineLC;
-import com.zimbra.cs.util.yauth.RawAuthManager;
-import com.zimbra.cs.util.yauth.Authenticator;
+import com.zimbra.cs.mailbox.SyncExceptionHandler;
+import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.common.service.ServiceException;
 
 import java.util.List;
 
 public class YabImport implements DataSource.DataImport {
     private final OfflineDataSource ds;
-    private SyncSession session;
+    private final SyncSession session;
 
+    private static final String ERROR = "Yahoo address book synchronization failed";
+    
     public YabImport(DataSource ds) throws ServiceException {
         this.ds = (OfflineDataSource) ds;
         session = newSyncSession();
@@ -43,11 +46,24 @@ public class YabImport implements DataSource.DataImport {
     public void importData(List<Integer> folderIds, boolean fullSync)
         throws ServiceException {
         ds.getMailbox().beginTrackingSync();
-        session.sync();
+        try {
+            session.sync();
+        } catch (Exception e) {
+            SyncExceptionHandler.checkRecoverableException(ERROR, e);
+            ds.reportError(Mailbox.ID_FOLDER_CONTACTS, ERROR + " - contact synchronization disabled.", e);
+            disableYabSync();
+        }
     }
 
     private SyncSession newSyncSession() throws ServiceException {
         Session session = new Session(OfflineYAuth.newAuthenticator(ds));
         return new SyncSession(ds, session);
+    }
+
+    private void disableYabSync() throws ServiceException {
+        OfflineProvisioning op = (OfflineProvisioning) Provisioning.getInstance();
+        op.setDataSourceAttribute(ds,
+            OfflineProvisioning.A_zimbraDataSourceYabSyncEnabled,
+            Provisioning.FALSE);
     }
 }
