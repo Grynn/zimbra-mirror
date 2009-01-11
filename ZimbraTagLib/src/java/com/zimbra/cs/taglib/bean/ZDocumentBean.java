b15c95531cae68d212e2c701aa98c4173afe1abe
 Unknown Perforce User <ssugumaran>
2009-01-11 13:50:48 +0000

    bug: 33931 added GetDocumentTag tag library which will be used in the rest module for getting slides and other document info
    http://bugzilla.zimbra.com/show_bug.cgi?id=33931

---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZDocumentBean.java
@@ -0,0 +1,79 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2007 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.taglib.bean;
+
+import com.zimbra.cs.zclient.ZDocument;
+import com.zimbra.common.mime.ContentType;
+
+import java.util.Date;
+
+public class ZDocumentBean {
+
+    private ZDocument mDoc;
+
+    public ZDocumentBean(ZDocument doc) {
+        mDoc = doc;
+    }
+
+    public Date getCreatedDate() {
+        return new Date(mDoc.getCreatedDate());
+    }
+
+    public Date getModifiedDate() {
+        return new Date(mDoc.getModifiedDate()/1000);
+    }
+
+    public Date getMetaDataChangedDate() {
+        return new Date(mDoc.getMetaDataChangedDate());
+    }
+
+	public String getId() {
+		return mDoc.getId();
+	}
+	public String getName() {
+		return mDoc.getName();
+	}
+	public String getFolderId() {
+		return mDoc.getFolderId();
+	}
+	public String getVersion() {
+		return mDoc.getVersion();
+	}
+	public String getEditor() {
+		return mDoc.getEditor();
+	}
+	public String getCreator() {
+		return mDoc.getCreator();
+	}
+	public String getRestUrl() {
+		return mDoc.getRestUrl();
+	}
+	public boolean isWiki() {
+		return mDoc.isWiki();
+    }
+    public String getContentType() {
+        String contentType = mDoc.getContentType();
+        return new ContentType(contentType).getValue();
+    }
+    public long getSize() {
+        return mDoc.getSize();
+    }
+    public String getTagIds() {
+        return mDoc.getTagIds();
+    }
+  
+}
---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/briefcase/GetDocumentTag.java
@@ -0,0 +1,62 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2007 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.cs.taglib.tag.briefcase;
+
+import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.taglib.bean.ZMessageComposeBean;
+import com.zimbra.cs.taglib.bean.ZMessageBean;
+import com.zimbra.cs.taglib.bean.ZMailboxBean;
+import com.zimbra.cs.taglib.bean.ZDocumentBean;
+import com.zimbra.cs.zclient.ZMailbox;
+import com.zimbra.cs.zclient.ZDocument;
+import com.zimbra.common.service.ServiceException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+
+public class GetDocumentTag extends ZimbraSimpleTag {
+
+    private String mVar;
+    private String mId;
+    private ZMailboxBean mMailbox;
+
+    public void setId(String id) { this.mId = id; }
+    public void setVar(String var) { this.mVar = var; }
+    public void setBox(ZMailboxBean mailbox) { this.mMailbox = mailbox; }    
+
+    public void doTag() throws JspException, IOException {
+        JspContext jctxt = getJspContext();
+        PageContext pc = (PageContext) jctxt;
+        try {
+            ZMailbox mbox = mMailbox != null ? mMailbox.getMailbox() :  getMailbox();
+			ZDocument doc = mbox.getDocument(this.mId);
+            jctxt.setAttribute(mVar, new ZDocumentBean(doc), PageContext.PAGE_SCOPE);
+        } catch (ServiceException e) {
+            throw new JspTagException(e.getMessage(), e);
+        }
+
+    }
+
+}
