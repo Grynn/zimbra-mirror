eaf7bd97568307cba3662af6522cbc17e60c8e46
 Unknown Perforce User <jmhe>
2009-01-12 23:38:21 +0000

    bug 34127 - integration from franklin - make load rss feed work for non-zimbra accounts - code cleanup
    http://bugzilla.zimbra.com/show_bug.cgi?id=34127

--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineFolderAction.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineFolderAction.java
@@ -21,11 +21,13 @@ import java.util.Map;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.common.soap.SoapFaultException;
+import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.cs.mailbox.OfflineMailbox;
 import com.zimbra.cs.mailbox.OfflineMailboxManager;
 import com.zimbra.cs.mailbox.OfflineServiceException;
+import com.zimbra.cs.mailbox.Mailbox.OperationContext;
 import com.zimbra.cs.service.mail.FolderAction;
 import com.zimbra.common.soap.Element;
 import com.zimbra.soap.ZimbraSoapContext;
@@ -34,39 +36,48 @@ public class OfflineFolderAction extends FolderAction {
 
     @Override
     public Element handle(Element request, Map<String, Object> context) throws ServiceException, SoapFaultException {
-        ZimbraSoapContext zsc = getZimbraSoapContext(context);
-        Mailbox mbox = getRequestedMailbox(zsc);
-        
-        Element action = request.getElement(MailConstants.E_ACTION);
-        String operation = action.getAttribute(MailConstants.A_OPERATION).toLowerCase();
-        
-        if (operation.equals(OP_GRANT) && (mbox instanceof OfflineMailbox)) {
-            Element parent = request.getParent();
-            boolean fromBatch = parent != null && parent.getName().equals("BatchRequest");            
-            Element response = ((OfflineMailbox)mbox).proxyRequest(request, zsc.getResponseProtocol(), false, "grant permission");
-            if (fromBatch)
-                response.detach();
-            return response;
-        }
-        
-        if (!operation.equals(OP_REFRESH) && !operation.equals(OP_IMPORT))
-            return super.handle(request, context);
-
         MailboxManager mmgr = MailboxManager.getInstance();
         if (!(mmgr instanceof OfflineMailboxManager))
             return super.handle(request, context);
- 
+        
+        Element action = request.getElement(MailConstants.E_ACTION);
+        String operation = action.getAttribute(MailConstants.A_OPERATION).toLowerCase();           
+        if (!operation.equals(OP_REFRESH) && !operation.equals(OP_IMPORT) && !operation.equals(OP_GRANT))
+            return super.handle(request, context);        
+        
+        String folderId = action.getAttribute(MailConstants.A_ID);
+        int pos = folderId.indexOf(':');
+        if (pos >= 0)
+            folderId = folderId.substring(pos + 1);
+        int fid = Integer.parseInt(folderId);
+        
+        ZimbraSoapContext zsc = getZimbraSoapContext(context);
+        Mailbox mbox = getRequestedMailbox(zsc);
+        OperationContext octxt = getOperationContext(zsc, context);
+        Folder folder = mbox.getFolderById(octxt, fid);
+        if (operation.equals(OP_REFRESH) && !folder.getUrl().equals("")) //e.g. load rss feed
+            return super.handle(request, context);
+
+        // Ops below apply to OfflineMailbox only
         if (!(mbox instanceof OfflineMailbox))
             throw OfflineServiceException.MISCONFIGURED("incorrect mailbox class: " + mbox.getClass().getSimpleName());
         OfflineMailbox ombx = (OfflineMailbox) mbox;
         
-        // before doing anything, make sure all data sources are pushed to the server
-        ombx.sync(true, false);
-        // proxy this operation to the remote server
-        Element response = ombx.proxyRequest(request, zsc.getResponseProtocol(), true, operation);
-        // and get a head start on the sync of the newly-pulled-in messages
-        ombx.sync(true, false);
-
+        Element response;
+        if (operation.equals(OP_GRANT)) {
+            Element parent = request.getParent();
+            boolean fromBatch = parent != null && parent.getName().equals("BatchRequest");            
+            response = ombx.proxyRequest(request, zsc.getResponseProtocol(), false, operation);
+            if (fromBatch)
+                response.detach();
+        } else { 
+            // before doing anything, make sure all data sources are pushed to the server
+            ombx.sync(true, false);
+            // proxy this operation to the remote server
+            response = ombx.proxyRequest(request, zsc.getResponseProtocol(), true, operation);
+            // and get a head start on the sync of the newly-pulled-in messages
+            ombx.sync(true, false);
+        }
         return response;
     }
 }
