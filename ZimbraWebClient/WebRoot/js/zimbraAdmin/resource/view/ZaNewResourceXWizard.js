26fa78cb5fddd2b8b657c7899524d410d5c5026b
 Unknown Perforce User <greg>
2009-02-14 01:07:45 +0000

    bug none fix resource email auto-fill

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/resource/view/ZaNewResourceXWizard.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/resource/view/ZaNewResourceXWizard.js
@@ -238,9 +238,13 @@ ZaNewResourceXWizard.myXFormModifier = function(xFormObject) {
 					//auto fill the account name when autodisplayname is true
 					if(this.getInstance()[ZaResource.A2_autodisplayname]=="TRUE") {
 						try {
+							
+							var oldAccName = this.getInstanceValue(ZaResource.A_name);
+							var regEx = /[^a-zA-Z0-9_\-\.]/g ;
+							var newName = elementValue.replace(regEx, "") + oldAccName.substring(oldAccName.indexOf("@")) ;	
+							this.getModel().setInstanceValue(this.getInstance(),ZaResource.A_name,newName);
 							ZaResource.setAutoAccountName(this.getInstance(), elementValue );
-							this.getForm().itemChanged(this.getForm().getItemById(this.getForm().getId()+"_case").__xform.getItemById(this.getForm().getId()+"_resource_email_addr"), this.getInstance()[ZaResource.A_name], event);
-							this.getInstance()[ZaResource.A2_autodisplayname]="TRUE";
+							this.getModel().setInstanceValue(this.getInstance(),ZaResource.A2_autodisplayname,"TRUE");
 						} catch (ex) {
 							ZaApp.getInstance().getCurrentController()._handleException(ex, "XForm." + ZaResource.A_displayname + ".elementChanged", null, false);
 						}
@@ -252,10 +256,10 @@ ZaNewResourceXWizard.myXFormModifier = function(xFormObject) {
 				labelLocation:_LEFT_, choices:ZaResource.resTypeChoices
 			},	*/	
 			{ref:ZaResource.A_name, type:_EMAILADDR_, msgName:ZaMsg.NAD_ResAccountName,label:ZaMsg.NAD_ResAccountName, 
-				labelLocation:_LEFT_,id:"resource_email_addr", forceupdate: true, 
+				labelLocation:_LEFT_,id:"resource_email_addr", bmolsnr: true, 
 				onChange: function(value, event, form) {
 					//disable the autodisplayname whenever user does some action on the account name
-					this.getInstance()[ZaResource.A2_autodisplayname] = "FALSE";							
+					this.getModel().setInstanceValue(this.getInstance(),ZaResource.A2_autodisplayname,"FALSE");							
 					this.setInstanceValue(value);	
 				},visibilityChecks:[],enableDisableChecks:[]
 			}				
