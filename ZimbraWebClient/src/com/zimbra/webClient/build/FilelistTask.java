d4734d68313867fff2afd4f3c3f37db2319f0935
 Unknown Perforce User <aclark>
2009-02-20 02:40:36 +0000

    bug: none Modified build to help identify which file from a jammed package is causing the JS minimizer to fail.

---
+++ ZimbraWebClient/src/com/zimbra/webClient/build/FilelistTask.java
@@ -0,0 +1,86 @@
+package com.zimbra.webClient.build;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.ant.types.*;
+
+import java.io.*;
+import java.util.*;
+
+public class FilelistTask extends Task {
+
+	//
+	// Data
+	//
+
+	private File file;
+	private String propertyName;
+	private String separator = ",";
+	private String encoding;
+
+	//
+	// Public methods
+	//
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public void setProperty(String name) {
+		this.propertyName = name;
+	}
+
+	public void setSep(String sep) {
+		this.separator = sep;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	// Task methods
+
+	public void execute() throws BuildException {
+		// check values
+		if (this.file == null) {
+			throw new BuildException("missing attribute 'file'");
+		}
+		if (this.propertyName == null) {
+			throw new BuildException("missing attribute 'property'");
+		}
+		if (!this.file.exists()) {
+			throw new BuildException("file does not exist");
+		}
+
+		// load file
+		InputStream stream = null;
+		StringBuilder str = new StringBuilder();
+		try {
+			stream = new FileInputStream(this.file);
+			Reader reader = this.encoding != null ? new InputStreamReader(stream, this.encoding) : new InputStreamReader(stream);
+			BufferedReader in = new BufferedReader(reader);
+			String line;
+			for (int i = 0; (line = in.readLine()) != null; i++) {
+				if (i > 0) {
+					str.append(this.separator);
+				}
+				str.append(line);
+			}
+			in.close();
+		}
+		catch (IOException e) {
+			throw new BuildException(e);
+		}
+		finally {
+			try {
+				stream.close();
+			}
+			catch (Exception e) {
+				// ignore
+			}
+		}
+
+		// set property
+		getProject().setProperty(this.propertyName, str.toString());
+	}
+
+} // class FilelistTask
---
+++ ZimbraWebClient/src/com/zimbra/webClient/build/TryTask.java
@@ -0,0 +1,135 @@
+package com.zimbra.webClient.build;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.ant.taskdefs.*;
+import org.apache.tools.ant.types.*;
+
+import java.io.*;
+import java.util.*;
+
+public class TryTask {
+
+	//
+	// Data
+	//
+
+	private Sequential block;
+	private List<CatchBlock> catchBlocks = new ArrayList<CatchBlock>();
+	private Sequential finallyBlock;
+
+	//
+	// Public methods
+	//
+
+	public Sequential createBlock() {
+		return this.block = new Sequential();
+	}
+
+	public CatchBlock createCatch() {
+		CatchBlock block = new CatchBlock();
+		this.catchBlocks.add(block);
+		return block;
+	}
+
+	public Sequential createFinally() {
+		return this.finallyBlock = new Sequential();
+	}
+
+	//
+	// Task methods
+	//
+
+	public void execute() throws BuildException {
+		BuildException ex = null;
+		CatchBlock catchBlock = null;
+
+		// execute main block
+		if (this.block == null) {
+			// TODO: log that nothing to do
+		}
+		else {
+			try {
+				this.block.execute();
+			}
+			catch (BuildException e) {
+				ex = e;
+			}
+			catch (Throwable t) {
+				ex = new BuildException(t);
+			}
+		}
+
+		// find matching catch block
+		if (ex != null) {
+			Throwable t = ex.getCause();
+			for (CatchBlock block : this.catchBlocks) {
+				if (block.catches(t)) {
+					catchBlock = block;
+					break;
+				}
+			}
+		}
+
+		// handle exception and finally
+		if (catchBlock != null) {
+			catchBlock.execute();
+		}
+		if (this.finallyBlock != null) {
+			this.finallyBlock.execute();
+		}
+		if (ex != null) {
+			throw ex;
+		}
+
+	} // execute()
+
+	//
+	// Classes
+	//
+
+	static class CatchBlock extends Sequential {
+		// Constants
+		public static final String ANY = "*";
+		// Data
+		private Class exClass;
+		// Public methods
+		public void setType(String exceptionClassName) {
+			// empty or "*" value == ANY
+			exceptionClassName = exceptionClassName.trim();
+			if (exceptionClassName.length() == 0 || exceptionClassName.equals(ANY)) {
+				return;
+			}
+
+			// try literal name
+			try {
+				this.exClass = Class.forName(exceptionClassName);
+			}
+			catch (ClassNotFoundException e) {
+				// assume java.lang exception
+				try {
+					this.exClass = Class.forName("java.lang."+exceptionClassName);
+				}
+				catch (ClassNotFoundException e2) {
+					throw new BuildException(e2);
+				}
+			}
+
+			// ensure that it's of type Throwable
+			if (!this.exClass.isAssignableFrom(Throwable.class)) {
+				this.exClass = null;
+				throw new BuildException("Specified class does not derive from java.lang.Throwable");
+			}
+		}
+		public String getType() {
+			return exClass != null ? exClass.getName() : ANY;
+		}
+		public boolean isAny() {
+			return getType().equals(ANY);
+		}
+		public boolean catches(Throwable t) {
+			if (t == null) return isAny();
+			return isAny() || t.getClass().isAssignableFrom(this.exClass);
+		}
+	} // class CatchBlock
+
+} // class TryTask
