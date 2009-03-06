6b39fd0f4921c9756f4084ebb5d4d225c7939299
 Unknown Perforce User <greg>
2009-03-06 18:22:21 +0000

    bug 35700 another forgotten file
    http://bugzilla.zimbra.com/show_bug.cgi?id=35700

---
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/dl/view/ZaSharesListView.js
@@ -0,0 +1,98 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2006, 2007 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+ 
+ /**
+ * @author Greg Solovyev
+ **/
+ZaSharesListView = function(parent, className, posStyle, headerList) {
+	//var headerList = this._getHeaderList();
+	ZaListView.call(this, parent, className, posStyle, headerList);
+}
+
+ZaSharesListView.prototype = new ZaListView;
+ZaSharesListView.prototype.constructor = ZaSharesListView;
+
+ZaSharesListView.prototype.toString = function() {
+	return "ZaSharesListView";
+};
+
+ZaSharesListView.prototype._createItemHtml =
+function(item) {
+	var html = new Array(50);
+	var	div = document.createElement("div");
+	div[DwtListView._STYLE_CLASS] = "Row";
+	div[DwtListView._SELECTED_STYLE_CLASS] = div[DwtListView._STYLE_CLASS] + "-" + DwtCssStyle.SELECTED;
+	div.className = div[DwtListView._STYLE_CLASS];
+	this.associateItemWithElement(item, div, DwtListView.TYPE_LIST_ITEM);
+	
+	var idx = 0;
+	html[idx++] = "<table width='100%' cellspacing='2' cellpadding='0'>";
+
+	html[idx++] = "<tr>";
+	if(this._headerList) {
+		var cnt = this._headerList.length;
+		for(var i = 0; i < cnt; i++) {
+			var field = this._headerList[i]._field;
+			if(field == ZaShare.A_folderPath) {
+
+				html[idx++] = "<td width='" + this._headerList[i]._width + "' align=left>";
+				html[idx++] = AjxStringUtil.htmlEncode(item[ZaShare.A_folderPath]);
+				html[idx++] = "</td>";
+			} else if(field == ZaShare.A_ownerName) {
+				html[idx++] = "<td width='" + this._headerList[i]._width + "' align=left>";
+				html[idx++] = item[ZaShare.A_ownerName];
+				html[idx++] = "</td>";
+			} else if(field == ZaShare.A_granteeName) {
+				html[idx++] = "<td width='" + this._headerList[i]._width + "' align=left>";
+				html[idx++] = item[ZaShare.A_granteeName];
+				html[idx++] = "</td>";
+			} 			
+		}
+	} else {
+		html[idx++] = "<td width=100%>";
+		html[idx++] = AjxStringUtil.htmlEncode(item);
+		html[idx++] = "</td>";
+	}
+	
+	html[idx++] = "</tr></table>";
+	div.innerHTML = html.join("");
+	return div;
+}
+
+
+ZaSharesListView.prototype._setNoResultsHtml = function() {
+	var buffer = new AjxBuffer();
+	var	div = document.createElement("div");
+	
+	buffer.append("<table width='100%' cellspacing='0' cellpadding='1'>",
+				  "<tr><td class='NoResults'><br>",ZaMsg.Shares_DLNoResults,
+				  "</td></tr></table>");
+	
+	div.innerHTML = buffer.toString();
+	this._addRow(div);
+};
+
+ZaSharesListView.prototype._sortColumn = function (columnItem, bSortAsc){
+	if (bSortAsc) {
+		var comparator = function (a, b) {
+			return (a < b)? 1 :((a > b)? -1 : 0);
+		};
+		this.getList().sort(comparator);
+	} else {
+		this.getList().sort();
+	}
+};
