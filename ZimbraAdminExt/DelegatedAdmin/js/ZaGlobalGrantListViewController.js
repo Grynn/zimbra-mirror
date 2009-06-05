0c5120fce582d16bb94a9838c6a8c2661049ca83
 Unknown Perforce User <ccao>
2009-06-05 23:33:53 +0000

    bug: 37868 - add paging to the global ACL list view
    http://bugzilla.zimbra.com/show_bug.cgi?id=37868

--- ZimbraAdminExt/DelegatedAdmin/js/ZaGlobalGrantListViewController.js
+++ ZimbraAdminExt/DelegatedAdmin/js/ZaGlobalGrantListViewController.js
@@ -19,7 +19,7 @@ ZaGlobalGrantListViewController = function(appCtxt, container) {
 	ZaListViewController.call(this, appCtxt, container,"ZaGlobalGrantListViewController");
    	this._toolbarOperations = new Array();
    	this._popupOperations = new Array();
-
+    
 	this._helpURL = location.pathname + ZaUtil.HELP_URL + "da_process/da_global_acl.htm?locid="+AjxEnv.DEFAULT_LOCALE;
     this.addCreationListener(new AjxListener(this, this.handleGrantCreation));
     this.addRemovalListener(new AjxListener(this, this.handleGrantRemoval));			
@@ -40,32 +40,109 @@ function(list, openInNewTab) {
     if (!this._UICreated) {
 		this._createUI();
 	}
-	if (list != null)
-        this._contentView.set(list.getVector());
-//        this._contentView.set(list);
 
-	ZaApp.getInstance().pushView(this.getContentViewId());
-	this._removeList = new Array();
-	if (list != null)
+    ZaApp.getInstance().pushView(this.getContentViewId());
+	if (list != null) {
 		this._list = list;
-
-	this.changeActionsState();
+        this._updateUI () ;
+    }
 }
+
+/*
+1) Display the new list with either added or removed. Make sure that sorting or paging are correct
+2) update the paging information.
+
+ */
+ZaListViewController.prototype._updateUI =
+function() {
+    if (!this._list) return ;
+
+//    this.RESULTSPERPAGE = 2 ;
+    this.numPages = this._list.getVector().size() / this.RESULTSPERPAGE ;
+    var s_result_start_n = (this._currentPageNum - 1) * this.RESULTSPERPAGE + 1;
+	var s_result_end_n = this._currentPageNum  * this.RESULTSPERPAGE;
+    if(this.numPages <= this._currentPageNum) {
+		s_result_end_n = this._list.getVector().size () ;
+		this._toolbar.enable([ZaOperation.PAGE_FORWARD], false);
+	} else {
+		this._toolbar.enable([ZaOperation.PAGE_FORWARD], true);
+	}
+	if(this._currentPageNum == 1) {
+		this._toolbar.enable([ZaOperation.PAGE_BACK], false);
+	} else {
+		this._toolbar.enable([ZaOperation.PAGE_BACK], true);
+	}
+
+    var tmpArr = new Array();
+
+    for(var ix = s_result_start_n - 1; ix < s_result_end_n; ix++) {
+        tmpArr.push(this._list.getVector().getArray()[ix]);
+    }
+    //add the default column sortable
+    this._contentView._bSortAsc = (this._currentSortOrder=="1");
+    this._contentView.set(AjxVector.fromArray(tmpArr), this._contentView._defaultColumnSortable);
+
+	//update the search result number count now
+	var srCountBt = this._toolbar.getButton (ZaOperation.SEARCH_RESULT_COUNT) ;
+	if (srCountBt ) {
+        var total ;
+		if  (this._list == null || this._list.getVector().size () <= 0) {
+			s_result_end_n = 0;
+			s_result_start_n = 0;
+            total = 0 ;
+		} else{
+            total = this._list.getVector().size () ;
+        }
+		srCountBt.setText ( AjxMessageFormat.format (ZaMsg.searchResultCount,
+				[s_result_start_n + " - " + s_result_end_n, total]));
+	}
+
+    this._removeList = new Array();
+    this.changeActionsState();
+
+}
+
+ZaListViewController.prototype._nextPageListener =
+function (ev) {
+	if(this._currentPageNum < this.numPages) {
+		this._currentPageNum++;
+	}
+    this._updateUI () ;
+}
+
+ZaListViewController.prototype._prevPageListener =
+function (ev) {
+	if(this._currentPageNum > 1) {
+		this._currentPageNum--;
+	}
+    this._updateUI () ;
+}
+
 
 ZaGlobalGrantListViewController.initToolbarMethod =
 function () {
-
-    this._toolbarOperations[ZaOperation.NEW] = new ZaOperation(ZaOperation.NEW, com_zimbra_delegatedadmin.Bt_grant, com_zimbra_delegatedadmin.Grant_New_tt, "GlobalPermission", "GlobalPermissionDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._newButtonListener));
-//    this._toolbarOperations[ZaOperation.EDIT] = new ZaOperation(ZaOperation.EDIT, ZaMsg.TBB_Edit,com_zimbra_delegatedadmin.RIGHT_Edit_tt, "Properties", "PropertiesDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._editButtonListener));
-   	this._toolbarOperations[ZaOperation.DELETE] = new ZaOperation(ZaOperation.DELETE, com_zimbra_delegatedadmin.Bt_revoke, com_zimbra_delegatedadmin.Grant_Delete_tt, "Delete", "DeleteDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._deleteButtonListener));
-	this._toolbarOperations[ZaOperation.NONE] = new ZaOperation(ZaOperation.NONE);
-	this._toolbarOperations[ZaOperation.HELP] = new ZaOperation(ZaOperation.HELP, ZaMsg.TBB_Help, ZaMsg.TBB_Help_tt, "Help", "Help", new AjxListener(this, this._helpButtonListener));
 
     this._toolbarOrder.push(ZaOperation.NEW);
 //    this._toolbarOrder.push(ZaOperation.EDIT);
     this._toolbarOrder.push(ZaOperation.DELETE);
     this._toolbarOrder.push(ZaOperation.NONE);
-	this._toolbarOrder.push(ZaOperation.HELP);
+
+    this._toolbarOrder.push(ZaOperation.PAGE_BACK) ;
+    this._toolbarOrder.push(ZaOperation.PAGE_FORWARD) ;
+    this._toolbarOrder.push(ZaOperation.HELP);
+
+    this._toolbarOperations[ZaOperation.NEW] = new ZaOperation(ZaOperation.NEW, com_zimbra_delegatedadmin.Bt_grant, com_zimbra_delegatedadmin.Grant_New_tt, "GlobalPermission", "GlobalPermissionDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._newButtonListener));
+//    this._toolbarOperations[ZaOperation.EDIT] = new ZaOperation(ZaOperation.EDIT, ZaMsg.TBB_Edit,com_zimbra_delegatedadmin.RIGHT_Edit_tt, "Properties", "PropertiesDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._editButtonListener));
+   	this._toolbarOperations[ZaOperation.DELETE] = new ZaOperation(ZaOperation.DELETE, com_zimbra_delegatedadmin.Bt_revoke, com_zimbra_delegatedadmin.Grant_Delete_tt, "Delete", "DeleteDis", new AjxListener(this, ZaGlobalGrantListViewController.prototype._deleteButtonListener));
+	this._toolbarOperations[ZaOperation.NONE] = new ZaOperation(ZaOperation.NONE);
+    this._toolbarOperations[ZaOperation.PAGE_BACK] = new ZaOperation(ZaOperation.PAGE_BACK, ZaMsg.Previous, ZaMsg.PrevPage_tt, "LeftArrow", "LeftArrowDis",  new AjxListener(this, this._prevPageListener));
+
+    //add the acount number counts
+    ZaSearch.searchResultCountsView(this._toolbarOperations, this._toolbarOrder);
+    this._toolbarOperations[ZaOperation.PAGE_FORWARD] = new ZaOperation(ZaOperation.PAGE_FORWARD, ZaMsg.Next, ZaMsg.NextPage_tt, "RightArrow", "RightArrowDis", new AjxListener(this, this._nextPageListener));
+    this._toolbarOperations[ZaOperation.HELP] = new ZaOperation(ZaOperation.HELP, ZaMsg.TBB_Help, ZaMsg.TBB_Help_tt, "Help", "Help", new AjxListener(this, this._helpButtonListener));
+
+
 }
 ZaController.initToolbarMethods["ZaGlobalGrantListViewController"].push(ZaGlobalGrantListViewController.initToolbarMethod);
 
@@ -96,7 +173,7 @@ ZaGlobalGrantListViewController.prototype._createUI = function () {
 		}
 		ZaApp.getInstance().createView(this.getContentViewId(), elements, tabParams) ;
 //      Disable double click for the global grant list
-//		this._contentView.addSelectionListener(new AjxListener(this, this._listSelectionListener));
+		this._contentView.addSelectionListener(new AjxListener(this, this._listSelectionListener));
 		this._contentView.addActionListener(new AjxListener(this, this._listActionListener));
 		this._removeConfirmMessageDialog = new ZaMsgDialog(ZaApp.getInstance().getAppCtxt().getShell(), null, [DwtDialog.YES_BUTTON, DwtDialog.NO_BUTTON]);
 
@@ -107,18 +184,6 @@ ZaGlobalGrantListViewController.prototype._createUI = function () {
 		return;
 	}
 }
-
-
-/*
-ZaGlobalGrantListViewController.prototype.refresh =
-function() {
-	try {
-		this._contentView.set(ZaApp.getInstance().getGrantList(true).getVector());
-	} catch (ex) {
-		this._handleException(ex, ZaGlobalGrantListViewController.prototype.refresh, null, false);
-	}
-}
-*/
 
 ZaGlobalGrantListViewController.prototype.set =
 function(grantsList) {
@@ -137,11 +202,9 @@ function (ev) {
 		//if(details["modFields"] && (details["modFields"][ZaGrant.A_description] )) {
 		if (details) {
 			if (this._list) this._list.replace (details);
-			if (this._contentView) this._contentView.setUI();
 			if(ZaApp.getInstance().getCurrentController() == this) {
-				this.show();
+				this.show(this._list);
 			}
-			this.changeActionsState();
 		}
 	}
 }
@@ -156,9 +219,10 @@ function (ev) {
 		//add the new ZaGrant to the controlled list
 		if(ev.getDetails()) {
 			if (this._list) this._list.add(ev.getDetails());
-			if (this._contentView) this._contentView.setUI();
+            this._currentPageNum = 1;
+//			if (this._contentView) this._contentView.setUI();
 			if(ZaApp.getInstance().getCurrentController() == this) {
-				this.show();
+				this.show(this._list);
 			}
 		}
 	}
@@ -174,15 +238,14 @@ function (ev) {
 		//add the new ZaAccount to the controlled list
 		if(ev.getDetails()) {
 			if (this._list) this._list.remove(ev.getDetails());
-			if (this._contentView ) this._contentView.setUI();
+            this._currentPageNum = 1;
+//			if (this._contentView ) this._contentView.setUI();
 			if(ZaApp.getInstance().getCurrentController() == this) {
-				this.show();
+			    this.show(this._list);
 			}
 		}
 	}
 }
-
-
 
 // new button was pressed
 ZaGlobalGrantListViewController.prototype._newButtonListener =
@@ -236,10 +299,11 @@ function(ev) {
 ZaGlobalGrantListViewController.prototype._listSelectionListener =
 function(ev) {
 	if (ev.detail == DwtListView.ITEM_DBL_CLICKED) {
-		if(ev.item) {
+		/*
+        if(ev.item) {
 			this._selectedItem = ev.item;
 			ZaApp.getInstance().getGrantViewController().show(ev.item);
-		}
+		} */
 	} else {
 		this.changeActionsState();
 	}
@@ -263,23 +327,23 @@ function(ev) {
 	}
 }
 
-ZaGlobalGrantListViewController.changeActionsState =
+ZaGlobalGrantListViewController.changeActionsStateMethod =
 function () {
 	if(this._contentView) {
 		var cnt = this._contentView.getSelectionCount();
-		 if (cnt > 1){
-			if(this._toolbarOperations[ZaOperation.EDIT])
-				this._toolbarOperations[ZaOperation.EDIT].enabled = false;
+		 if (cnt == 1){
+            if(this._toolbarOperations[ZaOperation.DELETE])
+				this._toolbarOperations[ZaOperation.DELETE].enabled = true;
 
-			if(this._popupOperations[ZaOperation.EDIT])
-				this._popupOperations[ZaOperation.EDIT].enabled = false;
+			if(this._popupOperations[ZaOperation.DELETE])
+				this._popupOperations[ZaOperation.DELETE].enabled = true;
 
-		} else if (cnt <1) {
-			if(this._toolbarOperations[ZaOperation.EDIT])
-				this._toolbarOperations[ZaOperation.EDIT].enabled = false;
+		} else {
+			if(this._toolbarOperations[ZaOperation.DELETE])
+				this._toolbarOperations[ZaOperation.DELETE].enabled = false;
 
-			if(this._popupOperations[ZaOperation.EDIT])
-				this._popupOperations[ZaOperation.EDIT].enabled = false;
+			if(this._popupOperations[ZaOperation.DELETE])
+				this._popupOperations[ZaOperation.DELETE].enabled = false;
 
 		}
 	}
