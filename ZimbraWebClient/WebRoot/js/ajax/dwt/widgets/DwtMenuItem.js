35925a2d8ad4a367ad126bb86816f5a092bd3429
 Unknown Perforce User <pshah>
2009-07-14 23:57:07 +0000

    bug: 29918 - checkpoint -- - Disallow New Folder option from account header action menu - Fix search bar dropdown missing icon when clicked - Fix zimlet tree not showing up - this is still a WIP - Fix spacing issue for Tag and Search tree header items - more code cleanup
    http://bugzilla.zimbra.com/show_bug.cgi?id=29918

--- ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtMenuItem.js
@@ -32,41 +32,41 @@
  *        id			[string]*			an explicit ID to use for the control's HTML element
  */
 DwtMenuItem = function(params) {
-    if (arguments.length == 0) { return; }
+	if (arguments.length == 0) { return; }
 	params = Dwt.getParams(arguments, DwtMenuItem.PARAMS);
 
-    // check parameters
-    var parent = params.parent;
-    if (!(parent instanceof DwtMenu)) {
+	// check parameters
+	var parent = params.parent;
+	if (!(parent instanceof DwtMenu)) {
 		throw new DwtException("Parent must be a DwtMenu object", DwtException.INVALIDPARENT, "DwtMenuItem");
-    }
-		
+	}
+
 	var style = params.style = params.style || DwtMenuItem.NO_STYLE;
 	if (parent._style == DwtMenu.BAR_STYLE && style != DwtMenuItem.PUSH_STYLE) {
 		throw new DwtException("DwtMenuItemInit: invalid style", DwtException.INVALID_PARAM, "DwtMenuItem");
 	}
 
-    // call super constructor
-    style &= ~DwtLabel.IMAGE_RIGHT; // remove image right style
-    style |= DwtButton.ALWAYS_FLAT | DwtLabel.IMAGE_LEFT; // set default styles
-    params.className = (style & DwtMenuItem.SEPARATOR_STYLE) ?
-    					"ZMenuItemSeparator" : (params.className || "ZMenuItem");
-    params.listeners = DwtMenuItem._listeners;
-    DwtButton.call(this, params);
+	// call super constructor
+	style &= ~DwtLabel.IMAGE_RIGHT; // remove image right style
+	style |= DwtButton.ALWAYS_FLAT | DwtLabel.IMAGE_LEFT; // set default styles
+	params.className = (style & DwtMenuItem.SEPARATOR_STYLE)
+		? "ZMenuItemSeparator" : (params.className || "ZMenuItem");
+	params.listeners = DwtMenuItem._listeners;
+	DwtButton.call(this, params);
 
-    this.setDropDownImages("Cascade", "Cascade", "Cascade", "Cascade");
-    this._radioGroupId = params.radioGroupId;
+	this.setDropDownImages("Cascade", "Cascade", "Cascade", "Cascade");
+	this._radioGroupId = params.radioGroupId;
 
-    // add this item at the specified index
-    if (parent._addItem) {
+	// add this item at the specified index
+	if (parent._addItem) {
 		parent._addItem(this, params.index);
-    }
+	}
 
-    // add listeners if not menu item separator
+	// add listeners if not menu item separator
 	if (!(style & DwtMenuItem.SEPARATOR_STYLE)) {
 		this.addSelectionListener(new AjxListener(this, this.__handleItemSelect));
 	}
-}
+};
 
 DwtMenuItem.PARAMS = ["parent", "style", "radioGroupId", "index", "className", "posStyle"];
 
@@ -76,43 +76,35 @@ DwtMenuItem.prototype.constructor = DwtMenuItem;
 DwtMenuItem.prototype.toString = 
 function() {
 	return "DwtMenuItem";
-}
+};
 
 //
 // Constants
 //
 
-DwtMenuItem.CHECKED = 1;
-DwtMenuItem.UNCHECKED = 2;
-
-DwtMenuItem.NO_STYLE = 0;
-DwtMenuItem.CHECK_STYLE = DwtButton._LAST_STYLE * 2;
-DwtMenuItem.RADIO_STYLE = DwtButton._LAST_STYLE * 4;
+DwtMenuItem.CHECKED			= 1;
+DwtMenuItem.UNCHECKED		= 2;
+DwtMenuItem.NO_STYLE		= 0;
+DwtMenuItem.CHECK_STYLE		= DwtButton._LAST_STYLE * 2;
+DwtMenuItem.RADIO_STYLE		= DwtButton._LAST_STYLE * 4;
 DwtMenuItem.SEPARATOR_STYLE = DwtButton._LAST_STYLE * 8;
-DwtMenuItem.CASCADE_STYLE = DwtButton._LAST_STYLE * 16;
-DwtMenuItem.PUSH_STYLE = DwtButton._LAST_STYLE * 32;
-DwtMenuItem.SELECT_STYLE = DwtButton._LAST_STYLE * 64;
+DwtMenuItem.CASCADE_STYLE	= DwtButton._LAST_STYLE * 16;
+DwtMenuItem.PUSH_STYLE		= DwtButton._LAST_STYLE * 32;
+DwtMenuItem.SELECT_STYLE	= DwtButton._LAST_STYLE * 64;
+DwtMenuItem._LAST_STYLE		= DwtMenuItem.SELECT_STYLE;
 
-DwtMenuItem._LAST_STYLE = DwtMenuItem.SELECT_STYLE; 
-
-DwtMenuItem._MENU_POPUP_DELAY = 250;
-DwtMenuItem._MENU_POPDOWN_DELAY = 250
-
-/***
-DwtMenuItem._evt = new DwtUiEvent(true);
-/***/
+DwtMenuItem._MENU_POPUP_DELAY	= 250;
+DwtMenuItem._MENU_POPDOWN_DELAY	= 250;
 
 //
 // Data
 //
 
-DwtMenuItem.prototype.TEMPLATE = "dwt.Widgets#ZMenuItem";
-
-DwtMenuItem.prototype.SEPARATOR_TEMPLATE = "dwt.Widgets#ZMenuItemSeparator";
-
-DwtMenuItem.prototype.BLANK_CHECK_TEMPLATE = "dwt.Widgets#ZMenuItemBlankCheck";
-DwtMenuItem.prototype.BLANK_ICON_TEMPLATE = "dwt.Widgets#ZMenuItemBlankIcon";
-DwtMenuItem.prototype.BLANK_CASCADE_TEMPLATE = "dwt.Widgets#ZMenuItemBlankCascade";
+DwtMenuItem.prototype.TEMPLATE					= "dwt.Widgets#ZMenuItem";
+DwtMenuItem.prototype.SEPARATOR_TEMPLATE		= "dwt.Widgets#ZMenuItemSeparator";
+DwtMenuItem.prototype.BLANK_CHECK_TEMPLATE		= "dwt.Widgets#ZMenuItemBlankCheck";
+DwtMenuItem.prototype.BLANK_ICON_TEMPLATE		= "dwt.Widgets#ZMenuItemBlankIcon";
+DwtMenuItem.prototype.BLANK_CASCADE_TEMPLATE	= "dwt.Widgets#ZMenuItemBlankCascade";
 
 //
 // Public methods
@@ -134,23 +126,24 @@ function(params) {
 	}
 	mi.setEnabled(params.enabled !== false);
 	return mi;
-}
+};
 
 DwtMenuItem.prototype.getChecked =
 function() {
 	return this._itemChecked;
-}
+};
 
 DwtMenuItem.prototype.setChecked =
 function(checked, skipNotify) {
 	this._setChecked(checked, null, skipNotify);
 	this.parent._checkItemAdded(this);
-}
+};
 
-DwtMenuItem.prototype.setImage = function(imageInfo) {
+DwtMenuItem.prototype.setImage =
+function(imageInfo) {
 	DwtButton.prototype.setImage.call(this, imageInfo);
 	this.parent._iconItemAdded(this);
-}
+};
 
 DwtMenuItem.prototype.setText =
 function(text) {
@@ -161,10 +154,11 @@ function(text) {
 	}
 };
 
-DwtMenuItem.prototype.setMenu = function(menuOrCallback, shouldToggle, followIconStyle) {
+DwtMenuItem.prototype.setMenu =
+function(menuOrCallback, shouldToggle, followIconStyle) {
 	DwtButton.prototype.setMenu.call(this, menuOrCallback, shouldToggle, followIconStyle);
 	this.parent._submenuItemAdded(this);
-}
+};
 
 DwtMenuItem.prototype.setHoverDelay =
 function(delay) {
@@ -182,76 +176,57 @@ function(shortcut) {
 // Protected methods
 //
 
-DwtMenuItem.prototype._createHtml = function(templateId) {
-    var defaultTemplate = this._style & DwtMenuItem.SEPARATOR_STYLE ? this.SEPARATOR_TEMPLATE : this.TEMPLATE;
-    DwtButton.prototype._createHtml.call(this, templateId || defaultTemplate);
+DwtMenuItem.prototype._createHtml =
+function(templateId) {
+	var defaultTemplate = (this._style & DwtMenuItem.SEPARATOR_STYLE)
+		? this.SEPARATOR_TEMPLATE : this.TEMPLATE;
+	DwtButton.prototype._createHtml.call(this, templateId || defaultTemplate);
 };
 
-DwtMenuItem.prototype._createHtmlFromTemplate = function(templateId, data) {
-    DwtButton.prototype._createHtmlFromTemplate.call(this, templateId, data);
-    this._checkEl = document.getElementById(data.id+"_check");
+DwtMenuItem.prototype._createHtmlFromTemplate =
+function(templateId, data) {
+	DwtButton.prototype._createHtmlFromTemplate.call(this, templateId, data);
+	this._checkEl = document.getElementById(data.id+"_check");
 };
 
 DwtMenuItem.prototype._setChecked =
 function(checked, ev, skipNotify) {
-    var isCheck = this._style & DwtMenuItem.CHECK_STYLE;
-    var isRadio = this._style & DwtMenuItem.RADIO_STYLE;
-    if ((isCheck || isRadio) && this._itemChecked != checked) {
+	var isCheck = this._style & DwtMenuItem.CHECK_STYLE;
+	var isRadio = this._style & DwtMenuItem.RADIO_STYLE;
+	if ((isCheck || isRadio) && this._itemChecked != checked) {
 		this._itemChecked = checked;
 
-        if (this._checkEl) {
-            this._checkEl.innerHTML = "";
-            var icon = checked ? (isCheck ? "MenuCheck" : "MenuRadio") : "Blank_9";
-            AjxImg.setImage(this._checkEl, icon);
-            if (checked) {
-                // deselect currently selected radio button
-                if (isRadio) {
-                    this.parent._radioItemSelected(this, skipNotify);
-                }
-
-				/***
-				// notify listeners
-				if (this.isListenerRegistered(DwtEvent.ONCHANGE)) {
-					var event = DwtMenuItem._evt;
-					event._args = {
-						selectObj: this,
-						newValue: checked,
-						oldValue: !checked
-					};
-					this.notifyListeners(DwtEvent.ONCHANGE, event);
+		if (this._checkEl) {
+			this._checkEl.innerHTML = "";
+			var icon = checked ? (isCheck ? "MenuCheck" : "MenuRadio") : "Blank_9";
+			AjxImg.setImage(this._checkEl, icon);
+			if (checked) {
+				// deselect currently selected radio button
+				if (isRadio) {
+					this.parent._radioItemSelected(this, skipNotify);
 				}
-				/***/
-
-                // follow icon
-                var gp = this.parent.parent ? this.parent.parent : null;
-                if (gp && (gp instanceof DwtButton) && (gp._followIconStyle == this._style)) {
-                    gp.setImage(this._imageInfo);
-                }
-            }
-        }
+			}
+		}
 	}
-}
-
-DwtMenuItem.prototype._addIconCell = function() {
-//    this.setImage(this.getImage());
 };
 
 DwtMenuItem.prototype._checkItemAdded = function(className) {};
-DwtMenuItem.prototype._checkedItemsRemoved = function() {}
+DwtMenuItem.prototype._checkedItemsRemoved = function() {};
 
 DwtMenuItem.prototype._submenuItemAdded =
 function() {
-	if (this._style & DwtMenuItem.SEPARATOR_STYLE) return;
+	if (this._style & DwtMenuItem.SEPARATOR_STYLE) { return; }
 
-    if (this._cascCell == null) {
-        this._cascCell = this._row.insertCell(-1);
-        this._cascCell.noWrap = true;
-        this._cascCell.style.width = DwtMenuItem._CASCADE_DIM;
-        this._cascCell.style.height = (this._style != DwtMenuItem.SEPARATOR_STYLE) ?  DwtMenuItem._CASCADE_DIM : DwtMenuItem._SEPAARATOR_DIM;
-    }
+	if (this._cascCell == null) {
+		this._cascCell = this._row.insertCell(-1);
+		this._cascCell.noWrap = true;
+		this._cascCell.style.width = DwtMenuItem._CASCADE_DIM;
+		this._cascCell.style.height = (this._style != DwtMenuItem.SEPARATOR_STYLE) ?  DwtMenuItem._CASCADE_DIM : DwtMenuItem._SEPAARATOR_DIM;
+	}
 };
 
-DwtMenuItem.prototype._submenuItemRemoved = function() {
+DwtMenuItem.prototype._submenuItemRemoved =
+function() {
 	if (this._dropDownEl) {
 		this._dropDownEl.innerHTML = "";
 	}
@@ -276,33 +251,31 @@ function(delay, kbGenerated) {
 		hBorder += (ppHtmlElement.style.borderBottomWidth == "") ? 0 : parseInt(ppHtmlElement.style.borderBottonWidth);
 		y = pb.y + pb.height + hBorder;		
 		x = ((x + s.x) >= ws.x) ? x - (x + s.x - ws.x): x;
-		//y = ((y + s.y) >= ws.y) ? y - (y + s.y - ws.y) : y;
-	} else { // Drop Down
+	}
+	else { // Drop Down
 		vBorder = (ppHtmlElement.style.borderLeftWidth == "") ? 0 : parseInt(ppHtmlElement.style.borderLeftWidth);
 		vBorder += (ppHtmlElement.style.borderRightWidth == "") ? 0 : parseInt(ppHtmlElement.style.borderRightWidth);
 		x = pb.x + pb.width + vBorder;
 		hBorder = (ppHtmlElement.style.borderTopWidth == "") ? 0 : parseInt(ppHtmlElement.style.borderTopWidth);
 		y = pb.y + hBorder;
 		x = ((x + s.x) >= ws.x) ? pb.x - s.x - vBorder: x;
-		//y = ((y + s.y) >= ws.y) ? y - (y + s.y - ws.y) : y;
 	}
-	//this.setLocation(x, y);
-    menu.popup(delay, x, y, kbGenerated);
+	menu.popup(delay, x, y, kbGenerated);
 };
 
 DwtMenuItem.prototype._popdownMenu =
 function() {
-    var menu = this.getMenu();
-    if (menu) {
-        menu.popdown();
-    }
+	var menu = this.getMenu();
+	if (menu) {
+		menu.popdown();
+	}
 };
 
 DwtMenuItem.prototype._isMenuPoppedUp =
 function() {
 	var menu = this.getMenu();
-	return (menu && menu.isPoppedUp()) ? true : false;
-}
+	return (menu && menu.isPoppedUp());
+};
 
 
 //
@@ -312,31 +285,31 @@ function() {
 DwtMenuItem.prototype.__handleItemSelect =
 function(event) {
 	this.setDisplayState(DwtControl.NORMAL);
-    if (this.isStyle(DwtMenuItem.CHECK_STYLE)) {
-        this._setChecked(!this._itemChecked, null, true);
-        event.detail = this.getChecked() ? DwtMenuItem.CHECKED : DwtMenuItem.UNCHECKED;
-    }
-    else if (this.isStyle(DwtMenuItem.RADIO_STYLE)) {
-        this._setChecked(true, true);
-        this.parent._radioItemSelected(this, true);
-        event.detail = this.getChecked() ? DwtMenuItem.CHECKED : DwtMenuItem.UNCHECKED;
-    }
-    else if (this.isStyle(DwtMenuItem.PUSH_STYLE)) {
-        if (this._menu) {
-            if (this._isMenuPoppedUp()) {
-                DwtMenu.closeActiveMenu();
-            }
-            else {
-                this._popupMenu();
-            }
-        }
-        return;
-    }
-    if (!this.isStyle(DwtMenuItem.CASCADE_STYLE)) {
+	if (this.isStyle(DwtMenuItem.CHECK_STYLE)) {
+		this._setChecked(!this._itemChecked, null, true);
+		event.detail = this.getChecked() ? DwtMenuItem.CHECKED : DwtMenuItem.UNCHECKED;
+	}
+	else if (this.isStyle(DwtMenuItem.RADIO_STYLE)) {
+		this._setChecked(true, true);
+		this.parent._radioItemSelected(this, true);
+		event.detail = this.getChecked() ? DwtMenuItem.CHECKED : DwtMenuItem.UNCHECKED;
+	}
+	else if (this.isStyle(DwtMenuItem.PUSH_STYLE)) {
+		if (this._menu) {
+			if (this._isMenuPoppedUp()) {
+				DwtMenu.closeActiveMenu();
+			}
+			else {
+				this._popupMenu();
+			}
+		}
+		return;
+	}
+	if (!this.isStyle(DwtMenuItem.CASCADE_STYLE)) {
 		if (!this._menu || !this._menu.isPoppedUp || !this._menu.isPoppedUp()) {
 			DwtMenu.closeActiveMenu();
 		}
-    }
+	}
 };
 
 DwtMenuItem._mouseOverListener =
@@ -350,12 +323,12 @@ function(ev) {
 		DwtButton._mouseOutListener(mouseEv);
 	}
 	if (menuItem._style & DwtMenuItem.SEPARATOR_STYLE) { return false; }
-    DwtButton._mouseOverListener(ev, menuItem);
+	DwtButton._mouseOverListener(ev, menuItem);
 	menu._hoveredItem = menuItem;
-    menu._popdownSubmenus();
-    if (menuItem._menu && !ev.ersatz) {
-        menuItem._popupMenu(menuItem._hoverDelay);
-    }
+	menu._popdownSubmenus();
+	if (menuItem._menu && !ev.ersatz) {
+		menuItem._popupMenu(menuItem._hoverDelay);
+	}
 };
 
 DwtMenuItem._mouseOutListener =
@@ -376,4 +349,3 @@ DwtMenuItem._listeners[DwtEvent.ONMOUSEDOWN] = new AjxListener(null, DwtButton._
 DwtMenuItem._listeners[DwtEvent.ONMOUSEUP] = new AjxListener(null, DwtButton._mouseUpListener);
 DwtMenuItem._listeners[DwtEvent.ONMOUSEENTER] = new AjxListener(null, DwtMenuItem._mouseOverListener);
 DwtMenuItem._listeners[DwtEvent.ONMOUSELEAVE] = new AjxListener(null, DwtButton._mouseOutListener);
-
