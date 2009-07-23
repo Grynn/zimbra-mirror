8a6dfc6c5d5b5c94894338fa1a171a89183ef523
 Unknown Perforce User <tfr>
2009-07-23 23:57:52 +0000

    bug: 39280 enable compression on msg volume
    http://bugzilla.zimbra.com/show_bug.cgi?id=39280

--- ZimbraOffline/src/java/com/zimbra/cs/offline/OfflineApplication.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/OfflineApplication.java
@@ -16,33 +16,51 @@
  */
 package com.zimbra.cs.offline;
 
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.db.DbPool;
+import com.zimbra.cs.store.file.Volume;
 import com.zimbra.cs.util.ZimbraApplication;
 
 public class OfflineApplication extends ZimbraApplication {
 
-	@Override
-	public String getId() {
-		return OfflineLC.zdesktop_app_id.value();
-	}
-	
-	@Override
-	public String getInstallId() {
-	    return OfflineLC.zdesktop_installation_key.value();
-	}
-	
-	@Override
-	public boolean supports(String className) {
-		return false;
-	}
-	
-	@Override
-	public void shutdown() {
-		super.shutdown();
-		try {
-			DbPool.shutdown();
-		} catch (Exception x) {
-			OfflineLog.offline.warn("Exception during shutdown", x);
-		}
-	}
+    @Override
+    public String getId() {
+        return OfflineLC.zdesktop_app_id.value();
+    }
+
+    @Override
+    public String getInstallId() {
+        return OfflineLC.zdesktop_installation_key.value();
+    }
+
+    @Override
+    public boolean supports(String className) {
+        return false;
+    }
+
+    @Override
+    public void initialize(boolean forMailboxd) {
+        long threshold = OfflineLC.zdesktop_volume_compression_threshold.longValue();
+        Volume vol = Volume.getCurrentMessageVolume();
+        
+        try {
+            Volume.update(vol.getId(), vol.getType(), vol.getName(),
+                vol.getRootPath(), vol.getMboxGroupBits(), vol.getMboxBits(),
+                vol.getFileGroupBits(), vol.getFileBits(), threshold != 0,
+                threshold == 0 ? vol.getCompressionThreshold() : threshold, true);
+            Volume.reloadVolumes();
+        } catch (ServiceException e) {
+            OfflineLog.offline.warn("Unable to update volume compression", e);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        try {
+            DbPool.shutdown();
+        } catch (Exception x) {
+            OfflineLog.offline.warn("Exception during shutdown", x);
+        }
+    }
 }
