1bc18fe350f1c6bc65787ac5bd70484d36c80ebd
Quanah Gibson-Mount <quanah@zimbra.com>
2009-08-11 06:23:32 +0000

    bug: 35751 Break out system modules and zimbra modules into different arrays.  Validate that the zimbra modules are loaded from the zimbramon directory and not a system directory.
    http://bugzilla.zimbra.com/show_bug.cgi?id=35751

--- ThirdParty/Perl/zmcheckbuiltmodules.pl
+++ ThirdParty/Perl/zmcheckbuiltmodules.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 # 
 # ***** BEGIN LICENSE BLOCK *****
 # 
@@ -19,18 +19,30 @@
 use strict;
 use lib "./zimbramon/lib";
 
-my @modules =qw(AnyDBM_File Archive::Tar Archive::Zip Array::Compare Benchmark BerkeleyDB Bit::Vector Cache::FastMmap Carp Compress::Raw::Zlib Compress::Zlib Config::IniFiles Convert::ASN1 Convert::TNEF Convert::UUlib Crypt::OpenSSL::Random Crypt::OpenSSL::RSA Crypt::SaltedHash Crypt::SSLeay Cwd DB_File DBD::mysql DBD::SQLite DBI Data::Dumper Data::UUID Date::Calc Date::Format Date::Manip Date::Parse Device::SerialPort Digest::HMAC Digest::HMAC_MD5 Digest::MD5 Digest::SHA Digest::SHA1 Encode English Errno Error Exporter Fcntl File::Basename File::Copy File::Find File::Grep File::Path File::Spec File::Tail File::Temp FileHandle FindBin Getopt::Easy Getopt::Long Getopt::Std HTML::Parser HTML::Tagset HTTP::Parser HTTP::Request IO IO::Compress::Base IO::Compress::Gzip IO::File IO::Handle IO::Select IO::Socket IO::Socket::INET IO::Socket::INET6 IO::Socket::SSL IO::Socket::UNIX IO::Stringy IO::Wrap IO::Zlib IPC::Open3 List::Compare LWP::UserAgent MIME::Base64 MIME::Entity MIME::Lite MIME::Parser MIME::Words Mail::DKIM Mail::Mailer Mail::SpamAssassin Mail::SpamAssassin::ArchiveIterator Mail::SpamAssassin::Message Mail::SpamAssassin::PerMsgLearner Mail::SpamAssassin::Util::Progress Math::BigFloat Net::DNS::Resolver Net::HTTP Net::IP Net::LDAP Net::LDAP::Entry Net::LDAP::LDIF Net::LDAPapi Net::Ping Net::SMTP Net::SSLeay Net::Server Net::Telnet POSIX Package::Constants Parallel::ForkManager Pod::Usage Proc::ProcessTable Scalar::Util SOAP::Lite SOAP::Transport::HTTP Socket Socket6 Sub::Uplevel Swatch::Actions Swatch::Throttle Sys::Hostname Term::ReadLine Test::Exception Test::Warn Time::HiRes Time::Local Tree::DAG_Node Unix::Syslog URI XML::Parser XML::SAX::Base XML::SAX::Expat XML::Simple bytes constant lib re sigtrap strict subs vars warnings);
+my @zmodules =qw(Archive::Tar Archive::Zip Array::Compare BerkeleyDB Bit::Vector Cache::FastMmap Compress::Raw::Zlib Compress::Zlib Config::IniFiles Convert::ASN1 Convert::TNEF Convert::UUlib Crypt::OpenSSL::Random Crypt::OpenSSL::RSA Crypt::SaltedHash Crypt::SSLeay DB_File DBD::mysql DBD::SQLite DBI Data::UUID Date::Calc Date::Format Date::Manip Date::Parse Device::SerialPort Digest::HMAC Digest::HMAC_MD5 Digest::SHA Digest::SHA1 Error File::Grep File::Tail File::Temp Getopt::Easy HTML::Parser HTML::Tagset HTTP::Parser HTTP::Request IO IO::Compress::Base IO::Compress::Gzip IO::File IO::Handle IO::Select IO::Socket IO::Socket::INET IO::Socket::INET6 IO::Socket::SSL IO::Socket::UNIX IO::Stringy IO::Wrap IO::Zlib List::Compare LWP::UserAgent MIME::Entity MIME::Lite MIME::Parser MIME::Words Mail::DKIM Mail::Mailer Mail::SpamAssassin Mail::SpamAssassin::ArchiveIterator Mail::SpamAssassin::Message Mail::SpamAssassin::PerMsgLearner Mail::SpamAssassin::Util::Progress Net::DNS::Resolver Net::HTTP Net::IP Net::LDAP Net::LDAP::Entry Net::LDAP::LDIF Net::LDAPapi Net::SMTP Net::SSLeay Net::Server Net::Telnet Package::Constants Parallel::ForkManager Proc::ProcessTable Scalar::Util SOAP::Lite SOAP::Transport::HTTP Socket6 Sub::Uplevel Swatch::Actions Swatch::Throttle Test::Exception Test::Warn Time::HiRes Tree::DAG_Node Unix::Syslog URI XML::Parser XML::SAX::Base XML::SAX::Expat XML::Simple);
+
+my @smodules =qw(AnyDBM_File Benchmark Carp Cwd Data::Dumper Digest::MD5 Encode English Errno Exporter Fcntl File::Basename File::Copy File::Find File::Path File::Spec FileHandle FindBin Getopt::Long Getopt::Std IPC::Open3 MIME::Base64 Math::BigFloat Net::Ping POSIX Pod::Usage Socket Sys::Hostname Term::ReadLine Time::Local bytes constant lib re sigtrap strict subs vars warnings);
 
 my $exit_status=0;
+my $pm;
 
-foreach my $m (@modules) {
-  load_module($m); 
+foreach my $m (@zmodules) {
+  load_module($m);
+  if ($INC{$pm} !~ /zimbramon\/lib/ && $INC{$pm} ne "") {
+    print "$INC{$pm}\n";
+    $exit_status=1;
+  }
+}
+
+foreach my $m (@smodules) {
+  load_module($m);
 }
 
 exit $exit_status;
 
 sub load_module($) {
   my ($m) = @_;
+  my $mpath;
   local($_) = $m;
   $_ .= /^auto::/ ? '.al' : '.pm'  if !m{^/} && !m{\.(pm|pl|al|ix)\z};
   s{::}{/}g;
@@ -39,5 +51,6 @@ sub load_module($) {
     my($eval_stat) = $@ ne '' ? $@ =~ /(\S+\s\S+\s\S+)/ : "errno=$!";  chomp $eval_stat;
     print "$m: $eval_stat\n";
     $exit_status=1;
-  }
+  };
+  $pm=$_;
 }
