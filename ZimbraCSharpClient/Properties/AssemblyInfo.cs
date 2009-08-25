2506f10d087be31b620387942ebddba40ea31a1c
 Unknown Perforce User <build>
2009-08-25 07:27:03 +0000

    bug: 6038 AUTO UPDATE OF Toaster BUILD NUMBER 6.0.5674.1
    http://bugzilla.zimbra.com/show_bug.cgi?id=6038

--- ZimbraCSharpClient/Properties/AssemblyInfo.cs
+++ ZimbraCSharpClient/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("6.0.5673.1")]
-[assembly: AssemblyFileVersion("6.0.5673.1")]
+[assembly: AssemblyVersion("6.0.5674.1")]
+[assembly: AssemblyFileVersion("6.0.5674.1")]
 
 
