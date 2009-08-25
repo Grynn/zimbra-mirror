d721427f3e1ae469ca5d0cebaea122eef37146ff
 Unknown Perforce User <jylee>
2009-08-25 20:35:52 +0000

    bug 22008 changed WebDAV request dispatch mechanism from HTTP redirect to internal webapp dispatch.
    http://bugzilla.zimbra.com/show_bug.cgi?id=22008

--- ZimbraWebClient/src/com/zimbra/webClient/filters/SetHeaderFilter.java
+++ ZimbraWebClient/src/com/zimbra/webClient/filters/SetHeaderFilter.java
@@ -30,6 +30,7 @@ import javax.naming.NamingException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -264,7 +265,9 @@ public class SetHeaderFilter implements Filter {
         // CalDAV service discovery (bug 35008). 
         // redirect any WebDAV request sent to the root URI "/" to "/dav/"
         if (req.getPathInfo() == null && req.getMethod().equals("PROPFIND")) {
-        	resp.sendRedirect("/dav/");
+            ServletContext targetContext = config.getServletContext().getContext("/service");
+            RequestDispatcher dispatcher = targetContext.getRequestDispatcher("/dav");
+            dispatcher.forward(req, resp);
         	return;
         }
         
