a3ce46ef9b5b3627309c4de804c8899c33c495fb
Quanah Gibson-Mount <quanah@zimbra.com>
2009-09-11 17:15:24 +0000

    bug: none Add unzip to the binary requirements

--- ThirdParty/buildThirdParty.sh
+++ ThirdParty/buildThirdParty.sh
@@ -223,7 +223,7 @@ else
 fi
 
 echo "Checking for prerequisite binaries"
-for req in autoconf autoheader automake libtool bison flex gcc g++ perl make patch wget bzip2
+for req in autoconf autoheader automake libtool bison flex gcc g++ perl make patch wget bzip2 unzip
 do
 	echo "	Checking $req"
 	command=`which $req 2>/dev/null`
