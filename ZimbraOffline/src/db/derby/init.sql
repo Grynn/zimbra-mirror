370e1eab6f2eb9a5da366c834cd65f6624063583
 Unknown Perforce User <tfr>
2009-09-18 20:47:43 +0000

    bug: 35710 put offline.db.version value into init.sql
    http://bugzilla.zimbra.com/show_bug.cgi?id=35710

--- ZimbraOffline/src/db/derby/init.sql
+++ ZimbraOffline/src/db/derby/init.sql
@@ -5,6 +5,7 @@ RUN '@ZIMBRA_HOME@/db/directory.sql';
 RUN '@ZIMBRA_HOME@/db/wildfire.sql';
 RUN '@ZIMBRA_HOME@/db/versions-init.sql';
 RUN '@ZIMBRA_HOME@/db/default-volumes.sql';
+INSERT INTO config(name, value, description) VALUES ('offline.db.version', '3', 'offline db schema version');
 
 EXIT;
 
--- ZimbraOffline/src/db/sqlite/init.sql
+++ ZimbraOffline/src/db/sqlite/init.sql
@@ -4,4 +4,6 @@
 .read "@ZIMBRA_HOME@/db/wildfire.sql"
 .read "@ZIMBRA_HOME@/db/versions-init.sql"
 .read "@ZIMBRA_HOME@/db/default-volumes.sql"
+INSERT INTO config(name, value, description) VALUES ('offline.db.version', '3', 'offline db schema version');
+
 .exit
