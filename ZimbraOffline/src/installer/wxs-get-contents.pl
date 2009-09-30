c76a696bb38ad77ca31abdfd892baeb046ca29d1
 Unknown Perforce User <jmhe>
2009-09-30 08:03:47 +0000

    bug: 35709 - add ant target for building windows installer
    http://bugzilla.zimbra.com/show_bug.cgi?id=35709

---
+++ ZimbraOffline/src/installer/wxs-get-contents.pl
@@ -0,0 +1,135 @@
+#/*
+# * ***** BEGIN LICENSE BLOCK *****
+# * 
+# * Zimbra Desktop
+# * Copyright (C) 2009 Zimbra, Inc.
+# * 
+# * The contents of this file are subject to the Yahoo! Public License
+# * Version 1.0 ("License"); you may not use this file except in
+# * compliance with the License.  You may obtain a copy of the License at
+# * http://www.zimbra.com/license.
+# * 
+# * Software distributed under the License is distributed on an "AS IS"
+# * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# * 
+# * ***** END LICENSE BLOCK *****
+# */
+#
+# A build tool that fills in content elements for Wix Project file (.wxs)
+#
+# Usage: wxs-get-contents.pl <build root>
+#
+
+use strict;
+use warnings;
+use Digest::MD5 qw(md5_hex);
+
+# Generate GUID for a given string
+sub gen_guid {
+    my $seed = shift;
+    my $md5 = uc md5_hex($seed);
+    my @octets = $md5 =~ /(.{2})/g;
+
+    substr $octets[6], 0, 1, '4'; # GUID Version 4
+    substr $octets[8], 0, 1, '8'; # draft-leach-uuids-guids-01.txt GUID variant
+    my $GUID = "@octets[0..3]-@octets[4..5]-@octets[6..7]-@octets[8..9]-@octets[10..15]";
+
+    $GUID =~ s/ //g;
+    return $GUID;
+}
+
+sub find_and_replace($$) {
+    my ($file, $tokens) = @_;
+    my $tmpfile = $file . '.tmp';
+    
+    open(FIN, "<$file") or die("Error: cannot open file $file\n");
+    open(FOUT, ">$tmpfile") or die("Error: cannot open file $tmpfile\n");
+    
+    my $line;
+    while($line = <FIN>){
+        foreach my $key (keys(%$tokens)) {
+            my $pos = index($line, $key);
+            while ($pos >= 0) {
+                substr($line, $pos, length($key), $tokens->{$key});    
+                $pos = index($line, $key);
+            }
+        }
+        print FOUT $line;
+    }
+    
+    close FIN;
+    close FOUT;    
+    unlink $file;
+    rename $tmpfile, $file;
+}
+
+my $build_root = $ARGV[0];
+my $build_root_len = length($build_root);
+my $wxs_file = "$build_root\\build\\win_installer.wxs";
+my $dist_dir = "$build_root\\build\\dist";
+my ($xml_app, $xml_data, $xml_refs) = ("", "", "");
+my $seed_prefix = "zimbra_desktop_";
+my $cid_counter = 20000;
+
+sub next_cid {
+	$cid_counter++;
+	my $cid = 'ID' . "$cid_counter";
+	$xml_refs .= "      <ComponentRef Id=\"$cid\" />\r\n";
+	return $cid;
+}
+
+sub guid_by_path {
+	my $path = shift;
+	my $seed = $seed_prefix . substr($path, $build_root_len);
+	return gen_guid($seed);	
+}
+
+sub traverse {
+    my ($dir, $indent, $id, $name, $xml) = @_;
+    my ($dh, $path, $n, $p);
+    my @subdirs = ();
+    
+    if (!opendir($dh, $dir)){
+        print "Warning: cannot open directory $dir\n";
+        return;
+    }
+    
+    my $guid = guid_by_path($dir);
+    my $dirid = $id ? $id : "DIRID_$guid";
+    $dirid =~ tr/-/_/;
+    my $cid = next_cid();
+    $$xml .= "$indent<Directory Id=\"$dirid\" Name=\"$name\">\r\n";
+    
+    $$xml .= "$indent  <Component Id=\"$cid\" Guid=\"$guid\">\r\n";
+    $$xml .= "$indent    <CreateFolder />\r\n";   
+    while($n = readdir($dh)) {
+        next if ($n eq '.' || $n eq '..');
+        
+        $path = "$dir\\$n";        
+        if (-d $path) {
+            push(@subdirs, $n);
+        } else {
+        	my $fileid = 'FILEID_' . guid_by_path($path);
+        	$fileid =~ tr/-/_/;
+            $$xml .= "$indent    <File Id=\"$fileid\" Source=\"$path\" DiskId=\"1\" Name=\"$n\" />\r\n";  
+        }
+    }
+    close($dh);  
+    $$xml .= "$indent  </Component>\r\n";
+	
+	foreach my $subdir (@subdirs) {
+		traverse("$dir\\$subdir", "$indent  ", "", $subdir, $xml);	
+	}
+	
+	$$xml .= "$indent</Directory>\r\n";
+}
+
+traverse("$dist_dir\\app", "          ", "APPLICATIONFOLDER", "Zimbra Desktop", \$xml_app);
+traverse("$dist_dir\\data", "          ", "ZDDATAFOLDER", "Zimbra Desktop", \$xml_data);
+
+my $tokens = {
+	'@build.application.folder.contents@' => $xml_app, 
+	'@build.data.folder.contents@' => $xml_data, 
+	'@build.component.references@' => $xml_refs
+};
+find_and_replace($wxs_file, $tokens);
---
+++ ZimbraOffline/src/installer/zwixutils_src/CustomAction.cpp
@@ -0,0 +1,136 @@
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+//no "deprecated" warnings
+#pragma warning(disable : 4996)
+#pragma warning(disable : 4995)
+
+static int FindAndReplace(const char *file, int num, char* tokens[], char* values[]) {
+	char line[512];
+	char *tmpfile = new char[strlen(file) + 5];
+	FILE *fi, *fo;
+	int i;
+  
+	fi = fopen(file, "r");
+	if (!fi) {
+		WcaLog(LOGMSG_STANDARD, "Unable to open file %s", file);
+		return 1;
+	}
+
+	sprintf(tmpfile, "%s.tmp", file);
+	fo = fopen(tmpfile, "w");
+	if (!fo) {
+		WcaLog(LOGMSG_STANDARD, "Unable to open file %s", tmpfile);
+		fclose(fi);
+		return 1;
+	}
+
+	while(fgets(line, 512, fi) != NULL) {
+		char *out = line;
+		char buf[1024];
+		char *tok;
+
+		buf[0] = '\0';
+		for (i = 0; i < num; ++i) {
+			tok = strstr(line, tokens[i]);
+			if (tok) {
+				WcaLog(LOGMSG_VERBOSE, "Token replacement: token=%s, value=%s, file=%s",
+					tokens[i], values[i], file);
+				strncat(buf, line, tok - line);
+				strcat(buf, values[i]);
+				strcat(buf, tok + strlen(tokens[i]));	
+				out = buf;
+				break;
+			}
+		}
+		fprintf(fo, "%s", out);
+	}  
+
+	fclose(fi);
+	fclose(fo);
+
+	unlink(file);
+	rename(tmpfile, file);
+	return 0;
+}
+
+UINT __stdcall ZTokenReplace(MSIHANDLE hInstall) {
+	HRESULT hr = S_OK;
+	UINT er = ERROR_SUCCESS;
+
+	hr = WcaInitialize(hInstall, "ZTokenReplace");
+	ExitOnFailure(hr, "Failed to initialize");
+	WcaLog(LOGMSG_STANDARD, "Initialized.");
+
+    UINT rc;
+	char file[2048];
+    char prop[1024];
+	DWORD filesize = sizeof(file);
+    DWORD propsize = sizeof(prop);
+
+    rc = MsiGetProperty(hInstall, "File", &file[0], &filesize);
+    if (rc != ERROR_SUCCESS) {
+		WcaLog(LOGMSG_STANDARD, "Unable to get property: File");
+        return WcaFinalize(ERROR_INSTALL_FAILURE);
+    }
+
+    rc = MsiGetProperty(hInstall, "NumberOfTokens", &prop[0], &propsize);
+    if (rc != ERROR_SUCCESS) {
+		WcaLog(LOGMSG_STANDARD, "Unable to get property: NumberOfTokens");
+        return WcaFinalize(ERROR_INSTALL_FAILURE);
+    }
+
+	int num = atoi(prop);
+	if (num > 0) {
+		char **tokens = new char *[num];
+		char **values = new char *[num];
+		for (int i = 0; i < num; ++i) {
+			char key[32];
+
+			propsize = sizeof(prop);
+			sprintf(key, "Token%u", i + 1);
+			MsiGetProperty(hInstall, key, &prop[0], &propsize);
+			tokens[i] = strdup(prop);
+
+			propsize = sizeof(prop);
+			sprintf(key, "Value%u", i + 1);
+			MsiGetProperty(hInstall, key, &prop[0], &propsize);
+			values[i] = strdup(prop);
+			if (values[i][propsize - 1] == '\\')
+				values[i][propsize - 1] = '\0';
+		}
+
+		int ret = FindAndReplace(file, num, tokens, values);
+	
+		for (int i = 0; i < num; ++i) {
+			free(tokens[i]);
+			free(values[i]);
+		}
+
+		if (ret)
+			return WcaFinalize(ERROR_INSTALL_FAILURE);
+	}
+
+LExit:
+	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
+	return WcaFinalize(er);
+}
+
+// DllMain - Initialize and cleanup WiX custom action utils.
+extern "C" BOOL WINAPI DllMain(__in HINSTANCE hInst, __in ULONG ulReason, __in LPVOID) {
+	switch(ulReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		WcaGlobalInitialize(hInst);
+		break;
+
+	case DLL_PROCESS_DETACH:
+		WcaGlobalFinalize();
+		break;
+	}
+
+	return TRUE;
+}
