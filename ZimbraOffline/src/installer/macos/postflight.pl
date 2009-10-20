5d070589fc4a6c7402c1c047b448057152148062
 Unknown Perforce User <jmhe>
2009-10-20 23:59:41 +0000

    bug: 35709 - no more desktop icon - add scripts for start/stop zdesktop service on mac - show zimbra desktop app in finder at the end of installation - auto-launch zimbra desktop at the end of installation
    http://bugzilla.zimbra.com/show_bug.cgi?id=35709

--- ZimbraOffline/src/installer/macos/postflight.pl
+++ ZimbraOffline/src/installer/macos/postflight.pl
@@ -22,6 +22,34 @@
 use strict;
 use warnings;
 
+sub find_and_replace($$) {
+    my ($file, $tokens) = @_;
+    my $tmpfile = $file . '.tmp';
+   
+    open(FIN, "<$file") or die("Error: cannot open file $file\n");
+    open(FOUT, ">$tmpfile") or die("Error: cannot open file $tmpfile\n");
+   
+    my $line;
+    while($line = <FIN>){
+        foreach my $key (keys(%$tokens)) {
+            my $pos = index($line, $key);
+            while ($pos >= 0) {
+                substr($line, $pos, length($key), $tokens->{$key});
+                $pos = index($line, $key);
+            }
+        }
+        print FOUT $line;
+    }
+   
+    close FIN;
+    close FOUT;
+   
+    my (undef, undef, $mode) = stat($file);
+    unlink $file;
+    rename $tmpfile, $file;
+    chmod $mode, $file;
+}
+
 my $app_root = $ARGV[1];
 my $updater_app = "$app_root/macos/prism/Prism.app/Contents/Frameworks/XUL.framework/updater.app";
 my $prism_app = "$app_root/macos/prism/Prism.app";
@@ -30,4 +58,12 @@ system("mv \"${prism_app}_noreloc\" \"$prism_app\"");
 system("mv \"${updater_app}_noreloc\" \"$updater_app\"");
 system("mv \"$app_root/macos/Zimbra Desktop.app_noreloc\" \"$app_root/Zimbra Desktop.app\"");
 system("chown -R root:admin \"$app_root\"");
+
+my $tokens = {
+    '@INSTALL.APP.ROOT@' => $app_root,
+};
+find_and_replace("$app_root/Zimbra Desktop.app/Contents/MacOS/zdrun", $tokens);
+
+# open zd app in finder
+system("open \"$app_root\"");
 
