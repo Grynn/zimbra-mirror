3e46fc80bd09eba367aaef883836940fd1102427
 Unknown Perforce User <jmhe>
2009-11-26 07:43:43 +0000

    bug: 35709 - support multi-user installation - allow upgrade
    http://bugzilla.zimbra.com/show_bug.cgi?id=35709

--- ZimbraOffline/src/installer/linux/install.pl
+++ ZimbraOffline/src/installer/linux/install.pl
@@ -1,38 +1,65 @@
 #!/usr/bin/perl
 
+#/*
+# * ***** BEGIN LICENSE BLOCK *****
+# *
+# * Zimbra Desktop
+# * Copyright (C) 2009 Zimbra, Inc.
+# *
+# * The contents of this file are subject to the Yahoo! Public License
+# * Version 1.0 ("License"); you may not use this file except in
+# * compliance with the License.  You may obtain a copy of the License at
+# * http://www.zimbra.com/license.
+# *
+# * Software distributed under the License is distributed on an "AS IS"
+# * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# *
+# * ***** END LICENSE BLOCK *****
+# */
+#
+# ZD installer for application files
+#
+
 use strict;
 use warnings;
 
 my $license = "PLEASE READ THIS AGREEMENT CAREFULLY BEFORE USING THE SOFTWARE. YAHOO! INC., ON BEHALF OF ITS ZIMBRA BUSINESS UNIT, (\"ZIMBRA\") WILL ONLY LICENSE THIS SOFTWARE TO YOU IF YOU FIRST ACCEPT THE TERMS OF THIS AGREEMENT. BY DOWNLOADING OR INSTALLING THE SOFTWARE, OR USING THE PRODUCT, YOU ARE CONSENTING TO BE BOUND BY THIS AGREEMENT. IF YOU DO NOT AGREE TO ALL OF THE TERMS OF THIS AGREEMENT, THEN DO NOT DOWNLOAD, INSTALL OR USE THE PRODUCT.\n\nLicense Terms for this Zimbra Collaboration Suite Software: http://www.zimbra.com/license/zimbra_public_eula_2.1.html";
-
 my $locale = "en_US";
-my $home_dir = $ENV{HOME} || die("Error: unable to get user home directory");
-my ($app_root, $data_root, $icon_dir);
+my $app_root; 
 
 my $messages = {
     en_US => {
         AcceptDecline => "(A)ccept or (D)ecline",
         ChooseAppRoot => "Choose the folder where you would like to install Zimbra Desktop's application files",
-        ChooseDataRoot => "Choose the folder where you would like to install Zimbra Desktop's user data files",
-        ChooseIconDir => "Choose the folder where you would like to create desktop icon",
-        Configuring => "Configuring...",
         Continue => "Press enter to continue",
-        CreateIcon => "Creating desktop icon...",
-        Done => "done",
-        Installing => "Installing files...",
-        RunCommand => "You can start Zimbra Desktop by double-clicking the desktop icon or by running the following from command line:",
-        Success => 'Zimbra Desktop is installed successfully.',
+        Done => "done",        
+        Installing => "Installing application files...",
+        Success => 'You have finished installing application files.',
+        UserInstall => "Would you like to continue to install data files for user: {0}?",
+        UserInstallNote => "To install data files for additional users, please login as the user and run this command:", 
         Welcome => "Welcome to Zimbra Desktop setup wizard. This will install Zimbra Desktop on you computer.",
         YesNo => "(Y)es or (N)o"
     }
 };
 
-sub get_message($) {
-    my $key = shift();
+sub get_message($;$) {
+    my ($key, $vars) = @_;
 
     my $msgs = $messages->{$locale};
     $msgs = $messages->{'en_US'} unless ($msgs);
-    return $msgs->{$key} || '';
+    my $msg = $msgs->{$key};
+    return '' unless ($msg);
+    
+    if ($vars) {
+        my $c = 0;
+        for my $v (@$vars) {
+            my $k = '{' . $c . '}';
+            my $pos = index($msg, $k);
+            substr($msg, $pos, length($k)) = $v if ($pos >= 0);
+            $c++;
+        }
+    }
+    return $msg;
 }
 
 sub get_input($;$$) {
@@ -55,34 +82,6 @@ sub get_input($;$$) {
     return $ret;
 }
 
-sub find_and_replace($$) {
-    my ($file, $tokens) = @_;
-    my $tmpfile = $file . '.tmp';
-    
-    open(FIN, "<$file") or die("Error: cannot open file $file\n");
-    open(FOUT, ">$tmpfile") or die("Error: cannot open file $tmpfile\n");
-    
-    my $line;
-    while($line = <FIN>){
-        foreach my $key (keys(%$tokens)) {
-            my $pos = index($line, $key);
-            while ($pos >= 0) {
-                substr($line, $pos, length($key), $tokens->{$key});    
-                $pos = index($line, $key);   
-            }
-        }
-        print FOUT $line;
-    }
-    
-    close FIN;
-    close FOUT;
-
-    my (undef, undef, $mode) = stat($file);
-    unlink $file;
-    rename $tmpfile, $file;
-    chmod $mode, $file;
-}
-
 sub dialog_welcome() {
     print "\n\n";
     print get_message('Welcome'), "\n";
@@ -99,12 +98,9 @@ sub dialog_app_root() {
     return get_input(get_message("ChooseAppRoot"), '/opt/zimbra/zdesktop');
 }
 
-sub dialog_data_root() { 
-    return get_input(get_message("ChooseDataRoot"), "$home_dir/zdesktop");
-}
-
-sub dialog_desktop_icon() { 
-    return get_input(get_message("ChooseIconDir"), "$home_dir/Desktop");
+sub dialog_user_install() {
+    print get_message('UserInstall', [$ENV{USER}]), "\n";
+    return lc(get_input(get_message('YesNo'), 'Y'));
 }
 
 # main
@@ -114,43 +110,18 @@ chdir($script_dir);
 dialog_welcome();
 dialog_license();
 $app_root = dialog_app_root();
-$data_root = dialog_data_root();
-$icon_dir = dialog_desktop_icon();
 
-# copy files;
-print "\n", get_message('Installing');
-unless (-e $app_root) {
-    exit 1 if system("mkdir -p $app_root");
-}
-unless (-e $data_root) {
-    exit 1 if system("mkdir -p $data_root");
-}
+# copy app files;
+print get_message('Installing');
+system("rm -rf $app_root") if (-e $app_root);
+exit 1 if system("mkdir -p $app_root");
 exit 1 if system("cp -r -p ./app/* $app_root");
-exit 1 if system("cp -r -p ./app/data/* $data_root");
-print get_message('Done'), "\n";
-
-my $tokens = {
-	'@install.app.root@' => $app_root, 
-	'@install.data.root@' => $data_root,
-	'#@install.linux.java.home@' => "JAVA_HOME=\"$app_root/linux/jre\""
-};
-
-# fix data files
-print get_message('Configuring');
-find_and_replace("$data_root/conf/localconfig.xml", $tokens);
-find_and_replace("$data_root/jetty/etc/jetty.xml", $tokens);
-find_and_replace("$data_root/bin/zdesktop", $tokens);
-unlink("$data_root/zdesktop.webapp/override.ini");
-rename("$data_root/zdesktop.webapp/override.ini.linux", "$data_root/zdesktop.webapp/override.ini");
-print get_message('Done'), "\n";
-
-# create desktop icon
-print get_message('CreateIcon');
-find_and_replace("$app_root/linux/zd.desktop", $tokens);
-exit 1 if system("cp -p $app_root/linux/zd.desktop $icon_dir");
-print get_message('Done'), "\n";
-
+print get_message('Done'), "\n\n";
 print get_message('Success'), "\n\n";
-print get_message('RunCommand'), "\n";
-print "\"$app_root/linux/prism/zdclient\" -webapp \"$data_root/zdesktop.webapp\" -override \"$data_root/zdesktop.webapp/override.ini\" -profile \"$data_root/profile\"\n\n";
 
+if (dialog_user_install() eq 'y') {
+    system("$app_root/linux/user-install.pl");
+}
+
+print get_message('UserInstallNote'), "\n";
+print "$app_root/linux/user-install.pl\n\n";
---
+++ ZimbraOffline/src/installer/linux/user-install.pl
@@ -0,0 +1,238 @@
+#!/usr/bin/perl
+
+#/*
+# * ***** BEGIN LICENSE BLOCK *****
+# *
+# * Zimbra Desktop
+# * Copyright (C) 2009 Zimbra, Inc.
+# *
+# * The contents of this file are subject to the Yahoo! Public License
+# * Version 1.0 ("License"); you may not use this file except in
+# * compliance with the License.  You may obtain a copy of the License at
+# * http://www.zimbra.com/license.
+# *
+# * Software distributed under the License is distributed on an "AS IS"
+# * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# *
+# * ***** END LICENSE BLOCK *****
+# */
+#
+# ZD installer for user data files
+#
+
+use strict;
+use warnings;
+
+my $locale = "en_US";
+my $home_dir = $ENV{HOME} || die("Error: unable to get user home directory");
+
+my $messages = {
+    en_US => {
+        ChooseDataRoot => "Choose the folder where you would like to install Zimbra Desktop's user data files",
+        ChooseIconDir => "Choose the folder where you would like to create desktop icon",
+        Configuring => "Initializing user data...",
+        CreateIcon => "Creating desktop icon...",
+		Installing => "Installing user data files...",
+        Done => "done",
+        RunCommand => "You can start Zimbra Desktop by double-clicking the desktop icon or by running the following command:",
+		RunWithAbsPath => 'You must run user-install.pl with absolute path.',
+        Success => 'Zimbra Desktop has been installed successfully for user {0}.'
+    }
+};
+
+sub get_message($;$) {
+    my ($key, $vars) = @_;
+
+    my $msgs = $messages->{$locale};
+    $msgs = $messages->{'en_US'} unless ($msgs);
+    my $msg = $msgs->{$key};
+    return '' unless ($msg);
+    
+    if ($vars) {
+        my $c = 0;
+        for my $v (@$vars) {
+            my $k = '{' . $c . '}';
+            my $pos = index($msg, $k);
+            substr($msg, $pos, length($k)) = $v if ($pos >= 0);
+            $c++;
+        }
+    }
+    return $msg;
+}
+
+sub get_input($;$$) {
+    my ($prompt, $default, $allow_null) = @_;
+    my $ret = '';
+
+    print "------------------------------\n";
+    while (!$ret) {
+	print $prompt;
+	print " [$default]" if ($default);
+	print ": ";
+
+	$| = 1;
+	$_ = <STDIN>;
+	chomp();
+	$ret = $default ? ($_ ? $_ : $default) : $_;
+	last if ($allow_null);
+    }
+    print "\n\n";
+    return $ret;
+}
+
+sub find_and_replace($$) {
+    my ($file, $tokens) = @_;
+    my $tmpfile = $file . '.tmp';
+    
+    open(FIN, "<$file") or die("Error: cannot open file $file\n");
+    open(FOUT, ">$tmpfile") or die("Error: cannot open file $tmpfile\n");
+    
+    my $line;
+    while($line = <FIN>){
+        foreach my $key (keys(%$tokens)) {
+            my $pos = index($line, $key);
+            while ($pos >= 0) {
+                substr($line, $pos, length($key), $tokens->{$key});    
+                $pos = index($line, $key);   
+            }
+        }
+        print FOUT $line;
+    }
+    
+    close FIN;
+    close FOUT;
+
+    my (undef, undef, $mode) = stat($file);
+    unlink $file;
+    rename $tmpfile, $file;
+    chmod $mode, $file;
+}
+
+sub get_random_id() {
+    my @n;
+    srand(time ^ ($$ + ($$ << 15)) ^ int(rand(0xFFFF)));
+    push(@n, sprintf("%04x", int(rand(0xFFFF)))) for (0..7);
+    return "$n[0]$n[1]-$n[2]-$n[3]-$n[4]-$n[5]$n[6]$n[7]";
+}
+
+sub move_no_overwrite($$) {
+	my ($src, $dest) = @_;
+	my @files;
+	
+	if (! opendir(DH, $src)) {
+		print "Unable to open directory $src\n";
+		return;
+	}
+	@files = readdir(DH);
+	closedir(DH);
+
+	foreach my $file (@files) {
+		next if ( $file eq "." || $file eq ".." );
+		unless ( -e "$dest/$file") {
+			system("mv \"$src/$file\" \"$dest\"");
+		}
+	}
+}
+
+sub dialog_data_root() { 
+    return get_input(get_message("ChooseDataRoot"), "$home_dir/zdesktop");
+}
+
+sub dialog_desktop_icon() { 
+    return get_input(get_message("ChooseIconDir"), "$home_dir/Desktop");
+}
+
+# main
+my ($app_root, $data_root, $icon_dir);
+
+my $script_path = $0;
+if ($script_path eq 'user-install.pl' || $script_path eq './user-install.pl') {
+	$script_path = `pwd`;
+	chomp($script_path);
+	$script_path .= '/user-install.pl';
+}
+
+unless ($script_path =~ /^\/.+/) {
+	print get_message('RunWithAbsPath'), "\n";
+	exit 1;
+}
+
+$app_root = substr($script_path, 0, length($script_path) - 22); # 22: "/linux/user-install.pl"
+chdir($app_root);
+
+$data_root = dialog_data_root();
+$icon_dir = dialog_desktop_icon();
+
+my $tmpdir = "$data_root" . ".tmp";
+my @user_files = ("index", "store", "sqlite", "log", "zimlets-properties",
+    "profile/prefs.js", "profile/persdict.dat", "profile/localstore.json");
+
+my $is_upgrade = 0;
+if (-e $data_root) {
+	$is_upgrade = 1;	
+
+	# backup user data	
+    mkdir($tmpdir);
+    system("rm -rf \"$tmpdir/*\"");
+    mkdir("$tmpdir/profile");
+    mkdir("$tmpdir/zimlets");
+
+    for (@user_files) {
+        my $src = "$data_root/$_";
+        system("mv -f \"$src\" \"$tmpdir/$_\"") if (-e $src);
+    }
+    my $zimbak = "$data_root/zimlets/backup";
+    system("mv -f \"$zimbak\"/* \"$tmpdir/zimlets\"") if (-e $zimbak);
+
+    system("rm -rf \"$data_root\"");
+}
+
+# copy files;
+print "\n", get_message('Installing');
+exit 1 if system("mkdir -p \"$data_root\"");
+exit 1 if system("cp -r -p ./data/* \"$data_root\"");
+print get_message('Done'), "\n";
+
+my $tokens = {
+	'@install.app.root@' => $app_root, 
+	'@install.data.root@' => $data_root,
+	'@install.key@' => get_random_id(),
+	'#@install.linux.java.home@' => "JAVA_HOME=\"$app_root/linux/jre\""
+};
+
+# fix data files
+print get_message('Configuring');
+find_and_replace("$data_root/conf/localconfig.xml", $tokens);
+find_and_replace("$data_root/jetty/etc/jetty.xml", $tokens);
+find_and_replace("$data_root/bin/zdesktop", $tokens);
+find_and_replace("$data_root/zdesktop.webapp/webapp.ini", $tokens);
+print get_message('Done'), "\n";
+
+# create desktop icon
+print get_message('CreateIcon');
+exit 1 if system("cp -f -p \"$app_root/linux/zd.desktop\" \"$icon_dir\"");
+find_and_replace("$icon_dir/zd.desktop", $tokens);
+print get_message('Done'), "\n";
+
+if ($is_upgrade) {
+	for (@user_files) {
+        my $src = "$tmpdir/$_";
+        next if (! -e $src);
+
+        my $dest = "$data_root/$_";
+        if ((-d $src) && (-e $dest)) {
+            system("mv -f \"$src\"/* \"$dest\""); # must move '/*' outside the quote
+        } else {
+            system("mv -f \"$src\" \"$dest\"");
+        }
+    }
+	
+    move_no_overwrite("$tmpdir/zimlets", "$data_root/zimlets");
+
+    system("rm -rf \"$tmpdir\"");
+}
+
+print get_message('Success', [$ENV{USER}]), "\n\n";
+print get_message('RunCommand'), "\n";
+print "\"$app_root/linux/prism/zdclient\" -webapp \"$data_root/zdesktop.webapp\" -override \"$data_root/zdesktop.webapp/override.ini\" -profile \"$data_root/profile\"\n\n";
+
