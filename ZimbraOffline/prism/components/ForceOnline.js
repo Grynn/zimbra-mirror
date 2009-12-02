e1e6af6010371e143b15d062e8f40b2d9149e588
 Unknown Perforce User <jjzhuang>
2009-12-02 01:36:16 +0000

    bug: 21178
    update ForceOnline.js to provide the network detection hooks.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=21178

--- ZimbraOffline/prism/components/ForceOnline.js
+++ ZimbraOffline/prism/components/ForceOnline.js
@@ -1,10 +1,74 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+const ONLINE_STATUS = "online";
+const OFFLINE_STATUS = "offline";
+
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function DummyEvent(type)
+{
+  this.type = type;
+}
+
+DummyEvent.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMEvent, Ci.nsIClassInfo, Ci.nsISecurityCheckedComponent]),
+
+  // nsIClassInfo
+  implementationLanguage: Ci.nsIProgrammingLanguage.JAVASCRIPT,
+  flags: Ci.nsIClassInfo.DOM_OBJECT,
+
+  getInterfaces: function getInterfaces(aCount) {
+    var interfaces = [Ci.nsIDOMEvent,
+                      Ci.nsISecurityCheckedComponent,
+                      Ci.nsIClassInfo];
+    aCount.value = interfaces.length;
+    return interfaces;
+  },
+
+  getHelperForLanguage: function getHelperForLanguage(aLanguage) {
+    return null;
+  },
+
+  //nsISecurityCheckedComponent
+  canCallMethod: function canCallMethod(iid, methodName) {
+    Components.utils.reportError(methodName);
+    return "NoAccess";
+  },
+
+  canCreateWrapper: function canCreateWrapper(iid) {
+    return "AllAccess";
+  },
+
+  canGetProperty: function canGetProperty(iid, propertyName) {
+    Components.utils.reportError(propertyName);
+    return "AllAccess";
+  },
+
+  canSetProperty: function canSetProperty(iid, propertyName) {
+    Components.utils.reportError(propertyName);
+    return "NoAccess";
+  }
+}
+
+var gSingleton = null;
+var ForceOnlineFactory = {
+  createInstance: function af_ci(aOuter, aIID) {
+    if (aOuter != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+
+    if (gSingleton == null) {
+      gSingleton = new ForceOnline();
+    }
+
+    return gSingleton.QueryInterface(aIID);
+  }
+}
 
 function ForceOnline()
 {
+  this._listenerMap = [];
+  this._ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
 }
 
 ForceOnline.prototype = {
@@ -12,10 +76,52 @@ ForceOnline.prototype = {
   classID: Components.ID("{1a63c05f-caba-4b26-8d2e-f70d4ccc1e97}"),
   contractID: "@zimbra.com/force-online;1",
   
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsIDOMEventTarget, Ci.nsIClassInfo, Ci.nsISecurityCheckedComponent]),
 
-  _xpcom_categories: [{ category: "app-startup", service: true }],
+  _xpcom_factory : ForceOnlineFactory,
+
+  _xpcom_categories: [
+    { category: "app-startup", service: true },
+    { category: "JavaScript global constructor", entry: "ZimbraNetworkChecker" }
+  ],
   
+  // nsIClassInfo
+  implementationLanguage: Ci.nsIProgrammingLanguage.JAVASCRIPT,
+  flags: Ci.nsIClassInfo.DOM_OBJECT,
+
+  getInterfaces: function getInterfaces(aCount) {
+    var interfaces = [Ci.nsIObserver,
+                      Ci.nsIDOMEventTarget,
+                      Ci.nsISecurityCheckedComponent,
+                      Ci.nsIClassInfo];
+    aCount.value = interfaces.length;
+    return interfaces;
+  },
+
+  getHelperForLanguage: function getHelperForLanguage(aLanguage) {
+    return null;
+  },
+
+  //nsISecurityCheckedComponent
+  canCallMethod: function canCallMethod(iid, methodName) {
+    Components.utils.reportError(methodName);
+    return "AllAccess";
+  },
+
+  canCreateWrapper: function canCreateWrapper(iid) {
+    return "AllAccess";
+  },
+
+  canGetProperty: function canGetProperty(iid, propertyName) {
+    Components.utils.reportError(propertyName);
+    return "AllAccess";
+  },
+
+  canSetProperty: function canSetProperty(iid, propertyName) {
+    Components.utils.reportError(propertyName);
+    return "NoAccess";
+  },
+
   observe : function(aSubject, aTopic, aData) {
     switch (aTopic) {
       case "app-startup":
@@ -23,19 +129,50 @@ ForceOnline.prototype = {
                          getService(Ci.nsIObserverService);
         obsService.addObserver(this, "network:offline-status-changed", false);
         obsService.addObserver(this, "profile-change-net-teardown", false);
+        
+        this._networkLinkService = Cc["@mozilla.org/network/network-link-service;1"].getService(Ci.nsINetworkLinkService);
+        this._timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+        this._timer.init(this, 1000, Ci.nsITimer.TYPE_REPEATING_SLACK);
+        this._online = this._networkLinkService.isLinkUp;
         break;
       case "profile-change-net-teardown":
-        var obsService = Cc["@mozilla.org/observer-service;1"].
-                         getService(Ci.nsIObserverService);
+        var obsService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
         obsService.removeObserver(this, "network:offline-status-changed");
         break;
       case "network:offline-status-changed":
         if (aData == "offline") {
-          var ioService = Cc["@mozilla.org/network/io-service;1"].
-                          getService(Ci.nsIIOService);
-          ioService.offline = false;
+          this._ios.offline = false;
         }
         break;
+      case "timer-callback":
+        var online = this._networkLinkService.isLinkUp;
+        if (online != this._online) {
+          this._online = online;
+          this._dispatchNetworkStatusEvent(this._online ? ONLINE_STATUS : OFFLINE_STATUS);
+        }
+        break;
+    }
+  },
+  
+  addEventListener : function(type, listener, capture) {
+    if (!(type in this._listenerMap)) {
+      this._listenerMap[type] = [];
+    }
+    this._listenerMap[type].push(listener);
+  },
+  
+  removeEventListener : function(type, listener, capture) {
+    // TODO
+  },
+  
+  _dispatchNetworkStatusEvent : function(status) {
+    if (status in this._listenerMap) {
+      var listeners = this._listenerMap[status];
+      for (var i=0; i<listeners.length; i++) {
+        var event = new DummyEvent(status);
+        event.currentTarget = listeners[i];
+        listeners[i].handleEvent(event);
+      }
     }
   }
 };
