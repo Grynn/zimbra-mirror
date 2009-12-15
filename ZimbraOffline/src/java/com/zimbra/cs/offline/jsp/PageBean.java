973d77d51808ab6ee31b5cca85214597ffd39ac4
 Unknown Perforce User <tfr>
2009-12-15 22:29:51 +0000

    bug: 43280 i18n bundle is now in messages instead of desktop
    http://bugzilla.zimbra.com/show_bug.cgi?id=43280

--- ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/PageBean.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/PageBean.java
@@ -31,13 +31,13 @@ import com.zimbra.cs.account.Provisioning;
 
 public class PageBean {
 
-	private static final String APP_VERSION = LC.get("zdesktop_relabel") + LC.get("zdesktop_version") + " (build " + LC.get("zdesktop_buildid") + ")";
-	
-	private Locale clientLocale = new Locale("en");
-	private static Locale serverLocale = null; 
-	
-	private String getMsgFromBundle(String key) {
-        ResourceBundle bundle = ResourceBundle.getBundle("/desktop/ZdMsg", clientLocale);
+    private static final String APP_VERSION = LC.get("zdesktop_relabel") + LC.get("zdesktop_version") + " (build " + LC.get("zdesktop_buildid") + ")";
+
+    private Locale clientLocale = new Locale("en");
+    private static Locale serverLocale = null; 
+
+    private String getMsgFromBundle(String key) {
+        ResourceBundle bundle = ResourceBundle.getBundle("/messages/ZdMsg", clientLocale);
         String msg;
         try {
             msg = bundle.getString(key);
@@ -45,92 +45,92 @@ public class PageBean {
             msg = null;
         }
         return msg;
-	}
-	
-	protected String getMessage(String key) {
-	    String msg = getMsgFromBundle(key);
-		return msg == null ? "??" + key + "??" : msg; 
-	}
-	
-	protected String getMessage(String key, boolean keyAsDefault) {
-	    return keyAsDefault ? getMessage(key) : getMsgFromBundle(key);
-	}
-	
-	protected String getMessage(String key, Object[] params) {
-		return MessageFormat.format(getMessage(key), params);
-	}
-	
-	protected String getMessage(String key, Object[] params, boolean keyAsDefault) {
-	    if (keyAsDefault)
-	        return getMessage(key, params);
-	    String msg = getMsgFromBundle(key);
-	    return msg == null ? null : MessageFormat.format(msg, params);
-	}
-	
-	public PageBean() {}
-	
-	public String getAppVersion() {
-		return APP_VERSION;
-	}
-	
-	public String getLoginUsername() {
-		try {
-			return JspProvStub.getInstance().getLoginAccountName();
-		} catch (ServiceException x) {
-			return JspConstants.LOCAL_ACCOUNT;
-		}
-	}
-	
-	public void setLocale(Locale locale) throws ServiceException {
-		clientLocale = locale;
-		updateServerLocale(locale);
-	}
-	
-        public String getSkin() {
-            try {
-                return JspProvStub.getInstance().getOfflineAccount(
-                    OfflineConstants.LOCAL_ACCOUNT_ID).getAttr(Provisioning.A_zimbraPrefSkin);
-            } catch (ServiceException x) {
-                return "twilight";
-            }
+    }
+
+    protected String getMessage(String key) {
+        String msg = getMsgFromBundle(key);
+        return msg == null ? "??" + key + "??" : msg; 
+    }
+
+    protected String getMessage(String key, boolean keyAsDefault) {
+        return keyAsDefault ? getMessage(key) : getMsgFromBundle(key);
+    }
+
+    protected String getMessage(String key, Object[] params) {
+        return MessageFormat.format(getMessage(key), params);
+    }
+
+    protected String getMessage(String key, Object[] params, boolean keyAsDefault) {
+        if (keyAsDefault)
+            return getMessage(key, params);
+        String msg = getMsgFromBundle(key);
+        return msg == null ? null : MessageFormat.format(msg, params);
+    }
+
+    public PageBean() {}
+
+    public String getAppVersion() {
+        return APP_VERSION;
+    }
+
+    public String getLoginUsername() {
+        try {
+            return JspProvStub.getInstance().getLoginAccountName();
+        } catch (ServiceException x) {
+            return JspConstants.LOCAL_ACCOUNT;
         }
-    
-	public static synchronized void updateServerLocale(Locale locale) throws ServiceException {
-	    if (serverLocale == null || !serverLocale.equals(locale)) {	        
-	        Map<String, Object> attrs = new HashMap<String, Object>();
-	        attrs.put(Provisioning.A_zimbraPrefLocale, locale.toString());
-	        JspProvStub stub = JspProvStub.getInstance();
-	        stub.modifyOfflineAccount(OfflineConstants.LOCAL_ACCOUNT_ID, attrs);
-	        
-	        serverLocale = (Locale)locale.clone();						
-	    }
-	}
-	
-	public static String getLocalConfig(String key) {
-		return LC.get(key);
-	}
-	
-	public static boolean checkAuthToken(HttpServletRequest request) {
-	    String key = LC.get("zdesktop_installation_key");
-	    if (key == null || key.startsWith("@"))
-	        return true;	    
-	    String at = request.getParameter("at");
-	    return (at != null && at.equals(key));
-	}
-	
-	public static String addAuthToken(String url) {
+    }
+
+    public void setLocale(Locale locale) throws ServiceException {
+        clientLocale = locale;
+        updateServerLocale(locale);
+    }
+
+    public String getSkin() {
+        try {
+            return JspProvStub.getInstance().getOfflineAccount(
+                OfflineConstants.LOCAL_ACCOUNT_ID).getAttr(Provisioning.A_zimbraPrefSkin);
+        } catch (ServiceException x) {
+            return "twilight";
+        }
+    }
+
+    public static synchronized void updateServerLocale(Locale locale) throws ServiceException {
+        if (serverLocale == null || !serverLocale.equals(locale)) {	        
+            Map<String, Object> attrs = new HashMap<String, Object>();
+            attrs.put(Provisioning.A_zimbraPrefLocale, locale.toString());
+            JspProvStub stub = JspProvStub.getInstance();
+            stub.modifyOfflineAccount(OfflineConstants.LOCAL_ACCOUNT_ID, attrs);
+
+            serverLocale = (Locale)locale.clone();						
+        }
+    }
+
+    public static String getLocalConfig(String key) {
+        return LC.get(key);
+    }
+
+    public static boolean checkAuthToken(HttpServletRequest request) {
+        String key = LC.get("zdesktop_installation_key");
+        if (key == null || key.startsWith("@"))
+            return true;	    
+        String at = request.getParameter("at");
+        return (at != null && at.equals(key));
+    }
+
+    public static String addAuthToken(String url) {
         String key = LC.get("zdesktop_installation_key");
         if (key == null || key.startsWith("@"))
             return url;
-	    int pos = url.indexOf('?');
-	    return url + (pos < 0 ? "?" : "&") + "at=" + key;
-	}
-	
-	public static boolean isPrism(String userAgent) {
-		return userAgent != null && userAgent.indexOf("Prism") >= 0;
-	}
-	
-	public static String getBaseUri() {
-		return "http://127.0.0.1:" + LC.zimbra_admin_service_port.value();
-	}
+        int pos = url.indexOf('?');
+        return url + (pos < 0 ? "?" : "&") + "at=" + key;
+    }
+
+    public static boolean isPrism(String userAgent) {
+        return userAgent != null && userAgent.indexOf("Prism") >= 0;
+    }
+
+    public static String getBaseUri() {
+        return "http://127.0.0.1:" + LC.zimbra_admin_service_port.value();
+    }
 }
