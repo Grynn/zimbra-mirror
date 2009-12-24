0dea559b51a44154d3454c3829bf07eed27530eb
 Unknown Perforce User <tfr>
2009-12-24 00:39:04 +0000

    bug: none fix dnd extension for prism remove y! for prism app

--- ZimbraOffline/prism/profile/user.js
+++ ZimbraOffline/prism/profile/user.js
@@ -2,12 +2,18 @@ user_pref("general.useragent.locale", "@install.locale@");
 user_pref("intl.accept_languages", "@install.locale@");
 user_pref("spellchecker.dictionary", "@install.locale@");
 
-user_pref("browser.cache.disk.capacity", "10240");
-user_pref("browser.cache.memory.capacity", "4096");
-user_pref("browser.sessionhistory.max_total_viewers", "2");
+user_pref("browser.cache.disk.capacity", "12288");
+user_pref("browser.cache.memory.capacity", "3072");
+user_pref("browser.sessionhistory.max_total_viewers", "0");
 user_pref("config.trim_on_minimize", "true");
+user_pref("geo.enabled", "false");
+user_pref("keyword.enabled", "false");
 user_pref("network.http.keep-alive-timeout", "600");
-user_pref("network.http.max-connections-per-server", "4");
+user_pref("network.http.max-connections-per-server", "6");
 user_pref("network.http.max-persistent-connections-per-server", "4");
+user_pref("network.http.request.max-start-delay", "1");
 user_pref("network.http.pipelining", "true");
-user_pref("nglayout.initialpaint.delay", "150");
+user_pref("network.http.pipelining.firstrequest", "true");
+user_pref("network.http.pipelining.maxrequests", "3");
+user_pref("network.prefetch-next", "false");
+
