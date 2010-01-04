912494c34dd57f4666c4ef23176587c3f0866230
 Unknown Perforce User <dcomfort>
2010-01-04 18:19:38 +0000

    Bug: 43522 Fix license headers
    http://bugzilla.zimbra.com/show_bug.cgi?id=43522

--- ZimbraWebClient/ffext/src/zimbratoolbar/chrome/content/zmtb_ajax/ZMTB_AjxException.js
+++ ZimbraWebClient/ffext/src/zimbratoolbar/chrome/content/zmtb_ajax/ZMTB_AjxException.js
@@ -1,17 +1,16 @@
 /*
- * Copyright (C) 2006, The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2009, 2010 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
  */
 
 /**
--- ZimbraWebClient/ffext/src/zimbratoolbar/chrome/content/zmtb_ajax/ZMTB_AjxStringUtil.js
+++ ZimbraWebClient/ffext/src/zimbratoolbar/chrome/content/zmtb_ajax/ZMTB_AjxStringUtil.js
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2006, The Apache Software Foundation.
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Web Client
+ * Copyright (C) 2009, 2010 Zimbra, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The contents of this file are subject to the Yahoo! Public License
+ * Version 1.0 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
  */
+
 
 /**
 * Does nothing (static class).
