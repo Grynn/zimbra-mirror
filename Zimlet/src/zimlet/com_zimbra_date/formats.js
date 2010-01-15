3184fb7730ba9d58f0a4ac68660e31870e4669c9
Quanah Gibson-Mount <quanah@zimbra.com>
2010-01-15 03:07:15 +0000

    bug: none license preamble update

--- Zimlet/src/zimlet/com_zimbra_date/formats.js
+++ Zimlet/src/zimlet/com_zimbra_date/formats.js
@@ -1,3 +1,19 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Zimlets
+ * Copyright (C) 2008, 2009 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.2 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 Com_Zimbra_Date.DEFAULT_FORMATS = [
 	// conversion to UTC done with http://timeanddate.com/
 	"Brooke Ellen Henrikson",		"2009-10-17T10:20:00Z",
