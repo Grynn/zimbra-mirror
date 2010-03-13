d120c7dc1a02603fd93753607fabbf24967697d7
Komal Kakani <kkakani@zimbra.com>
2010-03-13 18:59:36 +0000

    bug:43387 -In case of safari, get the browser version from "Version" token in the userAgent string. -Increment the check for gecko from 1.8 to 1.9 since firefox 3.0/above use Gecko 1.9 or above.
    http://bugzilla.zimbra.com/show_bug.cgi?id=43387

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZUserAgentBean.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZUserAgentBean.java
@@ -105,7 +105,6 @@ public class ZUserAgentBean {
                     geckoDate = Float.parseFloat(token.substring(index + 6));
                 } else if ((index = token.indexOf("rv:")) != -1){
                     mozVersion = new Version(token.substring(index + 3));
-                    browserVersion = mozVersion;
                 } else if ((index = token.indexOf("firefox/")) != -1){
                     isFirefox = true;
                     browserVersion = new Version(token.substring(index + 8));
@@ -117,7 +116,6 @@ public class ZUserAgentBean {
                     browserVersion = new Version(token.substring(index + 9));
                 } else if ((index = token.indexOf("safari/")) != -1){
                     isSafari = true;
-                    browserVersion = new Version(token.substring(index + 7));
                 } else if ((index = token.indexOf("chrome/")) != -1){
                     isChrome = true;
                     browserVersion = new Version(token.substring(index + 7));
@@ -130,6 +128,9 @@ public class ZUserAgentBean {
                     isOsLinux = true;
                 }else if (token.indexOf("android") != -1){
                     isOsAndroid = true;
+                }else if ((index = token.indexOf("version/")) != -1){
+                    //In case of safari, get the browser version here
+                    browserVersion = new Version(token.substring(index + 8));
                 }
 
                 token = agtArr.hasMoreTokens() ? agtArr.nextToken() : null;
@@ -219,7 +220,7 @@ public class ZUserAgentBean {
 
     public boolean getIsFirefox3up() { return (isFirefox && browserVersion.greaterOrEqual(3,0)); }
 
-	public boolean getIsGecko1_8up() { return (isGeckoBased && browserVersion.greaterOrEqual(1,8)); }
+	public boolean getIsGecko1_9up() { return (isGeckoBased && mozVersion.greaterOrEqual(1,9)); }
 
     public boolean getIsGecko() { return isGeckoBased; }
     
