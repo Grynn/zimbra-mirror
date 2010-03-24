44f56d0b774263a24aa22804e46e3b0dba0cebd4
Quanah Gibson-Mount <quanah@zimbra.com>
2010-03-24 22:14:51 +0000

    bug: 45577 New OpenSSL release
    http://bugzilla.zimbra.com/show_bug.cgi?id=45577

--- ThirdParty/versions.sh
+++ ThirdParty/versions.sh
@@ -4,7 +4,7 @@ cyrus_version=${cyrus_release}.${cyrus_patchlevel}
 cyrus_src=cyrus-sasl-${cyrus_version}
 
 heimdal_version=1.3.2
-openssl_version=0.9.8m
+openssl_version=0.9.8n
 curl_version=7.20.0
 xml2_version=2.6.32.1
 
