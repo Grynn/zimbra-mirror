1027759cc09f857d9f2a32c94f16c7cbff2019c7
 Unknown Perforce User <rrao>
2010-04-11 04:38:47 +0000

    bug:46113,15658 Rewrite most of the Amazon Zimlet. -  It now generates Amazon HMAC Version 2 signature required by Amazon Service. - Rewrote tooltip-view (It now clearly shows book related to the isbn) - Rewrote Book Search dialog (It now shows all 10 book search results) - Standardized as a part of bug 46113 - Made localizable as a part of bug 46113
    http://bugzilla.zimbra.com/show_bug.cgi?id=46113
    http://bugzilla.zimbra.com/show_bug.cgi?id=15658

---
+++ Zimlet/src/zimlet/com_zimbra_amzn/amzn.js
@@ -0,0 +1,268 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Zimlets
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+ *
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+ * Amazon Zimlet. Performs book search and shows "hover" information for ISBN numbers in mail or appointments
+ *
+ * @author Raja Rao DV (rrao@zimbra.com) - Rewrote most of the Zimlet
+ * @author Kevin Henrikson - Original Author
+ */
+
+/**
+ * Default constructor.
+ */
+function Com_Zimbra_Amazon_HandlerObject() {
+}
+
+Com_Zimbra_Amazon_HandlerObject.prototype = new ZmZimletBase();
+Com_Zimbra_Amazon_HandlerObject.prototype.constructor = Com_Zimbra_Amazon_HandlerObject;
+
+/**
+ * Simplify Zimlet handler name
+ */
+var AmazonZimlet = Com_Zimbra_Amazon_HandlerObject;
+
+/**
+ * Defines the "alert on checkbox" element.
+ */
+AmazonZimlet.ELEMENT_RESULTS_DIV_ID = "amznZimlet_bookSearchResultsDiv";
+
+
+
+
+/**
+ * This method is called by Zimbra Framework when the tool tip is popped-up.
+ *
+ * @param {Object} spanElement Html span element
+ * @param {object} obj The matched string
+ * @param {object} context ZmZimletContext
+ * @param {HTMLElement} canvas Main Html element of the tooltip
+ */
+AmazonZimlet.prototype.toolTipPoppedUp =
+function(spanElement, obj, context, canvas) {
+	this._currentTooltipId = Dwt.getNextId();
+	canvas.innerHTML = ["<div id='",this._currentTooltipId,"'>",this.getMessage("AmazonZimlet_searching"),"</div>"].join("");
+	var searchKeys = obj.replace(/[- A-WY-Z]/ig, '');
+	var paramsArray = [
+		["Keywords", searchKeys]
+	];
+	var url = this._getUrlFromJSP(paramsArray);
+	var completeUrl = ZmZimletBase.PROXY + AjxStringUtil.urlComponentEncode(url);
+	AjxRpc.invoke(null, completeUrl, null, new AjxCallback(this, this._searchCallback, this._currentTooltipId), true);
+};
+
+/**
+ * Called by Framework when isbn link is clicked. This opens Amazon webpage for that isbn number.
+ *
+ * @param Object} spanElement Html span element
+ * @param {object} contentObjText The matched string
+ * @param  {object} matchContext ZmZimletContext
+ * @param {object} event  Html event object
+ */
+AmazonZimlet.prototype.clicked =
+function(spanElement, contentObjText, matchContext, event) {
+	window.open("http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dstripbooks&field-keywords=" + AjxStringUtil.urlComponentEncode(contentObjText) + "&x=0&y=0");
+};
+
+/**
+ * Performs Amazon Book Search
+ *
+ */
+AmazonZimlet.prototype._doAmazonBookSearch =
+function() {
+	var searchKeys = this._searchField.value;
+	var paramsArray = [
+		["Keywords",searchKeys]
+	];
+	var url = this._getUrlFromJSP(paramsArray);
+	var completeUrl = ZmZimletBase.PROXY + AjxStringUtil.urlComponentEncode(url);
+	AjxRpc.invoke(null, completeUrl, null, new AjxCallback(this, this._searchCallback, AmazonZimlet.ELEMENT_RESULTS_DIV_ID), true);
+};
+
+/**
+ * Gets AWS url for ItemSearch API. It internally generates AWS HMAC Version 2 Signature and appends it to the url.
+ *
+ *@params {Array} args A hash array of keys and value parameters that needs to be sent to generate AWS URL.
+ * @params {Array} args[String, String] First item of the array is the Key and 2nd item is the value 
+ */
+AmazonZimlet.prototype._getUrlFromJSP =
+function(args) {
+	var params = new Array;
+	for (var i = 0; i < args.length; i++) {
+		var item = args[i];
+		params.push(item[0] + "=" + item[1]);
+	}
+	var url = this.getResource("generateAMZNSignature.jsp") + "?" + params.join("&");
+	var response = AjxRpc.invoke(null, url, null, null, true);
+	return response.text;
+};
+
+
+/**
+ * This method is called when the panel item is double-clicked.
+ *
+ */
+AmazonZimlet.prototype.doubleClicked = function() {
+	this.singleClicked();
+};
+
+/**
+ * This method is called when the panel item is single-clicked.
+ *
+ */
+AmazonZimlet.prototype.singleClicked = function() {
+	this._showBookSearchDlg();
+};
+
+/**
+ * Displays information on multiple books.
+ *
+ * @param {array} itemList An array of objects representing Book information
+ * @param {string} resultsDiv The id of the div into which the generated Html should be inserted into.
+ */
+AmazonZimlet.prototype._displayBooks =
+function(itemList, resultsDiv) {
+	var items = itemList;
+	//dont use yellow div if its tooltip
+	var divClass = (resultsDiv == AmazonZimlet.ELEMENT_RESULTS_DIV_ID) ? "amznZimlet_Yellow" : "";
+	if (itemList && (typeof itemList.length == "undefined")) {
+		items = [itemList];
+	}
+	var html = [];
+	var j = 0;
+	for (var i = 0; i < items.length; i++) {
+		var item = items[i];
+		var attributes = item.ItemAttributes;
+		var imageInfo = item.MediumImage ? item.MediumImage : ( item.SmallImage ? item.SmallImage : null);
+		var url = item.DetailPageURL ? item.DetailPageURL.toString() : "";
+		html[j++] = ["<div class='",divClass,"'>"].join("");
+		html[j++] = "<table width=100%><tr>";
+		if (imageInfo != null) {
+			html[j++] = ["<td width=130px><a href='",url,"' target='_blank'><img  align=center src='",imageInfo.URL.toString(),
+				"' width='",imageInfo.Width.toString(),"px' height='",imageInfo.Height.toString(),"px' /></a></td>"].join("");
+		}
+		html[j++] = "<td valign=top><table>";
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_title"),"</td><td class='amznZimlet_TDBlack14Bold'>",(attributes.Title ? attributes.Title.toString() : "") ,"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_author"),"</td><td>",(attributes.Author ? attributes.Author.toString() : ""),"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_listPrice"),"</td><td class='amznZimlet_TDBlueBold'>",(attributes.ListPrice ? (attributes.ListPrice.FormattedPrice ? attributes.ListPrice.FormattedPrice.toString() : "") : ""),"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_isbn"),"</td><td>",(attributes.ISBN ? attributes.ISBN.toString() : ""),"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_edition"),"</td><td>",(attributes.Edition ? attributes.Edition.toString() : ""),"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_publisher"),"</td><td>",(attributes.Publisher ? attributes.Publisher.toString() : ""),"</td></tr>"].join("");
+		html[j++] = ["<tr><td class='amznZimlet_HdrTD'>",this.getMessage("AmazonZimlet_amazonPage"),"</td><td><a href='",url,"' target='_blank'>",this.getMessage("AmazonZimlet_openInAmazon"),"</a></td></tr>"].join("");
+
+		html[j++] = "</table></td></tr></table>";
+		html[j++] = "</div>";
+		html[j++] = "<br/>";
+
+	}
+	document.getElementById(resultsDiv).innerHTML = html.join("");
+};
+
+/**
+ *  Shows Book Search Dialog
+ */
+AmazonZimlet.prototype._showBookSearchDlg = function() {
+	if (this._bookSearchDialog) {
+		this._bookSearchDialog.popup();
+		return;
+	}
+	this._bookSearchDlgView = new DwtComposite(this.getShell());
+	this._bookSearchDlgView.setSize("510", "300");
+	this._bookSearchDlgView.getHtmlElement().style.overflow = "auto";
+	this._bookSearchDlgView.getHtmlElement().innerHTML = this._createBookSearchView();
+	var sendPrefEmailBtnId = Dwt.getNextId();
+	this._bookSearchDialog = new ZmDialog({title:this.getMessage("AmazonZimlet_SearchBooksOnAmzn"), view:this._bookSearchDlgView, standardButtons:[DwtDialog.CANCEL_BUTTON], parent:this.getShell()});
+	this._setBookSearchDlgListeners();
+	this._bookSearchDialog.popup();
+	this._searchField.focus();
+};
+
+/**
+ *  Sets Search Dialog's listeners
+ */
+AmazonZimlet.prototype._setBookSearchDlgListeners = function() {
+	this._searchField = document.getElementById("amznZimlet_searchField");
+	var callback = AjxCallback.simpleClosure(this._searchFieldKeyHdlr, this);
+	Dwt.setHandler(this._searchField, DwtEvent.ONKEYUP, callback);
+
+	this._searchField.onclick = callback;
+
+	var btn = new DwtButton({parent:this.getShell()});
+	btn.setText(this.getMessage("AmazonZimlet_SearchBooks"));
+	btn.setImage("search");
+	btn.addSelectionListener(new AjxListener(this, this._doAmazonBookSearch));
+	document.getElementById("amznZimlet_searchButtonTD").appendChild(btn.getHtmlElement());
+};
+
+/**
+ * Creates an empty Book Search dialog view
+ */
+AmazonZimlet.prototype._createBookSearchView = function() {
+	var html = new Array();
+	var i = 0;
+	html[i++] = "<DIV id='amznZimlet_bookSearchDiv'>";
+	html[i++] = "<table  align=center class='amzn_table'><tr><td><input type=text id='amznZimlet_searchField'> </input></td><td id='amznZimlet_searchButtonTD'></td></tr></table>";
+	html[i++] = "</DIV>";
+	html[i++] = ["<DIV  id='",AmazonZimlet.ELEMENT_RESULTS_DIV_ID,"'>"].join("");
+	html[i++] = "</DIV>";
+	return html.join("");
+};
+
+/**
+ * Listens to Keyboard clicks and performs search when Enter-key is clicked
+ * @param ev
+ */
+AmazonZimlet.prototype._searchFieldKeyHdlr =
+function(ev) {
+	var event = ev || window.event;
+	if (event.keyCode == undefined) {
+		return;
+	}
+	if (event.keyCode != 13) {//if not enter key
+		return;
+	}
+	this._doAmazonBookSearch();
+};
+
+
+
+/**
+ * Callback for search.
+ *
+ */
+
+/**
+ * Callback for search.
+ *
+ * @param {string} resultsDiv Id of the div where the search result should be inserted into
+ * @param {object} results Results Object
+ */
+AmazonZimlet.prototype._searchCallback =
+function(resultsDiv, results) {
+	if (results && results.success) {
+		var result = AjxXmlDoc.createFromXml(results.text).toJSObject(true, false);
+		var items = result.Items;
+		if (items.Item) {
+			this._displayBooks(items.Item, resultsDiv);
+		} else {
+			try {
+				var message = items.Request.Errors.Error.Message.toString();
+				document.getElementById(resultsDiv).innerHTML = ["<b>", message, "</b>"].join("");
+			} catch(e) {
+				document.getElementById(resultsDiv).innerHTML = ["<b>",this.getMessage("AmazonZimlet_noResults"),"</b>"].join("");
+			}
+		}
+	}
+};
---
+++ Zimlet/src/zimlet/com_zimbra_amzn/generateAMZNSignature.jsp
@@ -0,0 +1,216 @@
+<%@ page language="java" import="java.io.UnsupportedEncodingException"%>
+
+<%@ page language="java" import="java.net.URLDecoder"%>
+<%@ page language="java" import="java.net.URLEncoder"%>
+
+<%@ page language="java" import="java.security.InvalidKeyException"%>
+<%@ page language="java" import="java.security.NoSuchAlgorithmException"%>
+
+<%@ page language="java" import="java.text.DateFormat"%>
+<%@ page language="java" import="java.text.SimpleDateFormat"%>
+
+<%@ page language="java" import="java.util.Calendar"%>
+<%@ page language="java" import="java.util.HashMap"%>
+<%@ page language="java" import="java.util.Iterator"%>
+<%@ page language="java" import="java.util.Map"%>
+<%@ page language="java" import="java.util.SortedMap"%>
+<%@ page language="java" import="java.util.TimeZone"%>
+<%@ page language="java" import="java.util.TreeMap"%>
+
+<%@ page language="java" import="javax.crypto.Mac"%>
+<%@ page language="java" import="javax.crypto.spec.SecretKeySpec"%>
+
+<%@ page language="java" import="org.apache.commons.codec.binary.Base64"%>
+
+<%--
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Zimlets
+ * Copyright (C) 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+// Copyright (C) Amazon (Original Java Format)
+//Author: Raja Rao DV (rrao@zimbra.com) Converted to JSP
+--%>
+<%@ page language="java" import="java.io.UnsupportedEncodingException"%>
+
+<%@ page language="java" import="java.net.URLDecoder"%>
+<%@ page language="java" import="java.net.URLEncoder"%>
+
+<%@ page language="java" import="java.security.InvalidKeyException"%>
+<%@ page language="java" import="java.security.NoSuchAlgorithmException"%>
+
+<%@ page language="java" import="java.text.DateFormat"%>
+<%@ page language="java" import="java.text.SimpleDateFormat"%>
+
+<%@ page language="java" import="java.util.Calendar"%>
+<%@ page language="java" import="java.util.HashMap"%>
+<%@ page language="java" import="java.util.Iterator"%>
+<%@ page language="java" import="java.util.Map"%>
+<%@ page language="java" import="java.util.SortedMap"%>
+<%@ page language="java" import="java.util.TimeZone"%>
+<%@ page language="java" import="java.util.TreeMap"%>
+
+<%@ page language="java" import="javax.crypto.Mac"%>
+<%@ page language="java" import="javax.crypto.spec.SecretKeySpec"%>
+
+<%@ page language="java" import="sun.misc.BASE64Encoder"%>
+
+<%--
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Zimlets
+ * Copyright (C) 2009, 2010 Zimbra, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+// Copyright (C) Amazon (Original Java Format)
+//Author: Raja Rao DV (rrao@zimbra.com) Converted to JSP
+--%>
+
+<%!String awsAccessKeyId = "AKIAJMLVLU3IPT6AP7SQ";%>
+<%!String awsSecretKey = "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";%>
+<%!String endpoint = "webservices.amazon.com";%>
+<%!String UTF8_CHARSET = "UTF-8";%>
+<%!String HMAC_SHA256_ALGORITHM = "HmacSHA256";%>
+<%!String REQUEST_URI = "/onca/xml";%>
+<%!String REQUEST_METHOD = "GET";%>
+<%!Mac mac = null;%>
+
+
+<%
+	SecretKeySpec secretKeySpec = null;
+	Mac mac = null;
+	String keywords = request.getParameter("Keywords");
+	Map<String, String> map = new HashMap<String, String>();
+	map.put("Service", "AWSECommerceService");
+	map.put("Operation", "ItemSearch");
+	map.put("SearchIndex", "Books");
+	map.put("ResponseGroup", "Medium");
+	map.put("Keywords", keywords);
+
+	//map.put("ItemId", "0679722769");
+
+	//map.put("Operation", "ItemLookup");
+	//map.put("ItemId", "0679722769");
+	//map.put("ResponseGroup", "ItemAttributes,Offers,Images,Reviews");
+	//map.put("Version", "2009-01-06");
+	byte[] secretyKeyBytes = null;
+	try {
+		secretyKeyBytes = awsSecretKey.getBytes(UTF8_CHARSET);
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	secretKeySpec = new SecretKeySpec(secretyKeyBytes,
+			HMAC_SHA256_ALGORITHM);
+	String resp = "";
+	try {
+		mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+		mac.init(secretKeySpec);
+	 	resp = sign(map, mac);
+
+	} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}catch (InvalidKeyException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+%>
+
+<%=resp%>
+
+
+<%!public String sign(Map<String, String> params, Mac mac) {
+		params.put("AWSAccessKeyId", awsAccessKeyId);
+		params.put("Timestamp", timestamp());
+		SortedMap<String, String> sortedParamMap = new TreeMap<String, String>(
+				params);
+		String canonicalQS = canonicalize(sortedParamMap);
+		String toSign = REQUEST_METHOD + "\n" + endpoint + "\n" + REQUEST_URI
+				+ "\n" + canonicalQS;
+
+		String hmac = hmac(toSign, mac);
+		String sig = percentEncodeRfc3986(hmac);
+		String url = "http://" + endpoint + REQUEST_URI + "?" + canonicalQS
+				+ "&Signature=" + sig;
+
+		return url;
+		//return sig;
+	}%>
+
+<%!private String hmac(String stringToSign, Mac mac) {
+		String signature = null;
+		byte[] data;
+		byte[] rawHmac;
+		try {
+			data = stringToSign.getBytes(UTF8_CHARSET);
+			rawHmac = mac.doFinal(data);
+			BASE64Encoder encoder = new BASE64Encoder();
+			signature = new String(encoder.encode(rawHmac));
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(UTF8_CHARSET + " is unsupported!", e);
+		} catch(Exception e) {
+			throw new RuntimeException(UTF8_CHARSET + " is unsupported!", e);
+	
+		}
+		return signature;
+	}%>
+
+<%!private String timestamp() {
+		String timestamp = null;
+		Calendar cal = Calendar.getInstance();
+		DateFormat dfm = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
+		timestamp = dfm.format(cal.getTime());
+		return timestamp;
+	}%>
+
+<%!private String canonicalize(SortedMap<String, String> sortedParamMap) {
+		if (sortedParamMap.isEmpty()) {
+			return "";
+		}
+
+		StringBuffer buffer = new StringBuffer();
+		Iterator<Map.Entry<String, String>> iter = sortedParamMap.entrySet()
+				.iterator();
+
+		while (iter.hasNext()) {
+			Map.Entry<String, String> kvpair = iter.next();
+			buffer.append(percentEncodeRfc3986(kvpair.getKey()));
+			buffer.append("=");
+			buffer.append(percentEncodeRfc3986(kvpair.getValue()));
+			if (iter.hasNext()) {
+				buffer.append("&");
+			}
+		}
+		String cannoical = buffer.toString();
+		return cannoical;
+	}%>
+
+<%!private String percentEncodeRfc3986(String s) {
+		String out;
+		try {
+			out = URLEncoder.encode(s, UTF8_CHARSET).replace("+", "%20")
+					.replace("*", "%2A").replace("%7E", "~");
+		} catch (UnsupportedEncodingException e) {
+			out = s;
+		}
+		return out;
+	}%>
