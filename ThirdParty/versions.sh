ee4bc107e8c5e7210b6dce9f0237ca24a4115857
Quanah Gibson-Mount <quanah@zimbra.com>
2010-04-27 19:41:08 +0000

    bug: none Nothing in beta now

--- ThirdParty/versions.sh
+++ ThirdParty/versions.sh
@@ -3,9 +3,9 @@ cyrus_patchlevel=3z
 cyrus_version=${cyrus_release}.${cyrus_patchlevel}
 cyrus_src=cyrus-sasl-${cyrus_version}
 
-heimdal_version=1.3.1
+heimdal_version=1.3.2
 openssl_version=0.9.8n
-curl_version=7.20.0
+curl_version=7.20.1
 xml2_version=2.6.32.1
 
 openssl_lib_dir=/opt/zimbra/openssl-${openssl_version}/lib
