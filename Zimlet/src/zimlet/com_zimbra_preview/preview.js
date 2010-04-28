0a3bd7926418285bfe53520622772ea81e42f9fc
 Unknown Perforce User <rsegu>
2010-04-28 11:19:15 +0000

    bug: 46632 Com_Zimbra_Preview zimlet to enable Preview view under Briefcase. Added PREVIEW_ENABLED setting to Briefcase APP.
    http://bugzilla.zimbra.com/show_bug.cgi?id=46632

---
+++ Zimlet/src/zimlet/com_zimbra_preview/preview.js
@@ -0,0 +1,11 @@
+function Com_Zimbra_Preview () {
+
+}
+
+Com_Zimbra_Preview.prototype = new ZmZimletBase();
+Com_Zimbra_Preview.prototype.constructor = Com_Zimbra_Preview;
+
+Com_Zimbra_Preview.prototype.init = function () {
+    appCtxt.set(ZmSetting.PREVIEW_ENABLED, true);
+    alert('ENABLED');
+};
