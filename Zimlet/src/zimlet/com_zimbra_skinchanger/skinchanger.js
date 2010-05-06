d006c39b57f99e727de2ca8df3b8207fe1544da0
 Unknown Perforce User <sposetti>
2010-05-06 04:45:54 +0000

    bug:46113 cleanup and i18n
    http://bugzilla.zimbra.com/show_bug.cgi?id=46113

--- Zimlet/src/zimlet/com_zimbra_skinchanger/skinchanger.js
+++ Zimlet/src/zimlet/com_zimbra_skinchanger/skinchanger.js
@@ -16,54 +16,123 @@
 /**
  * Constructor.
  */
-function com_zimbra_skinchanger() {
+function com_zimbra_skinchanger_HandlerObject() {
 }
 
-com_zimbra_skinchanger.prototype = new ZmZimletBase();
-com_zimbra_skinchanger.prototype.constructor = com_zimbra_skinchanger;
+com_zimbra_skinchanger_HandlerObject.prototype = new ZmZimletBase();
+com_zimbra_skinchanger_HandlerObject.prototype.constructor = com_zimbra_skinchanger_HandlerObject;
+
+/**
+ * Simplify handler object
+ *
+ */
+var SkinChangerZimlet = com_zimbra_skinchanger_HandlerObject;
 
 /**
  * Defines the "enable zimlet" user property.
  */
-com_zimbra_skinchanger.USER_PROP_ENABLE_ZIMLET = "turnONSkinChangerZimletNew";
+SkinChangerZimlet.USER_PROP_ENABLE_ZIMLET = "turnONSkinChangerZimletNew";
 /**
  * Defines the "frequency" user property.
  */
-com_zimbra_skinchanger.USER_PROP_FREQUENCY = "skinc_selectedFreq";
+SkinChangerZimlet.USER_PROP_FREQUENCY = "skinc_selectedFreq";
 /**
  * Defines the "changed date" user property.
  */
-com_zimbra_skinchanger.USER_PROP_CHANGED_DATE = "skinc_skinWasChangedOnDate";
+SkinChangerZimlet.USER_PROP_CHANGED_DATE = "skinc_skinWasChangedOnDate";
 
-com_zimbra_skinchanger.prototype.init =
+/**
+ * Defines the "frequency" element id.
+ */
+SkinChangerZimlet.ELEMENT_ID_FREQUENCY = "skinc_skinUpdateFrequencyList";
+/**
+ * Defines the "enable" element id.
+ */
+SkinChangerZimlet.ELEMENT_ID_ENABLE = "turnONSkinChangerZimletNew_chkbx";
+
+/**
+ * Defines the index to the everyday frequency key.
+ */
+SkinChangerZimlet.FREQUENCY_IDX_EVERYDAY = 7;
+
+/**
+ * Initializes the zimlet.
+ * 
+ */
+SkinChangerZimlet.prototype.init =
 function() {
-	this.turnONSkinChangerZimletNew = this.getUserProperty(com_zimbra_skinchanger.USER_PROP_ENABLE_ZIMLET) == "true";
+	this.turnONSkinChangerZimletNew = this.getUserProperty(SkinChangerZimlet.USER_PROP_ENABLE_ZIMLET) == "true";
 	if(!this.turnONSkinChangerZimletNew)
 		return;
 
-	this.skinc_selectedFreq = this.getUserProperty(com_zimbra_skinchanger.USER_PROP_FREQUENCY);
-	this.skinc_skinWasChangedOnDate = this.getUserProperty(com_zimbra_skinchanger.USER_PROP_CHANGED_DATE);
+	this.skinc_selectedFreq = this.getUserProperty(SkinChangerZimlet.USER_PROP_FREQUENCY);
+	this.skinc_skinWasChangedOnDate = this.getUserProperty(SkinChangerZimlet.USER_PROP_CHANGED_DATE);
 
-	var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	this._day = weekdays[new Date().getDay()];
-	if (this.skinc_skinWasChangedOnDate == this._formatChangedOnDate())//change only once a day
+	// index frequency list to day keys...do NOT i18n the keys for backwards compat
+	this._frequencyDaysList = [
+	                	["Sunday", this.getMessage("SkinChangerZimlet_weekday_sunday")], // idx=0
+	                	["Monday", this.getMessage("SkinChangerZimlet_weekday_monday")], // idx=1
+	                	["Tuesday", this.getMessage("SkinChangerZimlet_weekday_tuesday")], // idx=2
+	                	["Wednesday", this.getMessage("SkinChangerZimlet_weekday_wednesday")], // idx=3
+	                	["Thursday", this.getMessage("SkinChangerZimlet_weekday_thursday")], // idx=4
+	                	["Friday", this.getMessage("SkinChangerZimlet_weekday_friday")], // idx=5
+	                	["Saturday", this.getMessage("SkinChangerZimlet_weekday_saturday")], // idx=6
+	                	["Everyday", this.getMessage("SkinChangerZimlet_weekday_everyday")], // idx=7
+	              ];
+	
+	var todayDay = this._frequencyDaysList[new Date().getDay()][0];
+	// was the skin already changed today? if not, just return
+	if (this.skinc_skinWasChangedOnDate == this._formatChangedOnDate())
 		return;
 
-	if (this.skinc_selectedFreq != "Everyday" && this.skinc_selectedFreq != this._day)
+	// if frequency is not everyday or today is not the day to update, just return
+	if (this.skinc_selectedFreq != this._frequencyList[SkinChangerZimlet.FREQUENCY_IDX_EVERYDAY][0] && this.skinc_selectedFreq != todayDay)
 		return;
-
-
-	this._loadAvailableSkins(new AjxCallback(this, this._handleResponseLoadAvailableSkins));
-
+		
+	this._loadAndUpdateSkin();
 };
 
-com_zimbra_skinchanger.prototype._loadAvailableSkins =
-function(callback) {
+/**
+ * Called by framework on double-click.
+ * 
+ */
+SkinChangerZimlet.prototype.doubleClicked = function() {
+	this.singleClicked();
+};
+
+/**
+ * Called by framework on single-click.
+ * 
+ */
+SkinChangerZimlet.prototype.singleClicked = function() {
+	this._showChangerDialog();
+};
+
+/**
+ * Loads and updates the skin.
+ * 
+ */
+SkinChangerZimlet.prototype._loadAndUpdateSkin =
+function() {
 	var soapDoc = AjxSoapDoc.create("GetAvailableSkinsRequest", "urn:zimbraAccount");
-	appCtxt.getAppController().sendRequest({soapDoc:soapDoc, asyncMode:true, callback:callback});
+	
+	var loadAndUpdateCallback = new AjxCallback(this, this._handleResponseLoadAndUpdateSkin);
+	
+	var params = {
+			soapDoc		: soapDoc,
+			asyncMode	: true,
+			callback	: loadAndUpdateCallback
+			};
+	
+	appCtxt.getAppController().sendRequest(params);
 };
 
-com_zimbra_skinchanger.prototype._handleResponseLoadAvailableSkins =
+/**
+ * Handles the load skins response.
+ * 
+ * @param	{hash}	response		the response
+ */
+SkinChangerZimlet.prototype._handleResponseLoadAndUpdateSkin =
 function(response) {
 	this._availableSkins = [];
 	var resp = response.getResponse().GetAvailableSkinsResponse;
@@ -74,40 +143,30 @@ function(response) {
 		}
 	}
 
-	this._updateSkin();
-};
-
-com_zimbra_skinchanger.prototype._updateSkin =
-function() {
 	var randomnumber = Math.floor(Math.random() * this._availableSkins.length);
 	var soapDoc = AjxSoapDoc.create("ModifyPrefsRequest", "urn:zimbraAccount");
 	var node = soapDoc.set("pref", this._availableSkins[randomnumber]);
 	node.setAttribute("name", "zimbraPrefSkin");
 
-	var respCallback = new AjxCallback(this, this._handleSkinChangeResponse);
+	var respCallback = new AjxCallback(this, this._handleSkinUpdateResponse);
 	appCtxt.getAppController().sendRequest({soapDoc:soapDoc, asyncMode:true, callback:respCallback});
 };
 
-com_zimbra_skinchanger.prototype._formatChangedOnDate =
+/**
+ * Handles the skin update response.
+ * 
+ * @see		_handleResponseLoadAndUpdateSkin
+ */
+SkinChangerZimlet.prototype._handleSkinUpdateResponse =
 function() {
-	var dArry = (new Date()).toString().split(" ");
-	return dArry[0] + " " + dArry[1] + " " + dArry[2] + " " + dArry[3];
-
-};
-com_zimbra_skinchanger.prototype._handleSkinChangeResponse =
-function() {
-	this.setUserProperty(com_zimbra_skinchanger.USER_PROP_CHANGED_DATE, this._formatChangedOnDate(), true);
+	this.setUserProperty(SkinChangerZimlet.USER_PROP_CHANGED_DATE, this._formatChangedOnDate(), true);
 };
 
-com_zimbra_skinchanger.prototype.doubleClicked = function() {
-	this.singleClicked();
-};
-
-com_zimbra_skinchanger.prototype.singleClicked = function() {
-	this.showPrefDialog();
-};
-
-com_zimbra_skinchanger.prototype.showPrefDialog =
+/**
+ * Shows the changer dialog.
+ * 
+ */
+SkinChangerZimlet.prototype._showChangerDialog =
 function() {
 	//if zimlet dialog already exists...
 	if (this.pbDialog) {
@@ -115,60 +174,73 @@ function() {
 		return;
 	}
 	this.pView = new DwtComposite(this.getShell());
-	this.pView.getHtmlElement().innerHTML = this.createPrefView();
+	this.pView.getHtmlElement().innerHTML = this._createChangerView();
 
-	if (this.getUserProperty(com_zimbra_skinchanger.USER_PROP_ENABLE_ZIMLET) == "true") {
-		document.getElementById("turnONSkinChangerZimletNew_chkbx").checked = true;
+	if (this.getUserProperty(SkinChangerZimlet.USER_PROP_ENABLE_ZIMLET) == "true") {
+		document.getElementById(SkinChangerZimlet.ELEMENT_ID_ENABLE).checked = true;
 	}
 
 	var dialog_args = {
-			title	: "'Skin Changer' Zimlet Preferences",
+			title	: this.getMessage("SkinChangerZimlet_dialog_title"),
 			view 	: this.pView,
 			parent	: this.getShell(),
-			standardButtons : [DwtDialog.OK_BUTTON]
+			standardButtons : [DwtDialog.CANCEL_BUTTON, DwtDialog.OK_BUTTON]
 		};
 	
 	this.pbDialog = new ZmDialog(dialog_args);
 	this.pbDialog.setButtonListener(DwtDialog.OK_BUTTON, new AjxListener(this, this._okBtnListner));
-	this._updateSelectedItem();
+	this._updateSelectedFrequency();
 	this.pbDialog.popup();
 
 };
 
-com_zimbra_skinchanger.prototype.createPrefView =
+/**
+ * Creates the changer dialog view.
+ * 
+ * @see	_showChangerDialog
+ */
+SkinChangerZimlet.prototype._createChangerView =
 function() {
 	var html = new Array();
 	var i = 0;
 	html[i++] = "<DIV>";
-	html[i++] = "Please Select the frequency or day on which you want the skin to be changed";
+	html[i++] = this.getMessage("SkinChangerZimlet_selectDay");
 	html[i++] = "</DIV>";
 	html[i++] = "<DIV>";
-	html[i++] = "<select id='skinc_availSkinList'>";
-	var days = [ "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Everyday"];
-	var len = days.length;
+	html[i++] = "<select id='";
+	html[i++] = SkinChangerZimlet.ELEMENT_ID_FREQUENCY;
+	html[i++] = "'>";
+	var len = this._frequencyDaysList.length;
 	for (var j = 0; j < len; j++) {
-		html[i++] = "<option value=" + days[j] + ">" + days[j] + "</option>";
+		html[i++] = "<option value=" + this._frequencyDaysList[j][0] + ">" + this._frequencyDaysList[j][1] + "</option>";
 	}
 	html[i++] = "</select>";
 	html[i++] = "</DIV>";
 	html[i++] = "<BR>";
-	html[i++] = "<BR>";
 	if (this.skinc_skinWasChangedOnDate != "") {
 		html[i++] = "<DIV>";
-		html[i++] = "Note: Skin was last changed on: " + this.skinc_skinWasChangedOnDate;
+		var lastChangeStr = AjxMessageFormat.format(this.getMessage("SkinChangerZimlet_lastChange"), this.skinc_skinWasChangedOnDate);
+		html[i++] = lastChangeStr;
 		html[i++] = "</DIV>";
 	}
 	html[i++] = "<BR>";
 	html[i++] = "<DIV>";
-	html[i++] = "<input id='turnONSkinChangerZimletNew_chkbx'  type='checkbox'/>Enable 'Skin Changer' Zimlet (Changing this would refresh browser)";
+	html[i++] = "<input id='";
+	html[i++] = SkinChangerZimlet.ELEMENT_ID_ENABLE;
+	html[i++] = "' type='checkbox'/>";
+	html[i++] = this.getMessage("SkinChangerZimlet_enable");
 	html[i++] = "</DIV>";
+	
 	return html.join("");
-
 };
 
-com_zimbra_skinchanger.prototype._updateSelectedItem =
+/**
+ * Updates the selected frequency list.
+ * 
+ */
+SkinChangerZimlet.prototype._updateSelectedFrequency =
 function() {
-	var optn = document.getElementById("skinc_availSkinList").options;
+	var optn = document.getElementById(SkinChangerZimlet.ELEMENT_ID_FREQUENCY).options;
 	for (var i = 0; i < optn.length; i++) {
 		if (optn[i].value == this.skinc_selectedFreq) {
 			optn[i].selected = true;
@@ -177,22 +249,39 @@ function() {
 	}
 };
 
-com_zimbra_skinchanger.prototype._okBtnListner =
+/**
+ * Formats changed-on date.
+ * 
+ * @return	{string}	the changed-on date
+ */
+SkinChangerZimlet.prototype._formatChangedOnDate =
+	function() {
+		var dArry = (new Date()).toString().split(" ");
+		return dArry[0] + " " + dArry[1] + " " + dArry[2] + " " + dArry[3];
+
+	};
+
+/**
+ * Handles the OK button.
+ * 
+ * @see	_showChangerDialog
+ */
+SkinChangerZimlet.prototype._okBtnListner =
 function() {
 	this._reloadRequired = false;
-	if (document.getElementById("turnONSkinChangerZimletNew_chkbx").checked) {
+	if (document.getElementById(SkinChangerZimlet.ELEMENT_ID_ENABLE).checked) {
 		if (!this.turnONSkinChangerZimletNew) {
 			this._reloadRequired = true;
 		}
-		this.setUserProperty(com_zimbra_skinchanger.USER_PROP_ENABLE_ZIMLET, "true", true);
+		this.setUserProperty(SkinChangerZimlet.USER_PROP_ENABLE_ZIMLET, "true", true);
 	} else {
-		this.setUserProperty(com_zimbra_skinchanger.USER_PROP_ENABLE_ZIMLET, "false", true);
+		this.setUserProperty(SkinChangerZimlet.USER_PROP_ENABLE_ZIMLET, "false", true);
 		if (this.turnONSkinChangerZimletNew)
 			this._reloadRequired = true;
 	}
-	var lst = document.getElementById("skinc_availSkinList");
+	var lst = document.getElementById(SkinChangerZimlet.ELEMENT_ID_FREQUENCY);
 	if (lst.value != this.skinc_selectedFreq) {
-		this.setUserProperty(com_zimbra_skinchanger.USER_PROP_FREQUENCY, lst.value, true);
+		this.setUserProperty(SkinChangerZimlet.USER_PROP_FREQUENCY, lst.value, true);
 		this._reloadRequired = true;
 	}
 
@@ -200,12 +289,18 @@ function() {
 
 	if (this._reloadRequired) {
 		var transitions = [ ZmToast.FADE_IN, ZmToast.PAUSE, ZmToast.PAUSE, ZmToast.FADE_OUT ];
-		appCtxt.getAppController().setStatusMsg("Browser will be refreshed for changes to take effect..", ZmStatusView.LEVEL_INFO, null, transitions);
+		var browserRefreshMsg = this.getMessage("SkinChangerZimlet_refreshBrowser");
+		appCtxt.getAppController().setStatusMsg(browserRefreshMsg,
+				ZmStatusView.LEVEL_INFO, null, transitions);
 		setTimeout(AjxCallback.simpleClosure(this._refreshBrowser, this), 2000);
 	}
 };
 
-com_zimbra_skinchanger.prototype._refreshBrowser =
+/**
+ * Refreshes the browser.
+ * 
+ */
+SkinChangerZimlet.prototype._refreshBrowser =
 function() {
 	window.onbeforeunload = null;
 	var url = AjxUtil.formatUrl({});
