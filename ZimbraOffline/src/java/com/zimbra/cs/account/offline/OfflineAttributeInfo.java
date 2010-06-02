d3aec18303306a9eaf4b813f61c5c6ed32d6bb53
Vishal Mahajan <vmahajan@zimbra.com>
2010-06-02 04:54:08 +0000

    bug: 46266
    - Introduced a "zimbra_class_attrmanager" localconfig property.
    - Introduced OfflineAttributeManager and OfflineAttributeInfo classes.
    - Added "attrsToModify" as an arg to AttributeInfo.checkValue().
    - Overridden checkValue() in OfflineAttributeInfo to catch and ignore
    invalid-value exception for attributes having enum values, and replace the bad
    value with the default attribute value in the "attrsToModify" map.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=46266

---
+++ ZimbraOffline/src/java/com/zimbra/cs/account/offline/OfflineAttributeInfo.java
@@ -0,0 +1,58 @@
+package com.zimbra.cs.account.offline;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.SystemUtil;
+import com.zimbra.cs.account.AccountServiceException;
+import com.zimbra.cs.account.AttributeCallback;
+import com.zimbra.cs.account.AttributeCardinality;
+import com.zimbra.cs.account.AttributeClass;
+import com.zimbra.cs.account.AttributeFlag;
+import com.zimbra.cs.account.AttributeInfo;
+import com.zimbra.cs.account.AttributeOrder;
+import com.zimbra.cs.account.AttributeServerType;
+import com.zimbra.cs.account.AttributeType;
+import com.zimbra.cs.offline.OfflineLog;
+import com.zimbra.cs.util.BuildInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author vmahajan
+ */
+public class OfflineAttributeInfo extends AttributeInfo {
+
+    public OfflineAttributeInfo(String attrName, int id, String parentId, int groupId, AttributeCallback callback,
+                                AttributeType type, AttributeOrder order, String value, boolean immutable, String min,
+                                String max, AttributeCardinality cardinality, Set<AttributeClass> requiredIn, 
+                                Set<AttributeClass> optionalIn, Set<AttributeFlag> flags, List<String> globalConfigValues,
+                                List<String> defaultCOSValues, List<String> globalConfigValuesUpgrade,
+                                List<String> defaultCOSValuesUpgrade, String description, List<AttributeServerType> requiresRestart,
+                                BuildInfo.Version since, BuildInfo.Version deprecatedSince) {
+        super(attrName, id, parentId, groupId, callback, type, order, value, immutable, min, max, cardinality,
+              requiredIn, optionalIn, flags, globalConfigValues, defaultCOSValues, globalConfigValuesUpgrade,
+              defaultCOSValuesUpgrade, description, requiresRestart, since, deprecatedSince);
+    }
+
+    @Override
+    protected void checkValue(String value, Map attrsToModify) throws ServiceException {
+        try {
+            super.checkValue(value, attrsToModify);
+        } catch (AccountServiceException e) {
+            if (AccountServiceException.INVALID_ATTR_VALUE.equals(e.getCode()) && mType == AttributeType.TYPE_ENUM) {
+                // use the default value and ignore exception
+                if (mDefaultCOSValues != null && !mDefaultCOSValues.isEmpty()) {
+                    if (mDefaultCOSValues.size() == 1)
+                        attrsToModify.put(mName, mDefaultCOSValues.get(0));
+                    else
+                        attrsToModify.put(mName, mDefaultCOSValues.toArray());
+                    OfflineLog.offline.warn(SystemUtil.getStackTrace(e));
+                    OfflineLog.offline.warn("Using default value '" + mDefaultCOSValues + "' for attribute '" + mName + "'");
+                    return;
+                }
+            }
+            throw e;
+        }
+    }
+}
---
+++ ZimbraOffline/src/java/com/zimbra/cs/account/offline/OfflineAttributeManager.java
@@ -0,0 +1,43 @@
+package com.zimbra.cs.account.offline;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.AttributeCallback;
+import com.zimbra.cs.account.AttributeCardinality;
+import com.zimbra.cs.account.AttributeClass;
+import com.zimbra.cs.account.AttributeFlag;
+import com.zimbra.cs.account.AttributeInfo;
+import com.zimbra.cs.account.AttributeManager;
+import com.zimbra.cs.account.AttributeOrder;
+import com.zimbra.cs.account.AttributeServerType;
+import com.zimbra.cs.account.AttributeType;
+import com.zimbra.cs.util.BuildInfo;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author vmahajan
+ */
+public class OfflineAttributeManager extends AttributeManager {
+
+    public OfflineAttributeManager(String dir) throws ServiceException {
+        super(dir);
+    }
+
+    @Override
+    protected AttributeInfo createAttributeInfo(String name, int id, String parentOid, int groupId,
+                                                AttributeCallback callback, AttributeType type, AttributeOrder order,
+                                                String value, boolean immutable, String min, String max,
+                                                AttributeCardinality cardinality, Set<AttributeClass> requiredIn,
+                                                Set<AttributeClass> optionalIn, Set<AttributeFlag> flags,
+                                                List<String> globalConfigValues, List<String> defaultCOSValues,
+                                                List<String> globalConfigValuesUpgrade, List<String> defaultCOSValuesUpgrade,
+                                                String description, List<AttributeServerType> requiresRestart,
+                                                BuildInfo.Version sinceVer, BuildInfo.Version deprecatedSinceVer) {
+        return new OfflineAttributeInfo(
+                name, id, parentOid, groupId, callback, type, order, value, immutable, min, max,
+                cardinality, requiredIn, optionalIn, flags, globalConfigValues, defaultCOSValues,
+                globalConfigValuesUpgrade, defaultCOSValuesUpgrade,
+                description, requiresRestart, sinceVer, deprecatedSinceVer);
+    }    
+}
