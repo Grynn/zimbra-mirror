d4180cc9679f6742e80f1197b437e15b60a7260d
Girish Bhamare <girish@zimbra.com>
2010-07-09 12:07:47 +0000

    bug:none Added 8 missing tests for clearing/deleting html and plain text signature

--- ZimbraSelenium/src/java/projects/zcs/tests/preferences/signatures/BasicSignatureTests.java
+++ ZimbraSelenium/src/java/projects/zcs/tests/preferences/signatures/BasicSignatureTests.java
@@ -20,7 +20,15 @@ public class BasicSignatureTests extends CommonTest {
 	public Object[][] createData(Method method) throws ServiceException {
 		String test = method.getName();
 		if (test.equals("createTextSignature")
-				|| test.equals("editTextSignature")) {
+				|| test.equals("editTextSignature")
+				|| test.equals("clearTextSignatureWoSave")
+				|| test.equals("clearHtmlSignatureWoSave")
+				|| test.equals("clearTextSignatureWithSave")
+				|| test.equals("clearHtmlSignatureWithSave")
+				|| test.equals("deleteTextSignatureWoSave")
+				|| test.equals("deleteHtmlSignatureWoSave")
+				|| test.equals("deleteTextSignatureWithSave")
+				|| test.equals("deleteHtmlSignatureWithSave")) {
 			return new Object[][] { { getLocalizedData_NoSpecialChar(),
 					getLocalizedData_NoSpecialChar() } };
 		} else {
@@ -92,6 +100,266 @@ public class BasicSignatureTests extends CommonTest {
 		Thread.sleep(1000);
 		obj.zToastAlertMessage.zAlertMsgExists(localize(locator.optionsSaved),
 				"Edited Signature should be saved");
+
+		needReset = false;
+	}
+
+	/**
+	 * This test case is to create text signature ClearTextSignatureWithoutClick
+	 * on Save
+	 * 
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void clearTextSignatureWoSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"TEXT");
+		Thread.sleep(1000);
+
+		obj.zButton.zClick(localize(locator.clear));
+		obj.zEditor.zExists(signatureBody);
+
+		needReset = false;
+	}
+
+	/**
+	 * This test case is to create text signature
+	 * ClearHtmlSignatureWithoutClickonSave
+	 * 
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void clearHtmlSignatureWoSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"HTML");
+		Thread.sleep(1000);
+
+		obj.zButton.zClick(localize(locator.clear));
+		obj.zEditor.zExists(signatureBody);
+
+		needReset = false;
+	}
+
+	/**
+	 * This test case is to create text signature
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void clearTextSignatureWithSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(page.zABCompose.zPreferencesSaveIconBtn);
+		Thread.sleep(1000);
+		obj.zToastAlertMessage.zAlertMsgExists(localize(locator.optionsSaved),
+				"Signature should be saved");
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		// obj.zMenuItem.zClick(signatureName);
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		obj.zButton.zClick(localize(locator.clear));
+		obj.zEditor.zExists(signatureBody);
+		obj.zEditField.zExists(signatureName);
+
+		needReset = false;
+	}
+
+	/**
+	 * This test case is to create text signature
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void clearHtmlSignatureWithSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(page.zABCompose.zPreferencesSaveIconBtn);
+		Thread.sleep(1000);
+		obj.zToastAlertMessage.zAlertMsgExists(localize(locator.optionsSaved),
+				"Signature should be saved");
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		obj.zButton.zClick(localize(locator.clear));
+		obj.zEditor.zExists(signatureBody);
+		obj.zEditField.zExists(signatureName);
+
+		needReset = false;
+	}
+
+	/**
+	 * Test Case: Delete Text signature without saving.
+	 * 
+	 * @param signatureName
+	 * @param signatureBody
+	 * @throws Exception
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void deleteTextSignatureWoSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+		String signatureName1 = getLocalizedData_NoSpecialChar();
+		String signatureBody1 = getLocalizedData_NoSpecialChar();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		page.zSignaturePref.zCreateSignature(signatureName1, signatureBody1,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		selenium
+				.clickAt(
+						"xpath=//td[contains(@class,'ZOptionsField')]//table//tr/td[contains(@id,'_title') and contains(text(),'"
+								+ localize(locator.del) + "')]", "");
+		obj.zEditField.zNotExists(signatureName);
+
+		needReset = false;
+	}
+
+	/**
+	 * Test Case: Delete Text signature without saving.
+	 * 
+	 * @param signatureName
+	 * @param signatureBody
+	 * @throws Exception
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void deleteHtmlSignatureWoSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+		String signatureName1 = getLocalizedData_NoSpecialChar();
+		String signatureBody1 = getLocalizedData_NoSpecialChar();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"HTML");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		page.zSignaturePref.zCreateSignature(signatureName1, signatureBody1,
+				"HTML");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		selenium
+				.clickAt(
+						"xpath=//td[contains(@class,'ZOptionsField')]//table//tr/td[contains(@id,'_title') and contains(text(),'"
+								+ localize(locator.del) + "')]", "");
+		obj.zEditor.zExists(signatureBody);
+		obj.zEditField.zNotExists(signatureName);
+
+		needReset = false;
+	}
+
+	/**
+	 * Test Case:deleteTextSignatureWithSave
+	 * 
+	 * @param signatureName
+	 * @param signatureBody
+	 * @throws Exception
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void deleteTextSignatureWithSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		String signatureName1 = getLocalizedData_NoSpecialChar();
+		String signatureBody1 = getLocalizedData_NoSpecialChar();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		page.zSignaturePref.zCreateSignature(signatureName1, signatureBody1,
+				"TEXT");
+		Thread.sleep(1000);
+		obj.zButton.zClick(page.zABCompose.zPreferencesSaveIconBtn);
+		Thread.sleep(1000);
+		obj.zToastAlertMessage.zAlertMsgExists(localize(locator.optionsSaved),
+				"Signature should be saved");
+		Thread.sleep(500);
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		selenium
+				.clickAt(
+						"xpath=//td[contains(@class,'ZOptionsField')]//table//tr/td[contains(@id,'_title') and contains(text(),'"
+								+ localize(locator.del) + "')]", "");
+		obj.zEditor.zExists(signatureBody);
+		obj.zEditField.zNotExists(signatureName);
+
+		needReset = false;
+	}
+
+	/**
+	 * Test Case:deleteTextSignatureWithSave
+	 * 
+	 * @param signatureName
+	 * @param signatureBody
+	 * @throws Exception
+	 * @author Girish
+	 */
+	@Test(dataProvider = "SigPrefDataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void deleteHtmlSignatureWithSave(String signatureName,
+			String signatureBody) throws Exception {
+		if (isExecutionARetry)
+			handleRetry();
+
+		String signatureName1 = getLocalizedData_NoSpecialChar();
+		String signatureBody1 = getLocalizedData_NoSpecialChar();
+
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		page.zSignaturePref.zCreateSignature(signatureName, signatureBody,
+				"HTML");
+		Thread.sleep(1000);
+		obj.zButton.zClick(localize(locator.addSignature));
+		page.zSignaturePref.zCreateSignature(signatureName1, signatureBody1,
+				"HTML");
+		Thread.sleep(1000);
+		obj.zButton.zClick(page.zABCompose.zPreferencesSaveIconBtn);
+		Thread.sleep(1000);
+		obj.zToastAlertMessage.zAlertMsgExists(localize(locator.optionsSaved),
+				"Signature should be saved");
+		Thread.sleep(500);
+		page.zSignaturePref.zNavigateToPreferenceSignature();
+		selenium.clickAt("xpath=//tr[contains(@id,'DWT')]/td[contains(text(),'"
+				+ signatureName + "')]", "");
+		selenium
+				.clickAt(
+						"xpath=//td[contains(@class,'ZOptionsField')]//table//tr/td[contains(@id,'_title') and contains(text(),'"
+								+ localize(locator.del) + "')]", "");
+		obj.zEditor.zExists(signatureBody);
+		obj.zEditField.zNotExists(signatureName);
 
 		needReset = false;
 	}
