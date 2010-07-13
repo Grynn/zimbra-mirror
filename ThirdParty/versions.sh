de4841058427f8562738d435e3a89b4248488cb9
Quanah Gibson-Mount <quanah@zimbra.com>
2010-07-13 02:09:03 +0000

    bug: 44634,45704,48818,48820,48861,48862 New versions
    http://bugzilla.zimbra.com/show_bug.cgi?id=44634
    http://bugzilla.zimbra.com/show_bug.cgi?id=45704
    http://bugzilla.zimbra.com/show_bug.cgi?id=48818
    http://bugzilla.zimbra.com/show_bug.cgi?id=48820
    http://bugzilla.zimbra.com/show_bug.cgi?id=48861
    http://bugzilla.zimbra.com/show_bug.cgi?id=48862

--- ThirdParty/versions.sh
+++ ThirdParty/versions.sh
@@ -3,10 +3,10 @@ cyrus_patchlevel=3z
 cyrus_version=${cyrus_release}.${cyrus_patchlevel}
 cyrus_src=cyrus-sasl-${cyrus_version}
 
-heimdal_version=1.3.2
-openssl_version=0.9.8n
-curl_version=7.20.1
-xml2_version=2.6.32.1
+heimdal_version=1.3.3
+openssl_version=1.0.0a
+curl_version=7.21.0
+xml2_version=2.7.7
 
 openssl_lib_dir=/opt/zimbra/openssl-${openssl_version}/lib
 heimdal_lib_dir=/opt/zimbra/heimdal-${heimdal_version}/lib
