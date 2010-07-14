74622208199674b4a322843493a2c59bb587d64c
Jeff Flanigan <jflanigan@zimbra.com>
2010-07-14 20:05:58 +0000

    bug: 48233
    Fix case 3, save attachment to shared briefcase in same account.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48233

--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineSaveDocument.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineSaveDocument.java
@@ -26,11 +26,12 @@ public class OfflineSaveDocument extends OfflineDocumentHandlers.SaveDocument {
     @Override
     protected Element proxyRequest(Element request, Map<String, Object> context, ItemId iidRequested, ItemId iidResolved)
         throws ServiceException {
-        Element eUpload = request.getElement(MailConstants.E_DOC).getElement(MailConstants.E_UPLOAD);
-        String id = eUpload.getAttribute(MailConstants.A_ID);
-        String acctId = iidRequested.getAccountId();        
-        eUpload.addAttribute(MailConstants.A_ID, OfflineDocumentHandlers.uploadOfflineDocument(id, acctId)); 
-        
+        Element eUpload = request.getElement(MailConstants.E_DOC).getOptionalElement(MailConstants.E_UPLOAD);
+        if (eUpload != null) {
+            String id = eUpload.getAttribute(MailConstants.A_ID);
+            String acctId = iidRequested.getAccountId();        
+            eUpload.addAttribute(MailConstants.A_ID, OfflineDocumentHandlers.uploadOfflineDocument(id, acctId)); 
+        }
         return super.proxyRequest(request, context, iidRequested, iidResolved);
     }
 }
