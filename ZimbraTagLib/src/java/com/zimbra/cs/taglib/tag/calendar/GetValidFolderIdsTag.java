a92fab34dee7ec3854a4dea1bcdcb15f06384dc9
Komal Kakani <kkakani@zimbra.com>
2010-07-15 19:51:34 +0000

    Integrated the following change from GNR to main Change 242896 by kkakani@komalk_macGNR on 2010/07/15 12:43:06
            bug:48819
            -The folderids sent to getApptSummaries() consisted of all the 
            checked calendars in the UI, which can be a local folder or a 
            mountpoint. Mountpoints can be invalid, in case the user does not 
            exist or has deleted the shared folder.
            -Make sure that the folderids sent to fetch the appointments from 
            are all valid folder ids. Remove the invalid mountpoints before 
            sending the request.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=48819

---
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/calendar/GetValidFolderIdsTag.java
@@ -0,0 +1,44 @@
+
+package com.zimbra.cs.taglib.tag.calendar;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.taglib.bean.ZMailboxBean;
+import com.zimbra.cs.taglib.tag.ZimbraSimpleTag;
+import com.zimbra.cs.zclient.ZMailbox;
+
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspTagException;
+import java.io.IOException;
+
+
+public class GetValidFolderIdsTag extends ZimbraSimpleTag {
+    private String mVar;
+    private String mVarException;
+    private String mFolderId;
+    private ZMailboxBean mMailbox;
+
+    public void setVar(String var) { this.mVar = var; }
+    public void setVarexception(String varException) { this.mVarException = varException; }
+    public void setFolderid(String folderId) { this.mFolderId = folderId; }
+    public void setBox(ZMailboxBean mailbox) { this.mMailbox = mailbox; }
+    
+    public void doTag() throws JspException, IOException {
+        JspContext jctxt = getJspContext();
+        try {
+            ZMailbox mbox = mMailbox != null ? mMailbox.getMailbox() :  getMailbox();
+            String result = mbox.getValFolderIds(mFolderId);
+            jctxt.setAttribute(mVar, result,  PageContext.PAGE_SCOPE);
+        } catch (ServiceException e){
+            if (mVarException != null) {
+                jctxt.setAttribute(mVarException, e,  PageContext.PAGE_SCOPE);
+                jctxt.setAttribute(mVar, "",  PageContext.PAGE_SCOPE);
+            } else {
+                throw new JspTagException(e);
+            }
+        }
+
+    }
+}
