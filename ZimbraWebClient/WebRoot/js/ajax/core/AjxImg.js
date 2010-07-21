b5f2210c6b840564b37fdebcc143cdba7c309c46
 Unknown Perforce User <aclark>
2010-07-21 21:56:53 +0000

    bug: 14962 Converted tag display to generated images instead of static images; brought choice of tag colors into sync with folder color choices -- was missing pink and gray.
    http://bugzilla.zimbra.com/show_bug.cgi?id=14962

--- ZimbraWebClient/WebRoot/js/ajax/core/AjxImg.js
+++ ZimbraWebClient/WebRoot/js/ajax/core/AjxImg.js
@@ -47,7 +47,8 @@ AjxImg.RE_COLOR = /^(.*?),color=(.*)$/;
  */
 AjxImg.setImage =
 function(parentEl, imageName, useParentEl, _disabled) {
-	var color, m = imageName.match(AjxImg.RE_COLOR);
+	var origImageName = imageName;
+    var color, m = imageName.match(AjxImg.RE_COLOR);
 	if (m) {
 		imageName = m && m[1];
 		color = m && m[2];
@@ -65,64 +66,7 @@ function(parentEl, imageName, useParentEl, _disabled) {
 		color = (color.match(/^\d$/) ? ZmOrganizer.COLOR_VALUES[color] : color) ||
 				ZmOrganizer.COLOR_VALUES[ZmOrganizer.ORG_DEFAULT_COLOR];
 
-		var overlay = AjxImgData[overlayName], mask = AjxImgData[maskName];
-		if (AjxEnv.isIE) {
-            var clip = "";
-			var size = [
-				"width:",overlay.w,";",
-				"height:",overlay.h,";"
-			].join("");
-			var position = [
-				"top:",mask.t,";",
-				"left:",mask.l,";"
-			].join("");
-            if(typeof document.documentMode != 'undefined'){ //IE8 is the first one to define this. IE8 can lie when in compat mode, so we need to really know it's it.
-                clip = [
-                    'clip:rect(',
-                    (-1*mask.t)-1,'px, ',
-                    overlay.w-1,'px, ',
-                    (mask.t*-1)+overlay.h-1,'px, ',
-                    overlay.l,'px);'
-                ].join('');
-            }
-			parentEl.innerHTML = [
-				"<div style='position:relative;overflow:hidden;",size,"'>",
-					"<div style='overflow:hidden;position:relative;",size,"'>",
-						"<img src='",mask.f,"' ",
-							 "style='filter:mask(color=",color,");position:absolute;",position,clip,"'>",
-					"</div>",
-					"<div class='",overlayName,"' style='",size,";position:absolute;top:",overlay.t,";left:",overlay.l,"'></div>",
-				"</div>"
-			].join("");
-			return;
-		}
-
-		if (!overlay[color]) {
-			var width = overlay.w, height = overlay.h;
-
-			var canvas = document.createElement("CANVAS");
-			canvas.width = width;
-			canvas.height = height;
-
-			var ctx = canvas.getContext("2d");
-
-			ctx.save();
-			ctx.clearRect(0,0,width,height);
-
-			ctx.save();
-			ctx.drawImage(document.getElementById(maskName),mask.l,mask.t);
-			ctx.globalCompositeOperation = "source-out";
-			ctx.fillStyle = color;
-			ctx.fillRect(0,0,width,height);
-			ctx.restore();
-
-			ctx.drawImage(document.getElementById(overlayName),overlay.l,overlay.t);
-			ctx.restore();
-
-			overlay[color] = canvas.toDataURL();
-		}
-
-		parentEl.innerHTML = ["<img src='",overlay[color],"'>"].join("");
+        parentEl.innerHTML = AjxImg.getImageHtml(origImageName, null, null, false, _disabled);
 		return;
 	}
 
@@ -170,15 +114,100 @@ function(imageEl) {
  * @return	{string}	the image string
  */
 AjxImg.getImageHtml = 
-function(imageName, styleStr, attrStr, wrapInTable) {
+function(imageName, styleStr, attrStr, wrapInTable, _disabled) {
+    styleStr = styleStr || "";
 	attrStr = attrStr || "";
-	styleStr = styleStr ? (["style='", styleStr, "' "].join("")) : "";
 	var pre = wrapInTable ? "<table style='display:inline' cellpadding=0 cellspacing=0 border=0><tr><td align=center valign=bottom>" : "";
+    var html = "";
 	var post = wrapInTable ? "</td></tr></table>" : "";
 	if (imageName) {
-		return [pre, "<div class='", "Img", imageName, "' ", styleStr, " ", attrStr, "></div>", post].join("");
+        var color, m = imageName.match(AjxImg.RE_COLOR);
+        if (m) {
+            imageName = m && m[1];
+            color = m && m[2];
+        }
+
+        var className = AjxImg.getClassForImage(imageName, _disabled);
+        var overlayName = className+"Overlay";
+        var maskName = className+"Mask";
+        if (color && window.AjxImgData && AjxImgData[overlayName] && AjxImgData[maskName]) {
+            color = (color.match(/^\d$/) ? ZmOrganizer.COLOR_VALUES[color] : color) ||
+                    ZmOrganizer.COLOR_VALUES[ZmOrganizer.ORG_DEFAULT_COLOR];
+
+            var overlay = AjxImgData[overlayName], mask = AjxImgData[maskName];
+            if (AjxEnv.isIE) {
+                var clip = "";
+                var size = [
+                    "width:",overlay.w,";",
+                    "height:",overlay.h,";"
+                ].join("");
+                var position = [
+                    "top:",mask.t,";",
+                    "left:",mask.l,";"
+                ].join("");
+                if(typeof document.documentMode != 'undefined'){ //IE8 is the first one to define this. IE8 can lie when in compat mode, so we need to really know it's it.
+                    clip = [
+                        'clip:rect(',
+                        (-1*mask.t)-1,'px, ',
+                        overlay.w-1,'px, ',
+                        (mask.t*-1)+overlay.h-1,'px, ',
+                        overlay.l,'px);'
+                    ].join('');
+                }
+                html = [
+                    "<div style='display:inline-block;position:relative;overflow:hidden;",size,styleStr,"' ",attrStr,">",
+                        "<div style='overflow:hidden;position:relative;",size,"'>",
+                            "<img src='",mask.f,"' border=0 ",
+                                 "style='filter:mask(color=",color,");position:absolute;",position,clip,"'>",
+                        "</div>",
+                        "<div class='",overlayName,"' style='",size,";position:absolute;top:",overlay.t,";left:",overlay.l,"'></div>",
+                    "</div>"
+                ].join("");
+            }
+
+            else {
+                if (!overlay[color]) {
+                    var width = overlay.w, height = overlay.h;
+
+                    var canvas = document.createElement("CANVAS");
+                    canvas.width = width;
+                    canvas.height = height;
+
+                    var ctx = canvas.getContext("2d");
+
+                    ctx.save();
+                    ctx.clearRect(0,0,width,height);
+
+                    ctx.save();
+                    ctx.drawImage(document.getElementById(maskName),mask.l,mask.t);
+                    ctx.globalCompositeOperation = "source-out";
+                    ctx.fillStyle = color;
+                    ctx.fillRect(0,0,width,height);
+                    ctx.restore();
+
+                    ctx.drawImage(document.getElementById(overlayName),overlay.l,overlay.t);
+                    ctx.restore();
+
+                    overlay[color] = canvas.toDataURL();
+                }
+
+                html = [
+                    "<img src='",overlay[color],"' border=0 style='",styleStr,"' ",attrStr,">"
+                ].join("");
+            }
+        }
+        else {
+            html = [
+                "<div class='", "Img", imageName, "' style='", styleStr, "' ", attrStr, "></div>"
+            ].join("");
+        }
 	}
-	return [pre, "<div ", styleStr, " ", attrStr, "></div>", post].join("");
+    else {
+        html = [
+            "<div style='", styleStr, "' ", attrStr, "></div>"
+        ].join("");
+    }
+	return pre || post ? [pre,html,post].join("") : html;
 };
 
 /**
@@ -194,17 +223,14 @@ AjxImg.getImageSpanHtml =
 function(imageName, styleStr, attrStr, label) {
 	var className = AjxImg.getClassForImage(imageName);
 
-	var html = [];
-	var i = 0;
-	html[i++] = "<span style='white-space:nowrap'>";
-	html[i++] = "<span class='";
-	html[i++] = className;
-	html[i++] = " inlineIcon'";
-	html[i++] = styleStr ? ([" style='", styleStr, "' "].join("")) : "";
-	html[i++] = attrStr ? ([" ", attrStr].join("")) : "";
-	html[i++] = ">&nbsp;&nbsp;&nbsp;</span>";
-	html[i++] = (label || "");
-	html[i++] = "</span>";
+	var html = [
+        "<span style='white-space:nowrap'>",
+        "<span class='inlineIcon'>",
+        AjxImg.getImageHtml(imageName, styleStr, attrStr),
+        (label || ""),
+        "</span>",
+        "</span>"
+    ];
 
 	return html.join("");
 };
