e0ea3048c26e8510423886d77052593ed63c3acf
 Unknown Perforce User <dieunguyen>
2010-08-24 01:35:00 +0000

    bug:none fixed iTestContext error message

--- ZimbraSelenium/src/java/framework/util/TestStatusReporter.java
+++ ZimbraSelenium/src/java/framework/util/TestStatusReporter.java
@@ -14,6 +14,7 @@ import java.util.Calendar;
 import java.util.ArrayList;
 
 import org.testng.ITestResult;
+import org.testng.ITestContext;
 import org.testng.TestListenerAdapter;
 
 import framework.core.SelNGBase;
