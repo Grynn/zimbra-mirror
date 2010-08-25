0d7bbb340cf88233604ce44c3ecb997c844a963a
 Unknown Perforce User <rrao>
2010-08-25 17:05:54 +0000

    bug:49565         Hide the meebo space if Meebo server fails to respond [Integrate from GNR to main]
    http://bugzilla.zimbra.com/show_bug.cgi?id=49565

--- Zimlet/src/zimlet/com_zimbra_meebo/meebo.js
+++ Zimlet/src/zimlet/com_zimbra_meebo/meebo.js
@@ -15,7 +15,7 @@
 
 /**
  * Constructor.
- * 
+ *
  * @author		Raja Rao
  */
 function com_zimbra_meebo_HandlerObject() {
@@ -41,26 +41,31 @@ MeeboZimlet.LOAD_TIMEOUT = 60000;
 /**
  * Meebo class
  */
-var Meebo = {exec:function(){Meebo._.push(arguments)},_:[]}; 
-Meebo.disableSharePageButton=true;
+ 
+var Meebo = {exec:function() {
+	Meebo._.push(arguments)
+},_:[]};
+Meebo.disableSharePageButton = true;
 
 /**
  * Initializes the zimlet.
  */
 MeeboZimlet.prototype.init =
 function() {
+
+
 	this._addResizeHandler();
 	this.callCount = 0;
 	this._createMeeboFrame();
-	
-	this.timer = setInterval(AjxCallback.simpleClosure(this._showMeebo, this), MeeboZimlet.LOAD_INTERVAL);	
- };
+
+	this.timer = setInterval(AjxCallback.simpleClosure(this._showMeebo, this), MeeboZimlet.LOAD_INTERVAL);
+};
 
 /**
  * Creates the Meebo iframe.
- * 
+ *
  */
- MeeboZimlet.prototype._createMeeboFrame =
+MeeboZimlet.prototype._createMeeboFrame =
 function() {
 	var network = "zimbra";
 	var el = document.createElement("div");
@@ -68,38 +73,53 @@ function() {
 	el.style.display = "none";
 	var frame = document.createElement("iframe");
 	frame.id = "meebo-iframe";
-	frame.src = this.getResource("/meebo.html?network=" + network); 
+	frame.src = this.getResource("/meebo.html?network=" + network);
 	el.appendChild(frame);
 	this.getShell().getHtmlElement().appendChild(el);
 
-	this._taskBar  = appCtxt.getAppViewMgr().getCurrentViewComponent(ZmAppViewMgr.C_TASKBAR);
-	if(!this._taskBar) {//add space so the rest of the widgets think something will occupy this space
-		document.getElementById("skin_container_taskbar").innerHTML = "<br/><br/>";
-	}
+	this._taskBar = appCtxt.getAppViewMgr().getCurrentViewComponent(ZmAppViewMgr.C_TASKBAR);
 	this.meeboBarEl = el;
 };
 
 /**
  * Shows the meebo iframe.
- * 
+ *
  */
 MeeboZimlet.prototype._showMeebo =
 function() {
 	this.callCount++;
-	if (this.callCount == (MeeboZimlet.LOAD_TIMEOUT/MeeboZimlet.LOAD_INTERVAL) ) { //after 60 seconds, stop checking
+
+	if (this.callCount == (MeeboZimlet.LOAD_TIMEOUT / MeeboZimlet.LOAD_INTERVAL)) { //after 60 seconds, stop checking
 		clearInterval(this.timer);
-		var errMsg = AjxMessageFormat.format(this.getMessage("MeeboZimlet_error_loadBar"), MeeboZimlet.LOAD_TIMEOUT/1000);
+		var errMsg = AjxMessageFormat.format(this.getMessage("MeeboZimlet_error_loadBar"), MeeboZimlet.LOAD_TIMEOUT / 1000);
 		appCtxt.getAppController().setStatusMsg(errMsg, ZmStatusView.LEVEL_WARNING);
+		if (!this._taskBar) {//remove space
+			document.getElementById("skin_container_taskbar").innerHTML = "";
+			document.getElementById("skin_container_taskbar").style.display = "none";
+		}
+		return;
 	}
-	if(Meebo) {
-		try{
+	if (typeof Meebo == 'undefined') {
+		return;
+	}
+
+	if (Meebo) {
+		try {
 			Meebo.unhide(1);
 		} catch(e) {
+			clearInterval(this.timer);
 			appCtxt.setStatusMsg(this.getMessage("MeeboZimlet_couldNotUnhide"), ZmStatusView.LEVEL_WARNING);
+			if (!this._taskBar) {//remove space
+				document.getElementById("skin_container_taskbar").innerHTML = "";
+				document.getElementById("skin_container_taskbar").style.display = "none";
+			}
+			return;
 		}
 		clearInterval(this.timer);
-		if(this._taskBar) {
+		if (this._taskBar) {
 			this._taskBar.getHtmlElement().style.display = "none";
+		} else {//add space so the rest of the widgets think something will occupy this space
+			document.getElementById("skin_container_taskbar").innerHTML = "<br/><br/>";
 		}
 		this.meeboBarEl.style.display = "block";
 		this._resizeHandler();
@@ -109,7 +129,7 @@ function() {
 
 /**
  * Adds the resize handler.
- * 
+ *
  */
 MeeboZimlet.prototype._addResizeHandler =
 function() {
@@ -118,15 +138,15 @@ function() {
 
 /**
  * Resize handler.
- * 
+ *
  */
 MeeboZimlet.prototype._resizeHandler =
 function() {
-	var treeWidth = (document.getElementById("skin_container_tree").clientWidth +5);
-	var shellWidth =  this.getShell().getHtmlElement().clientWidth;
+	var treeWidth = (document.getElementById("skin_container_tree").clientWidth + 5);
+	var shellWidth = this.getShell().getHtmlElement().clientWidth;
 	this.meeboBarEl.style.left = treeWidth + "px";//move the bar to the left
-	this.meeboBarEl.style.width = (shellWidth - treeWidth)+ "px"; //resize the width of the bar
-	if(!this._meeboBarShown) {//hide if its not supposed to be shown
+	this.meeboBarEl.style.width = (shellWidth - treeWidth) + "px"; //resize the width of the bar
+	if (!this._meeboBarShown) {//hide if its not supposed to be shown
 		this._meeboBarOldLeft = document.getElementById("meebo").style.left;
 		this.menuItemSelected();
 	}
@@ -134,7 +154,7 @@ function() {
 
 /**
  * Called by framework upon single click
- * 
+ *
  */
 MeeboZimlet.prototype.singleClicked = function() {
 	this.menuItemSelected();
@@ -142,7 +162,7 @@ MeeboZimlet.prototype.singleClicked = function() {
 
 /**
  * Called by framework upon double click
- * 
+ *
  */
 MeeboZimlet.prototype.doubleClicked = function() {
 	this.menuItemSelected();
@@ -151,14 +171,14 @@ MeeboZimlet.prototype.doubleClicked = function() {
 
 /**
  * Called by framework when context menu was clicked
- * 
+ *
  */
 MeeboZimlet.prototype.menuItemSelected = function() {
-	if(this._meeboBarShown) {//hide
+	if (this._meeboBarShown) {//hide
 		document.getElementById("meebo").style.display = "none";
 		this._meeboBarOldLeft = document.getElementById("meebo").style.left;
 		document.getElementById("meebo").style.left = "-9999px";
-		if(this._taskBar) {
+		if (this._taskBar) {
 			this._taskBar.getHtmlElement().style.display = "block";
 		} else {
 			document.getElementById("skin_container_taskbar").innerHTML = "";
@@ -167,7 +187,7 @@ MeeboZimlet.prototype.menuItemSelected = function() {
 	} else {
 		document.getElementById("meebo").style.display = "block";
 		document.getElementById("meebo").style.left = this._meeboBarOldLeft;
-		if(this._taskBar) {
+		if (this._taskBar) {
 			this._taskBar.getHtmlElement().style.display = "none";
 		} else {
 			document.getElementById("skin_container_taskbar").innerHTML = "<br/><br/>";
