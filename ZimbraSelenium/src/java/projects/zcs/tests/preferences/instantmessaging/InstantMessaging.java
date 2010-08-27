63f94f472cebe1e463522f4432d3890b89c32faa
 Unknown Perforce User <amit>
2010-08-27 14:22:37 +0000

    bug:none   -- Added package for instant messaging testcases

---
+++ ZimbraSelenium/src/java/projects/zcs/tests/preferences/instantmessaging/InstantMessaging.java
@@ -0,0 +1,50 @@
+package projects.zcs.tests.preferences.instantmessaging;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import projects.zcs.tests.CommonTest;
+import framework.core.SelNGBase;
+import framework.util.HarnessException;
+import framework.util.RetryFailedTests;
+
+public class InstantMessaging extends CommonTest {
+	// --------------
+	// section 2 BeforeClass
+	// --------------
+	@BeforeClass(groups = { "always" })
+	private void zLogin() throws Exception {
+		zLoginIfRequired();
+		//zGoToApplication("Briefcase");
+		SelNGBase.isExecutionARetry.set(false);
+	}
+
+	@BeforeMethod(groups = { "always" })
+	public void zResetIfRequired() throws Exception {
+		if (SelNGBase.needReset.get() && !SelNGBase.isExecutionARetry.get()) {
+			zLogin();
+		}
+		SelNGBase.needReset.set(true);
+	}
+
+	@Test(groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void instantMessaging_1() throws Exception {
+		if (SelNGBase.isExecutionARetry.get())
+			handleRetry();
+
+		// dummy test
+
+		SelNGBase.needReset.set(false);
+		
+		throw new HarnessException("implement me!");
+	}
+
+	//--------------------------------------------------------------------------
+	// SECTION 4: RETRY-METHODS
+	//--------------------------------------------------------------------------
+	private void handleRetry() throws Exception {
+		SelNGBase.isExecutionARetry.set(false);
+		zLogin();
+	}
+}
