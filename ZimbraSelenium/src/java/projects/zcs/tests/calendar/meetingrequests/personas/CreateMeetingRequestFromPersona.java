42372c7fe4a938146ec58095c93f3ec8530f7e5f
Girish Bhamare <girish@zimbra.com>
2010-08-27 10:32:25 +0000

    bug:none updated with 2 test case as sendingInvitationWithPersonaFrom sendingInvitationWithPersonaFromWithAlias

--- ZimbraSelenium/src/java/projects/zcs/tests/calendar/meetingrequests/personas/CreateMeetingRequestFromPersona.java
+++ ZimbraSelenium/src/java/projects/zcs/tests/calendar/meetingrequests/personas/CreateMeetingRequestFromPersona.java
@@ -25,15 +25,13 @@ public class CreateMeetingRequestFromPersona extends CommonTest {
 	@DataProvider(name = "dataProvider")
 	public Object[][] createData(Method method) throws ServiceException {
 		String test = method.getName();
-		if (test.equals("sendingInvitationFromPersona")) {
-			return new Object[][] {
-					{ getLocalizedData_NoSpecialChar(),
-							getLocalizedData_NoSpecialChar(), "" },
-					{
-							getLocalizedData_NoSpecialChar(),
-							getLocalizedData_NoSpecialChar(),
-							getOnlyEnglishAlphabetCharAndNumber()
-									+ "@testdomain.com" } };
+		if (test.equals("sendingInvitationWithPersonaFrom")) {
+			return new Object[][] { { getLocalizedData_NoSpecialChar(),
+					getLocalizedData_NoSpecialChar(), "" } };
+		} else if (test.equals("sendingInvitationWithPersonaFromWithAlias")) {
+			return new Object[][] { { getLocalizedData_NoSpecialChar(),
+					getLocalizedData_NoSpecialChar(),
+					getOnlyEnglishAlphabetCharAndNumber() + "@testdomain.com" } };
 		} else {
 			return new Object[][] { { "" } };
 		}
@@ -61,24 +59,16 @@ public class CreateMeetingRequestFromPersona extends CommonTest {
 	// --------------------------------------------------------------------------
 	/**
 	 * Sending invitation from persona using just from address and from & alias
-	 * address. #Test1: Steps, 1.Create persona with ONLY from address 2.Compose
+	 * address. Steps, 1.Create persona with ONLY from address 2.Compose
 	 * appointment and invite to user2 3.Login to user2 and verify received mail
-	 * with persona from address. #Test2: Steps, 1.Create persona with from &
-	 * alias address 2.Compose appointment and invite to user2 3.Login to user2
-	 * and verify received mail with persona from & alias address.
+	 * with persona from address.
 	 */
 	@Test(dataProvider = "dataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
-	public void sendingInvitationFromPersona(String subject,
+	public void sendingInvitationWithPersonaFrom(String subject,
 			String personaName, String alias) throws Exception {
 		if (SelNGBase.isExecutionARetry.get())
 			handleRetry();
 
-		if (!alias.equals("")) {
-			ProvZCS.addAlias(SelNGBase.selfAccountName.get(), alias);
-			resetSession();
-			SleepUtil.sleep(1000);
-			page.zLoginpage.zLoginToZimbraAjax(SelNGBase.selfAccountName.get());
-		}
 		zGoToApplication("Preferences");
 		zGoToPreferences("Accounts");
 		String user1 = SelNGBase.selfAccountName.get().toLowerCase();
@@ -97,20 +87,13 @@ public class CreateMeetingRequestFromPersona extends CommonTest {
 								+ SelNGBase.selfAccountName.get().toLowerCase()
 								+ "')]", "");
 		SleepUtil.sleep(500);
-		if (alias.equals("")) {
-			SelNGBase.selenium
-					.get()
-					.clickAt(
-							"//div[contains(@class, 'ZSelectMenuItem ZWidget ZHasText')]//td[contains(text(), '"
-									+ SelNGBase.selfAccountName.get()
-											.toLowerCase() + "')]", "");
-		} else {
-			SelNGBase.selenium
-					.get()
-					.clickAt(
-							"//div[contains(@class, 'ZSelectMenuItem ZWidget ZHasText')]//td[contains(text(), '"
-									+ alias.toLowerCase() + "')]", "");
-		}
+		SelNGBase.selenium
+				.get()
+				.clickAt(
+						"//div[contains(@class, 'ZSelectMenuItem ZWidget ZHasText')]//td[contains(text(), '"
+								+ SelNGBase.selfAccountName.get().toLowerCase()
+								+ "')]", "");
+
 		page.zCalCompose.zCalendarEnterSimpleDetails(subject, "", user2,
 				"body of" + subject);
 		obj.zButton.zClick(page.zCalCompose.zApptSaveBtn);
@@ -130,13 +113,74 @@ public class CreateMeetingRequestFromPersona extends CommonTest {
 		String showOrigText = SelNGBase.selenium.get().getBodyText();
 		SleepUtil.sleep(1000);
 		String expectedValue;
-		if (alias.equals("")) {
-			expectedValue = "From: " + fromName + " <" + user1.toLowerCase()
-					+ ">";
-		} else {
-			expectedValue = "From: " + fromName + " <" + alias.toLowerCase()
-					+ ">";
-		}
+		expectedValue = "From: " + fromName + " <" + user1.toLowerCase() + ">";
+		Assert.assertTrue(showOrigText.indexOf(expectedValue) >= 0,
+				"Expected value(" + expectedValue + "), Actual Value("
+						+ showOrigText + ")");
+		SelNGBase.selenium.get().selectWindow(null);
+
+		SelNGBase.needReset.set(false);
+	}
+
+	/**
+	 * Sending invitation from persona using just from address and from & alias
+	 * address. Steps, 1.Create persona with from & alias address 2.Compose
+	 * appointment and invite to user2 3.Login to user2 and verify received mail
+	 * with persona from & alias address.
+	 */
+	@Test(dataProvider = "dataProvider", groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	public void sendingInvitationWithPersonaFromWithAlias(String subject,
+			String personaName, String alias) throws Exception {
+		if (SelNGBase.isExecutionARetry.get())
+			handleRetry();
+
+		ProvZCS.addAlias(SelNGBase.selfAccountName.get(), alias);
+		resetSession();
+		SleepUtil.sleep(1000);
+		page.zLoginpage.zLoginToZimbraAjax(SelNGBase.selfAccountName.get());
+
+		zGoToApplication("Preferences");
+		zGoToPreferences("Accounts");
+		String user2 = ProvZCS.getRandomAccount();
+		String fromName = getLocalizedData_NoSpecialChar();
+		page.zAccPref.zCreatePersona(personaName, fromName, alias);
+
+		SelNGBase.selenium.get().refresh();
+		SleepUtil.sleep(3500);
+		zWaitTillObjectExist("id", "ztih__main_Mail__ZIMLET_textCell");
+		page.zCalApp.zNavigateToCalendar();
+		page.zCalApp.zNavigateToApptCompose();
+		SelNGBase.selenium.get()
+				.clickAt(
+						"//td[contains(@id, '_select_container')]//td[contains(text(), '"
+								+ SelNGBase.selfAccountName.get().toLowerCase()
+								+ "')]", "");
+		SleepUtil.sleep(500);
+		SelNGBase.selenium
+				.get()
+				.clickAt(
+						"//div[contains(@class, 'ZSelectMenuItem ZWidget ZHasText')]//td[contains(text(), '"
+								+ alias.toLowerCase() + "')]", "");
+		page.zCalCompose.zCalendarEnterSimpleDetails(subject, "", user2,
+				"body of" + subject);
+		obj.zButton.zClick(page.zCalCompose.zApptSaveBtn);
+		zWaitTillObjectExist("button", page.zCalApp.zCalNewApptBtn);
+		obj.zAppointment.zExists(subject);
+
+		resetSession();
+		SleepUtil.sleep(1000);
+		SelNGBase.selfAccountName.set(user2);
+		page.zLoginpage.zLoginToZimbraAjax(user2);
+		MailApp.ClickCheckMailUntilMailShowsUp(fromName);
+		SleepUtil.sleep(500);
+		obj.zMessageItem.zRtClick(subject);
+		obj.zMenuItem.zClick(page.zMailApp.zShowOriginalMenuIconBtn);
+		SleepUtil.sleep(4000); // failed because of timing issue
+		SelNGBase.selenium.get().selectWindow("_blank");
+		String showOrigText = SelNGBase.selenium.get().getBodyText();
+		SleepUtil.sleep(1000);
+		String expectedValue;
+		expectedValue = "From: " + fromName + " <" + alias.toLowerCase() + ">";
 		Assert.assertTrue(showOrigText.indexOf(expectedValue) >= 0,
 				"Expected value(" + expectedValue + "), Actual Value("
 						+ showOrigText + ")");
