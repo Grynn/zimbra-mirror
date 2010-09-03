5ca4d211ca881405d451232828514176758b8783
Matt Rhoades <matt@zimbra.com>
2010-09-03 17:32:25 +0000

    Bug: none
    Selenium Harness: small cleanup in BrowserUtil

--- ZimbraSelenium/src/java/framework/util/BrowserUtil.java
+++ ZimbraSelenium/src/java/framework/util/BrowserUtil.java
@@ -3,49 +3,57 @@ package framework.util;
 import framework.core.SelNGBase;
 
 public class BrowserUtil extends SelNGBase {
-    public static String userAgent = "";
+	private static String userAgent = null;
 
-    public static void setBrowserAgent() {
-	//ps: getZimbraVersion is being passed just to trick selenium.call to return string(because of get*)
-	if (userAgent.equals(""))
-	    userAgent = SelNGBase.selenium.get().getEval("navigator.userAgent;");
-    }
-
-    public static String getBrowserName() {
-	setBrowserAgent();
-	String browserName = "";
-	if (userAgent.indexOf("Firefox/") >= 0){
-	    browserName = "FF " + userAgent.split("Firefox/")[1];
-		String[] temp = browserName.split(" ");
-		browserName = temp[0]+ " "+ temp[1];
-	} else if (userAgent.indexOf("MSIE") >= 0) {
-	    String[] arry = userAgent.split(";");
-	    for (int t = 0; t < arry.length; t++) {
-		if (arry[t].indexOf("MSIE") >= 0) {
-		    browserName = arry[t];
-		    break;
+	private static void setBrowserAgent() {
+		//ps: getZimbraVersion is being passed just to trick selenium.call to return string(because of get*)
+		if (userAgent == null) {
+			userAgent = SelNGBase.selenium.get().getEval("navigator.userAgent;");
+			if ( userAgent.equals("") ) {
+				userAgent = null;
+			}
 		}
-	    }
-	} else if (userAgent.indexOf("Chrome") >= 0) {
-	    String[] arry = userAgent.split("/");
-	    for (int t = 0; t < arry.length; t++) {
-		if (arry[t].indexOf("Chrome") >= 0) {
-		    String [] tmp = arry[t].split(" ");
-		    browserName = tmp[1] + " " +tmp[0];
-		    break;
-		}
-	    }
-	}else if (userAgent.indexOf("Safari") >= 0) {
-	    String[] arry = userAgent.split("/");
-	    for (int t = 0; t < arry.length; t++) {
-		if (arry[t].indexOf("Safari") >= 0) {
-		    String [] tmp = arry[t].split(" ");
-		    browserName = tmp[1] + " " +tmp[0];
-		    break;
-		}
-	    }
 	}
-	return browserName;
-    }
+
+	/**
+	 * Get the browser name
+	 * @return the browser name
+	 */
+	public static String getBrowserName() {
+		setBrowserAgent();
+		String browserName = "";
+		if (userAgent.indexOf("Firefox/") >= 0){
+			browserName = "FF " + userAgent.split("Firefox/")[1];
+			String[] temp = browserName.split(" ");
+			browserName = temp[0]+ " "+ temp[1];
+		} else if (userAgent.indexOf("MSIE") >= 0) {
+			String[] arry = userAgent.split(";");
+			for (int t = 0; t < arry.length; t++) {
+				if (arry[t].indexOf("MSIE") >= 0) {
+					browserName = arry[t];
+					break;
+				}
+			}
+		} else if (userAgent.indexOf("Chrome") >= 0) {
+			String[] arry = userAgent.split("/");
+			for (int t = 0; t < arry.length; t++) {
+				if (arry[t].indexOf("Chrome") >= 0) {
+					String [] tmp = arry[t].split(" ");
+					browserName = tmp[1] + " " +tmp[0];
+					break;
+				}
+			}
+		}else if (userAgent.indexOf("Safari") >= 0) {
+			String[] arry = userAgent.split("/");
+			for (int t = 0; t < arry.length; t++) {
+				if (arry[t].indexOf("Safari") >= 0) {
+					String [] tmp = arry[t].split(" ");
+					browserName = tmp[1] + " " +tmp[0];
+					break;
+				}
+			}
+		}
+		return browserName;
+	}
 
 }
