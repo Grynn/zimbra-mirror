390da878f9b9991c361d6cf925bb31ba4e56364c
 Unknown Perforce User <aclark>
2010-09-08 23:19:13 +0000

    bug: 12937 Implement token replacement for documentation. Added a new Zimbra "branding" message file, ZbMsg.properties, that contains the default tokens that will be replaced when serving doc files.
    http://bugzilla.zimbra.com/show_bug.cgi?id=12937

---
+++ ZimbraWebClient/src/com/zimbra/webClient/filters/InsertHelpBranding.java
@@ -0,0 +1,205 @@
+package com.zimbra.webClient.filters;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.zimbra.common.util.ZimbraLog;
+
+public class InsertHelpBranding implements Filter {
+
+    //
+    // Constants
+    //
+
+    private static final String P_EXTENSIONS = "exts";
+
+    private static final String RB_BRANDING = "/messages/ZbMsg";
+
+    private static final Pattern RE_LOCALE_ID = Pattern.compile("/([a-z]{2}(_[A-Z]{2}))/");
+
+    //
+    // Data
+    //
+
+    private ServletContext context;
+
+    private List<FilenameFilter> filters;
+
+    //
+    // Filter methods
+    //
+
+    public void init(FilterConfig config) throws ServletException {
+        this.context = config.getServletContext();
+        this.filters = createFiltersFor(config.getInitParameter(P_EXTENSIONS));
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+
+        String uri = httpRequest.getRequestURI();
+        for (FilenameFilter filter : this.filters) {
+            if (filter.accept(null, uri)) {
+                doGet(httpRequest, httpResponse);
+                return;
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        this.context = null;
+    }
+
+    //
+    // Private methods
+    //
+
+    private void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException {
+        Locale locale = getRequestedLocale(request);
+        ResourceBundle bundle = ResourceBundle.getBundle(RB_BRANDING, locale);
+
+        String filename = this.context.getRealPath(request.getServletPath());
+        File file = new File(filename);
+
+        // TODO: assume input and output encodings are UTF-8
+        // TODO: this seems to be case even for Asian languages
+        InputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(file));
+            OutputStream out = response.getOutputStream();
+
+            // replace tokens
+            Buffer buffer = new Buffer();
+            int count;
+            while (buffer.fill(in)) {
+                // look for tokens to replace
+                int last = 0;
+                for (int i = 0; i < buffer.count; i++) {
+                    int c = buffer.data[i];
+                    // start of token
+                    if (c == '@') {
+                        // NOTE: + 1 to get past the initial '@'
+                        String token = readToken(in, buffer, i + 1);
+                        if (token == null) continue;
+
+                        String value;
+                        try {
+                            value = bundle.getString(token);
+                            if (value == null) continue;
+                        }
+                        catch (MissingResourceException e) {
+                            continue;
+                        }
+
+                        // dump from last up to start of token
+                        out.write(buffer.data, last, i - last);
+
+                        // dump token replacement
+                        byte[] utf8bytes = value.getBytes("UTF-8");
+                        out.write(utf8bytes, 0, utf8bytes.length);
+
+                        // advance past token
+                        // NOTE: We assume token is ASCII, one byte per character
+                        // NOTE: + 2 to get past the '@' delimiters
+                        i += token.length() + 2;
+                        last = i;
+                    }
+                }
+                // dump whatever is left over
+                out.write(buffer.data, last, buffer.count - last);
+            }
+        }
+        finally {
+            try {
+                in.close();
+            }
+            catch (Exception e) {
+                // ignore
+            }
+        }
+    }
+
+    private static String readToken(InputStream in, Buffer buffer, int offset)
+    throws IOException {
+        for (int i = offset; i <= buffer.count; i++) {
+            if (i == buffer.count) {
+                if (!buffer.extend(in)) break;
+                continue;
+            }
+            int c = buffer.data[i];
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_') continue;
+            if (c == '@') return new String(buffer.data, offset, i-offset, "UTF-8");
+            break;
+        }
+        return null;
+    }
+
+    private static List<FilenameFilter> createFiltersFor(String exts) {
+        List<FilenameFilter> filters = new LinkedList<FilenameFilter>();
+        StringTokenizer tokenizer = new StringTokenizer(exts, ",");
+        while (tokenizer.hasMoreTokens()) {
+            String ext = tokenizer.nextToken().trim();
+            filters.add(new ExtensionFilter(ext));
+        }
+        return filters;
+    }
+
+    private static Locale getRequestedLocale(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        Matcher matcher = RE_LOCALE_ID.matcher(uri);
+        if (!matcher.matches()) return request.getLocale();
+
+        String locid = matcher.group(1);
+        String[] parts = locid.split("_");
+        if (parts.length == 3) return new Locale(parts[0], parts[1], parts[2]);
+        if (parts.length == 2) return new Locale(parts[0], parts[1]);
+        return new Locale(parts[0]);
+    }
+
+    //
+    // Classes
+    //
+
+    static class Buffer {
+        // Data
+        public byte[] data = new byte[4096];
+        public int count;
+        // Public methods
+        public boolean fill(InputStream in) throws IOException {
+            count = in.read(data);
+            return count != -1;
+        }
+        public boolean extend(InputStream in) throws IOException {
+            // increase storage
+            if (this.count == data.length) {
+                byte[] array = new byte[data.length*2];
+                System.arraycopy(data, 0, array, 0, this.count);
+                data = array;
+            }
+            // now try filling more
+            int count = in.read(data, this.count, data.length-this.count);
+            if (count != -1) this.count += count;
+            return count != -1;
+        }
+    }
+
+    static class ExtensionFilter implements FilenameFilter {
+        private String ext;
+        public ExtensionFilter(String ext) {
+            this.ext = ext.toLowerCase();
+        }
+        public boolean accept(File dir, String filename) {
+            return filename.toLowerCase().endsWith(this.ext);
+        }
+    }
+
+} // class InsertHelpBranding
