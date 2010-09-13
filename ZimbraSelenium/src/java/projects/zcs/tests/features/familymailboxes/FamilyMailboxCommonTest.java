5d208a432284a4a8ce56783d44109168206b396e
 Unknown Perforce User <amit>
2010-09-13 10:11:00 +0000

    bug:none    -- Added test case for mail.

--- ZimbraSelenium/src/java/projects/zcs/tests/features/familymailboxes/FamilyMailboxCommonTest.java
+++ ZimbraSelenium/src/java/projects/zcs/tests/features/familymailboxes/FamilyMailboxCommonTest.java
@@ -1,45 +1,85 @@
 package projects.zcs.tests.features.familymailboxes;
 
+import org.testng.Assert;
+
+import projects.zcs.tests.CommonTest;
+import projects.zcs.ui.ComposeView;
+import projects.zcs.ui.MailApp;
 import framework.core.ClientSessionFactory;
 import framework.util.SleepUtil;
 import framework.util.Stafzmprov;
-import projects.zcs.tests.CommonTest;
 
 public class FamilyMailboxCommonTest extends CommonTest {
-		public static String PARENT_ACCOUNT = "";
-		public static String CHILD_ACCOUNT ="";
-		
-		public static void addChildAccount() throws Exception {
-			/**
-			 * Add child Account
-			 */
-			PARENT_ACCOUNT=ClientSessionFactory.session().currentUserName().toLowerCase();
-			CHILD_ACCOUNT=Stafzmprov.getRandomAccount().toLowerCase();
-			String childUserAccountId=Stafzmprov.getAccountPreferenceValue(CHILD_ACCOUNT, "zimbraId");
+	public static String PARENT_ACCOUNT = "parent@testdomain.com";
+	public static String CHILD_ACCOUNT ="child@testdomain.com";
+	public static String CC_EMAIL_ADDRESS ="ccuser@testdomain.com";
+	public static String BCC_EMAIL_ADDRESS ="bccuser@testdomain.com";
+	public static String SUBJECT="";
+	public static String BODY="";
 
-			Stafzmprov.modifyAccount(PARENT_ACCOUNT, "zimbraChildAccount", childUserAccountId);
-			Stafzmprov.modifyAccount(PARENT_ACCOUNT, "zimbraPrefChildVisibleAccount", childUserAccountId);
 
-			ClientSessionFactory.session().selenium().refresh();
-			SleepUtil.sleep(3500);
-			zWaitTillObjectExist("class", "ZmOverviewZimletHeader");
+
+
+	public static void addChildAccount() throws Exception {
+		/**
+		 * Add child Account
+		 */
+		PARENT_ACCOUNT=ClientSessionFactory.session().currentUserName().toLowerCase();
+		CHILD_ACCOUNT=Stafzmprov.getRandomAccount().toLowerCase();
+		String childUserAccountId=Stafzmprov.getAccountPreferenceValue(CHILD_ACCOUNT, "zimbraId");
+
+		Stafzmprov.modifyAccount(PARENT_ACCOUNT, "zimbraChildAccount", childUserAccountId);
+		Stafzmprov.modifyAccount(PARENT_ACCOUNT, "zimbraPrefChildVisibleAccount", childUserAccountId);
+
+		ClientSessionFactory.session().selenium().refresh();
+		SleepUtil.sleep(3500);
+		zWaitTillObjectExist("class", "ZmOverviewZimletHeader");
+	}
+
+	public void clickAt(String accountName, String tabName) throws Exception{
+		ClientSessionFactory.session().selenium().clickAt("//*[contains(@id,'"+accountName+"') and contains(text(),'"+tabName+"')]","");
+	}
+
+	public String createXpath(String accountName, String tabName) throws Exception{
+		return "//*[contains(@id,'"+accountName+"') and contains(text(),'"+tabName+"')]";
+	}
+
+	public void checkHeaders(String headerText) throws Exception {
+		ClientSessionFactory.session().selenium().isElementPresent("//*[contains(@class, 'ZOptionsHeader ImgPrefsHeader') and contains(text(), '"+headerText+"')]");
+	}
+
+	public void checkLabels(String labelText) throws Exception {
+		ClientSessionFactory.session().selenium().isElementPresent("//*[contains(@class, 'ZOptionsLabel') and contains(text(), '"+labelText+"')]");
+	}
+
+	@SuppressWarnings("static-access")
+	public static void sendMailAndSelect(String to, String cc, String bcc, String subject, String body) throws Exception {
+		page.zComposeView.zEnterComposeValues(to, cc, bcc, subject, body,"");
+		obj.zButton.zClick(ComposeView.zSendIconBtn);
+		SleepUtil.sleep(1000);
+		Boolean found = false;
+
+		for (int i = 0; i < 5; i++) {
+			obj.zButton.zClick(MailApp.zGetMailIconBtn);
+			String rc = obj.zMessageItem.zExistsDontWait(SUBJECT);
+			if (rc.equals("false")) {
+				SleepUtil.sleep(500);
+			} else {
+				found = true;
+				obj.zMessageItem.zClick(subject);
+				break;
+			}
 		}
-		
-		public void clickAt(String accountName, String tabName) throws Exception{
-			ClientSessionFactory.session().selenium().clickAt("//*[contains(@id,'"+accountName+"') and contains(text(),'"+tabName+"')]","");
-		}
 
-		public String createXpath(String accountName, String tabName) throws Exception{
-			return "//*[contains(@id,'"+accountName+"') and contains(text(),'"+tabName+"')]";
-		}
+		if (!found)
+			Assert.fail("Mail(" + SUBJECT + ") appeared after " + 30
+					+ " seconds in Inbox");
+	}
 
-		public void checkHeaders(String headerText) throws Exception {
-			ClientSessionFactory.session().selenium().isElementPresent("//*[contains(@class, 'ZOptionsHeader ImgPrefsHeader') and contains(text(), '"+headerText+"')]");
-		}
-
-		public void checkLabels(String labelText) throws Exception {
-			ClientSessionFactory.session().selenium().isElementPresent("//*[contains(@class, 'ZOptionsLabel') and contains(text(), '"+labelText+"')]");
-		}
-
-	
 }
+
+
+
+
+
+
--- ZimbraSelenium/src/java/projects/zcs/tests/features/familymailboxes/mail/Mail.java
+++ ZimbraSelenium/src/java/projects/zcs/tests/features/familymailboxes/mail/Mail.java
@@ -1,33 +1,38 @@
 package projects.zcs.tests.features.familymailboxes.mail;
 
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import projects.zcs.tests.CommonTest;
-import framework.core.*;
-import framework.util.HarnessException;
+import projects.zcs.tests.features.familymailboxes.FamilyMailboxCommonTest;
+import framework.core.SelNGBase;
 import framework.util.RetryFailedTests;
-public class Mail extends CommonTest {
+public class Mail extends FamilyMailboxCommonTest {
 
 	// --------------
 	// section 2 BeforeClass
 	// --------------
 	@BeforeClass(groups = { "always" })
 	public void zLogin() throws Exception {
-		//super.NAVIGATION_TAB="documents";
+		super.NAVIGATION_TAB="mail";
 		super.zLogin();
 	}
 
-	@Test(groups = { "smoke", "full" }, retryAnalyzer = RetryFailedTests.class)
+	@SuppressWarnings("static-access")
+	@Test(groups = { "smoke", "test" }, retryAnalyzer = RetryFailedTests.class)
 	public void familyMail_1() throws Exception {
 		if (SelNGBase.isExecutionARetry.get())
 			handleRetry();
 
-		// dummy test
-
-		SelNGBase.needReset.set(false);
+		addChildAccount();
+		page.zComposeView.zNavigateToMailCompose();
+		sendMailAndSelect(PARENT_ACCOUNT, CC_EMAIL_ADDRESS, BCC_EMAIL_ADDRESS, SUBJECT=getLocalizedData(5), BODY=getLocalizedData(15));
 		
-		throw new HarnessException("implement me!");
+		clickAt(CHILD_ACCOUNT, localize(locator.inbox));
+		page.zComposeView.zNavigateToMailCompose();
+		sendMailAndSelect(CHILD_ACCOUNT, PARENT_ACCOUNT, BCC_EMAIL_ADDRESS, SUBJECT=getLocalizedData(5), BODY=getLocalizedData(15));
+		clickAt(CHILD_ACCOUNT, localize(locator.inbox));
+		obj.zMenuItem.zClick(SUBJECT);
+		
+		SelNGBase.needReset.set(false);
 	}
 }
