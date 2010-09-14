108da8bff69531c7d86b74896353ac37c265c623
 Unknown Perforce User <brian>
2010-09-14 16:46:48 +0000

    bug: 50557    faster to use ALTER TABLE tablename ENGINE=InnoDB instead of OPTIMIZE TABLE tablename
    http://bugzilla.zimbra.com/show_bug.cgi?id=50557

--- ZimbraServer/src/db/migration/migrate20100913-Mysql51.pl
+++ ZimbraServer/src/db/migration/migrate20100913-Mysql51.pl
@@ -20,7 +20,7 @@ use Getopt::Long;
 my $concurrent = 10;
 
 sub usage() {
-	print STDERR "Usage: optimizeMboxgroups.pl\n";
+	print STDERR "Usage: $0\n";
 	exit(1);
 }
 my $opt_h;
@@ -33,17 +33,19 @@ my @sql = ();
 foreach my $group (@groups) {
   foreach my $table qw(mail_item appointment imap_folder imap_message open_conversation pop3_message revision tombstone data_source_item) {
    print "Adding $group.$table to be optimized\n";
-    push(@sql, "OPTIMIZE TABLE $group.$table;");
+    #push(@sql, "OPTIMIZE TABLE $group.$table;");
+    push(@sql, "ALTER TABLE $group.$table ENGINE=InnoDB;");
   }
 }
-foreach my $table qw(volume current_volumes mailbox deleted_account mailbox_metadata out_of_office config table_maintenance service_status scheduled_task mobile_devices jiveUserProp jiveGroupProp jiveGroupUser jivePrivate jiveOffline jiveRoster jiveRosterGroups jiveVCard jiveID jiveProperty jiveExtComponentConf jiveRemoteServerConf jivePrivacyList jiveSASLAuthorized mucRoom mucRoomProp mucAffiliation mucMember mucConverstationLog) {
+foreach my $table qw(volume current_volumes mailbox deleted_account mailbox_metadata out_of_office config table_maintenance scheduled_task mobile_devices ) {
   print "Adding zimbra.$table to be optimized\n";
-  push(@sql, "OPTIMIZE TABLE zimbra.$table;");
+  #push(@sql, "OPTIMIZE TABLE zimbra.$table;");
+  push(@sql, "ALTER TABLE zimbra.$table ENGINE=InnoDB;");
 }
 my $start = time();
 Migrate::runSqlParallel($concurrent, @sql);
 my $elapsed = time() - $start;
 my $numGroups = scalar @groups;
-print "\nOptimized $numGroups mailbox groups in $elapsed seconds\n";
+print "\nAltered $numGroups mailbox groups in $elapsed seconds\n";
 
 exit(0);
