e3776a02dafb91de1129c885b27ab6f4f81904b5
 Unknown Perforce User <ming>
2010-09-21 06:19:32 +0000

    Bug 50196 ShortURL_XModelItem use AjxUtil.SHORT_URL_RE,AjxUtil.IP_SHORT_URL_RE pattern to validate input string. Modify this two pattern to match port part zero or one times. The port part shouldn't be matched for more than two times.
    http://bugzilla.zimbra.com/show_bug.cgi?id=50196

--- ZimbraWebClient/WebRoot/js/ajax/util/AjxUtil.js
+++ ZimbraWebClient/WebRoot/js/ajax/util/AjxUtil.js
@@ -58,8 +58,8 @@ AjxUtil.HOST_NAME_RE = /^[A-Za-z0-9\-]{2,}(\.[A-Za-z0-9\-]{1,})*(\.[A-Za-z0-9\-]
 AjxUtil.HOST_NAME_WITH_PORT_RE = /^[A-Za-z0-9\-]{2,}(\.[A-Za-z0-9\-]{2,})*:([0-9])+$/;
 AjxUtil.EMAIL_SHORT_RE = /^[^@\s]+$/;
 AjxUtil.EMAIL_FULL_RE = /^[^@\s]+@[A-Za-z0-9\-]{2,}(\.[A-Za-z0-9\-]{2,})+$/;
-AjxUtil.SHORT_URL_RE = /^[A-Za-z0-9]{2,}:\/\/[A-Za-z0-9\-]+(\.[A-Za-z0-9\-]+)*(:([0-9])+)*$/;
-AjxUtil.IP_SHORT_URL_RE = /^[A-Za-z0-9]{2,}:\/\/\d{1,3}(\.\d{1,3}){3}(\.\d{1,3}\.\d{1,3})?(:([0-9])+)*$/;
+AjxUtil.SHORT_URL_RE = /^[A-Za-z0-9]{2,}:\/\/[A-Za-z0-9\-]+(\.[A-Za-z0-9\-]+)*(:([0-9])+)?$/;
+AjxUtil.IP_SHORT_URL_RE = /^[A-Za-z0-9]{2,}:\/\/\d{1,3}(\.\d{1,3}){3}(\.\d{1,3}\.\d{1,3})?(:([0-9])+)?$/;
 
 AjxUtil.isIpAddress 		= function(s) { return AjxUtil.IP_ADDR_RE.test(s); };
 AjxUtil.isDomain 			= function(s) {	return AjxUtil.DOMAIN_RE.test(s); };
