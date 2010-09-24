9d8c6feb3e3d363aff6ceb373b36e348583514af
 Unknown Perforce User <dkarp>
2010-09-24 16:50:31 +0000

    bug: 19222
    Switch MimeCompoundHeader to a subtype of MimeHeader and convert its main() to a JUnit test.  Clean up ContentDisposition and ContentType method names and add a "cleanup" method to normalize them.  Formalize MimeHeader "dirty" and reserialization on dirty.  MimeHeader.toString() now returns the folded encoded header value minus the trailing CRLF, not the entire header line.
    
    Break MimeAddressHeader out into its own class.  Flesh out ByteBuilder to more closely parallel StringBuilder.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=19222

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZDocumentBean.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZDocumentBean.java
@@ -14,10 +14,10 @@
  */
 package com.zimbra.cs.taglib.bean;
 
-import com.zimbra.cs.zclient.ZDocument;
-import com.zimbra.common.mime.ContentType;
-
 import java.util.Date;
+
+import com.zimbra.common.mime.ContentType;
+import com.zimbra.cs.zclient.ZDocument;
 
 public class ZDocumentBean {
 
@@ -32,46 +32,56 @@ public class ZDocumentBean {
     }
 
     public Date getModifiedDate() {
-        return new Date(mDoc.getModifiedDate()/1000);
+        return new Date(mDoc.getModifiedDate() / 1000);
     }
 
     public Date getMetaDataChangedDate() {
         return new Date(mDoc.getMetaDataChangedDate());
     }
 
-	public String getId() {
-		return mDoc.getId();
-	}
-	public String getName() {
-		return mDoc.getName();
-	}
-	public String getFolderId() {
-		return mDoc.getFolderId();
-	}
-	public String getVersion() {
-		return mDoc.getVersion();
-	}
-	public String getEditor() {
-		return mDoc.getEditor();
-	}
-	public String getCreator() {
-		return mDoc.getCreator();
-	}
-	public String getRestUrl() {
-		return mDoc.getRestUrl();
-	}
-	public boolean isWiki() {
-		return mDoc.isWiki();
+    public String getId() {
+        return mDoc.getId();
     }
+
+    public String getName() {
+        return mDoc.getName();
+    }
+
+    public String getFolderId() {
+        return mDoc.getFolderId();
+    }
+
+    public String getVersion() {
+        return mDoc.getVersion();
+    }
+
+    public String getEditor() {
+        return mDoc.getEditor();
+    }
+
+    public String getCreator() {
+        return mDoc.getCreator();
+    }
+
+    public String getRestUrl() {
+        return mDoc.getRestUrl();
+    }
+
+    public boolean isWiki() {
+        return mDoc.isWiki();
+    }
+
     public String getContentType() {
         String contentType = mDoc.getContentType();
-        return new ContentType(contentType).getValue();
+        return new ContentType(contentType).getContentType();
     }
+
     public long getSize() {
         return mDoc.getSize();
     }
+
     public String getTagIds() {
         return mDoc.getTagIds();
     }
-  
+
 }
