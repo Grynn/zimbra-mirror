100f58be056ab270024d86cde19c177833c99f80
 Unknown Perforce User <dnault>
2010-09-28 20:18:29 +0000

    bug: 51238 NPE when setting up external IMAP account
    http://bugzilla.zimbra.com/show_bug.cgi?id=51238

--- ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/ConfigServlet.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/jsp/ConfigServlet.java
@@ -15,23 +15,31 @@
 package com.zimbra.cs.offline.jsp;
 
 import javax.servlet.http.HttpServlet;
+
 import com.zimbra.common.localconfig.LC;
+import com.zimbra.cs.datasource.DataSourceManager;
 
 public class ConfigServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 8124246834674440988L;
+    private static final long serialVersionUID = 8124246834674440988L;
 
     private static final String LOCALHOST_URL_PREFIX = "http://127.0.0.1:";
-    
+
     public static String LOCALHOST_SOAP_URL;
     public static String LOCALHOST_ADMIN_URL;
-    
-	@Override
-	public void init() {
-		String port = LC.zimbra_admin_service_port.value();
-		
-		//setting static variables
-		LOCALHOST_SOAP_URL = LOCALHOST_URL_PREFIX + port + "/service/soap/";
-		LOCALHOST_ADMIN_URL = LOCALHOST_URL_PREFIX + port + "/service/admin/soap/";
+
+    @Override
+    public void init() {
+        String port = LC.zimbra_admin_service_port.value();
+
+        //setting static variables
+        LOCALHOST_SOAP_URL = LOCALHOST_URL_PREFIX + port + "/service/soap/";
+        LOCALHOST_ADMIN_URL = LOCALHOST_URL_PREFIX + port + "/service/admin/soap/";
+
+        try {
+            DataSourceManager.init();
+        } catch (Exception x) {
+            throw new RuntimeException(x);
+        }
     }
 }
