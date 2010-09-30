54cb159c3b44005947f6a157f3be1a2dbecac3cc
 Unknown Perforce User <ysasaki>
2010-09-30 19:20:01 +0000

    bug:45280 Factor out inner Tokenizer classes into analysis package
    http://bugzilla.zimbra.com/show_bug.cgi?id=45280

--- AnalyzerSample/src/java/com/zimbra/analyzersample/Analyzer.java
+++ AnalyzerSample/src/java/com/zimbra/analyzersample/Analyzer.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -22,60 +22,54 @@ import com.zimbra.cs.index.LuceneFields;
 import com.zimbra.cs.index.ZimbraAnalyzer;
 
 /**
- * Our sample "customized" Analyzer: all we do is 
- * pass all requests on do the default Zimbra analyzer.
+ * Our sample "customized" Analyzer: all we do is pass all requests on do the
+ * default Zimbra analyzer.
  */
 public class Analyzer extends ZimbraAnalyzer {
-    public TokenStream tokenStream(String fieldName, Reader reader) {
 
-        //
-        // Return a TokenStream instance depending on the specified Field
-        // (and, potentially, the contents of the Reader itself)
-        //
-        //
-        // There are a number of different Lucene fields that get
-        // analyzed.  In many cases our custom analyzer will not
-        // want to change the default behavior (e.g. L_MIMETYPE
-        // which is guaranteed to be ASCII) but for other fields
-        // (e.g. L_CONTENT) we probably do want to plug in.
-        // 
-        
-        if (      // L_ATTACHMENTS: comma-separated list of attachment types (e.g. "application/binary, application/pdf")
-                    fieldName.equals(LuceneFields.L_ATTACHMENTS) ||
-                    // L_MIMETYPE: (possibly comma-separated list) mime type, e.g. "text/plain"
-                    fieldName.equals(LuceneFields.L_MIMETYPE)       )
-        {
-            return super.tokenStream(fieldName, reader);
-            
-        } else if (fieldName.equals(LuceneFields.L_SIZE)) {
-            // L_SIZE: numeric size, tokenize via NumberTokenStream
-            return super.tokenStream(fieldName, reader);
-            
-        } else if (fieldName.equals(LuceneFields.L_H_FROM) 
-                    || fieldName.equals(LuceneFields.L_H_TO) 
-                    || fieldName.equals(LuceneFields.L_H_CC)) 
-        {
-            // L_H_FROM, L_H_TO, L_H_CC are the RFC822 headers, Custom analyzers  almost certainly want to
-            // pass these up to the inherited TokenStream, unless you know exactly what you're doing.
-            return super.tokenStream(fieldName, reader);
-            
-        } else if (fieldName.equals(LuceneFields.L_FILENAME)) {
-            return super.tokenStream(fieldName, reader);
-            
-        } else if (fieldName.equals(LuceneFields.L_CONTENT)) {
-            // L_CONTENT field means all the text in the document.  We probably want
-            // to plug our custom analyzer in here
-            return super.tokenStream(fieldName, reader);
-            
-        } else if (fieldName.equals(LuceneFields.L_H_SUBJECT)) {
-            // L_H_SUBJECT field means the subject of the document.  We probably want
-            // to plug our custom analyzer in here
-            return super.tokenStream(fieldName, reader);
-            
+    /**
+     * Return a {@link TokenStream} instance depending on the specified Field
+     * and, potentially, the contents of the Reader itself.
+     *
+     * There are a number of different Lucene fields that get analyzed. In many
+     * cases our custom analyzer will not want to change the default behavior
+     * (e.g. L_MIMETYPE which is guaranteed to be ASCII) but for other fields
+     * (e.g. L_CONTENT) we probably do want to plug in.
+     */
+    @Override
+    public TokenStream tokenStream(String field, Reader reader) {
+        if (field.equals(LuceneFields.L_ATTACHMENTS) ||
+                field.equals(LuceneFields.L_MIMETYPE)) {
+            // L_ATTACHMENTS: comma-separated list of attachment types
+            //  e.g. "application/binary, application/pdf"
+            // L_MIMETYPE: (possibly comma-separated list) mime type,
+            //  e.g. "text/plain"
+            return super.tokenStream(field, reader);
+        } else if (field.equals(LuceneFields.L_SORT_SIZE)) {
+            // L_SORT_SIZE: numeric size, tokenize via NumberTokenStream
+            return super.tokenStream(field, reader);
+        } else if (field.equals(LuceneFields.L_H_FROM)
+                || field.equals(LuceneFields.L_H_TO)
+                || field.equals(LuceneFields.L_H_CC)) {
+            // L_H_FROM, L_H_TO, L_H_CC are the RFC822 headers.
+            // Custom analyzers must pass these up to the inherited TokenStream.
+            return super.tokenStream(field, reader);
+        } else if (field.equals(LuceneFields.L_FILENAME)) {
+            return super.tokenStream(field, reader);
+        } else if (field.equals(LuceneFields.L_CONTENT)) {
+            // L_CONTENT field means all the text in the document.
+            // We probably want to plug our custom analyzer in here
+            return super.tokenStream(field, reader);
+
+        } else if (field.equals(LuceneFields.L_H_SUBJECT)) {
+            // L_H_SUBJECT field means the subject of the document.
+            // We probably want to plug our custom analyzer in here
+            return super.tokenStream(field, reader);
+
         } else {
-            // Catch-All: probably want to pass this up to the standard Zimbra Analyzer 
-            return super.tokenStream(fieldName, reader);
-            
+            // Catch-All: probably want to pass this up to the standard Zimbra Analyzer
+            return super.tokenStream(field, reader);
         }
     }
+
 }
--- AnalyzerSample/src/java/com/zimbra/analyzersample/AnalyzerSample.java
+++ AnalyzerSample/src/java/com/zimbra/analyzersample/AnalyzerSample.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2006, 2007, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -23,42 +23,37 @@ import com.zimbra.cs.index.ZimbraAnalyzer;
 import com.zimbra.common.service.ServiceException;
 
 /**
- * 
- * A sample Zimbra Extension which provides a custom
- * Lucene analyzer.  The extension must call ZimbraAnalyzer.registerAnalyzer()
- * on startup to register itself with the system.  The custom analyzer is
- * invoked based on the COS or Account setting zimbraTextAnalyzer.
- * 
+ * A sample Zimbra Extension which provides a custom Lucene analyzer.
+ * <p>
+ * The extension must call {@link ZimbraAnalyzer#registerAnalyzer(String, org.apache.lucene.analysis.Analyzer)})
+ * on startup to register itself with the system. The custom analyzer is invoked
+ * based on the COS or Account setting {@code zimbraTextAnalyzer}.
  */
 public class AnalyzerSample implements ZimbraExtension {
 
     private static Log sLog = LogFactory.getLog(AnalyzerSample.class);
 
-    public AnalyzerSample() {
-    }
-
-    /* (non-Javadoc)
-     * @see com.zimbra.cs.extension.ZimbraExtension#init()
-     * 
-     * The extension can provide any name to ZimbraAnalyzer.registerAnalyzer() 
-     * however that name must be unique or else the registration will fail.
-     * 
-     */
+    @Override
     public synchronized void init() {
         sLog.info("Initializing "+getName());
         try {
+            // The extension can provide any name, however that name must be
+            // unique or else the registration will fail.
             ZimbraAnalyzer.registerAnalyzer(getName(), new Analyzer());
         } catch (ServiceException e) {
             sLog.error("Error while registering extension "+getName(), e);
         }
-    }            
+    }
 
+    @Override
     public synchronized void destroy() {
         sLog.info("Destroying "+getName());
         ZimbraAnalyzer.unregisterAnalyzer(getName());
     }
 
+    @Override
     public String getName() {
         return "AnalyzerSample";
     }
+
 }
