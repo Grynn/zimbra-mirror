0f937b5ab50eed42633eb6681ed25084ee389698
 Unknown Perforce User <rsegu>
2010-10-08 18:39:48 +0000

    bug: 51484
    GetItemInfoJSON code changed to adopt the new ZimbraSoap related changes.
    
    Corrected all instances of <zm:getItemInfoJSON/> occurances and handled the change in response.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=51484

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/GetItemInfoJSONTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/GetItemInfoJSONTag.java
@@ -17,9 +17,12 @@ package com.zimbra.cs.taglib.tag;
 import com.zimbra.common.auth.ZAuthToken;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.SoapTransport;
 import com.zimbra.cs.taglib.ZJspSession;
-import com.zimbra.cs.taglib.bean.ZMailboxBean;
-import com.zimbra.cs.zclient.ZMailbox;
+import com.zimbra.common.soap.MailConstants;
+import com.zimbra.common.soap.SoapProtocol;
+import com.zimbra.common.zclient.ZClientException;
+import com.zimbra.cs.taglib.tag.TagUtil.JsonDebugListener;
 
 import javax.servlet.jsp.JspContext;
 import javax.servlet.jsp.JspException;
@@ -27,7 +30,6 @@ import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import java.io.IOException;
 import java.util.regex.Pattern;
-import java.util.TimeZone;
 
 public class GetItemInfoJSONTag extends ZimbraSimpleTag {
 
@@ -35,27 +37,40 @@ public class GetItemInfoJSONTag extends ZimbraSimpleTag {
 
     private String mVar;
     private String mId;
-    private ZMailboxBean mMailbox;
+    private String mAuthToken;
     
 
     public void setVar(String var) { this.mVar = var; }
     public void setId(String id) { this.mId = id; }
-    public void setBox(ZMailboxBean mailbox) { this.mMailbox = mailbox; }
+    public void setAuthtoken(String authToken) { this.mAuthToken = authToken; }
 
     public void doTag() throws JspException, IOException {
         try {
             JspContext ctxt = getJspContext();
-            ZMailbox mbox = mMailbox != null ? mMailbox.getMailbox() :  getMailbox();
-            Element e = mbox.getItemInfoJSON(mId);
-
-			// Replace "</script>" with "</scr" + "ipt>" because html parsers recognize the close script tag.
-			String json = e.toString();
-			String json2 = sSCRIPT.matcher(json).replaceAll("</scr\"+\"ipt>");
-
-			ctxt.setAttribute(mVar, json2,  PageContext.REQUEST_SCOPE);
-
+            PageContext pageContext = (PageContext) ctxt;
+            String url = ZJspSession.getSoapURL(pageContext);
+            String remoteAddr = ZJspSession.getRemoteAddr(pageContext);
+            ZAuthToken authToken = new ZAuthToken(mAuthToken);
+            Element e = getItemInfoJSON(url, remoteAddr, authToken, mId);
+            ctxt.setAttribute(mVar, sSCRIPT.matcher(e.toString()).replaceAll("</scr\"+\"ipt>"),  PageContext.REQUEST_SCOPE);
         } catch (ServiceException e) {
             throw new JspTagException(e.getMessage(), e);
         }
     }
+
+    public static Element getItemInfoJSON(String url, String remoteAddr, ZAuthToken authToken, String mId) throws ServiceException{
+
+        JsonDebugListener debug = new TagUtil.JsonDebugListener();
+        SoapTransport transport = TagUtil.newJsonTransport(url, remoteAddr, authToken, debug);
+
+        try {
+            Element req = Element.create(SoapProtocol.SoapJS, MailConstants.GET_ITEM_REQUEST);            
+            Element item = req.addElement(MailConstants.E_ITEM);
+            item.addAttribute(MailConstants.A_ID, mId);
+            transport.invokeWithoutSession(req);
+            return debug.getEnvelope();
+        } catch (IOException e) {
+            throw ZClientException.IO_ERROR("invoke "+e.getMessage(), e);
+        }
+    }    
 }
