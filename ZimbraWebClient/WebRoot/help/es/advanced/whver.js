f2b625ffa1720c4b7f701410fbeb77e7197d85e8
 Unknown Perforce User <zchao>
2010-11-05 22:16:54 +0000

    Bug 51994 for checking Advanced ES help draft
    http://bugzilla.zimbra.com/show_bug.cgi?id=51994

--- ZimbraWebClient/WebRoot/help/es/advanced/whver.js
+++ ZimbraWebClient/WebRoot/help/es/advanced/whver.js
@@ -22,7 +22,9 @@ var gbKonqueror=(gAgent.indexOf("konqueror")!= -1);
 var gbSafari=(gAgent.indexOf("safari")!= -1);
 var gbWindows=((gAgent.indexOf('win')!= -1)||(gAgent.indexOf('16bit')!= -1));
 var gbMozilla=((gAgent.indexOf('gecko')!=-1) && (gAgent.indexOf('netscape')==-1));
-
+var gbAIR=(gAgent.indexOf('adobeair')!=-1);
+var gbChrome = (gAgent.indexOf('chrome')!=-1);
+var gbAIRSSL= false ;
 
 var gVersion=navigator.appVersion.toLowerCase();
 
@@ -112,5 +114,11 @@ if(gbSafari)
 			gbSafari3=true;
 		}
 	}
+}
+if(gbChrome)
+{
+	//for the time being use same tests as safari
+	gbSafari = true ;
+	gbSafari3=true;
 }
 var gbWhVer=true;
