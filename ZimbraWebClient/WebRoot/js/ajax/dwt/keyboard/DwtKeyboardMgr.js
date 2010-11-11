77a577456093dc6f3a8ff007d769cbf321e43348
 Unknown Perforce User <kkarthikeyan>
2010-11-11 23:09:03 +0000

    Integrate change 257604 from ZDESKTOP-608 to main --------------------- bug:53138 - this.__keySequence undefined --------------------- reassign __keysequence to new array to clean up instead of making length zero.
    http://bugzilla.zimbra.com/show_bug.cgi?id=53138

--- ZimbraWebClient/WebRoot/js/ajax/dwt/keyboard/DwtKeyboardMgr.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/keyboard/DwtKeyboardMgr.js
@@ -357,7 +357,7 @@ function(timeout) {
 DwtKeyboardMgr.prototype.clearKeySeq =
 function() {
 	this.__killKeySeqTimedActionId = -1;
-	this.__keySequence.length = 0;
+	this.__keySequence = [];
 };
 
 /**
