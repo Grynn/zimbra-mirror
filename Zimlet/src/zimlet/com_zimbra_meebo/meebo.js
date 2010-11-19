987b202c888d3e992a90e2fa0c7224cb6abd3122
 Unknown Perforce User <rajar>
2010-11-19 00:19:55 +0000

    bug:53489  add new Meebo code
    http://bugzilla.zimbra.com/show_bug.cgi?id=53489

--- Zimlet/src/zimlet/com_zimbra_meebo/meebo.js
+++ Zimlet/src/zimlet/com_zimbra_meebo/meebo.js
@@ -81,62 +81,20 @@ function() {
  */
 MeeboZimlet.prototype._initializeMeebo =
 function() {
-	window.Meebo || function(b) {
-		function p() {
-			return["<",i,' onload="var d=',g,";d.getElementsByTagName('head')[0].",
-				j,"(d.",h,"('script')).",k,"='//",b.stage ? "stage-" : "","cim.meebo.com/cim?iv=",a.v,
-				"&",q,"=",b[q],b[l] ? "&" + l + "=" + b[l] : "",b[e] ? "&" + e + "=" + b[e] : "","'\"></",i,">"].join("")
-		}
-
-		var f = window,a = f.Meebo = f.Meebo || function() {
-			(a._ = a._ || []).push(arguments)
-		},d = document,
-		i = "body",m = d[i],r;
-		if (!m) {
-			r = arguments.callee;
-			return setTimeout(function() {
-				r(b)
-			},
-			100)
-		}
-		a.$ = {0:+new Date};
-		a.T = function(u) {
-			a.$[u] = new Date - a.$[0]
-		};
-		a.v = 4;
-		var j = "appendChild",
-		h = "createElement",k = "src",l = "lang",q = "network",e = "domain",n = d[h]("div"),v = n[j](d[h]("m")),
-		c = d[h]("iframe"),g = "document",o,s = function() {
-			a.T("load");
-			a("load")
-		};
-		f.addEventListener ?
-		f.addEventListener("load", s, false) : f.attachEvent("onload", s);
-		n.style.display = "none";
-		m.insertBefore(n, m.firstChild).id = "meebo";
-		c.frameBorder = "0";
-		c.id = "meebo-iframe";
-		c.allowTransparency = "true";
-		v[j](c);
-		try {
-			c.contentWindow[g].open()
-		} catch(w) {
-			b[e] =
-			d[e];
-			o = "javascript:var d=" + g + ".open();d.domain='" + d.domain + "';";
-			c[k] = o + "void(0);"
-		}
-		try {
-			var t =
-			c.contentWindow[g];
-			t.write(p());
-			t.close()
-		} catch(x) {
-			c[k] = o + 'd.write("' + p().replace(/"/g,
-			'\\"') + '");d.close();'
-		}
-		a.T(1)
-	}({network:"zimbra"});
+window.Meebo||function(c){function p(){return["<",i,' onload="var d=',g,";d.getElementsByTagName('head')[0].",
+j,"(d.",h,"('script')).",k,"='//cim.meebo.com/cim?iv=",a.v,"&",q,"=",c[q],c[l]?
+"&"+l+"="+c[l]:"",c[e]?"&"+e+"="+c[e]:"","'\"></",i,">"].join("")}var f=window,
+a=f.Meebo=f.Meebo||function(){(a._=a._||[]).push(arguments)},d=document,i="body",
+m=d[i],r;if(!m){r=arguments.callee;return setTimeout(function(){r(c)},100)}a.$=
+{0:+new Date};a.T=function(u){a.$[u]=new Date-a.$[0]};a.v=4;var j="appendChild",
+h="createElement",k="src",l="lang",q="network",e="domain",n=d[h]("div"),v=n[j](d[h]("m")),
+b=d[h]("iframe"),g="document",o,s=function(){a.T("load");a("load")};f.addEventListener?
+f.addEventListener("load",s,false):f.attachEvent("onload",s);n.style.display="none";
+m.insertBefore(n,m.firstChild).id="meebo";b.frameBorder="0";b.id="meebo-iframe";
+b.allowTransparency="true";v[j](b);try{b.contentWindow[g].open()}catch(w){c[e]=
+d[e];o="javascript:var d="+g+".open();d.domain='"+d.domain+"';";b[k]=o+"void(0);"}try{var t=
+b.contentWindow[g];t.write(p());t.close()}catch(x){b[k]=o+'d.write("'+p().replace(/"/g,
+'\\"')+'");d.close();'}a.T(1)}({network:"zimbra"});
 	Meebo.disableSharePageButton = true;
 };
 
