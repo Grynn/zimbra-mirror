59c91178e9fb531a66fec252411c5406129b7ca2
 Unknown Perforce User <jhidayat>
2011-01-17 22:01:09 +0000

    Integrate Change 270588 by jhidayat@ZimbraQA_jhidayat on 1/17/2011 1:59:11 PM Bug: None Adding new method: cmdExecWithOutput for executing command line and spitting out the console output to a String.

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/util/CommandLine.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/util/CommandLine.java
@@ -7,11 +7,38 @@ import org.apache.log4j.Logger;
 
 public class CommandLine {
 	private static Logger logger = LogManager.getLogger(CommandLine.class);
-	
+
+	/**
+	 * Execute Command line and return the execution status
+	 * @param command Command line to be executed
+	 * @return (Integer) Execution status code
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static int CmdExec(String command) throws IOException, InterruptedException {
 		Process p = Runtime.getRuntime().exec(command);
 		int exitValue = p.waitFor();
 		logger.info(command + " - " + exitValue);
 		return (exitValue);
+	}
+
+	/**
+	 * Execute command line and return the output as a String
+	 * @param command Command line to be executed
+	 * @return (String) output from the console
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public static String cmdExecWithOutput(String command) throws IOException, InterruptedException {
+	   logger.debug("Executing command: " + command);
+      Process process = Runtime.getRuntime().exec(command);
+
+      StreamReader reader = new StreamReader(process.getInputStream());
+      logger.debug("Starting the reader thread");
+      reader.start();
+      process.waitFor();
+      reader.join();
+      String output = reader.getResult();
+      return output;
 	}
 }
