6ec21e3cb8199a843e7cfb11221af911541c8bee
 Unknown Perforce User <clarka>
2011-01-28 17:55:41 +0000

    bug: 47983 Re-order name, job info, and address fields for AJAX client when user locale is Japanese. Still need to do the same for the standard and mobile clients.
    http://bugzilla.zimbra.com/show_bug.cgi?id=47983

--- ZimbraWebClient/WebRoot/js/ajax/boot/AjxTemplateCompiler.js
+++ ZimbraWebClient/WebRoot/js/ajax/boot/AjxTemplateCompiler.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2008, 2009, 2010 Zimbra, Inc.
+ * Copyright (C) 2008, 2009, 2010, 2011 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -52,7 +52,7 @@ AjxTemplate.compile = function(pkg, authoritative, define, templateText) {
 			var id = templateId ? packageId+"#"+templateId : packageId;
 
 			var func = AjxTemplate.__convertLines(body);
-			AjxTemplate.register(packageId, func, attrs, authoritative);
+			AjxTemplate.register(id, func, attrs, authoritative);
 
 			if (first && define) {
 				AjxPackage.define(packageId);
