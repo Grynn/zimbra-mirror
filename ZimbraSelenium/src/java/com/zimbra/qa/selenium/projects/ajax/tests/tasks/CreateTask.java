6956abff5d446a2b84e20e308891c0a66bd88ea1
 Unknown Perforce User <girishb>
2011-02-10 11:00:28 +0000

    bug:none
    Added new Class as CreateTask for creating task related test cases

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/CreateTask.java
@@ -0,0 +1,51 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.TaskItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.tasks.TaskNew;
+
+public class CreateTask extends AjaxCommonTest {
+
+	public CreateTask() {
+		// logger.info("New " + CreateDocument.class.getCanonicalName());
+		logger.info("New " + CreateTask.class.getCanonicalName());
+
+		super.startingPage = app.zPageTasks;
+
+		super.startingAccountPreferences = null;
+	}
+
+	@Test(description = "Create task through GUI - verify through GUI", groups = { "sanity" })
+	public void CreateTask_01() throws HarnessException {
+		ZimbraAccount account = app.zGetActiveAccount();
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(account,
+				SystemFolder.Tasks);
+
+		// Create task item
+		TaskItem task = new TaskItem();
+		String sub = task.gettaskSubject();
+		String body = task.gettaskBody();
+		TaskNew taskNew = (TaskNew) app.zPageTasks
+				.zToolbarPressButton(Button.B_NEW);
+
+		taskNew.zFill(task);
+
+		taskNew.zSubmit();
+
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		ZAssert.assertNotNull(sub, "Task is crerated successfully");
+
+	}
+
+}
