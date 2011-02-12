9eb47343acde76252321e5245ae6c083b14a8b56
komalk <komalk@localhost>
2011-02-12 07:30:58 +0000

    Integrate the following change from HELIX to main Change 275827 by komalk@komalk_macmain1 on 2011/02/11 23:27:46
            bug:56599
            -Respect the "first day of week" calendar preference for work week 
            as well.
    
            review URL:https://reviewboard.eng.vmware.com/r/208330
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=56599

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/BeanUtils.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/BeanUtils.java
@@ -861,15 +861,8 @@ public class BeanUtils {
          int dow = cal.get(Calendar.DAY_OF_WEEK);
 
         // pref goes 0-6, Calendar goes 1-7
-        if ("workWeek".equalsIgnoreCase(view)) {
-                if (dow == Calendar.SUNDAY)
-                    cal.add(Calendar.DAY_OF_MONTH, 1);
-                else if (dow != Calendar.MONDAY)
-                    cal.add(Calendar.DAY_OF_MONTH, - (dow - Calendar.MONDAY));
-        } else if ("week".equalsIgnoreCase(view)) {
-                if (dow != prefFirstDayOfWeek)
-                    cal.add(Calendar.DAY_OF_MONTH, - (((dow-1) + (7- (int)prefFirstDayOfWeek)) % 7));
-        }
+         if (dow != prefFirstDayOfWeek)
+             cal.add(Calendar.DAY_OF_MONTH, - (((dow-1) + (7- (int)prefFirstDayOfWeek)) % 7));
          return cal;
      }
 
