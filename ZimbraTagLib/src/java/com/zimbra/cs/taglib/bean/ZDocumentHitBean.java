9675ad7bfa27d18aada9d2514f4fbfaa3e0e8bea
 Unknown Perforce User <ysasaki>
2011-02-14 21:53:12 +0000

    bug:56015 Deprecate "score" attribute in SearchResponse - Since we upgraded Lucene, the score attribute in SearchResponse is always NaN (score="NaN") because in new Lucene the score retrieval is optional and the default is off. Since it's not used by clients, we shall remove it.
    http://bugzilla.zimbra.com/show_bug.cgi?id=56015

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZDocumentHitBean.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZDocumentHitBean.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2008, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2008, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -28,20 +28,16 @@ public class ZDocumentHitBean extends ZSearchHitBean {
     }
 
     public ZDocument getDocument() {
-    	return mHit.getDocument();
+        return mHit.getDocument();
     }
 
-	public String getDocId() {
-		return mHit.getId();
-	}
+    public String getDocId() {
+        return mHit.getId();
+    }
 
-	public float getDocScore() {
-		return mHit.getScore();
-	}
-
-	public String getDocSortField() {
-		return mHit.getSortField();
-	}
+    public String getDocSortField() {
+        return mHit.getSortField();
+    }
 
     public Date getCreatedDate() {
         return new Date(mHit.getDocument().getCreatedDate()/1000);
@@ -54,5 +50,5 @@ public class ZDocumentHitBean extends ZSearchHitBean {
     public Date getMetaDataChangedDate() {
         return new Date(mHit.getDocument().getMetaDataChangedDate()/1000);
     }
-  
+
 }
--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZSearchHitBean.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/bean/ZSearchHitBean.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -17,7 +17,7 @@ package com.zimbra.cs.taglib.bean;
 import com.zimbra.cs.zclient.ZSearchHit;
 
 public abstract class ZSearchHitBean {
-    
+
     public enum HitType { conversation, contact, message, voiceMailItem, call, task, briefcase, appointment, wiki }
 
     private HitType mHitType;
@@ -31,8 +31,6 @@ public abstract class ZSearchHitBean {
     public final String getId() { return mHit.getId(); }
 
     public final String getSortField() { return mHit.getSortField(); }
-
-    public final float getScore() { return mHit.getScore(); }
 
     public final String getHitType() { return mHitType.name(); }
 
