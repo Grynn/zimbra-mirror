9500e7fa5d13902da4e2da254f2cb9d903c7288e
 Unknown Perforce User <rajar>
2011-02-15 19:33:18 +0000

    bug:56777 - fix npe
    http://bugzilla.zimbra.com/show_bug.cgi?id=56777

--- Zimlet/src/zimlet/com_zimbra_phonelookup/phonelookup.js
+++ Zimlet/src/zimlet/com_zimbra_phonelookup/phonelookup.js
@@ -1,7 +1,7 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Zimlets
- * Copyright (C) 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
+ * Copyright (C) 2010 Zimbra, Inc.
  * 
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
@@ -111,7 +111,7 @@ function(jsonObj) {
 		return;
 	}
 	var listings = jsonObj.listings;
-	if(!listings.listing) {
+	if(!listings || !listings.listing) {
 		this._slide.setErrorMessage(this.getMessage("mustBeValidNumber"));
 		return;
 	}
