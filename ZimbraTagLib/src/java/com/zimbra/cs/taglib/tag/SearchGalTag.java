14a77c74546bf683999083265c61bb26d574fd81
komalk <komalk@localhost>
2011-02-28 10:13:27 +0000

    Integrate the following change from HELIX to main Change 278679 by komalk@komalk_macmain1 on 2011/02/28 02:06:09
            bug:57093
            -Add support for searchFilter attribute to SearchGalTag.
            Review :https://reviewboard.eng.vmware.com/r/211752/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57093

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/SearchGalTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/SearchGalTag.java
@@ -15,6 +15,7 @@
 package com.zimbra.cs.taglib.tag;
 
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.cs.taglib.bean.ZSearchGalResultBean;
 import com.zimbra.cs.zclient.ZMailbox;
 import com.zimbra.cs.zclient.ZMailbox.GalEntryType;
@@ -24,12 +25,15 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SearchGalTag extends ZimbraSimpleTag {
 
     private String mVar;
     private GalEntryType mType = null;
     private String mQuery = null;
+    private String mDept = null;
+    private String mEmail = null;
 
     public void setVar(String var) { this.mVar = var; }
 
@@ -39,11 +43,32 @@ public class SearchGalTag extends ZimbraSimpleTag {
         this.mQuery = query == null ? "" : query;
     }
 
+    public void setDept(String dept) {
+        this.mDept =  dept == null ? "" : dept;
+    }
+
+    public void setEmail(String email) {
+        this.mEmail =  email == null ? "" : email;
+    }
+
     public void doTag() throws JspException, IOException {
         JspContext jctxt = getJspContext();
         try {
+            final ArrayList<Object> conditions = new ArrayList<Object>();
+            if(mDept != null && !mDept.isEmpty())
+            {
+                conditions.add(new String[]{AccountConstants.A_DEPARTMENT,"has", mDept});
+            }
+            if(mEmail != null && !mEmail.isEmpty())
+            {
+                final ArrayList<Object> emailCond = new ArrayList<Object>();
+                emailCond.add(new String[]{AccountConstants.E_EMAIL,"has", mEmail});
+                emailCond.add(new String[]{AccountConstants.E_EMAIL2,"has", mEmail});
+                emailCond.add(new String[]{AccountConstants.E_EMAIL3,"has", mEmail});
+                conditions.add(emailCond);
+            }
             ZMailbox mbox = getMailbox();
-            jctxt.setAttribute(mVar, new ZSearchGalResultBean(mbox.searchGal(mQuery, mType)),  PageContext.PAGE_SCOPE);
+            jctxt.setAttribute(mVar, new ZSearchGalResultBean(mbox.searchGal(mQuery, conditions, mType)),  PageContext.PAGE_SCOPE);
         } catch (ServiceException e) {
             throw new JspTagException(e);
         }
