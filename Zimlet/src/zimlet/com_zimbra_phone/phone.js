cbb1a7758a2c3be496c72ec9932516908b29a308
 Unknown Perforce User <rajar>
2011-03-01 03:25:26 +0000

    bug:57203 Makesure to use boundaries to avoid partial matching
    http://bugzilla.zimbra.com/show_bug.cgi?id=57203

--- Zimlet/src/zimlet/com_zimbra_phone/phone.js
+++ Zimlet/src/zimlet/com_zimbra_phone/phone.js
@@ -42,7 +42,7 @@ function(line, startIndex) {
 Com_Zimbra_Phone.prototype.init =
 function() {
 	var regexps = [
-        new RegExp(this.getMessage("localPhoneRegEx"),"ig")
+        new RegExp("\\b" + this.getMessage("localPhoneRegEx") + "\\b","ig")
     ];
 	this.regexps = regexps;
 	this.countryCode = this.getMessage("countryCode");
