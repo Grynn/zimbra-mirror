5617a5bd5c67f27706ee093788c0b00079563c3a
Andrei Bareli <abareli@zimbra.com>
2011-03-08 21:39:18 +0000

    bug:none -integration of //depot/zimbra/HELIX/ZimbraSelenium/...@280570

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/file/TagFile.java
@@ -0,0 +1,101 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.briefcase.file;
+
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogTag;
+
+public class TagFile extends AjaxCommonTest {
+
+	public TagFile() {
+		logger.info("New " + TagFile.class.getCanonicalName());
+
+		// All tests start at the Briefcase page
+		super.startingPage = app.zPageBriefcase;
+
+		super.startingAccountPreferences = null;
+	}
+
+	@Test(description = "Tag a File using Toolbar -> Tag -> New Tag", groups = { "smoke" })
+	public void TagFile_01() throws HarnessException {
+		ZimbraAccount account = app.zGetActiveAccount();
+
+		FolderItem briefcaseFolder = FolderItem.importFromSOAP(account,
+				SystemFolder.Briefcase);
+
+		// Create document item
+		DocumentItem document = new DocumentItem();
+
+		String filePath = ZimbraSeleniumProperties.getBaseDirectory()
+				+ "/data/public/other/testpptfile.ppt";
+
+		String fileName = document.getFileName(filePath);
+
+		// Upload file to server through RestUtil
+		String attachmentId = account.uploadFile(filePath);
+
+		// Save uploaded file to briefcase through SOAP
+		account.soapSend("<SaveDocumentRequest xmlns='urn:zimbraMail'>"
+				+ "<doc l='" + briefcaseFolder.getId() + "'><upload id='"
+				+ attachmentId + "'/></doc></SaveDocumentRequest>");
+
+		// refresh briefcase page
+		app.zTreeBriefcase.zTreeItem(Action.A_LEFTCLICK, briefcaseFolder, true);
+
+		// Click on created document
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+		app.zPageBriefcase.zListItem(Action.A_LEFTCLICK, fileName);
+
+		// Create a tag using GUI
+		String tagName = "tag" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Click on New Tag
+		DialogTag dialogTag = (DialogTag) app.zPageBriefcase
+				.zToolbarPressPulldown(Button.B_TAG, Button.O_TAG_NEWTAG);
+
+		dialogTag.zSetTagName(tagName);
+		dialogTag.zClickButton(Button.B_OK);
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		// Make sure the tag was created on the server (get the tag ID)
+		account.soapSend("<GetTagRequest xmlns='urn:zimbraMail'/>");
+
+		String tagId = account.soapSelectValue(
+				"//mail:GetTagResponse//mail:tag[@name='" + tagName + "']",
+				"id");
+
+		// Verify tagged document name
+		account
+				.soapSend("<SearchRequest xmlns='urn:zimbraMail' types='document'>"
+						+ "<query>tag:"
+						+ tagName
+						+ "</query>"
+						+ "</SearchRequest>");
+
+		String name = account.soapSelectValue(
+				"//mail:SearchResponse//mail:doc", "name");
+
+		ZAssert.assertEquals(name, fileName, "Verify tagged document name");
+
+		// Make sure the tag was applied to the document
+		// account.soapSend("<SearchRequest xmlns='urn:zimbraMail' types='document'>"
+		// + "<query>in:briefcase</query></SearchRequest>");
+
+		// String id = account.soapSelectValue(
+		// "//mail:SearchResponse//mail:doc[@name='" + docName + "']", "t");
+
+		account
+				.soapSend("<SearchRequest xmlns='urn:zimbraMail' types='document'>"
+						+ "<query>" + fileName + "</query>" + "</SearchRequest>");
+
+		String id = account.soapSelectValue("//mail:SearchResponse//mail:doc",
+				"t");
+
+		ZAssert.assertEquals(id, tagId,
+				"Verify the tag was attached to the document");
+	}
+}
