1f9e75f377ca00dbccab74271dcde82463f0f0df
 Unknown Perforce User <rhoades>
2011-03-15 21:59:51 +0000

    Integrate Change 281742 by rhoades@MATTXPDEV on 2011/03/15 14:59:04
    Bug: 31535, 34401
    
    Selenium Harness: add test case for bug 31535
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=31535
    http://bugzilla.zimbra.com/show_bug.cgi?id=34401

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/bugs/Bug31535.java
@@ -0,0 +1,60 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.bugs;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail.Field;
+
+
+public class Bug31535 extends AjaxCommonTest {
+
+
+	
+	@SuppressWarnings("serial")
+	public Bug31535() {
+		logger.info("New "+ Bug31535.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+
+		// Make sure we are using an account with message view
+		super.startingAccountPreferences = new HashMap<String, String>() {{
+			put("zimbraPrefGroupMailBy", "message");
+			put("zimbraPrefMessageViewHtmlPreferred", "TRUE");
+		}};
+
+
+	}
+
+	@Test(	description = "Verify bug 34401",
+			groups = { "functional" })
+	public void Bug_34401() throws HarnessException {
+
+		String subject = "subject13002239738283";
+
+		String MimeFolder = ZimbraSeleniumProperties.getBaseDirectory() + "/data/private/mime/Bugs/Bug31535";
+		LmtpInject.injectFile(ZimbraAccount.AccountZWC().EmailAddress, new File(MimeFolder));
+
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail display = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		HtmlElement bodyElement = display.zGetMailPropertyAsHtml(Field.Body);
+		
+		HtmlElement.evaluate(bodyElement, "//body//img", "class", "InlineImage", 1);
+
+
+	}
+
+
+
+}
