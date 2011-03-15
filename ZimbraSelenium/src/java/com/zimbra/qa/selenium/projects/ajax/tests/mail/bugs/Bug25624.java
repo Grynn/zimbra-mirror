1414f38e544e1d850e9e9e486a95ae970dd03e32
 Unknown Perforce User <rhoades>
2011-03-15 00:18:06 +0000

    Integrate Change 281569 by rhoades@MATTXPDEV on 2011/03/14 17:17:26
    Bug: 25624
    
    Selenium Harness: add test case
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=25624

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/bugs/Bug25624.java
@@ -0,0 +1,57 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.bugs;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail.Field;
+
+
+public class Bug25624 extends AjaxCommonTest {
+
+
+	
+	@SuppressWarnings("serial")
+	public Bug25624() {
+		logger.info("New "+ Bug25624.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+
+		// Make sure we are using an account with message view
+		super.startingAccountPreferences = new HashMap<String, String>() {{
+			put("zimbraPrefGroupMailBy", "message");
+			put("zimbraPrefMessageViewHtmlPreferred", "TRUE");
+		}};
+
+
+	}
+
+	@Test(	description = "Verify bug 25624",
+			groups = { "functional" })
+	public void Bug_25624() throws HarnessException {
+
+		String subject = "subject13001430504373";
+
+		String MimeFolder = ZimbraSeleniumProperties.getBaseDirectory() + "/data/private/mime/Bugs/Bug25624";
+		LmtpInject.injectFile(ZimbraAccount.AccountZWC().EmailAddress, new File(MimeFolder));
+
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail display = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		String body = display.zGetMailProperty(Field.Body);
+		ZAssert.assertStringContains(body, "Hi Candace", "Verify the message content is not blank");
+	}
+
+
+
+}
