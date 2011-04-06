26d6613ce17f6ff33938b346b87d5fdec5ca4bdf
 Unknown Perforce User <mizhang>
2011-04-06 04:44:30 +0000

    Integrate change 288701 from zimbra to zimbra
    -------------
    Bug 58652
    Add missed attribute in ZaAccount Model to make it work well.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=58652

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/accounts/model/ZaAccount.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/accounts/model/ZaAccount.js
@@ -1791,6 +1791,7 @@ ZaAccount.myXModel = {
         {id:ZaAccount.A_zimbraPrefMailFlashTitle, choices:ZaModel.BOOLEAN_CHOICES, ref:"attrs/"+ZaAccount.A_zimbraPrefMailFlashTitle, type:_COS_ENUM_},
         {id:ZaAccount.A_zimbraPrefMailSoundsEnabled, choices:ZaModel.BOOLEAN_CHOICES, ref:"attrs/"+ZaAccount.A_zimbraPrefMailSoundsEnabled, type:_COS_ENUM_},
         {id:ZaAccount.A_zimbraPrefOutOfOfficeReply, type:_STRING_, ref:"attrs/"+ZaAccount.A_zimbraPrefOutOfOfficeReply},
+	{id:ZaAccount.A_zimbraPrefOutOfOfficeReplyEnabled, choices:ZaModel.BOOLEAN_CHOICES, ref:"attrs/"+ZaAccount.A_zimbraPrefOutOfOfficeReplyEnabled, type:_COS_ENUM_},
 
         {id:ZaAccount.A_zimbraPrefReplyToAddress, type:_STRING_, ref:"attrs/"+ZaAccount.A_zimbraPrefReplyToAddress},
         {id:ZaAccount.A_zimbraPrefUseKeyboardShortcuts, type:_COS_ENUM_, ref:"attrs/"+ZaAccount.A_zimbraPrefUseKeyboardShortcuts, choices:ZaModel.BOOLEAN_CHOICES},
