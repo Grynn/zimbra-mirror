111652366f4812023eedb58b82b763807e478486
Andrei Bareli <abareli@zimbra.com>
2011-04-11 23:54:43 +0000

    bug:none -integration of //depot/zimbra/HELIX/ZimbraSelenium/...@291764

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/file/DeleteFile.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/file/DeleteFile.java
@@ -1,8 +1,5 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.briefcase.file;
 
-import java.util.EnumMap;
-import java.util.HashMap;
-
 import org.testng.annotations.Test;
 import com.zimbra.qa.selenium.framework.items.FileItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem;
@@ -17,7 +14,6 @@ import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
 import com.zimbra.qa.selenium.projects.ajax.ui.briefcase.DialogDeleteConfirm;
-import com.zimbra.qa.selenium.projects.ajax.ui.briefcase.PageBriefcase;
 
 public class DeleteFile extends AjaxCommonTest {
 
