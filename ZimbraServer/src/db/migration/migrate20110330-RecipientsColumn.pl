4c2bedcb256038759af67ba50b72adf07419e481
 Unknown Perforce User <ysasaki>
2011-04-11 17:41:09 +0000

    bug:43789,57606	DB schema migration scripts for MAIL_ITEM table and SENDER_ID column - Revert the previous DB version bump where I removed AUTO_INCREMENT from MAIL_ADDRESS.ID. This change is now included in the first schema change.
    https://reviewboard.eng.vmware.com/r/224108/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=43789
    http://bugzilla.zimbra.com/show_bug.cgi?id=57606

---
+++ ZimbraServer/src/db/migration/migrate20110330-RecipientsColumn.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Zimbra Collaboration Suite Server
+# Copyright (C) 2011 Zimbra, Inc.
+#
+# The contents of this file are subject to the Zimbra Public License
+# Version 1.3 ("License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://www.zimbra.com/license.
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# ***** END LICENSE BLOCK *****
+#
+
+use strict;
+use Migrate;
+
+########################################################################################################################
+
+Migrate::verifySchemaVersion(68);
+
+foreach my $group (Migrate::getMailboxGroups()) {
+  addRecipientsColumn($group);
+}
+
+Migrate::updateSchemaVersion(68, 69);
+
+exit(0);
+
+########################################################################################################################
+
+sub addRecipientsColumn($) {
+  my ($group) = @_;
+
+  my $sql = <<_EOF_;
+ALTER TABLE $group.mail_item ADD COLUMN recipients VARCHAR(128);
+ALTER TABLE $group.mail_item_dumpster ADD COLUMN recipients VARCHAR(128);
+_EOF_
+
+  Migrate::log("Adding RECIPIENTS column to $group.MAIL_ITEM and $group.MAIL_ITEM_DUMPSTER...");
+  Migrate::runSql($sql);
+}
