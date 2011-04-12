c09c398cb23fdf86e747062e6849a914b9928133
Eran Yarkon <eyarkon@zimbra.com>
2011-04-12 22:57:03 +0000

    Integrate change 292288 from HELIX to main
    -------------
    Bug 57542
    new help support - online help/new features/about. a new link-button is used instead of the old help link. It looks like a link but also has a drop-down.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=57542

---
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtLinkButton.js
@@ -0,0 +1,29 @@
+/**
+ * @overview
+ * A link that is a button. Currently used only for the "help" link/button
+ *
+ * @author Eran Yarkon
+ *
+ * @extends	DwtButton
+
+ */
+DwtLinkButton = function(params) {
+	params.className = params.className || "ZButtonLink";
+	DwtButton.call(this, params);
+};
+
+
+DwtLinkButton.prototype = new DwtButton;
+DwtLinkButton.prototype.constructor = DwtLinkButton;
+
+DwtButton.prototype.TEMPLATE = "dwt.Widgets#ZLinkButton";
+
+// defaults for drop down images (set here once on prototype rather than on each button instance)
+DwtLinkButton.prototype._dropDownImg 	= "HelpPullDownArrow";
+DwtLinkButton.prototype._dropDownDepImg	= "HelpPullDownArrow";
+DwtLinkButton.prototype._dropDownHovImg = "HelpPullDownArrowHover";
+
+DwtLinkButton.prototype.toString =
+function() {
+	return "DwtLinkButton";
+};
