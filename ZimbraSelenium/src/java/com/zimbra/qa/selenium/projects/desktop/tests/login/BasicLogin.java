f73612472ff5f8cffb2abfc7ee68de76c4253c58
 Unknown Perforce User <jhidayat>
2011-04-20 19:07:19 +0000

    Integrate Change 296450 by jhidayat@ZimbraQA_jhidayat on 4/20/2011 12:04:36 PM Bug: None Fix the login tests for Desktop.

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/tests/login/BasicLogin.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/tests/login/BasicLogin.java
@@ -2,45 +2,26 @@ package com.zimbra.qa.selenium.projects.desktop.tests.login;
 
 import org.testng.annotations.Test;
 
-public class BasicLogin{
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.projects.desktop.core.AjaxCommonTest;
+
+public class BasicLogin extends AjaxCommonTest {
 	
 	public BasicLogin() {	
 	}
-	
-	/**@Test(	description = "Login to the Ajax Client",
+
+	@Test(	description = "Login to the Ajax Client",
 			groups = { "sanity" })
 	public void BasicLogin01() throws HarnessException {
-		
+
 		// Login
 		app.zPageLogin.zLogin(ZimbraAccount.AccountZMC());
 		
 		// Verify main page becomes active
 		ZAssert.assertTrue(app.zPageMain.zIsActive(), "Verify that the account is logged in");
-		
-	}*/
 
-	@Test(groups={"sanity"})
-   public void Pos001() {
-      System.out.println("pos001");
-   }
+	}
 
-   @Test(groups={"sanity", "non-desktop"})
-   public void Pos002() {
-      System.out.println("pos002");
-   }
-
-   @Test(groups={"sanity", "desktop-specific"})
-   public void Pos003() {
-      System.out.println("pos003");
-   }
-
-   @Test(groups={"smoke"})
-   public void Pos004() {
-      System.out.println("pos004");
-   }
-
-   @Test(groups={"functional"})
-   public void Pos005() {
-      System.out.println("pos005");
-   }
 }
