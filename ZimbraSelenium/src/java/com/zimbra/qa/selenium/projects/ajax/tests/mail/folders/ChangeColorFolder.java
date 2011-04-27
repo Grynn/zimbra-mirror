65ebee42e6129eba12c3e30943f2cf4baae35ff7
 Unknown Perforce User <rhoades>
2011-04-27 18:51:51 +0000

    Integrate Change 300104 by rhoades@MATTXPDEV on 2011/04/27 11:51:14
    Bug: none
    
    Selenium Harness: refactor EditFolder
    
    1. Rename folder exists in RenameFolder.java
    2. Put Change Color into ChangeColorFolder.java

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/folders/ChangeColorFolder.java
@@ -0,0 +1,66 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.folders;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DialogEditFolder;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DialogEditFolder.FolderColor;
+
+public class ChangeColorFolder extends AjaxCommonTest {
+
+	public ChangeColorFolder() {
+		logger.info("New " + ChangeColorFolder.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+		super.startingAccountPreferences = null;
+
+	}
+
+	@Test(description = "Edit a folder, change the color (Context menu -> Edit)", groups = { "functional" })
+	public void ChangeColorFolder_01() throws HarnessException {
+
+		FolderItem inbox = FolderItem.importFromSOAP(app.zGetActiveAccount(),
+				SystemFolder.Inbox);
+		ZAssert.assertNotNull(inbox, "Verify the inbox is available");
+
+		// Create the subfolder
+		String name1 = "folder" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + name1 + "' l='" + inbox.getId() + "'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subfolder1 = FolderItem.importFromSOAP(app
+				.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder1, "Verify the subfolder is available");
+
+		// Click on Get Mail to refresh the folder list
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Rename the folder using context menu
+		DialogEditFolder dialog = (DialogEditFolder) app.zTreeMail.zTreeItem(
+				Action.A_RIGHTCLICK, Button.B_TREE_EDIT, subfolder1);
+		ZAssert.assertNotNull(dialog, "Verify the dialog opened");
+
+		// Change the color, click OK
+		dialog.zSetNewColor(FolderColor.Gray);
+		dialog.zClickButton(Button.B_OK);
+
+		// Get all the folders and verify the new name appears and the old name
+		// disappears
+		FolderItem subfolder2 = FolderItem.importFromSOAP(app
+				.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder2, "Verify the subfolder is available");
+
+		// Check the color
+		throw new HarnessException("implement the verification");
+
+	}
+
+}
