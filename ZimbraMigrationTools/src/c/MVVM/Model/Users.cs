b1e99ac864838c719715289de85464a41126e9df
Karuna Nuthi <knuthi@zimbra.com>
2011-05-17 18:46:00 +0000

    Reading and writing to CSV, user mapping information bug: none

--- ZimbraMigrationTools/src/c/MVVM/Model/Users.cs
+++ ZimbraMigrationTools/src/c/MVVM/Model/Users.cs
@@ -2,9 +2,13 @@
 namespace MVVM.Model
 {
     using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
 
     public class Users
     {
+        public Users() { }
         internal Users(string usernameEntered, int currentUserSelection)
         {
             this.UsernameEntered = usernameEntered;
@@ -25,5 +29,34 @@ namespace MVVM.Model
         {
             get; set;
         }
+        public static string ToCsv<T>(string separator, IEnumerable<T> objectlist)
+        {
+            Type t = typeof(T);
+            System.Reflection.FieldInfo[] fields = t.GetFields();
+            string header = String.Join(separator, fields.Select(f => f.Name).ToArray());
+            StringBuilder csvdata = new StringBuilder();
+            csvdata.AppendLine(header);
+            foreach (var o in objectlist)
+                csvdata.AppendLine(ToCsvFields(separator, fields, o));
+            return csvdata.ToString();
+        }
+
+        public static string ToCsvFields(string separator, System.Reflection.FieldInfo[] fields, object o)
+        {
+            StringBuilder linie = new StringBuilder();
+            foreach (var f in fields)
+            {
+                if (linie.Length > 0)
+                    linie.Append(separator); var x = f.GetValue(o);
+                if (x != null)
+                    linie.Append(x.ToString());
+            }
+            return linie.ToString();
+        }
+        public string UserName;
+        public string MappedName;
+        public bool ChangePWD;
+        public string PWDdefault;
+
     }
 }
