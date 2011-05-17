54c2fb729f03bf6f9906bc0e540629c5c63c8798
 Unknown Perforce User <dieunguyen>
2011-05-17 22:42:57 +0000

    Integrate to main bug:none - Add contanst action A_CHECKBOX

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/ui/Action.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/ui/Action.java
@@ -38,6 +38,7 @@ public class Action {
 	public static final Action A_CTRLSELECT = new Action("A_CTRLSELECT");
 	public static final Action A_RIGHTCLICK = new Action("A_RIGHTCLICK");
 	public static final Action A_DOUBLECLICK = new Action("A_DOUBLECLICK");
+	public static final Action A_CHECKBOX = new Action("A_CHECK");
 
 
 	// Mail page actions
