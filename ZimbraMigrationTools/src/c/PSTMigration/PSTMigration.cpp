29eeae715d4528ef26b87069ea7624f1cc7f9c48
 Unknown Perforce User <tfr>
2011-05-24 18:50:31 +0000

    bug: none convert to console app

--- ZimbraMigrationTools/src/c/PSTMigration/PSTMigration.cpp
+++ ZimbraMigrationTools/src/c/PSTMigration/PSTMigration.cpp
@@ -4,15 +4,10 @@
 #include "Zimbra/Rpc.h"
 
 
-int APIENTRY wWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+int main(int argc, TCHAR *argv[])
 {
-	UNREFERENCED_PARAMETER(hInstance);
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	UNREFERENCED_PARAMETER(nCmdShow);
+	UNREFERENCED_PARAMETER(argc);
+	UNREFERENCED_PARAMETER(argv);
 	
 	Zimbra::Rpc::UserSession::CreateInstance(_T("dummy profile"),L"av1@zcs2.zmexch.in.zimbra.com",L"test123",L"10.117.82.163",7071,false, false , NULL);
 	//Zimbra::Rpc::SendMsgRequest sendMsg( L"testtoken" );
