a7de184bd63b82fedfa7a5ad3136a5e7340c6355
Amitkumar Jagtap <ajagtap@zimbra.com>
2011-05-30 05:54:43 +0000

    bug:none   -- Integration from HELIX to MAIN.

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/admin/tests/resources/CreateResource.java
@@ -0,0 +1,60 @@
+package com.zimbra.qa.selenium.projects.admin.tests.resources;
+
+
+
+import org.testng.annotations.Test;
+
+import com.zimbra.common.soap.Element;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAdminAccount;
+import com.zimbra.qa.selenium.projects.admin.core.AdminCommonTest;
+import com.zimbra.qa.selenium.projects.admin.items.ResourceItem;
+import com.zimbra.qa.selenium.projects.admin.ui.WizardCreateResource;
+
+public class CreateResource extends AdminCommonTest {
+	public CreateResource() {
+		logger.info("New " + CreateResource.class.getCanonicalName());
+		
+		//Every test starts at Resource page
+		super.startingPage=app.zPageManageResources;
+	}
+	
+	
+	/**
+	 * Testcase : Create a basic resource.
+	 * Steps :
+	 * 1. Create a resource from GUI.
+	 * 2. Verify resource is created using SOAP.
+	 * @throws HarnessException
+	 */
+	@Test(	description = "Create a basic resource",
+			groups = { "sanity" })
+			public void CreateResource_01() throws HarnessException {
+
+		// Create a new resource in the Admin Console
+		ResourceItem resource = new ResourceItem();
+		
+
+		// Click "New"
+		WizardCreateResource wizard = 
+			(WizardCreateResource)app.zPageManageResources.zToolbarPressButton(Button.B_NEW);
+
+		// Fill out the wizard and click Finish
+		wizard.zCompleteWizard(resource);
+
+
+		// Verify the resource exists in the ZCS
+		ZimbraAdminAccount.AdminConsoleAdmin().soapSend(
+				"<GetCalendarResourceRequest xmlns='urn:zimbraAdmin'>"
+				+ 		"<calresource by='name'>" +  resource.getEmailAddress() + "</calresource>"  
+				+		"</GetCalendarResourceRequest>");
+
+		Element response = ZimbraAdminAccount.AdminConsoleAdmin().soapSelectNode("//admin:GetCalendarResourceResponse/admin:calresource", 1); 
+		ZAssert.assertNotNull(response, "Verify the RESOURCE is created successfully");
+
+	}
+
+}
+
