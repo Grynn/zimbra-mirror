a89d049c7be7bdb0ae84b64f742c67f302139c60
Karuna Nuthi <knuthi@zimbra.com>
2011-06-04 00:17:08 +0000

    bug:58320 MigrationConsole app in C#. Its a csharp application. This communicates to the exchangemigration com dlls using the C# wrapper dll.
    http://bugzilla.zimbra.com/show_bug.cgi?id=58320

---
+++ ZimbraMigrationTools/src/c/ZimbraMigrationConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CssLib;
+
+namespace ZimbraMigrationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CssLib.CSMigrationwrapper TestObj = new CSMigrationwrapper();
+            TestObj.ConfigXMLFile = @"C:\Temp\ZimbraAdminOverview.xml";
+            TestObj.UserMapFile = @"UserMap3.csv";
+            TestObj.MailClient = "MAPI";
+            TestObj.Initalize();
+        }
+    }
+}
