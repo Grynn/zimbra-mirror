27ad6184c30414dabfd360701189f0428e8c0c9c
 Unknown Perforce User <dieunguyen>
2011-06-16 23:37:21 +0000

    Integration to main bug:none - Add test cases to check functionality of addressbook alphabet bar

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contactgroups/ViewContactGroup.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contactgroups/ViewContactGroup.java
@@ -146,9 +146,8 @@ public class ViewContactGroup extends AjaxCommonTest  {
         GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
         app.zTreeContacts.zTreeItem(Action.A_LEFTCLICK, contactFolder);
         
-        //click 123      
+        //click B      
 		app.zPageAddressbook.zToolbarPressButton(Button.B_AB_B);
-		SleepUtil.sleepLong();
 		
 		//verify all group started with B listed, other groups not listed
 		List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts();
@@ -166,6 +165,49 @@ public class ViewContactGroup extends AjaxCommonTest  {
 	
 		ZAssert.assertTrue(countGroup==2, "Verify contact groups " + group1.fileAs + " " + group0.fileAs + " displayed, and " + group2.fileAs + "," + group3.fileAs + " not displayed ");
 	}
-
+	
+	@Test(	description = "Click all Alphabetbar buttons: Verify only contact group started with the alphabet is listed ",
+			groups = { "functional" })
+	public void ClickAllAlphabetBarButtons() throws HarnessException {
+	
+		 // Create  contact groups 
+		ContactGroupItem[]    cgiArray = new ContactGroupItem[26];
+		Button[]           buttonArray = 
+		 {Button.B_AB_A,Button.B_AB_B,Button.B_AB_C,Button.B_AB_D,Button.B_AB_E,Button.B_AB_F,Button.B_AB_G,
+		  Button.B_AB_H,Button.B_AB_I,Button.B_AB_J,Button.B_AB_K,Button.B_AB_L,Button.B_AB_M,Button.B_AB_N,
+		  Button.B_AB_O,Button.B_AB_P,Button.B_AB_Q,Button.B_AB_R,Button.B_AB_S,Button.B_AB_T,Button.B_AB_U,
+		  Button.B_AB_V,Button.B_AB_W,Button.B_AB_X,Button.B_AB_Y,Button.B_AB_Z};
+		
+		for (int i=0; i<26; i++) {
+			cgiArray[i] = createContactGroup(Character.toString((char)((int)'a' + i)));
+		}		
+        
+        // Refresh the view, to pick up the new contact
+        FolderItem contactFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), "Contacts");
+        GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+        app.zTreeContacts.zTreeItem(Action.A_LEFTCLICK, contactFolder);
+        
+        for (int i=0; i<26; i++) {
+          //click button      
+		  app.zPageAddressbook.zToolbarPressButton(buttonArray[i]);
+		
+		  //verify group started with button name listed
+		  List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts();
+		  int countGroup= 0;
+		  for (ContactItem ci : contacts) {
+			if (ci.fileAs.equals(cgiArray[i].fileAs) || ci.fileAs.equals(cgiArray[i].fileAs)) 
+			{
+	            countGroup++;
+			}
+		//	if (ci.fileAs.equals(group2.fileAs) || ci.fileAs.equals(group3.fileAs) )  {
+			//	countGroup--; 
+		//	}
+				
+		}
+	
+		ZAssert.assertTrue(countGroup==1, "Verify contact groups " + cgiArray[i].fileAs + " " + cgiArray[i].fileAs + " displayed");
+       
+        }
+	}   
 }
 
