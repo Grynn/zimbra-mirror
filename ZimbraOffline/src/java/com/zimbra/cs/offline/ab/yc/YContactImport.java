1d0adfc9cbfbdb8f5bbde41abff0001d47a9bfd4
David Wang <dywang@zimbra.com>
2011-06-21 16:25:41 +0000

    bug: 60890 Integrate from ZDESKTOP-711 to main
    	Change 317650 by dywang@dywang-mac-zd711 on 2011/06/20 10:14:29
    
           bug: 60890
           Make migration transactional
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=60890

--- ZimbraOffline/src/java/com/zimbra/cs/offline/ab/yc/YContactImport.java
+++ ZimbraOffline/src/java/com/zimbra/cs/offline/ab/yc/YContactImport.java
@@ -62,7 +62,7 @@ public class YContactImport implements DataSource.DataImport {
             }
             session.sync();
         } catch (Exception e) {
-            OfflineLog.yab.error("Failed to import Yahoo contacts for account '%s'", ds.getName());
+            OfflineLog.yab.error("Failed to import Yahoo contacts for account '%s'", ds.getName(), e);
             if (e instanceof OAuthException) {
                 throw new YContactException("Failed to import yahoo contacts, please edit account setup and verify access.", 
                         e.getMessage(), false, e, null);
