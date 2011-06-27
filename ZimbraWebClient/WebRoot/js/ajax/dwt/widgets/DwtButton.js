398910266b58f6374e483ac53eb701a0db7f31ce
Conrad Damon <cdamon@zimbra.com>
2011-06-27 18:06:39 +0000

    Bug: 46222 - Improve display of conversations Part two of previous checkin. IntelliJ seems to randomly omit files from checkins occasionally.
    http://bugzilla.zimbra.com/show_bug.cgi?id=46222

--- ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtButton.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtButton.js
@@ -229,30 +229,9 @@ function (enabledImg, disImg, hovImg, depImg) {
  * Sets the Drop Down Hover Image
  */
 DwtButton.prototype.setDropDownHovImage =
-function(hovImg)
-{
+function(hovImg) {
     this._dropDownHovImg = hovImg;    
 }
-
-/**
- * @private
- */
-DwtButton.prototype._addEventListeners =
-function(listeners, events) {
-	for (var i = 0; i < events.length; i++) {
-		this.addListener(event, listeners[event]);
-	}
-};
-
-/**
- * @private
- */
-DwtButton.prototype._removeEventListeners =
-function(listeners, events) {
-	for (var i = 0; i < events.length; i++) {
-		this.removeListener(event, listeners[event]);
-	}
-};
 
 /**
  * @private
@@ -840,8 +819,8 @@ function(ev) {
 	mouseEv.setFromDhtmlEvent(ev);
 
 	if (mouseEv.button == DwtMouseEvent.LEFT) {
-	    if (this._hovImg && !this.noMenuBar) {
-			AjxImg.setImage(this, this._hovImg);
+	    if (this._dropDownHovImg && !this.noMenuBar) {
+			AjxImg.setImage(this, this._dropDownHovImg);
 	    }
 
 		DwtEventManager.notifyListeners(DwtEvent.ONMOUSEDOWN, mouseEv);
