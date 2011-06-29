5ef1dff324a8e288669f8be7432ca0e562a96fe4
Amitkumar Jagtap <ajagtap@zimbra.com>
2011-06-29 12:09:50 +0000

    bug:none   -- Integration from HELIX to MAIN.

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/admin/tests/domains/DeleteDomain.java
@@ -0,0 +1,79 @@
+package com.zimbra.qa.selenium.projects.admin.tests.domains;
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.common.soap.Element;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAdminAccount;
+import com.zimbra.qa.selenium.projects.admin.core.AdminCommonTest;
+import com.zimbra.qa.selenium.projects.admin.items.AccountItem;
+import com.zimbra.qa.selenium.projects.admin.items.DomainItem;
+import com.zimbra.qa.selenium.projects.admin.ui.DialogForDeleteOperation;
+
+public class DeleteDomain extends AdminCommonTest {
+	public DeleteDomain() {
+		logger.info("New" + DeleteDomain.class.getCanonicalName());
+
+		//All tests starts from domain page
+		this.startingPage=app.zPageManageDomains;
+	}
+
+	/**
+	 * Testcase : Verify delete domain operation.
+	 * Steps :
+	 * 1. Create a domain using SOAP.
+	 * 2. Remove galsync account created for domain using soap.
+	 * 3. Select the domain to delete.
+	 * 4. Verify domain is deleted using soap.
+	 * @throws HarnessException
+	 */
+	@Test(	description = "Verify delete domain operation",
+			groups = { "smoke" })
+			public void DeleteDomain_01() throws HarnessException {
+
+		// Create a new domain in the Admin Console using SOAP
+		DomainItem domain = new DomainItem();
+
+		ZimbraAdminAccount.AdminConsoleAdmin().soapSend(
+				"<CreateDomainRequest xmlns='urn:zimbraAdmin'>"
+				+			"<name>" + domain.getName() + "</name>"
+				+		"</CreateDomainRequest>");
+
+
+		// Enter the search string to find the domain
+		app.zPageSearchResults.zAddSearchQuery(domain.getName());
+
+		// Click search
+		app.zPageSearchResults.zToolbarPressButton(Button.B_SEARCH);
+
+		// Click on domain to be deleted.
+		app.zPageSearchResults.zListItem(Action.A_LEFTCLICK, domain.getName());
+
+		// Click on Delete button
+		DialogForDeleteOperation dialog = (DialogForDeleteOperation) app.zPageSearchResults.zToolbarPressButton(Button.B_DELETE);
+
+		// Click Yes in Confirmation dialog.
+		dialog.zClickButton(Button.B_YES);
+
+		// Click Ok on "Delete Items" dialog
+		dialog.zClickButton(Button.B_OK);
+
+		// Verify the domain do not exists in the ZCS
+		ZimbraAdminAccount.AdminConsoleAdmin().soapSend(
+				"<GetDomainRequest xmlns='urn:zimbraAdmin'>"
+				+	"<domain by='name'>" + domain.getName() + "</domain>"
+				+	"</GetDomainRequest>");
+
+
+		Element response = ZimbraAdminAccount.AdminConsoleAdmin().soapSelectNode("//admin:GetDomainResponse/admin:domain", 1);
+		ZAssert.assertNull(response, "Verify the domain is deleted successfully");
+
+	}
+
+
+}
