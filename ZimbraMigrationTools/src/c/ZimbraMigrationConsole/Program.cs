7a81c60241492d2ff64a34b7565e0bf637f5c9ea
Karuna Nuthi <knuthi@zimbra.com>
2011-07-07 22:23:13 +0000

    bug:58320
    changes to integretae ZimbraAPI in the console program.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=58320

--- ZimbraMigrationTools/src/c/ZimbraMigrationConsole/Program.cs
+++ ZimbraMigrationTools/src/c/ZimbraMigrationConsole/Program.cs
@@ -14,45 +14,74 @@ namespace ZimbraMigrationConsole
     {
         static void Main(string[] args)
         {
-            /*CssLib.CSMigrationwrapper TestObj = new CSMigrationwrapper();
-           
-            TestObj.ConfigXMLFile = @"C:\Temp\ZimbraAdminOverview.xml";
-            TestObj.UserMapFile = @"UserMap3.csv";
-            TestObj.MailClient = "MAPI";*/
-
-
-            
-
-            
-
-            
-
-
-            XmlConfig myXmlConfig = new XmlConfig(@"C:\Temp\ZimbraAdminOverview.xml",@"C:\Temp\UserMap.csv");
-
-            myXmlConfig.InitializeConfig();
-
-           
-           /* TestObj.Initalize(myXmlConfig.ConfigObj.zimbraServer.HostName,myXmlConfig.ConfigObj.zimbraServer.Port,myXmlConfig.ConfigObj.zimbraServer.AdminAccount);
-           
-            Thread.Sleep(500);*/
-           // string test = myXmlConfig.ConfigObj.zimbraServer.AdminAccount;
-
-            myXmlConfig.GetUserList();
-            foreach( MVVM.Model.Users user in  myXmlConfig.UserList)
+            if (File.Exists(@"C:\Temp\ZimbraAdminOverView.xml") && File.Exists(@"C:\Temp\UserMap.csv"))
             {
-                Account userAcct = new Account();
-                
-                System.Console.WriteLine("Connecting to to Zimbra Server \n");
-                System.Console.WriteLine("......... \n");
-                userAcct.InitializeMigration(myXmlConfig.ConfigObj.zimbraServer.HostName,myXmlConfig.ConfigObj.zimbraServer.Port,myXmlConfig.ConfigObj.zimbraServer.AdminAccount);
-                System.Console.WriteLine("Migration to Zimbra Started  for UserAccount " + user.UserName);
-                userAcct.StartMigration(user.UserName, myXmlConfig.ConfigObj.importOptions.Mail.ToString());
-                System.Console.WriteLine("......... \n");
-                Thread.Sleep(9000);
-                
+
+                XmlConfig myXmlConfig = new XmlConfig(@"C:\Temp\ZimbraAdminOverview.xml", @"C:\Temp\UserMap.csv");
+
+                myXmlConfig.InitializeConfig();
+
+                myXmlConfig.GetUserList();
+                if (myXmlConfig.UserList.Count > 0)
+                {
+                    foreach (MVVM.Model.Users user in myXmlConfig.UserList)
+                    {
+                        Account userAcct = new Account();
+
+                        System.Console.WriteLine("Connecting to to Zimbra Server \n");
+                        System.Console.WriteLine("......... \n");
+                        ZimbraAPI zimbraAPI = new ZimbraAPI();
+
+                        int stat = zimbraAPI.Logon(myXmlConfig.ConfigObj.zimbraServer.HostName, myXmlConfig.ConfigObj.zimbraServer.Port, myXmlConfig.ConfigObj.zimbraServer.AdminAccount, myXmlConfig.ConfigObj.zimbraServer.AdminPassword, true);
+                        if (stat != 0)
+                        {
+                            zimbraAPI.LastError.Count();
+                            System.Console.WriteLine("Logon to to Zimbra Server  for adminAccount failed ");
+                        }
+
+                        userAcct.InitializeMigration(myXmlConfig.ConfigObj.zimbraServer.HostName, myXmlConfig.ConfigObj.zimbraServer.Port, myXmlConfig.ConfigObj.zimbraServer.AdminAccount);
+
+
+                        string acctName = user.UserName + '@' + myXmlConfig.ConfigObj.UserProvision.Domain;
+                        if (zimbraAPI.GetAccount(acctName) == 0)
+                        {
+                            System.Console.WriteLine("Migration to Zimbra Started  for UserAccount " + user.UserName);
+                            userAcct.StartMigration(user.UserName, myXmlConfig.ConfigObj.importOptions.Mail.ToString());
+                            System.Console.WriteLine("......... \n");
+                            Thread.Sleep(9000);
+                        }
+                        else
+                        {
+
+                            System.Console.WriteLine("User is not provisioned on Zimbra Server " + user.UserName);
+                            System.Console.WriteLine("provisioning user " + user.UserName);
+                            if (zimbraAPI.CreateAccount(acctName, myXmlConfig.ConfigObj.UserProvision.DefaultPWD, myXmlConfig.ConfigObj.UserProvision.COS) == 0)
+                            {
+                                System.Console.WriteLine("provisioning user success " + user.UserName);
+                                System.Console.WriteLine("Migration to Zimbra Started  for UserAccount " + user.UserName);
+                                userAcct.StartMigration(user.UserName, myXmlConfig.ConfigObj.importOptions.Mail.ToString());
+                                System.Console.WriteLine("......... \n");
+                                Thread.Sleep(9000);
+                            }
+                            else
+                                System.Console.WriteLine("error provisioning user " + user.UserName);
+
+
+
+                        }
+
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("There are no user accounts to be migrated in the usermap file ");
+                }
             }
-            //Thread.Sleep(18000);
+            else
+            {
+                System.Console.WriteLine("There are no configuration or usermap files.make sure the xml and CSV files are at temp folder ");
+            }
+             //Thread.Sleep(18000);
         }
          
         }
