5488156ad43841e8ec1a76237b29a7a9460d34ba
 Unknown Perforce User <ysasaki>
2011-07-11 20:59:35 +0000

    bug:61865 Use ConcurrentHashMap The profiler indicated that RESPONSE_HEADERS (synchronized map) in SetHeaderFilter class is one the hotspots. Instead of synchronized HashMap, we shall use ConcurrentHashMap which should serve better concurrency performance.
    http://bugzilla.zimbra.com/show_bug.cgi?id=61865

--- ZimbraWebClient/src/com/zimbra/webClient/filters/SetHeaderFilter.java
+++ ZimbraWebClient/src/com/zimbra/webClient/filters/SetHeaderFilter.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Web Client
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -36,10 +36,13 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.zimbra.common.util.Log;
 import com.zimbra.common.util.ZimbraLog;
 
-public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
-    
+public final class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
+    private static final Log LOG = ZimbraLog.webclient;
+
     // --------------------------------------------------------------
     // Class constants
     // --------------------------------------------------------------
@@ -59,7 +62,7 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
     private static final String HEADER_CONTENT_ENCODING= "Content-encoding";
     private static final String TIME_FORMAT = "EEE, d MMM yyyy HH:mm:ss";
     private static final String GMT = "GMT";
-    
+
     protected int debug = 0;
     protected String jsVersion = null;
     protected FilterConfig config = null;
@@ -75,13 +78,11 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
     private String mailUrlHome;
     private String mailUrlUser;
 
-	//
-	// Constructors
-	//
 
-	public SetHeaderFilter() {
-		super(ZimbraLog.webclient);
-	}
+    @Override
+    protected Log getLogger() {
+        return LOG;
+    }
 
     // --------------------------------------------------------------
     // init helper methods
@@ -96,7 +97,7 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
         }
         return value;
     }
-    
+
     private int getInitParameterInt (String key, int def) {
         int retVal = def;
         String value = this.config.getInitParameter(key);
@@ -145,16 +146,14 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
         noCachePattern = Pattern.compile(noCachePatternRegex);
     }
 
-    // --------------------------------------------------------------
-    // init methods
-    // --------------------------------------------------------------
     /**
      * Place this filter into service.
      *
      * @param filterConfig The filter configuration object
      */
-	public void init(FilterConfig filterConfig) throws ServletException {
-		super.init(filterConfig);
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        super.init(filterConfig);
         this.config = filterConfig;
         if (filterConfig != null) {
             debug = getInitParameterInt("debug", 0);
@@ -166,20 +165,20 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
             expiresValue = getInitParameterInt("Expires", 0);
             futureCacheControl = "public, max-age=" + expiresValue;
             isProdMode = getInitParameterBool("ProdMode", true);
-	    
-	    try {
-		Context initCtx = new InitialContext();
-		Context envCtx = (Context) initCtx.lookup("java:comp/env");
-		mailUrl = (String) envCtx.lookup("mailUrl");
-	    } catch (NamingException ne) {
-		ne.printStackTrace();
-	    }
 
-	    if (mailUrl == null) {
-		mailUrl = "/zimbra";
-	    }
-	    mailUrlHome = mailUrl + "/home/";
-  	    mailUrlUser = mailUrl + "/user/";
+        try {
+        Context initCtx = new InitialContext();
+        Context envCtx = (Context) initCtx.lookup("java:comp/env");
+        mailUrl = (String) envCtx.lookup("mailUrl");
+        } catch (NamingException ne) {
+        ne.printStackTrace();
+        }
+
+        if (mailUrl == null) {
+        mailUrl = "/zimbra";
+        }
+        mailUrlHome = mailUrl + "/home/";
+          mailUrlUser = mailUrl + "/user/";
 
             if (debug > 0) {
                 System.out.println("Filter variables:");
@@ -192,32 +191,33 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
                 System.out.println("  extensionRegex = " + extensionPattern.pattern());
                 System.out.println("  noCachPattern = " + noCachePattern.pattern());
                 System.out.println("  noCachPattern = " + noCachePattern.pattern());
-		System.out.println("  mailUrl = " + mailUrl);
-		System.out.println("  mailUrlHome = " + mailUrlHome);
-		System.out.println("  mailUrlUser = " + mailUrlUser);
+        System.out.println("  mailUrl = " + mailUrl);
+        System.out.println("  mailUrlHome = " + mailUrlHome);
+        System.out.println("  mailUrlUser = " + mailUrlUser);
             }
         }
     }
 
     /**
-    * Take this filter out of service.
-    */
+     * Take this filter out of service.
+     */
+    @Override
     public void destroy() {
-		super.destroy();
+        super.destroy();
         this.config = null;
     }
-    
-    private boolean isHttpRequest(ServletRequest request, 
+
+    private boolean isHttpRequest(ServletRequest request,
                                   ServletResponse response) {
         boolean ret = false;
         if ((request instanceof HttpServletRequest) &&
-        	(response instanceof HttpServletResponse)){
+            (response instanceof HttpServletResponse)){
             ret = true;
         }
         return ret;
     }
-    
-    private boolean shouldProcess(ServletRequest request, 
+
+    private boolean shouldProcess(ServletRequest request,
                                   ServletResponse response) {
         boolean process = false;
         if ( isHttpRequest(request, response) && extensionPattern != null){
@@ -231,37 +231,37 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
     // --------------------------------------------------------------
 
     /**
-     * Main filter method. 
+     * Main filter method.
      */
-    public void doFilter ( ServletRequest request, ServletResponse response,
-                           FilterChain chain ) throws IOException,
-                                                      ServletException {
-		super.doFilter(request, response, chain);
-		// Clear logging context before each servlet request.
-		ZimbraLog.clearContext();
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        super.doFilter(request, response, chain);
+        // Clear logging context before each servlet request.
+        ZimbraLog.clearContext();
     }
 
-	public boolean doFilter(ServletRequest request, ServletResponse response)
-	throws IOException, ServletException {
+    @Override
+    public boolean doFilter(ServletRequest request, ServletResponse response) throws IOException, ServletException {
         if (debug > 0) {
             System.out.println("@doFilter");
         }
-		if (!super.doFilter(request, response)) {
-			return false;
-		}
+        if (!super.doFilter(request, response)) {
+            return false;
+        }
 
-	    /***/
+        /***/
         // TODO: We could pass all the init-params through but we only need one for now.
-	    request.setAttribute("init.Expires", String.valueOf(expiresValue));
-	    /***
-	    Enumeration pnames = this.config.getInitParameterNames();
-	    while (pnames.hasMoreElements()) {
-		    String pname = (String)pnames.nextElement();
-		    String pvalue = this.config.getInitParameter(pname);
-		    if (pvalue == null) continue;
-		    request.setAttribute("init."+pname, pvalue);
-	    }
-	    /***/
+        request.setAttribute("init.Expires", String.valueOf(expiresValue));
+        /***
+        Enumeration pnames = this.config.getInitParameterNames();
+        while (pnames.hasMoreElements()) {
+            String pname = (String)pnames.nextElement();
+            String pvalue = this.config.getInitParameter(pname);
+            if (pvalue == null) continue;
+            request.setAttribute("init."+pname, pvalue);
+        }
+        /***/
 
         // make sure we're dealing with an http request
         HttpServletRequest req;
@@ -274,19 +274,19 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
             if (debug > 0){
                 System.out.println("not proceeding with filter");
             }
-			return true;
+            return true;
         }
-        
+
         String uri = req.getRequestURI();
 
         // before we check whether we can compress, let's check
         // what sort of cache control headers we should use for this
         // request.
         setCacheControlHeaders(req, resp);
-        
+
         boolean supportCompression;
         supportCompression = this.supportsGzip(req, resp);
-        setRequestAttributes(req, resp, supportCompression);
+        setRequestAttributes(req, supportCompression);
         secureCookieIfNecessary(req);
         if (!supportCompression) {
             if (debug > 0) {
@@ -295,31 +295,29 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
         } else {
             Matcher m = extensionPattern.matcher(uri);
             if (debug > 0) {
-                System.out.println("Seeing if uri " + uri +
-                                   " matches the extension regex of " 
-                                   + extensionPattern.pattern());
+                System.out.println("Seeing if uri " + uri + " matches the extension regex of "
+                        + extensionPattern.pattern());
                 System.out.println("Does it? " + m.matches());
             }
             if ( m.matches() ) {
-                // not sure why I can't set the headers after 
+                // not sure why I can't set the headers after
                 // the chain.doFilter call, but ...
                 // I can't.
-	            // [A] Because you can't write the headers after
-	            // [A] content has already been written to the
-	            // [A] response!
+                // [A] Because you can't write the headers after
+                // [A] content has already been written to the
+                // [A] response!
                 if (debug > 0) {
-                    System.out.println("setting headers");                    
+                    System.out.println("setting headers");
                 }
                 resp.setHeader(HEADER_CONTENT_ENCODING, HEADER_VAL_GZIP);
                 resp.setHeader(HEADER_VARY, HEADER_ACCEPT_ENCODING);
             }
         }
 
-		return true;
+        return true;
     }
 
-    private void setRequestAttributes( HttpServletRequest req, HttpServletResponse resp,
-            boolean supportsCompression){
+    private void setRequestAttributes( HttpServletRequest req, boolean supportsCompression){
         if (supportsCompression) {
             req.setAttribute(ATTR_NAME_FILE_EXTENSION, gzipExtension);
         }
@@ -333,42 +331,42 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
             }
         }
         req.setAttribute("mode", mode);
-		req.setAttribute("prodMode", isProdMode);
+        req.setAttribute("prodMode", isProdMode);
     }
-    
+
     private void secureCookieIfNecessary(HttpServletRequest req) {
-        
+
         boolean currentHttps = req.getScheme().equals("https");
-        if (!currentHttps)
+        if (!currentHttps) {
             return;
-        
+        }
         HttpSession httpSession = req.getSession(false);
-        if (httpSession != null)
+        if (httpSession != null) {
             return;
-        
+        }
         /*
          * This is a https req, and we don't have a session yet.
-         * This is probably the first req of the session, which means user 
+         * This is probably the first req of the session, which means user
          * browsed to https://, which means we should stay in https for all
          * mail modes.  We need to secure the JSESSIONID cookie.
-         * 
+         *
          * We do this by SessionManager.setSecureCookies(true), jetty
-         * will set secure on the session cookie.  This has to be done 
+         * will set secure on the session cookie.  This has to be done
          * here because it has to be done *before* the session is created.
-         * After the session is created, jetty would create the return 
-         * the JSESSIONID cookie in the response for the request for that 
-         * the session is created; and it will be too late to call 
+         * After the session is created, jetty would create the return
+         * the JSESSIONID cookie in the response for the request for that
+         * the session is created; and it will be too late to call
          * setSecureCookies because jetty has already baked the session cookie.
          */
         ServletContext servletContext = config.getServletContext();
         if (servletContext instanceof org.mortbay.jetty.handler.ContextHandler.SContext) {
             org.mortbay.jetty.handler.ContextHandler.SContext sContext = (org.mortbay.jetty.handler.ContextHandler.SContext)servletContext;
-            
+
             // get the WebAppContext
             org.mortbay.jetty.handler.ContextHandler contextHandler = sContext.getContextHandler();
             if (contextHandler instanceof org.mortbay.jetty.servlet.Context) {
                 org.mortbay.jetty.servlet.Context context= (org.mortbay.jetty.servlet.Context)contextHandler;
-                
+
                 // get SessionManager
                 org.mortbay.jetty.SessionManager sessionManager = context.getSessionHandler().getSessionManager();
                 if (sessionManager instanceof org.mortbay.jetty.servlet.AbstractSessionManager) {
@@ -382,15 +380,14 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
     /**
      * Returns whether the browser supports gzipped content.
      * Checks a query parameter first, then the Accept-Encoding header.
-     * 
+     *
      * Note: The serverSupportsGzip configuration variable can override
      * this method.
      * @param req
      * @param resp
      * @return boolean.
      */
-    public boolean supportsGzip (HttpServletRequest req, 
-                                 HttpServletResponse resp){
+    public boolean supportsGzip(HttpServletRequest req, HttpServletResponse resp){
         // If this filter has been configured to not respond
         // with gzipped content, just return false.
         if (!serverSupportsGzip){
@@ -404,26 +401,26 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
         if (debug > 0){
             System.out.println("Header " + HEADER_VAL_GZIP + " = " + s);
         }
-        // We'll handle dev mode as an odd case, where only if 
+        // We'll handle dev mode as an odd case, where only if
         // the user specifies that he wants gzip, will he get it.
         if (!isProdMode) {
-            if (s != null ){ 
+            if (s != null ){
                 if (TRUE.equals(s)) {
                     supportCompression = true;
                 }
             }
-    	    if (debug > 0) {
-    	        System.out.print("in dev mode -- compression on? " + supportCompression + " ");
-    	    }
+            if (debug > 0) {
+                System.out.print("in dev mode -- compression on? " + supportCompression + " ");
+            }
             return supportCompression;
-    	}
-        
-    	if (FALSE.equals(s)) {
-    	    if (debug > 0) {
-    	        System.out.print("got parameter gzip=false -->");
-    	        System.out.println("don't compress, just chain filter");
-    	    }
-    	    supportCompression = false;
+        }
+
+        if (FALSE.equals(s)) {
+            if (debug > 0) {
+                System.out.print("got parameter gzip=false -->");
+                System.out.println("don't compress, just chain filter");
+            }
+            supportCompression = false;
         } else {
             // we didn't find the query parameter, so check the accept
             // encoding headers
@@ -445,56 +442,48 @@ public class SetHeaderFilter extends com.zimbra.cs.servlet.SetHeaderFilter {
         }
         return supportCompression;
     }
-    
+
     // ----------------------------------------------------------
     // Cache control methods
     // ----------------------------------------------------------
-    
-    public void setCacheControlHeaders (HttpServletRequest req,
-                                        HttpServletResponse resp){
+
+    public void setCacheControlHeaders(HttpServletRequest req, HttpServletResponse resp) {
         String uri = req.getRequestURI();
         // is this a request for a jsp page?
         if (debug > 0){
             System.out.println("URI = " + uri + " ");
         }
         if (isDynamicContent(uri)){
-            setJspCacheControlHeaders(req, resp);
+            setJspCacheControlHeaders(resp);
         } else {
             setStaticResourceCacheControlHeaders(req, resp);
         }
     }
-    
-    private boolean isDynamicContent (String uri) 
-    {
+
+    private boolean isDynamicContent(String uri) {
         Matcher m = noCachePattern.matcher(uri);
         if (debug > 0) {
-            System.out.println("Seeing if uri " + uri +
-                               " matches the noCache pattern " 
-                               + noCachePattern.pattern());
+            System.out.println("Seeing if uri " + uri + " matches the noCache pattern " + noCachePattern.pattern());
             System.out.println("Does it? " + m.matches());
         }
-        if ( m.matches() ) {
+        if (m.matches()) {
             return true;
         }
-        return false;        
+        return false;
     }
-    
-    private void setJspCacheControlHeaders(HttpServletRequest req, 
-                                           HttpServletResponse resp){
+
+    private void setJspCacheControlHeaders(HttpServletResponse resp) {
         resp.setHeader(HEADER_EXPIRES, ALREADY_EXPIRED);
         resp.setHeader(HEADER_CACHE_CONTROL, NO_CACHE_CONTROL_VALUE);
         resp.setHeader(HEADER_PRAGMA, NO_CACHE_PRAGMA_VALUE);
     }
-    
-    
-    private void setStaticResourceCacheControlHeaders(
-                                                HttpServletRequest req, 
-                                                HttpServletResponse resp){
+
+    private void setStaticResourceCacheControlHeaders(HttpServletRequest req, HttpServletResponse resp) {
         if (expiresValue > 0 && req.getMethod().equals("GET")) {
             TimeZone gmt = TimeZone.getTimeZone(GMT);
             long now = System.currentTimeMillis();
             Date expiresDate = new Date(now + (expiresValue * 1000));
-            DateFormat df = new SimpleDateFormat(TIME_FORMAT); 
+            DateFormat df = new SimpleDateFormat(TIME_FORMAT);
             df.setTimeZone(gmt);
             resp.setHeader(HEADER_EXPIRES, df.format(expiresDate) + " " + GMT);
             resp.setHeader(HEADER_CACHE_CONTROL, futureCacheControl);
