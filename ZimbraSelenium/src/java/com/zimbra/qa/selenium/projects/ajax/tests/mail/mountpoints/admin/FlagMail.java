fef640d5d52614051d3099d49a0d64d3859d3ee2
 Unknown Perforce User <rhoades>
2011-07-11 20:43:52 +0000

    Bug: none
    Selenium Harness: add test cases for flagging messages in admin and manager shares

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mountpoints/admin/FlagMail.java
@@ -0,0 +1,177 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.mountpoints.admin;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderMountpointItem;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.ui.Shortcut;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+public class FlagMail extends AjaxCommonTest {
+
+	@SuppressWarnings("serial")
+	public FlagMail() {
+		logger.info("New "+ FlagMail.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+
+		// Make sure we are using an account with message view
+		super.startingAccountPreferences = new HashMap<String, String>() {{
+				    put("zimbraPrefGroupMailBy", "message");
+				}};
+		
+	}
+	
+	@Test(	description = "Verify success on Flag a shared mail (admin share)",
+			groups = { "functional" })
+	public void FlagMail_01() throws HarnessException {
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String mountpointname = "mountpoint" + ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem inbox = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox);
+		
+		// Create a folder to share
+		ZimbraAccount.AccountA().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + foldername + "' l='" + inbox.getId() + "'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem folder = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), foldername);
+		
+		// Share it
+		ZimbraAccount.AccountA().soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ folder.getId() +"' op='grant'>"
+				+			"<grant d='"+ app.zGetActiveAccount().EmailAddress +"' gt='usr' perm='rwidxa'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+		// Add a message to it
+		ZimbraAccount.AccountA().soapSend(
+					"<AddMsgRequest xmlns='urn:zimbraMail'>"
+        		+		"<m l='"+ folder.getId() +"' >"
+            	+			"<content>From: foo@foo.com\n"
+            	+				"To: foo@foo.com \n"
+            	+				"Subject: "+ subject +"\n"
+            	+				"MIME-Version: 1.0 \n"
+            	+				"Content-Type: text/plain; charset=utf-8 \n"
+            	+				"Content-Transfer-Encoding: 7bit\n"
+            	+				"\n"
+            	+				"simple text string in the body\n"
+            	+			"</content>"
+            	+		"</m>"
+				+	"</AddMsgRequest>");
+		
+		MailItem mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+
+		
+		// Mount it
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointname +"'  rid='"+ folder.getId() +"' zid='"+ ZimbraAccount.AccountA().ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+				
+		// Click on the mountpoint
+		app.zTreeMail.zTreeItem(Action.A_LEFTCLICK, mountpoint);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+		
+		// Flag the item
+		app.zPageMail.zListItem(Action.A_MAIL_FLAG, mail.dSubject);
+
+		// Verify the message is flagged
+		mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+		ZAssert.assertStringContains(mail.getFlags(), "f", "Verify the message is  flagged in the server");
+		
+	}
+
+	
+	@Test(	description = "Verify Permission Denied on Flag (keyboard='mf') a shared mail (read-only share)",
+			groups = { "functional" })
+	public void FlagMail_02() throws HarnessException {
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String mountpointname = "mountpoint" + ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem inbox = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox);
+		
+		// Create a folder to share
+		ZimbraAccount.AccountA().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + foldername + "' l='" + inbox.getId() + "'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem folder = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), foldername);
+		
+		// Share it
+		ZimbraAccount.AccountA().soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ folder.getId() +"' op='grant'>"
+				+			"<grant d='"+ app.zGetActiveAccount().EmailAddress +"' gt='usr' perm='rwidxa'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+		// Add a message to it
+		ZimbraAccount.AccountA().soapSend(
+					"<AddMsgRequest xmlns='urn:zimbraMail'>"
+        		+		"<m l='"+ folder.getId() +"' >"
+            	+			"<content>From: foo@foo.com\n"
+            	+				"To: foo@foo.com \n"
+            	+				"Subject: "+ subject +"\n"
+            	+				"MIME-Version: 1.0 \n"
+            	+				"Content-Type: text/plain; charset=utf-8 \n"
+            	+				"Content-Transfer-Encoding: 7bit\n"
+            	+				"\n"
+            	+				"simple text string in the body\n"
+            	+			"</content>"
+            	+		"</m>"
+				+	"</AddMsgRequest>");
+		
+		MailItem mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+
+		// Mount it
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointname +"'  rid='"+ folder.getId() +"' zid='"+ ZimbraAccount.AccountA().ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+				
+		// Click on the mountpoint
+		app.zTreeMail.zTreeItem(Action.A_LEFTCLICK, mountpoint);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+		
+		// Flag the item
+		app.zPageMail.zKeyboardShortcut(Shortcut.S_MAIL_MARKFLAG);
+		
+		// Verify the message is flagged
+		mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+		ZAssert.assertStringContains(mail.getFlags(), "f", "Verify the message is  flagged in the server");
+		
+	}
+
+
+}
