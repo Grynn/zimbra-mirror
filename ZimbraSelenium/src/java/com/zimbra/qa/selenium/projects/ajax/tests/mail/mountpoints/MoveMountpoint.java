55b7463a8b5015567d85eeb9ab08b5caccbb2771
 Unknown Perforce User <rhoades>
2011-07-11 20:18:58 +0000

    Bug: none
    Selenium Harness: add test case for moving a mountpoint

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mountpoints/MoveMountpoint.java
@@ -0,0 +1,107 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.mountpoints;
+
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderMountpointItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogMove;
+
+
+public class MoveMountpoint extends AjaxCommonTest {
+
+	private ZimbraAccount Owner = null;
+	
+	
+	public MoveMountpoint() {
+		logger.info("New "+ MoveMountpoint.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+		super.startingAccountPreferences = null;
+		
+		Owner = new ZimbraAccount();
+		Owner.provision();
+		Owner.authenticate();
+		
+	}
+	
+	@Test(	description = "Move a mountpoint under a subfolder - Right click, Move",
+			groups = { "smoke" })
+	public void MoveMountpoint_01() throws HarnessException {
+		
+		// Owner creates a folder, shares it with current user
+		String ownerFoldername = "ownerfolder"+ ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem ownerInbox = FolderItem.importFromSOAP(Owner, FolderItem.SystemFolder.Inbox);
+		ZAssert.assertNotNull(ownerInbox, "Verify the new owner folder exists");
+
+		Owner.soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + ownerFoldername +"' l='" + ownerInbox.getId() +"'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem ownerFolder = FolderItem.importFromSOAP(Owner, ownerFoldername);
+		ZAssert.assertNotNull(ownerFolder, "Verify the new owner folder exists");
+		
+		Owner.soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ ownerFolder.getId() +"' op='grant'>"
+				+			"<grant d='" + app.zGetActiveAccount().EmailAddress + "' gt='usr' perm='r'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+
+		// Current user creates the mountpoint that points to the share
+		String mountpointFoldername = "mountpoint"+ ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointFoldername +"' view='message' rid='"+ ownerFolder.getId() +"' zid='"+ Owner.ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointFoldername);
+		ZAssert.assertNotNull(mountpoint, "Verify the subfolder is available");
+
+		// Current user creates a subfolder for the move destination
+		FolderItem inbox = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+
+		String foldername = "subfolder" + ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ foldername +"' l='"+ inbox.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem folder = FolderItem.importFromSOAP(app.zGetActiveAccount(), foldername);
+		ZAssert.assertNotNull(folder, "Verify the subfolder is available");
+
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+		
+		
+		// Move the folder using context menu
+		DialogMove dialog = (DialogMove)app.zTreeMail.zTreeItem(Action.A_RIGHTCLICK, Button.B_MOVE, mountpoint);
+		dialog.zClickTreeFolder(folder);
+		dialog.zClickButton(Button.B_OK);
+		
+		// Verify the mountpoint is now in the other subfolder
+		mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointFoldername);
+		ZAssert.assertNotNull(mountpoint, "Verify the mountpoint is again available");
+		ZAssert.assertEquals(folder.getId(), mountpoint.getParentId(), "Verify the mountpoint's parent is now the other subfolder");
+		
+	}
+
+	
+	
+
+	
+
+}
