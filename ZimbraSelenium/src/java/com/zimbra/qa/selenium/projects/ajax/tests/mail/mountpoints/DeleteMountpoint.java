72c7dde245e85571ac42b61665958e5486cc4900
 Unknown Perforce User <rhoades>
2011-07-11 18:54:38 +0000

    Bug: none
    Selenium Harness: add test case to delete a mountpoint

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mountpoints/DeleteMountpoint.java
@@ -0,0 +1,111 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.mountpoints;
+
+
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderMountpointItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.XmlStringUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogShareAccept;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DisplayMail;
+
+
+public class DeleteMountpoint extends AjaxCommonTest {
+
+	private ZimbraAccount Owner = null;
+	
+	
+	public DeleteMountpoint() {
+		logger.info("New "+ DeleteMountpoint.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+		super.startingAccountPreferences = null;
+		
+		Owner = new ZimbraAccount();
+		Owner.provision();
+		Owner.authenticate();
+		
+	}
+	
+	@Test(	description = "Delete a mountpoint to a shared folder",
+			groups = { "smoke" })
+	public void DeleteMountpoint_01() throws HarnessException {
+		
+		// Owner creates a folder, shares it with current user
+		String ownerFoldername = "ownerfolder"+ ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem ownerInbox = FolderItem.importFromSOAP(Owner, FolderItem.SystemFolder.Inbox);
+		ZAssert.assertNotNull(ownerInbox, "Verify the new owner folder exists");
+
+		Owner.soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + ownerFoldername +"' l='" + ownerInbox.getId() +"'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem ownerFolder = FolderItem.importFromSOAP(Owner, ownerFoldername);
+		ZAssert.assertNotNull(ownerFolder, "Verify the new owner folder exists");
+		
+		Owner.soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ ownerFolder.getId() +"' op='grant'>"
+				+			"<grant d='" + app.zGetActiveAccount().EmailAddress + "' gt='usr' perm='r'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+
+		// Current user creates the mountpoint that points to the share
+		String mountpointFoldername = "mountpoint"+ ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointFoldername +"' view='message' rid='"+ ownerFolder.getId() +"' zid='"+ Owner.ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointFoldername);
+		ZAssert.assertNotNull(mountpoint, "Verify the subfolder is available");
+
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+		
+		// Verify the mountpoint exists
+		List<FolderItem> folders = app.zTreeMail.zListGetFolders();
+		
+		FolderItem found = null;
+		for(FolderItem f : folders) {
+			if ( mountpointFoldername.equals(f.getName()) ) {
+				found = f;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(found, "Verify the mountpoint exists in the folder tree");
+		
+		
+		// Delete the folder using context menu
+		app.zTreeMail.zTreeItem(Action.A_RIGHTCLICK, Button.B_DELETE, mountpoint);
+		
+		
+		// Verify the folder is now in the trash
+		FolderItem trash = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Trash);
+		mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointFoldername);
+		ZAssert.assertNotNull(mountpoint, "Verify the subfolder is again available");
+		ZAssert.assertEquals(trash.getId(), mountpoint.getParentId(), "Verify the subfolder's parent is now the trash folder ID");
+		
+	}
+
+	
+	
+
+	
+
+}
