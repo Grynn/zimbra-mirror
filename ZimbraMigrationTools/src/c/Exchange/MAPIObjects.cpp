9cb76dbcfa23be17e197581cbd78f6ba5e77b8cc
amitabhv <amitabhv@zimbra.com>
2011-07-14 13:10:37 +0000

    bug:none. Shows only exchange profiles (not only non-zimbra)

--- ZimbraMigrationTools/src/c/Exchange/MAPIObjects.cpp
+++ ZimbraMigrationTools/src/c/Exchange/MAPIObjects.cpp
@@ -192,59 +192,56 @@ HRESULT ExchangeAdmin::GetAllProfiles(vector<string> &vProfileList)
 			{
 				if (profrows->aRow[i].lpProps[0].ulPropTag == PR_DISPLAY_NAME_A) 
 				{
-						Zimbra::Util::ScopedInterface<IMsgServiceAdmin> spServiceAdmin ;
-						Zimbra::Util::ScopedInterface<IMAPITable> spServiceTable ;
-						string strpname= profrows->aRow[i].lpProps[0].Value.lpszA;
-						//get profile's admin service
-						hr = m_pProfAdmin->AdminServices((LPTSTR)strpname.c_str(), NULL, NULL, 0, spServiceAdmin.getptr()) ; 
-						if(FAILED(hr))
-						{
-							throw ExchangeAdminException(hr,L"GetAllProfiles(): AdminServices Failed.",__LINE__,__FILE__);
-						}
-						//get message service table
-						hr = spServiceAdmin->GetMsgServiceTable(0, spServiceTable.getptr()) ;
-						if(FAILED(hr))
-						{
-							throw ExchangeAdminException(hr,L"GetAllProfiles(): GetMsgServiceTable Failed.",__LINE__,__FILE__);
-						}
+					Zimbra::Util::ScopedInterface<IMsgServiceAdmin> spServiceAdmin ;
+					Zimbra::Util::ScopedInterface<IMAPITable> spServiceTable ;
+					string strpname= profrows->aRow[i].lpProps[0].Value.lpszA;
+					//get profile's admin service
+					hr = m_pProfAdmin->AdminServices((LPTSTR)strpname.c_str(), NULL, NULL, 0, spServiceAdmin.getptr()) ; 
+					if(FAILED(hr))
+					{
+						throw ExchangeAdminException(hr,L"GetAllProfiles(): AdminServices Failed.",__LINE__,__FILE__);
+					}
+					//get message service table
+					hr = spServiceAdmin->GetMsgServiceTable(0, spServiceTable.getptr()) ;
+					if(FAILED(hr))
+					{
+						throw ExchangeAdminException(hr,L"GetAllProfiles(): GetMsgServiceTable Failed.",__LINE__,__FILE__);
+					}
 						
-						//lets get the service name and the service uid for the primary service
-						SizedSPropTagArray( 2, tags ) = { 2, { PR_SERVICE_NAME, PR_SERVICE_UID} } ;
-						spServiceTable->SetColumns( (LPSPropTagArray)&tags, 0 ) ;
-						DWORD dwCount = 0 ;
-						hr = spServiceTable->GetRowCount(0, &dwCount) ;
-						if(FAILED(hr))
-						{
-							throw ExchangeAdminException(hr,L"GetAllProfiles(): GetRowCount Failed.",__LINE__,__FILE__);
-						}
-						Zimbra::Util::ScopedRowSet pRows ;
-						hr = spServiceTable->QueryRows( dwCount, 0, pRows.getptr() );
-						if(FAILED(hr))
-						{
-							throw ExchangeAdminException(hr,L"GetAllProfiles(): QueryRows Failed.",__LINE__,__FILE__);
-						}
+					//lets get the service name and the service uid for the primary service
+					SizedSPropTagArray( 2, tags ) = { 2, { PR_SERVICE_NAME, PR_SERVICE_UID} } ;
+					spServiceTable->SetColumns( (LPSPropTagArray)&tags, 0 ) ;
+					DWORD dwCount = 0 ;
+					hr = spServiceTable->GetRowCount(0, &dwCount) ;
+					if(FAILED(hr))
+					{
+						throw ExchangeAdminException(hr,L"GetAllProfiles(): GetRowCount Failed.",__LINE__,__FILE__);
+					}
+					Zimbra::Util::ScopedRowSet pRows ;
+					hr = spServiceTable->QueryRows( dwCount, 0, pRows.getptr() );
+					if(FAILED(hr))
+					{
+						throw ExchangeAdminException(hr,L"GetAllProfiles(): QueryRows Failed.",__LINE__,__FILE__);
+					}
 
-						//is it Zimbra profile?
-						ULONG j;
-						bool bFoundZimbra=false;
-						for(j = 0; j < pRows->cRows ; j++)
+					//is it Zimbra profile?
+					ULONG j;
+					//bool bFoundZimbra=false;
+					for(j = 0; j < pRows->cRows ; j++)
+					{
+						if(PR_SERVICE_NAME == pRows->aRow[j].lpProps[0].ulPropTag) 
 						{
-							if(PR_SERVICE_NAME == pRows->aRow[j].lpProps[0].ulPropTag) 
+							WCHAR* tmp=pRows->aRow[j].lpProps[0].Value.LPSZ;
+							UNREFERENCED_PARAMETER(tmp);
+							//if MSExchange service
+							if(0 == lstrcmpiW(pRows->aRow[j].lpProps[0].Value.LPSZ, L"MSEMS"))
 							{
-								if(0 == lstrcmpiW(pRows->aRow[j].lpProps[0].Value.LPSZ, L"LSMS"))
-								{
-									//its a Zimbra Profile
-									bFoundZimbra = true;
-									break ;
-								}
+								if (profrows->aRow[i].lpProps[0].ulPropTag == PR_DISPLAY_NAME_A) 
+									vProfileList.push_back(profrows->aRow[i].lpProps[0].Value.lpszA);
+								break ;
 							}
 						}
-						//use non-zimbra profiles only
-						if(!bFoundZimbra)
-						{
-							if (profrows->aRow[i].lpProps[0].ulPropTag == PR_DISPLAY_NAME_A) 
-								vProfileList.push_back(profrows->aRow[i].lpProps[0].Value.lpszA);
-						}						
+					}
 				}
 			}
 		}
