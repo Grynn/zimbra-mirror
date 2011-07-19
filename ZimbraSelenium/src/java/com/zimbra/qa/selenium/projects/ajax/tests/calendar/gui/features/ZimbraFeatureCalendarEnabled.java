baa7f53936b94522dcde1c95ccb0658bff3f45e8
 Unknown Perforce User <rhoades>
2011-07-19 17:20:31 +0000

    Bug: none
    Selenium Harness: add basic framework for test case where only calendar is enabled.  verification still needs to be added that a basic appointment appears correctly.

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/gui/features/ZimbraFeatureCalendarEnabled.java
@@ -0,0 +1,50 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.gui.features;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+public class ZimbraFeatureCalendarEnabled extends AjaxCommonTest {
+
+	
+	@SuppressWarnings("serial")
+	public ZimbraFeatureCalendarEnabled() {
+		logger.info("New "+ ZimbraFeatureCalendarEnabled.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageCalendar;
+
+		// Make sure we are using an account with message view
+		super.startingAccountPreferences = new HashMap<String, String>() {{
+				    			
+					// Only mail is enabled
+				    put("zimbraFeatureMailEnabled", "FALSE");
+				    put("zimbraFeatureContactsEnabled", "FALSE");
+				    put("zimbraFeatureCalendarEnabled", "TRUE");
+				    put("zimbraFeatureTasksEnabled", "FALSE");
+				    put("zimbraFeatureBriefcasesEnabled", "FALSE");
+				    put("zimbraFeatureOptionsEnabled", "FALSE");
+				    
+
+				}};
+
+
+	}
+	
+	/**
+	 * @throws HarnessException
+	 */
+	@Test(	description = "Load the mail tab with just Calendar enabled",
+			groups = { "functional" })
+	public void ZimbraFeatureCalendarEnabled_01() throws HarnessException {
+		
+		// TODO: add basic verification that a simple appointment appears
+		
+	}
+
+
+}
