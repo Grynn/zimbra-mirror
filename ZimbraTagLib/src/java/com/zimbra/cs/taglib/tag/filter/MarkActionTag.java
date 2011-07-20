371848e8c8313fe14da8351b3a4e74518bc6559b
 Unknown Perforce User <ysasaki>
2011-07-20 05:56:28 +0000

    bug:42185 Add PRIORITY flag - Added PRIORITY flag for priority messages. - Added "priority" to Flag filter action.
    https://reviewboard.eng.vmware.com/r/248880/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=42185

--- ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/filter/MarkActionTag.java
+++ ZimbraTagLib/src/java/com/zimbra/cs/taglib/tag/filter/MarkActionTag.java
@@ -1,13 +1,13 @@
 /*
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
- * Copyright (C) 2007, 2009, 2010 Zimbra, Inc.
- * 
+ * Copyright (C) 2007, 2009, 2010, 2011 Zimbra, Inc.
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -22,17 +22,21 @@ import com.zimbra.cs.zclient.ZFilterAction.ZMarkAction;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 
-public class MarkActionTag extends ZimbraSimpleTag {
+public final class MarkActionTag extends ZimbraSimpleTag {
 
-    private MarkOp mOp;
+    private MarkOp op;
 
-    public void setOp(String op) throws ServiceException { mOp = MarkOp.fromProtoString(op); }
+    public void setOp(String op) throws ServiceException {
+        this.op = MarkOp.fromString(op);
+    }
 
+    @Override
     public void doTag() throws JspException {
         FilterRuleTag rule = (FilterRuleTag) findAncestorWithClass(this, FilterRuleTag.class);
-        if (rule == null)
-                throw new JspTagException("The markAction tag must be used within a filterRule tag");
-        rule.addAction(new ZMarkAction(mOp));
+        if (rule == null) {
+            throw new JspTagException("The markAction tag must be used within a filterRule tag");
+        }
+        rule.addAction(new ZMarkAction(op));
     }
 
 }
