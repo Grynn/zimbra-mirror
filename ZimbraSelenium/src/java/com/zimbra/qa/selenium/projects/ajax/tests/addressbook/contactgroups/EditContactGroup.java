12bca45ff46db0a09786cb7d5cace8d275ec469d
 Unknown Perforce User <dieunguyen>
2011-08-02 22:49:54 +0000

    bug:none - Add test cases for handling popup as click Close in edit contact/group

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contactgroups/EditContactGroup.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contactgroups/EditContactGroup.java
@@ -11,6 +11,7 @@ import com.zimbra.qa.selenium.framework.ui.*;
 
 import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
 import com.zimbra.qa.selenium.projects.ajax.ui.addressbook.*;
 
 
@@ -26,6 +27,32 @@ public class EditContactGroup extends AjaxCommonTest  {
 		super.startingAccountPreferences = null;		
 		
 	}
+   
+	private DialogWarning EditClickClose(ContactGroupItem newGroup, FormContactGroupNew formContactGroupNew) throws HarnessException {
+		
+		//clear the form, 
+		formContactGroupNew.zReset();
+		
+        // Fill in the form
+	    formContactGroupNew.zFill(newGroup);
+	    
+	  
+		// Save the contact
+        formContactGroupNew.zSubmit();
+		
+      
+    	// Click Close
+	    DialogWarning dialogWarning = (DialogWarning) app.zPageAddressbook.zToolbarPressButton(Button.B_CANCEL);
+	    
+	    //Verify title Warning and content "Do you want to save changes?"
+	    String text="Warning";
+	    ZAssert.assertEquals(text,dialogWarning.zGetWarningTitle()," Verify title is " + text);
+	    text = "Do you want to save changes?";
+	    ZAssert.assertEquals(text,dialogWarning.zGetWarningContent()," Verify content is " + text);
+	
+	    return dialogWarning;
+	}
+	
 	
 	private void EditGroup(FormContactGroupNew formContactGroupNew , ContactGroupItem group) throws HarnessException {
         ContactGroupItem newGroup = ContactGroupItem.generateContactItem(GenerateItemType.Basic);
@@ -37,7 +64,7 @@ public class EditContactGroup extends AjaxCommonTest  {
         // Fill in the form
 	    formContactGroupNew.zFill(newGroup);
 	    
-	  
+	
 		// Save the contact
         formContactGroupNew.zSubmit();
 		
@@ -141,6 +168,114 @@ public class EditContactGroup extends AjaxCommonTest  {
 		for (int i=0; i<group.dlist.size(); i++) {
 	       ZAssert.assertStringContains(groupView.zGetContactProperty(DisplayContactGroup.Field.Email), group.dlist.get(i), "Verify contact group email (" + group.dlist.get(i) + ") displayed");	
 		}         
+	}
+
+	@Test(	description = "Cancel an edited contact group by click Close, then click No",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickNo() throws HarnessException {
+		
+		// Create a contact group via Soap then select
+		ContactGroupItem group = app.zPageAddressbook.createUsingSOAPSelectContactGroup(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact group
+		ContactGroupItem newGroup = ContactGroupItem.generateContactItem(GenerateItemType.Basic);
+				
+		//Right-Click, then Edit Group on Context Menu 	
+        FormContactGroupNew formContactGroupNew = (FormContactGroupNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, group.fileAs);        
+  
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newGroup,formContactGroupNew);
+	    
+	    // Click No in popup dialog 
+	    dialogWarning.zClickButton(Button.B_NO);
+	    
+	    // Select the contact group
+		DisplayContactGroup groupView = (DisplayContactGroup) app.zPageAddressbook.zListItem(Action.A_LEFTCLICK, group.fileAs);
+	  
+		ZAssert.assertStringContains(groupView.zGetContactProperty(DisplayContactGroup.Field.Company), group.fileAs  , "Verify contact group email (" + group.fileAs + ") displayed");	
+		
+		for (int i=0; i<group.dlist.size(); i++) {
+	       ZAssert.assertStringContains(groupView.zGetContactProperty(DisplayContactGroup.Field.Email), group.dlist.get(i), "Verify contact group email (" + group.dlist.get(i) + ") displayed");	
+		}     
+	}
+
+	@Test(	description = "Cancel an edited contact by click Close, then click Cancel",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickCancel() throws HarnessException {
+		
+		// Create a contact group via Soap then select
+		ContactGroupItem group = app.zPageAddressbook.createUsingSOAPSelectContactGroup(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact group
+		ContactGroupItem newGroup = ContactGroupItem.generateContactItem(GenerateItemType.Basic);
+				
+		//Right-Click, then Edit Group on Context Menu 	
+        FormContactGroupNew formContactGroupNew = (FormContactGroupNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, group.fileAs);        
+  
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newGroup,formContactGroupNew);
+       
+	    //Click Cancel in popup dialog 
+	    dialogWarning.zClickButton(Button.B_CANCEL);
+	 
+	    //Verify form contact is active
+	    ZAssert.assertTrue(formContactGroupNew.zIsActive(),"Verify contact form is active");   
+     }
+
+	@Test(	description = "Cancel an edited contact by click Close, then click Yes",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickYes() throws HarnessException {
+		// Create a contact group via Soap then select
+		ContactGroupItem group = app.zPageAddressbook.createUsingSOAPSelectContactGroup(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact group
+		ContactGroupItem newGroup = ContactGroupItem.generateContactItem(GenerateItemType.Basic);
+				
+		//Right-Click, then Edit Group on Context Menu 	
+        FormContactGroupNew formContactGroupNew = (FormContactGroupNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, group.fileAs);        
+  
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newGroup,formContactGroupNew);
+   
+	    // Click Yes in popup dialog 
+	    dialogWarning.zClickButton(Button.B_YES);
+
+        //verify toasted message Contact Saved
+        String expectedMsg ="Group Saved";
+        ZAssert.assertStringContains(app.zPageMain.zGetToaster().zGetToastMessage(),
+        		        expectedMsg , "Verify toast message '" + expectedMsg + "'");
+  
+        
+            
+        //verify new contact group item is displayed
+        List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts();   
+ 	           
+		boolean isFileAsEqual=false;
+		for (ContactItem ci : contacts) {
+			if (ci.fileAs.equals(newGroup.groupName)) {
+	            isFileAsEqual = true;	 
+				break;
+			}
+		}
+		
+        ZAssert.assertTrue(isFileAsEqual, "Verify contact fileAs (" + newGroup.groupName + ") existed ");
+
+        
+		//verify old contact group not displayed
+    	isFileAsEqual=false;
+		for (ContactItem ci : contacts) {
+			if (ci.fileAs.equals(group.groupName)) {
+	            isFileAsEqual = true;	 
+				break;
+			}
+		}
+		
+        ZAssert.assertFalse(isFileAsEqual, "Verify contact fileAs (" + group.groupName + ") not displayed");
+
+        // Select the new group
+		DisplayContactGroup contactView = (DisplayContactGroup) app.zPageAddressbook.zListItem(Action.A_LEFTCLICK, newGroup.fileAs);
+	  
+		ZAssert.assertNotNull(contactView," Verify contact " + newGroup + " is displayed");
 	}
 
 } 
--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/EditContact.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/EditContact.java
@@ -1,8 +1,6 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.addressbook.contacts;
 import java.util.List;
-
 import org.testng.annotations.Test;
-
 import com.zimbra.qa.selenium.framework.items.ContactItem;
 import com.zimbra.qa.selenium.framework.items.ContactItem.GenerateItemType;
 import com.zimbra.qa.selenium.framework.ui.Action;
@@ -10,6 +8,7 @@ import com.zimbra.qa.selenium.framework.ui.Button;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
 import com.zimbra.qa.selenium.projects.ajax.ui.addressbook.*;
 
 
@@ -26,7 +25,25 @@ public class EditContact extends AjaxCommonTest  {
 		
 	}
 	
-
+	private DialogWarning EditClickClose( ContactItem newContact, FormContactNew formContactNew) throws HarnessException {
+		
+		//clear the form, 
+		formContactNew.zReset();
+		
+        // Fill in the form
+	    formContactNew.zFill(newContact);
+   
+	    // Click Close 
+	    DialogWarning dialogWarning = (DialogWarning) app.zPageAddressbook.zToolbarPressButton(Button.B_CANCEL);
+	    
+	    //Verify title Warning and content "Do you want to save changes?"
+	    String text="Warning";
+	    ZAssert.assertEquals(text,dialogWarning.zGetWarningTitle()," Verify title is " + text);
+	    text = "Do you want to save changes?";
+	    ZAssert.assertEquals(text,dialogWarning.zGetWarningContent()," Verify content is " + text);
+	
+	    return dialogWarning;
+	}
 	
 	private void EditAndVerify(FormContactNew formContactNew, ContactItem contactItem) 
 	     throws HarnessException
@@ -135,5 +152,95 @@ public class EditContact extends AjaxCommonTest  {
 		ZAssert.assertNotNull(contactView," Verify contact " + contactItem + " is displayed");
 	}
 
+	@Test(	description = "Cancel an edited contact by click Close, then click No",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickNo() throws HarnessException {
+		
+	    ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact
+		ContactItem newContact = ContactItem.generateContactItem(GenerateItemType.Basic);
+
+		//Open edit contact form	
+        FormContactNew formContactNew = (FormContactNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, contactItem.fileAs);        
+
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newContact,formContactNew);
+	    
+	    // Click No in popup dialog 
+	    dialogWarning.zClickButton(Button.B_NO);
+	    
+        // Select the contact 
+		DisplayContact contactView = (DisplayContact) app.zPageAddressbook.zListItem(Action.A_LEFTCLICK, contactItem.fileAs);
+	  
+		ZAssert.assertNotNull(contactView," Verify contact " + contactItem + " is displayed");
+	}
+
+	@Test(	description = "Cancel an edited contact by click Close, then click Cancel",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickCancel() throws HarnessException {
+		
+	    ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact
+		ContactItem newContact = ContactItem.generateContactItem(GenerateItemType.Basic);
+
+		//Open edit contact form	
+        FormContactNew formContactNew = (FormContactNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, contactItem.fileAs);        
+
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newContact,formContactNew);
+	    
+	    //Click Cancel in popup dialog 
+	    dialogWarning.zClickButton(Button.B_CANCEL);
+	 
+	    //Verify form contact is active
+	    ZAssert.assertTrue(formContactNew.zIsActive(),"Verify contact form is active");   
+     }
+
+	@Test(	description = "Cancel an edited contact by click Close, then click Yes",
+			groups = { "functional"})
+	public void ClickToolbarCloseThenClickYes() throws HarnessException {
+		
+	    ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
+		
+	    //generate the new contact
+		ContactItem newContact = ContactItem.generateContactItem(GenerateItemType.Basic);
+
+		//Open edit contact form	
+        FormContactNew formContactNew = (FormContactNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_EDIT, contactItem.fileAs);        
+
+        //Click Close on Toolbar button	
+	    DialogWarning dialogWarning = EditClickClose(newContact,formContactNew);
+	    
+	    // Click Yes in popup dialog 
+	    dialogWarning.zClickButton(Button.B_YES);
+
+        //verify toasted message Contact Saved
+        String expectedMsg ="Contact Saved";
+        ZAssert.assertStringContains(app.zPageMain.zGetToaster().zGetToastMessage(),
+        		        expectedMsg , "Verify toast message '" + expectedMsg + "'");
+  
+        
+        //verify old contact item is not displayed
+        List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts();   
+ 	           
+		boolean isFileAsEqual=false;
+		for (ContactItem ci : contacts) {
+			if (ci.fileAs.equals(contactItem.fileAs)) {
+	            isFileAsEqual = true;	 
+				break;
+			}
+		}
+		
+        ZAssert.assertFalse(isFileAsEqual, "Verify contact fileAs (" + contactItem.fileAs + ") not displayed");
+
+        // Select the new contact 
+		DisplayContact contactView = (DisplayContact) app.zPageAddressbook.zListItem(Action.A_LEFTCLICK, newContact.fileAs);
+	  
+		ZAssert.assertNotNull(contactView," Verify contact " + newContact + " is displayed");
+	}
+
+	
 }
 
