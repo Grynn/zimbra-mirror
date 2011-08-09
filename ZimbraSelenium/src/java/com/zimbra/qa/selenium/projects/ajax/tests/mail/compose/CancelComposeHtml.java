b2e4e2cf44c8131be5a9e01569e7dcb025293b07
 Unknown Perforce User <girishb>
2011-08-09 13:32:15 +0000

    bug:63096
    Added Test case as 
    1. 'CancelComposeHtml_02' :-Cancel Html composed mail using keyboard shortcut 'Escape'
    2.'CancelComposeText_02' :-Cancel Text composed mail using keyboard shortcut 'Escape' 
    
    into selenium harness
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63096

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/CancelComposeHtml.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/CancelComposeHtml.java
@@ -1,15 +1,19 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.testng.annotations.Test;
 
+import com.zimbra.qa.selenium.framework.items.MailItem;
 import com.zimbra.qa.selenium.framework.ui.AbsDialog;
 import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.ui.Shortcut;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
 import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew;
 import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
 
@@ -58,5 +62,52 @@ public class CancelComposeHtml extends AjaxCommonTest {
 		
 
 	}
+	/**
+	 * Test Case: Cancel HTML composed mail using keyboard shortcut 'Escape'
+	 * 1.Compose Html mail
+	 * 2.Press 'Esc' key of keyboard
+	 * 3.Verify 'SaveCurrentMessageAsDraft'Warning Dialog
+	 * 4.Press No
+	 * 5.Verify Message no longer present in inbox
+	 * @throws HarnessException
+	 */
+	
+	@Test(	description = "Cancel html composed mail using keyboard shortcut 'Escape'",
+			groups = { "functional" })
+	public void CancelComposeHtml_02() throws HarnessException {
+		
+		Shortcut shortcut = Shortcut.S_ESCAPE;
+		// Create the message data to be sent
+		String body = "body" + ZimbraSeleniumProperties.getUniqueString();
+		String Subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		
 
+		// Open the new mail form
+		FormMailNew mailform = (FormMailNew) app.zPageMail.zToolbarPressButton(Button.B_NEW);
+		ZAssert.assertTrue(mailform.zIsActive(), "Verify the new form opened");	
+		
+		
+		// Fill out the form with the data
+		mailform.zFillField(Field.Subject, Subject);
+		mailform.zFillField(Field.Body, body);		
+		
+		DialogWarning warning =(DialogWarning)app.zPageMail.zKeyboardShortcut(shortcut);
+		ZAssert.assertNotNull(warning, "Verify the dialog is opened");
+		
+		warning.zClickButton(Button.B_NO);
+		warning.zWaitForClose(); // Make sure the dialog is dismissed
+		
+		List<MailItem> messages = app.zPageMail.zListGetMessages();
+		
+		MailItem found = null;
+		for (MailItem m : messages) {
+			logger.info("Subject: looking for "+Subject +" found: "+ m.gSubject);
+			if ( Subject.equals(m.gSubject) ) {
+				found = m;
+				break;
+			}
+		}
+		ZAssert.assertNull(found, "Verify the message is no longer in the inbox");
+		
+	}
 }
