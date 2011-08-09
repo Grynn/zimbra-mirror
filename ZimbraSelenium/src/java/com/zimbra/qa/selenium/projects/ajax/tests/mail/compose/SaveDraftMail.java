1a872533d72e083eeaace96c414c2de51b91a702
 Unknown Perforce User <girishb>
2011-08-09 13:51:49 +0000

    bug:none
    --Added test case
    SaveDraftMail_02() :- Save draft using keyboard shortcut 'Escape'

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/SaveDraftMail.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/compose/SaveDraftMail.java
@@ -1,16 +1,20 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.mail.compose;
 
+
+
 import org.testng.annotations.Test;
 
 import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.MailItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.ui.Shortcut;
 import com.zimbra.qa.selenium.framework.util.GeneralUtility;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
 import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew;
 import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
 
@@ -57,6 +61,48 @@ public class SaveDraftMail extends AjaxCommonTest {
 		ZAssert.assertEquals(draft.dSubject, subject, "Verify the subject field is correct");
 		ZAssert.assertEquals(draft.dFolderId, draftsFolder.getId(), "Verify the draft is saved in the drafts folder");
 
+
+	}
+	/**
+	 * Test Case: Save draft using keyboard shortcut 'Escape''
+	 * 1.Compose Text mail
+	 * 2.Press 'Esc' key of keyboard
+	 * 3.Verify 'SaveCurrentMessageAsDraft'Warning Dialog
+	 * 4.Press Yes
+	 * 5.Verify Message is present in Draft
+	 * @throws HarnessException
+	 */
+	
+	@Test(description = "Save draft using keyboard shortcut 'Escape'", groups = { "functional" })
+	public void SaveDraftMail_02() throws HarnessException {
+
+		Shortcut shortcut = Shortcut.S_ESCAPE;
+		// Create the message data to be sent
+		String body = "body" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Open the new mail form
+		FormMailNew mailform = (FormMailNew) app.zPageMail.zToolbarPressButton(Button.B_NEW);
+		ZAssert.assertTrue(mailform.zIsActive(), "Verify the new form opened");
+
+		// Fill out the form with the data
+		mailform.zFillField(Field.Subject, subject);
+		mailform.zFillField(Field.Body, body);
+
+		DialogWarning warning = (DialogWarning) app.zPageMail.zKeyboardShortcut(shortcut);
+		ZAssert.assertNotNull(warning, "Verify the dialog is opened");
+
+		warning.zClickButton(Button.B_YES);
+		warning.zWaitForClose(); // Make sure the dialog is dismissed
+
+		// Get the message from the server
+		MailItem draft = MailItem.importFromSOAP(app.zGetActiveAccount(),"subject:(" + subject + ")");
+
+		FolderItem draftsFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Drafts);
+
+		// Verify the draft data matches
+		ZAssert.assertEquals(draft.dSubject, subject,"Verify the subject field is correct");
+		ZAssert.assertEquals(draft.dFolderId, draftsFolder.getId(),"Verify the draft is saved in the drafts folder");
 
 	}
 
