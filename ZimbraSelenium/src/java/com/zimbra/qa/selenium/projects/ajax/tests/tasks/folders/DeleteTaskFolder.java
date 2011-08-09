e93820ebe53adff979dc9f9d30b38e2b309da419
 Unknown Perforce User <girishb>
2011-08-09 11:29:59 +0000

    bug:none
    --Added new class for deleting task list related test cases

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/folders/DeleteTaskFolder.java
@@ -0,0 +1,56 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks.folders;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogConfirm;
+
+
+public class DeleteTaskFolder extends AjaxCommonTest {
+	public DeleteTaskFolder() {
+		logger.info("New " + DeleteTaskFolder.class.getCanonicalName());
+
+		// test starts at the task tab
+		super.startingPage = app.zPageTasks;
+		super.startingAccountPreferences = null;
+	}	
+
+	
+	
+	@Test(description = "Delete Task list -right click delete", groups = { "smoke" })
+	public void DeleteTaskFolder_01() throws HarnessException {
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+		ZAssert.assertNotNull(taskFolder, "Verify the task is available");
+		
+		// Create the subTaskList
+		String name = "taskList" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name +"' l='"+ taskFolder.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subTaskList = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+		ZAssert.assertNotNull(subTaskList, "Verify the subfolder is available");
+		
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Delete the folder using context menu
+		DialogConfirm dlgConfirm =(DialogConfirm)app.zTreeTasks.zTreeItem(Action.A_RIGHTCLICK, Button.B_DELETE, subTaskList);
+		dlgConfirm.zClickButton(Button.B_YES);
+		
+		app.zGetActiveAccount().soapSend("<GetFolderRequest xmlns = 'urn:zimbraMail'/>");
+		String id = app.zGetActiveAccount().soapSelectValue("//mail:folder[@name='"+ name +"']", "id");
+		
+		ZAssert.assertNull(id, "Verify the subfolder is deleted");
+	}
+
+}
