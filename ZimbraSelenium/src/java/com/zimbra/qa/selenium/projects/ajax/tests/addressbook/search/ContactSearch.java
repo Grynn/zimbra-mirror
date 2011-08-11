29b985722ead96411329a4c6f36e6a76ea7b1775
 Unknown Perforce User <dieunguyen>
2011-08-11 20:58:31 +0000

    bug:none - Modified contact search test cases

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/search/ContactSearch.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/search/ContactSearch.java
@@ -1,11 +1,16 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.addressbook.search;
 
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 import com.zimbra.qa.selenium.framework.items.ContactItem;
 import com.zimbra.qa.selenium.framework.items.ContactItem.GenerateItemType;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
 
+import com.zimbra.qa.selenium.framework.ui.Action;
 import com.zimbra.qa.selenium.framework.ui.Button;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
 
@@ -24,48 +29,88 @@ public class ContactSearch extends AjaxCommonTest {
 
 	}
 	
-	@Test( description = "select all, search contact + gal ",
+	@Test( description = "select all, search contact in Addressbook + gal ",
 			groups = { "functional" })
 	public void searchAll() throws HarnessException {
+		// Create a contact via soap 
+		ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
+ 
 		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_ALL);
-		app.zPageSearch.zAddSearchQuery("first");
-		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
-	    //TODO verify contact displayed
-		
-		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_ALL);
-		app.zPageSearch.zAddSearchQuery("admin");
-		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
-		//TODO verify admin displayed
-	}
-
-	@Test(	description = "select contact, search contact in addressbook  ",
-			groups = { "functional" })
-	public void searchContactInAddressbook() throws HarnessException {
-		// Create a contact item
-		ContactItem contactItem = ContactItem.generateContactItem(GenerateItemType.Basic);
-	
-		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_CONTACTS);	  
 		app.zPageSearch.zAddSearchQuery(contactItem.firstName);
 		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
+		ZAssert.assertTrue(app.zPageAddressbook.zIsContactDisplayed(contactItem), "Verify contact " + contactItem.fileAs + " displayed");
 		
-		//TODO verify contact displayed
+		String name="admin";		
+		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_ALL);
+		app.zPageSearch.zAddSearchQuery("admin");
+		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
+		List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts(); 
+         
+        boolean isFound=false;
+	      for (ContactItem ci : contacts) {
+		    if (ci.fileAs.equals(name)) {
+		    	isFound = true;
+		    	break;
+	  	    }
+	      }
+			
+        ZAssert.assertTrue(isFound, "Verify contact " + name + " displayed");
+
+	}
+
+	@Test(	description = "select contact, search a contact existing in addressbook  ",
+			groups = { "functional" })
+	public void searchContactInAddressbook() throws HarnessException {
+		// Create a contact via soap 
+		ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
+ 
+		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_CONTACTS);	 		
+		app.zPageSearch.zAddSearchQuery(contactItem.firstName);
+		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
+		ZAssert.assertTrue(app.zPageAddressbook.zIsContactDisplayed(contactItem), "Verify contact " + contactItem.fileAs + " displayed");
+				
 		app.zPageSearch.zAddSearchQuery(contactItem.lastName);
 		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
-		
-		//TODO verify contact displayed
-		app.zPageSearch.zAddSearchQuery(contactItem.email);
+		ZAssert.assertTrue(app.zPageAddressbook.zIsContactDisplayed(contactItem), "Verify contact " + contactItem.fileAs + " displayed");
+	
+		app.zPageSearch.zAddSearchQuery(contactItem.email.substring(0,contactItem.email.indexOf('@')));
 		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
-		
-		//TODO verify contact displayed
+		ZAssert.assertTrue(app.zPageAddressbook.zIsContactDisplayed(contactItem), "Verify contact " + contactItem.fileAs + " displayed");
 	}
 	
-	@Test (	description = "select GAL, search contact in GAL ",
+	@Test(	description = "select contact, search a non-existing contact",
+			groups = { "functional" })
+	public void searchNonExistContact() throws HarnessException {
+		// Create a contact via soap 
+		ContactItem contactItem = ContactItem.generateContactItem(GenerateItemType.Basic);
+		
+		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_CONTACTS);	 		
+		app.zPageSearch.zAddSearchQuery(contactItem.firstName);
+		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
+		ZAssert.assertFalse(app.zPageAddressbook.zIsContactDisplayed(contactItem), "Verify contact " + contactItem.fileAs + " not displayed");
+
+	}
+
+	@Test (	description = "select GAL, search a contact in GAL ",
 			groups = { "functional" })
 	public void searchGAL() throws HarnessException {
-		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_GAL);
+		String name="admin";
+		app.zPageSearch.zToolbarPressPulldown(Button.B_SEARCHTYPE, Button.O_SEARCHTYPE_GAL);			
 		app.zPageSearch.zAddSearchQuery("admin");
 		app.zPageSearch.zToolbarPressButton(Button.B_SEARCH);
 		
-		//TODO: verify admin displayed
+		List<ContactItem> contacts = app.zPageAddressbook.zListGetContacts(); 
+ 	           
+        boolean isFound=false;
+	      for (ContactItem ci : contacts) {
+		    if (ci.fileAs.equals(name)) {
+		    	isFound = true;
+		    	break;
+	  	    }
+	      }
+			
+        ZAssert.assertTrue(isFound, "Verify contact " + name + " displayed");
+
+
 	}
 }
