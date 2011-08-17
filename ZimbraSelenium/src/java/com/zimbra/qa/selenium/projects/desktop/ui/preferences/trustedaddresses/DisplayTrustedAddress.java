5766652e199393ace35d94ea9f2939f88af3dbed
 Unknown Perforce User <jhidayat>
2011-08-17 23:01:08 +0000

    Integrate Change 326139 by jhidayat@ZimbraQA_jhidayat on 8/17/2011 3:58:23 PM Bug: None ZDesktop Selenium: Imported TrustedEmailAddrMsgView, TrustedDomainMsgView, and TrustedDomainConvView tests from Ajax to ZDesktop.

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/ui/preferences/trustedaddresses/DisplayTrustedAddress.java
@@ -0,0 +1,103 @@
+package com.zimbra.qa.selenium.projects.desktop.ui.preferences.trustedaddresses;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.zimbra.qa.selenium.framework.ui.AbsApplication;
+import com.zimbra.qa.selenium.framework.ui.AbsDisplay;
+import com.zimbra.qa.selenium.framework.ui.AbsPage;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+
+public class DisplayTrustedAddress extends AbsDisplay {
+
+   public static class Locators {
+      public static final String zMsgViewDisplayImgLink = "css=a#zv__TV__MSG_displayImages_dispImgs";
+      public static final String zMsgViewDomainLink = "css=a#zv__TV__MSG_displayImages_domain";
+      public static final String zMsgViewWarningIcon = "css=div#zv__TV__MSG_displayImages.DisplayImages div div.ImgWarning";
+
+      public static final String zConViewDisplayImgLink = "css=a#zv__CLV__MSG_displayImages_dispImgs";
+      public static final String zConViewDomainLink = "css=a#zv__CLV__MSG_displayImages_domain";
+      public static final String zConViewWarningIcon = "css=div#zv__CLV__MSG_displayImages.DisplayImages div div.ImgWarning";
+
+      public static final String IsConViewActive = "css=[parentid='zv__CLV']";
+      public static final String IsMsgViewActive = "css=[parentid='zv__TV']";
+   }
+
+   public DisplayTrustedAddress(AbsApplication application) {
+      super(application);
+      // TODO Auto-generated constructor stub
+   }
+
+   @Override
+   public String myPageName() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public AbsPage zPressButton(Button button) throws HarnessException {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean zIsActive() throws HarnessException {
+      // logger.warn("implement me", new Throwable());
+      return (sIsElementPresent(Locators.IsConViewActive) || sIsElementPresent(Locators.IsMsgViewActive));
+
+   }
+
+   public String zDisplayImageLink(String zimbraPrefGroupMailBy)
+         throws HarnessException {
+
+      String DisplayImgLink = null;
+      if (zimbraPrefGroupMailBy == "message") {
+         DisplayImgLink = sGetEval("selenium.browserbot.getCurrentWindow().document.getElementById('zv__TV__MSG_displayImages').style.display");
+         return DisplayImgLink;
+      } else if (zimbraPrefGroupMailBy == "conversation") {
+         DisplayImgLink = sGetEval("selenium.browserbot.getCurrentWindow().document.getElementById('zv__CLV__MSG_displayImages').style.display");
+         return DisplayImgLink;
+      } else {
+         throw new HarnessException("no logic defined  ");
+      }
+   }
+
+   /**
+    * Check warning icon,Display Image link,Domain link
+    * 
+    * @return
+    * @throws HarnessException
+    */
+   public boolean zHasWDDLinks(String zimbraPrefGroupMailBy)
+         throws HarnessException {
+
+      if (zimbraPrefGroupMailBy == "message") {
+         List<String> locators = Arrays.asList(
+               Locators.zMsgViewDisplayImgLink,
+               Locators.zMsgViewDomainLink, Locators.zMsgViewWarningIcon);
+
+         for (String locator : locators) {
+            if (!this.sIsElementPresent(locator))
+               return (false);
+         }
+
+         return (true);
+
+      } else if (zimbraPrefGroupMailBy == "conversation") {
+         List<String> locators = Arrays.asList(
+               Locators.zConViewDisplayImgLink,
+               Locators.zConViewDomainLink, Locators.zConViewWarningIcon);
+
+         for (String locator : locators) {
+            if (!this.sIsElementPresent(locator))
+               return (false);
+         }
+
+         return (true);
+      } else {
+         throw new HarnessException("no logic defined  ");
+      }
+   }
+
+}
