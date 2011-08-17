a2b93af9672688e658c5c26a5ac679e86b661c72
 Unknown Perforce User <girishb>
2011-08-17 13:42:11 +0000

    bug:none
    -- Added test cases as
    CreateTask_02() : Create new task using keyboard shortcut Esc- Verify through Soap
    CreateTask_03() : Create task using New menu pulldown  - verify through SOAP

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/CreateTask.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/CreateTask.java
@@ -2,13 +2,15 @@ package com.zimbra.qa.selenium.projects.ajax.tests.tasks;
 
 import java.util.HashMap;
 import org.testng.annotations.Test;
-
 import com.zimbra.qa.selenium.framework.items.TaskItem;
 import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.ui.Shortcut;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew;
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew.Field;
 
@@ -37,11 +39,69 @@ public class CreateTask extends AjaxCommonTest {
 		taskNew.zFillField(Field.Subject, subject);
 		taskNew.zFillField(Field.Body, body);
 		taskNew.zSubmit();
+		SleepUtil.sleepMedium();
 
 		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
 		ZAssert.assertEquals(task.getName(), subject, "Verify task subject");
 		ZAssert.assertEquals(task.gettaskBody().trim(), body.trim(), "Verify the task body");
 
 	}
+	/**
+	 * Test Case :Create new task using keyboard shortcut Esc
+	 * Open new Task 
+	 * Enter Subject and body
+	 * Press Escape 'Esc' shortcut 
+	 * Waring dialog should pop up and press Yes
+	 * Task should show in list 
+	 * @throws HarnessException
+	 */
+	@Test(description = "Create new task using keyboard shortcut Esc- Verify through Soap", groups = { "smoke" })
+	public void CreateTask_02() throws HarnessException {
+
+		Shortcut shortcut = Shortcut.S_ESCAPE;
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+		String body = "taskbody" + ZimbraSeleniumProperties.getUniqueString();
+		
+		//Click NEW button
+		FormTaskNew taskNew = (FormTaskNew) app.zPageTasks.zToolbarPressButton(Button.B_NEW);
+		
+		//Fill out resulting form		
+		taskNew.zFillField(Field.Subject, subject);
+		taskNew.zFillField(Field.Body, body);
+				
+		//Click Escape shortcut 'Esc'	
+		DialogWarning warning =(DialogWarning)app.zPageTasks.zKeyboardShortcut(shortcut);
+		ZAssert.assertNotNull(warning, "Verify the dialog is opened");
+
+		//Click Yes button of warning dialog
+		warning.zClickButton(Button.B_YES);
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertEquals(task.getName(), subject, "Verify task subject");
+		ZAssert.assertEquals(task.gettaskBody().trim(), body.trim(), "Verify the task body");
+
+	}
+	
+	@Test(	description = "Create task using New menu pulldown  - verify through SOAP",	groups = { "smoke" })
+	public void CreateTask_03() throws HarnessException {
+
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+		String body = "taskbody" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Click NEW drop down and click Task
+		FormTaskNew taskNew = (FormTaskNew) app.zPageTasks
+				.zToolbarPressPulldown(Button.B_NEW, Button.O_NEW_TASK);
+
+		// Fill out the resulting form
+		taskNew.zFillField(Field.Subject, subject);
+		taskNew.zFillField(Field.Body, body);
+		taskNew.zSubmit();
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(),subject);
+		ZAssert.assertEquals(task.getName(), subject, "Verify task subject");
+		ZAssert.assertEquals(task.gettaskBody().trim(), body.trim(),"Verify the task body");
+
+	}
+
 
 }
