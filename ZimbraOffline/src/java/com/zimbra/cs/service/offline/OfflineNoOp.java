34dfde7715f9e691985f6b4fc79a495c68b0eb49
Jeff Flanigan <jflanigan@zimbra.com>
2011-08-18 18:41:35 +0000

    Integrate change 326263 from ZDESKTOP-712        bug: 63444
           Add wait=(true|false) and time=(elapsed) to NoOpResponse for ZD. 
           This will help correlate the responses in dev=1 window and test the 
           theory that UI is sometimes blocking on NoOp when wait=true.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63444

--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineNoOp.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineNoOp.java
@@ -16,18 +16,31 @@ package com.zimbra.cs.service.offline;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.offline.OfflineSyncManager;
 import com.zimbra.cs.service.mail.NoOp;
 import com.zimbra.soap.SoapServlet;
 
 public class OfflineNoOp extends NoOp {
 
+    private static final String TIME_KEY = "ZDNoOpStartTime";
+
 	public Element handle(Element request, Map<String, Object> context) throws ServiceException {
 		if (!context.containsKey(SoapServlet.IS_RESUMED_REQUEST))
 			OfflineSyncManager.getInstance().clientPing();
-		
-		return super.handle(request, context);
+        boolean wait = request.getAttributeBool(MailConstants.A_WAIT, false);
+        long start = System.currentTimeMillis();
+        HttpServletRequest servletRequest = (HttpServletRequest) context.get(SoapServlet.SERVLET_REQUEST);
+        if (!context.containsKey(SoapServlet.IS_RESUMED_REQUEST)) {
+            servletRequest.setAttribute(TIME_KEY, start);
+        }
+		Element response = super.handle(request, context);
+		response.addAttribute(MailConstants.A_WAIT, wait);
+		response.addAttribute(MailConstants.A_TIME, System.currentTimeMillis()-(Long) servletRequest.getAttribute(TIME_KEY));
+		return response;
 	}
 }
