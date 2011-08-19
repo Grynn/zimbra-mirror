b2a2aae0baab20ea917c98a2e956fad65524be78
Andrei Bareli <abareli@zimbra.com>
2011-08-19 23:45:04 +0000

    bug:none -implemented test for OOO alert on login

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/bugs/PromptToTurnOffAutoreply.java
@@ -0,0 +1,71 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.briefcase.bugs;
+
+import java.util.HashMap;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.MailItem;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.AppAjaxClient;
+import com.zimbra.qa.selenium.projects.ajax.ui.briefcase.DialogOOOAlert;
+
+public class PromptToTurnOffAutoreply extends AjaxCommonTest {
+	final String  autoreplyMessage = "OOO";
+
+	public PromptToTurnOffAutoreply() {
+		logger.info("New " + PromptToTurnOffAutoreply.class.getCanonicalName());
+
+		// test starts in the Briefcase tab
+		super.startingPage = app.zPageBriefcase;
+
+		// use an account with OOO auto-reply enabled
+		super.startingAccountPreferences = new HashMap<String, String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				put("zimbraPrefOutOfOfficeReplyEnabled", "TRUE");
+				put("zimbraPrefOutOfOfficeReply", autoreplyMessage);
+			}
+		};
+	}
+
+	@Bugs(ids = "51990")
+	@Test(description = "Enable auto-reply message - Verify after login  alert dialog promts to turn off auto-reply", groups = { "unctional" })
+	public void PromptToTurnOffAutoreply_01() throws HarnessException {
+		ZimbraAccount account = app.zGetActiveAccount();
+		
+		DialogOOOAlert alert = new DialogOOOAlert(app,((AppAjaxClient) app).zPageBriefcase);
+		
+		ZAssert.assertTrue(alert.zIsActive() , "Verify turn off auto-reply alert dialog is displayed");
+
+		alert.zClickButton(Button.B_NO);
+		
+		// Create the message data to be sent
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Send message to self
+		account.soapSend("<SendMsgRequest xmlns='urn:zimbraMail'>" + "<m>"
+				+ "<e t='t' a='" + account.EmailAddress + "'/>" + "<su>"
+				+ subject + "</su>" + "<mp ct='text/plain'>"
+				+ "<content>content"
+				+ ZimbraSeleniumProperties.getUniqueString() + "</content>"
+				+ "</mp>" + "</m>" + "</SendMsgRequest>");
+
+		// Click on Mail tab
+		app.zPageMain.zClickAt("id=zb__App__Mail_title", "0,0");
+		
+		// Click Get Mail button to view folder in list
+		// app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		SleepUtil.sleepMedium();
+
+		MailItem mailItem = MailItem.importFromSOAP(account, "in:inbox " + autoreplyMessage);
+
+		ZAssert.assertTrue(mailItem.dBodyText.contains(autoreplyMessage),
+				"Verify auto-reply message is received");
+	}
+}
---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/briefcase/DialogOOOAlert.java
@@ -0,0 +1,105 @@
+package com.zimbra.qa.selenium.projects.ajax.ui.briefcase;
+
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+
+/**
+ * Represents an "Out Of Office Alert" dialog box
+ * <p>
+ */
+public class DialogOOOAlert extends AbsDialog {
+	public static class Locators {
+		public static final String zDialogClass = "DwtDialog";
+		public static final String zDialogButtonsClass = "DwtDialogButtonBar";
+		public static final String zDialogContentClass = "DwtDialogBody";		
+	}
+
+	public DialogOOOAlert(AbsApplication application, AbsTab tab) {
+		super(application, tab);
+		
+		logger.info("new " + DialogOOOAlert.class.getCanonicalName());
+	}
+
+	@Override
+	public String myPageName() {
+		return (this.getClass().getName());
+	}
+
+	@Override
+	public AbsPage zClickButton(Button button) throws HarnessException {
+		logger.info(myPageName() + " zClickButton(" + button + ")");
+
+		tracer.trace("Click dialog button " + button);
+		if ( button == null )
+			throw new HarnessException("button cannot be null");
+	
+		String locator = null;
+		AbsPage page = null; 
+
+		if (button == Button.B_YES) {
+			locator = "css=div[class='" + Locators.zDialogClass + "'] "
+					+ "div[class='" + Locators.zDialogButtonsClass
+					+ "'] td[class=ZWidgetTitle]:contains(Yes)";			
+		} else if (button == Button.B_NO) {
+			locator = "css=div[class='" + Locators.zDialogClass + "'] "
+					+ "div[class='" + Locators.zDialogButtonsClass
+					+ "'] td[class=ZWidgetTitle]:contains(No)";
+		} else {
+			throw new HarnessException("Button " + button + " not implemented");
+		}
+
+		// Make sure the locator was set
+		if (locator == null) {
+			throw new HarnessException("Button " + button + " not implemented");
+		}
+
+		// Make sure the locator exists
+		if (!this.sIsElementPresent(locator)) {
+			throw new HarnessException("Button " + button + " locator "
+					+ locator + " not present!");
+		}
+
+		// if(zIsActive())
+		// zGetDisplayedText("css=div." + Locators.zDialogClass + ":contains(Out Of Office)");
+
+		this.zClickAt(locator,"0,0");
+
+		// If the app is busy, wait for it to become active
+		this.zWaitForBusyOverlay();
+		
+		// If page was specified, make sure it is active
+		if ( page != null ) {			
+			// This function (default) throws an exception if never active
+			page.zWaitForActive();			
+		}
+
+		return (page);
+	}
+
+	@Override
+	public String zGetDisplayedText(String locator) throws HarnessException {
+		logger.info(myPageName() + " zGetDisplayedText(" + locator + ")");
+		if (locator == null)
+			throw new HarnessException("locator was null");
+
+		return (this.sGetText(locator));
+	}
+	
+	@Override
+	public boolean zIsActive() throws HarnessException {
+		logger.info(myPageName() + " zIsActive()");
+
+		String locator = "css=div." + Locators.zDialogClass + ":contains(Out Of Office Alert)";
+
+		if (!this.sIsElementPresent(locator)) {
+			return (false); // Not even present
+		}
+
+		if (!this.zIsVisiblePerPosition(locator, 0, 0)) {
+			return (false); // Not visible per position
+		}
+
+		logger.info(myPageName() + " zIsActive() = true");
+		return (true);
+	}
+}
