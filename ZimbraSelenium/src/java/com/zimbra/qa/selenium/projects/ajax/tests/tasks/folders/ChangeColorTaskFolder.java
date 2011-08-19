920235efa32ae788e365e7e044a5ee97cc78a062
 Unknown Perforce User <girishb>
2011-08-19 11:04:57 +0000

    bug:none
    Added 2 classes as ChangeColorTaskFolder.java and DragAndDropTaskFolder.java

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/folders/ChangeColorTaskFolder.java
@@ -0,0 +1,68 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks.folders;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DialogEditFolder;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DialogEditFolder.FolderColor;
+
+
+public class ChangeColorTaskFolder extends AjaxCommonTest {
+	public ChangeColorTaskFolder() {
+		logger.info("New " + ChangeColorTaskFolder.class.getCanonicalName());
+
+		// test starts at the task tab
+		super.startingPage = app.zPageTasks;
+		super.startingAccountPreferences = null;
+	}	
+
+	
+	
+	@Test(description = "Edit a folder, change the color (Context menu -> Edit)", groups = { "functional" })
+	public void ChangeColorTaskFolder_01() throws HarnessException {
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+		ZAssert.assertNotNull(taskFolder, "Verify the task is available");
+		
+		// Create the subTaskList
+		String name = "taskList" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name +"' l='"+ taskFolder.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subTaskList = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+		ZAssert.assertNotNull(subTaskList, "Verify the subfolder is available");
+		
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Edit the folder using context menu		
+		DialogEditFolder dialog = (DialogEditFolder) app.zTreeTasks.zTreeItem(
+				Action.A_RIGHTCLICK, Button.B_TREE_EDIT, subTaskList);
+		ZAssert.assertNotNull(dialog, "Verify the dialog opened");
+		
+		// Change the color, click OK
+		dialog.zSetNewColor(FolderColor.Blue);
+		dialog.zClickButton(Button.B_OK);
+
+		// Check the color
+		app.zGetActiveAccount().soapSend(
+				"<GetFolderRequest xmlns='urn:zimbraMail'>"
+			+		"<folder id='" + subTaskList.getId() + "'/>"
+			+	"</GetFolderRequest>");
+
+		String color = app.zGetActiveAccount().soapSelectValue("//mail:folder[@name='" + subTaskList.getName() + "']", "color");
+		ZAssert.assertEquals(color, "1", "Verify the color of the folder is set to blue (1)");
+	}
+	
+}
+
+
---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/folders/DragAndDropTaskFolder.java
@@ -0,0 +1,68 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks.folders;
+
+
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+
+public class DragAndDropTaskFolder extends AjaxCommonTest {
+	public DragAndDropTaskFolder() {
+		logger.info("New " + DragAndDropTaskFolder.class.getCanonicalName());
+
+		// test starts at the task tab
+		super.startingPage = app.zPageTasks;
+		super.startingAccountPreferences = null;
+	}	
+
+	
+	
+	@Test(description = "Drag one Task folder and Drop into other", groups = { "smoke" })
+	public void DragAndDropTaskFolder_01() throws HarnessException {
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+		ZAssert.assertNotNull(taskFolder, "Verify the task is available");
+		
+		// Create the subTaskList
+		String name1 = "taskList1" + ZimbraSeleniumProperties.getUniqueString();
+		String name2 = "taskList2" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name1 +"' l='"+ taskFolder.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subTaskList1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subTaskList1, "Verify the first subfolder is available");
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name2 +"' l='"+ taskFolder.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subTaskList2 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name2);
+		ZAssert.assertNotNull(subTaskList2, "Verify the second subfolder is available");	
+		
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+		
+		app.zPageMail.zDragAndDrop(
+				"//td[contains(@id, 'zti__main_Tasks__" + subTaskList1.getId() + "_textCell') and contains(text(), '"+ name1 + "')]",
+				"//td[contains(@id, 'zti__main_Tasks__" + subTaskList2.getId() + "_textCell') and contains(text(),'"+ name2 + "')]");
+
+		// Verify the folder is now in the other subfolder
+		subTaskList1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subTaskList1, "Verify the subfolder is again available");
+		ZAssert.assertEquals(subTaskList2.getId(), subTaskList1.getParentId(), "Verify the subfolder's parent is now the other subfolder");
+
+	}
+
+}
