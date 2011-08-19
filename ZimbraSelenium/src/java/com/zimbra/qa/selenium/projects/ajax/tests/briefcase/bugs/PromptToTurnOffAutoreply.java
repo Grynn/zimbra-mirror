0830080f3cd27cbebe51a8b9deb1583ae860f6c3
Andrei Bareli <abareli@zimbra.com>
2011-08-19 23:59:09 +0000

    bug:none -added check for version

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/bugs/PromptToTurnOffAutoreply.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/briefcase/bugs/PromptToTurnOffAutoreply.java
@@ -15,7 +15,7 @@ import com.zimbra.qa.selenium.projects.ajax.ui.AppAjaxClient;
 import com.zimbra.qa.selenium.projects.ajax.ui.briefcase.DialogOOOAlert;
 
 public class PromptToTurnOffAutoreply extends AjaxCommonTest {
-	final String  autoreplyMessage = "OOO";
+	final String autoreplyMessage = "OOO";
 
 	public PromptToTurnOffAutoreply() {
 		logger.info("New " + PromptToTurnOffAutoreply.class.getCanonicalName());
@@ -34,16 +34,20 @@ public class PromptToTurnOffAutoreply extends AjaxCommonTest {
 	}
 
 	@Bugs(ids = "51990")
-	@Test(description = "Enable auto-reply message - Verify after login  alert dialog promts to turn off auto-reply", groups = { "unctional" })
+	@Test(description = "Enable auto-reply message - Verify after login  alert dialog promts to turn off auto-reply", groups = { "functional" })
 	public void PromptToTurnOffAutoreply_01() throws HarnessException {
 		ZimbraAccount account = app.zGetActiveAccount();
-		
-		DialogOOOAlert alert = new DialogOOOAlert(app,((AppAjaxClient) app).zPageBriefcase);
-		
-		ZAssert.assertTrue(alert.zIsActive() , "Verify turn off auto-reply alert dialog is displayed");
 
-		alert.zClickButton(Button.B_NO);
-		
+		if (ZimbraSeleniumProperties.zimbraGetVersionString().contains("8.0.")) {
+			DialogOOOAlert alert = new DialogOOOAlert(app,
+					((AppAjaxClient) app).zPageBriefcase);
+
+			ZAssert.assertTrue(alert.zIsActive(),
+					"Verify turn off auto-reply alert dialog is displayed");
+
+			alert.zClickButton(Button.B_NO);
+		}
+
 		// Create the message data to be sent
 		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
 
@@ -57,13 +61,14 @@ public class PromptToTurnOffAutoreply extends AjaxCommonTest {
 
 		// Click on Mail tab
 		app.zPageMain.zClickAt("id=zb__App__Mail_title", "0,0");
-		
+
 		// Click Get Mail button to view folder in list
 		// app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
 
 		SleepUtil.sleepMedium();
 
-		MailItem mailItem = MailItem.importFromSOAP(account, "in:inbox " + autoreplyMessage);
+		MailItem mailItem = MailItem.importFromSOAP(account, "in:inbox "
+				+ autoreplyMessage);
 
 		ZAssert.assertTrue(mailItem.dBodyText.contains(autoreplyMessage),
 				"Verify auto-reply message is received");
