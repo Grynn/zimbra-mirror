ea4cc14383f402b3f88dba075b917250c9433db3
 Unknown Perforce User <rhoades>
2011-08-24 04:26:12 +0000

    Bug: 56320
    Selenium Harness: clean up zimlets test case to look specifically at the locators for the rows.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=56320

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/zimlets/GetZimlets.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/zimlets/GetZimlets.java
@@ -1,7 +1,5 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.preferences.zimlets;
 
-import java.util.Hashtable;
-
 import org.testng.annotations.Test;
 
 import com.zimbra.qa.selenium.framework.ui.Action;
@@ -49,35 +47,17 @@ public class GetZimlets extends AjaxCommonTest {
 			)
 	public void GetZimlets_02() throws HarnessException {
 
-		Hashtable<String, String> table = new Hashtable<String, String>();
-
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Zimlets);
-
-		// The locator to the table
-		String tablelocator = "css=div[id='ZmPrefZimletListView']";
 		
 		// The locator to the rows (not including the header)
-		String locator = tablelocator + " div[id$='__rows']";
+		String locator = "css=div[id='ZmPrefZimletListView'] div[id$='__rows']";
 
-		// Get how many rows there are
-		int count = app.zPagePreferences.sGetCssCount(locator + ">div");
-		
-		// Scroll through all the rows.  Make sure the details are correct
-		// TODO: need to I18N
-		for (int i = 1; i <= count; i++) {
-			String itemlocator = locator +">div:nth-of-type("+ i +")";
+		String name = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_linkedin__na']");
+		String description = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_linkedin__ds']");
 
-			String name = app.zPagePreferences.sGetText(itemlocator + " td[id$='__na']");
-			String description = app.zPagePreferences.sGetText(itemlocator + " td[id$='__ds']");
-
-			table.put(name, description);
-			
-		}
-		
-		ZAssert.assertNotNull(table.get("LinkedIn"), "Verify the LinkedIn entry exists");
-		ZAssert.assertEquals(table.get("LinkedIn"), "Hooks on to email Zimlet; shows LinkedIn search result for a given email.", "Verify the LinkedIn description");
-		
+		ZAssert.assertEquals(name, "LinkedIn", "Verify the LinkedIn entry name");
+		ZAssert.assertEquals(description, "Hooks on to email Zimlet; shows LinkedIn search result for a given email.", "Verify the LinkedIn description");		
 		
 	}
 
@@ -87,34 +67,17 @@ public class GetZimlets extends AjaxCommonTest {
 			)
 	public void GetZimlets_03() throws HarnessException {
 
-		Hashtable<String, String> table = new Hashtable<String, String>();
-
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Zimlets);
 
 		// The locator to the table
-		String tablelocator = "css=div[id='ZmPrefZimletListView']";
-		
-		// The locator to the rows (not including the header)
-		String locator = tablelocator + " div[id$='__rows']";
+		String locator = "css=div[id='ZmPrefZimletListView'] div[id$='__rows']";
 
-		// Get how many rows there are
-		int count = app.zPagePreferences.sGetCssCount(locator + ">div");
+		String name = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_phone__na']");
+		String description = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_phone__ds']");
 		
-		// Scroll through all the rows.  Make sure the details are correct
-		// TODO: need to I18N
-		for (int i = 1; i <= count; i++) {
-			String itemlocator = locator +">div:nth-of-type("+ i +")";
-
-			String name = app.zPagePreferences.sGetText(itemlocator + " td[id$='__na']");
-			String description = app.zPagePreferences.sGetText(itemlocator + " td[id$='__ds']");
-
-			table.put(name, description);
-			
-		}
-		
-		ZAssert.assertNotNull(table.get("Phone"), "Verify the Phone entry exists");
-		ZAssert.assertEquals(table.get("Phone"), "Highlights phone numbers to enable Skype calls.", "Verify the Phone description");
+		ZAssert.assertEquals(name, "Phone", "Verify the Phone entry exists");
+		ZAssert.assertEquals(description, "Highlights phone numbers to enable Skype calls.", "Verify the Phone description");
 		
 		
 	}
@@ -126,34 +89,17 @@ public class GetZimlets extends AjaxCommonTest {
 			)
 	public void GetZimlets_04() throws HarnessException {
 
-		Hashtable<String, String> table = new Hashtable<String, String>();
-
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Zimlets);
 
 		// The locator to the table
-		String tablelocator = "css=div[id='ZmPrefZimletListView']";
-		
-		// The locator to the rows (not including the header)
-		String locator = tablelocator + " div[id$='__rows']";
+		String locator = "css=div[id='ZmPrefZimletListView'] div[id$='__rows']";
 
-		// Get how many rows there are
-		int count = app.zPagePreferences.sGetCssCount(locator + ">div");
+		String name = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_srchhighlighter__na']");
+		String description = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_srchhighlighter__ds']");
 		
-		// Scroll through all the rows.  Make sure the details are correct
-		// TODO: need to I18N
-		for (int i = 1; i <= count; i++) {
-			String itemlocator = locator +">div:nth-of-type("+ i +")";
-
-			String name = app.zPagePreferences.sGetText(itemlocator + " td[id$='__na']");
-			String description = app.zPagePreferences.sGetText(itemlocator + " td[id$='__ds']");
-
-			table.put(name, description);
-			
-		}
-		
-		ZAssert.assertNotNull(table.get("Search Highlighter"), "Verify the Search Highlighter entry exists");
-		ZAssert.assertEquals(table.get("Search Highlighter"), "After a mail search, this Zimlet highlights Search terms with yellow color.", "Verify the Search Highlighter description");
+		ZAssert.assertEquals(name, "Search Highlighter", "Verify the Search Highlighter entry exists");
+		ZAssert.assertEquals(description, "After a mail search, this Zimlet highlights Search terms with yellow color.", "Verify the Search Highlighter description");
 		
 		
 	}
@@ -165,34 +111,17 @@ public class GetZimlets extends AjaxCommonTest {
 			)
 	public void GetZimlets_05() throws HarnessException {
 
-		Hashtable<String, String> table = new Hashtable<String, String>();
-
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Zimlets);
 
 		// The locator to the table
-		String tablelocator = "css=div[id='ZmPrefZimletListView']";
-		
-		// The locator to the rows (not including the header)
-		String locator = tablelocator + " div[id$='__rows']";
+		String locator = "css=div[id='ZmPrefZimletListView'] div[id$='__rows']";
 
-		// Get how many rows there are
-		int count = app.zPagePreferences.sGetCssCount(locator + ">div");
+		String name = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_webex__na']");
+		String description = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_webex__ds']");
 		
-		// Scroll through all the rows.  Make sure the details are correct
-		// TODO: need to I18N
-		for (int i = 1; i <= count; i++) {
-			String itemlocator = locator +">div:nth-of-type("+ i +")";
-
-			String name = app.zPagePreferences.sGetText(itemlocator + " td[id$='__na']");
-			String description = app.zPagePreferences.sGetText(itemlocator + " td[id$='__ds']");
-
-			table.put(name, description);
-			
-		}
-		
-		ZAssert.assertNotNull(table.get("WebEx"), "Verify the WebEx entry exists");
-		ZAssert.assertEquals(table.get("WebEx"), "Easily schedule, start or join WebEx meetings.", "Verify the WebEx description");
+		ZAssert.assertEquals(name, "WebEx", "Verify the WebEx entry exists");
+		ZAssert.assertEquals(description, "Easily schedule, start or join WebEx meetings.", "Verify the WebEx description");
 		
 		
 	}
@@ -203,34 +132,17 @@ public class GetZimlets extends AjaxCommonTest {
 			)
 	public void GetZimlets_06() throws HarnessException {
 
-		Hashtable<String, String> table = new Hashtable<String, String>();
-
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Zimlets);
 
 		// The locator to the table
-		String tablelocator = "css=div[id='ZmPrefZimletListView']";
-		
-		// The locator to the rows (not including the header)
-		String locator = tablelocator + " div[id$='__rows']";
+		String locator = "css=div[id='ZmPrefZimletListView'] div[id$='__rows']";
 
-		// Get how many rows there are
-		int count = app.zPagePreferences.sGetCssCount(locator + ">div");
+		String name = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_social__na']");
+		String description = app.zPagePreferences.sGetText(locator + " td[id$='__com_zimbra_social__ds']");
 		
-		// Scroll through all the rows.  Make sure the details are correct
-		// TODO: need to I18N
-		for (int i = 1; i <= count; i++) {
-			String itemlocator = locator +">div:nth-of-type("+ i +")";
-
-			String name = app.zPagePreferences.sGetText(itemlocator + " td[id$='__na']");
-			String description = app.zPagePreferences.sGetText(itemlocator + " td[id$='__ds']");
-
-			table.put(name, description);
-			
-		}
-		
-		ZAssert.assertNotNull(table.get("Zimbra Social"), "Verify the Zimbra Social entry exists");
-		ZAssert.assertEquals(table.get("Zimbra Social"), "Access social services like Twitter, Facebook, Digg and TweetMeme.", "Verify the Zimbra Social description");
+		ZAssert.assertEquals(name, "Zimbra Social", "Verify the Zimbra Social entry exists");
+		ZAssert.assertEquals(description, "Access social services like Twitter, Facebook, Digg and TweetMeme.", "Verify the Zimbra Social description");
 		
 		
 	}
