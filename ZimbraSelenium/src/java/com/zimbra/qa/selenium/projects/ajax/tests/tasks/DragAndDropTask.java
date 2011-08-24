a7549641cafd2e1a07845d0f09a8abe02185d34c
 Unknown Perforce User <girishb>
2011-08-24 06:49:23 +0000

    bug:none
    -- Added new test case as DragAndDropTask_01() for drag and drop task into subfolder

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/DragAndDropTask.java
@@ -0,0 +1,107 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks;
+import java.util.List;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+public class DragAndDropTask extends AjaxCommonTest {
+
+	public DragAndDropTask() {
+		logger.info("DragAndDropTask " + DragAndDropTask.class.getCanonicalName());		
+		super.startingPage = app.zPageTasks;
+		super.startingAccountPreferences = null;
+	}
+	
+	@Test(description = "Drag and Drop a task from Tasks to subfolder", groups = { "smoke" })
+	public void DragAndDropTask_01() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		String name = "TaskFolder" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Create a subfolder to move the message into i.e. tasks/subfolder
+		String taskFolderID = taskFolder.getId();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + name + "' l='" + taskFolderID + "'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+
+		// refresh task page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Send a task to the account
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>"
+				+		"<m >"
+				+			"<inv>"
+				+				"<comp name='" + subject + "'>"
+				+					"<or a='"+ app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"</comp>" 
+				+			"</inv>"
+				+			"<su>" + subject + "</su>"
+				+			"<mp ct='text/plain'>"
+				+				"<content>content" + ZimbraSeleniumProperties.getUniqueString() + "</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		// Get the task item for the new task
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(),subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the created task item
+		app.zPageTasks.zListItem(Action.A_LEFTCLICK, subject);
+		
+		//Drag and drop task item into subfolder
+		app.zPageMail.zDragAndDrop(
+				"css=td[id$='"+task.getId() +"__su']",
+				"css=td[id='zti__main_Tasks__"+ subFolder.getId() + "_textCell']:contains('"+ name + "')");
+		
+		// refresh tasks page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
+		TaskItem found = null;
+		for (TaskItem t : tasks) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				found = t;
+				break;
+			}
+		}
+
+		ZAssert.assertNull(found,"Verify the  task no longer  present in the task list");
+
+		// click on subfolder in tree view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, subFolder);
+		List<TaskItem> tasks1 = app.zPageTasks.zGetTasks();
+
+		TaskItem movetask = null;
+		for (TaskItem t : tasks1) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				movetask = t;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(movetask,	"Verify the task is drag and drop into the selected folder");
+	}
+}
