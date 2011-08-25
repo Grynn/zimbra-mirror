d5c4078f6e38009d67f5afa9c93e830cf221b8f9
 Unknown Perforce User <rhoades>
2011-08-25 19:20:46 +0000

    Bug: 63289
    Selenium Harness: clean up export test cases - click the checkbox using sClick()
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=63289

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/importexport/ExportAccount.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/importexport/ExportAccount.java
@@ -12,7 +12,7 @@ import com.zimbra.qa.selenium.projects.ajax.ui.preferences.TreePreferences.TreeI
 public class ExportAccount extends AjaxCommonTest {
 
 	public ExportAccount() {
-		
+
 		super.startingPage = app.zPagePreferences;
 		super.startingAccountPreferences = null;
 	}
@@ -21,10 +21,10 @@ public class ExportAccount extends AjaxCommonTest {
 	@Test(
 			description = "Verify clicking on 'export account' radio button",
 			groups = { "functional" }
-			)
+	)
 	public void ExportAccount_01() throws HarnessException {
 
-		
+
 		/**
 		 * 
 		 * TODO: Since selenium doesn't handle the
@@ -33,8 +33,8 @@ public class ExportAccount extends AjaxCommonTest {
 		 * verify the display.
 		 * 
 		 */
-		
-		
+
+
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.ImportExport);
 
@@ -47,15 +47,17 @@ public class ExportAccount extends AjaxCommonTest {
 		// Verify the help hint
 		String hint = app.zPagePreferences.sGetText("css=div[id$='_TYPE_HINT']");
 		ZAssert.assertStringContains(hint, "All account data can be exported", "Verify help hint text changed");
-		
+
 	}
-	
+
 	@Test(
 			description = "Verify clicking on 'Advanced Settings' checkbox",
 			groups = { "functional" }
-			)
+	)
 	public void ExportAccount_02() throws HarnessException {
 
+		String locator;
+		
 		
 		/**
 		 * 
@@ -65,23 +67,46 @@ public class ExportAccount extends AjaxCommonTest {
 		 * verify the display.
 		 * 
 		 */
-		
-		
+
+
 		// Navigate to preferences -> notifications
 		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.ImportExport);
 
-		// TODO: See https://bugzilla.zimbra.com/show_bug.cgi?id=63289
-		// I don't think this Locator is unique enough to find the Advanced Search checkbox
-		String locator = "css=div[id$='_ADVANCED_control'] input[type='checkbox']";
-		if ( !app.zPagePreferences.sIsChecked(locator) ) {
-			app.zPagePreferences.sCheck(locator);
-			app.zPagePreferences.zWaitForBusyOverlay();
-		}
+		// Make srue the fields are invisible first
+		
+		locator = "css=tr[id='ZmExportView_DATA_TYPES_row'][style='display: table-row;']";
+		ZAssert.assertFalse(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is invisible");
 
-		throw new HarnessException("See https://bugzilla.zimbra.com/show_bug.cgi?id=63289");
+		locator = "css=tr[id='ZmExportView_DATE_row'][style='display: table-row;']";
+		ZAssert.assertFalse(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is invisible");
+
+		locator = "css=tr[id='ZmExportView_SEARCH_FILTER_row'][style='display: table-row;']";
+		ZAssert.assertFalse(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is invisible");
+
+		locator = "css=tr[id='ZmExportView_SKIP_META_row'][style='display: table-row;']";
+		ZAssert.assertFalse(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is invisible");
+
+		// See https://bugzilla.zimbra.com/show_bug.cgi?id=63289
+		// Although it looks like a checkbox, it seems sClick is ok for checking it.
+		locator = "css=input[id='ZmExportView_ADVANCED']";
+		app.zPagePreferences.sClick(locator);
+		app.zPagePreferences.zWaitForBusyOverlay();
+
+		// Verify the extra options now appear
 		
-		// TODO: add some verification for the other advanced options displayed text.
-		
+		locator = "css=tr[id='ZmExportView_DATA_TYPES_row'][style='display: table-row;']";
+		ZAssert.assertTrue(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is visible");
+
+		locator = "css=tr[id='ZmExportView_DATE_row'][style='display: table-row;']";
+		ZAssert.assertTrue(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is visible");
+
+		locator = "css=tr[id='ZmExportView_SEARCH_FILTER_row'][style='display: table-row;']";
+		ZAssert.assertTrue(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is visible");
+
+		locator = "css=tr[id='ZmExportView_SKIP_META_row'][style='display: table-row;']";
+		ZAssert.assertTrue(app.zPagePreferences.sIsElementPresent(locator), "Verify the advanced data is visible");
+
+
 	}
 
 }
