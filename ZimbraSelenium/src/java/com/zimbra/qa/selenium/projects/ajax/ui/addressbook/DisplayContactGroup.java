2e1d70ee1de0dfef39b1ed3ab40a32564545bc84
 Unknown Perforce User <dieunguyen>
2011-08-28 23:22:15 +0000

    bug:none - Fixed css locator

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/addressbook/DisplayContactGroup.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/addressbook/DisplayContactGroup.java
@@ -95,7 +95,7 @@ public class DisplayContactGroup extends AbsDisplay {
 			 throw new HarnessException("Unable to find the field = "+ field +" using locator = "+ locator);
 		
 		   // Get the element value
-		    value += this.sGetText(locator).trim();		
+		    value += sGetText(locator).trim();		
 		}
 		
 		logger.info("DisplayContactGroup.zGetContactProperty(" + field + ") = " + value);
@@ -111,11 +111,11 @@ public class DisplayContactGroup extends AbsDisplay {
 	
 
     private void getAllLocators(ArrayList<String> array, String postfix) throws HarnessException {
-  	   //String css= "css=div[id$='_content'][class='ZmContactInfoView'] table:nth-of-type(2) tbody tr";
-  	   String css= "css=div[id$='_content'][class='ZmContactInfoView'] table.contactHeaderTable";
-     
+  	   String css= "css=div[id$='_content'][class='ZmContactInfoView'] table:nth-of-type(2) tbody tr";
+  	   
   	   int count= this.sGetCssCount(css);
 
+    	
        for (int i=1; i<=count; i++) {
     	 String tdLocator=  css + ":nth-of-type(" + i + ")" + " td[id$='_" + postfix + "']";  
 	     if (sIsElementPresent(tdLocator)) {
