7df951aa3469fe52a3c88f5d55ff9051fad1a3cc
 Unknown Perforce User <rhoades>
2011-09-01 18:56:54 +0000

    Bug: none
    Selenium Harness: run postqueue after LMTP inject.

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/util/LmtpInject.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/util/LmtpInject.java
@@ -9,6 +9,7 @@ import com.zimbra.common.util.ByteUtil;
 import com.zimbra.common.lmtp.LmtpClient;
 import com.zimbra.common.lmtp.LmtpProtocolException;
 import com.zimbra.qa.selenium.framework.core.ExecuteHarnessMain;
+import com.zimbra.qa.selenium.framework.util.staf.Stafpostqueue;
 
 /**
  * @author Matt Rhoades
@@ -24,15 +25,15 @@ public class LmtpInject {
 	 * @throws HarnessException
 	 */
 	public static void injectFile(String recipient, File mime) throws HarnessException {
-		
+
 		// conver the recipient to an array
 		injectFile(
 				Arrays.asList(recipient.split(",")),
 				mime
-			);
-		
+		);
+
 	}
-	
+
 	/**
 	 * Inject a mime file to a list of email addresses
 	 * @param recipients an array of email addresses
@@ -40,13 +41,13 @@ public class LmtpInject {
 	 * @throws HarnessException
 	 */
 	public static void injectFile(List<String> recipients, File mime) throws HarnessException {
-		
+
 		// Use default sender
 		injectFile(
 				recipients, 
 				"foo@example.com", 
 				mime);
-		
+
 	}
 
 	/**
@@ -57,28 +58,18 @@ public class LmtpInject {
 	 * @throws HarnessException
 	 */
 	public static void injectFile(List<String> recipients, String sender, File mime) throws HarnessException  {
-		
+
 		try {
 			
-			if ( mime.isFile() ) { 
-			
-				// Inject a single file
-				inject(recipients, sender, mime);
-				
-			} else if ( mime.isDirectory() ) {
-				
-				// Inject a folder full of files
-				for (File f : mime.listFiles()) {
-					
-					injectFile(recipients, sender, f);
-					
-				}
-				
-			} else {
-				
-				// Unknown File type
-				logger.warn("MIME file was not file or directory.  Skipping. " + mime.getAbsolutePath());
-				
+			try {
+
+				injectFolder(recipients, sender, mime);
+
+			} finally {
+
+				Stafpostqueue sp = new Stafpostqueue();
+				sp.waitForPostqueue();
+
 			}
 			
 		} catch (IOException e) {
@@ -86,46 +77,71 @@ public class LmtpInject {
 		} catch (LmtpProtocolException e) {
 			throw new HarnessException("Unable to inject mime file "+ mime.getAbsolutePath(), e);
 		}
-				
+
 	}
+
+	protected static void injectFolder(List<String> recipients, String sender, File mime) throws IOException, LmtpProtocolException {
+
+		if ( mime.isFile() ) { 
+
+			// Inject a single file
+			inject(recipients, sender, mime);
+
+		} else if ( mime.isDirectory() ) {
+
+			for (File f : mime.listFiles()) {
+
+				injectFolder(recipients, sender, f);
+
+			}
+
+		} else {
+
+			// Unknown File type
+			logger.warn("MIME file was not file or directory.  Skipping. " + mime.getAbsolutePath());
+
+		}
+
+	}
+	
 	
 	protected static void inject(List<String> recipients, String sender, File mime) throws IOException, LmtpProtocolException {
 		logger.info("LMTP: to: "+ recipients.toString());
 		logger.info("LMTP: from: "+ sender);
 		logger.info("LMTP: filename: "+ mime.getAbsolutePath());
-		
+
 		long length = mime.length();
 
 		logger.info( length > 2000 ? "LMTP: large mime" : "LMTP:\n" + new String(ByteUtil.getContent(mime)));
-		
+
 		LogManager.getLogger(ExecuteHarnessMain.TraceLoggerName).trace(
 				"Inject using LMTP: " +
-                     " to:"+ recipients.toString() + 
-                   " from:"+ sender +
-               " filename:"+ mime.getAbsolutePath()
-			);
-		
-		
+				" to:"+ recipients.toString() + 
+				" from:"+ sender +
+				" filename:"+ mime.getAbsolutePath()
+		);
+
+
 		LmtpClient lmtp = null;
 		try {
-			
+
 			lmtp = new LmtpClient( 
 					ZimbraSeleniumProperties.getStringProperty("server.host"),
 					7025);
-			
+
 			lmtp.sendMessage(new FileInputStream(mime), (String[])recipients.toArray(), sender, "Selenium", length);
 
 		} finally {
-			
+
 			// TODO: do we need to close the FileInputStream?
-			
+
 			if (lmtp != null ) {
 				lmtp.close();
 				lmtp = null;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 }
