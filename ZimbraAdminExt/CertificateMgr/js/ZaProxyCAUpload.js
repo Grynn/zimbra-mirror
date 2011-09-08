f63787ff603c4c6b778456161835427650c2ea2e
 Unknown Perforce User <wanglu>
2011-09-08 02:35:18 +0000

    Bug 62895        Add lable for certificate.
    http://bugzilla.zimbra.com/show_bug.cgi?id=62895

--- ZimbraAdminExt/CertificateMgr/js/ZaProxyCAUpload.js
+++ ZimbraAdminExt/CertificateMgr/js/ZaProxyCAUpload.js
@@ -3,9 +3,10 @@ ZaProxyCAUpload = function() {}
 
 ZaProxyCAUpload.myXFormModifier = function(xFormObject) {
     var uploadCertUI =
-        {type:_GROUP_,  numCols: 2, colSizes: ["250px","100px"], colSpan: "*",
-            cssStyle: "margin-bottom: 10px;margin-left: 270px",
+        {type:_GROUP_,  numCols: 3, colSizes: ["120px","250px","100px"], colSpan: "*",
+            cssStyle: "margin-bottom: 10px;margin-left: 150px",
             items: [
+{type:_OUTPUT_, value: com_zimbra_cert_manager.certFile,cssStyle:"margin-left:10px"},
             {type:_OUTPUT_, value: ZaProxyCAUpload.getUploadFormHtml(this) },
             {type: _DWT_BUTTON_ , label: com_zimbra_cert_manager.CERT_UploadButton, width: "9em",
                onActivate: ZaProxyCAUpload.uploadCertKeyFile
