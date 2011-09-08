dec3a9be51cc24b4941f223c0570fefd088db47c
 Unknown Perforce User <rhoades>
2011-09-08 20:57:18 +0000

    Bug: none
    Selenium Harness: add test cases for deleting messages in a view-only share

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mountpoints/viewer/DeleteMail.java
@@ -0,0 +1,189 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.mountpoints.viewer;
+
+import java.awt.event.KeyEvent;
+import java.util.*;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.DialogError;
+
+
+public class DeleteMail extends AjaxCommonTest {
+
+	@SuppressWarnings("serial")
+	public DeleteMail() {
+		logger.info("New "+ DeleteMail.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageMail;
+
+		// Make sure we are using an account with message view
+		super.startingAccountPreferences = new HashMap<String, String>() {{
+				    put("zimbraPrefGroupMailBy", "message");
+				}};
+		
+	}
+	
+	@Test(	description = "Verify Delete button is disabled and non-functional on mountpoint message (read-only share)",
+			groups = { "functional" })
+	public void DeleteMail_01() throws HarnessException {
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String mountpointname = "mountpoint" + ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem inbox = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox);
+		
+		// Create a folder to share
+		ZimbraAccount.AccountA().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + foldername + "' l='" + inbox.getId() + "'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem folder = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), foldername);
+		
+		// Share it
+		ZimbraAccount.AccountA().soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ folder.getId() +"' op='grant'>"
+				+			"<grant d='"+ app.zGetActiveAccount().EmailAddress +"' gt='usr' perm='r'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+		// Add a message to it
+		ZimbraAccount.AccountA().soapSend(
+					"<AddMsgRequest xmlns='urn:zimbraMail'>"
+        		+		"<m l='"+ folder.getId() +"' >"
+            	+			"<content>From: foo@foo.com\n"
+            	+				"To: foo@foo.com \n"
+            	+				"Subject: "+ subject +"\n"
+            	+				"MIME-Version: 1.0 \n"
+            	+				"Content-Type: text/plain; charset=utf-8 \n"
+            	+				"Content-Transfer-Encoding: 7bit\n"
+            	+				"\n"
+            	+				"simple text string in the body\n"
+            	+			"</content>"
+            	+		"</m>"
+				+	"</AddMsgRequest>");
+		
+		MailItem mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+
+		
+		// Mount it
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointname +"'  rid='"+ folder.getId() +"' zid='"+ ZimbraAccount.AccountA().ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+				
+		// Click on the mountpoint
+		app.zTreeMail.zTreeItem(Action.A_LEFTCLICK, mountpoint);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+		
+		// Verify delete is disabled
+		ZAssert.assertTrue(app.zPageMail.sIsElementPresent("css=div[id='zb__TV__DELETE'].ZDisabled"), "Verify Delete button is disabled");
+		
+		// Delete the item
+		app.zPageMail.zToolbarPressButton(Button.B_DELETE);
+
+		
+		// A "Permission Denied" error popup should NOT occur
+		DialogError dialog = app.zPageMain.zGetErrorDialog(DialogError.DialogErrorID.Zimbra);
+		ZAssert.assertNotNull(dialog, "Verify the PERM DENIED Error Dialog is created");
+		ZAssert.assertFalse(dialog.zIsActive(), "Verify the PERM DENIED Error Dialog is active");
+				
+		// Make sure the server does not show "flagged" for the owner
+		mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+		ZAssert.assertNotNull(mail, "Verify the message still exists in accountA's mailbox");
+
+		
+	}
+
+	
+	@Test(	description = "Verify Delete keyboard shortcut is non-functional on mountpoint message (read-only share)",
+			groups = { "functional" })
+	public void DeleteMail_02() throws HarnessException {
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		String mountpointname = "mountpoint" + ZimbraSeleniumProperties.getUniqueString();
+		
+		FolderItem inbox = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), FolderItem.SystemFolder.Inbox);
+		
+		// Create a folder to share
+		ZimbraAccount.AccountA().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + foldername + "' l='" + inbox.getId() + "'/>"
+				+	"</CreateFolderRequest>");
+		
+		FolderItem folder = FolderItem.importFromSOAP(ZimbraAccount.AccountA(), foldername);
+		
+		// Share it
+		ZimbraAccount.AccountA().soapSend(
+					"<FolderActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action id='"+ folder.getId() +"' op='grant'>"
+				+			"<grant d='"+ app.zGetActiveAccount().EmailAddress +"' gt='usr' perm='r'/>"
+				+		"</action>"
+				+	"</FolderActionRequest>");
+		
+		// Add a message to it
+		ZimbraAccount.AccountA().soapSend(
+					"<AddMsgRequest xmlns='urn:zimbraMail'>"
+        		+		"<m l='"+ folder.getId() +"' >"
+            	+			"<content>From: foo@foo.com\n"
+            	+				"To: foo@foo.com \n"
+            	+				"Subject: "+ subject +"\n"
+            	+				"MIME-Version: 1.0 \n"
+            	+				"Content-Type: text/plain; charset=utf-8 \n"
+            	+				"Content-Transfer-Encoding: 7bit\n"
+            	+				"\n"
+            	+				"simple text string in the body\n"
+            	+			"</content>"
+            	+		"</m>"
+				+	"</AddMsgRequest>");
+		
+		MailItem mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+
+		// Mount it
+		app.zGetActiveAccount().soapSend(
+					"<CreateMountpointRequest xmlns='urn:zimbraMail'>"
+				+		"<link l='1' name='"+ mountpointname +"'  rid='"+ folder.getId() +"' zid='"+ ZimbraAccount.AccountA().ZimbraId +"'/>"
+				+	"</CreateMountpointRequest>");
+		
+		FolderMountpointItem mountpoint = FolderMountpointItem.importFromSOAP(app.zGetActiveAccount(), mountpointname);
+
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+				
+		// Click on the mountpoint
+		app.zTreeMail.zTreeItem(Action.A_LEFTCLICK, mountpoint);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, mail.dSubject);
+		
+		// Delete the item
+		app.zPageMail.zKeyboardKeyEvent(KeyEvent.VK_DELETE);
+		
+		// A "Permission Denied" error popup should NOT occur
+		DialogError dialog = app.zPageMain.zGetErrorDialog(DialogError.DialogErrorID.Zimbra);
+		ZAssert.assertNotNull(dialog, "Verify the PERM DENIED Error Dialog is created");
+		ZAssert.assertFalse(dialog.zIsActive(), "Verify the PERM DENIED Error Dialog is active");
+		
+		
+		// Make sure the server does not show "flagged" for the owner
+		mail = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
+		ZAssert.assertNotNull(mail, "Verify the message still exists in accountA's mailbox");
+
+		
+	}
+
+
+}
