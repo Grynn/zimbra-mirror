99ffca3ed5f815fd1283659e3cf3b878b0ee364b
 Unknown Perforce User <rajar>
2011-09-21 22:54:20 +0000

    bug:62262 Checkpoint #1. ZmMP3VoicemailListView.js - Created a new class ZmMP3VoicemailListView that sub-classes from ZmVoicemailListView to cleanly separate mp3/flash specific code and keep earlier logic intact. ZmFlashPlayer.js - ZmFlashPlayer.js a simple class that embeds Flash player. It only embeds one player at a time. AjxPluginDetector.js - Moved getFlashVersion function to AjxPluginDetector class as per review ZmMsg.properties - Fixed typo as per review ZmVoicemailListController.js - Decides which kind of listview to use depending on audio-format ZmVoiceApp.js - Has two audio-formats info (mp3 and wave)
    Review: https://reviewboard.eng.vmware.com/r/264787/
    
    Todo:
    - Fix all the buttons
    - Remove Call Manager
    - May need to update ZmVoicemailListController.js once we get Server starts sending audio-format info
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=62262

--- ZimbraWebClient/WebRoot/js/ajax/util/AjxPluginDetector.js
+++ ZimbraWebClient/WebRoot/js/ajax/util/AjxPluginDetector.js
@@ -112,6 +112,42 @@ function() {
 	}
 };
 
+/**
+ * This code is part of JQuery's Flash plugin.
+ * http://jquery.lukelutman.com/plugins/flash/
+ *
+ * @return Flash plugin version
+ */
+AjxPluginDetector.getFlashVersion =
+function() {
+    var flashVersion = "0,0,0";
+    // ie
+    try {
+        try {
+            // avoid fp6 minor version lookup issues
+            // see: http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
+            var axo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash.6');
+            try {
+                axo.AllowScriptAccess = 'always';
+            }
+            catch(e) {
+                return '6,0,0';
+            }
+        } catch(e) {
+            }
+        flashVersion = new ActiveXObject('ShockwaveFlash.ShockwaveFlash').GetVariable('$version').replace(/\D+/g, ',').match(/^,?(.+),?$/)[1];
+        // other browsers
+    } catch(e) {
+        try {
+            if (navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin) {
+                flashVersion = (navigator.plugins["Shockwave Flash 2.0"] || navigator.plugins["Shockwave Flash"]).description.replace(/\D+/g, ",").match(/^,?(.+),?$/)[1];
+            }
+        } catch(e) {
+        }
+    }
+	return flashVersion;
+};
+
 AjxPluginDetector.detectReal =
 function() {
 	if(AjxEnv.isIE) {
