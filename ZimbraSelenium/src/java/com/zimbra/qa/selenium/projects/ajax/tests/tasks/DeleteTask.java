38379afbea3fb61af8b9849ebf7c20271f9cd20e
 Unknown Perforce User <girishb>
2011-09-28 10:14:19 +0000

    bug:61625
    --added test case for hard-deleting one as well as multiple tasks
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=61625

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/DeleteTask.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/DeleteTask.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.testng.annotations.*;
 
+import com.zimbra.common.soap.Element;
 import com.zimbra.qa.selenium.framework.core.Bugs;
 import com.zimbra.qa.selenium.framework.items.*;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
@@ -417,6 +418,176 @@ public class DeleteTask extends AjaxCommonTest {
 		ZAssert.assertNull(found, "Verify the task is no longer present");
 	
 	}
+	
+	@Bugs(ids="61625")
+	@Test(	description = "Hard-delete a task by selecting and typing 'shift-del' shortcut",
+			groups = { "functional" })
+	public void HardDeleteTask_07() throws HarnessException {
 
+		
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);		
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+				
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+		
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+						
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+		
+		// Click shift-delete
+		app.zPageTasks.zKeyboardShortcut(Shortcut.S_TASK_HARDELETE);
+		
+		//Verify the task is no longer present in tasks folder
+		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
+		ZAssert.assertNotNull(tasks, "Verify the task list exists");
+
+		TaskItem found = null;
+		for (TaskItem t : tasks) {
+			logger.info("Subject: looking for "+ subject +" found: "+ t.gSubject);
+			if ( subject.equals(t.gSubject) ) {
+				found = t;
+				break;
+			}
+		}
+		ZAssert.assertNull(found, "Verify the task is no longer present");
+	
+		
+		// Verify the task is not in the  trash
+		app.zGetActiveAccount().soapSend(
+					"<SearchRequest xmlns='urn:zimbraMail' types='task'>"
+				+		"<query>is:anywhere "+ subject +"</query>"
+				+	"</SearchRequest>");
+
+		Element[] nodes = app.zGetActiveAccount().soapSelectNodes("//mail:task");
+		ZAssert.assertEquals(nodes.length, 0, "Verify the task is not in the  trash");
+
+	}
+
+	@Bugs(ids="61625")
+	@Test(	description = "Hard-delete multiple tasks(3) by selecting and typing 'shift-del' shortcut",
+			groups = { "functional" })
+	public void HardDeleteTask_08() throws HarnessException {
+		
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);		
+		// Create a basic task to delete
+		String subject1 = "task1"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject2 = "task2"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject3 = "task3"+ ZimbraSeleniumProperties.getUniqueString();
+				
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject1 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject1 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject2 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject2 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject3 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject3 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+
+		
+
+		TaskItem task1 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject1);
+		TaskItem task2 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject2);
+		TaskItem task3 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject3);
+		
+		ZAssert.assertNotNull(task1, "Verify the task1 is created");
+		ZAssert.assertNotNull(task2, "Verify the task2 is created");
+		ZAssert.assertNotNull(task3, "Verify the task3 is created");
+		
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+						
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject1);
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject2);
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject3);
+		
+		// Click shift-delete
+		app.zPageTasks.zKeyboardShortcut(Shortcut.S_TASK_HARDELETE);
+		
+		//Verify the task is no longer present in tasks/trash folder
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='task'>"
+			+		"<query>is:anywhere "+ subject1 +"</query>"
+			+	"</SearchRequest>");
+
+		Element[] nodes = app.zGetActiveAccount().soapSelectNodes("//mail:task");
+		ZAssert.assertEquals(nodes.length, 0, "Verify the task1 is not in the  tasks/trash");
+		
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='task'>"
+			+		"<query>is:anywhere "+ subject2 +"</query>"
+			+	"</SearchRequest>");
+
+		nodes = app.zGetActiveAccount().soapSelectNodes("//mail:task");
+		ZAssert.assertEquals(nodes.length, 0, "Verify the task2 is not in the  tasks/trash");
+		
+		app.zGetActiveAccount().soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='task'>"
+			+		"<query>is:anywhere "+ subject3 +"</query>"
+			+	"</SearchRequest>");
+
+		nodes = app.zGetActiveAccount().soapSelectNodes("//mail:task");
+		ZAssert.assertEquals(nodes.length, 0, "Verify the task3 is not in the  tasks/trash");
+
+	}
 
 }
