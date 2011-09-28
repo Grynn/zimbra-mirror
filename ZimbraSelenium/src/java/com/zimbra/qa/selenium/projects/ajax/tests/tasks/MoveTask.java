b76195f7caf4153a6c27669b75c9b450ed20f913
 Unknown Perforce User <girishb>
2011-09-28 12:29:34 +0000

    bug:none
    -- Added move task related test cases as
    1.Move a task by selecting task, then click 'm' shortcut
    2.Create task through SOAP - move using Right Click Context Menu & verify through GUI
    3.Move a task by using Move -> New folder & verify through GUI

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/MoveTask.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/MoveTask.java
@@ -11,6 +11,7 @@ import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
 import com.zimbra.qa.selenium.projects.ajax.ui.AppAjaxClient;
 import com.zimbra.qa.selenium.projects.ajax.ui.DialogMove;
+import com.zimbra.qa.selenium.projects.ajax.ui.tasks.DialogCreateTaskFolder;
 
 
 public class MoveTask extends AjaxCommonTest {
@@ -103,8 +104,251 @@ public class MoveTask extends AjaxCommonTest {
 			}
 		}
 		ZAssert.assertNotNull(movetask,	"Verify the task is moved to the selected folder");
+		
 	}
+	@Test(description = "Move a task by selecting task, then click 'm' shortcut", groups = { "functional" })
+	public void MoveTask_02() throws HarnessException {
 
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		String name = "TaskFolder" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Create a subfolder to move the message into i.e. tasks/subfolder
+		String taskFolderID = taskFolder.getId();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + name + "' l='" + taskFolderID + "'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+
+		// refresh task page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>"
+				+		"<m >"
+				+			"<inv>"
+				+				"<comp name='" + subject + "'>"
+				+					"<or a='"+ app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"</comp>" 
+				+			"</inv>"
+				+			"<su>" + subject + "</su>"
+				+			"<mp ct='text/plain'>"
+				+				"<content>content" + ZimbraSeleniumProperties.getUniqueString() + "</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(),subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+				
+		// Click the Move keyboard shortcut
+		DialogMove chooseFolder = (DialogMove) app.zPageTasks.zKeyboardShortcut(Shortcut.S_MOVE);
+		chooseFolder.zClickTreeFolder(subFolder);
+		chooseFolder.zClickButton(Button.B_OK);
+
+		// refresh tasks page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
+		TaskItem found = null;
+		for (TaskItem t : tasks) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				found = t;
+				break;
+			}
+		}
+
+		ZAssert.assertNull(found,"Verify the  task no longer  present in the task list");
+
+		// click on subfolder in tree view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, subFolder);
+		List<TaskItem> tasks1 = app.zPageTasks.zGetTasks();
+
+		TaskItem movetask = null;
+		for (TaskItem t : tasks1) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				movetask = t;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(movetask,	"Verify the task is moved to the selected folder");
+	}
+	
+
+	@Test(description = "Create task through SOAP - move using Right Click Context Menu & verify through GUI", groups = { "functional" })
+	public void MoveTask_03() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		String name = "TaskFolder" + ZimbraSeleniumProperties.getUniqueString();
+
+		// Create a subfolder to move the message into i.e. tasks/subfolder
+		String taskFolderID = taskFolder.getId();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + name + "' l='" + taskFolderID + "'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+
+		// refresh task page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>"
+				+		"<m >"
+				+			"<inv>"
+				+				"<comp name='" + subject + "'>"
+				+					"<or a='"+ app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"</comp>" 
+				+			"</inv>"
+				+			"<su>" + subject + "</su>"
+				+			"<mp ct='text/plain'>"
+				+				"<content>content" + ZimbraSeleniumProperties.getUniqueString() + "</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(),subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+				
+		// Click the Move keyboard shortcut
+		DialogMove chooseFolder = (DialogMove) app.zPageTasks.zListItem(Action.A_RIGHTCLICK, Button.O_MOVE_MENU, subject);
+		chooseFolder.zClickTreeFolder(subFolder);
+		chooseFolder.zClickButton(Button.B_OK);
+
+		// refresh tasks page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
+		TaskItem found = null;
+		for (TaskItem t : tasks) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				found = t;
+				break;
+			}
+		}
+
+		ZAssert.assertNull(found,"Verify the  task no longer  present in the task list");
+
+		// click on subfolder in tree view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, subFolder);
+		List<TaskItem> tasks1 = app.zPageTasks.zGetTasks();
+
+		TaskItem movetask = null;
+		for (TaskItem t : tasks1) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				movetask = t;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(movetask,	"Verify the task is moved to the selected folder");
+	}
+	
+	@Test(description = "Move a task by using Move -> New folder & verify through GUI", groups = { "functional" })
+	public void MoveTask_04() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+		String name = "TaskFolder" + ZimbraSeleniumProperties.getUniqueString();	
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>"
+				+		"<m >"
+				+			"<inv>"
+				+				"<comp name='" + subject + "'>"
+				+					"<or a='"+ app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"</comp>" 
+				+			"</inv>"
+				+			"<su>" + subject + "</su>"
+				+			"<mp ct='text/plain'>"
+				+				"<content>content" + ZimbraSeleniumProperties.getUniqueString() + "</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(),subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+				
+		// Click the Move dropdown and enter new task list name		
+		DialogCreateTaskFolder createTaskFolderDialog =(DialogCreateTaskFolder)app.zPageTasks.zToolbarPressPulldown(Button.B_MOVE, Button.O_NEW_TASKFOLDER);
+		
+		createTaskFolderDialog.zEnterFolderName(name);
+		createTaskFolderDialog.zClickButton(Button.B_OK);
+
+		// refresh tasks page
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		//Verify the  task no longer  present in the task list
+		List<TaskItem> tasks = app.zPageTasks.zGetTasks();
+		TaskItem found = null;
+		for (TaskItem t : tasks) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				found = t;
+				break;
+			}
+		}
+
+		ZAssert.assertNull(found,"Verify the  task no longer  present in the task list");
+		
+		FolderItem newTasklist = FolderItem.importFromSOAP(app.zGetActiveAccount(), name);
+		
+		// click on subfolder in tree view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, newTasklist);
+		List<TaskItem> tasks1 = app.zPageTasks.zGetTasks();
+
+		TaskItem movetask = null;
+		for (TaskItem t : tasks1) {
+			logger.info("Task: looking for " + subject + " found: "
+					+ t.gSubject);
+			if (subject.equals(t.gSubject)) {
+				// Found it!
+				movetask = t;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(movetask,	"Verify the task is moved to the newTasklist");
+	}
+	
+	
 	@AfterMethod(groups = { "always" })
 	public void afterMethod() throws HarnessException {
 		logger.info("Checking for the Move Dialog ...");
