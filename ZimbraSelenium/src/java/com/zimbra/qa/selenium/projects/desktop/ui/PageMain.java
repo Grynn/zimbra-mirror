322467908aa10d0912de6f84e9888bb493b117b7
 Unknown Perforce User <jhidayat>
2011-09-30 18:21:03 +0000

    ZDesktop Selenium: Manually integrate cn#332576 for PageMain.java due to forgotten integration, which causes compilation error. Bug: None

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/ui/PageMain.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/ui/PageMain.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.zimbra.qa.selenium.projects.desktop.ui;
 
@@ -15,32 +15,32 @@ import com.zimbra.qa.selenium.projects.desktop.ui.DialogError.DialogErrorID;
 public class PageMain extends AbsTab {
 
 	public static class Locators {
-				
+
 		public static final String zLogoffButton		= "css=td[id=skin_container_logoff] a";
-		
+
 		public static final String zAppbarMail			= "id=zb__App__Mail_title";
 		public static final String zAppbarContact		= "id=zb__App__Contacts_title";
 		public static final String zAppbarCal			= "id=zb__App__Calendar_title";
 		public static final String zAppbarTasks			= "id=zb__App__Tasks_title";
 		public static final String zAppbarBriefcase		= "css=td[id='zb__App__Briefcase_left_icon']";
 		public static final String zAppbarPreferences	= "id=zb__App__Options_title";
-
+		public static final String rssFolders = "css=div[id$='main_Mail-parent-FOLDER'] div[class='DwtTreeItemLevel1ChildDiv'] div td:contains('<FOLDER_NAME>')";
 		// For Social tab, see Zimlet classes
-		
+
 	}
-	
-	
+
+
 	public PageMain(AbsApplication application) {
 		super(application);
-		
+
 		logger.info("new " + PageMain.class.getCanonicalName());
 
 	}
-	
+
 	public Toaster zGetToaster() throws HarnessException {
 		return (new Toaster(this.MyApplication));
 	}
-	
+
 	public DialogError zGetErrorDialog(DialogErrorID zimbra) {
 		return (new DialogError(zimbra, this.MyApplication, this));
 	}
@@ -50,30 +50,30 @@ public class PageMain extends AbsTab {
 	public boolean zIsZimletLoaded() throws HarnessException {
 		return ("true".equals(sGetEval("this.browserbot.getUserWindow().top.appCtxt.getZimletMgr().loaded")));
 	}
-	
+
 	public boolean zIsMinicalLoaded() throws HarnessException {
 		return ("true".equals(sGetEval("this.browserbot.getUserWindow().top.appCtxt.getAppViewMgr().getCurrentViewComponent(this.browserbot.getUserWindow().top.ZmAppViewMgr.C_TREE_FOOTER) != null")));
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see projects.admin.ui.AbsPage#isActive()
 	 */
 	@Override
 	public boolean zIsActive() throws HarnessException {
 
-		// Look for the Logout button 
+		// Look for the Logout button
 		// check if zimlet + minical loaded
-		boolean present = sIsElementPresent(Locators.zLogoffButton) 
+		boolean present = sIsElementPresent(Locators.zLogoffButton)
 		               && zIsZimletLoaded()
 		           //    && zIsMinicalLoaded()
 		               ;
-			
-		
+
+
 		if ( !present ) {
 			logger.debug("isActive() present = "+ present);
 			return (false);
 		}
-		
+
 		logger.debug("isActive() = "+ true);
 		return (true);
 
@@ -98,7 +98,7 @@ public class PageMain extends AbsTab {
 			// This page is already active
 			return;
 		}
-			
+
 		// 1. Logout
 		// 2. Login as the default account
 		if ( !((AppAjaxClient)MyApplication).zPageLogin.zIsActive() ) {
@@ -106,7 +106,7 @@ public class PageMain extends AbsTab {
 		}
 		((AppAjaxClient)MyApplication).zPageLogin.zLogin(ZimbraAccount.AccountZWC());
 		zWaitForActive();
-		
+
 	}
 
 	/**
@@ -140,7 +140,7 @@ public class PageMain extends AbsTab {
 		// Q. Should the tabs or help or logout be processed here?
 		// A. I don't think those are considered "toolbars", so don't handle here for now (Matt)
 		throw new HarnessException("Main page does not have a Toolbar");
-		
+
 	}
 
 	@Override
@@ -162,27 +162,27 @@ public class PageMain extends AbsTab {
 	public AbsPage zListItem(Action action, Button option, Button subOption ,String item)
 			throws HarnessException {
 		throw new HarnessException("Main page does not have lists");
-	}	
+	}
 	/**
 	 * Close any extra compose tabs
 	 */
 	public void zCloseComposeTabs() throws HarnessException {
-		
+
 		String locator = "//td[contains(@id,'ztb_appChooser_item_')]//div[contains(@id,'zb__App__tab_COMPOSE')]";
 		if ( sIsElementPresent(locator) ) {
 			logger.debug("Found compose tabs");
-			
+
 			String xpath = "//td[contains(@id,'ztb_appChooser_item_')]//div[contains(@id,'zb__App__tab_COMPOSE')]";
 			int count = this.sGetXpathCount(xpath);
 			for (int i = 1; i <= count; i++) {
 				locator = xpath + "//td[contains(@id,'_left_icon')]["+ i +"]";
-				if ( !sIsElementPresent(locator) ) 
+				if ( !sIsElementPresent(locator) )
 					throw new HarnessException("Unable to find compose tab close icon "+ locator);
 				this.zClick(locator);
 			}
 		}
 	}
 
-	
+
 
 }
