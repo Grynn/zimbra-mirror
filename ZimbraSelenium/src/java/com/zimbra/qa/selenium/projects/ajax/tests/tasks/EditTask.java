9ea399e2f4bc3c15c94ca63b51b3fe7eb2f23dd6
 Unknown Perforce User <girishb>
2011-09-30 06:18:10 +0000

    bug:64647
    --Added test case for "List view of tasks shows NaN/NaN/0Na for due date"
    
    com.zimbra.qa.selenium.projects.ajax.tests.tasks.EditTask.EditTask_02
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=64647

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/EditTask.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/EditTask.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 
+import com.zimbra.qa.selenium.framework.core.Bugs;
 import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.TaskItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
@@ -13,8 +14,10 @@ import com.zimbra.qa.selenium.framework.ui.Button;
 import com.zimbra.qa.selenium.framework.util.GeneralUtility;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
 import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
 import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.tasks.DisplayTask;
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew;
 import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew.Field;
 
@@ -101,6 +104,70 @@ public class EditTask extends AjaxCommonTest{
 
 		ZAssert.assertNull(foundoldtask, "Verify the old task no longer  present in the task list");
 
+	}
+	
+	/**
+	 * 	1. Go to Tasks
+	 * 	2. Create a new task with no due date
+	 * 	3. Refresh list view to see new task 
+	 * 	4. Edit task and add due date
+	 * 	5. Refresh list view again
+	 * 	   Expected result:Task should show due date
+	 * @throws HarnessException
+	 */
+	@Bugs(ids="64647")
+	@Test(	description = "Create task through SOAP - edit duedate >> Refresh task >>verify Due Date in list view through GUI",groups = { "functional" })
+	public void EditTask_02() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+		FolderItem trashFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Trash);
+
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+		ZDate dueDate      = new ZDate(2015, 11, 17, 12, 0, 0);
+		
+		//Create task
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_LEFTCLICK, subject);
+		// Click edit
+		FormTaskNew taskedit = (FormTaskNew) app.zPageTasks.zToolbarPressButton(Button.B_EDIT);
+
+		//Fill due date field
+		taskedit.zFillField(Field.DueDate, dueDate.toMM_DD_YYYY());
+		taskedit.zSubmit();
+
+		DisplayTask actual = (DisplayTask) app.zPageTasks.zListItem(Action.A_LEFTCLICK, subject);
+		
+		//Verify Due Date before refresh
+		ZAssert.assertEquals(actual.zGetTaskListViewProperty(com.zimbra.qa.selenium.projects.ajax.ui.tasks.DisplayTask.Field.DueDate), dueDate.toMM_DD_YYYY(), "Verify the due date matches");
+		
+		// click on Trash folder
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, trashFolder);		
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+		
+		//Verify the due date matches after refresh
+		ZAssert.assertEquals(actual.zGetTaskListViewProperty(com.zimbra.qa.selenium.projects.ajax.ui.tasks.DisplayTask.Field.DueDate), dueDate.toMM_DD_YYYY(), "Verify the due date matches after refresh");
+			
 	}
 
 
