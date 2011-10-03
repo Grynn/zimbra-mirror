68d659ee4cf8aa2f44b76e546e6df74d548f8188
David Wang <dywang@zimbra.com>
2011-10-03 16:51:14 +0000

    Integrate from HELIX to main
    	Change 332873 by dywang@dywang-mac-hlx on 2011/10/03 09:47:38
    
           bug: 64860
           Store some zimlet's properties under local account.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=64860

---
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineModifyProperties.java
@@ -0,0 +1,49 @@
+package com.zimbra.cs.service.offline;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.AccountConstants;
+import com.zimbra.common.soap.Element;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.offline.OfflineProvisioning;
+import com.zimbra.cs.service.account.ModifyProperties;
+import com.zimbra.cs.zimlet.ZimletUserProperties;
+import com.zimbra.soap.ZimbraSoapContext;
+
+public class OfflineModifyProperties extends ModifyProperties {
+
+    private static final Set<String> PROP_UNDER_LOCAL_ACCT_ZIMLETS = new HashSet<String>();
+    static {
+        PROP_UNDER_LOCAL_ACCT_ZIMLETS.add("com_zimbra_apptsummary");
+        PROP_UNDER_LOCAL_ACCT_ZIMLETS.add("com_zimbra_social");
+        PROP_UNDER_LOCAL_ACCT_ZIMLETS.add("com_zdesktop_survey");
+    }
+
+    public Element handle(Element request, Map<String, Object> context) throws ServiceException {
+        ZimbraSoapContext zsc = getZimbraSoapContext(context);
+        Account account = getRequestedAccount(zsc);
+
+        if (!canModifyOptions(zsc, account))
+            throw ServiceException.PERM_DENIED("can not modify options");
+
+        ZimletUserProperties props = ZimletUserProperties.getProperties(account);
+        Account localAccount = OfflineProvisioning.getOfflineInstance().getLocalAccount();
+        ZimletUserProperties localAcctProps = ZimletUserProperties.getProperties(localAccount);
+
+        for (Element e : request.listElements(AccountConstants.E_PROPERTY)) {
+            String zimlet = e.getAttribute(AccountConstants.A_ZIMLET);
+            if (PROP_UNDER_LOCAL_ACCT_ZIMLETS.contains(zimlet)) {
+                localAcctProps.setProperty(zimlet, e.getAttribute(AccountConstants.A_NAME), e.getText());
+            } else {
+                props.setProperty(zimlet, e.getAttribute(AccountConstants.A_NAME), e.getText());
+            }
+        }
+        props.saveProperties(account);
+        localAcctProps.saveProperties(localAccount);
+        Element response = zsc.createElement(AccountConstants.MODIFY_PROPERTIES_RESPONSE);
+        return response;
+    }
+}
--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineService.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineService.java
@@ -17,6 +17,7 @@ package com.zimbra.cs.service.offline;
 import com.zimbra.common.soap.AccountConstants;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.offline.common.OfflineConstants;
+import com.zimbra.cs.service.account.ModifyProperties;
 import com.zimbra.soap.DocumentDispatcher;
 import com.zimbra.soap.DocumentService;
 import com.zimbra.soap.SoapContextExtension;
@@ -73,6 +74,7 @@ public class OfflineService implements DocumentService {
         dispatcher.registerHandler(OfflineConstants.ACCOUNT_BACKUP_ENUM_REQUEST, new OfflineBackupEnumService());
         dispatcher.registerHandler(AccountConstants.GET_DISTRIBUTION_LIST_MEMBERS_REQUEST, new OfflineGetDistributionListMembers());
         dispatcher.registerHandler(MailConstants.MODIFY_CONTACT_REQUEST, new OfflineModifyContact());
+        dispatcher.registerHandler(AccountConstants.MODIFY_PROPERTIES_REQUEST, new OfflineModifyProperties());
 
         // not the most suitable place to do this, but it's just too easy.
         SoapContextExtension.register(OfflineContextExtension.ZDSYNC, new OfflineContextExtension());
