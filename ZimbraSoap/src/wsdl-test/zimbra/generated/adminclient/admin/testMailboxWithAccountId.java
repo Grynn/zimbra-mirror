73570c52b50f819381fa30e1a04ec66e1b08a8c3
 Unknown Perforce User <pburgu>
2011-10-04 23:48:20 +0000

    bug:57427 Checking-in the missing test file.
    http://bugzilla.zimbra.com/show_bug.cgi?id=57427

---
+++ ZimbraSoap/src/wsdl-test/zimbra/generated/adminclient/admin/testMailboxWithAccountId.java
@@ -0,0 +1,89 @@
+
+package zimbra.generated.adminclient.admin;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for mailboxWithAccountId complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="mailboxWithAccountId">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;all>
+ *       &lt;/all>
+ *       &lt;attribute name="id" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *       &lt;attribute name="s" type="{http://www.w3.org/2001/XMLSchema}long" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "mailboxWithAccountId", propOrder = {
+
+})
+public class testMailboxWithAccountId {
+
+    @XmlAttribute(name = "id", required = true)
+    protected String id;
+    @XmlAttribute(name = "s")
+    protected Long s;
+
+    /**
+     * Gets the value of the id property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the value of the id property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setId(String value) {
+        this.id = value;
+    }
+
+    /**
+     * Gets the value of the s property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
+     */
+    public Long getS() {
+        return s;
+    }
+
+    /**
+     * Sets the value of the s property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setS(Long value) {
+        this.s = value;
+    }
+
+}
