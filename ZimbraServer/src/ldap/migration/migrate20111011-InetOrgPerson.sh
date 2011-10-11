146701cb3fcca96f7cc30b507f9049b48fe5b3fb
Quanah Gibson-Mount <quanah@zimbra.com>
2011-10-11 17:07:54 +0000

    bug: 60444 Change backup suffix
    http://bugzilla.zimbra.com/show_bug.cgi?id=60444

--- ZimbraServer/src/ldap/migration/migrate20111011-InetOrgPerson.sh
+++ ZimbraServer/src/ldap/migration/migrate20111011-InetOrgPerson.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i .bak -e '/^objectClass: organizationalPerson/ a objectClass: inetOrgPerson' -e 's/^structuralObjectClass: organizationalPerson/structuralObjectClass: inetOrgPerson/' /opt/zimbra/data/ldap/ldap.bak
+sed -i .orig -e '/^objectClass: organizationalPerson/ a objectClass: inetOrgPerson' -e 's/^structuralObjectClass: organizationalPerson/structuralObjectClass: inetOrgPerson/' /opt/zimbra/data/ldap/ldap.bak
