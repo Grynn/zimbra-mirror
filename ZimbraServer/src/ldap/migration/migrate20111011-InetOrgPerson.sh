7659c9f9daef80b8b731a4733a35d025ff73fed3
Quanah Gibson-Mount <quanah@zimbra.com>
2011-10-11 22:41:32 +0000

    bug: 60444 Move migration bit into the upgrade perl module
    http://bugzilla.zimbra.com/show_bug.cgi?id=60444

--- ZimbraServer/src/ldap/migration/migrate20111011-InetOrgPerson.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i.orig -e '/^objectClass: organizationalPerson/ a objectClass: inetOrgPerson' -e 's/^structuralObjectClass: organizationalPerson/structuralObjectClass: inetOrgPerson/' /opt/zimbra/data/ldap/ldap.bak
