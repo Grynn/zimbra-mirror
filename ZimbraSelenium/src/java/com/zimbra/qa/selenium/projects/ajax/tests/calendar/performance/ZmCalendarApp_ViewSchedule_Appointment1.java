b7c5ff5958565bd396947ca7e681fabfaaa1e4a3
 Unknown Perforce User <rhoades>
2011-11-01 18:44:58 +0000

    Selenium Harness: add performance tests for the different calendar views - 1 appointment
    Bug: 64273
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=64273

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/performance/ZmCalendarApp_ViewSchedule_Appointment1.java
@@ -0,0 +1,64 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.performance;
+
+import java.util.*;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.framework.util.performance.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+public class ZmCalendarApp_ViewSchedule_Appointment1 extends AjaxCommonTest {
+
+	
+	public ZmCalendarApp_ViewSchedule_Appointment1() throws HarnessException {
+		logger.info("New "+ ZmCalendarApp_ViewSchedule_Appointment1.class.getCanonicalName());
+		
+		super.startingPage = app.zPageMail;
+		
+		super.startingAccountPreferences = new HashMap<String, String>() {
+			private static final long serialVersionUID = 3038458962443347843L;
+			{
+				put("zimbraPrefCalendarInitialView", "schedule");
+			}};
+
+
+	}
+	
+	
+	@Test(	description = "Measure the time to load the Calendar, schedule view, 1 appointment",
+			groups = { "performance" })
+	public void ZmCalendarApp_01() throws HarnessException {
+		
+		// Create an appointment
+		String subject = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+		AppointmentItem.createAppointmentSingleDay(
+				app.zGetActiveAccount(),
+				Calendar.getInstance(),
+				60,
+				null,
+				subject,
+				"content" + ZimbraSeleniumProperties.getUniqueString(),
+				"location" + ZimbraSeleniumProperties.getUniqueString(),
+				null);
+
+
+		// Start the perf token
+		PerfToken token = PerfMetrics.startTimestamp(PerfKey.ZmCalendarApp, "Load the calendar app, schedule view, 1 appointment");
+
+		// Go to calendar
+		app.zPageCalendar.zNavigateTo();
+
+		PerfMetrics.waitTimestamp(token);
+				
+		// Wait for the app to load
+		app.zPageCalendar.zWaitForActive();
+		
+		app.zPageMain.zLogout();
+
+	}
+
+
+}
