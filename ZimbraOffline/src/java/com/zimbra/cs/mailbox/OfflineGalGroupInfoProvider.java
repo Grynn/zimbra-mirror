a46e3e4dda040a051c2c3430ffcec735c9839d80
David Wang <dywang@zimbra.com>
2011-11-03 17:52:39 +0000

    Integrate from ZDESKTOP-713 to main
    	Change 337489 by dywang@dywang-mac-zd713 on 2011/11/03 10:48:57
    
           bug: 66682
           Short circuit OfflineGalGroupInfoProvider::encodeAddrsWithGroupInfo
           for non-Zcs account.
           Also changed getGroupInfo().
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=66682

--- ZimbraOffline/src/java/com/zimbra/cs/mailbox/OfflineGalGroupInfoProvider.java
+++ ZimbraOffline/src/java/com/zimbra/cs/mailbox/OfflineGalGroupInfoProvider.java
@@ -23,51 +23,57 @@ import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.gal.GalGroupInfoProvider;
+import com.zimbra.cs.account.offline.OfflineAccount;
 import com.zimbra.cs.gal.GalGroup.GroupInfo;
+import com.zimbra.cs.gal.GalGroupInfoProvider;
 import com.zimbra.cs.offline.OfflineLog;
 
 /**
- * Provide GroupInfo from entries in OfflineGal 
- *
+ * Provide GroupInfo from entries in OfflineGal
+ * 
  */
 public class OfflineGalGroupInfoProvider extends GalGroupInfoProvider {
 
     @Override
-    public GroupInfo getGroupInfo(String addr, boolean needCanExpand,
-            Account requestedAcct, Account authedAcct) {
-        try {
-            Contact con = GalSyncUtil.getGalDlistContact(requestedAcct, addr);
-            if (con != null && con.isGroup()) {
-                return needCanExpand ? GroupInfo.CAN_EXPAND : GroupInfo.IS_GROUP;
+    public GroupInfo getGroupInfo(String addr, boolean needCanExpand, Account requestedAcct, Account authedAcct) {
+        OfflineAccount reqAccount = (OfflineAccount) requestedAcct;
+        if (reqAccount.isZcsAccount() && reqAccount.isFeatureGalEnabled() && reqAccount.isFeatureGalSyncEnabled()) {
+            try {
+                Contact con = GalSyncUtil.getGalDlistContact(reqAccount, addr);
+                if (con != null && con.isGroup()) {
+                    return needCanExpand ? GroupInfo.CAN_EXPAND : GroupInfo.IS_GROUP;
+                }
+            } catch (ServiceException e) {
+                OfflineLog.offline.error("Unable to find group %s addr due to exception", e, addr);
             }
-        } catch (ServiceException e) {
-            OfflineLog.offline.error("Unable to find group %s addr due to exception",e,addr);
         }
         return null;
     }
 
     @Override
-    public void encodeAddrsWithGroupInfo(Provisioning prov, Element eParent,
-            String emailElem, Account requestedAcct, Account authedAcct) {
-        Map<String, Element> emailElems = new HashMap<String,Element>();
-        for (Element eEmail : eParent.listElements(emailElem)) { 
-            String addr = eEmail.getAttribute(MailConstants.A_ADDRESS, null);
-            if (addr != null) {
-                // shortcut the check if the email address is the authed or requested account - it cannot be a group
-                if (addr.equalsIgnoreCase(requestedAcct.getName()) || addr.equalsIgnoreCase(authedAcct.getName()))
-                    continue;
-                emailElems.put(addr, eEmail);
+    public void encodeAddrsWithGroupInfo(Provisioning prov, Element eParent, String emailElem, Account requestedAcct,
+            Account authedAcct) {
+        OfflineAccount reqAccount = (OfflineAccount) requestedAcct;
+        if (reqAccount.isZcsAccount() && reqAccount.isFeatureGalEnabled() && reqAccount.isFeatureGalSyncEnabled()) {
+            Map<String, Element> emailElems = new HashMap<String, Element>();
+            for (Element eEmail : eParent.listElements(emailElem)) {
+                String addr = eEmail.getAttribute(MailConstants.A_ADDRESS, null);
+                if (addr != null) {
+                    // shortcut the check if the email address is the authed or requested account - it cannot be a group
+                    if (addr.equalsIgnoreCase(requestedAcct.getName()) || addr.equalsIgnoreCase(authedAcct.getName()))
+                        continue;
+                    emailElems.put(addr, eEmail);
+                }
+
+                if (emailElems.size() >= 100) { // 100 at a time seems reasonable; more will overflow SQLite param limit
+                    encodeGroups(emailElems, reqAccount);
+                    emailElems.clear();
+                }
             }
-            
-            if (emailElems.size() >= 100) { //100 at a time seems reasonable; more will overflow SQLite param limit
-                encodeGroups(emailElems, requestedAcct);
-                emailElems.clear();
-            }
+            encodeGroups(emailElems, reqAccount); // last chunk
         }
-        encodeGroups(emailElems, requestedAcct); //last chunk
     }
-    
+
     private void encodeGroups(Map<String, Element> emailElems, Account requestedAcct) {
         try {
             if (emailElems == null || emailElems.size() <= 0) {
@@ -82,8 +88,7 @@ public class OfflineGalGroupInfoProvider extends GalGroupInfoProvider {
                 }
             }
         } catch (ServiceException e) {
-            OfflineLog.offline.error("Unable to find groups due to exception",e);
+            OfflineLog.offline.error("Unable to find groups due to exception", e);
         }
     }
-
 }
