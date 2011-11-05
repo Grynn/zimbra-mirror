aaba5d414ef350b616e7eee31520566e2564fcc7
David Wang <dywang@zimbra.com>
2011-11-05 00:41:23 +0000

    Integrate from HELIX to main
    	Change 337741 by dywang@dywang-mac-hlx on 2011/11/04 17:40:14
    
           bug: 66671
           Silently catch PROXY_ERROR in waitsetRequest().
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=66671

--- ZimbraOffline/src/java/com/zimbra/cs/mailbox/OfflinePoller.java
+++ ZimbraOffline/src/java/com/zimbra/cs/mailbox/OfflinePoller.java
@@ -22,68 +22,70 @@ import com.zimbra.common.soap.MailConstants;
 import com.zimbra.common.util.Constants;
 import com.zimbra.cs.offline.OfflineLog;
 import com.zimbra.cs.offline.OfflineSyncManager;
+import com.zimbra.cs.service.admin.AdminServiceException;
 
-public class OfflinePoller implements Runnable {
-	
-	private ZcsMailbox ombx;
-	
-	private String setId;
-	private String lastSequence;
-	private String lastKnownToken;
+public final class OfflinePoller implements Runnable {
 
-	private boolean isPolling;
-	private boolean hasChanges;
-	
-	OfflinePoller(ZcsMailbox ombx) {
-		this.ombx = ombx;
-		Thread t = new Thread(this, "offline-poller-" + ombx.getAccountName());
-		t.setDaemon(true); //no obvious shutdown hook point
-		t.start();
-	}
-	
-	public synchronized boolean hasChanges(String lastKnownToken) {
-		if (hasChanges) {
-			hasChanges = false;
-			return true;
-		}
-		if (!isPolling) {
-			this.lastKnownToken = lastKnownToken;
-			isPolling = true;
-			notify();
-		}
-		return false;
-	}
-	
-	public void run() {
-		while(true) {
-			synchronized (this) {
-				isPolling = false;
-				while(!isPolling)
-					try {
-						wait();
-					} catch (InterruptedException x) {}
-			}
-			runPoll();
-		}
-	}
-	
-	private void runPoll() {
-		try {
-			if (setId == null)
-				createWaitset();
-			waitsetRequest();
-		} catch (Exception x) {
-			try {
-			    if (!SyncExceptionHandler.isCausedBy(x, SocketTimeoutException.class)) { //workaround for bug #55186, see bug #56686
-			        OfflineSyncManager.getInstance().processSyncException(ombx.getAccount(), x);
-			    }
-			} catch (Exception e) {
-				OfflineLog.offline.error("unexpected exception", e);
-			}
-		}
-	}
+    private ZcsMailbox ombx;
 
-	private void createWaitset() throws ServiceException {
+    private String setId;
+    private String lastSequence;
+    private String lastKnownToken;
+
+    private boolean isPolling;
+    private boolean hasChanges;
+
+    OfflinePoller(ZcsMailbox ombx) {
+        this.ombx = ombx;
+        Thread t = new Thread(this, "offline-poller-" + ombx.getAccountName());
+        t.setDaemon(true); // no obvious shutdown hook point
+        t.start();
+    }
+
+    public synchronized boolean hasChanges(String lastKnownToken) {
+        if (hasChanges) {
+            hasChanges = false;
+            return true;
+        }
+        if (!isPolling) {
+            this.lastKnownToken = lastKnownToken;
+            isPolling = true;
+            notify();
+        }
+        return false;
+    }
+
+    public void run() {
+        while (true) {
+            synchronized (this) {
+                isPolling = false;
+                while (!isPolling)
+                    try {
+                        wait();
+                    } catch (InterruptedException x) {
+                    }
+            }
+            runPoll();
+        }
+    }
+
+    private void runPoll() {
+        try {
+            if (setId == null)
+                createWaitset();
+            waitsetRequest();
+        } catch (Exception x) {
+            try {
+                if (!SyncExceptionHandler.isCausedBy(x, SocketTimeoutException.class)) { // workaround for bug #55186, see bug #56686
+                    OfflineSyncManager.getInstance().processSyncException(ombx.getAccount(), x);
+                }
+            } catch (Exception e) {
+                OfflineLog.offline.error("unexpected exception", e);
+            }
+        }
+    }
+
+    private void createWaitset() throws ServiceException {
         Element request = new Element.XMLElement(MailConstants.CREATE_WAIT_SET_REQUEST);
         request.addAttribute(MailConstants.A_DEFTYPES, "all");
         Element account = request.addElement(MailConstants.E_WAITSET_ADD).addElement(MailConstants.E_A);
@@ -96,23 +98,23 @@ public class OfflinePoller implements Runnable {
 
         synchronized (this) {
             if (hasError(response)) {
-                hasChanges = true; //when there's error force a sync
+                hasChanges = true; // when there's error force a sync
                 return;
             }
         }
 
         setId = response.getAttribute(MailConstants.A_WAITSET_ID);
         lastSequence = response.getAttribute(MailConstants.A_SEQ);
-	}
+    }
 
-	private void waitsetRequest() throws ServiceException {
+    private void waitsetRequest() throws ServiceException {
         Element request = new Element.XMLElement(MailConstants.WAIT_SET_REQUEST);
         request.addAttribute(MailConstants.A_WAITSET_ID, setId);
         request.addAttribute(MailConstants.A_DEFTYPES, "all");
         request.addAttribute(MailConstants.A_SEQ, lastSequence);
         request.addAttribute(MailConstants.A_BLOCK, "1");
         request.addAttribute(MailConstants.A_TIMEOUT, "300");
-        
+
         Element account = request.addElement(MailConstants.E_WAITSET_UPDATE).addElement(MailConstants.E_A);
         account.addAttribute(MailConstants.A_ID, ombx.getAccountId());
         synchronized (this) {
@@ -121,9 +123,12 @@ public class OfflinePoller implements Runnable {
 
         Element response = null;
         try {
-            response = ombx.sendRequest(request, true, true, 1 * Constants.SECONDS_PER_MINUTE * 1000); //will block
+            response = ombx.sendRequest(request, true, true, 1 * Constants.SECONDS_PER_MINUTE * 1000); // will block
         } catch (ServiceException x) {
-            if (x.getCode().equals("admin.NO_SUCH_WAITSET") || x.getCode().equals(ServiceException.PERM_DENIED)) {
+            if (AdminServiceException.NO_SUCH_WAITSET.equals(x.getCode())
+                    || ServiceException.PERM_DENIED.equals(x.getCode())
+                    || ServiceException.PROXY_ERROR.equals(x.getCode())) {
+                OfflineLog.offline.debug("failed sending request in waitsetRequest(), error code: %s", x.getCode(), x);
                 setId = null;
                 lastSequence = null;
                 return;
@@ -135,22 +140,23 @@ public class OfflinePoller implements Runnable {
 
         synchronized (this) {
             if (hasError(response)) {
-                hasChanges = true; //when there's error force a sync
+                hasChanges = true; // when there's error force a sync
                 return;
             }
 
             account = response.getOptionalElement(MailConstants.E_A);
             if (account != null && account.getAttribute(MailConstants.A_ID).equals(ombx.getAccountId()))
                 hasChanges = true;
-		}
-	}
-	
-	private boolean hasError(Element response) throws ServiceException {
+        }
+    }
+
+    private boolean hasError(Element response) throws ServiceException {
         Element error = response.getOptionalElement(MailConstants.E_ERROR);
         if (error != null) {
-            OfflineLog.offline.warn("waitset error account=%s type=%s", error.getAttribute(MailConstants.A_ID), error.getAttribute(MailConstants.A_TYPE));
+            OfflineLog.offline.warn("waitset error account=%s type=%s", error.getAttribute(MailConstants.A_ID),
+                    error.getAttribute(MailConstants.A_TYPE));
             return true;
         }
-		return false;
-	}
+        return false;
+    }
 }
