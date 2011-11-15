875e3b84b905673bd82c520e8574aa936b5c85f0
 Unknown Perforce User <rhoades>
2011-11-15 22:33:01 +0000

    Selenium Harness: update phone zimlet tests. Include "+" in the phone number (bug 52337).  Phone number does not include starting parenthesis "(" bug (67424).
    Bug: 52337, 67424
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=52337
    http://bugzilla.zimbra.com/show_bug.cgi?id=67424

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/phone/GetMessage.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/phone/GetMessage.java
@@ -148,13 +148,13 @@ public class GetMessage extends AjaxCommonTest {
 			private static final long serialVersionUID = -3501450894693465698L;
 		{
 			put("//a[@href='callto:1-877-486-9273']",				"1-877-486-9273");
-			put("//a[@href='callto:877-486-9273']",					" 877-486-9273");
-			put("//a[@href='callto:%28877%29%20456-7890']",			" (877) 456-7890");
+			put("//a[@href='callto:877-486-9273']",					"877-486-9273");
+			put("//a[@href='callto:%28877%29%20456-7890']",			" (877) 456-7890");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
 			put("//a[@href='callto:6503501010']",					"6503501010");
-			put("//a[@href='callto:1%20650%20350%201010']",	"1 650 350 1010");
-			put("//a[@href='callto:650.350.1010']",					" 650.350.1010");
-			put("//a[@href='callto:%28650%29%20350%201010']",		" (650) 350 1010");
-			put("//a[@href='callto:1%20%28650%29%20350%201010']",	"1 (650) 350 1010");
+			put("//a[@href='callto:+1%20650%20350%201010']",		"+1 650 350 1010");		// http://bugzilla.zimbra.com/show_bug.cgi?id=52337#c15
+			put("//a[@href='callto:650.350.1010']",					"650.350.1010");
+			put("//a[@href='callto:%28650%29%20350%201010']",		" (650) 350 1010");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
+			put("//a[@href='callto:+1%20%28650%29%20350%201010']",	"+1 (650) 350 1010");	// http://bugzilla.zimbra.com/show_bug.cgi?id=52337#c15
 			put("//a[@href='callto:1-650-350-1010']",				"1-650-350-1010");
 		}};
 
