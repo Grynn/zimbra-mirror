11ef246247c76af78fef808b3e6fbf997025743b
 Unknown Perforce User <rhoades>
2011-11-16 20:09:31 +0000

    Integrate Change 339378 by rhoades@MATTXPDEV on 2011/11/16 12:05:22
    Selenium Harness: add test case to verify the 'example' portal
    
    Bug: 67462
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=67462

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/login/portal/ExamplePortal.java
@@ -0,0 +1,63 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.login.portal;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+
+public class ExamplePortal extends AjaxCommonTest {
+	
+	public ExamplePortal() {
+		logger.info("New "+ ExamplePortal.class.getCanonicalName());
+		
+		// All tests start at the login page
+		super.startingPage = app.zPageLogin;
+		super.startingAccountPreferences = new HashMap<String , String>() {
+			private static final long serialVersionUID = -3123410183252792255L;
+		{
+		    put("zimbraFeaturePortalEnabled", "TRUE");
+		    put("zimbraPortalName", "example");
+		}};
+		
+	}
+	
+	@Test(	description = "Login to the Ajax Client with the 'example' portal enabled",
+			groups = { "functional" })
+	public void BasicLogin01() throws HarnessException {
+		
+		// Login
+		app.zPageLogin.zLogin(ZimbraAccount.AccountZWC());
+		
+		// Verify main page becomes active
+		ZAssert.assertTrue(app.zPageMain.zIsActive(), "Verify that the account is logged in");
+		
+		
+		ZAssert.assertTrue(app.zPageMain.sIsElementPresent("css=td#zb__App__Portal_title"), "Verify the 'Home' tab is present");
+		ZAssert.assertTrue(app.zPageMain.zIsVisiblePerPosition("css=td#zb__App__Portal_title", 0, 0), "Verify the 'Home' tab is present");
+		
+	}
+
+
+}
