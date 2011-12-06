06c578d3e049fc15fc2a4e4fc275aabb0aa2f5cf
 Unknown Perforce User <dieunguyen>
2011-12-06 20:07:05 +0000

    bug:none - Fixed failed test case due to locator id change

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/TagContact.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/TagContact.java
@@ -153,7 +153,7 @@ public class TagContact extends AjaxCommonTest  {
 		
 	    // Dnd on the new tag
 		app.zPageAddressbook.zDragAndDrop(
-				"css=td#zlif__CNS__" + contactItem.getId() + "__fileas:contains("+ contactItem.fileAs + ")",
+				"css=td#zlif__CNS-main__" + contactItem.getId() + "__fileas:contains("+ contactItem.fileAs + ")",
 				"css=td#zti__main_Contacts__" + tagItem.getId() + "_textCell:contains("+ tagItem.getName() + ")");
 			
 		Verify(contactItem, tagItem.getName());
