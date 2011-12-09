29f6eef549020beeaacacf4022f174b72615dae4
 Unknown Perforce User <rhoades>
2011-12-09 01:07:24 +0000

    Selenium Harness: add test cases for getting a folder and renaming a folder with non-ascii character names
    Bug: none

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/folders/GetFolder.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/folders/GetFolder.java
@@ -2,12 +2,13 @@ package com.zimbra.qa.selenium.projects.ajax.tests.mail.folders;
 
 import java.util.List;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.framework.util.ZimbraCharsets.*;
 import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
 
 
@@ -60,5 +61,48 @@ public class GetFolder extends PrefGroupMailByMessageTest {
 		
 	}	
 
+	@DataProvider(name = "DataProviderFilenames")
+	public Object[][] DataProviderDeleteKeys() throws HarnessException {
+		return (ZimbraCharsets.getInstance().getSampleTable());
+	}
+
+
+	@Test(
+			description = "Get a folder with non-ASCII special characters", 
+			groups = { "functional", "matt" },
+			dataProvider = "DataProviderFilenames")
+	public void GetFolder_02(ZCharset charset, String foldername) throws HarnessException {
+		
+		FolderItem inbox = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+		ZAssert.assertNotNull(inbox, "Verify the inbox is available");
+		
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ foldername +"' l='"+ inbox.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subfolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), foldername);
+		ZAssert.assertNotNull(subfolder, "Verify the subfolder is available");
+		
+		
+		// Click on Get Mail to refresh the folder list
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		
+		// Verify the folder appears in the list
+		List<FolderItem> folders = app.zTreeMail.zListGetFolders();
+		
+		FolderItem found = null;
+		for (FolderItem f : folders) {
+			if ( foldername.equals(f.getName()) ) {
+				found = f;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(found, "Verify the folder was in the tree");
+
+		
+	}	
 
 }
--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/folders/RenameFolder.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/folders/RenameFolder.java
@@ -1,12 +1,13 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.mail.folders;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import com.zimbra.common.soap.Element;
 import com.zimbra.qa.selenium.framework.items.FolderItem;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.framework.util.ZimbraCharsets.*;
 import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
 import com.zimbra.qa.selenium.projects.ajax.ui.DialogError;
 import com.zimbra.qa.selenium.projects.ajax.ui.DialogRenameFolder;
@@ -108,6 +109,55 @@ public class RenameFolder extends PrefGroupMailByMessageTest {
 		
 	}
 
+	@DataProvider(name = "DataProviderFilenames")
+	public Object[][] DataProviderDeleteKeys() throws HarnessException {
+		return (ZimbraCharsets.getInstance().getSampleTable());
+	}
+
+	@Test(
+			description = "Get a folder with non-ASCII special characters", 
+			groups = { "functional", "matt" },
+			dataProvider = "DataProviderFilenames")
+	public void RenameFolder_03(ZCharset charset, String foldername) throws HarnessException {
+		
+		FolderItem inbox = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+		ZAssert.assertNotNull(inbox, "Verify the inbox is available");
+				
+		// Create the subfolder
+		String name1 = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name1 +"' l='"+ inbox.getId() +"'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subfolder1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder1, "Verify the subfolder is available");
+		
+		
+		// Click on Get Mail to refresh the folder list
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Rename the folder using context menu
+		DialogRenameFolder dialog = (DialogRenameFolder)app.zTreeMail.zTreeItem(Action.A_RIGHTCLICK, Button.B_RENAME, subfolder1);
+		ZAssert.assertNotNull(dialog, "Verify the dialog opened");
+		
+		// Set the name, click OK
+		dialog.zSetNewName(foldername);
+		dialog.zClickButton(Button.B_OK);
+
+		
+		// Get all the folders and verify the new name appears and the old name disappears
+		app.zGetActiveAccount().soapSend("<GetFolderRequest xmlns = 'urn:zimbraMail'/>");
+		
+		Element[] eFolder1 = app.zGetActiveAccount().soapSelectNodes("//mail:folder[@name='"+ name1 +"']");
+		ZAssert.assertEquals(eFolder1.length, 0, "Verify the old folder name no longer exists");
+		
+		Element[] eFolder2 = app.zGetActiveAccount().soapSelectNodes("//mail:folder[@name='"+ foldername +"']");
+		ZAssert.assertEquals(eFolder2.length, 1, "Verify the new folder name exists");
+		
+		
+	}
 
 
 }
