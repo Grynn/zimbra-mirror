375752cca4360eb5474e016535138e504c13e68c
 Unknown Perforce User <jhidayat>
2011-12-10 00:15:58 +0000

    Integrate Change 342008 by jhidayat@ZimbraQA_jhidayat on 12/9/2011 4:14:05 PM Bug: None ZD Selenium: Added 3 forward Local Folders' contact tests.

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/tests/addressbook/contacts/ForwardContact.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/desktop/tests/addressbook/contacts/ForwardContact.java
@@ -84,5 +84,68 @@ public class ForwardContact extends AjaxCommonTest  {
         dialogWarning.zClickButton(Button.B_NO);
 	}
 
+	@Test(   description = "Forward a local Folders' contact by click Forward on the toolbar",
+	      groups = { "functional" })
+   public void LocalInDisplayViewClickForwardOnToolbar() throws HarnessException {
+
+	   // Create a contact via Soap then select
+	   ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectLocalContact(app, Action.A_LEFTCLICK);
+
+	   //click Forward icon on toolbar
+	   FormMailNew formMail = (FormMailNew) app.zPageAddressbook.zToolbarPressButton(Button.B_FORWARD);
+
+	   Assert.assertTrue(formMail.zHasAttachment(contactItem.fileAs + ".vcf"), "Verify there is  attachment named: " + contactItem.fileAs );
+
+	   //TODO: verify attachment file content
+
+	   //click Cancel
+	   DialogWarning dialogWarning= (DialogWarning) formMail.zToolbarPressButton(Button.B_CANCEL);
+
+	   //close the dialog
+	   dialogWarning.zClickButton(Button.B_NO);
+	}
+
+	@Test(   description = "Forward an editing Local Folders contact by click Forward on the toolbar",
+	      groups = { "functional" })
+   public void LocalInEditViewClickForwardOnToolbar() throws HarnessException {
+
+	   // Create a contact via Soap then select
+	   ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectLocalContact(app, Action.A_LEFTCLICK);
+
+	   // click Edit button
+	   app.zPageAddressbook.zToolbarPressButton(Button.B_EDIT);
+
+	   //click Forward icon on toolbar
+	   FormMailNew formMail = (FormMailNew) app.zPageAddressbook.zToolbarPressButton(Button.B_FORWARD);
+
+	   Assert.assertTrue(formMail.zHasAttachment(contactItem.fileAs + ".vcf"), "Verify there is  attachment named: " + contactItem.fileAs );
+
+	   //click Cancel
+	   DialogWarning dialogWarning= (DialogWarning) formMail.zToolbarPressButton(Button.B_CANCEL);
+
+	   //close the dialog
+	   dialogWarning.zClickButton(Button.B_NO);
+	}
+
+	@Test( description = "Forward a Local Folders contact by click Forward on the context menu",
+	      groups = { "functional" })
+	public void LocalClickForwardOnContextmenu() throws HarnessException {
+	   // Create a contact via Soap then select
+	   ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectLocalContact(app, Action.A_LEFTCLICK);
+
+	   //click Forward icon on context menu
+	   FormMailNew formMail = (FormMailNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_FORWARD, contactItem.fileAs);        
+
+	   Assert.assertTrue(formMail.zHasAttachment(contactItem.fileAs + ".vcf"), "Verify there is  attachment named: " + contactItem.fileAs );
+
+	   //TODO: verify attachment file content
+
+	   //click Cancel
+	   DialogWarning dialogWarning= (DialogWarning) formMail.zToolbarPressButton(Button.B_CANCEL);
+
+	   //close the dialog
+	   dialogWarning.zClickButton(Button.B_NO);
+	}
+
 }
 
