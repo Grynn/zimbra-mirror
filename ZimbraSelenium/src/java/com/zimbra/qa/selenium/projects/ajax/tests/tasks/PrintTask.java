6969256385580ad511129db70754514d1dc26616
 Unknown Perforce User <girishb>
2011-12-13 11:20:34 +0000

    bug:none
    --Added test cases for Print functionality related to task

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/PrintTask.java
@@ -0,0 +1,216 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks;
+
+
+
+import java.util.HashMap;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.TaskItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.ui.Shortcut;
+
+import com.zimbra.qa.selenium.framework.util.GeneralUtility;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+
+
+public class PrintTask extends AjaxCommonTest {
+
+	@SuppressWarnings("serial")
+	public PrintTask() {
+		logger.info("New "+ PrintTask.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageTasks;
+
+		super.startingAccountPreferences = new HashMap<String , String>() {{
+			put("zimbraPrefTasksReadingPaneLocation", "bottom");
+		}};
+
+	}
+
+
+
+	@Test(	description = "Print Task using RightClick -> Print and Verify Contents in Print view",groups = { "functional" })
+	public void PrintTask_01() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task 
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+		String bodyText = "text" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>"+ bodyText +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+
+		// Right click the item, select Show Original
+		app.zPageTasks.zListItem(Action.A_RIGHTCLICK, Button.O_PRINT_MENU, subject);
+		SleepUtil.sleepMedium();
+
+		//Verify content in Print view.
+		String Printcontent=app.zPageTasks.zGetPrintWindowContent();
+		ZAssert.assertStringContains(Printcontent, subject, "Verify subject in Print view");
+		ZAssert.assertStringContains(Printcontent, bodyText, "Verify content in Print view");
+
+	}
+
+	@Test(	description = "Print Task using shortcut 'p' and verify its content from GUI",groups = { "functional" })
+	public void PrintTask_02() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task 
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+		String bodyText = "text" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>"+ bodyText +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+		//Press keyboard shortcut p
+		app.zPageTasks.zKeyboardShortcut(Shortcut.S_PRINTTASK);
+
+		SleepUtil.sleepMedium();
+
+		//Verify content in Print view.
+		String Printcontent=app.zPageTasks.zGetPrintWindowContent();
+		ZAssert.assertStringContains(Printcontent, subject, "Verify subject in Print view");
+		ZAssert.assertStringContains(Printcontent, bodyText, "Verify content in Print view");
+
+	}
+	@Test(	description = "Print multiple tasks using Print-> Print TaskFolder and  and verify its content from GUI",groups = { "functional" })
+	public void PrintTask_03() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);		
+		// Create a basic task to delete
+		String subject1 = "task1"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject2 = "task2"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject3 = "task3"+ ZimbraSeleniumProperties.getUniqueString();
+				
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject1 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject1 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject2 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject2 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+					"<m >" +
+			        	"<inv>" +
+			        		"<comp name='"+ subject3 +"'>" +
+			        			"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+			        		"</comp>" +
+			        	"</inv>" +
+			        	"<su>"+ subject3 +"</su>" +
+			        	"<mp ct='text/plain'>" +
+			        		"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+			        	"</mp>" +
+					"</m>" +
+				"</CreateTaskRequest>");
+
+		
+
+		TaskItem task1 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject1);
+		TaskItem task2 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject2);
+		TaskItem task3 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject3);
+		
+		ZAssert.assertNotNull(task1, "Verify the task1 is created");
+		ZAssert.assertNotNull(task2, "Verify the task2 is created");
+		ZAssert.assertNotNull(task3, "Verify the task3 is created");
+		
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+		
+		//Pull down Print button and select Print Task folder.
+		app.zPageTasks.zToolbarPressPulldown(Button.B_PRINT, Button.O_PRINT_TASKFOLDER);
+		
+		SleepUtil.sleepMedium();
+
+		//Verify subjects in Print view.
+		String Printcontent=app.zPageTasks.zGetPrintWindowContent();
+		ZAssert.assertStringContains(Printcontent, subject1, "Verify subject in Print view");
+		ZAssert.assertStringContains(Printcontent, subject2, "Verify subject2 in Print view");
+		ZAssert.assertStringContains(Printcontent, subject3, "Verify subject2 in Print view");
+		
+
+	}
+}
+
