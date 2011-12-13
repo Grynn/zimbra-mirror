b67644b83e15cdbacf2ef8521c430f40e5f9c81d
 Unknown Perforce User <rhoades>
2011-12-13 18:38:54 +0000

    Selenium Harness: add test case for multiple-select then mark spam
    Bug: 68246
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=68246

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mail/MarkSpamMessage.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/mail/MarkSpamMessage.java
@@ -1,12 +1,12 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.mail.mail;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import com.zimbra.qa.selenium.framework.items.*;
-import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.items.FolderItem.*;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
-import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.core.*;
 
 
 public class MarkSpamMessage extends PrefGroupMailByMessageTest {
@@ -113,6 +113,74 @@ public class MarkSpamMessage extends PrefGroupMailByMessageTest {
 		
 		ZAssert.assertEquals(mail.dFolderId, junk.getId(), "Verify the message is in the spam folder");
 				
+	}
+
+	@Test(	description = "Mark multiple messages (3) as spam by select and toolbar delete",
+			groups = { "functional" })
+	public void MarkSpamMessage_03() throws HarnessException {
+		
+		FolderItem junk = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Junk);
+
+		// Create the message data to be sent
+		String subject1 = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject2 = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject3 = "subject"+ ZimbraSeleniumProperties.getUniqueString();
+				
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+					"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<su>"+ subject1 +"</su>" +
+						"<mp ct='text/plain'>" +
+							"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+					"</m>" +
+				"</SendMsgRequest>");
+
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+					"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<su>"+ subject2 +"</su>" +
+						"<mp ct='text/plain'>" +
+							"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+					"</m>" +
+				"</SendMsgRequest>");
+
+		ZimbraAccount.AccountA().soapSend(
+				"<SendMsgRequest xmlns='urn:zimbraMail'>" +
+					"<m>" +
+						"<e t='t' a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+						"<su>"+ subject3 +"</su>" +
+						"<mp ct='text/plain'>" +
+							"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+						"</mp>" +
+					"</m>" +
+				"</SendMsgRequest>");
+
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+				
+		// Select all three items
+		app.zPageMail.zListItem(Action.A_MAIL_CHECKBOX, subject1);
+		app.zPageMail.zListItem(Action.A_MAIL_CHECKBOX, subject2);
+		app.zPageMail.zListItem(Action.A_MAIL_CHECKBOX, subject3);
+		
+		// Click toolbar delete button
+		app.zPageMail.zToolbarPressButton(Button.B_RESPORTSPAM);
+				
+
+		MailItem mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject1 +") is:anywhere");
+		ZAssert.assertEquals(mail.dFolderId, junk.getId(), "Verify the message is in the spam folder");
+
+		mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject2 +") is:anywhere");
+		ZAssert.assertEquals(mail.dFolderId, junk.getId(), "Verify the message is in the spam folder");
+
+		mail = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject3 +") is:anywhere");
+		ZAssert.assertEquals(mail.dFolderId, junk.getId(), "Verify the message is in the spam folder");
+
 	}
 
 
