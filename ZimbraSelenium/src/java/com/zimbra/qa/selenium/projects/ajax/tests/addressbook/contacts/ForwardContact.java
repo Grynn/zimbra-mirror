4e7673db579df16c01433a70f0dab998fccca6d5
 Unknown Perforce User <dieunguyen>
2011-12-21 23:59:04 +0000

    bug:none - mark as "deprecated" for the test case of forwarding a contact in diaplay view

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/ForwardContact.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/contacts/ForwardContact.java
@@ -26,7 +26,7 @@ public class ForwardContact extends AjaxCommonTest  {
 	}
 	
 	@Test(	description = "Forward a contact by click Forward on the toolbar",
-			groups = { "functional" })
+			groups = { "deprecated" })
 	public void InDisplayViewClickForwardOnToolbar() throws HarnessException {
 			
 		  // Create a contact via Soap then select
@@ -35,6 +35,7 @@ public class ForwardContact extends AjaxCommonTest  {
         //click Forward icon on toolbar
         FormMailNew formMail = (FormMailNew) app.zPageAddressbook.zToolbarPressButton(Button.B_FORWARD);
 
+        
         //wait for attachment link present
         for (int i=0; (i<20) && !app.zPageAddressbook.sIsElementPresent("css=div[id$=_attachments_div] div[class='ImgAttachment']") ; i++ , SleepUtil.sleepVerySmall());
    
@@ -81,15 +82,15 @@ public class ForwardContact extends AjaxCommonTest  {
 	public void ClickForwardOnContextmenu() throws HarnessException {
 		  // Create a contact via Soap then select
 		ContactItem contactItem = app.zPageAddressbook.createUsingSOAPSelectContact(app, Action.A_LEFTCLICK);
-	
+		SleepUtil.sleepLong();SleepUtil.sleepLong();
         //click Forward icon on context menu
         FormMailNew formMail = (FormMailNew) app.zPageAddressbook.zListItem(Action.A_RIGHTCLICK, Button.B_FORWARD, contactItem.fileAs);        
         
-        
+        SleepUtil.sleepLong();SleepUtil.sleepLong();
         //wait for attachment link present
         for (int i=0; (i<20) && !app.zPageAddressbook.sIsElementPresent("css=div[id$=_attachments_div] div[class='ImgAttachment']") ; i++ , SleepUtil.sleepVerySmall());
         	
-                
+        SleepUtil.sleepLong();SleepUtil.sleepLong();SleepUtil.sleepLong();        
         Assert.assertTrue(formMail.zHasAttachment(contactItem.fileAs + ".vcf"), "Verify there is  attachment named: " + contactItem.fileAs);
         	           
         
