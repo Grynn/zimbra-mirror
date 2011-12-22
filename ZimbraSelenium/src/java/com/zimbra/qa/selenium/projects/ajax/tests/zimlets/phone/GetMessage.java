99cc6b5492e94bd7a73f905d01435116b18e808f
 Unknown Perforce User <rhoades>
2011-12-22 22:57:34 +0000

    Selenium Harness: update regex not to include the space before the '('.  Likely doesn't matter in terms of callto.  If the zimlet continues to be flaky, it would probably be best to update the regex to accept either space or no space as success ... right now, it will flag 'space' + '(' as a failure.
    Bug: 67424
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=67424

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/phone/GetMessage.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/phone/GetMessage.java
@@ -149,11 +149,11 @@ public class GetMessage extends AjaxCommonTest {
 		{
 			put("//a[@href='callto:1-877-486-9273']",				"1-877-486-9273");
 			put("//a[@href='callto:877-486-9273']",					"877-486-9273");
-			put("//a[@href='callto:%28877%29%20456-7890']",			" (877) 456-7890");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
+			put("//a[@href='callto:%28877%29%20456-7890']",			"(877) 456-7890");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
 			put("//a[@href='callto:6503501010']",					"6503501010");
 			put("//a[@href='callto:+1%20650%20350%201010']",		"+1 650 350 1010");		// http://bugzilla.zimbra.com/show_bug.cgi?id=52337#c15
 			put("//a[@href='callto:650.350.1010']",					"650.350.1010");
-			put("//a[@href='callto:%28650%29%20350%201010']",		" (650) 350 1010");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
+			put("//a[@href='callto:%28650%29%20350%201010']",		"(650) 350 1010");		// http://bugzilla.zimbra.com/show_bug.cgi?id=67424
 			put("//a[@href='callto:+1%20%28650%29%20350%201010']",	"+1 (650) 350 1010");	// http://bugzilla.zimbra.com/show_bug.cgi?id=52337#c15
 			put("//a[@href='callto:1-650-350-1010']",				"1-650-350-1010");
 		}};
