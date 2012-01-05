a3590771883a7c1887a4bfaeb64a5eb4c10750ee
 Unknown Perforce User <tfr>
2012-01-05 23:11:15 +0000

    bug: none add pre-link step so that exchange.dll can be registered

---
+++ ZimbraMigrationTools/src/c/Exchange/pre-link.bat
@@ -0,0 +1,6 @@
+copy ..\..\..\..\ZimbraMAPI\%2\%1\LSLIB32.dll ..\%2\%1
+copy ..\..\..\..\ZimbraMAPI\%2\%1\LSMIME32.dll ..\%2\%1
+copy ..\..\..\..\ZimbraMAPI\%2\%1\LSMSCFG32.dll ..\%2\%1
+copy ..\..\..\..\ZimbraMAPI\%2\%1\LSMSUTIL32.dll ..\%2\%1
+
+copy ..\..\..\..\ZimbraMAPI\src\lib\%3\zlib*.dll ..\%2\%1
