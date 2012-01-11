6518c90cbb73845dd264216cca58cc2c05a4593f
Vince B. Bellows <vbellows@zimbra.com>
2012-01-11 16:05:02 +0000

    Bug 66504 : Continuation of 56463, Make suggestions for recurring appointments in Meeting Details, Part 3 This checking has the Location Resolution links and dialog hidden, awaiting the completion of bug 56464, extensions to the CreateAppointment & ModifyAppointment SOAP API. - Added getException call to retrieve the existing exceptions of an appointment. - Added processing to setLocationStatus to format the proper message and link (hidden for now) - Added the ResolveLocationConflictDialog and View.  These present a list of dates that have location conflicts (bounded by number of recurrences to consider in the preferences dialog) and a list of alternate locations that can be selected for each conflict date.
    Also has a trivial fix for Bug 68531 : Js error (e.getApp().getFreeBusyCache is not a function) while trying to create new appointment using zimlet context menu
    
    
    Review : https://reviewboard.eng.vmware.com/r/294736/
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=66504

--- ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtSelect.js
+++ ZimbraWebClient/WebRoot/js/ajax/dwt/widgets/DwtSelect.js
@@ -52,6 +52,8 @@ DwtSelect = function(params) {
     this._selectedValue = this._selectedOption = null;
 	this._maxRows = params.maxRows || 0;
 	this._layout = params.layout;
+    this._congruent = params.congruent;
+    this._hrCount = 0;
 
     // add options
     var options = params.options;
@@ -185,6 +187,13 @@ function(option, selected, value, image) {
     // return the index of the option.
     this._optionValuesToIndices[opt.getValue()] = this._options.size() - 1;
     return (this._options.size() - 1);
+};
+
+DwtSelect.prototype.addHR =
+function() {
+    opt = new DwtSelectOption("hr" + this._hrCount.toString(), false, "", this, null, null, null, true);
+    this._hrCount++;
+	this._options.add(opt);
 };
 
 /**
@@ -591,21 +600,28 @@ function(templateId, data) {
 DwtSelect.prototype._createMenu =
 function() {
     var menu = new DwtSelectMenu(this);
+    var mi;
     for (var i = 0, len = this._options.size(); i < len; ++i) {
-	var option = this._options.get(i);
-	var mi = new DwtSelectMenuItem(menu, Dwt.getNextId(option._value + "_"));
-        var image = option.getImage();
-        if (image) {
-            mi.setImage(image);
-        }
-        var text = option.getDisplayValue();
-		if (text) {
-			mi.setText(AjxStringUtil.htmlEncode(text));
-		}
-		mi.setEnabled(option.enabled);
+	    var option = this._options.get(i);
+        if (option._hr) {
+            mi = new DwtMenuItem({parent:menu, style:DwtMenuItem.SEPARATOR_STYLE});
+            mi.setEnabled(false);
+        } else {
+            var mi = new DwtSelectMenuItem(menu, Dwt.getNextId(option._value + "_"));
+            var image = option.getImage();
+            if (image) {
+                mi.setImage(image);
+            }
+            var text = option.getDisplayValue();
+            if (text) {
+                mi.setText(AjxStringUtil.htmlEncode(text));
+            }
+            mi.setEnabled(option.enabled);
 
-		mi.addSelectionListener(new AjxListener(this, this._handleOptionSelection));
-		mi._optionIndex = i;
+            mi.addSelectionListener(new AjxListener(this, this._handleOptionSelection));
+            mi._optionIndex = i;
+        }
+        mi._optionIndex = i;
 		option.setItem(mi);
     }
 	return menu;
@@ -739,13 +755,15 @@ DwtSelectOptionData = function(value, displayValue, isSelected, selectedValue, i
  * @param {DwtSelect}	owner 	not used
  * @param {String}	optionalDOMId		not used
  * @param {String}	[selectedValue] 	the text value to use when this value is the currently selected value
+ * @param {Boolean}	hr                  True => This option will be usd to create a unselectable horizontal rule
  */
-DwtSelectOption = function(value, selected, displayValue, owner, optionalDOMId, image, selectedValue) {
+DwtSelectOption = function(value, selected, displayValue, owner, optionalDOMId, image, selectedValue, hr) {
 	this._value = value;
 	this._selected = selected;
 	this._displayValue = displayValue;
 	this._image = image;
 	this._selectedValue = selectedValue;
+    this._hr = hr;
 
 	this._internalObjectId = DwtSelect._assignId(this);
 	this.enabled = true;
@@ -873,7 +891,9 @@ function() {
  * @extends		DwtMenu
  */
 DwtSelectMenu = function(parent) {
-    DwtMenu.call(this, {parent:parent, style:DwtMenu.DROPDOWN_STYLE, className:"DwtMenu", layout:parent._layout, maxRows:parent._maxRows, id:Dwt.getNextId(parent.getHTMLElId() + "_Menu_")});
+    DwtMenu.call(this, {parent:parent, style:DwtMenu.DROPDOWN_STYLE, className:"DwtMenu", layout:parent._layout,
+        maxRows:parent._maxRows, congruent:parent._congruent,
+        id:Dwt.getNextId(parent.getHTMLElId() + "_Menu_")});
 // Dwt.getNextId should be removed once Bug 66510 is fixed
 };
 DwtSelectMenu.prototype = new DwtMenu;
