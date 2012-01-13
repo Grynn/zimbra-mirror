61ce7540bf2fa68f46a0e36f7914d9c987e4ff95
 Unknown Perforce User <rgadipuuri>
2012-01-13 06:09:16 +0000

    bug:67080 changes made to throw an exception when user tries to create address book for Gmail, Yahoo and live
    http://bugzilla.zimbra.com/show_bug.cgi?id=67080

--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineCreateFolder.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineCreateFolder.java
@@ -19,6 +19,9 @@ import java.util.Map;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
+import com.zimbra.cs.account.offline.OfflineDataSource;
+import com.zimbra.cs.account.offline.OfflineProvisioning;
+import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.ZcsMailbox;
 import com.zimbra.cs.service.FeedManager;
@@ -30,10 +33,20 @@ public class OfflineCreateFolder extends CreateFolder {
     public Element handle(Element request, Map<String, Object> context) throws ServiceException {
         ZimbraSoapContext zsc = getZimbraSoapContext(context);
         Mailbox mbox = getRequestedMailbox(zsc);
+        Element t = request.getElement(MailConstants.E_FOLDER);
+        String viewStr = t.getAttribute(MailConstants.A_DEFAULT_VIEW, null);
+        if (viewStr != null) {
+            if(MailItem.getTypeForName(viewStr) == MailItem.TYPE_CONTACT) {
+                OfflineProvisioning prov = OfflineProvisioning.getOfflineInstance();
+                OfflineDataSource dataSource = (OfflineDataSource) prov.getDataSource(mbox.getAccount());
+                if (dataSource.isGmail() || dataSource.isYahoo() || dataSource.isLive()) {
+                    throw ServiceException.FAILURE("ZD does not allow creation of Address book for Gmail, Yahoo and Live", null);
+                }
+            }
+        }
         if (!(mbox instanceof ZcsMailbox))
             return super.handle(request, context);
-            
-        Element t = request.getElement(MailConstants.E_FOLDER);
+
         String url = t.getAttribute(MailConstants.A_URL, null);
 
         if (url != null && !url.equals("")) {
