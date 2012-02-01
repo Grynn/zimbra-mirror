300a76fb9ee5a47cd1ca3a130c633c931e8ccd23
 Unknown Perforce User <rhoades>
2012-02-01 19:20:11 +0000

    Bug: none
    Selenium Harness: add test cases to search for files by filename, text content, binary content (word doc), and files in trash.

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/octopus/tests/search/SearchFile.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/octopus/tests/search/SearchFile.java
@@ -38,9 +38,7 @@ public class SearchFile extends OctopusCommonTest {
 		String attachmentId = app.zGetActiveAccount().uploadFile(filePath);
 
 		// Save uploaded file through SOAP
-		FolderItem briefcaseRootFolder = FolderItem.importFromSOAP(
-				app.zGetActiveAccount(), 
-				SystemFolder.Briefcase);
+		FolderItem briefcaseRootFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Briefcase);
 
 		app.zGetActiveAccount().soapSend(
 					"<SaveDocumentRequest xmlns='urn:zimbraMail'>"
@@ -76,19 +74,183 @@ public class SearchFile extends OctopusCommonTest {
 
 	@Test(
 			description = "Search for a text file by content", 
-			groups = { "sanity" })
+			groups = { "smoke" })
 	public void SearchFile_02() throws HarnessException {
+		
+
+		String filename = "filename"+ ZimbraSeleniumProperties.getUniqueString() +".txt";
+		String query = "reunification";
+		String filePath = ZimbraSeleniumProperties.getBaseDirectory()
+				+ "/data/public/documents/doc01/plaintext.txt";
+		
+		
+
+		// Upload file to server through RestUtil
+		String attachmentId = app.zGetActiveAccount().uploadFile(filePath);
+
+		// Save uploaded file through SOAP
+		FolderItem briefcaseRootFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Briefcase);
+
+		app.zGetActiveAccount().soapSend(
+					"<SaveDocumentRequest xmlns='urn:zimbraMail'>"
+				+		"<doc name='"+ filename +"' l='" + briefcaseRootFolder.getId() + "'>"
+				+			"<upload id='" + attachmentId + "'/>"
+				+		"</doc>"
+				+	"</SaveDocumentRequest>");
+
+
+		
+		// Sync up
+//		app.zPageOctopus.zToolbarPressButton(Button.B_GETMAIL);
+
+
+		// Search for the message
+		app.zPageSearch.zExecuteSearchQuery(query);
+		
+		// Get all the messages in the view
+		// Verify the uploaded file exists
+		boolean found = false;
+		List<IOctListViewItem> items = app.zPageSearch.zGetListViewItems();
+		for (IOctListViewItem item : items) {
+			if ( item.getListViewName().equalsIgnoreCase(filename) ) {
+				// found it
+				found = true;
+				break;
+			}
+		}
+				
+		ZAssert.assertTrue(found, "Verify the item is found in the list after searching");
+
+		
 	}
 	
 	@Test(
-			description = "Search for a binary (spreadsheet) file by content", 
-			groups = { "sanity" })
+			description = "Search for a binary (word doc) file by content", 
+			groups = { "smoke" })
 	public void SearchFile_03() throws HarnessException {
+		
+
+		String filename = "filename"+ ZimbraSeleniumProperties.getUniqueString() +".docx";
+		String query = "reunification";
+		String filePath = ZimbraSeleniumProperties.getBaseDirectory()
+				+ "/data/public/documents/doc01/word2007.docx";
+		
+		
+
+		// Upload file to server through RestUtil
+		String attachmentId = app.zGetActiveAccount().uploadFile(filePath);
+
+		// Save uploaded file through SOAP
+		FolderItem briefcaseRootFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Briefcase);
+
+		app.zGetActiveAccount().soapSend(
+					"<SaveDocumentRequest xmlns='urn:zimbraMail'>"
+				+		"<doc name='"+ filename +"' l='" + briefcaseRootFolder.getId() + "'>"
+				+			"<upload id='" + attachmentId + "'/>"
+				+		"</doc>"
+				+	"</SaveDocumentRequest>");
+
+
+		
+		// Sync up
+//		app.zPageOctopus.zToolbarPressButton(Button.B_GETMAIL);
+
+
+		// Search for the message
+		app.zPageSearch.zExecuteSearchQuery(query);
+		
+		// Get all the messages in the view
+		// Verify the uploaded file exists
+		boolean found = false;
+		List<IOctListViewItem> items = app.zPageSearch.zGetListViewItems();
+		for (IOctListViewItem item : items) {
+			if ( item.getListViewName().equalsIgnoreCase(filename) ) {
+				// found it
+				found = true;
+				break;
+			}
+		}
+				
+		ZAssert.assertTrue(found, "Verify the item is found in the list after searching");
+
+		
+
 	}
 	
 	@Test(description = "Search for a file in trash", 
-			groups = { "smoke" })
+			groups = { "functional" })
 	public void SearchFile_04() throws HarnessException {
+		
+		
+
+		String filename = "filename"+ ZimbraSeleniumProperties.getUniqueString() +".txt";
+		String query = "reunification";
+		String filePath = ZimbraSeleniumProperties.getBaseDirectory()
+				+ "/data/public/documents/doc01/plaintext.txt";
+		
+		
+
+		// Upload file to server through RestUtil
+		String attachmentId = app.zGetActiveAccount().uploadFile(filePath);
+
+		// Save uploaded file through SOAP
+		FolderItem briefcaseRootFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Briefcase);
+		FolderItem trashFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Trash);
+
+		app.zGetActiveAccount().soapSend(
+					"<SaveDocumentRequest xmlns='urn:zimbraMail'>"
+				+		"<doc name='"+ filename +"' l='" + briefcaseRootFolder.getId() + "'>"
+				+			"<upload id='" + attachmentId + "'/>"
+				+		"</doc>"
+				+	"</SaveDocumentRequest>");
+		String documentId = app.zGetActiveAccount().soapSelectValue("//mail:doc", "id");
+
+
+		// Delete the doc
+		app.zGetActiveAccount().soapSend(
+					"<ItemActionRequest xmlns='urn:zimbraMail'>"
+				+		"<action op='move' id='"+ documentId +"' l='" + trashFolder.getId() +"'/>"
+				+	"</ItemActionRequest>");
+
+		// Sync up
+//		app.zPageOctopus.zToolbarPressButton(Button.B_GETMAIL);
+
+
+		// Search for the message
+		app.zPageSearch.zExecuteSearchQuery(query);
+		
+		// Get all the messages in the view
+		// Verify the uploaded file exists
+		boolean found = false;
+		List<IOctListViewItem> items = app.zPageSearch.zGetListViewItems();
+		for (IOctListViewItem item : items) {
+			if ( item.getListViewName().equalsIgnoreCase(filename) ) {
+				// found it
+				found = true;
+				break;
+			}
+		}
+				
+		ZAssert.assertFalse(found, "Verify the item is not found when query does not include is:anywhere");
+
+		// Search for the message
+		app.zPageSearch.zExecuteSearchQuery(query + " is:anywhere");
+		
+		// Get all the messages in the view
+		// Verify the uploaded file exists
+		found = false;
+		items = app.zPageSearch.zGetListViewItems();
+		for (IOctListViewItem item : items) {
+			if ( item.getListViewName().equalsIgnoreCase(filename) ) {
+				// found it
+				found = true;
+				break;
+			}
+		}
+				
+		ZAssert.assertTrue(found, "Verify the item is found when query includes is:anywhere");
+
+
 	}
 
 }
