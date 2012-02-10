21d91d0bd7182bf16bb73e3b7dba7f2c6867ea8c
Vishal Mahajan <vmahajan@zimbra.com>
2012-02-10 19:12:22 +0000

    bug: 50655 - Product Telemetry Phase 1
    Adding a python script that can read last few days (var numdays) of access_log files from /opt/zimbra/log. The script iterates over each log entry in an access_log file and reads User-Agent and client IP from it, and counts requests received for each User-Agent & client IP pair. The output is a csv file containing three columns - user_agent, client_IP, and req_count.
    
    The generated csv file can be used for reporting which clients (browsers,sync clients,mobile clients,etc) are used by users and which ones are more popular amongst users.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=50655

---
+++ ZimbraServer/src/bin/client_usage_report.py
@@ -0,0 +1,65 @@
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Zimbra Collaboration Suite Server
+# Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 Zimbra, Inc.
+#
+# The contents of this file are subject to the Zimbra Public License
+# Version 1.3 ("License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://www.zimbra.com/license.
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+# ***** END LICENSE BLOCK *****
+#
+
+import fileinput
+import subprocess
+import re
+from datetime import date
+
+# Number of days of log files to read
+numdays = 1
+
+# Find the current total number of log files
+p = subprocess.Popen('ls /opt/zimbra/log/access_log* | wc -l', shell=True, stdout=subprocess.PIPE)
+numlogfiles = p.stdout.read()
+
+# Get the list of log files to read
+lscmdfmt = 'ls /opt/zimbra/log/access_log* | tail -{0} | head -{1}'
+if numlogfiles > numdays:
+    lscmd = lscmdfmt.format(numdays + 1, numdays)
+else:
+    lscmd = lscmdfmt.format(numdays, numlogfiles - 1)
+p = subprocess.Popen(lscmd, shell=True, stdout=subprocess.PIPE)
+
+resultmap = {}
+
+for file in p.stdout.readlines():
+    file = file.rstrip()
+    subprocess.call('echo Reading {0} ..'.format(file), shell=True)
+    for line in fileinput.input([file]):
+        l = line.split('"')
+        if len(l) < 6:
+            continue
+        ua = l[5].rstrip()
+        if not re.match('(zm.*|ZCS.*|zclient.*|.*ZCB.*|Jakarta.*|curl.*|-)', ua) is None:
+            continue
+        ip = line.split('-')[0].rstrip()
+        if ip == '127.0.0.1':
+            continue
+        key = ua, ip
+        curval = resultmap.get(key)
+        if curval is None:
+            resultmap[key] = 1
+        else:
+            resultmap[key] = curval + 1
+
+reportfile = '/opt/zimbra/zmstat/client_usage_report_{0}.csv'.format(date.today().isoformat())
+subprocess.call('rm -f {0}'.format(reportfile), shell=True)
+csv = open(reportfile, 'w')
+subprocess.call('echo Writing {0} ..'.format(reportfile), shell=True)
+csv.write('"user_agent","client_IP","req_count"\n')
+for key, value in resultmap.iteritems():
+    csv.write('"{0}","{1}","{2}"\n'.format(key[0], key[1], value))
+csv.close()
