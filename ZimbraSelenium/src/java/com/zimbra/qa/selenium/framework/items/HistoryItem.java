fa27e9de0e7258d6a72296b0773347ab2be3bdef
 Unknown Perforce User <dieunguyen>
2012-02-11 18:40:42 +0000

    bug:none - Modified HistoryItem.java

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/items/HistoryItem.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/items/HistoryItem.java
@@ -1,40 +1,90 @@
 package com.zimbra.qa.selenium.framework.items;
 
-public class HistoryItem {
+public class HistoryItem extends AItem {
 
-	public Object getHistoryText() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object getHistoryUser() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setLocator(String itemLocator) {
-		// TODO Auto-generated method stub
+	private String historyLocator = null;
+	private String historyText = null;
+	private String historyUser = null;
+	private String historyAction = null;
+	private String historyItem = null;
+	private String historyTime = null;
+	private String ParentId = null;
+	
+	public HistoryItem() {
 		
 	}
-
-	public void setHistoryUser(String sGetText) {
-		// TODO Auto-generated method stub
-		
+	
+	/**
+	 * @param historyLocator 
+	 */
+	public void setLocator(String locator) {
+		historyLocator = locator;
 	}
 
-	public void setHistoryTime(String sGetText) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * @return the theLocator
+	 */
+	public String getLocator() {
+		return historyLocator;
 	}
 
-	public void setHistoryText(String sGetText) {
-		// TODO Auto-generated method stub
-		
+	public void setParentId(String parentId) {
+		ParentId = parentId;
 	}
 
-	public Object prettyPrint() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getParentId() {
+		return ParentId;
+	}
+
+	public void setHistoryText(String historyText) {
+		this.historyText = historyText;
+	}
+
+	public String getHistoryText() {
+		return historyText;
+	}
+
+	public void setHistoryTime(String historyTime) {
+		this.historyTime = historyTime;
+	}
+
+	public String getHistoryTime() {
+		return historyTime;
+	}
+
+	public void setHistoryAction(String historyAction) {
+		this.historyAction = historyAction;
+	}
+
+	public String getHistoryAction() {
+		return historyAction;
+	}
+	
+	public void setHistoryItem(String historyItem) {
+		this.historyItem = historyItem;
+	}
+
+	public String getHistoryItem() {
+		return historyItem;
+	}
+	
+	public void setHistoryUser(String historyUser) {
+		this.historyUser = historyUser;
+	}
+
+	public String getHistoryUser() {
+		return historyUser;
+	}
+
+	@Override
+	public String prettyPrint() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(HistoryItem.class.getSimpleName()).append('\n');
+		sb.append("GUI Data:\n");
+		sb.append("HistoryText: ").append(getHistoryText()).append('\n');
+		sb.append("HistoryUser: ").append(getHistoryUser()).append('\n');
+		sb.append("HistoryTime: ").append(getHistoryTime()).append('\n');		
+		return (sb.toString());
 	}
 
 }
