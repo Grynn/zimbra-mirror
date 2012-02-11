8b0733a4b95a34687ac522e29a9f1fdf90929093
 Unknown Perforce User <rhoades>
2012-02-11 05:41:47 +0000

    Bug: none
    Selenium Harness: add stub for missing HistoryItem.java

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/framework/items/HistoryItem.java
@@ -0,0 +1,40 @@
+package com.zimbra.qa.selenium.framework.items;
+
+public class HistoryItem {
+
+	public Object getHistoryText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getHistoryUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setLocator(String itemLocator) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHistoryUser(String sGetText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHistoryTime(String sGetText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setHistoryText(String sGetText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object prettyPrint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
