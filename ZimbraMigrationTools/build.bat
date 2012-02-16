1dafa263e4d6ea367d2d956cf5d9d3dc46e0e5d0
Karuna Nuthi <knuthi@zimbra.com>
2012-02-16 19:22:33 +0000

    bug:none
    disable 64bit build for migration  unless we resolve the build machine issues.

--- ZimbraMigrationTools/build.bat
+++ ZimbraMigrationTools/build.bat
@@ -20,17 +20,6 @@ echo --------------
 MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=rtl;Platform=Win32
 IF ERRORLEVEL 1 exit /B 1 
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\bin\amd64\vcvars64.bat"
-
-echo "dbg|x64"
-echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=dbg;Platform=x64
-IF ERRORLEVEL 1 exit /B 1 
-
-echo "rtl|x64"
-echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=rtl;Platform=x64
-IF ERRORLEVEL 1 exit /B 1 
 
 ECHO Source Indexing the Project...
 ECHO Starting Point is %BASE%
@@ -43,6 +32,5 @@ ECHO Adding Binaries to the Symbol Server...
 ECHO symstore add /f %BASE%\src\c\*\rtl\*.* /s \\%INDEX_HOST%\Zbuild3\symbols\ZCO /t "Zimbra Migration Tools" /v "%BUILD_VERSION%" /c "%BUILD_DATE%"
 symstore add /f %BASE%\src\c\Win32\rtl\*.* /s \\%INDEX_HOST%\Zbuild3\symbols\ZCO /t "Zimbra Migration Tools x86" /v "%BUILD_VERSION%" /c "%BUILD_DATE%"
 IF ERRORLEVEL 1 exit /B 1 
-symstore add /f %BASE%\src\c\x64\rtl\*.* /s \\%INDEX_HOST%\Zbuild3\symbols\ZCO /t "Zimbra Migration Tools x64" /v "%BUILD_VERSION%" /c "%BUILD_DATE%"
-IF ERRORLEVEL 1 exit /B 1 
+
 
