ddc51f76e2b7db6a3e4b09ac4f8328c5380dc350
 Unknown Perforce User <tfr>
2012-03-02 23:47:28 +0000

    bug: none increase build logging

--- ZimbraMigrationTools/build.bat
+++ ZimbraMigrationTools/build.bat
@@ -12,24 +12,24 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\bin\vcvars32.bat"
 
 echo "dbg|Win32"
 echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=dbg;Platform=Win32
+MSBuild Migration.sln /t:Build /verbosity:n /p:Configuration=dbg;Platform=Win32
 IF ERRORLEVEL 1 exit /B 1 
 
 echo "rtl|Win32"
 echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=rtl;Platform=Win32
+MSBuild Migration.sln /t:Build /verbosity:n /p:Configuration=rtl;Platform=Win32
 IF ERRORLEVEL 1 exit /B 1 
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\vc\bin\amd64\vcvars64.bat"
 
 echo "dbg|x64"
 echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=dbg;Platform=x64
+MSBuild Migration.sln /t:Build /verbosity:n /p:Configuration=dbg;Platform=x64
 IF ERRORLEVEL 1 exit /B 1 
 
 echo "rtl|x64"
 echo --------------
-MSBuild Migration.sln /t:Build /verbosity:m /p:Configuration=rtl;Platform=x64
+MSBuild Migration.sln /t:Build /verbosity:n /p:Configuration=rtl;Platform=x64
 IF ERRORLEVEL 1 exit /B 1 
 
 ECHO Source Indexing the Project...
