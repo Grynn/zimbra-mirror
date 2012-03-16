a0648a379921a709ff410836801e22f2ac6a4703
Karuna Nuthi <knuthi@zimbra.com>
2012-03-16 23:28:55 +0000

    bug:70698
    add a new class to check compatiblity of outlook and Exchangemigration dll.
    This check is more appropriate for Outlook2010 since 2010 has both 32 bit and 64 bit editions.
    The call to check the compatibiltiy has to be invoked from UI and Console at appropriate place.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=70698

--- ZimbraMigrationTools/src/c/CssLib/CSMigrationWrapper.cs
+++ ZimbraMigrationTools/src/c/CssLib/CSMigrationWrapper.cs
@@ -87,6 +87,95 @@ public class MigrationOptions
     public string SkipFolders;
 }
 
+
+public class CompatibilityChk
+{
+
+
+    public enum MachineType : ushort
+    {
+        IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
+        IMAGE_FILE_MACHINE_AM33 = 0x1d3,
+        IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+        IMAGE_FILE_MACHINE_ARM = 0x1c0,
+        IMAGE_FILE_MACHINE_EBC = 0xebc,
+        IMAGE_FILE_MACHINE_I386 = 0x14c,
+        IMAGE_FILE_MACHINE_IA64 = 0x200,
+        IMAGE_FILE_MACHINE_M32R = 0x9041,
+        IMAGE_FILE_MACHINE_MIPS16 = 0x266,
+        IMAGE_FILE_MACHINE_MIPSFPU = 0x366,
+        IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,
+        IMAGE_FILE_MACHINE_POWERPC = 0x1f0,
+        IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,
+        IMAGE_FILE_MACHINE_R4000 = 0x166,
+        IMAGE_FILE_MACHINE_SH3 = 0x1a2,
+        IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,
+        IMAGE_FILE_MACHINE_SH4 = 0x1a6,
+        IMAGE_FILE_MACHINE_SH5 = 0x1a8,
+        IMAGE_FILE_MACHINE_THUMB = 0x1c2,
+        IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169,
+    }
+    public static MachineType GetDllMachineType(string dllPath)
+    {
+        // http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx 
+
+        FileStream fs = new FileStream(dllPath, FileMode.Open, FileAccess.Read);
+        BinaryReader br = new BinaryReader(fs);
+        fs.Seek(0x3c, SeekOrigin.Begin);
+        Int32 peOffset = br.ReadInt32();
+        fs.Seek(peOffset, SeekOrigin.Begin);
+        UInt32 peHead = br.ReadUInt32();
+        if (peHead != 0x00004550) // "PE\0\0", little-endian 
+            throw new Exception("Can't find PE header");
+        MachineType machineType = (MachineType)br.ReadUInt16();
+        br.Close();
+        fs.Close();
+        return machineType;
+    }
+
+    // returns true if the dll is 64-bit, false if 32-bit, and null if unknown 
+    public static bool? UnmanagedDllIs64Bit(string dllPath)
+    {
+        switch (GetDllMachineType(dllPath))
+        {
+        case MachineType.IMAGE_FILE_MACHINE_AMD64:
+        case MachineType.IMAGE_FILE_MACHINE_IA64:
+            return true;
+        case MachineType.IMAGE_FILE_MACHINE_I386:
+            return false;
+        default:
+            return null;
+        }
+    }
+
+
+    public static string CheckCompat(string path)
+    {
+        string status = "";
+        string absolutepath = Path.GetFullPath("Exchange.dll");
+
+        bool retval = UnmanagedDllIs64Bit(absolutepath).Value;
+
+
+        string Bitness = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\14.0\Outlook", "Bitness", null);
+        if (Bitness != null)
+        {
+            if ((Bitness == "x64") && (!retval))
+            {
+
+                status = "Outlook is 64 bit and migration is 32 bit";
+
+            }
+
+        }
+        return status;
+
+    }
+
+
+}
+
+
 public class CSMigrationWrapper
 {
     enum foldertype
