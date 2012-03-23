9dfc2f54c050757e47d455f3885049cd8a3c91c9
 Unknown Perforce User <mizhang>
2012-03-23 09:28:37 +0000

    Bug 71338 	Make the version number can be wrapped and it can leave some space.
    http://bugzilla.zimbra.com/show_bug.cgi?id=71338

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/home/view/ZaHomeXFormView.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/home/view/ZaHomeXFormView.js
@@ -278,9 +278,9 @@ ZaHomeXFormView.myXFormModifier = function(xFormObject, entry) {
             ]},
             {type:_GROUP_, colSpan: "*", numCols: 3,  colSizes:["33%", "34%", "33%"], width: "100%",
                 containerCssClass:"ZaHomeInfoPanel",items:[
-                {type:_GROUP_, numCols: 2, valign: _TOP_, items:[
+                {type:_GROUP_, numCols: 2, valign: _TOP_,  colSizes:["auto"], width:"100%", items:[
                     {type:_OUTPUT_, colSpan:"2", value:ZaMsg.LBL_HomeSummary, cssClass:"ZaHomeInfoTitle"},
-                    {type:_OUTPUT_, label:ZabMsg.LBL_HomeZimbraVersion, ref: ZaHome.A2_version},
+                    {type:_OUTPUT_, label:ZabMsg.LBL_HomeZimbraVersion, cssStyle:"word-wrap:break-word",ref: ZaHome.A2_version},
                     {type:_OUTPUT_, label:ZaMsg.LBL_HomeServerNum, ref: ZaHome.A2_serverNum, bmolsnr: true},
                     {type:_OUTPUT_, label:ZaMsg.LBL_HomeAccountNum, ref: ZaHome.A2_accountNum, bmolsnr: true},
                     {type:_OUTPUT_, label:ZaMsg.LBL_HomeDomainNum, ref: ZaHome.A2_domainNum, bmolsnr: true},
