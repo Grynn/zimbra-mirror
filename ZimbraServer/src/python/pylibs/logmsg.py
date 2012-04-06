9f12a3748e7c6b27f6722df2932c2be570b80523
Quanah Gibson-Mount <quanah@zimbra.com>
2012-04-06 18:35:20 +0000

    bug: 72558, 61587 Add syslog4j and native JNA library for /dev/log syslog support Update zmconfigd to log to syslog, matching zmmtaconfig behavior
    http://bugzilla.zimbra.com/show_bug.cgi?id=72558
    http://bugzilla.zimbra.com/show_bug.cgi?id=61587

--- ZimbraServer/src/python/pylibs/logmsg.py
+++ ZimbraServer/src/python/pylibs/logmsg.py
@@ -23,59 +23,30 @@ import logging.handlers
 import re
 import threading
 
-lvlmap = {
-	5	: logging.DEBUG,
-	4	: logging.INFO,
-	3	: logging.WARNING,
-	2	: logging.ERROR,
-	1	: logging.CRITICAL,
-	0	: logging.FATAL,
-	}
+from org.productivity.java.syslog4j import Syslog
+from org.productivity.java.syslog4j import SyslogIF
+from org.productivity.java.syslog4j import SyslogConstants
+from org.productivity.java.syslog4j.impl.unix import UnixSyslog
+from org.productivity.java.syslog4j.impl.unix.socket import UnixSocketSyslogConfig
 
 class Log:
-	loghandler = None
-	sysloghandler = None
-
-	@classmethod
-	def initLogging(cls, c = None):
-		if c:
-			cls.cf = c
-			if cls.cf.loglevel > 5:
-				cls.cf.loglevel = 5
-		else:
-			cls.cf = conf.Config()
-
-		fmt = logging.Formatter("%(asctime)s %(name)s %(levelname)s [%(process)d-%(threadName)s] %(message)s")
-		sfmt = logging.Formatter("%(name)s %(levelname)s [%(process)d-%(threadName)s-%(thread)d] %(message)s")
-
-		cls.logger = logging.getLogger('zmconfigd')
-		cls.logger.setLevel(logging.DEBUG)
-		if (cls.loghandler):
-			cls.logger.removeHandler(cls.loghandler)
-		cls.loghandler = logging.handlers.RotatingFileHandler("/opt/zimbra/log/zmconfigd.log",maxBytes=10000000,backupCount=5)
-		cls.loghandler.setFormatter(fmt)
-		cls.loghandler.setLevel(lvlmap[cls.cf.loglevel])
-		cls.logger.addHandler(cls.loghandler)
-
-		if (cls.sysloghandler):
-			cls.logger.removeHandler(cls.sysloghandler)
-		cls.sysloghandler = logging.handlers.SysLogHandler(('localhost',514),logging.handlers.SysLogHandler.LOG_LOCAL0)
-		cls.sysloghandler.setFormatter(sfmt)
-		cls.sysloghandler.setLevel(logging.CRITICAL)
-
-		cls.logger.addHandler(cls.sysloghandler)
+	zmconfigdSyslogInstance = UnixSocketSyslogConfig(SyslogConstants.FACILITY_LOCAL0, "/dev/log")
+	zmsyslog = Syslog.createInstance("zmSyslog",zmconfigdSyslogInstance)
+	zmsyslog.getConfig().setLocalName("zmconfigd[%d]:" % os.getpid())
 
 	@classmethod
 	def logMsg(cls, lvl, msg):
 
+		cls.cf = conf.Config()
+
 		if lvl > 5:
 			lvl = 5
 		msg = re.sub(r"\s|\n", " ", msg)
-		# print "Logging at %d (%d)" % (lvl, lvlmap[lvl])
-		cls.logger.log( lvlmap[lvl], msg) 
+
+		if lvl <= cls.cf.loglevel:
+			Log.zmsyslog.log(lvl, msg)
 
 		if lvl == 0:
-			cls.logger.log( lvlmap[lvl], "%s: shutting down" % (cls.cf.progname,) )
+			Log.zmsyslog.fatal("%s: shutting down" % (cls.cf.progname,) )
 			os._exit(1)
 
-Log.initLogging()
