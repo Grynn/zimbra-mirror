28550cd63a9c5c22b2b08500493786f3198fc972
Quanah Gibson-Mount <quanah@zimbra.com>
2012-04-18 20:18:02 +0000

    bug: 66950 Update for new unboundID SDK
    http://bugzilla.zimbra.com/show_bug.cgi?id=66950

--- ZimbraServer/src/python/pylibs/ldap.py
+++ ZimbraServer/src/python/pylibs/ldap.py
@@ -13,33 +13,36 @@
 # ***** END LICENSE BLOCK *****
 #
 
-TODO this doesn't work, the jython ldap implementation doesn't support ldapi access.  
-TODO we use the external perl libraries instead.
-
 import conf
 
-from logmsg import *
-from java.util import Hashtable
-import sys
-# Hack for standalone jython bug
-sys.packageManager.makeJavaPackage("javax.naming", "*", None)
-sys.packageManager.makeJavaPackage("javax.naming.directory", "*", None)
-from javax.naming import Context
-from javax.naming.directory import InitialDirContext
+import com.zimbra.cs.ldap.LdapClient;
+import com.zimbra.cs.ldap.LdapUsage;
+import com.zimbra.cs.ldap.ZAttributes;
+import com.zimbra.cs.ldap.ZLdapContext;
+import com.zimbra.cs.ldap.ZLdapFilter;
+import com.zimbra.cs.ldap.ZLdapFilterFactory;
+import com.zimbra.cs.ldap.ZMutableEntry;
+import com.zimbra.cs.ldap.ZSearchControls;
+import com.zimbra.cs.ldap.ZSearchResultEntry;
+import com.zimbra.cs.ldap.ZSearchResultEnumeration;
+import com.zimbra.cs.ldap.ZSearchScope;
+import com.zimbra.cs.ldap.LdapException.LdapSizeLimitExceededException;
+import com.zimbra.cs.ldap.LdapServerConfig.GenericLdapConfig;
+import com.zimbra.cs.ldap.ZLdapFilterFactory.FilterId;
 
 # (Key, DN, requires_master)
 keymap = {
-	"ldap_common_loglevel"		:	("olcLogLevel",		"cn=config", False),
-	"ldap_common_threads"		:	("olcThreads",		"cn=config", False),
-	"ldap_common_toolthreads"	:	("olcToolThreads",	"cn=config", False),
-	"ldap_common_require_tls"	:	("olcSecurity",		"cn=config", False),
-	"ldap_common_writetimeout"	:	("olcWriteTimeout",	"cn=config", False),
+	"ldap_common_loglevel"			:	("olcLogLevel",		"cn=config", False),
+	"ldap_common_threads"			:	("olcThreads",		"cn=config", False),
+	"ldap_common_toolthreads"		:	("olcToolThreads",	"cn=config", False),
+	"ldap_common_require_tls"		:	("olcSecurity",		"cn=config", False),
+	"ldap_common_writetimeout"		:	("olcWriteTimeout",	"cn=config", False),
 
-	"ldap_db_checkpoint"		:	("olcDbCheckpoint",		"olcDatabase={3}mdb,cn=config", False),
+	"ldap_db_checkpoint"			:	("olcDbCheckpoint",		"olcDatabase={3}mdb,cn=config", False),
 	"ldap_db_maxsize"			:	("olcDbMaxsize",			"olcDatabase={3}mdb,cn=config", False),
 
 	"ldap_accesslog_checkpoint"		:	("olcDbCheckpoint",		"olcDatabase={2}mdb,cn=config", True),
-	"ldap_accesslog_maxsize"			:	("olcDbMaxsize",			"olcDatabase={2}mdb,cn=config", True),
+	"ldap_accesslog_maxsize"		:	("olcDbMaxsize",			"olcDatabase={2}mdb,cn=config", True),
 
 	"ldap_overlay_syncprov_checkpoint"	:	("olcSpCheckpoint",	"olcOverlay={0}syncprov,olcDatabase={3}mdb,cn=config", True),
 
@@ -49,7 +52,7 @@ keymap = {
 class Ldap:
 
 	cf = None
-	mLdapContext = None
+	mLdapConfig = None
 	master = False
 
 	@classmethod
@@ -57,39 +60,45 @@ class Ldap:
 		Log.logMsg(4, "Initializing ldap")
 		if c:
 			cls.cf = c
-			cls.createLdapContext()
+			cls.LdapClient.initialize()
 		else:
 			raise Exception("Ldap not initialized")
 
 	@classmethod
-	def createLdapContext(cls):
+	def createLdapConfig(cls):
 		Log.logMsg(5, "Creating ldap context")
 		if cls.cf is None:
 			raise Exception("Ldap not initialized")
 
-		env=Hashtable()
-		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory")
-		env.put(Context.PROVIDER_URL, 'ldapi:///opt/zimbra/openldap/var/run/ldapi/') 
-		env.put(Context.SECURITY_AUTHENTICATION, "simple")
-		env.put(Context.SECURITY_PRINCIPAL, "cn=config")
-		env.put(Context.SECURITY_CREDENTIALS, cls.cf.ldap_root_password)
-		cls.mLdapContext = InitialDirContext(env)
+		ldapUrl = "ldapi:///"
+		bindDN = "cn=config"
+		if cls.cf.ldap_starttls_required=="false":
+			startTLSEnabled = False
+		else
+			startTLSEnabled = True
+		
+		bindPassword = cls.cf.ldap_root_password
+		cls.mLdapConfig = GenericLdapConfig(ldapUrl, startTLSEnabled, bindDN, bindPassword)
+		mLdapContext = LdapClient.getContext(cls.mLdapConfig, LdapUsage.SEARCH)
 
 		if cls.cf.ldap_is_master:
-			#s = SearchControls()
-			#s.setSearchScope(SearchControls.OBJECT_SCOPE)
 			Log.logMsg(5, "Creating ldap context")
-			ats = BasicAttributes("objectClass", None)
-			atr = ['1.1']
-			results = cls.mLdapContext.search("cn=accesslog", ats, atr)
-			if results.hasMore():
+			atbase = "cn=accesslog"
+			atfilter = "(objectClass=*)"
+			atreturn = ['1.1']
+			zfilter = ZLdapFilterFactory.getInstance().fromFilterString(FilterId.ZMCONFIGD, atfilter)
+			searchControls = ZSearchControls.createSearchControls(ZSearchScope.SEARCH_SCOPE_BASE, ZSearchControls.SIZE_UNLIMITED, atreturn);
+			ne = mLdapContext.searchDir(base, zFilter, searchControls);
+			if ne.hasMore():
 				Log.logMsg(5, "Ldap config is master")
 				cls.master = True
 
+		LdapClient.closeContext(mLdapContext)
+
 	@classmethod
 	def getLdapContext(cls):
-		if cls.mLdapContext is None:
-			cls.createLdapContext()
+		if cls.mLdapConfig is None:
+			cls.createLdapConfig()
 		return cls.mLdapContext
 
 	@classmethod
@@ -98,8 +107,11 @@ class Ldap:
 		if attr is not None:
 			v = xform % (value,)
 			Log.logMsg(4, "Setting %s to %s" % (key, v))
-			myAttrs = BasicAttributes(attr, v, True)
-			cls.ctx.modifyAttributes(dn, DirContext.REPLACE_ATTRIBUTE, myAttrs)
+			mLdapContext = LdapClient.getContext(cls.mLdapConfig, LdapUsage.MOD)
+			mEntry = LdapClient.createMutableEntry()
+			mEntry.setAttr(attr, v)
+			mLdapContext.replaceAttributes(dn, mEntry.getAttributes())
+			LdapClient.closeContext(mLdapContext)
 
 	@classmethod
 	def lookupKey(cls, key, master):
