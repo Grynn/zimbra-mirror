6ef4b9c1614e44cb4f032f97b30b69c33dbc03a2
 Unknown Perforce User <charrison>
2012-04-25 20:46:50 +0000

    Bug 64368  submitting the ZWC standard help DRAFT for 8
    http://bugzilla.zimbra.com/show_bug.cgi?id=64368

--- ZimbraWebClient/WebRoot/help/en_US/standard/whstart.js
+++ ZimbraWebClient/WebRoot/help/en_US/standard/whstart.js
@@ -31,7 +31,7 @@ function delayReload()
 	}
 }
 
-var gsToolbarOrder = "toc|idx|fts|blankblock|banner";
+var gsToolbarOrder = "toc|idx|fts|blankblock";
 var gsMinibarOrder = "blankblock|hide2|";
 
 var gsTopic = "Getting_Started/Zimbra_Basic_User_Help.htm";
