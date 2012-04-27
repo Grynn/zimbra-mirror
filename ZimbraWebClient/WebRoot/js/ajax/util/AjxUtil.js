dff0bdb3a676e082dd7c6184b505920adb2f613b
 Unknown Perforce User <rottikunta>
2012-04-27 21:02:14 +0000

    Bug: 72570 - Attachment name in Chinese does not display properly in the bubble
    non-ascii characteters in file name needs to be converted to html unicode characters
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=72570

--- ZimbraWebClient/WebRoot/js/ajax/util/AjxUtil.js
+++ ZimbraWebClient/WebRoot/js/ajax/util/AjxUtil.js
@@ -829,3 +829,28 @@ AjxUtil.get = function(object /* , propName1, ... */) {
     }
     return object;
 };
+
+
+/**
+ *  Convert non-ASCII characters to valid HTML UNICODE entities 
+ * @param {string}
+ * 
+*/
+AjxUtil.convertToEntities = function (source){
+	var result = '', temp, length = 0, i = 0;
+    
+    if (!source || !(length = source.length)) return result;
+    
+	for(i; i < length; ++i){
+		if(source.charCodeAt(i) > 127){
+			temp = source.charCodeAt(i).toString(10);
+			while(temp.length < 4){
+				temp = '0' + temp;
+			}
+			result += '&#' + temp + ';';
+		} else {
+			result += source.charAt(i);
+		}
+	}
+	return result;
+};
