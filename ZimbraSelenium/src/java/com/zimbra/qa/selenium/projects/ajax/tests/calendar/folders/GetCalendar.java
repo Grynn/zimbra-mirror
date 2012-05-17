8aa9f18e4024ab50baaeb3afa696afe3c7d57c90
 Unknown Perforce User <rhoades>
2012-05-17 22:20:29 +0000

    Bug: 28846
    Selenium Harness: add test case for viewing appointments in sub-calendars
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=28846

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/folders/GetCalendar.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/folders/GetCalendar.java
@@ -1,24 +1,21 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.calendar.folders;
 
-import java.util.List;
+import java.util.*;
 
 import org.testng.annotations.Test;
 
-import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.*;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
-import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.core.*;
 
 
-public class GetCalendar extends AjaxCommonTest {
+public class GetCalendar extends CalendarWorkWeekTest {
 
 	public GetCalendar() {
 		logger.info("New "+ GetCalendar.class.getCanonicalName());
-		
-		// All tests start at the login page
-		super.startingPage = app.zPageCalendar;
-		super.startingAccountPreferences = null;
 		
 	}
 	
@@ -109,6 +106,100 @@ public class GetCalendar extends AjaxCommonTest {
 		}
 		ZAssert.assertNotNull(found1, "Verify the calendar was in the tree");
 		ZAssert.assertNotNull(found2, "Verify the sub-calendar was in the tree");
+
+		
+	}	
+
+	@Bugs(ids = "28846")
+	@Test(	description = "Get a calendar (under subcalendar) - verify appointments appear",
+			groups = { "functional" })
+	public void GetCalendar_03() throws HarnessException {
+		
+		FolderItem root = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.UserRoot);
+		ZAssert.assertNotNull(root, "Verify the inbox is available");
+		
+		// Create a subfolder
+		String name1 = "calendar" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name1 +"' l='"+ root.getId() +"' view='appointment'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subfolder1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder1, "Verify the subfolder is available");
+		
+		app.zGetActiveAccount().soapSend(
+				"<FolderActionRequest xmlns='urn:zimbraMail'>" +
+                	"<action op='check' id='"+ subfolder1.getId() +"'/>" +
+                "</FolderActionRequest>");
+		
+		// Create a subfolder of the subfolder
+		String name2 = "calendar" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+				"<CreateFolderRequest xmlns='urn:zimbraMail'>" +
+                	"<folder name='"+ name2 +"' l='"+ subfolder1.getId() +"' view='appointment'/>" +
+                "</CreateFolderRequest>");
+
+		FolderItem subfolder2 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name2);
+		ZAssert.assertNotNull(subfolder2, "Verify the subfolder is available");
+		
+		app.zGetActiveAccount().soapSend(
+				"<FolderActionRequest xmlns='urn:zimbraMail'>" +
+                	"<action op='check' id='"+ subfolder2.getId() +"'/>" +
+                "</FolderActionRequest>");
+		
+		
+		// Create the appointment in the subfolder
+		String subject = "appointment" + ZimbraSeleniumProperties.getUniqueString();
+		String location = "location" + ZimbraSeleniumProperties.getUniqueString();
+		String content = "content" + ZimbraSeleniumProperties.getUniqueString();
+		
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		// EST timezone string
+		String tz = ZTimeZone.TimeZoneEST.getID();
+
+		// Create an appointment in the subcalendar
+		app.zGetActiveAccount().soapSend(
+					"<CreateAppointmentRequest xmlns='urn:zimbraMail'>"
+				+		"<m l='"+ subfolder2.getId() +"'>"
+				+			"<inv>"
+				+				"<comp status='CONF' fb='B' class='PUB' transp='O' allDay='0' name='"+ subject +"' loc='"+ location +"' >"
+				+					"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>"
+				+					"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>"
+				+					"<or a='"+ app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"</comp>"
+				+			"</inv>"
+				+			"<su>"+ subject + "</su>"
+				+			"<mp ct='text/plain'>"
+				+				"<content>"+ content +"</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateAppointmentRequest>");
+		
+
+		// Click on Get Mail to refresh the folder list
+		app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+		
+		//-- Verification
+		
+	    //verify appt displayed in workweek view
+		boolean found = false;
+		List<AppointmentItem> items = app.zPageCalendar.zListGetAppointments();
+		for (AppointmentItem item : items ) {
+			if ( subject.equals(item.getSubject()) ) {
+				found = true;
+				break;
+			}
+		}
+		
+		ZAssert.assertTrue(found, "Verify appt gets displayed in work week view");
 
 		
 	}	
