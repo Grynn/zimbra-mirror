fede179d204c61b6952c685da10f0a4cc550ff03
Hem Aravind Eswaran <heswaran@zimbra.com>
2012-05-18 05:46:56 +0000

    Bug:70964
    TinyMCE Internationalization done.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=70964

--- ZimbraWebClient/WebRoot/js/ajax/package/TinyMCE.js
+++ ZimbraWebClient/WebRoot/js/ajax/package/TinyMCE.js
@@ -1,6 +1,7 @@
 AjxPackage.require("ajax.3rdparty.tinymce.tiny_mce_src");
 AjxPackage.require("ajax.3rdparty.tinymce.themes.advanced.editor_template_src");
 AjxPackage.require("ajax.3rdparty.tinymce.themes.advanced.Zmeditor_template");
+AjxPackage.require("ajax.3rdparty.tinymce.langs.en");
 AjxPackage.require("ajax.3rdparty.tinymce.themes.advanced.langs.en");
 AjxPackage.require("ajax.3rdparty.tinymce.plugins.autolink.editor_plugin_src");
 AjxPackage.require("ajax.3rdparty.tinymce.plugins.advlist.editor_plugin_src");
