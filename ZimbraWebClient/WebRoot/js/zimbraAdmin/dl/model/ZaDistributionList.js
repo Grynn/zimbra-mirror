43156c0f6b1d53d81a98686b04608c763e0c47fc
 Unknown Perforce User <wgan>
2012-05-21 08:52:17 +0000

    bug 72804        we should use ZaXFormViewController.prototype._findAlias()
           https://reviewboard.eng.vmware.com/r/339227/	
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=72804

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/dl/model/ZaDistributionList.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/dl/model/ZaDistributionList.js
@@ -434,44 +434,45 @@ ZaDistributionList.addRemoveAliases = function (mods, obj) {
 		if(ZaItem.hasRight(ZaDistributionList.ADD_DL_ALIAS_RIGHT, this)) {
 			try {
 				for(var ix=0; ix < tmpObjCnt; ix++) {
+					var aliasName = obj.attrs[ZaAccount.A_zimbraMailAlias][ix];
 					try {
-						if(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
-							if(obj.attrs[ZaAccount.A_zimbraMailAlias][ix].indexOf("@") != obj.attrs[ZaAccount.A_zimbraMailAlias][ix].lastIndexOf("@")) {
+						if(aliasName) {
+							if(aliasName.indexOf("@") != aliasName.lastIndexOf("@")) {
 								//show error msg
-								ZaApp.getInstance().getCurrentController()._errorDialog.setMessage(AjxMessageFormat.format(ZaMsg.ERROR_ALIAS_INVALID,[obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]), null, DwtMessageDialog.CRITICAL_STYLE, null);
+								ZaApp.getInstance().getCurrentController()._errorDialog.setMessage(AjxMessageFormat.format(ZaMsg.ERROR_ALIAS_INVALID,[aliasName]), null, DwtMessageDialog.CRITICAL_STYLE, null);
 								ZaApp.getInstance().getCurrentController()._errorDialog.popup();		
 								break;						
 							}						
-							this.addAlias(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]);
+							this.addAlias(aliasName);
 						}
 					} catch (ex) {
-						if(ex.code == ZmCsfeException.ACCT_EXISTS) {
+						if((ex.code == ZmCsfeException.ACCT_EXISTS) && aliasName) {
 							//if failed because account exists just show a warning
-							var account = this._findAlias(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]);
+							var account = ZaXFormViewController.prototype._findAlias.call(this, aliasName);
 							switch(account.type) {
 								case ZaItem.DL:
-									if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+									if(account.name == aliasName) {
 										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS3,[account.name]);								
 									} else {
-										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS4,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS4,[account.name, aliasName]);
 									}
 								break;
 								case ZaItem.ACCOUNT:
-									if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+									if(account.name == aliasName) {
 										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS2,[account.name]);								
 									} else {
-										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS1,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS1,[account.name, aliasName]);
 									}							
 								break;	
 								case ZaItem.RESOURCE:
-									if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+									if(account.name == aliasName) {
 										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS5,[account.name]);								
 									} else {
-										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS6,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+										failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS6,[account.name, aliasName]);
 									}							
 								break;							
 								default:
-									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS0,[obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);							
+									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS0,[aliasName]);
 								break;
 							}
 							failedAliasesCnt++;
@@ -514,44 +515,45 @@ ZaDistributionList.addAliases = function (obj, dl) {
 		var failedAliasesCnt = 0;
 		try {
 			for(var ix=0; ix < tmpObjCnt; ix++) {
+				var aliasName = obj.attrs[ZaAccount.A_zimbraMailAlias][ix];
 				try {
-					if(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
-						if(obj.attrs[ZaAccount.A_zimbraMailAlias][ix].indexOf("@") != obj.attrs[ZaAccount.A_zimbraMailAlias][ix].lastIndexOf("@")) {
+					if(aliasName) {
+						if(aliasName.indexOf("@") != aliasName.lastIndexOf("@")) {
 							//show error msg
-							ZaApp.getInstance().getCurrentController()._errorDialog.setMessage(AjxMessageFormat.format(ZaMsg.ERROR_ALIAS_INVALID,[obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]), null, DwtMessageDialog.CRITICAL_STYLE, null);
+							ZaApp.getInstance().getCurrentController()._errorDialog.setMessage(AjxMessageFormat.format(ZaMsg.ERROR_ALIAS_INVALID,[aliasName]), null, DwtMessageDialog.CRITICAL_STYLE, null);
 							ZaApp.getInstance().getCurrentController()._errorDialog.popup();		
 							break;						
 						}						
-						dl.addAlias(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]);
+						dl.addAlias(aliasName);
 					}
 				} catch (ex) {
-					if(ex.code == ZmCsfeException.ACCT_EXISTS) {
+					if((ex.code == ZmCsfeException.ACCT_EXISTS) && aliasName) {
 						//if failed because account exists just show a warning
-						var account = this._findAlias(obj.attrs[ZaAccount.A_zimbraMailAlias][ix]);
+						var account =  ZaXFormViewController.prototype._findAlias.call(this, aliasName);
 						switch(account.type) {
 							case ZaItem.DL:
-								if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+								if(account.name == aliasName) {
 									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS3,[account.name]);								
 								} else {
-									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS4,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS4,[account.name, aliasName]);
 								}
 							break;
 							case ZaItem.ACCOUNT:
-								if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+								if(account.name == aliasName) {
 									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS2,[account.name]);								
 								} else {
-									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS1,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS1,[account.name, aliasName]);
 								}							
 							break;	
 							case ZaItem.RESOURCE:
-								if(account.name == obj.attrs[ZaAccount.A_zimbraMailAlias][ix]) {
+								if(account.name == aliasName) {
 									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS5,[account.name]);								
 								} else {
-									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS6,[account.name, obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);								
+									failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS6,[account.name, aliasName]);
 								}							
 							break;							
 							default:
-								failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS0,[obj.attrs[ZaAccount.A_zimbraMailAlias][ix]]);							
+								failedAliases += "<br>" +AjxMessageFormat.format(ZaMsg.WARNING_EACH_ALIAS0,[aliasName]);
 							break;
 						}
 						failedAliasesCnt++;
