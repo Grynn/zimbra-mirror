734ba282e785c63affe32462e1ecd4838a0fee3c
 Unknown Perforce User <jwagner>
2012-05-22 15:45:34 +0000

    Bug: 71108 Chinese locales zh_CN and zh_HK  appear in English override getCandidateLocales for zh_CN & zh_HK
    http://bugzilla.zimbra.com/show_bug.cgi?id=71108

--- ZimbraWebClient/src/com/zimbra/kabuki/servlets/Props2JsServlet.java
+++ ZimbraWebClient/src/com/zimbra/kabuki/servlets/Props2JsServlet.java
@@ -350,9 +350,6 @@ public class Props2JsServlet extends HttpServlet {
                     }
                 });
                 for (File file : loader.getFiles()) {
-                    if (locale.getLanguage() == "zh") {
-                        out.writeBytes("// file: " + getCommentSafeString(file.getName()) + '\n');    
-                    }
                     Props2Js.convert(out, file, classname);
                 }
             } catch (MissingResourceException e) {
