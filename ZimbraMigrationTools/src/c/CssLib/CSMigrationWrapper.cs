ab90467beae4bcc812e8e6c8c5b1a99ccb8dfcf9
Karuna Nuthi <knuthi@zimbra.com>
2012-05-30 22:36:26 +0000

    bug:70698
    Verify the bit compatibility of mapi and migration dll before loading the migration dll.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=70698

--- ZimbraMigrationTools/src/c/CssLib/CSMigrationWrapper.cs
+++ ZimbraMigrationTools/src/c/CssLib/CSMigrationWrapper.cs
@@ -97,92 +97,6 @@ public class MigrationOptions
 }
 
 
-public class CompatibilityChk
-{
-
-
-    public enum MachineType : ushort
-    {
-        IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
-        IMAGE_FILE_MACHINE_AM33 = 0x1d3,
-        IMAGE_FILE_MACHINE_AMD64 = 0x8664,
-        IMAGE_FILE_MACHINE_ARM = 0x1c0,
-        IMAGE_FILE_MACHINE_EBC = 0xebc,
-        IMAGE_FILE_MACHINE_I386 = 0x14c,
-        IMAGE_FILE_MACHINE_IA64 = 0x200,
-        IMAGE_FILE_MACHINE_M32R = 0x9041,
-        IMAGE_FILE_MACHINE_MIPS16 = 0x266,
-        IMAGE_FILE_MACHINE_MIPSFPU = 0x366,
-        IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,
-        IMAGE_FILE_MACHINE_POWERPC = 0x1f0,
-        IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,
-        IMAGE_FILE_MACHINE_R4000 = 0x166,
-        IMAGE_FILE_MACHINE_SH3 = 0x1a2,
-        IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,
-        IMAGE_FILE_MACHINE_SH4 = 0x1a6,
-        IMAGE_FILE_MACHINE_SH5 = 0x1a8,
-        IMAGE_FILE_MACHINE_THUMB = 0x1c2,
-        IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169,
-    }
-    public static MachineType GetDllMachineType(string dllPath)
-    {
-        // http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx 
-
-        FileStream fs = new FileStream(dllPath, FileMode.Open, FileAccess.Read);
-        BinaryReader br = new BinaryReader(fs);
-        fs.Seek(0x3c, SeekOrigin.Begin);
-        Int32 peOffset = br.ReadInt32();
-        fs.Seek(peOffset, SeekOrigin.Begin);
-        UInt32 peHead = br.ReadUInt32();
-        if (peHead != 0x00004550) // "PE\0\0", little-endian 
-            throw new Exception("Can't find PE header");
-        MachineType machineType = (MachineType)br.ReadUInt16();
-        br.Close();
-        fs.Close();
-        return machineType;
-    }
-
-    // returns true if the dll is 64-bit, false if 32-bit, and null if unknown 
-    public static bool? UnmanagedDllIs64Bit(string dllPath)
-    {
-        switch (GetDllMachineType(dllPath))
-        {
-        case MachineType.IMAGE_FILE_MACHINE_AMD64:
-        case MachineType.IMAGE_FILE_MACHINE_IA64:
-            return true;
-        case MachineType.IMAGE_FILE_MACHINE_I386:
-            return false;
-        default:
-            return null;
-        }
-    }
-
-
-    public static string CheckCompat(string path)
-    {
-        string status = "";
-        string absolutepath = Path.GetFullPath("Exchange.dll");
-
-        bool retval = UnmanagedDllIs64Bit(absolutepath).Value;
-
-
-        string Bitness = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\14.0\Outlook", "Bitness", null);
-        if (Bitness != null)
-        {
-            if ((Bitness == "x64") && (!retval))
-            {
-
-                status = "Outlook is 64 bit and migration is 32 bit";
-
-            }
-
-        }
-        return status;
-
-    }
-
-
-}
 
 
 public class CSMigrationWrapper
@@ -198,7 +112,7 @@ public class CSMigrationWrapper
         set { m_MailClient = value; }
     }
     
-    dynamic MailWrapper;
+    dynamic MailWrapper = null;
 
     dynamic m_umUser = null; // used to store user object so Exit can do a user.Uninit
     public dynamic UmUser {
@@ -208,21 +122,22 @@ public class CSMigrationWrapper
 
     public CSMigrationWrapper(string mailClient)
     {
-        try
+       
         {
             InitLogFile("migration", Log.Level.Trace);
             Log.info("Initializing migration");
+
+            
             MailClient = mailClient;
             if (MailClient == "MAPI")
             {
-                //MailWrapper = new Exchange.MapiWrapper();
+               
                 MailWrapper = new MapiMigration();
+        
+              
             }
         }
-        catch (Exception e)
-        {
-            Log.err("Exception in CSMigrationWrapper construcor", e.Message);
-        }
+      
     }
 
     public string GlobalInit(string Target, string AdminUser, string AdminPassword)
--- ZimbraMigrationTools/src/c/CssLib/MapiMigration.cs
+++ ZimbraMigrationTools/src/c/CssLib/MapiMigration.cs
@@ -2,15 +2,157 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace CssLib
 {
+    public class CompatibilityChk
+    {
+
+
+        public enum MachineType : ushort
+        {
+            IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
+            IMAGE_FILE_MACHINE_AM33 = 0x1d3,
+            IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+            IMAGE_FILE_MACHINE_ARM = 0x1c0,
+            IMAGE_FILE_MACHINE_EBC = 0xebc,
+            IMAGE_FILE_MACHINE_I386 = 0x14c,
+            IMAGE_FILE_MACHINE_IA64 = 0x200,
+            IMAGE_FILE_MACHINE_M32R = 0x9041,
+            IMAGE_FILE_MACHINE_MIPS16 = 0x266,
+            IMAGE_FILE_MACHINE_MIPSFPU = 0x366,
+            IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,
+            IMAGE_FILE_MACHINE_POWERPC = 0x1f0,
+            IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,
+            IMAGE_FILE_MACHINE_R4000 = 0x166,
+            IMAGE_FILE_MACHINE_SH3 = 0x1a2,
+            IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,
+            IMAGE_FILE_MACHINE_SH4 = 0x1a6,
+            IMAGE_FILE_MACHINE_SH5 = 0x1a8,
+            IMAGE_FILE_MACHINE_THUMB = 0x1c2,
+            IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169,
+        }
+        public static MachineType GetDllMachineType(string dllPath)
+        {
+            // http://www.microsoft.com/whdc/system/platform/firmware/PECOFF.mspx 
+
+            FileStream fs = new FileStream(dllPath, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            fs.Seek(0x3c, SeekOrigin.Begin);
+            Int32 peOffset = br.ReadInt32();
+            fs.Seek(peOffset, SeekOrigin.Begin);
+            UInt32 peHead = br.ReadUInt32();
+            if (peHead != 0x00004550) // "PE\0\0", little-endian 
+                throw new Exception("Can't find PE header");
+            MachineType machineType = (MachineType)br.ReadUInt16();
+            br.Close();
+            fs.Close();
+            return machineType;
+        }
+
+        // returns true if the dll is 64-bit, false if 32-bit, and null if unknown 
+        public static bool? UnmanagedDllIs64Bit(string dllPath)
+        {
+            switch (GetDllMachineType(dllPath))
+            {
+                case MachineType.IMAGE_FILE_MACHINE_AMD64:
+                case MachineType.IMAGE_FILE_MACHINE_IA64:
+                    return true;
+                case MachineType.IMAGE_FILE_MACHINE_I386:
+                    return false;
+                default:
+                    return null;
+            }
+        }
+
+
+        public static string CheckCompat(string path)
+        {
+            string status = "";
+            string absolutepath = Path.GetFullPath("Exchange.dll");
+
+            bool retval = UnmanagedDllIs64Bit(absolutepath).Value;
+
+
+            string Bitness = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\14.0\Outlook", "Bitness", null);
+            if (Bitness != null)
+            {
+                if ((Bitness == "x64") && (!retval))
+                {
+
+                    status = "Outlook is 64 bit and migration is 32 bit";
+
+                }
+
+            }
+            return status;
+
+        }
+
+
+    }
+
     public class MapiMigration : MailMigration
     {
-        public dynamic MapiWrapper;
+       // public dynamic MapiWrapper;
+        public Exchange.MapiWrapper MapiWrapper;
+        static public string  checkPrereqs()
+        {
+            string str = "";
+            string path  = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+            string absolutepath = Path.GetFullPath("Exchange.dll");
+
+            bool bitness = CompatibilityChk.UnmanagedDllIs64Bit(absolutepath).Value;
+            if (bitness)
+            {
+                path = path + @"Program Files\Common Files\System\MSMAPI\1033";
+                if (System.IO.Directory.Exists(path))
+                {
+                   
+                }
+                else
+                {
+                    str = "MAPI and Migration tool are not compatabile versions .check the bitness of outlook and Migration tool";
+                }
+            }
+            else
+            {
+                 
+                
+                 path = path + @"Program Files (x86)\Common Files\System\MSMAPI\1033";
+
+               // path =@" C:\Program Files (x86)\Common Files\System\MSMAPI\1033";
+                if (System.IO.Directory.Exists(path))// (System.IO.File.Exists(@"ProgramFiles(x86)\\CommonFiles\\System\\msmapi\\1033\\msmapi32.dll"))
+                {
+                   
+                }
+                else
+                {
+                    str = "Outlook and Migration are not comaptbile.check the bitness fo the apps";
+                }
+            }
+
+
+            return str;
+
+        }
         public  MapiMigration()
         {
-             MapiWrapper = new Exchange.MapiWrapper();
+            string message = MapiMigration.checkPrereqs();
+            if (message == "")
+            {
+
+                MapiWrapper = new Exchange.MapiWrapper();
+            }
+            else
+            {
+                Log.err("Exception in CSMigrationWrapper construcor", message);
+                throw new Exception(message);
+            }
+            
+
+             
         }
 
         public override string GlobalInit(string Target, string AdminUser, string AdminPassword)
@@ -39,7 +181,7 @@ namespace CssLib
             }
             catch (Exception e)
             {
-                string msg = string.Format("GetListofMapiProfiles Exception: {0}", e.Message);
+                string msg = string.Format("GlobalUninit Exception: {0}", e.Message);
                 return msg;
             }
 
