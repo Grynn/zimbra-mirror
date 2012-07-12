f9ef50b7a7a1499b52b94c6c4b5869a2a2006593
 Unknown Perforce User <gsolovyev>
2012-07-12 06:30:58 +0000

    bug 75957 adapt certificate extension to new framework
    http://bugzilla.zimbra.com/show_bug.cgi?id=75957

--- ZimbraAdminExt/CertificateMgr/js/ZaCertsServerListController.js
+++ ZimbraAdminExt/CertificateMgr/js/ZaCertsServerListController.js
@@ -21,17 +21,13 @@
 **/
 ZaCertsServerListController = function(appCtxt, container) {
 	ZaListViewController.call(this, appCtxt, container, "ZaCertsServerListController");
-   	this._toolbarOperations = new Array();
    	this._popupOperations = new Array();			
-	
-	//TODO helpURL needs to be changed
 	this._helpURL = location.pathname + "help/admin/html/tools/creating_certificates.htm?locid=" + AjxEnv.DEFAULT_LOCALE;	
 }
 
 ZaCertsServerListController.prototype = new ZaListViewController();
 ZaCertsServerListController.prototype.constructor = ZaCertsServerListController;
-
-ZaController.initToolbarMethods["ZaCertsServerListController"] = new Array();
+ZaController.changeActionsStateMethods["ZaCertsServerListController"] = new Array();
 ZaController.initPopupMenuMethods["ZaCertsServerListController"] = new Array();
 
 /**
@@ -52,15 +48,10 @@ function(list, openInNewTab) {
 	this.changeActionsState();	
 }
 
-ZaCertsServerListController.initToolbarMethod =
-function () {
-}
-ZaController.initToolbarMethods["ZaCertsServerListController"].push(ZaCertsServerListController.initToolbarMethod);
-
 ZaCertsServerListController.initPopupMenuMethod =
 function () {
-   	this._popupOperations.push(new ZaOperation(ZaOperation.VIEW, com_zimbra_cert_manager.TBB_view_cert, com_zimbra_cert_manager.TBB_view_cert_tt, "ViewCertificate", "ViewCertificate", new AjxListener(this, ZaCertsServerListController.prototype.viewCertListener)));	
-   	this._popupOperations.push(new ZaOperation(ZaOperation.NEW, com_zimbra_cert_manager.TBB_launch_cert_wizard, com_zimbra_cert_manager.TBB_launch_cert_wizard_tt, "InstallCertificate", "InstallCertificate", new AjxListener(this, ZaCertsServerListController.prototype._newCertListener)));				
+   	this._popupOperations[ZaOperation.VIEW] = new ZaOperation(ZaOperation.VIEW, com_zimbra_cert_manager.TBB_view_cert, com_zimbra_cert_manager.TBB_view_cert_tt, "ViewCertificate", "ViewCertificate", new AjxListener(this, ZaCertsServerListController.prototype.viewCertListener));	
+   	this._popupOperations[ZaOperation.NEW] = new ZaOperation(ZaOperation.NEW, com_zimbra_cert_manager.TBB_launch_cert_wizard, com_zimbra_cert_manager.TBB_launch_cert_wizard_tt, "InstallCertificate", "InstallCertificate", new AjxListener(this, ZaCertsServerListController.prototype._newCertListener));				
 }
 ZaController.initPopupMenuMethods["ZaCertsServerListController"].push(ZaCertsServerListController.initPopupMenuMethod);
 
@@ -68,16 +59,11 @@ ZaCertsServerListController.prototype._createUI = function () {
 	try {
 		var elements = new Object();
 		this._contentView = new ZaCertsServerListView(this._container);
-		this._initToolbar();
-		if(this._toolbarOperations && this._toolbarOperations.length) {
-			this._toolbar = new ZaToolBar(this._container, this._toolbarOperations);
-		}
 		this._initPopupMenu();
 		if(this._popupOperations && this._popupOperations.length) {
 			this._actionMenu =  new ZaPopupMenu(this._contentView, "ActionMenu", null, this._popupOperations);
 		}
 		elements[ZaAppViewMgr.C_APP_CONTENT] = this._contentView;
-		//ZaApp.getInstance().createView(ZaZimbraAdmin._SERVERS_LIST_VIEW, elements);
         ZaApp.getInstance().getAppViewMgr().createView(this.getContentViewId(), elements);
 		this._contentView.addSelectionListener(new AjxListener(this, this._listSelectionListener));
 		this._contentView.addActionListener(new AjxListener(this, this._listActionListener));			
@@ -110,13 +96,9 @@ function(ev) {
                                     
 ZaCertsServerListController.prototype.viewCertListener = function (ev) {
 	if(window.console && window.console.log) console.log("View the certificates ... ") ;
-	ZaApp.getInstance().getCertViewController().show(
-		ZaCert.getCerts(ZaApp.getInstance(), this._selectedItem.id),
-		this._selectedItem.id) ;
-    if (appNewUI) {
-                var parentPath = ZaTree.getPathByArray([ZaMsg.OVP_home, ZaMsg.OVP_configure, com_zimbra_cert_manager.OVP_certs]);
-                ZaZimbraAdmin.getInstance().getOverviewPanelController().addObjectItem(parentPath, this._selectedItem.name, null, false, false, this._selectedItem);
-            }
+	ZaApp.getInstance().getCertViewController().show(ZaCert.getCerts(ZaApp.getInstance(), this._selectedItem.id),this._selectedItem.id);
+    var parentPath = ZaTree.getPathByArray([ZaMsg.OVP_home, ZaMsg.OVP_configure, com_zimbra_cert_manager.OVP_certs]);
+    ZaZimbraAdmin.getInstance().getOverviewPanelController().addObjectItem(parentPath, this._selectedItem.name, null, false, false, this._selectedItem);
 }
 
 /**
@@ -126,16 +108,14 @@ ZaCertsServerListController.prototype.viewCertListener = function (ev) {
 ZaCertsServerListController.prototype._listSelectionListener =
 function(ev) {
 	if(ev.item) {
-			this._selectedItem = ev.item;
+		this._selectedItem = ev.item;
 	}
 	if (ev.detail == DwtListView.ITEM_DBL_CLICKED) {
 			ZaApp.getInstance().getCertViewController().show(
 				ZaCert.getCerts(ZaApp.getInstance(), this._selectedItem.id),
 				this._selectedItem.id);
-            if (appNewUI) {
-                var parentPath = ZaTree.getPathByArray([ZaMsg.OVP_home, ZaMsg.OVP_configure, com_zimbra_cert_manager.OVP_certs]);
-                ZaZimbraAdmin.getInstance().getOverviewPanelController().addObjectItem(parentPath, ev.item.name, null, false, false, ev.item);
-            }
+            var parentPath = ZaTree.getPathByArray([ZaMsg.OVP_home, ZaMsg.OVP_configure, com_zimbra_cert_manager.OVP_certs]);
+            ZaZimbraAdmin.getInstance().getOverviewPanelController().addObjectItem(parentPath, ev.item.name, null, false, false, ev.item);
 	} else {
 		this.changeActionsState();	
 	}
@@ -148,7 +128,7 @@ function (ev) {
 	this._actionMenu.popup(0, ev.docX, ev.docY);
 }
 
-ZaCertsServerListController.prototype.changeActionsState = 
+ZaCertsServerListController.changeActionsStateMethod = 
 function () {
 	if(this._contentView) {
 		if(this._popupOperations[ZaOperation.NEW]) {
@@ -159,4 +139,8 @@ function () {
 			this._popupOperations[ZaOperation.VIEW].enabled = (cnt == 1);		
 		}
 	}
+}
+
+if(ZaController.changeActionsStateMethods["ZaCertsServerListController"]) {
+    ZaController.changeActionsStateMethods["ZaCertsServerListController"].push(ZaCertsServerListController.changeActionsStateMethod);
 }
