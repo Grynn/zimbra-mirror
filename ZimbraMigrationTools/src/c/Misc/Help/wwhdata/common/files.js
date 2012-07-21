54f18baeaa664ecb16856a6c7acd4f07a2de7fe3
Gayle Neumann <gneumann@zimbra.com>
2012-07-21 00:01:11 +0000

    Bug 64368 Submit Migration Tool help pages
    http://bugzilla.zimbra.com/show_bug.cgi?id=64368

--- ZimbraMigrationTools/src/c/Misc/Help/wwhdata/common/files.js
+++ ZimbraMigrationTools/src/c/Misc/Help/wwhdata/common/files.js
@@ -1,12 +1,13 @@
 function  WWHBookData_Files(P)
 {
 P.fA("Starting the Migration","welcome.html");
-P.fA("Destination Server Configuration","destination_server.html");
-P.fA("Destination Server Configuration","destination_user.html");
-P.fA("Scheduling the Migration","migrate.html");
-P.fA("Selecting Options to Migrate","options.html");
-P.fA("Viewing Migration Results","results.html");
 P.fA("Server Migration Source Information","source_server.html");
 P.fA("User Migration Source Information","source_user.html");
+P.fA("Destination Server Configuration","destination_server.html");
+P.fA("Destination Server Configuration","destination_user.html");
+P.fA("Selecting Options to Migrate","options_server.html");
+P.fA("Selecting Options to Migrate","options_user.html");
 P.fA("Selecting Users to Migrate","users.html");
+P.fA("Scheduling the Migration","migrate.html");
+P.fA("Viewing Migration Results","results.html");
 }
