fe026b74fd0e3e6d5b86bfb51160c9f5a0ce8905
Jitesh Sojitra <jsojitra@zimbra.com>
2012-09-12 06:49:33 +0000

    bug:50432 - Adding selenium test for bug 50432 - Unwanted navigate away dialog after opening appointment/task
    http://bugzilla.zimbra.com/show_bug.cgi?id=50432

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/bugs/Bug50432.java
@@ -0,0 +1,93 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.bugs;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
+import com.zimbra.qa.selenium.framework.util.ZTimeZone;
+import com.zimbra.qa.selenium.framework.util.ZimbraAdminAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+
+@SuppressWarnings("unused")
+public class Bug50432 extends AjaxCommonTest {
+
+	public Bug50432() {
+		logger.info("New " + Bug50432.class.getCanonicalName());
+		
+		this.startingAccountPreferences.put("zimbraPrefWarnOnExit", "TRUE");
+
+		// All tests start at the Calendar page
+		super.startingPage = app.zPageCalendar;
+		
+		// Make sure we are using an account with day view
+		super.startingAccountPreferences = new HashMap<String, String>() {
+			private static final long serialVersionUID = -2913827779459595178L;
+		{
+		    put("zimbraPrefCalendarInitialView", "workWeek");
+		}};
+	}
+
+	@Test(
+			description = "Bug 50432 - Unwanted navigate away dialog after opening appointment/task", 
+			groups = { "functional" })
+	public void Bug50432_01() throws HarnessException {
+		
+		// Modify the test account to disable GAL
+		ZimbraAdminAccount.GlobalAdmin().soapSend(
+				"<ModifyAccountRequest xmlns='urn:zimbraAdmin'>"
+			+		"<id>"+ app.zGetActiveAccount().ZimbraId +"</id>"
+			+		"<a n='zimbraPrefWarnOnExit'>TRUE</a>"
+			+	"</ModifyAccountRequest>");
+
+		// Logout and login to pick up the changes
+		app.zPageLogin.zNavigateTo();
+		this.startingPage.zNavigateTo();
+
+		// Creating object for appointment data
+		AppointmentItem appt = new AppointmentItem();
+		String tz, apptSubject, apptBody;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		apptBody = ZimbraSeleniumProperties.getUniqueString();
+		
+		// Absolute dates in UTC zone
+        Calendar now = Calendar.getInstance();
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+        app.zGetActiveAccount().soapSend(
+                          "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                               "<m>"+
+                               "<inv method='REQUEST' type='event' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                               "<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                               "<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                               "<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                               "</inv>" +
+                               "<mp content-type='text/plain'>" +
+                               "<content>"+ apptBody +"</content>" +
+                               "</mp>" +
+                               "<su>"+ apptSubject +"</su>" +
+                               "</m>" +
+                         "</CreateAppointmentRequest>");
+
+        String apptId = app.zGetActiveAccount().soapSelectValue("//mail:CreateAppointmentResponse", "apptId");
+    
+        // Switch to day view
+        app.zPageCalendar.zToolbarPressPulldown(Button.B_LISTVIEW, Button.O_LISTVIEW_DAY);
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+        // Open appointment & close it
+        app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+		FormApptNew apptForm = (FormApptNew) new FormApptNew(app).zToolbarPressButton(Button.B_CLOSE);
+		// if logout stucks then assume that browser dialog appeared
+		app.zPageMain.zLogout();
+	}
+}
