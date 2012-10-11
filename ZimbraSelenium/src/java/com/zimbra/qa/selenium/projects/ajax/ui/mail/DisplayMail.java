1d6d57792b89453961d05df22aeee047233e3c53
 Unknown Perforce User <girishb>
2012-10-11 11:30:55 +0000

    bug:none
    --Remove extra spaces and warnings

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/mail/DisplayMail.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/mail/DisplayMail.java
@@ -3,17 +3,12 @@ package com.zimbra.qa.selenium.projects.ajax.ui.mail;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.testng.Assert;
-
-
 import com.zimbra.qa.selenium.framework.items.AttachmentItem;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.framework.util.staf.Stafpostqueue;
 import com.zimbra.qa.selenium.projects.ajax.ui.*;
-import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
-import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Locators;
 
 /**
  * The <code>DisplayMail<code> object defines a read-only view of a message
