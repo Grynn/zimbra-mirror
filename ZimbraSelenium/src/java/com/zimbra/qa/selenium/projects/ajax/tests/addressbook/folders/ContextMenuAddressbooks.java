c6f0871f2d4e4952d8227a0f7cb92d818ff1b9a7
 Unknown Perforce User <rhoades>
2012-10-12 21:18:19 +0000

    Bug: none
    Selenium Harness: fix context menu test cases

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/folders/ContextMenuAddressbooks.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/addressbook/folders/ContextMenuAddressbooks.java
@@ -1,13 +1,13 @@
 package com.zimbra.qa.selenium.projects.ajax.tests.addressbook.folders;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import com.zimbra.qa.selenium.framework.items.*;
 import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
-import com.zimbra.qa.selenium.projects.ajax.ui.addressbook.DialogEditFolder;
+
 
 
 
@@ -29,68 +29,68 @@ public class ContextMenuAddressbooks extends AjaxCommonTest {
 	
 
 				
-	@Test(	description = "Cannot delete an addressbook system folder- Right click, Delete",
-					groups = { "smoke" })
-	public void SystemFoldersDeleteButtonDisabledFromContextmenu() throws HarnessException {
-		
-		FolderItem folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Contacts);
-		ZAssert.assertNotNull(folder, "Verify can get the Contacts ");
-											
-		// Delete the folder using context menu
-		AbsPage page= app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_DELETE, folder);
-
-		// Verify Delete option is disabled
-		ZAssert.assertNull(page, "Verify Delete option is disabled");
-		
-
-        folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.EmailedContacts);
-		ZAssert.assertNotNull(folder, "Verify can get the EmailedContacts ");
-											
-		// Delete the folder using context menu
-		page= app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_DELETE, folder);
-
-		// Verify Delete option is disabled
-		ZAssert.assertNull(page, "Verify Delete option is disabled");
-
-		folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Trash);
-		ZAssert.assertNotNull(folder, "Verify can get the Trash ");
-											
-		// Delete the folder using context menu
-		page= app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_DELETE, folder);
-
-		// Verify Delete option is disabled
-		ZAssert.assertNull(page, "Verify Delete option is disabled");
-
-	}	
-
-	
-	@Test(	
-			description = "Cannot rename an addressbook system folder- Right Click - Edit Properties - verify input name not displayed",	
-			groups = { "functional" }
+	@Test(
+			description = "Cannot delete an addressbook system folder- Right click, Delete",
+			groups = { "functional" },
+			dataProvider = "DataProviderSystemFolders"
 			)
-	public void CannotRenameSystemFolders() throws HarnessException {
-
-		FolderItem folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Contacts);
-		ZAssert.assertNotNull(folder, "Verify can get the Contacts ");	
-
-		// Rename the folder 
-		DialogEditFolder dialog = (DialogEditFolder)app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_TREE_EDIT, folder);
-		dialog.zClickButton(Button.B_OK);
-
-		folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.EmailedContacts);
-		ZAssert.assertNotNull(folder, "Verify can get the EmailedContacts ");
+	public void SystemFoldersDeleteButtonDisabledFromContextmenu(String name, SystemFolder systemFolder) throws HarnessException {
 		
-		// Rename the folder 
-		dialog = (DialogEditFolder)app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_TREE_EDIT, folder);
-		dialog.zClickButton(Button.B_OK);
+		FolderItem folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), systemFolder);
+		ZAssert.assertNotNull(folder, "Verify can get the folder: "+ name);	
 
-		folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Trash);
-		ZAssert.assertNotNull(folder, "Verify can get the Trash ");	
+		// Right click on Folder 
+		app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, folder);
+		
+		
+		// Get the context menu
+		String divLocator = "css=div#ZmActionMenu_contacts_ADDRBOOK";
+		ZAssert.assertTrue(app.zTreeContacts.zIsVisiblePerPosition(divLocator, 0, 0), "Verify the context menu is visible");
+		
+		
+		// Determine if the Edit option is enabled
+		String editLocator = divLocator + " div#DELETE_WITHOUT_SHORTCUT.ZDisabled";
+		ZAssert.assertTrue(app.zTreeContacts.sIsElementPresent(editLocator), "Verify the Delete Folder option is disabled");
 
-		// Rename the folder 
-		dialog = (DialogEditFolder)app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, Button.B_TREE_EDIT, folder);
-		ZAssert.assertNull(dialog, "Verify Edit Properties not enabled for Trash folder ");		
 	}	
+
+	// These folders can change color or share only (i.e. has an edit dialog)
+	@DataProvider(name = "DataProviderSystemFolders")
+	public Object[][] DataProviderSystemFolders() {
+	  return new Object[][] {
+	    new Object[] { "Contacts", SystemFolder.Contacts },
+	    new Object[] { "Emailed Contacts", SystemFolder.EmailedContacts },
+//	    new Object[] { "Distribution Lists", SystemFolder.DistributionLists },
+	    new Object[] { "Trash", SystemFolder.Trash },
+	  };
+	}
+	
+	@Test(
+			description = "Verify 'Rename folder' dialog is not present from right click context menu",
+			groups = { "functional" },
+			dataProvider = "DataProviderSystemFolders"
+			)
+	public void CannotRenameSystemFolders(String name, SystemFolder systemFolder) throws HarnessException {
+
+		FolderItem folder= FolderItem.importFromSOAP(app.zGetActiveAccount(), systemFolder);
+		ZAssert.assertNotNull(folder, "Verify can get the folder: "+ name);	
+
+		// Right click on Folder 
+		app.zTreeContacts.zTreeItem(Action.A_RIGHTCLICK, folder);
+		
+		
+		// Get the context menu
+		String divLocator = "css=div#ZmActionMenu_contacts_ADDRBOOK";
+		ZAssert.assertTrue(app.zTreeContacts.zIsVisiblePerPosition(divLocator, 0, 0), "Verify the context menu is visible");
+		
+		
+		// Determine if the Edit option is enabled
+		String editLocator = divLocator + " div#RENAME_FOLDER.ZDisabled";
+		ZAssert.assertTrue(app.zTreeContacts.sIsElementPresent(editLocator), "Verify the Rename Folder option is disabled");
+
+		
+	}	
+
 
 
 }
