3b4b0db225e51105092b9256da4ee86684a032f6
 Unknown Perforce User <girishb>
2012-10-16 11:04:49 +0000

    bug:none
    ---Added test case for verifying Toast Message for Deleted Task as well as for Edited Task

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/toaster/DeleteTask.java
@@ -0,0 +1,423 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks.toaster;
+
+
+
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import org.testng.annotations.*;
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.Toaster;
+
+
+public class DeleteTask extends AjaxCommonTest {
+
+	@SuppressWarnings("serial")
+	public DeleteTask() {
+		logger.info("New "+ DeleteTask.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageTasks;
+
+		super.startingAccountPreferences = new HashMap<String , String>() {{
+
+			put("zimbraPrefShowSelectionCheckbox", "TRUE");
+			put("zimbraPrefTasksReadingPaneLocation", "bottom");
+		}};
+
+	}
+
+	@Test(	description = "Delete a task using toolbar delete button and Verify Toast message through GUI",
+			groups = { "smoke" })
+			public void DeleteTaskToastMsg_01() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Click delete
+		app.zPageTasks.zToolbarPressButton(Button.B_DELETE);
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved to Trash");
+
+	}
+
+	@Test(	description = "Delete a task using checkbox and toolbar delete button-Verify Toast message through GUI",
+			groups = { "smoke" })
+			public void DeleteTaskToastMsg_02() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+		// Click delete
+		app.zPageTasks.zToolbarPressButton(Button.B_DELETE);
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved to Trash");
+
+
+	}
+
+	@DataProvider(name = "DataProviderDeleteKeys")
+	public Object[][] DataProviderDeleteKeys() {
+		return new Object[][] {
+				new Object[] { "VK_DELETE", KeyEvent.VK_DELETE },
+				new Object[] { "VK_BACK_SPACE", KeyEvent.VK_BACK_SPACE },
+		};
+	}
+
+	@Test(	description = "Delete a task by selecting and typing 'delete' keyboard-Verify Toast message through GUI",
+			groups = { "smoke" },
+			dataProvider = "DataProviderDeleteKeys")
+			public void DeleteTaskToastMsg_03(String name, int keyEvent) throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+
+		// Click delete keyboard
+		logger.info("Typing shortcut key "+ name + " KeyEvent: "+ keyEvent);
+		app.zPageMail.zKeyboardKeyEvent(keyEvent);
+
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved To trash");
+
+
+	}
+	@Bugs(ids="56467")
+	@Test(	description = "Delete a task by selecting and typing '.t' shortcut : Verify Toast message through GUI",
+			groups = { "functional" } )
+			public void DeleteTaskToastMsg_04() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+
+		// Use Delete Keyboard Shortcut
+		app.zPageTasks.zKeyboardShortcut(Shortcut.S_MAIL_MOVETOTRASH);
+
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved to Trash");
+
+
+	}
+
+	@Test(	description = "Delete multiple tasks (3) by select and toolbar delete : Verify Toast message through GUI",
+			groups = { "functional" })
+			public void DeleteTaskToastMsg_05() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create the message data to be sent
+		String subject1 = "task"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject2 = "task"+ ZimbraSeleniumProperties.getUniqueString();
+		String subject3 = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject1 +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject1 +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject2 +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject2 +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject3 +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject3 +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		// Import each message into MailItem objects
+		TaskItem task1 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject1);
+		ZAssert.assertNotNull(task1, "Verify the task is created");
+
+		TaskItem task2 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject2);
+		ZAssert.assertNotNull(task2, "Verify the task is created");
+
+		TaskItem task3 = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject3);
+		ZAssert.assertNotNull(task3, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the items
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject1);
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject2);
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject3);
+
+		// Click toolbar delete button
+		app.zPageTasks.zToolbarPressButton(Button.B_DELETE);
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "3 tasks moved to Trash","Verify toast message: N Tasks Moved to Trash");
+
+	}
+
+
+	@Test(	description = "Delete a task using context menu delete button:Verify Toast message through GUI",
+			groups = { "smoke" })
+			public void DeleteTaskToastMsg_06() throws HarnessException {
+
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+
+		// Right click the item, select delete
+		app.zPageTasks.zListItem(Action.A_RIGHTCLICK, Button.B_DELETE, subject);
+
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved to Trash");
+
+	}
+
+
+	@Test(	description = "Create task through SOAP - delete using Backspace Key & verify Toast message through GUI",
+			groups = { "functional" } )
+			public void DeleteTaskToastMsg_07() throws HarnessException {
+
+		FolderItem taskFolder = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Tasks);
+
+		// Create a basic task to delete
+		String subject = "task"+ ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zPageTasks.zToolbarPressButton(Button.B_REFRESH);
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_MAIL_CHECKBOX, subject);
+
+		// Use Backspace Keyboard Shortcut
+		app.zPageTasks.zKeyboardShortcut(Shortcut.S_BACKSPACE);
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "1 task moved to Trash","Verify toast message: Task Moved to Trash");
+
+
+	}
+
+
+
+}
+
---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/tasks/toaster/EditTask.java
@@ -0,0 +1,94 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.tasks.toaster;
+
+import java.util.HashMap;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.TaskItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.GeneralUtility;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.Toaster;
+import com.zimbra.qa.selenium.projects.ajax.ui.tasks.FormTaskNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.tasks.PageTasks.Locators;
+
+public class EditTask extends AjaxCommonTest{
+
+	@SuppressWarnings("serial")
+	public EditTask() {
+		logger.info("Edit " + EditTask.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageTasks;
+
+		super.startingAccountPreferences = new HashMap<String , String>() {{
+			put("zimbraPrefShowSelectionCheckbox", "TRUE");
+			put("zimbraPrefTasksReadingPaneLocation", "bottom");
+		}};
+	}
+
+	@Test(description = "Unchecked Attachment from edit window and - verify Toast message", groups = { "smoke" })
+	public void EditTaskToastMsg() throws HarnessException {
+
+		String subject = "task" + ZimbraSeleniumProperties.getUniqueString();
+
+		ZimbraAccount account = app.zGetActiveAccount();
+		FolderItem taskFolder = FolderItem.importFromSOAP(account,SystemFolder.Tasks);
+
+		// Create file item
+		String filePath = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/Files/Basic01/BasicExcel2007.xlsx";
+		// Upload file to server through RestUtil
+		String attachmentId = account.uploadFile(filePath);		
+
+
+		app.zGetActiveAccount().soapSend(
+				"<CreateTaskRequest xmlns='urn:zimbraMail'>" +
+				"<m >" +
+				"<inv>" +
+				"<comp name='"+ subject +"'>" +
+				"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+				"</comp>" +
+				"</inv>" +
+				"<su>"+ subject +"</su>" +
+				"<mp ct='text/plain'>" +
+				"<content>content"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+				"</mp>" +
+				"<attach aid='"+attachmentId+"'>"+
+				"</attach>"+
+				"</m>" +
+		"</CreateTaskRequest>");
+
+		GeneralUtility.syncDesktopToZcsWithSoap(app.zGetActiveAccount());
+
+		TaskItem task = TaskItem.importFromSOAP(app.zGetActiveAccount(), subject);
+		ZAssert.assertNotNull(task, "Verify the task is created");
+
+		// Refresh the tasks view
+		app.zTreeTasks.zTreeItem(Action.A_LEFTCLICK, taskFolder);
+
+		// Select the item
+		app.zPageTasks.zListItem(Action.A_LEFTCLICK, subject);
+		ZAssert.assertTrue(app.zPageTasks.sIsElementPresent(Locators.zAttachmentsLabel),"Verify Attachments: label");
+		
+		//Press Edit tool bar button
+		FormTaskNew taskedit = (FormTaskNew) app.zPageTasks.zToolbarPressButton(Button.B_EDIT);
+		SleepUtil.sleepMedium();
+
+		//Uncheck Attachment		
+		app.zPageTasks.sUncheck(Locators.zEditAttachmentCheckbox);
+		taskedit.zSubmit();
+		
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "Task Saved","Verify toast message: Task Saved");
+	}
+
+
+}
