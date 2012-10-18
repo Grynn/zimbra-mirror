e4eb9d759927d98a5132132e382d0757e23c43cf
 Unknown Perforce User <girishb>
2012-10-18 12:06:28 +0000

    bug:78058
    --Added test case as per comment#0
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=78058

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/mail/signatures/toaster/Bug_78058.java
@@ -0,0 +1,70 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.preferences.mail.signatures.toaster;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.Toaster;
+import com.zimbra.qa.selenium.projects.ajax.ui.preferences.TreePreferences.TreeItem;
+import com.zimbra.qa.selenium.projects.ajax.ui.preferences.signature.FormSignatureNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.preferences.signature.FormSignatureNew.Field;
+
+
+public class Bug_78058 extends AjaxCommonTest {
+	public Bug_78058() {
+		super.startingPage = app.zPagePreferences;
+		super.startingAccountPreferences = null;
+	}
+
+	@Test(description = "Verify Toast Msg for Empty Signature Name", groups = { "smoke" })
+	public void VerifyToastMsgForEmptySigName() throws HarnessException {
+
+		String sigName = "";		
+		String sigBody = "sigbody" + ZimbraSeleniumProperties.getUniqueString();
+
+		// click on signature from left pane
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK,TreeItem.MailSignatures);
+
+		//Click on New signature button
+		FormSignatureNew signew =(FormSignatureNew) app.zPageSignature.zToolbarPressButton(Button.B_NEW);
+
+		// Empty Signature Name and some text in body
+		signew.zFillField(Field.SignatureName, sigName);
+		signew.zFillField(Field.SignatureBody, sigBody);
+		signew.zSubmit();
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "Signature name is empty. It's required","Verify toast message:Signature name is empty. It's required");
+
+	}
+
+	@Test(description = "Verify Toast Msg for Empty Signature body", groups = { "smoke" })
+	public void VerifyToastMsgForEmptySigBody_Bug78058() throws HarnessException {
+
+		String sigName = "signame" + ZimbraSeleniumProperties.getUniqueString();
+		String sigBody = "";
+
+		// click on signature from left pane
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK,TreeItem.MailSignatures);
+
+		//Click on New signature button
+		FormSignatureNew signew =(FormSignatureNew) app.zPageSignature.zToolbarPressButton(Button.B_NEW);
+
+		// Empty Signature body and some text in Name
+		signew.zFillField(Field.SignatureName, sigName);
+		signew.zFillField(Field.SignatureBody, sigBody);
+		signew.zSubmit();
+
+		// Verifying the toaster message
+		Toaster toast = app.zPageMain.zGetToaster();
+		String toastMsg = toast.zGetToastMessage();
+		ZAssert.assertStringContains(toastMsg, "Signature value is empty. It's required","Verify toast message:Signature value is empty. It's required");
+
+	}
+}
