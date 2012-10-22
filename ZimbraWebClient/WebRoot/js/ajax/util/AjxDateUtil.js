ab3a7a62011113ffeb584989d913585d7386b37a
Eran Yarkon <eyarkon@zimbra.com>
2012-10-22 23:41:15 +0000

    Bug 78043
    Receiving multiple reminders that "You're OOO message is still on"
    Only set zimbraPrefOutOfOfficeStatusAlertOnLogin to true if something substantial changed in the OOO settings... whether OOO is on or off, or the vacation period changed (either the "time period" checkbox, or the from/to if it's set to true) - that likely indicates a new vacation.
    Also fixed the OOO warning to work in case to/from is set ("Send auto-replies during the following time period" is checked). Also fixed the parsing to use the format we save the prefs with, so the exact time of day is calculated correctly.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=78043

--- ZimbraWebClient/WebRoot/js/ajax/util/AjxDateUtil.js
+++ ZimbraWebClient/WebRoot/js/ajax/util/AjxDateUtil.js
@@ -913,6 +913,78 @@ function(fromThisDate, count) {
 	return r;
 }
 
+/**
+ * note - this deals with the format we save from Prefs page. Careful if using for other cases.
+ * @param value
+ * @return {String}
+ */
+AjxDateUtil.dateLocal2GMT =
+function(value) {
+	if (!value) { return ""; }
+
+	var yr, mo, da, hr, mi, se; // really smart parsing.
+	yr = parseInt(value.substr(0,  4), 10);
+	mo = parseInt(value.substr(4,  2), 10);
+	da = parseInt(value.substr(6,  2), 10);
+	hr = parseInt(value.substr(8,  2), 10);
+	mi = parseInt(value.substr(10, 2), 10);
+	se = parseInt(value.substr(12, 2), 10);
+	var date = new Date(yr, mo - 1, da, hr, mi, se, 0);
+	yr = date.getUTCFullYear();
+	mo = date.getUTCMonth() + 1;
+	da = date.getUTCDate();
+	hr = date.getUTCHours();
+	mi = date.getUTCMinutes();
+	se = date.getUTCSeconds();
+	var a = [ yr, mo, da, hr, mi, se ];
+	for (var i = a.length; --i > 0;) {
+		var n = a[i];
+		if (n < 10)
+			a[i] = "0" + n;
+	}
+	return (a.join("") + "Z");
+};
+
+/**
+ * note - this deals with the format we save from Prefs page. Careful if using for other cases.
+ * @param value
+ * @return {String}
+ */
+AjxDateUtil.dateGMT2Local =
+function(value) {
+	if (!value) { return ""; }
+
+	var yr, mo, da, hr, mi, se; // really smart parsing.
+	yr = parseInt(value.substr(0,  4), 10);
+	mo = parseInt(value.substr(4,  2), 10);
+	da = parseInt(value.substr(6,  2), 10);
+	hr = parseInt(value.substr(8,  2), 10);
+	mi = parseInt(value.substr(10, 2), 10);
+	se = parseInt(value.substr(12, 2), 10);
+	var date = new Date();
+	date.setUTCMilliseconds(0);
+	date.setUTCSeconds(se);
+	date.setUTCMinutes(mi);
+	date.setUTCHours(hr);
+	date.setUTCDate(da);
+	date.setUTCMonth(mo - 1);
+	date.setUTCFullYear(yr);
+	yr = date.getFullYear();
+	mo = date.getMonth() + 1;
+	da = date.getDate();
+	hr = date.getHours();
+	mi = date.getMinutes();
+	se = date.getSeconds();
+	var a = [yr, mo, da, hr, mi, se];
+	for (var i = a.length; --i > 0;) {
+		var n = a[i];
+		if (n < 10)
+			a[i] = "0" + n;
+	}
+	return (a.join("") + "Z");
+};
+
+
 AjxDateUtil._getDateForNextWeekday =
 function(fromThisDate,thisWeekday) {
 	var newDate = new Date(fromThisDate);
