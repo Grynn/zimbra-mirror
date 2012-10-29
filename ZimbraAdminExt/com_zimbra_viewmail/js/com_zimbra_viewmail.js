66494ba3fc8fccd3eeb4682edb884d3c3aefbbd1
Rahul Shah <rshah@zimbra.com>
2012-10-29 21:05:03 +0000

    Integrate change 413397 from zimbra to zimbra
    -------------
    Bug 77477 - View Mail missing from open source upgrade
    The View Mail code was extracted from the admin console and created as an extension. This extension got created in the ZimbraNetwork due to which the FOSS users were not able to use View Mail. Moved the View Mail extension from ZimbraNetwork to FOSS ZimbraAdminExt and updated the build scripts.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=77477

---
+++ ZimbraAdminExt/com_zimbra_viewmail/js/com_zimbra_viewmail.js
@@ -0,0 +1,204 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: qinan
+ * Date: 8/19/11
+ * Time: 2:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+if(ZaSettings && ZaSettings.EnabledZimlet["com_zimbra_viewmail"]){
+ZaAccountViewMail = function () {}
+
+/*ZaAccountViewMail.initExtraToolbarButton = function () {
+
+    this._toolbarOperations[ZaOperation.VIEW_MAIL] = new ZaOperation(ZaOperation.VIEW_MAIL,
+        com_zimbra_viewmail.ACTBB_ViewMail, com_zimbra_viewmail.ACTBB_ViewMail_tt, "ReadMailbox", "ReadMailbox",
+        new AjxListener(this, ZaAccountViewMail._viewMailListener));
+
+    if (!this._toolbarOrder) {
+        this._toolbarOrder == [];
+    }
+    this._toolbarOrder.push (ZaOperation.VIEW_MAIL)  ;
+}
+*/
+
+
+ZaAccountViewMail.initExtraPopupButton = function () {
+    this._popupOperations[ZaOperation.VIEW_MAIL] = new ZaOperation(ZaOperation.VIEW_MAIL,
+        com_zimbra_viewmail.ACTBB_ViewMail, com_zimbra_viewmail.ACTBB_ViewMail_tt, "ReadMailbox", "ReadMailbox",
+        new AjxListener(this, ZaAccountViewMail._viewMailListener));
+}
+
+if (ZaController.initPopupMenuMethods["ZaAccountListController"]) {
+    ZaController.initPopupMenuMethods["ZaAccountListController"].push(ZaAccountViewMail.initExtraPopupButton);
+}
+
+if (ZaController.initPopupMenuMethods["ZaSearchListController"]) {
+    ZaController.initPopupMenuMethods["ZaSearchListController"].push(ZaAccountViewMail.initExtraPopupButton);
+}
+
+if (ZaController.initPopupMenuMethods["ZaAccountViewController"]) {
+    ZaController.initPopupMenuMethods["ZaAccountViewController"].push(ZaAccountViewMail.initExtraPopupButton);
+}
+
+if (ZaController.initPopupMenuMethods["ZaDLController"]) {
+    ZaController.initPopupMenuMethods["ZaDLController"].push(ZaAccountViewMail.initExtraPopupButton);
+}
+
+if (ZaController.initPopupMenuMethods["ZaResourceController"]) {
+    ZaController.initPopupMenuMethods["ZaResourceController"].push(ZaAccountViewMail.initExtraPopupButton);
+}
+
+ZaAccountViewMail._viewMailListenerLauncher = ZaAccountListController._viewMailListenerLauncher;
+
+ZaAccountViewMail._viewMailListener =
+function(ev) {
+	try {
+		var account = null;
+		if (this instanceof ZaAccountListController || this instanceof ZaSearchListController){
+			var accounts = this._contentView.getSelection();
+			if(!accounts || accounts.length<=0) {
+				return;
+			}
+			account = accounts[0];
+
+		} else if (this instanceof ZaAccountViewController || this instanceof ZaDLController || this instanceof ZaResourceController){
+			account = this._currentObject;
+		} else {
+			return;
+		}
+		if (account){
+			ZaAccountViewMail._viewMailListenerLauncher.call(this, account);
+		}
+
+	} catch (ex) {
+		this._handleException(ex, "ZaAccountViewMail._viewMailListener", null, false);
+	}
+}
+
+ZaAccountViewMail.changeActionsStateMethod =
+function () {
+    var cnt, item;
+    if (this instanceof ZaAccountListController || this instanceof ZaSearchListController){
+        item = this._contentView.getSelection()[0];
+        cnt = this._contentView.getSelectionCount();
+    } else if (this instanceof ZaAccountViewController || this instanceof ZaDLController || this instanceof ZaResourceController){
+        item = this._currentObject;
+        cnt = 1;
+    }else {
+        return;
+    }
+
+    if (cnt == 1) {
+        if (item) {
+
+            if ((item.type == ZaItem.ALIAS) && (item.attrs[ZaAlias.A_targetType] == ZaItem.DL)) {
+                if(!item.targetObj)
+                    item.targetObj = item.getAliasTargetObj();
+
+                var enable = (item.targetObj.attrs[ZaDistributionList.A_mailStatus] == "enabled");
+                if(this._popupOperations[ZaOperation.VIEW_MAIL])
+                    this._popupOperations[ZaOperation.VIEW_MAIL].enabled = enable;
+
+            }
+            else if (item.type == ZaItem.DL) {
+                var enable = (item.attrs[ZaDistributionList.A_mailStatus] == "enabled");
+                if(this._popupOperations[ZaOperation.VIEW_MAIL])
+                    this._popupOperations[ZaOperation.VIEW_MAIL].enabled = enable;
+
+            }
+				else if (item.type == ZaItem.ACCOUNT) {
+				var enable = false;
+				if(ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] == 'TRUE') {
+					enable = true;
+				} else if (AjxUtil.isEmpty(item.rights)) {
+					item.loadEffectiveRights("id", item.id, false);
+				}
+                if(!enable) {
+					if(!ZaItem.hasRight(ZaAccount.VIEW_MAIL_RIGHT,item)) {
+						 if(this._popupOperations[ZaOperation.VIEW_MAIL])
+						 	this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+					}
+                }
+            } else if ((item.type == ZaItem.ALIAS) && (item.attrs[ZaAlias.A_targetType] == ZaItem.ACCOUNT))  {
+				if(!item.targetObj)
+					item.targetObj = item.getAliasTargetObj() ;
+
+				var enable = false;
+				if (ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] == 'TRUE') {
+					enable = true;
+				} else if (AjxUtil.isEmpty(item.targetObj.rights)) {
+					item.targetObj.loadEffectiveRights("id", item.id, false);
+				}
+				if(!enable) {
+					if(!ZaItem.hasRight(ZaAccount.VIEW_MAIL_RIGHT,item.targetObj)) {
+						 if(this._popupOperations[ZaOperation.VIEW_MAIL])
+						 	this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+					}
+                }
+            } else if ((item.type == ZaItem.ALIAS) && (item.attrs[ZaAlias.A_targetType] == ZaItem.RESOURCE))  {
+				if(!item.targetObj)
+					item.targetObj = item.getAliasTargetObj() ;
+
+				var enable = false;
+				if (ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] == 'TRUE') {
+					enable = true;
+				} else if (AjxUtil.isEmpty(item.targetObj.rights)) {
+					item.targetObj.loadEffectiveRights("id", item.id, false);
+				}
+                if(!enable) {
+                    if(!ZaItem.hasRight(ZaResource.VIEW_RESOURCE_MAIL_RIGHT,item.targetObj)) {
+                         if(this._popupOperations[ZaOperation.VIEW_MAIL])
+                            this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+                    }
+                }
+            } else if(item.type == ZaItem.RESOURCE) {
+				var enable = false;
+				if(ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] == 'TRUE') {
+					enable = true;
+				} else if (AjxUtil.isEmpty(item.rights)) {
+					item.loadEffectiveRights("id", item.id, false);
+				}
+				if(!enable) {
+					if(!ZaItem.hasRight(ZaResource.VIEW_RESOURCE_MAIL_RIGHT,item)) {
+						 if(this._popupOperations[ZaOperation.VIEW_MAIL]) {
+						 	this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+						 }
+
+					}
+                }
+            } else if(item.type == ZaItem.DOMAIN || item.type == ZaItem.COS){
+               if(this._popupOperations[ZaOperation.VIEW_MAIL]) {
+					this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+               }
+            }
+
+        } else {
+			if(this._popupOperations[ZaOperation.VIEW_MAIL]) {
+				this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+			}
+        }
+    } else {
+		if(this._popupOperations[ZaOperation.VIEW_MAIL]) {
+			this._popupOperations[ZaOperation.VIEW_MAIL].enabled = false;
+		}
+    }
+}
+if(ZaController.changeActionsStateMethods["ZaAccountListController"]) {
+    ZaController.changeActionsStateMethods["ZaAccountListController"].push(ZaAccountViewMail.changeActionsStateMethod);
+}
+if(ZaController.changeActionsStateMethods["ZaSearchListController"]) {
+    ZaController.changeActionsStateMethods["ZaSearchListController"].push(ZaAccountViewMail.changeActionsStateMethod);
+}
+
+if(ZaController.changeActionsStateMethods["ZaAccountViewController"]) {
+    ZaController.changeActionsStateMethods["ZaAccountViewController"].push(ZaAccountViewMail.changeActionsStateMethod);
+}
+if(ZaController.changeActionsStateMethods["ZaDLController"]) {
+    ZaController.changeActionsStateMethods["ZaDLController"].push(ZaAccountViewMail.changeActionsStateMethod);
+}
+if(ZaController.changeActionsStateMethods["ZaResourceController"]) {
+    ZaController.changeActionsStateMethods["ZaResourceController"].push(ZaAccountViewMail.changeActionsStateMethod);
+}
+
+
+}
