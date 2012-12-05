f3ae9350362f65809a088d2cec5eb2c95fa26f75
 Unknown Perforce User <rrao>
2012-12-05 06:57:33 +0000

    Bug : None Added 1 new tsetcase to Move appt to another calnder by context menu at organizer

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/actions/MoveAppointment.java
@@ -0,0 +1,105 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.organizer.actions;
+
+import java.awt.event.KeyEvent;
+import java.util.Calendar;
+
+import org.testng.annotations.*;
+
+import com.zimbra.common.soap.Element;
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.*;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.DialogConfirmDeleteOrganizer;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.FormMailNew.Field;
+
+@SuppressWarnings("unused")
+public class MoveAppointment extends CalendarWorkWeekTest {	
+	
+	
+	public MoveAppointment() {
+		logger.info("New "+ MoveAppointment.class.getCanonicalName());
+		
+	}
+	
+
+	@Test(description = "Move Appointment using context menu",
+			groups = { "functional" })
+	public void MoveAppointment_01() throws HarnessException {
+		
+		
+		//-- Data setup
+		
+		
+		// Creating object for meeting data
+		FolderItem root = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.UserRoot);
+		
+		String tz, apptSubject, apptBody, apptAttendee1;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		apptBody = ZimbraSeleniumProperties.getUniqueString();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+		
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		String name1 = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		String name2 = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		
+		app.zGetActiveAccount().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+	  	"<folder name='"+ name1 +"' l='"+ root.getId() +"' view='appointment'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subfolder1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder1, "Verify the first subfolder is available");
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     "<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     "<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     "<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     "<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     "<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" + 
+                     "</inv>" +
+                     "<e a='"+ apptAttendee1 +"' t='t'/>" +
+                     "<mp content-type='text/plain'>" +
+                     "<content>"+ apptBody +"</content>" +
+                     "</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+		String apptId = app.zGetActiveAccount().soapSelectValue("//mail:CreateAppointmentResponse", "apptId");
+        
+	
+		
+		//-- GUI actions
+		
+		
+        // Refresh the view
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        // Select the appointment
+        app.zPageCalendar.zListItem(Action.A_LEFTCLICK, apptSubject);
+        
+        // Right Click -> Move context menu
+ 
+        DialogMove dialog = (DialogMove)app.zPageCalendar.zListItem(Action.A_RIGHTCLICK, Button.B_MOVE, apptSubject);
+		dialog.zClickTreeFolder(subfolder1);
+		dialog.zClickButton(Button.B_OK);
+        
+		
+		//-- Server verification
+		AppointmentItem newAppointment = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ apptSubject +")");
+		ZAssert.assertEquals(newAppointment.getFolder(), subfolder1.getId(), "Verify the appointment moved folders");
+	}
+	
+}
