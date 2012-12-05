a288ecafe7866a416ef2807250d5ce45b3835d19
 Unknown Perforce User <rrao>
2012-12-05 07:00:55 +0000

    bug:none Check Reply to a meeting context option is disabled for organizer

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/actions/Reply.java
@@ -0,0 +1,66 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.organizer.actions;
+
+import java.util.Calendar;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
+import com.zimbra.qa.selenium.framework.util.ZTimeZone;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.PageCalendar.Locators;
+
+public class Reply extends CalendarWorkWeekTest {	
+	
+	public Reply() {
+		logger.info("New "+ Reply.class.getCanonicalName());
+		
+	}
+	
+	@Test(description = "Check Reply to a meeting is disabled for organizer ",
+			groups = { "smoke12" })
+	public void Reply_01() throws HarnessException {
+		
+		// Create the appointment on the server
+		String apptSubject = "appointment" + ZimbraSeleniumProperties.getUniqueString();
+		String attendee1 = ZimbraAccount.AccountA().EmailAddress;
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		// EST timezone string
+		String tz = ZTimeZone.TimeZoneEST.getID();
+
+		// Create a meeting request from AccountA to the test account
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + attendee1 + "' d='2'/>" + 
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+		
+		//-- GUI actions	
+        // Refresh the view
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+    
+       // check on the context menu 'Reply' option is disabled for organizer
+        app.zPageCalendar.zRightClickAt(app.zPageCalendar.zGetApptLocator(apptSubject),"");
+        ZAssert.assertFalse(app.zPageCalendar.zIsElementDisabled(Locators.ReplyMenu), "Reply Menu is disabled for organiser");
+
+	}
+	
+	
+}
