83784164e2f2a9b813d8d67864c857e19718d09d
 Unknown Perforce User <rrao>
2012-12-05 07:06:47 +0000

    bug:none move appointment to other calender by using toolbar button

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/attendee/MoveAppointment.java
@@ -0,0 +1,79 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.meetings.attendee;
+
+import java.util.Calendar;
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.PageCalendar.Locators;
+
+public class MoveAppointment extends CalendarWorkWeekTest {	
+	
+	public MoveAppointment() {
+		logger.info("New "+ MoveAppointment.class.getCanonicalName());
+		
+	}
+
+	@Test(description = "Move Appointment using toolbar menu as attendee",
+			groups = { "functional" })
+	public void MoveAppointment_01() throws HarnessException {
+
+		//-- Data setup
+
+		// Creating object for meeting data
+		FolderItem root = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.UserRoot);
+		String apptSubject;
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		String name1 = "folder" + ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+	  	"<folder name='"+ name1 +"' l='"+ root.getId() +"' view='appointment'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem subfolder1 = FolderItem.importFromSOAP(app.zGetActiveAccount(), name1);
+		ZAssert.assertNotNull(subfolder1, "Verify the first subfolder is available");
+		
+		// Get meeting invite where it has 2 attendees
+		ZimbraAccount.AccountA().soapSend(
+				"<CreateAppointmentRequest xmlns='urn:zimbraMail'>"
+				+		"<m>"
+				+			"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"
+				+				"<s d='"+ startUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<e d='"+ endUTC.toTimeZone(ZTimeZone.TimeZoneEST.getID()).toYYYYMMDDTHHMMSS() +"' tz='"+ ZTimeZone.TimeZoneEST.getID() +"'/>"
+				+				"<or a='"+ ZimbraAccount.AccountA().EmailAddress +"'/>"
+				+				"<at role='REQ' ptst='NE' rsvp='1' a='" + app.zGetActiveAccount().EmailAddress + "'/>"
+				+				"<at role='REQ' ptst='NE' rsvp='1' a='" + ZimbraAccount.AccountB().EmailAddress + "'/>"
+				+			"</inv>"
+				+			"<e a='"+ app.zGetActiveAccount().EmailAddress +"' t='t'/>"
+				+			"<e a='" + ZimbraAccount.AccountB().EmailAddress +"' t='t'/>"
+				+			"<su>"+ apptSubject +"</su>"
+				+			"<mp content-type='text/plain'>"
+				+				"<content>content</content>"
+				+			"</mp>"
+				+		"</m>"
+				+	"</CreateAppointmentRequest>");        
+
+		// Refresh the view
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+        // Select the appointment
+        app.zPageCalendar.zListItem(Action.A_LEFTCLICK, apptSubject);
+        
+        // Right Click -> Move context menu
+        app.zPageCalendar.zToolbarPressButton(Button.O_MOVE_MENU);
+        app.zPageCalendar.zClickAt(Locators.MOveFolderOption + name1 + "')" , "");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+		
+		//-- Server verification
+		AppointmentItem newAppointment = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ apptSubject +")");
+		ZAssert.assertEquals(newAppointment.getFolder(), subfolder1.getId(), "Verify the appointment moved folders");
+	}
+}
