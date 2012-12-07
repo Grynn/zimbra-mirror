ea178057d02421976079c8f0a9466985cd7e5a3b
Jeff Flanigan <jflanigan@zimbra.com>
2012-12-07 22:12:27 +0000

    Integrate change 425888 from HELIX bug: 79111
    Null check session object in case client is not using a session.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=79111

--- ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineGetInfo.java
+++ ZimbraOffline/src/java/com/zimbra/cs/service/offline/OfflineGetInfo.java
@@ -2,12 +2,12 @@
  * ***** BEGIN LICENSE BLOCK *****
  * Zimbra Collaboration Suite Server
  * Copyright (C) 2008, 2009, 2010 Zimbra, Inc.
- * 
+ *
  * The contents of this file are subject to the Zimbra Public License
  * Version 1.3 ("License"); you may not use this file except in
  * compliance with the License.  You may obtain a copy of the License at
  * http://www.zimbra.com/license.
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
  * ***** END LICENSE BLOCK *****
@@ -34,26 +34,26 @@ public class OfflineGetInfo extends GetInfo {
             //bug 47450
             //eventually want the UI to be able to show a meaningful error
             //currently, the main mailbox UI doesn't load if one or more accounts have corrupt DB files
-            isVisible=false; 
+            isVisible=false;
         }
         Element elem = super.encodeChildAccount(parent, child, isVisible);
-        
+
         accountName = accountName != null ? accountName : child.getAttr(
             OfflineConstants.A_offlineAccountName);
         if (elem != null && accountName != null) {
             Element attrsElem = elem.addUniqueElement(AccountConstants.E_ATTRS);
-            
+
             if (accountName != null)
                 attrsElem.addKeyValuePair(Provisioning.A_zimbraPrefLabel,
                     accountName, AccountConstants.E_ATTR, AccountConstants.A_NAME);
         }
         return elem;
     }
-    
+
     @Override
     protected Session getSession(ZimbraSoapContext zsc, Session.Type stype) {
         Session s = super.getSession(zsc, stype);
-        if (!s.isDelegatedSession())
+        if (s != null && !s.isDelegatedSession())
             ((SoapSession)s).setOfflineSoapSession();
         return s;
     }
