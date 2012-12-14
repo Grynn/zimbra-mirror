59494acc24598c85b6af9567ec47766d2450639f
 Unknown Perforce User <rrao>
2012-12-14 07:27:52 +0000

    bug:none added 3  new testcase to Cancel modifying appt and take action from warning dialog

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/appointments/views/workweek/singleday/CloseModifyAppointment.java
@@ -0,0 +1,221 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.appointments.views.workweek.singleday;
+
+import java.util.Calendar;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
+import com.zimbra.qa.selenium.framework.util.ZTimeZone;
+import com.zimbra.qa.selenium.framework.util.ZimbraAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.DialogSendUpdatetoAttendees;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.DialogConfirmModification;
+public class CloseModifyAppointment extends CalendarWorkWeekTest {
+	
+	public CloseModifyAppointment() {
+		logger.info("New " + CloseModifyAppointment.class.getCanonicalName());
+
+	}
+	
+	@Test(description = "Close modifying appt and take action from warning dialog : Save Chnages and send updates", 
+			groups = { "functional12"})
+	public void ModifyAppointment_01() throws HarnessException {
+
+		// create appointment data 
+		String tz, apptSubject, apptAttendee1,apptAttendee2;
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+        apptAttendee2 = ZimbraAccount.AccountB().EmailAddress;
+		
+        // Absolute dates in UTC zone
+        tz = ZTimeZone.TimeZoneEST.getID();
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		// create appointment 
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee2 + "' d='2'/>" +
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+
+        // Switch to work week view
+        app.zPageCalendar.zToolbarPressPulldown(Button.B_LISTVIEW, Button.O_LISTVIEW_WORKWEEK);
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+        //remove attendee and close the appt
+        app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+        FormApptNew apptForm = new FormApptNew(app);
+        apptForm.zRemoveAttendee(apptAttendee2);
+        apptForm.zToolbarPressButton(Button.B_CLOSE);
+        DialogConfirmModification confirmClose = (DialogConfirmModification) new DialogConfirmModification(app, app.zPageCalendar);
+        confirmClose.zClickButton(Button.B_OK);
+        DialogSendUpdatetoAttendees sendUpdateDialog = (DialogSendUpdatetoAttendees) new DialogSendUpdatetoAttendees(app, app.zPageCalendar);
+        sendUpdateDialog.zClickButton(Button.B_OK);
+        SleepUtil.sleepLong();
+        
+        // check if an attendee is 
+       
+        AppointmentItem actual = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ apptSubject +")");
+		ZAssert.assertEquals(actual.getSubject(), apptSubject, "Subject: Verify the appointment data");
+		ZAssert.assertStringDoesNotContain(actual.getAttendees(), apptAttendee2, "Attendees: Verify the attendee data");
+	}
+	
+	@Test(description = "Close modifying appt and take action from warning dialog : Discard and close", 
+			groups = { "functional12"})
+	public void ModifyAppointment_02() throws HarnessException {
+
+		// Creating object for appointment data
+		AppointmentItem appt = new AppointmentItem();
+		String tz, apptSubject, editApptSubject, apptAttendee1,apptAttendee2;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		editApptSubject = ZimbraSeleniumProperties.getUniqueString();
+        apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+        apptAttendee2 = ZimbraAccount.AccountB().EmailAddress;
+
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee2 + "' d='2'/>" +
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+        String apptId = app.zGetActiveAccount().soapSelectValue("//mail:CreateAppointmentResponse", "apptId");
+    
+        // Switch to work week view
+        app.zPageCalendar.zToolbarPressPulldown(Button.B_LISTVIEW, Button.O_LISTVIEW_WORKWEEK);
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+        // Open appointment & modify subject, body and save it
+        app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+        SleepUtil.sleepMedium();
+        FormApptNew apptForm = new FormApptNew(app);
+        
+        appt.setSubject(editApptSubject);
+        apptForm.zRemoveAttendee(apptAttendee2);
+        apptForm.zToolbarPressButton(Button.B_CLOSE);
+        DialogConfirmModification confirmClose = (DialogConfirmModification) new DialogConfirmModification(app, app.zPageCalendar);
+        String  oldPageName= app.zPageCalendar.sGetTitle();
+   
+        confirmClose.zClickButton(Button.B_DISCARD_CLOSE);
+        confirmClose.zClickButton(Button.B_OK);
+        SleepUtil.sleepSmall();
+        
+        // Use GetAppointmentRequest to verify the changes are saved
+        app.zGetActiveAccount().soapSend("<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ apptId +"'/>");
+        AppointmentItem modifyAppt = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")");
+
+        // check if appt still has old subject and not the modified subject 
+        ZAssert.assertEquals(app.zGetActiveAccount().soapMatch("//mail:GetAppointmentResponse//mail:comp", "name", apptSubject), true, "");
+        ZAssert.assertStringContains(modifyAppt.getAttendees(), apptAttendee1, "Attendees: Verify modified attendees");
+        String  newPageName= app.zPageCalendar.sGetTitle();
+        ZAssert.assertNotEqual(oldPageName, newPageName, "the Page has been closed");
+	}
+	
+	@Test(description = "Close modifying appt and take action from warning dialog : Dont save But keep open", 
+			groups = { "functional12"})
+	public void ModifyAppointment_03() throws HarnessException {
+		// Creating object for appointment data
+		AppointmentItem appt = new AppointmentItem();
+		String tz, apptSubject, apptAttendee1,apptAttendee2,editApptSubject;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject = ZimbraSeleniumProperties.getUniqueString();
+		editApptSubject = ZimbraSeleniumProperties.getUniqueString();
+		apptAttendee1 = ZimbraAccount.AccountA().EmailAddress;
+        apptAttendee2 = ZimbraAccount.AccountB().EmailAddress;
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+		app.zGetActiveAccount().soapSend(
+                "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                     "<m>"+
+                     	"<inv method='REQUEST' type='event' status='CONF' draft='0' class='PUB' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                     		"<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                     		"<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee1 + "' d='2'/>" +
+                     		"<at role='REQ' ptst='NE' rsvp='1' a='" + apptAttendee2 + "' d='2'/>" +
+                     	"</inv>" +
+                     	"<e a='"+ ZimbraAccount.AccountA().EmailAddress +"' t='t'/>" +
+                     	"<mp content-type='text/plain'>" +
+                     		"<content>"+ ZimbraSeleniumProperties.getUniqueString() +"</content>" +
+                     	"</mp>" +
+                     "<su>"+ apptSubject +"</su>" +
+                     "</m>" +
+               "</CreateAppointmentRequest>");
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+
+        String apptId = app.zGetActiveAccount().soapSelectValue("//mail:CreateAppointmentResponse", "apptId");
+    
+        // Switch to work week view
+        app.zPageCalendar.zToolbarPressPulldown(Button.B_LISTVIEW, Button.O_LISTVIEW_WORKWEEK);
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+     // Create appointment with attendee
+        app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+        FormApptNew apptForm = new FormApptNew(app);
+        appt.setSubject(editApptSubject);
+        apptForm.zRemoveAttendee(apptAttendee2);
+        apptForm.zToolbarPressButton(Button.B_CLOSE);
+        DialogConfirmModification confirmClose = (DialogConfirmModification) new DialogConfirmModification(app, app.zPageCalendar);
+        confirmClose.zClickButton(Button.B_DONTSAVE_KEEP_OPEN);
+        confirmClose.zClickButton(Button.B_OK);
+        SleepUtil.sleepLong();     
+        // Use GetAppointmentRequest to verify the changes are saved
+        app.zGetActiveAccount().soapSend("<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ apptId +"'/>");
+        AppointmentItem modifyAppt = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")");
+
+        // check if appt still has old subject and not the modified subject 
+        ZAssert.assertEquals(app.zGetActiveAccount().soapMatch("//mail:GetAppointmentResponse//mail:comp", "name", apptSubject), true, "");
+        ZAssert.assertStringContains(modifyAppt.getAttendees(), apptAttendee1, "Attendees: Verify modified attendees");
+        ZAssert.assertEquals(app.zPageCalendar.sGetTitle(),"Zimbra: Appointment" , "make sure appointment window is open");
+	}
+	
+	
+	
+	
+}
--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/calendar/DialogConfirmModification.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/ui/calendar/DialogConfirmModification.java
@@ -1,52 +1,60 @@
 package com.zimbra.qa.selenium.projects.ajax.ui.calendar;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.HarnessException;
-import com.zimbra.qa.selenium.framework.util.SleepUtil;
 import com.zimbra.qa.selenium.framework.util.staf.Stafpostqueue;
 import com.zimbra.qa.selenium.projects.ajax.ui.DialogWarning;
-import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew.Locators;
 
-public class DialogSendUpdatetoAttendees extends DialogWarning {
+public class DialogConfirmModification extends DialogWarning {
 
 	// The ID for the main Dialog DIV
 	public static final String LocatorDivID = "SEND_UPDATES_DIALOG";
-		
-	public DialogSendUpdatetoAttendees(AbsApplication application, AbsTab page) {
+	public static class Locators {
+	public static final String SaveAndSendUpdates = "css=div[class='DwtDialog WindowOuterContainer'] input[id$='_send']";
+	public static final String DontSaveAndKeepOpen = "css=div[class='DwtDialog WindowOuterContainer'] input[id$='_cancel']";
+	public static final String DiscardAndClose = "css= div[class='DwtDialog WindowOuterContainer'] input[id$='_discard']";
+	public static final String Ok_changes = "css=td[id='CHNG_DLG_ORG_1_button2_title']";
+	public static final String Cancel_changes =  "css=td[id='CHNG_DLG_ORG_1_button1_title']";
+	}
+	public DialogConfirmModification(AbsApplication application, AbsTab page) {
 		super(new DialogWarningID(LocatorDivID), application, page);
 				
-		logger.info("new " + DialogSendUpdatetoAttendees.class.getCanonicalName());
+		logger.info("new " + DialogConfirmModification.class.getCanonicalName());
 	}
 
 	@Override
 	public AbsPage zClickButton(Button button) throws HarnessException {
 		logger.info(myPageName() + " zClickButton(" + button + ")");
-		
+
 		tracer.trace("Click dialog button " + button);
 		if ( button == null )
 			throw new HarnessException("button cannot be null");
 	
 		String locator = null;
 		AbsPage page = null;
+		boolean waitForPostfix = false;
 
-		if (button == Button.B_SEND_UPDATES_ONLY_TO_ADDED_OR_REMOVED_ATTENDEES) {
+		if (button == Button.B_SAVE_SEND_UPDATES) {
 
-			locator = Locators.SendUpdatesToAddedRemovedRadioButton;
+			locator = Locators.SaveAndSendUpdates;
 			page = null;
 
-		} else if (button == Button.B_SEND_UPDATES_TO_ALL_ATTENDEES) {
+		} else if (button == Button.B_DONTSAVE_KEEP_OPEN) {
 
-			locator = Locators.SendUpdatesToAllRadioButton;
+			locator = Locators.DontSaveAndKeepOpen;
+			page = null;
+		
+		} else if (button == Button.B_DISCARD_CLOSE) {
+
+			locator = Locators.DiscardAndClose;
 			page = null;
 		
 		} else if (button == Button.B_OK) {
-
-			locator = "css=div[class='DwtDialog'] td[id$='_button2_title']";
-			locator = "css=div[id^='DWT'][class='DwtDialog'] div[id$='buttons'] td[id$='_button2_title']";
+			locator = Locators.Ok_changes;
 			page = null;
-		
+
 		} else if (button == Button.B_CANCEL) {
 
-			locator = "css=div[class='DwtDialog'] td[id$='_button1_title']";
+			locator =Locators.Cancel_changes;
 			page = null;
 			                              
 		} else {
@@ -60,15 +68,14 @@ public class DialogSendUpdatetoAttendees extends DialogWarning {
 			throw new HarnessException("Button " + button + " not implemented");
 		}
 
-		SleepUtil.sleepMedium();
+		// Make sure the locator exists
 		if (!this.sIsElementPresent(locator)) {
 			throw new HarnessException("Button " + button + " locator "
 					+ locator + " not present!");
 		}
-		
+		this.sFocus(locator);
 		this.sClickAt(locator, "");
 		this.zWaitForBusyOverlay();
-		
 		// If page was specified, make sure it is active
 		if ( page != null ) {
 			// This function (default) throws an exception if never active
@@ -77,8 +84,10 @@ public class DialogSendUpdatetoAttendees extends DialogWarning {
 		}
 
 		// This dialog could send messages, so wait for the queue
-		Stafpostqueue sp = new Stafpostqueue();
-		sp.waitForPostqueue();
+		if ( waitForPostfix ) {
+			Stafpostqueue sp = new Stafpostqueue();
+			sp.waitForPostqueue();
+		}
 
 		return (page);
 	}
