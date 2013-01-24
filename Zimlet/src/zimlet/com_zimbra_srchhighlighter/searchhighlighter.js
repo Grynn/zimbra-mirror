4f733140f796dc3355d6baddcc0e951ac1cd2b70
 Unknown Perforce User <jwagner>
2013-01-24 00:39:34 +0000

    Bug: 79790 Error message for "Search" not displayed properly html encode search string
    http://bugzilla.zimbra.com/show_bug.cgi?id=79790

--- Zimlet/src/zimlet/com_zimbra_srchhighlighter/searchhighlighter.js
+++ Zimlet/src/zimlet/com_zimbra_srchhighlighter/searchhighlighter.js
@@ -77,7 +77,7 @@ SearchHighlighterZimlet.prototype.generateSpan =
 function(html, idx, obj, spanId, context) {
 	var id = Dwt.getNextId();
 	this._spanIds.push(id);
-	html[idx++] = ["<span id= '",id,"'class='ZmSearchResult'>",obj,"</span>"].join("");
+	html[idx++] = ["<span id= '",id,"'class='ZmSearchResult'>",AjxStringUtil.htmlEncode(obj),"</span>"].join("");
 	return idx;
 };
 
