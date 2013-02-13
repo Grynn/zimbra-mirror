4d5676421805e05b7df54e53aa7fa1e609176cc2
 Unknown Perforce User <rhoades>
2013-02-13 22:27:45 +0000

    Bug: 80287
    Selenium Harness: add test case for bug 80287
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=80287

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/bugs/Bug80287.java
@@ -0,0 +1,86 @@
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.bugs;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.core.Bugs;
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.DialogEditFolder;
+
+public class Bug80287 extends PrefGroupMailByMessageTest {
+
+	public Bug80287() {
+		logger.info("New " + Bug80287.class.getCanonicalName());
+
+		super.startingAccountPreferences.put("zimbraFeatureSharingEnabled", "FALSE");
+		
+	}
+
+	@Bugs(	ids = "80287")
+	@Test(
+			description = "Modify a basic retention (Context menu -> Edit -> Retention) (zimbraFeatureSharingEnabled=FALSE)", 
+			groups = { "functional" }
+			)
+	public void Bug80287_01() throws HarnessException {
+
+		//-- Data
+		
+		// Create the subfolder
+		String foldername = "folder" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+					"<CreateFolderRequest xmlns='urn:zimbraMail'>"
+				+		"<folder name='" + foldername + "' l='" +  FolderItem.importFromSOAP(app.zGetActiveAccount(), FolderItem.SystemFolder.Inbox).getId() + "'/>"
+				+	"</CreateFolderRequest>");
+
+		FolderItem folder = FolderItem.importFromSOAP(app.zGetActiveAccount(), foldername);
+		ZAssert.assertNotNull(folder, "Verify the subfolder is available");
+
+		// Add a retention policy
+		app.zGetActiveAccount().soapSend(
+				"<FolderActionRequest xmlns='urn:zimbraMail'>"
+			+		"<action id='" + folder.getId() + "' op='retentionpolicy'>"
+			+			"<retentionPolicy>"
+			+				"<keep>"
+			+					"<policy lifetime='5d' type='user'/>"
+			+				"</keep>"
+			+			"</retentionPolicy>"
+			+		"</action>"
+			+	"</FolderActionRequest>");
+
+		
+		
+		
+		//-- GUI
+		
+		// Click on Get Mail to refresh the folder list
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Rename the folder using context menu
+		DialogEditFolder dialog = (DialogEditFolder) app.zTreeMail.zTreeItem(Action.A_RIGHTCLICK, Button.B_TREE_EDIT, folder);
+
+		// Set to 4 years
+		dialog.zNavigateToTab(DialogEditFolder.DialogTab.Retention);
+		dialog.zRetentionSetRangeValue(6);
+
+		// Save
+		dialog.zClickButton(Button.B_OK);
+		
+		
+		//-- Verification
+		
+		// Verify the retention policy on the folder
+		app.zGetActiveAccount().soapSend(
+				"<GetFolderRequest xmlns='urn:zimbraMail'>"
+			+		"<folder l='" + folder.getId() + "'/>"
+			+	"</GetFolderRequest>");
+		String lifetime = app.zGetActiveAccount().soapSelectValue("//mail:keep//mail:policy", "lifetime");
+		
+		ZAssert.assertEquals(lifetime, "6d", "Verify the policy lifetime is set to 6 days");
+		
+	}
+
+
+}
