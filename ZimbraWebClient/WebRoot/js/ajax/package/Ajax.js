a39712655402b1ce0c68f7ce37cc6b065cb2861c
 Unknown Perforce User <alim>
2013-03-05 22:48:04 +0000

    Bug: none
    Adding the missing reference to jquery for Admin Console to work with the latest changes to DWT.

--- ZimbraWebClient/WebRoot/js/ajax/package/Ajax.js
+++ ZimbraWebClient/WebRoot/js/ajax/package/Ajax.js
@@ -131,5 +131,5 @@ AjxPackage.require("ajax.dwt.widgets.DwtRadioButtonGroup");
 AjxPackage.require("ajax.dwt.widgets.DwtForm");
 AjxPackage.require("ajax.dwt.widgets.DwtComboBox");
 
-
+AjxPackage.require("ajax.3rdparty.jquery.jquery");
 
