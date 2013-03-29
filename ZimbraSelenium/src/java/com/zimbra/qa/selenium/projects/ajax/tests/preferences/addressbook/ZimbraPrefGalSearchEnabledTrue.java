94f7e4d74e225384ce58ba4aa8d20370163e785f
 Unknown Perforce User <rhoades>
2013-03-29 22:35:52 +0000

    Bug: none
    Selenium Harness: add test cases for toggling the preference value for zimbraPrefGalSearchEnabled

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/addressbook/ZimbraPrefGalSearchEnabledTrue.java
@@ -0,0 +1,80 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.preferences.addressbook;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.AjaxCommonTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.preferences.TreePreferences.TreeItem;
+
+public class ZimbraPrefGalSearchEnabledTrue extends AjaxCommonTest {
+
+	public ZimbraPrefGalSearchEnabledTrue() {
+		
+		
+		super.startingPage = app.zPagePreferences;
+		super.startingAccountPreferences = new HashMap<String, String>() {
+			private static final long serialVersionUID = 1548564137395983698L;
+			{				
+				put("zimbraPrefGalSearchEnabled", "FALSE");
+			}
+		};
+		
+		
+	}
+
+	@Test(
+			description = "Set zimbraPrefGalSearchEnabled to 'TRUE'",
+			groups = { "functional" }
+	)
+	public void ZimbraPrefGalSearchEnabledTrue_01() throws HarnessException {
+
+		//-- DATA Setup
+		
+		
+		
+		//-- GUI Actions
+		
+		// Navigate to preferences -> mail -> composing
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.AddressBook);
+
+		
+		// Click radio button for "	Initially search the Global Address List when using the contact picker"
+		app.zPagePreferences.sClick("css=div[id$='INITIALLY_SEARCH_GAL_control'] input[id$='INITIALLY_SEARCH_GAL']");
+
+		// Click save
+		app.zPagePreferences.zToolbarPressButton(Button.B_SAVE);
+
+		
+		
+		//-- VERIFICATION
+		
+		app.zGetActiveAccount().soapSend(
+						"<GetPrefsRequest xmlns='urn:zimbraAccount'>"
+				+			"<pref name='zimbraPrefGalSearchEnabled'/>"
+				+		"</GetPrefsRequest>");
+
+		String value = app.zGetActiveAccount().soapSelectValue("//acct:pref[@name='zimbraPrefGalSearchEnabled']", null);
+		ZAssert.assertEquals(value, "TRUE", "Verify the zimbraPrefGalSearchEnabled preference was changed to 'TRUE'");
+
+	}
+	
+}
