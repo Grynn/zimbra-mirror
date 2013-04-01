2b6c3b13f28730ef1a5faf19cd0b0e86bdcaaa90
komalk <komalk@localhost>
2013-04-01 07:09:07 +0000

    bug:80713 - Enable production builds -Add a build target "prod-touch" in ZWC/build.xml which takes care of generating production build for touch client. -Since the "sencha app build production" target can only be run from a valid app directory, introduce a shell script which will cd to the app dir to run the command. TODO: Add "prod-touch" dependency in prod-deploy and prod-build targets.
    http://bugzilla.zimbra.com/show_bug.cgi?id=80713

---
+++ ZimbraWebClient/build-prod-touch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Touch client production build script.
+# Uses the build-prod-touch ant script to initiate touch client production build.
+
+# This script is executed through the ZWC build script, under target "build-prod".
+# Sencha production build needs to be executed from the directory where the sencha configuration exists(valid application directory). In our case, the  build script for the client exists in ZWC and 
+# our touch client application exists in ZWC/WebRoot/t. As a result, if we trigger the command to build the touch client from the main ant script, it fails because the current directory doesn't 
+# contain the Sencha application.
+
+# This is a way around to solve this issue. The root ant script executes this script which in turn executes the touch client build script to build it in production mode.
+
+cd WebRoot/t
+echo "[Touch client production build started]"
+ant -buildfile build-prod-touch.xml
+echo "[Touch client production build ended]"
+cd ../..
