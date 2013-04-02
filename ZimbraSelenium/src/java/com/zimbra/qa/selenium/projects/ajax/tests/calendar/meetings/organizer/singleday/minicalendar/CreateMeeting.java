fd281ce533f83ed17e7755184518b84c3c3bb942
Jitesh Sojitra <jsojitra@zimbra.com>
2013-04-02 08:46:06 +0000

    bug:none - Fixed quick add dialog related issues

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/minicalendar/CreateMeeting.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/meetings/organizer/singleday/minicalendar/CreateMeeting.java
@@ -24,6 +24,7 @@ import com.zimbra.qa.selenium.framework.util.*;
 import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
 import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
 import com.zimbra.qa.selenium.projects.ajax.ui.calendar.QuickAddAppointment;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew.Field;
 
 public class CreateMeeting extends CalendarWorkWeekTest {
 
@@ -38,8 +39,7 @@ public class CreateMeeting extends CalendarWorkWeekTest {
 	public void CreateMeeting_01() throws HarnessException {
 		
 		// Create appointment
-		AppointmentItem appt1 = new AppointmentItem();
-		AppointmentItem appt2 = new AppointmentItem();
+		AppointmentItem appt = new AppointmentItem();
 		Calendar now = this.calendarWeekDayUTC;
 		ZimbraResource location = new ZimbraResource(ZimbraResource.Type.LOCATION);
 		
@@ -49,45 +49,44 @@ public class CreateMeeting extends CalendarWorkWeekTest {
 		apptLocation = location.EmailAddress;
 		apptContent = ZimbraSeleniumProperties.getUniqueString();
 		
-		appt1.setSubject(apptSubject);
-		appt1.setStartTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0));
-		appt1.setEndTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0));
-		appt2.setAttendees(apptAttendee);
-		appt1.setLocation(apptLocation);
-		appt2.setContent(apptContent);
+		appt.setSubject(apptSubject);
+		appt.setStartTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0));
+		appt.setEndTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0));
+		appt.setLocation(apptLocation);
 	
 		// Quick add appointment dialog
 		QuickAddAppointment quickAddAppt = new QuickAddAppointment(app) ;
 		quickAddAppt.zNewAppointmentUsingMiniCal();
-		quickAddAppt.zFill(appt1);
+		quickAddAppt.zFill(appt);
 		quickAddAppt.zMoreDetails();
 		
 		// Add attendees and body from main form
 		FormApptNew apptForm = new FormApptNew(app);
-		apptForm.zFill(appt2);
+        apptForm.zFillField(Field.Attendees, apptAttendee);
+        apptForm.zFillField(Field.Body, apptContent);
 		apptForm.zSubmit();
 		SleepUtil.sleepVeryLong(); // test fails while checking free/busy status, waitForPostqueue is not sufficient here
         // Tried sleepLong() as well but although fails so using sleepVeryLong()
 		
 		// Verify appointment exists on the server
-		AppointmentItem actual = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ appt1.getSubject() +")", appt1.getStartTime().addDays(-7), appt1.getEndTime().addDays(7));
+		AppointmentItem actual = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ apptSubject +")", appt.getStartTime().addDays(-7), appt.getEndTime().addDays(7));
 		ZAssert.assertNotNull(actual, "Verify the new appointment is created");
-		ZAssert.assertEquals(actual.getSubject(), appt1.getSubject(), "Subject: Verify the appointment data");
+		ZAssert.assertEquals(actual.getSubject(), apptSubject, "Subject: Verify the appointment data");
 		ZAssert.assertEquals(actual.getAttendees(), apptAttendee, "Attendees: Verify the appointment data");
 		ZAssert.assertEquals(actual.getLocation(), apptLocation, "Loction: Verify the appointment data");
-		ZAssert.assertEquals(actual.getContent(), appt2.getContent(), "Content: Verify the appointment data");
+		ZAssert.assertEquals(actual.getContent(), apptContent, "Content: Verify the appointment data");
 
 		// Verify the attendee receives the meeting
-		AppointmentItem received = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ appt1.getSubject() +")", appt1.getStartTime().addDays(-7), appt1.getEndTime().addDays(7));
-		ZAssert.assertEquals(received.getSubject(), appt1.getSubject(), "Subject: Verify the appointment data");
+		AppointmentItem received = AppointmentItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")", appt.getStartTime().addDays(-7), appt.getEndTime().addDays(7));
+		ZAssert.assertEquals(received.getSubject(), apptSubject, "Subject: Verify the appointment data");
 		ZAssert.assertEquals(received.getAttendees(), apptAttendee, "Attendees: Verify the appointment data");
 		ZAssert.assertEquals(actual.getLocation(), apptLocation, "Loction: Verify the appointment data");
-		ZAssert.assertEquals(received.getContent(), appt2.getContent(), "Content: Verify the appointment data");
+		ZAssert.assertEquals(received.getContent(), apptContent, "Content: Verify the appointment data");
 
 		// Verify the attendee receives the invitation
-		MailItem invite = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ appt1.getSubject() +")");
+		MailItem invite = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ apptSubject +")");
 		ZAssert.assertNotNull(invite, "Verify the invite is received");
-		ZAssert.assertEquals(invite.dSubject, appt1.getSubject(), "Subject: Verify the appointment data");
+		ZAssert.assertEquals(invite.dSubject, apptSubject, "Subject: Verify the appointment data");
 		
 		// Verify location free/busy status shows as psts=AC	
 		String locationStatus = app.zGetActiveAccount().soapSelectValue("//mail:at[@a='"+ apptLocation +"']", "ptst");
