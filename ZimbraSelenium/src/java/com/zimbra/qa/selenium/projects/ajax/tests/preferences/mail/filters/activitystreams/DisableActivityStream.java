af607229c43c9be19c3016e18dcc0f4fd688b9ad
 Unknown Perforce User <rhoades>
2013-04-11 00:36:39 +0000

    Bug: none
    Selenium Harness: add test case for disabling activity streams.
    
    1. Add test case for disabling activity streams
    2. Add check that the filter is active when enabling activity streams

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/mail/filters/activitystreams/DisableActivityStream.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/mail/filters/activitystreams/DisableActivityStream.java
@@ -16,7 +16,10 @@
  */
 package com.zimbra.qa.selenium.projects.ajax.tests.preferences.mail.filters.activitystreams;
 
-import org.testng.annotations.Test;
+import java.lang.reflect.Method;
+
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
@@ -25,21 +28,35 @@ import com.zimbra.qa.selenium.projects.ajax.ui.preferences.DialogActivityStream;
 import com.zimbra.qa.selenium.projects.ajax.ui.preferences.TreePreferences.TreeItem;
 
 
-public class EnableActivityStream extends AjaxCommonTest {
+public class DisableActivityStream extends AjaxCommonTest {
 
-	public EnableActivityStream() {
+	public DisableActivityStream() {
 		
 		super.startingPage = app.zPagePreferences;
 		super.startingAccountPreferences = null;
 		
 	}
 
+	/**
+	 * It is difficult to enable Activity Streams using SOAP
+	 * Use the client to do so before each test method
+	 */
+	@BeforeMethod( groups = { "always" } )
+	public void DisableActivityStream_BeforeMethod(Method method, ITestContext testContext) throws HarnessException {
 
+		app.zPagePreferences.zNavigateTo();
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.MailFilters);
+		DialogActivityStream dialog = (DialogActivityStream)app.zPagePreferences.zToolbarPressButton(Button.B_ACTIVITY_STREAM_SETTINGS);
+		dialog.zClickCheckbox(Button.B_ACTIVITY_STREAM_ENABLE, true);
+		dialog.zClickButton(Button.B_SAVE);
+
+	}
+	
 	@Test(
-			description = "Enable the Activity Streams feature",
+			description = "Disable the Activity Streams feature",
 			groups = { "functional" }
 			)
-	public void EnableActivityStream_01() throws HarnessException {
+	public void DisableActivityStream_01() throws HarnessException {
 
 		//-- DATA
 		
@@ -55,7 +72,7 @@ public class EnableActivityStream extends AjaxCommonTest {
 		DialogActivityStream dialog = (DialogActivityStream)app.zPagePreferences.zToolbarPressButton(Button.B_ACTIVITY_STREAM_SETTINGS);
 		
 		// Check "Enable"
-		dialog.zClickCheckbox(Button.B_ACTIVITY_STREAM_ENABLE, true);
+		dialog.zClickCheckbox(Button.B_ACTIVITY_STREAM_ENABLE, false);
 		
 		// Save
 		dialog.zClickButton(Button.B_SAVE);
@@ -74,7 +91,7 @@ public class EnableActivityStream extends AjaxCommonTest {
 		
 		String active = app.zGetActiveAccount().soapSelectValue("//mail:filterRule[@name='Activity Stream']", "active");
 		
-		ZAssert.assertEquals(active, "1", "Verify the activity stream filter is active");
+		ZAssert.assertEquals(active, "0", "Verify the activity stream filter is disabled");
 
 	}
 }
