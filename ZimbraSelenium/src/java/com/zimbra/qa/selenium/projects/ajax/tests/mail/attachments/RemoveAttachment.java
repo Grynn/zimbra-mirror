e901b74b24036efc498665ad8eaeab8a5bb991f0
 Unknown Perforce User <rhoades>
2013-05-08 21:08:26 +0000

    Bug: 81565
    Selenium Harness: add tests for removing attachments from invitations.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=81565

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/attachments/RemoveAttachment.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/attachments/RemoveAttachment.java
@@ -22,6 +22,7 @@ import java.util.List;
 import org.testng.annotations.Test;
 
 import com.zimbra.common.soap.Element;
+import com.zimbra.qa.selenium.framework.core.*;
 import com.zimbra.qa.selenium.framework.items.*;
 import com.zimbra.qa.selenium.framework.ui.*;
 import com.zimbra.qa.selenium.framework.util.*;
@@ -133,6 +134,170 @@ public class RemoveAttachment extends PrefGroupMailByMessageTest {
 		
 		final String mimeFile = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/mime/email06/mime.txt";
 		final String subject = "subject135219672356274";
+		ZimbraAccount account = app.zGetActiveAccount();
+		
+		// Inject the message
+		LmtpInject.injectFile(app.zGetActiveAccount().EmailAddress, new File(mimeFile));
+
+		// Double check that there is an attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+			+		"<query>subject:("+ subject +")</query>"
+			+	"</SearchRequest>");
+		String id = account.soapSelectValue("//mail:m", "id");
+		
+		account.soapSend(
+				"<GetMsgRequest xmlns='urn:zimbraMail' >"
+			+		"<m id='"+ id +"'/>"
+			+	"</GetMsgRequest>");
+		Element[] nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+		ZAssert.assertGreaterThan(nodes.length, 0, "Verify the message has the attachment");
+
+
+		//-- GUI actions
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail display = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+		ZAssert.assertNotNull(display, "Verify the message shows");
+		
+		// Click remove
+		DialogWarning dialog = (DialogWarning)display.zPressButton(Button.B_REMOVE_ALL);
+		dialog.zClickButton(Button.B_YES);
+
+		
+		//-- Verification
+
+		// Verify the message no longer has an attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+			+		"<query>subject:("+ subject +")</query>"
+			+	"</SearchRequest>");
+		id = account.soapSelectValue("//mail:m", "id");
+		
+		
+		try{
+			
+			int i = 0;
+			do {
+				SleepUtil.sleepSmall();
+
+		    	account.soapSend(
+		    			  "<GetMsgRequest xmlns='urn:zimbraMail' >"
+		    			+   "<m id='"+ id +"'/>"
+		    			+ "</GetMsgRequest>");
+		    	nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+
+			} while ( (i++ < 10) && (nodes.length > 0) );
+			
+		    
+		}catch(Exception ex){
+		    logger.error(ex);
+		}
+		
+		ZAssert.assertEquals(nodes.length, 0, "Verify the message no longer has the attachment");
+		
+
+	}
+
+	@Bugs(	ids = "81565")
+	@Test(	description = "Remove an attachment from a meeting invite",
+			groups = { "functional" })
+	public void RemoveAttachment_03() throws HarnessException {
+
+		//-- Data Setup
+		
+		final String mimeFile = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/mime/Bugs/Bug81565/mime1.txt";
+		final String subject = "Bug81565";
+		final String attachmentname = "Capture.PNG";
+		ZimbraAccount account = app.zGetActiveAccount();
+		
+		// Inject the message
+		LmtpInject.injectFile(app.zGetActiveAccount().EmailAddress, new File(mimeFile));
+
+		// Double check that there is an attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+			+		"<query>subject:("+ subject +")</query>"
+			+	"</SearchRequest>");
+		String id = account.soapSelectValue("//mail:m", "id");
+		
+		account.soapSend(
+				"<GetMsgRequest xmlns='urn:zimbraMail' >"
+			+		"<m id='"+ id +"'/>"
+			+	"</GetMsgRequest>");
+		Element[] nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+		ZAssert.assertGreaterThan(nodes.length, 0, "Verify the message has the attachment");
+
+
+		//-- GUI actions
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		DisplayMail display = (DisplayMail) app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		AttachmentItem item = null;
+		List<AttachmentItem> items = display.zListGetAttachments();
+		for (AttachmentItem i : items) {
+			if ( i.getAttachmentName().equals(attachmentname)) {
+				item = i;
+				break;
+			}
+		}
+		ZAssert.assertNotNull(item, "Verify one attachment is in the message");
+		
+		// Click remove
+		DialogWarning dialog = (DialogWarning)display.zListAttachmentItem(Button.B_REMOVE, item);
+		dialog.zClickButton(Button.B_YES);
+
+		
+		//-- Verification
+
+		// Verify the message no longer has an attachment
+		account.soapSend(
+				"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+			+		"<query>subject:("+ subject +")</query>"
+			+	"</SearchRequest>");
+		id = account.soapSelectValue("//mail:m", "id");
+		
+		
+		try{
+			
+			int i = 0;
+			do {
+				SleepUtil.sleepSmall();
+
+		    	account.soapSend(
+		    			  "<GetMsgRequest xmlns='urn:zimbraMail' >"
+		    			+   "<m id='"+ id +"'/>"
+		    			+ "</GetMsgRequest>");
+		    	nodes = account.soapSelectNodes("//mail:mp[@cd='attachment']");
+
+			} while ( (i++ < 10) && (nodes.length > 0) );
+			
+		    
+		}catch(Exception ex){
+		    logger.error(ex);
+		}
+		
+		ZAssert.assertEquals(nodes.length, 0, "Verify the message no longer has the attachment");
+		
+
+	}
+
+	@Bugs(	ids = "81565")
+	@Test(	description = "Remove all attachments (2 attachments) from a meeting invite",
+			groups = { "functional" })
+	public void RemoveAttachment_04() throws HarnessException {
+
+		//-- Data Setup
+		
+		final String mimeFile = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/mime/Bugs/Bug81565/mime2.txt";
+		final String subject = "Bug81565B";
 		ZimbraAccount account = app.zGetActiveAccount();
 		
 		// Inject the message
