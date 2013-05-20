192f1509790fe1fb4e995ea7b0dfa7a2cb3df1c7
 Unknown Perforce User <rhoades>
2013-05-20 22:10:04 +0000

    Bug: 71403
    Selenium Harness: add test cases for zimbraFeatureMailForwardingEnabled.  Selenium tests concentrate on the GUI presentation of the preference.  SOAP tests should cover the functionality.
    
    1. Make sure the field is disabled if zimbraFeatureMailForwardingEnabled = FALSE
    2. Make sure no duplicate labels exist if zimbraFeatureMailForwardingEnabled = FALSE (bug 71403)
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=71403

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/preferences/attributes/ZimbraFeatureMailForwardingEnabled.java
@@ -0,0 +1,118 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.preferences.attributes;
+
+import java.util.*;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.core.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.*;
+import com.zimbra.qa.selenium.projects.ajax.ui.preferences.TreePreferences.*;
+
+
+
+public class ZimbraFeatureMailForwardingEnabled extends AjaxCommonTest {
+	
+	public ZimbraFeatureMailForwardingEnabled() {
+		logger.info("New "+ ZimbraFeatureMailForwardingEnabled.class.getCanonicalName());
+
+		// All tests start at the login page
+		super.startingPage = app.zPageLogin;
+		super.startingAccountPreferences = new HashMap<String , String>() {
+			private static final long serialVersionUID = 2672327300661475816L;
+		{
+		    put("zimbraFeatureMailForwardingEnabled", "FALSE");
+		}};
+		
+	}
+	
+	@Test(	description = "Verify preferences does not show 'Forward a copy to', if zimbraFeatureMailForwardingEnabled=FALSE",
+			groups = { "functional" })
+	public void zimbraFeatureMailForwardingEnabled_01() throws HarnessException {
+		
+		//-- DATA		
+		
+		
+		//-- GUI
+		
+		// Login
+		app.zPageLogin.zLogin(ZimbraAccount.AccountZWC());
+
+		// Go to preferences - mail
+		app.zPagePreferences.zNavigateTo();
+		
+		//
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Mail);
+		
+		
+
+
+		//-- VERIFICATION
+		
+
+		// Verify the field is not present
+		String locator = "css=input[id$='MAIL_FORWARDING_ADDRESS']";
+		boolean exists = app.zPagePreferences.sIsElementPresent(locator);
+		ZAssert.assertFalse(exists, "Verify the forwarding address field is not present");
+
+		locator = "css=td.ZOptionsField:contains('Forward a copy to:')";	// TODO: I18N
+		exists = app.zPagePreferences.sIsElementPresent(locator);
+		ZAssert.assertFalse(exists, "Verify the 'Forward a copy to:' label is not present");
+
+	}
+
+
+	@Bugs(ids = "71403")
+	@Test(	description = "Bug 71403: Verify duplicate message lables do not exist in preferences ('When a message arrives')",
+			groups = { "functional" })
+	public void zimbraFeatureMailForwardingEnabled_02() throws HarnessException {
+		
+		//-- DATA		
+		
+		
+		//-- GUI
+		
+		// Login
+		app.zPageLogin.zLogin(ZimbraAccount.AccountZWC());
+
+		// Go to preferences - mail
+		app.zPagePreferences.zNavigateTo();
+		
+		//
+		app.zTreePreferences.zTreeItem(Action.A_LEFTCLICK, TreeItem.Mail);
+		
+		
+
+
+		//-- VERIFICATION
+		
+
+		// Verify the field is not present
+		String locator = "css=td.ZOptionsLabel:contains('Message Arrival')";	// TODO: I18N
+		
+		boolean exists = app.zPagePreferences.sIsElementPresent(locator);
+		ZAssert.assertTrue(exists, "Verify the 'Message Arrival' label exists");
+		
+		int count = app.zPagePreferences.sGetCssCount(locator);
+		ZAssert.assertEquals(count, 1, "Verify only 1 'Message Arrival' label exists");
+
+
+	}
+
+
+}
