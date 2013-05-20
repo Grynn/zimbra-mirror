daa33a3e6c2b0758868c7ee6040aebfc0622f9cd
 Unknown Perforce User <rhoades>
2013-05-20 22:32:09 +0000

    Bug: 69384, 67686
    Selenium Harness: verify extra draft message is not saved when doing reply-all.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=69384
    http://bugzilla.zimbra.com/show_bug.cgi?id=67686

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/bugs/Bug67686.java
@@ -0,0 +1,89 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.mail.bugs;
+
+import java.io.*;
+
+import org.testng.annotations.*;
+
+import com.zimbra.common.soap.*;
+import com.zimbra.qa.selenium.framework.core.*;
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.*;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.*;
+
+
+public class Bug67686 extends PrefGroupMailByMessageTest {
+
+
+	
+	public Bug67686() {
+		logger.info("New "+ Bug67686.class.getCanonicalName());
+
+
+	}
+
+	@Bugs(	ids = "67686, 69384")
+	@Test(	description = "Verify bug 67686",
+			groups = { "functional" })
+	public void Bug67686_01() throws HarnessException {
+
+		//-- DATA
+		
+		String subject = "subject13690880312762";
+
+		String MimeFolder = ZimbraSeleniumProperties.getBaseDirectory() + "/data/public/mime/Bugs/Bug67686";
+		LmtpInject.injectFile(ZimbraAccount.AccountZWC().EmailAddress, new File(MimeFolder));
+
+		
+		//-- GUI
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the item
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Reply the item
+		FormMailNew mailform = (FormMailNew) app.zPageMail.zToolbarPressButton(Button.B_REPLYALL);
+
+		// Send the message
+		mailform.zSubmit();
+
+		
+		//-- VERIFICATION
+		
+		// Verify no draft messages exist
+		FolderItem drafts = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Drafts);
+		app.zGetActiveAccount().soapSend(
+					"<SearchRequest xmlns='urn:zimbraMail' types='message'>"
+				+		"<query>subject:("+ subject +") inid:"+ drafts.getId() +"</query>"
+				+	"</SearchRequest>");
+
+		
+		Element[] nodes = app.zGetActiveAccount().soapSelectNodes("//mail:m");
+		ZAssert.assertEquals(nodes.length, 0, "Verify no drafts are saved");
+		
+		
+	}
+
+
+
+}
