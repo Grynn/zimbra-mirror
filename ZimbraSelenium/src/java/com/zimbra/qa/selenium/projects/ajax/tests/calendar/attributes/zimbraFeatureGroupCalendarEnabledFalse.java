e11c155249ab7c92cdae8291a573daa144ec59ff
 Unknown Perforce User <rrao>
2013-05-29 06:47:19 +0000

    bug:none added 2 new tests to check if appt can be created and modified when zimbraFeatureHtmlComposeEnabled zimbraFeatureGroupCalendarEnabled attributes are set to be FALSE resp.

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/attributes/zimbraFeatureGroupCalendarEnabledFalse.java
@@ -0,0 +1,104 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.attributes;
+
+import java.util.Calendar;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.ui.Action;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.SleepUtil;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
+import com.zimbra.qa.selenium.framework.util.ZTimeZone;
+import com.zimbra.qa.selenium.framework.util.ZimbraAdminAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+
+public class zimbraFeatureGroupCalendarEnabledFalse extends CalendarWorkWeekTest {
+
+	public zimbraFeatureGroupCalendarEnabledFalse() {
+		logger.info("New "+ zimbraFeatureGroupCalendarEnabledFalse.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+	
+	@Test(
+			description = "Bug 56440 - If zimbraFeatureGroupCalendarEnabled to FALSE then check if user can edit exisiting appt", 
+			groups = { "functional" })
+	public void zimbraFeatureGroupCalendarEnabledFalse_01() throws HarnessException {
+
+		// Modify the test account and change zimbraFeatureGroupCalendarEnabled to FALSE
+		ZimbraAdminAccount.GlobalAdmin().soapSend(
+				"<ModifyAccountRequest xmlns='urn:zimbraAdmin'>"
+			+		"<id>"+ app.zGetActiveAccount().ZimbraId +"</id>"
+			+		"<a n='zimbraFeatureGroupCalendarEnabled'>FALSE</a>"
+			+	"</ModifyAccountRequest>");
+
+		// Logout and login to pick up the changes
+		app.zPageLogin.zNavigateTo();
+		this.startingPage.zNavigateTo();
+		
+		// Creating object for appointment data
+		AppointmentItem appt = new AppointmentItem();
+		String tz, apptSubject, apptBody, editApptSubject, editApptBody;
+		tz = ZTimeZone.TimeZoneEST.getID();
+		apptSubject =  "Edited" + ZimbraSeleniumProperties.getUniqueString();
+		apptBody =  "Edited" + ZimbraSeleniumProperties.getUniqueString();
+		editApptSubject = "Edited" + ZimbraSeleniumProperties.getUniqueString();
+        editApptBody =  "Edited" + ZimbraSeleniumProperties.getUniqueString();
+		
+		// Absolute dates in UTC zone
+		Calendar now = this.calendarWeekDayUTC;
+		ZDate startUTC = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0);
+		ZDate endUTC   = new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0);
+		
+        app.zGetActiveAccount().soapSend(
+                          "<CreateAppointmentRequest xmlns='urn:zimbraMail'>" +
+                               "<m>"+
+                               "<inv method='REQUEST' type='event' fb='B' transp='O' allDay='0' name='"+ apptSubject +"'>"+
+                               "<s d='"+ startUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                               "<e d='"+ endUTC.toTimeZone(tz).toYYYYMMDDTHHMMSS() +"' tz='"+ tz +"'/>" +
+                               "<or a='"+ app.zGetActiveAccount().EmailAddress +"'/>" +
+                               "</inv>" +
+                               "<mp content-type='text/plain'>" +
+                               "<content>"+ apptBody +"</content>" +
+                               "</mp>" +
+                               "<su>"+ apptSubject +"</su>" +
+                               "</m>" +
+                         "</CreateAppointmentRequest>");
+
+        String apptId = app.zGetActiveAccount().soapSelectValue("//mail:CreateAppointmentResponse", "apptId");
+    
+        app.zPageCalendar.zToolbarPressButton(Button.B_REFRESH);
+
+        // Open appointment and check if appt can be modified and saved
+        app.zPageCalendar.zListItem(Action.A_DOUBLECLICK, apptSubject);
+        SleepUtil.sleepMedium();
+        FormApptNew apptForm = new FormApptNew(app);
+        appt.setSubject(editApptSubject);
+        appt.setContent(editApptBody);
+        apptForm.zFill(appt);
+        apptForm.zToolbarPressButton(Button.B_SAVEANDCLOSE);
+        
+        // Use GetAppointmentRequest to verify the changes are saved
+        app.zGetActiveAccount().soapSend("<GetAppointmentRequest  xmlns='urn:zimbraMail' id='"+ apptId +"'/>");
+        ZAssert.assertEquals(app.zGetActiveAccount().soapMatch("//mail:GetAppointmentResponse//mail:comp", "name", editApptSubject), true, "");
+        ZAssert.assertEquals(app.zGetActiveAccount().soapMatch("//mail:GetAppointmentResponse//mail:desc", null, editApptBody), true, "");
+	}
+}
---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/calendar/attributes/zimbraFeatureHtmlComposeEnabledFalse.java
@@ -0,0 +1,77 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.calendar.attributes;
+
+import java.util.Calendar;
+import org.testng.annotations.Test;
+import com.zimbra.qa.selenium.framework.items.AppointmentItem;
+import com.zimbra.qa.selenium.framework.ui.Button;
+import com.zimbra.qa.selenium.framework.util.HarnessException;
+import com.zimbra.qa.selenium.framework.util.ZAssert;
+import com.zimbra.qa.selenium.framework.util.ZDate;
+import com.zimbra.qa.selenium.framework.util.ZimbraAdminAccount;
+import com.zimbra.qa.selenium.framework.util.ZimbraSeleniumProperties;
+import com.zimbra.qa.selenium.projects.ajax.core.CalendarWorkWeekTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.calendar.FormApptNew;
+
+public class zimbraFeatureHtmlComposeEnabledFalse extends CalendarWorkWeekTest {
+
+	public zimbraFeatureHtmlComposeEnabledFalse() {
+		logger.info("New "+ zimbraFeatureHtmlComposeEnabledFalse.class.getCanonicalName());
+		super.startingPage = app.zPageCalendar;
+	}
+	
+	@Test(
+			description = "Bug 59504 - If zimbraFeatureHtmlComposeEnabled to FALSE then check if user can create appt", 
+			groups = { "functional" })
+	public void zimbraFeatureHtmlComposeEnabledFalse_01() throws HarnessException {
+		
+		// Modify the test account and change zimbraFeatureHtmlComposeEnabled to FALSE
+		ZimbraAdminAccount.GlobalAdmin().soapSend(
+				"<ModifyAccountRequest xmlns='urn:zimbraAdmin'>"
+			+		"<id>"+ app.zGetActiveAccount().ZimbraId +"</id>"
+			+		"<a n='zimbraFeatureHtmlComposeEnabled'>FALSE</a>"
+			+	"</ModifyAccountRequest>");
+
+		// Logout and login to pick up the changes
+		app.zPageLogin.zNavigateTo();
+		this.startingPage.zNavigateTo();
+
+		
+		// Create appointment
+		AppointmentItem appt = new AppointmentItem();
+		Calendar now = this.calendarWeekDayUTC;
+		appt.setSubject("appointment" + ZimbraSeleniumProperties.getUniqueString());
+		appt.setStartTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 12, 0, 0));
+		appt.setEndTime(new ZDate(now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1, now.get(Calendar.DAY_OF_MONTH), 14, 0, 0));
+		appt.setContent("content" + ZimbraSeleniumProperties.getUniqueString());
+		
+		// Open new appt page and check if the appt can be composed
+		FormApptNew apptForm = (FormApptNew) app.zPageCalendar.zToolbarPressButton(Button.B_NEW);
+		ZAssert.assertNotNull(apptForm, "Verify the new form opened");
+
+		// Fill out the form with the appt data & send it out
+		apptForm.zFill(appt);
+		apptForm.zSubmit();
+			
+		// Verify the new appointment exists on the server
+		AppointmentItem actual = AppointmentItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ appt.getSubject() +")", appt.getStartTime().addDays(-7), appt.getEndTime().addDays(7));
+		ZAssert.assertNotNull(actual, "Verify the new appointment is created");
+		ZAssert.assertEquals(actual.getSubject(), appt.getSubject(), "Subject: Verify the appointment data");
+		ZAssert.assertEquals(actual.getContent(), appt.getContent(), "Content: Verify the appointment data");
+	}
+}
