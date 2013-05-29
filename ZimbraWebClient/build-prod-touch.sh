44d56c39f940f7f0b8016db63600ab010e44b865
Vishal Mahajan <vmahajan@zimbra.com>
2013-05-29 11:24:03 +0000

    bug: 82405 - Upgrade to Sencha Cmd 3.1
    - Fixed touch client build which got broken after sencha framework 2.2 upgrade. Upgraded to sencha cmd 3.1.2.342.
    - Ran 'SenchaCmd/3.1.2.342/sencha app upgrade ./touch' command in the 't' folder to do the upgrade.
    - Fixed a few missing semi-colons
    - Put a few event names under quotes in ZtContactController without which the sencha build was weirdly failing at the compression stage probably due to probably overlap with some framework keywords. Ajinkya helped me identify this issue.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82405

--- ZimbraWebClient/build-prod-touch.sh
+++ ZimbraWebClient/build-prod-touch.sh
@@ -11,6 +11,6 @@
 
 cd WebRoot/t
 echo "[Touch client production build started]"
-ant -buildfile build-prod-touch.xml -Dbuild.mode=$1
+SenchaCmd/3.1.2.342/sencha app build $1
 echo "[Touch client production build ended]"
 cd ../..
