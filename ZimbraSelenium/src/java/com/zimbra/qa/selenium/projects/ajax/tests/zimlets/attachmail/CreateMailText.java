a788a8a8a5223deb1e62195cea79a89bb82cbc36
 Unknown Perforce User <rhoades>
2013-06-05 20:50:09 +0000

    Bug: 82354
    Selenium Harness: add stub test case for attaching an email to a message.  Test steps are dependent on ID's being added to the DOM, as per bug 82354
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82354

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/attachmail/CreateMailText.java
@@ -0,0 +1,94 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011, 2012, 2013 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.zimlets.attachmail;
+
+import org.testng.annotations.*;
+
+import com.zimbra.qa.selenium.framework.items.*;
+import com.zimbra.qa.selenium.framework.items.FolderItem.*;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.*;
+import com.zimbra.qa.selenium.projects.ajax.ui.mail.*;
+
+
+public class CreateMailText extends PrefGroupMailByMessageTest {
+
+	public CreateMailText() {
+		logger.info("New "+ CreateMailText.class.getCanonicalName());
+		
+		
+		
+		super.startingAccountPreferences.put("zimbraPrefComposeFormat", "text");
+		
+	}
+	
+	@Test(	description = "Attach an email to a mail",
+			groups = { "functional" })
+	public void CreateMailText_01() throws HarnessException {
+		
+		//-- DATA
+		
+		// Create an email to attach
+		final FolderItem inbox = FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox);
+		final String subject = "attached"+ ZimbraSeleniumProperties.getUniqueString();
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='"+ inbox.getId() +"' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"simple text string in the body\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+
+		
+		
+		// Create the message data to be sent
+		MailItem mail = new MailItem();
+		mail.dToRecipients.add(new RecipientItem(ZimbraAccount.AccountA()));
+		mail.dSubject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		mail.dBodyText = "body" + ZimbraSeleniumProperties.getUniqueString();
+		
+		
+		//-- GUI
+		
+		// Click Get Mail button to get the new contact
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Open the new mail form
+		FormMailNew mailform = (FormMailNew) app.zPageMail.zToolbarPressButton(Button.B_NEW);
+		ZAssert.assertNotNull(mailform, "Verify the new form opened");
+		
+		
+		throw new HarnessException("See https://bugzilla.zimbra.com/show_bug.cgi?id=82354");
+		
+		// Fill out the form with the data
+		// mailform.zFill(mail);
+		
+		
+	}
+
+	
+
+
+}
