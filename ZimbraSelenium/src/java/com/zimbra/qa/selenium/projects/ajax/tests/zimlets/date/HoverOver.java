679467b07eb7bdc83df96c7c117e12bfdce08010
 Unknown Perforce User <rhoades>
2013-06-05 20:42:26 +0000

    Bug: none
    Selenium Harness: add test case for hover over date in a message

---
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/zimlets/date/HoverOver.java
@@ -0,0 +1,95 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2013 VMware, Inc.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.3 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
+package com.zimbra.qa.selenium.projects.ajax.tests.zimlets.date;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import com.zimbra.qa.selenium.framework.items.FolderItem;
+import com.zimbra.qa.selenium.framework.items.FolderItem.SystemFolder;
+import com.zimbra.qa.selenium.framework.ui.*;
+import com.zimbra.qa.selenium.framework.util.*;
+import com.zimbra.qa.selenium.projects.ajax.core.PrefGroupMailByMessageTest;
+import com.zimbra.qa.selenium.projects.ajax.ui.TooltipContact;
+
+
+public class HoverOver extends PrefGroupMailByMessageTest {
+
+	
+	public HoverOver() throws HarnessException {
+		logger.info("New "+ HoverOver.class.getCanonicalName());
+		
+	}
+	
+	@Test(	description = "Hover over a date in a message body",
+			groups = { "functional" })
+	public void HoverOver_01() throws HarnessException {
+
+		//-- DATA Setup
+		final String date = "12/25/2016";
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+
+		app.zGetActiveAccount().soapSend(
+				"<AddMsgRequest xmlns='urn:zimbraMail'>"
+    		+		"<m l='" + FolderItem.importFromSOAP(app.zGetActiveAccount(), SystemFolder.Inbox).getId() + "' >"
+        	+			"<content>From: foo@foo.com\n"
+        	+				"To: foo@foo.com \n"
+        	+				"Subject: "+ subject +"\n"
+        	+				"MIME-Version: 1.0 \n"
+        	+				"Content-Type: text/plain; charset=utf-8 \n"
+        	+				"Content-Transfer-Encoding: 7bit\n"
+        	+				"\n"
+        	+				"Line 1\n"
+        	+				"abc "+ date +" def\n"
+        	+				"Line 2\n"
+        	+			"</content>"
+        	+		"</m>"
+			+	"</AddMsgRequest>");
+
+
+		
+		
+		//-- GUI Actions
+		
+		
+		// Click Get Mail button
+		app.zPageMail.zToolbarPressButton(Button.B_GETMAIL);
+
+		// Select the message so that it shows in the reading pane
+		app.zPageMail.zListItem(Action.A_LEFTCLICK, subject);
+
+		// Hover over the email address
+		String locator = "css=span[id$='_com_zimbra_date']:contains("+ date + ")";
+		app.zPageMail.sMouseOver(locator, (WebElement[]) null);
+		
+		
+		
+		//-- VERIFICATION
+		
+		
+		// Verify the contact tool tip opens
+		TooltipContact tooltip = new TooltipContact(app);
+		tooltip.zWaitForActive();
+		
+		ZAssert.assertTrue(tooltip.zIsActive(), "Verify the tooltip shows");
+		
+		
+	}
+	
+
+
+}
