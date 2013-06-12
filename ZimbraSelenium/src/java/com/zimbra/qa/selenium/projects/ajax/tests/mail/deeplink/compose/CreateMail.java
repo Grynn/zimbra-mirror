81d1dd69f9ce1cf23b69396f89446f7dc833da6f
 Unknown Perforce User <rhoades>
2013-06-12 21:30:29 +0000

    Bug: 82734
    Selenium Harness: update deep link compose test cases
    
    1. Associate CreateMail_03 with bug 82734
    2. Add test case CreateMail_04 with comma separated email address
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=82734

--- ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/deeplink/compose/CreateMail.java
+++ ZimbraSelenium/src/java/com/zimbra/qa/selenium/projects/ajax/tests/mail/deeplink/compose/CreateMail.java
@@ -136,8 +136,9 @@ public class CreateMail extends PrefGroupMailByMessageTest {
 		
 	}
 
+	@Bugs(	ids = "82734")
 	@Test(	description = "Create a mail with two 'to' adn two 'cc' addresses using the deep link URL",
-			groups = { "functional", "matt" })
+			groups = { "functional" })
 	public void CreateMail_03() throws HarnessException {
 		
 		
@@ -184,15 +185,108 @@ public class CreateMail extends PrefGroupMailByMessageTest {
 		//-- VERIFICATION
 		MailItem sent = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
 		ZAssert.assertNotNull(sent, "Verify the message is received correctly");
-		ZAssert.assertEquals(sent.dToRecipients.get(0).dEmailAddress, ZimbraAccount.AccountA().EmailAddress, "Verify the to field is correct");
-		ZAssert.assertEquals(sent.dCcRecipients.get(0).dEmailAddress, ZimbraAccount.AccountB().EmailAddress, "Verify the cc field is correct");
-		ZAssert.assertEquals(sent.dBccRecipients.get(0).dEmailAddress, ZimbraAccount.AccountC().EmailAddress, "Verify the bcc field is correct");
-
-		MailItem received = MailItem.importFromSOAP(ZimbraAccount.AccountA(), "subject:("+ subject +")");
-		ZAssert.assertNotNull(received, "Verify the message is received correctly");
+		
+		boolean found1 = false;
+		boolean found2 = false;
+		boolean found3 = false;
+		boolean found4 = false;
+		for ( RecipientItem r : sent.dToRecipients) {
+			if ( r.dEmailAddress.equals(account1.EmailAddress) ) {
+				found1 = true;
+			}
+			if ( r.dEmailAddress.equals(account2.EmailAddress) ) {
+				found2 = true;
+			}
+		}
+		for ( RecipientItem r : sent.dCcRecipients) {
+			if ( r.dEmailAddress.equals(account3.EmailAddress) ) {
+				found3 = true;
+			}
+			if ( r.dEmailAddress.equals(account4.EmailAddress) ) {
+				found4 = true;
+			}
+		}
+		ZAssert.assertTrue(found1, "Verify account1 (to) is correctly used");
+		ZAssert.assertTrue(found2, "Verify account2 (to) is correctly used");
+		ZAssert.assertTrue(found3, "Verify account3 (cc) is correctly used");
+		ZAssert.assertTrue(found4, "Verify account4 (cc) is correctly used");
 		
 	}
 
+
+	@Test(	description = "Create a mail with two 'to' adn two 'cc' addresses (comma separated) using the deep link URL",
+			groups = { "functional" })
+	public void CreateMail_04() throws HarnessException {
+		
+		
+		
+		//-- DATA
+		
+		
+		// Create the message data to be sent
+		final ZimbraAccount account1 = (new ZimbraAccount()).provision().authenticate();
+		final ZimbraAccount account2 = (new ZimbraAccount()).provision().authenticate();
+		final ZimbraAccount account3 = (new ZimbraAccount()).provision().authenticate();
+		final ZimbraAccount account4 = (new ZimbraAccount()).provision().authenticate();
+		final String subject = "subject" + ZimbraSeleniumProperties.getUniqueString();
+		final String body = "body" + ZimbraSeleniumProperties.getUniqueString();
+		
+		
+		
+		
+		//-- GUI
+		
+		// The account is already authenticated
+		// However, we need to change the URL and open
+		// the deep link form.
+		
+		ZimbraURI uri = new ZimbraURI(ZimbraURI.getCurrentURI());
+		uri.addQuery("view", "compose");	// Setting view=compose will make PageMain return a FormMailNew object
+		uri.addQuery("to", account1.EmailAddress +","+ account2.EmailAddress);
+		uri.addQuery("cc", account3.EmailAddress +","+ account4.EmailAddress);
+		uri.addQuery("subject", subject);
+		uri.addQuery("body", body);
+
+		// Open the Deep Link URL: http://server.com/?view=compose&to=addy&subject=text&body=value
+		//
+		FormMailNew mailform = (FormMailNew)app.zPageMain.zOpenDeeplink(uri);
+		ZAssert.assertNotNull(mailform, "Verify the deeplink page opens");
+		ZAssert.assertTrue(mailform.zIsActive(), "Verify the deeplink page opens");
+		
+		// The form should be filled out, so just submit
+		mailform.zSubmit();
+		
+		
+		//-- VERIFICATION
+		MailItem sent = MailItem.importFromSOAP(app.zGetActiveAccount(), "subject:("+ subject +")");
+		ZAssert.assertNotNull(sent, "Verify the message is received correctly");
+		
+		boolean found1 = false;
+		boolean found2 = false;
+		boolean found3 = false;
+		boolean found4 = false;
+		for ( RecipientItem r : sent.dToRecipients) {
+			if ( r.dEmailAddress.equals(account1.EmailAddress) ) {
+				found1 = true;
+			}
+			if ( r.dEmailAddress.equals(account2.EmailAddress) ) {
+				found2 = true;
+			}
+		}
+		for ( RecipientItem r : sent.dCcRecipients) {
+			if ( r.dEmailAddress.equals(account3.EmailAddress) ) {
+				found3 = true;
+			}
+			if ( r.dEmailAddress.equals(account4.EmailAddress) ) {
+				found4 = true;
+			}
+		}
+		ZAssert.assertTrue(found1, "Verify account1 (to) is correctly used");
+		ZAssert.assertTrue(found2, "Verify account2 (to) is correctly used");
+		ZAssert.assertTrue(found3, "Verify account3 (cc) is correctly used");
+		ZAssert.assertTrue(found4, "Verify account4 (cc) is correctly used");
+		
+	}
 
 
 
