c8c868dc499d6b3e04b95c2e89680fd27c761545
Quanah Gibson-Mount <quanah@zimbra.com>
2013-08-20 05:18:46 +0000

    bug: none license update

--- ZimbraAdminExt/ProxyConfig/js/com_zimbra_proxy_config.js
+++ ZimbraAdminExt/ProxyConfig/js/com_zimbra_proxy_config.js
@@ -1,3 +1,19 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * 
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2012, 2013 Zimbra Software, LLC.
+ * 
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.4 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * 
+ * ***** END LICENSE BLOCK *****
+ */
 /**
  * This zimlet will add proxy config features to Zimbra Admin Console.
  * It push additional XForm items and XModel items into the XForm Object
--- ZimbraServer/src/libexec/libreoffice-installer.sh
+++ ZimbraServer/src/libexec/libreoffice-installer.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #
 # ***** BEGIN LICENSE BLOCK *****
-# Copyright (C) 2012, 2013 VMware, Inc.  All Rights Reserved.
-# This product is protected by copyright and intellectual property laws in the
-# United States and other countries as well as by international treaties.
-# VMware products are covered by one or more patents listed at http://www.vmware.com/go/patents.
+# Zimbra Collaboration Suite Server
+# Copyright (C) 2013 Zimbra Software, LLC.
+# 
+# The contents of this file are subject to the Zimbra Public License
+# Version 1.4 ("License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://www.zimbra.com/license.
+# 
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
 # ***** END LICENSE BLOCK *****
 #
 #
