9a0edd8134cd8b2c510df592eb7bb59303d407b7
Quanah Gibson-Mount <quanah@zimbra.com>
2013-08-27 22:18:55 +0000

    bug: 83739 Update copyright, 1.2.0
    http://bugzilla.zimbra.com/show_bug.cgi?id=83739

--- ThirdParty/nginx/nginx-1.2.0-zimbra/src/core/ngx_memcache.c
+++ ThirdParty/nginx/nginx-1.2.0-zimbra/src/core/ngx_memcache.c
@@ -1,8 +1,16 @@
 /*
- * Copyright (c) VMware, Inc. [1998 – 2011]. All Rights Reserved.
+ * ***** BEGIN LICENSE BLOCK *****
+ * Zimbra Collaboration Suite Server
+ * Copyright (C) 2011 Zimbra Software, LLC.
  *
- * For more information, see –
- * http://vmweb.vmware.com/legal/corporate/VMwareCopyrightPatentandTrademarkNotices.pdf
+ * The contents of this file are subject to the Zimbra Public License
+ * Version 1.4 ("License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://www.zimbra.com/license.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * ***** END LICENSE BLOCK *****
  */
 
 #include <ngx_memcache.h>
