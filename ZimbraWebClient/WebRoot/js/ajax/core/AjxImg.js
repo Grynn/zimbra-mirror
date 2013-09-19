8ee3f966c0bf31c128787ff9946bf7a698aedcfe
Dan Christiansen (c) <dchristiansen@zimbra.com>
2013-09-19 09:10:58 +0000

    bug 83171 - restore missing images in IE8
    http://bugzilla.zimbra.com/show_bug.cgi?id=83171

--- ZimbraWebClient/WebRoot/js/ajax/core/AjxImg.js
+++ ZimbraWebClient/WebRoot/js/ajax/core/AjxImg.js
@@ -187,7 +187,7 @@ function(imageName, styles, attrStr, wrapInTable, disabled, classes) {
                 var filter = 'filter:mask(color=' + color + ');';
                 html = [
                     // NOTE: Keep in sync with output of ImageMerger.java.
-                    "<div class='IEImage' style='*display:inline;zoom:1;position:relative;overflow:hidden;", size, styles, "' ", attrStr,">",
+                    "<div class='IEImage' style='display:inline;zoom:1;position:relative;overflow:hidden;", size, styles, "' ", attrStr,">",
                         "<div class='IEImageMask' style='overflow:hidden;position:relative;", size, "'>",
                             "<img src='", mask.f, "?v=", window.cacheKillerVersion, "' border=0 style='position:absolute;", location, clip, filter, "'>",
                         "</div>",
