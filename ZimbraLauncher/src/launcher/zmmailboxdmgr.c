24428be68281bb7c670847ef4099e2338eff0d78
Jeff Flanigan <jflanigan@zimbra.com>
2014-02-03 19:47:09 +0000

    bug: 86355
    Remove etc/jetty.properties from Jetty command line. Properties are specified in modules.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=86355

--- ZimbraLauncher/src/launcher/zmmailboxdmgr.c
+++ ZimbraLauncher/src/launcher/zmmailboxdmgr.c
@@ -566,7 +566,6 @@ Start(int nextArg, int argc, char *argv[])
     AddArgFmt("%s/start.jar", MAILBOXD_HOME);
     AddArg("--module=zimbra,server,servlet,servlets,jsp,jmx,resources,websocket,ext,plus,rewrite,monitor,continuation,setuid");
     AddArgFmt("jetty.home=%s", MAILBOXD_HOME);
-    AddArgFmt("%s/etc/jetty.properties", MAILBOXD_HOME);
     AddArgFmt("%s/etc/jetty.xml", MAILBOXD_HOME);
 
     if (Verbose) {
