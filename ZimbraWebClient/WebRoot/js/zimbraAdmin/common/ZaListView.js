96d9c224c281a18cf84e5beb4b708b2ae0606d8f
Dan Christiansen (c) <dchristiansen@zimbra.com>
2014-03-13 10:28:48 +0000

    bug 83327 - admin console: retain list elements when scrolling
    Due to a minor copy-pasto, ZimbraAdmin list views re-rendered the
    entirety of the list on each fetch. One area where this was
    particularly obvious was when list accounts as a delegated admin, as
    the list view performed a GetEffectiveRightRequest for all rendered
    accounts when scrolling. As a result, scrolling through hundreds of
    accounts was somewhat unpleasant.
    
    The nature of the bug was rather simple: ZaListView refered to its own
    list -- an AjxVector managed by DwtListView -- rather than the
    intended ZaItemList. The fix is to obtain the correct object from the
    controller.
    
    http://bugzilla.zimbra.com/show_bug.cgi?id=83327

--- ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaListView.js
+++ ZimbraWebClient/WebRoot/js/zimbraAdmin/common/ZaListView.js
@@ -414,7 +414,8 @@ function(preParams, paramsArr) {
                 hasmore= resp.more|hasmore;
 			}
 		        if(ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] != 'TRUE') {
-		                var act = new AjxTimedAction(this._list, ZaItemList.prototype.loadEffectiveRights, null);
+		                var list = this.scrollSearchParams.controller.getList();
+		                var act = new AjxTimedAction(list, list.loadEffectiveRights, null);
 		                AjxTimedAction.scheduleAction(act, 150)
 		        }
 
@@ -514,7 +515,8 @@ function(params, resp) {
                     tempResultList = tempList;
                 }
 				if(ZaZimbraAdmin.currentAdminAccount.attrs[ZaAccount.A_zimbraIsAdminAccount] != 'TRUE') {
-					var act = new AjxTimedAction(this._list, ZaItemList.prototype.loadEffectiveRights, null);
+					var list = this.scrollSearchParams.controller.getList();
+					var act = new AjxTimedAction(list, list.loadEffectiveRights, null);
 					AjxTimedAction.scheduleAction(act, 150)
 				}
                  hasmore= response.more;
